f00a0633d32d1c3eeff387fff613fd8fc2e3b923
kejace <kejace@gmail.com>
2017-01-05 15:20:28 +0000

    Nicer formatting

--- strato-api/Handler/TransactionInfo.hs
+++ strato-api/Handler/TransactionInfo.hs
@@ -65,7 +65,7 @@ postTransactionR = do
           emitKafkaTransactions [tx']
           case h of
             (String h') -> do
-              $logDebug $ "Successfully inserted tx: " Import.++ h'
+              $logDebug $ "Successfully inserted tx: " Import.++ (format h')
               sendResponseStatus status200 (h' :: Text)
             _ -> invalidArgs ["invalid transaction hash"]   
        _ -> invalidArgs ["couldn't decode transaction"]
