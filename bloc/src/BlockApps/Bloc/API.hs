cbcad598383927a745677ad9577aa7643490d867
Eitan Chatav <eitan.chatav@gmail.com>
2017-01-10 10:48:17 -0500

    bloc API

--- bloc/src/BlockApps/Bloc/API.hs
+++ bloc/src/BlockApps/Bloc/API.hs
@@ -1,1 +1,56 @@
-module BlockApps.Bloc.API where
+{-# LANGUAGE
+    DataKinds
+  , TypeOperators
+#-}
+
+module BlockApps.Bloc.API (API) where
+
+import Data.Aeson
+import Data.Text
+import Servant.API
+
+import BlockApps.Strato.Types
+
+type API =
+  "users"
+    :> Get '[JSON] [Value]
+  :<|> "users"
+    :> ReqBody '[JSON] Value
+    :> Post '[JSON] Value
+  :<|> "users"
+    :> Capture "user" Text
+    :> Get '[JSON] [Value]
+  :<|> "users"
+    :> Capture "user" Text
+    :> Capture "address" Address
+    :> "send"
+    :> ReqBody '[JSON] Value
+    :> Post '[JSON] Value
+  :<|> "contract"
+    :> Capture "contractName" Text
+    :> Get '[JSON] [Value]
+  :<|> "users"
+    :> Capture "user" Text
+    :> Capture "address" Address
+    :> "contract"
+    :> ReqBody '[JSON] Value
+    :> Post '[JSON] Value
+  :<|> "contracts"
+    :> Capture "contractName" Text
+    :> Capture "contractAddress" Address
+    :> Get '[JSON] Value
+  :<|> "contracts"
+    :> Capture "contractName" Text
+    :> Capture "contractAddress" Address
+    :> "state"
+    :> Get '[JSON] Value -- change to HTML
+  :<|> "users"
+    :> Capture "user" Text
+    :> Capture "userAddress" Address
+    :> "contract"
+    :> Capture "contractName" Text
+    :> Capture "contractAddress" Address
+    :> "call"
+    :> Post '[JSON] NoContent
+  :<|> "addresses"
+    :> Get '[JSON] [Value]
--- bloc/src/BlockApps/Bloc/API/Client.hs
+++ bloc/src/BlockApps/Bloc/API/Client.hs
@@ -1,1 +1,50 @@
-module BlockApps.Bloc.API.Client where
+{-# LANGUAGE
+    TypeApplications
+#-}
+
+module BlockApps.Bloc.API.Client
+  ( getUsers
+  , postUsers
+  , getUserAddresses
+  , postSend
+  , getContracts
+  , postContract
+  , getContract
+  , getContractState
+  , postContractMethod
+  , getAddresses
+  , blocDev
+  ) where
+
+import Data.Aeson
+import Data.Proxy
+import Data.Text
+import Servant.API
+import Servant.Client
+
+import BlockApps.Bloc.API
+import BlockApps.Strato.Types
+
+getUsers :: ClientM [Value]
+postUsers :: Value -> ClientM Value
+getUserAddresses :: Text -> ClientM [Value]
+postSend :: Text -> Address -> Value -> ClientM Value
+getContracts :: Text -> ClientM [Value]
+postContract :: Text -> Address -> Value -> ClientM Value
+getContract :: Text -> Address -> ClientM Value
+getContractState :: Text -> Address -> ClientM Value
+postContractMethod :: Text -> Address -> Text -> Address -> ClientM NoContent
+getAddresses :: ClientM [Value]
+getUsers
+  :<|> postUsers
+  :<|> getUserAddresses
+  :<|> postSend
+  :<|> getContracts
+  :<|> postContract
+  :<|> getContract
+  :<|> getContractState
+  :<|> postContractMethod
+  :<|> getAddresses = client (Proxy @ API)
+
+blocDev :: BaseUrl
+blocDev = BaseUrl Http "tester12.westus.cloudapp.azure.com/" 80 "/bloc"
