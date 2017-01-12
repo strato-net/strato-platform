e41a70002b946fd4a053a0c13a886e9ad412ec0c
Ilya Ostrovskiy <ilya@blockapps.net>
2017-01-12 17:45:39 -0500

    ACTUALLY wait for kafka, and zookeeper

--- pkg/doit.sh
+++ pkg/doit.sh
@@ -127,9 +127,15 @@ then echo "$genesisBlock" > ${genesis}Genesis.json
 fi
 
 until nc -z zookeeper 2181 >&/dev/null
+do  echo "Waiting for Zookeeper to become available"
+    sleep 1
+done
+
+until nc -z kafka 9092 >&/dev/null
 do  echo "Waiting for Kafka to become available"
     sleep 1
 done
+
 
 global-db --pghost postgres || { echo "Ignoring."; true; } # If it fails, it just means we already created the global db
 newnode
