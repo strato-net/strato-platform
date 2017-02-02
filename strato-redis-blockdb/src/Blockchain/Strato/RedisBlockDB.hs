70deb065fb4930953f47fd3e92983bc5cdd11f51
Ilya Ostrovskiy <ilya@blockapps.net>
2017-02-02 13:39:17 -0500

    formatting and chain helpers

--- strato-redis-blockdb/src/Blockchain/Strato/RedisBlockDB.hs
+++ strato-redis-blockdb/src/Blockchain/Strato/RedisBlockDB.hs
@@ -5,13 +5,14 @@
 {-# LANGUAGE ScopedTypeVariables    #-}
 {-# OPTIONS -fno-warn-redundant-constraints #-}
 module Blockchain.Strato.RedisBlockDB
-    ( getHeader, getHeaders
-    , getSHAsForNumber
+    ( getSHAsByNumber
+    , getHeader, getHeaders, getHeadersByNumber, getHeadersByNumbers
+    , getBlock,  getBlocks,  getBlocksByNumber,  getBlocksByNumbers
     , getTransactions, getUncles
-    , getBlock, getBlocks
-    , getBlocksByNumber, getBlocksByNumbers
-    , getParent, getChildren
-    , putHeader, putBlock
+    , getParent, getParents
+    , getParentChain, getHeaderChain, getBlockChain
+    , getChildren
+    , putHeader, putHeaders, putBlock, putBlocks
     , HasRedisBlockDB(..), withRedisBlockDB
     ) where
 
@@ -20,12 +21,15 @@ import           Blockchain.Strato.Model.SHA
 import           Blockchain.Strato.RedisBlockDB.Models as Models
 
 import qualified Data.ByteString.Char8                 as S8
-import           Data.Maybe                            (fromJust, isNothing)
+import           Data.Maybe                            (fromJust, isJust, isNothing)
 import           Control.Monad
 import           Control.Monad.Trans
 import           Database.Redis
 
-zipM' :: (Traversable t, Monad m) => (a -> m b) -> t a -> m (t (a, b))
+zipM' :: (Traversable t, Monad m)
+      => (a -> m b)
+      -> t a
+      -> m (t (a, b))
 zipM' f = mapM (\x -> (,) x <$> f x)
 
 -- zipA' :: (Traversable t, Applicative a) => (x -> a b) -> t x -> a (t (x, y))
@@ -36,12 +40,17 @@ zipM' f = mapM (\x -> (,) x <$> f x)
 class (Monad m) => HasRedisBlockDB m where
     getRedisBlockDB :: m Connection
 
-withRedisBlockDB :: (MonadIO m, HasRedisBlockDB m) => Redis a -> m a
+withRedisBlockDB :: (MonadIO m, HasRedisBlockDB m)
+                 => Redis a
+                 -> m a
 withRedisBlockDB m = do
     db <- getRedisBlockDB
     liftIO $ runRedis db m
 
-inNamespace :: RedisDBKeyable k => BlockDBNamespace -> k -> S8.ByteString
+inNamespace :: RedisDBKeyable k
+            => BlockDBNamespace
+            -> k
+            -> S8.ByteString
 inNamespace ns k = ns' `S8.append` toKey k
     where ns' = case ns of
             Headers      -> "h:"
@@ -51,64 +60,115 @@ inNamespace ns k = ns' `S8.append` toKey k
             Parent       -> "p:"
             Children     -> "c:"
 
-getInNamespace :: BlockDBNamespace -> SHA -> Redis (Either Reply (Maybe S8.ByteString))
+getInNamespace :: BlockDBNamespace
+               -> SHA
+               -> Redis (Either Reply (Maybe S8.ByteString))
 getInNamespace ns sha = get $ inNamespace ns sha
 
-getMembersInNamespace :: (RedisDBKeyable key) => BlockDBNamespace -> key -> Redis (Either Reply [S8.ByteString])
+getMembersInNamespace :: (RedisDBKeyable key)
+                      => BlockDBNamespace
+                      -> key
+                      -> Redis (Either Reply [S8.ByteString])
 getMembersInNamespace ns = smembers . inNamespace ns
 
-getHeader :: BlockHeaderLike h => SHA -> Redis (Maybe h)
-getHeader sha = getInNamespace Headers sha >>= \case
-        Left _             -> return Nothing
-        Right Nothing      -> return Nothing
-        Right (Just rhead) -> let (RedisHeader h) = fromValue rhead in
-            return . Just $ morphBlockHeader h
+getSHAsByNumber :: Integer
+                -> Redis (Maybe [SHA])
+getSHAsByNumber n = getMembersInNamespace Numbers n >>= \case
+    Left _             -> return Nothing
+    Right hs           -> let hashes = fromValue <$> hs in
+        return (Just hashes)
 
-getHeaders :: BlockHeaderLike h => [SHA] -> Redis [(SHA, Maybe h)]
+getHeader :: BlockHeaderLike h
+          => SHA
+          -> Redis (Maybe h)
+getHeader sha = getInNamespace Headers sha >>= \case
+    Left _             -> return Nothing
+    Right Nothing      -> return Nothing
+    Right (Just rhead) -> let (RedisHeader h) = fromValue rhead in
+        return . Just $ morphBlockHeader h
+
+getHeaders :: BlockHeaderLike h
+           => [SHA]
+           -> Redis [(SHA, Maybe h)]
 getHeaders = zipM' getHeader
 
-getSHAsForNumber :: Integer -> Redis (Maybe [SHA])
-getSHAsForNumber n = getMembersInNamespace Numbers n >>= \case
-        Left _             -> return Nothing
-        Right hs           -> let hashes = fromValue <$> hs in
-            return (Just hashes)
+getHeadersByNumber :: BlockHeaderLike h
+                   => Integer
+                   -> Redis [(SHA, Maybe h)]
+getHeadersByNumber n = getMembersInNamespace Numbers n >>= \case
+    Left _       -> return []
+    Right hashes -> getHeaders (fromValue <$> hashes)
 
-getTransactions :: TransactionLike t => SHA -> Redis (Maybe [t])
+getHeadersByNumbers :: BlockHeaderLike h
+                    => [Integer]
+                    -> Redis [(Integer, [(SHA, Maybe h)])]
+getHeadersByNumbers = zipM' getHeadersByNumber
+
+getTransactions :: TransactionLike t
+                => SHA
+                -> Redis (Maybe [t])
 getTransactions sha = getInNamespace Transactions sha >>= \case
-        Left _             -> return Nothing
-        Right Nothing      -> return Nothing
-        Right (Just rtxs)  -> let (RedisTxs txs) = fromValue rtxs in
-            return . Just $ morphTx <$> txs
+    Left _             -> return Nothing
+    Right Nothing      -> return Nothing
+    Right (Just rtxs)  -> let (RedisTxs txs) = fromValue rtxs in
+        return . Just $ morphTx <$> txs
 
-getUncles :: BlockHeaderLike h => SHA -> Redis (Maybe [h])
+getUncles :: BlockHeaderLike h
+          => SHA
+          -> Redis (Maybe [h])
 getUncles sha = getInNamespace Uncles sha >>= \case
-        Left _             -> return Nothing
-        Right Nothing      -> return Nothing
-        Right (Just rus)   -> let (RedisUncles uncles) = fromValue rus in
-            return . Just $ morphBlockHeader <$> uncles
+    Left _             -> return Nothing
+    Right Nothing      -> return Nothing
+    Right (Just rus)   -> let (RedisUncles uncles) = fromValue rus in
+        return . Just $ morphBlockHeader <$> uncles
 
-getParent :: SHA -> Redis (Maybe SHA)
+getParent :: SHA
+          -> Redis (Maybe SHA)
 getParent sha = getInNamespace Parent sha >>= \case
-        Left _             -> return Nothing
-        Right Nothing      -> return Nothing
-        Right (Just rps)   -> let parent = fromValue rps in
-            return . Just $ parent
+    Left _             -> return Nothing
+    Right Nothing      -> return Nothing
+    Right (Just rps)   -> return . Just $ fromValue rps
 
-getChildren :: SHA -> Redis (Maybe [SHA])
+getParents :: (Traversable f)
+           => f SHA
+           -> Redis (f (SHA, Maybe SHA))
+getParents = zipM' getParent
+
+getParentChain :: SHA
+               -> Int
+               -> Redis [SHA]
+getParentChain start limit = (start:) <$> helper start limit
+    where helper h l | l <= 0    = return []
+                     | otherwise = getParent h >>= maybe (return []) chainDown
+          chainDown parent = (parent:) <$> helper parent (limit - 1)
+
+getZippedParentChain :: (SHA -> Redis (Maybe t)) -> SHA -> Int -> Redis [(SHA, Maybe t)]
+getZippedParentChain mapper start limit = do
+    shaChain <- getParentChain start limit
+    mapChain <- zipM' mapper shaChain
+    return $ takeWhile (isJust . snd) mapChain
+
+getHeaderChain :: (BlockHeaderLike h)
+               => SHA
+               -> Int
+               -> Redis [(SHA, Maybe h)]
+getHeaderChain = getZippedParentChain getHeader
+
+getBlockChain :: (BlockLike h t b)
+              => SHA
+              -> Int
+              -> Redis [(SHA, Maybe b)]
+getBlockChain = getZippedParentChain getBlock
+
+getChildren :: SHA
+            -> Redis (Maybe [SHA])
 getChildren sha = getMembersInNamespace Children sha >>= \case
-        Left _             -> return Nothing
-        Right chs          -> let children = fromValue <$> chs in
-            return (Just children)
+    Left _             -> return Nothing
+    Right chs          -> return . Just $ fromValue <$> chs
 
-getBlocksByNumber :: (BlockLike h t b) => Integer -> Redis [(SHA, Maybe b)]
-getBlocksByNumber n = getMembersInNamespace Numbers n >>= \case
-        Left _       -> return []
-        Right hashes -> getBlocks (fromValue <$> hashes)
-
-getBlocksByNumbers :: (BlockLike h t b) => [Integer] -> Redis [(Integer, [(SHA, Maybe b)])]
-getBlocksByNumbers = zipM' getBlocksByNumber
-
-getBlock :: BlockLike h t b => SHA -> Redis (Maybe b)
+getBlock :: BlockLike h t b
+         => SHA
+         -> Redis (Maybe b)
 getBlock sha = do
     mybHeader <- getHeader sha
     if isNothing mybHeader
@@ -126,23 +186,50 @@ getBlock sha = do
                      uncles = fromJust mybUncles
                  in return . Just $ buildBlock header txs uncles
 
-getBlocks :: BlockLike h t b => [SHA] -> Redis [(SHA, Maybe b)]
+getBlocks :: BlockLike h t b
+          => [SHA]
+          -> Redis [(SHA, Maybe b)]
 getBlocks = zipM' getBlock
 
-putHeader :: (BlockHeaderLike h) => h -> Redis (Either Reply Status)
+getBlocksByNumber :: (BlockLike h t b)
+                  => Integer
+                  -> Redis [(SHA, Maybe b)]
+getBlocksByNumber n = getMembersInNamespace Numbers n >>= \case
+    Left _       -> return []
+    Right hashes -> getBlocks (fromValue <$> hashes)
+
+getBlocksByNumbers :: (BlockLike h t b)
+                   => [Integer]
+                   -> Redis [(Integer, [(SHA, Maybe b)])]
+getBlocksByNumbers = zipM' getBlocksByNumber
+
+putHeader :: (BlockHeaderLike h)
+          => h
+          -> Redis (Either Reply Status)
 putHeader h = do
     let sha       = blockHeaderHash h
+        parent    = blockHeaderParentHash h
         number    = blockHeaderBlockNumber h
         storeHead = morphBlockHeader h :: RedisHeader
+        inNS'     = flip inNamespace sha
     res <- multiExec $ do
-        void $ setnx (inNamespace Headers sha) (toValue storeHead)
+        void $ setnx (inNS' Headers) (toValue storeHead)
+        void $ setnx (inNS' Parent) (toValue parent)
+        void $ sadd (inNamespace Children parent) [toValue sha]
         sadd (inNamespace Numbers number) [toValue sha]
     case res of
         TxSuccess _ -> pure $ Right Ok
         TxAborted   -> pure . Left $ SingleLine (S8.pack "Aborted")  
-        TxError e   -> pure . Left $ SingleLine (S8.pack e) 
+        TxError e   -> pure . Left $ SingleLine (S8.pack e)
 
-putBlock :: (BlockLike h t b, BlockHeaderLike h, TransactionLike t) => b -> Redis (Either Reply Status)
+putHeaders :: (Traversable f, BlockHeaderLike h)
+           => f h
+           -> Redis (f (Either Reply Status))
+putHeaders = mapM putHeader
+
+putBlock :: (BlockLike h t b, BlockHeaderLike h, TransactionLike t)
+         => b
+         -> Redis (Either Reply Status)
 putBlock b = do
     let sha     = blockHash b
         header  = blockHeader b
@@ -164,3 +251,8 @@ putBlock b = do
         TxSuccess _ -> pure $ Right Ok
         TxAborted   -> pure . Left $ SingleLine (S8.pack "Aborted")  
         TxError e   -> pure . Left $ SingleLine (S8.pack e) 
+
+putBlocks :: (Traversable f, BlockLike h t b, BlockHeaderLike h, TransactionLike t)
+          => f b
+          -> Redis (f (Either Reply Status))
+putBlocks = mapM putBlock
