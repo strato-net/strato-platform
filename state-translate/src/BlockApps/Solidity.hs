45469835a7c6e2a42099daa728d5b84d39f0e4e2
Eitan Chatav <eitan.chatav@gmail.com>
2017-02-13 08:11:16 -0800

    test

--- state-translate/src/BlockApps/Solidity.hs
+++ state-translate/src/BlockApps/Solidity.hs
@@ -11,9 +11,11 @@ import qualified Data.ByteString.Lazy as ByteString.Lazy
 import Data.Binary
 import Data.LargeWord
 import Data.Text (Text)
+import qualified Data.Text as Text
+import qualified Data.Text.Encoding as Text
 import Numeric.Natural
 
-import BlockApps.Data (Address)
+import BlockApps.Data (Address(..))
 
 data Type
   = TypeBool
@@ -25,6 +27,7 @@ data Type
   -- ^ int<M>: integer type of M bits,
   -- 0 < M <= 256, M % 8 == 0. e.g. int32, int8, int256.
   -- int: synonyms for int256
+  | TypeAddress
   | TypeFixed (Maybe (Int,Int))
   -- ^ fixed<M>x<N>: fixed-point signed number of M+N bits,
   -- 0 < M + N <= 256, M % 8 == N % 8 == 0.
@@ -57,6 +60,7 @@ data Value
   | ValueUFixed Double
   | ValueBytes ByteString
   | ValueString Text
+  | ValueFunction ByteString ByteString
   deriving (Eq,Show)
 
 decodeValue
@@ -65,17 +69,20 @@ decodeValue
   -> Type
   -> Value
 decodeValue storage offset = \case
-  TypeBool -> ValueBool (ByteString.index bytes 31 /= 0)
-    where
-      bytes = slice 32 storage
+  TypeBool -> ValueBool (ByteString.index (slice 32) 31 /= 0)
   TypeUInt (Just n) ->
-    ValueUInt . sum $ zipWith shiftL bytes [8*(n-1),8*(n-2)..0]
+    ValueUInt . sum $ zipWith shiftL bytes [n-8,n-16..0]
     where
-      bytes = map fromIntegral . ByteString.unpack $ slice n storage
+      bytes
+        = map fromIntegral
+        . ByteString.unpack
+        . ByteString.take (n `div` 8)
+        . ByteString.drop (32 - ((n `div` 8) `mod` 32))
+        $ ByteString.drop offset storage
   TypeUInt Nothing -> decodeValue storage offset (TypeUInt (Just 256))
   TypeInt (Just n) ->
     let
-      Just (byte,bytes) = ByteString.uncons $ slice n storage
+      Just (byte,bytes) = ByteString.uncons $ slice n
       (sign, significant) =
         if byte == 0xff
           then (negate, ByteString.dropWhile (==0xff) bytes)
@@ -86,16 +93,34 @@ decodeValue storage offset = \case
       ValueInt . sign . sum $
         zipWith shiftL significant' [8*(m-1),8*(m-2)..0]
   TypeInt Nothing -> decodeValue storage offset (TypeInt (Just 256))
-  TypeFixed (Just (n,m)) -> undefined
-  TypeFixed Nothing -> undefined
-  TypeBytes (Just n) -> undefined
-  TypeString -> undefined
+  TypeAddress ->
+    let
+      ValueUInt addr = decodeValue storage offset (TypeUInt (Just 160))
+    in
+      ValueAddress . Address $ fromIntegral addr
+  TypeFixed (Just (n,m)) ->
+    let
+      ValueInt x = decodeValue storage offset (TypeInt (Just 256))
+    in
+      ValueFixed $ fromIntegral x / 2 ** fromIntegral n
+  TypeFixed Nothing -> decodeValue storage offset (TypeFixed (Just (128,128)))
+  TypeBytes (Just n) -> ValueBytes $ slice n
+  TypeBytes Nothing ->
+    let
+      ValueUInt len = decodeValue storage offset (TypeUInt (Just 256))
+      padding = len `mod` 32
+    in
+      ValueBytes . ByteString.take (fromIntegral (padding + len)) $
+        ByteString.drop offset storage
+  TypeString ->
+    let
+      ValueBytes bytes = decodeValue storage offset (TypeBytes Nothing)
+    in
+      ValueString $ Text.decodeUtf8 bytes
   TypeFunction tys -> undefined
   TypeArray ty (Just n) -> undefined
   TypeArray ty Nothing -> undefined
   TypeMapping tyk tyv -> undefined
   where
-    decodeStrict :: Binary x => ByteString -> x
-    decodeStrict = decode . ByteString.Lazy.fromStrict
-    slice :: Int -> ByteString -> ByteString
-    slice len = ByteString.take len . ByteString.drop offset
+    slice :: Int -> ByteString
+    slice len = ByteString.take len $ ByteString.drop offset storage
--- state-translate/test/Tasty.hs
+++ state-translate/test/Tasty.hs
@@ -11,27 +11,32 @@ import SimpleDeclarations
 import PrimitiveState
 import ComplexState
 
-main = defaultMain tests
+import Test.HSpec
+import BlockApps.SoliditySpec
+
+main = do
+  hspec spec
+  defaultMain tests
 
 tests :: TestTree
 tests = testGroup "tests" [ parseTests
-                          , stateTests 
+                          , stateTests
                           ]
 
-parseTests = testGroup "parse tests" 
+parseTests = testGroup "parse tests"
   [
     test_simple_decl,
-    test_array_decl, 
+    test_array_decl,
     test_func_decl,
     test_struct_decl,
     test_syntax_fails,
     test_mapping_decl,
     test_struct_decl
-  ] 
-  
+  ]
+
 
 stateTests = testGroup "state tests"
-  [ 
+  [
     test_primitive_state
   , test_complex_state
   ]
