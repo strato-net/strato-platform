ad17692f2c546ebfbd7ec8c8f41ff0b2cf09b0c6
Kieren James-Lubin <kjameslubin@Kierens-MacBook-Pro.local>
2017-02-28 10:59:01 -0500

    updates to strato-model for testability

---
+++ strato-model/src/Blockchain/Strato/Model/BlockchainClass.hs
@@ -0,0 +1,13 @@
+
+module Blockchain.Strato.Model.BlockchainClass where
+
+import Blockchain.Strato.Model.Class
+import Blockchain.Strato.Model.Query
+
+class Monad m => Blockchain m  where
+  getBlocks :: BlockLike h t b => BlockQuery -> m [b]
+  getHeaders :: BlockHeaderLike h => HeaderQuery -> m [h] 
+  putBlocks :: (Traversable f, BlockLike h t b, BlockHeaderLike h, TransactionLike t) => f b -> m (f (Either l r)) 
+  putHeaders :: (Traversable f, BlockHeaderLike h) => f h -> m (f (Either l r ) ) 
+
+
