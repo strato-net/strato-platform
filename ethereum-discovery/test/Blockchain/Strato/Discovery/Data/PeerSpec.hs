ffb6ba493eb7ca326a557bc2221cdee8c87ee997
Todd Mohney <toddmohney@gmail.com>
2017-03-07 14:50:46 -0500

    Add test harness to ethereum-discovery

---
+++ ethereum-discovery/test/Blockchain/Strato/Discovery/Data/PeerSpec.hs
@@ -0,0 +1,9 @@
+module Blockchain.Strato.Discovery.Data.PeerSpec where
+
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "testing" $ do
+    it "can test stuff" $ do
+      True `shouldBe` True
