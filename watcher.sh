395d2d85e2882d0fbc52115d1ed23ab91b423d61
kejace <kejace@gmail.com>
2017-03-08 17:16:00 +0000

    Updated .tmuxinator

--- watcher.sh
+++ watcher.sh
@@ -12,15 +12,15 @@ function cirrus-count {
 }
 
 function explorer-blocktimes {
-  curl -s localhost/strato-api/eth/v1.2/block/last/100| jq -C '.[] | {blockData}' | grep 'timestamp' | cut -d : -f2,3,4 | cut -d , -f1 | xargs -L1 -I x date -d x +'%s' | awk '{$2 = $1 - prev1; prev1 = $1; print;}' | cut -d ' ' -f2 | tail -n +2 | spark | awk -F":" '{ print $1 "\tblocktimes"}'
+  curl -s localhost/strato-api/eth/v1.2/block/last/100| jq '.[] | {blockData}' | grep 'timestamp' | cut -d : -f2,3,4 | cut -d , -f1 | xargs -L1 -I x date -d x +'%s' | awk '{$2 = $1 - prev1; prev1 = $1; print;}' | cut -d ' ' -f2 | tail -n +2 | spark | awk -F":" '{ print $1 "\tblocktimes"}'
 }
 
 function explorer-numtxs {
-  curl -s localhost/strato-api/eth/v1.2/block/last/100 | jq -C '.[] | [.receiptTransactions | length] ' | grep -o '[0-9]*' | spark | awk -F":" '{ print $1 "\tnumber of transactions per block"}'
+  curl -s localhost/strato-api/eth/v1.2/block/last/100 | jq '.[] | [.receiptTransactions | length] ' | grep -o '[0-9]*' | spark | awk -F":" '{ print $1 "\tnumber of transactions per block"}'
 }
 
 function explorer-numuncles {
-  curl -s localhost/strato-api/eth/v1.2/block/last/100 | jq -C '.[] | [.blockUncles | length] ' | grep -o '[0-9]*' | spark | awk -F":" '{ print $1 "\tnumber of uncles per block"}'
+  curl -s localhost/strato-api/eth/v1.2/block/last/100 | jq '.[] | [.blockUncles | length] ' | grep -o '[0-9]*' | spark | awk -F":" '{ print $1 "\tnumber of uncles per block"}'
 }
 
 case $1 in
