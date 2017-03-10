b03f65b49ca2eeb1214cc3050f5f294fd096f781
Jamshid <qq@qq.com>
2017-03-10 10:45:56 -0800

    Adding version pragma support.

---
+++ solidity-abi/parse/Pragma.hs
@@ -0,0 +1,20 @@
+
+module Pragma where
+
+import Data.Maybe
+import Text.Parsec
+
+import Lexer
+import ParserTypes
+
+--Pragma param is currently just a generic string of chars up to the ';'
+--see https://github.com/ethereum/solidity/blob/develop/docs/grammar.txt for confirmation
+data Pragma = Pragma String
+
+pragma::SolidityParser Pragma
+pragma = do
+ string "pragma"
+ many1 $ char ' '
+ version <- many1 $ noneOf ";"
+ char ';'
+ return $ Pragma version
