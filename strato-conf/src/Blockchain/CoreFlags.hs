49176e92f0a2f61e685482e51d30644cc133f5de
Martin Allen <martyall@Martins-MacBook-Air.local>
2017-03-11 14:53:24 -0500

    add hack to use flags in both vm and quarry

--- strato-conf/src/Blockchain/CoreFlags.hs
+++ strato-conf/src/Blockchain/CoreFlags.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Blockchain.CoreFlags where
 
 import HFlags
+
+{-# RULES "make_this_orphan" id = id :: MakeThisOrphan -> MakeThisOrphan #-}
 
 defineFlag "difficultyBomb" False "turn difficulty bomb on or off"
 defineFlag "testnet" False "connect to testnet"
