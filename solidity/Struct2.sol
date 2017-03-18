02ce336ba55682304f6303877ba6d076a87a3aa3
Jamshid <qq@qq.com>
2017-03-18 01:25:18 -0400

    Adding a second Struct contract, without an enum, since enums currently crash old bloc.

---
+++ solidity/Struct2.sol
@@ -0,0 +1,17 @@
+
+contract Struct2 {
+
+  int x = 10;
+
+  struct Pet {
+    string animal;
+    string name;
+    int8 age;
+    bool fleasAndTicks;
+    }
+
+  Pet sammy = Pet("dog", "Sammy", 4, false);
+
+
+}
+  
