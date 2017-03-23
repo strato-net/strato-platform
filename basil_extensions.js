216ff090b52a01f6505896791a98cc10c626d0b3
kejace <kejace@gmail.com>
2017-03-23 23:07:45 +0000

    Cleanup

--- basil_extensions.js
+++ basil_extensions.js
@@ -18,26 +18,16 @@ util.RegisterCommand({
   var isRelease = c.String("release") != ""
   var stratoDeployment = _.first(_.filter(basilfile.Deployments, function(p) { return p.Name == "strato"; }))
 
-  //var newStrato = api.CloneDeployment(stratoDeployment)
-  //newStrato.ComposeArgs["environment"]["addBootnodes"] = 21123
-
-  //console.log("Creating multinode docker-compose.yml with " + flagVal + " nodes.")
-
   _.map(_.range(1, flagVal), function (i){
     //  console.log("Configuring node " + i)
       var stratoPeer = api.CloneDeployment(stratoDeployment)
       stratoPeer.ComposeArgs["environment"]["bootnode"] = stratoDeployment.Name;
-      stratoPeer.ComposeArgs["environment"]["useSyncMode"] = "true"
-      
-      var portIndex = stratoPeer.ComposeArgs["ports"].length;
-      var oldPort = parseInt(stratoPeer.ComposeArgs["ports"][portIndex - 1].split(':')[0])
-      stratoPeer.ComposeArgs["ports"][portIndex - 1] = (oldPort + i) + ":3000" 
+      stratoPeer.ComposeArgs["environment"]["useSyncMode"] = "true";
+      stratoPeer.ComoiseArgs["environment"]["redisBDBNumber"] = i;      
 
       stratoPeer.Name = "strato-"+i;
       api.SetDeployment(stratoPeer)
   })
-
-  //console.log("Creating multinode .tmuxinator with " + flagVal + " nodes.")
   
   console.log(api.DoCompose(isRelease))
 })
