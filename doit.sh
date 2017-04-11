c85e24936edaf46bb820eba1b9cd7b5e7480d4f0
Kishore Bhatia <bhatia.kishore@gmail.com>
2017-04-11 21:33:51 +0000

    fixing docker entrypoints

--- doit.sh
+++ doit.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-stratoHost=${stratoHost:-$(curl -s ident.me)}
-canonicalHost=$(getent hosts $stratoHost | tr -s ' ' | cut -d ' ' -f 2)
-if [[ $stratoHost == "0.0.0.0" || $canonicalHost == "localhost" ]]
-then stratoHost="strato:3000"
-fi
-
 cd /usr/bin/bloc/
 blocserver="/usr/bin/bloc/blockapps-bloc"
-apiUrl=${apiUrlOverride:-"http$(${ssl:-false} && echo "s")://$stratoHost/strato-api"}
-sed -i "s|^apiURL: .*\$|apiURL: '$apiUrl'|" config.yaml
-HOST=0.0.0.0 exec $blocserver start
+#HOST=0.0.0.0 exec $blocserver -P $pghost -u $pguser -p $pgpasswd --stratourl=$stratoHost/strato-api/eth/v1.2 --cirrusurl=$cirrusurl 
+HOST=0.0.0.0 exec $blocserver -papi --stratourl=${stratourl}/strato-api/eth/v1.2
