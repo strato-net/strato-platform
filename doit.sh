277780b09099da5fbf7c5171a09fda3e9c2dd4e2
Kishore Bhatia <bhatia.kishore@gmail.com>
2017-04-12 04:16:08 +0000

    fixed server launch args

--- doit.sh
+++ doit.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
 #cd /usr/bin/bloc/
+function setEnv {
+  [[ -n ${!1} ]] || eval $1=$2
+  echo "$1 = ${!1}"
+}
+echo "Environment variables:"
+
+setEnv pguser postgres
+setEnv pgpasswd api
+setEnv pghost postgres
+setEnv stratourl http://localhost 
+setEnv cirrusurl http://localhost/cirrus 
+
 blocserver="/usr/bin/blockapps-bloc"
 locale-gen "en_US.UTF-8"
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
-exec $blocserver -P $pghost -u $pguser -p $pgpasswd --stratourl=$stratourl/strato-api/eth/v1.2 --cirrusurl=$cirrusurl 
+echo "Executing this: $blocserver --pghost="$pghost" --pguser="$pguser" --password="$pgpasswd" --stratourl="$stratourl/strato-api/eth/v1.2" --cirrusurl="$cirrusurl""
+exec $blocserver --pghost="$pghost" --pguser="$pguser" --password="$pgpasswd" --stratourl="$stratourl/strato-api/eth/v1.2" --cirrusurl="$cirrusurl" 
