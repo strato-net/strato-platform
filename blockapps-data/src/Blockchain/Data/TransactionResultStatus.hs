81f127b94bfdd36bfba4917760505cf5d2a679e8
Ilya Ostrovskiy <ilya@blockapps.net>
2017-04-12 16:47:27 -0400

    transaction result status in tx results

--- blockapps-data/src/Blockchain/Data/TransactionResultStatus.hs
+++ blockapps-data/src/Blockchain/Data/TransactionResultStatus.hs
@@ -1,23 +1,53 @@
-{-# LANGUAGE DeriveGeneric   #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE TemplateHaskell   #-}
 module Blockchain.Data.TransactionResultStatus where
 
-import           Data.Aeson
-import           Database.Persist
-import           Database.Persist.Quasi
+import           Data.Aeson          hiding (Success)
+import           Data.Maybe          (maybe)
 import           Database.Persist.TH
-
 import           GHC.Generics
 
 data TransactionResultStatus = Success
                              | Failure { trfStage       :: String
-                                       , trfQueue       :: String
-                                       , trfCulprit     :: String
+                                       , trfQueue       :: Maybe String
+                                       , trfType        :: TransactionFailureType
                                        , trfExpectation :: Maybe Integer
                                        , trfReality     :: Maybe Integer
+                                       , trfDetails     :: Maybe String
                                        }
                              deriving (Eq, Read, Show, Generic)
 
+data TransactionFailureType = IncorrectNonce
+                            | InsufficientFunds
+                            | IntrinsicGasExceedsLimit
+                            | TrumpedByMoreLucrative
+                            | ExecutionFailure
+                            deriving (Eq, Read, Show, Generic)
+
 derivePersistField "TransactionResultStatus"
-instance FromJSON TransactionResultStatus
-instance ToJSON   TransactionResultStatus
+derivePersistField "TransactionFailureType"
+
+instance FromJSON TransactionResultStatus where
+    parseJSON (String "success") = pure Success
+    parseJSON x = flip (withObject "Failure") x $ \v -> Failure
+        <$> v .:  "stage"
+        <*> v .:? "queue"
+        <*> v .:  "type"
+        <*> v .:? "expectation"
+        <*> v .:? "reality"
+        <*> v .:? "details"
+
+instance ToJSON   TransactionResultStatus where
+    toJSON Success     = String "success"
+    toJSON Failure{..} = object $ [ "stage" .= trfStage
+                                  , "type"  .= trfType
+                                  ]
+                                ++ maybe [] (pure . ("queue" .=)) trfQueue
+                                ++ maybe [] (pure . ("expectation" .=)) trfExpectation
+                                ++ maybe [] (pure . ("reality" .=))     trfReality
+                                ++ maybe [] (pure . ("details" .=))     trfDetails
+
+instance FromJSON TransactionFailureType
+instance ToJSON   TransactionFailureType
