285d47cc351931680af1a1fad011cc6b946e77a4
Jamshid <qq@qq.com>
2017-04-13 19:29:29 -0400

    Removing more obsolete stuff.

--- solidity/src/BlockApps/Solidity/Parse/ParserTypes.hs
+++ solidity/src/BlockApps/Solidity/Parse/ParserTypes.hs
@@ -7,10 +7,6 @@ module BlockApps.Solidity.Parse.ParserTypes where
 
 import Text.Parsec
 
-import Numeric.Natural
-
-import qualified BlockApps.Solidity.Xabi.Type as Xabitype
-
 -- | Source file names; also source file /paths/.
 type FileName = SourceName
 -- | Names of types, variables, functions, etc. in Solidity code.
@@ -33,38 +29,6 @@ setContractName = setState
 -- constructors.
 getContractName :: SolidityParser ContractName
 getContractName = getState
-
--- | What can appear as the type of a variable.
-data SolidityBasicType =
-  -- | 'bool' type
-  Boolean |
-  -- | 'address' type
-  Address |
-  -- | 'intX' type, where X is converted from bits to bytes for consistency
-  -- Note that 'int == int256 == SignedInt 32'
-  SignedInt   { bytes :: Natural } |
-  -- | 'uintX' type, in bytes
-  -- Note that 'uint = uint256 == UnsignedInt 32'
-  UnsignedInt { bytes :: Natural } |
-  -- | 'bytesX' type.  Natively given in bytes.
-  -- Note that 'byte = bytes1 = FixedBytes 1'
-  FixedBytes  { bytes :: Natural } |
-  -- | 'bytes'.  The length is not known at compile time and the storage
-  -- location of the data is not computed here.
-  DynamicBytes|
-  -- | 'string'.  Same as above.
-  String |
-  -- | 'T[n]' array type, recording both 'T' and 'n'.  Can be nested; i.e.
-  -- 'T[n][m] == FixedArray (FixedArray T n) m'.  Note the order.
-  FixedArray  { elemType :: SolidityBasicType, fixedLength :: Natural } |
-  -- | 'T[]', recording only 'T'.  Can be nested.
-  DynamicArray{ elemType :: SolidityBasicType } |
-  -- | 'mapping(D => C)' type, recording domain 'D' and codomain 'C'.  We
-  -- don't enforce restrictions on what these types allow.
-  Mapping     { domType  :: Xabitype.IndexedType, codType :: Xabitype.IndexedType } |
-  -- | The name of a new type, whose definition is kept in 'contractTypes'.
-  Typedef     { typedefName :: Identifier }
-  deriving (Show, Eq)
 
 -- | Not actually used.
 type SolidityValue = String
