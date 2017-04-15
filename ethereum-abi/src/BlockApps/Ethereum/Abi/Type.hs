457e9fa8f239b0275110002d3f8cb11261fae05e
Eitan Chatav <eitan.chatav@gmail.com>
2017-04-15 13:37:42 -0700

    simplify ethereum abi

--- ethereum-abi/src/BlockApps/Ethereum/Abi/Type.hs
+++ ethereum-abi/src/BlockApps/Ethereum/Abi/Type.hs
@@ -4,240 +4,68 @@
 
 module BlockApps.Ethereum.Abi.Type
   ( Type(..)
-  , TypeStatic(..)
-  , TypeDynamic(..)
+  , validType
   , typeIsDynamic
-  , typeStaticByteSize
-  , typeStaticBitSize
+  , typeByteSize
+  , typeBitSize
   ) where
+
+import Data.Maybe
 
 import BlockApps.Ethereum.Abi.Int
 
 data Type
-  = TypeStatic TypeStatic
-  | TypeDynamic TypeDynamic
-  deriving (Eq,Show)
-
-typeIsDynamic :: Type -> Bool
-typeIsDynamic = \case
-  TypeStatic _ -> False
-  TypeDynamic _ -> True
-
-data TypeStatic
   = TypeBool
-  | TypeUInt8
-  | TypeUInt16
-  | TypeUInt24
-  | TypeUInt32
-  | TypeUInt40
-  | TypeUInt48
-  | TypeUInt56
-  | TypeUInt64
-  | TypeUInt72
-  | TypeUInt80
-  | TypeUInt88
-  | TypeUInt96
-  | TypeUInt104
-  | TypeUInt112
-  | TypeUInt120
-  | TypeUInt128
-  | TypeUInt136
-  | TypeUInt144
-  | TypeUInt152
-  | TypeUInt160
-  | TypeUInt168
-  | TypeUInt176
-  | TypeUInt184
-  | TypeUInt192
-  | TypeUInt200
-  | TypeUInt208
-  | TypeUInt216
-  | TypeUInt224
-  | TypeUInt232
-  | TypeUInt240
-  | TypeUInt248
-  | TypeUInt256
-  | TypeUInt
-  | TypeInt8
-  | TypeInt16
-  | TypeInt24
-  | TypeInt32
-  | TypeInt40
-  | TypeInt48
-  | TypeInt56
-  | TypeInt64
-  | TypeInt72
-  | TypeInt80
-  | TypeInt88
-  | TypeInt96
-  | TypeInt104
-  | TypeInt112
-  | TypeInt120
-  | TypeInt128
-  | TypeInt136
-  | TypeInt144
-  | TypeInt152
-  | TypeInt160
-  | TypeInt168
-  | TypeInt176
-  | TypeInt184
-  | TypeInt192
-  | TypeInt200
-  | TypeInt208
-  | TypeInt216
-  | TypeInt224
-  | TypeInt232
-  | TypeInt240
-  | TypeInt248
-  | TypeInt256
-  | TypeInt
+  | TypeUInt (Maybe Word256)
+  | TypeInt (Maybe Word256)
   | TypeAddress
   -- | TypeFixed
   -- | TypeUFixed
-  | TypeBytes1
-  | TypeBytes2
-  | TypeBytes3
-  | TypeBytes4
-  | TypeBytes5
-  | TypeBytes6
-  | TypeBytes7
-  | TypeBytes8
-  | TypeBytes9
-  | TypeBytes10
-  | TypeBytes11
-  | TypeBytes12
-  | TypeBytes13
-  | TypeBytes14
-  | TypeBytes15
-  | TypeBytes16
-  | TypeBytes17
-  | TypeBytes18
-  | TypeBytes19
-  | TypeBytes20
-  | TypeBytes21
-  | TypeBytes22
-  | TypeBytes23
-  | TypeBytes24
-  | TypeBytes25
-  | TypeBytes26
-  | TypeBytes27
-  | TypeBytes28
-  | TypeBytes29
-  | TypeBytes30
-  | TypeBytes31
-  | TypeBytes32
-  | TypeArrayStatic Word256 TypeStatic
-  deriving (Eq,Show)
-
-typeStaticByteSize :: TypeStatic -> Word256
-typeStaticByteSize = \case
-  TypeBool -> 1
-  TypeUInt8 -> 1
-  TypeUInt16 -> 2
-  TypeUInt24 -> 3
-  TypeUInt32 -> 4
-  TypeUInt40 -> 5
-  TypeUInt48 -> 6
-  TypeUInt56 -> 7
-  TypeUInt64 -> 8
-  TypeUInt72 -> 9
-  TypeUInt80 -> 10
-  TypeUInt88 -> 11
-  TypeUInt96 -> 12
-  TypeUInt104 -> 13
-  TypeUInt112 -> 14
-  TypeUInt120 -> 15
-  TypeUInt128 -> 16
-  TypeUInt136 -> 17
-  TypeUInt144 -> 18
-  TypeUInt152 -> 19
-  TypeUInt160 -> 20
-  TypeUInt168 -> 21
-  TypeUInt176 -> 22
-  TypeUInt184 -> 23
-  TypeUInt192 -> 24
-  TypeUInt200 -> 25
-  TypeUInt208 -> 26
-  TypeUInt216 -> 27
-  TypeUInt224 -> 28
-  TypeUInt232 -> 29
-  TypeUInt240 -> 30
-  TypeUInt248 -> 31
-  TypeUInt256 -> 32
-  TypeUInt -> 32
-  TypeInt8 -> 1
-  TypeInt16 -> 2
-  TypeInt24 -> 3
-  TypeInt32 -> 4
-  TypeInt40 -> 5
-  TypeInt48 -> 6
-  TypeInt56 -> 7
-  TypeInt64 -> 8
-  TypeInt72 -> 9
-  TypeInt80 -> 10
-  TypeInt88 -> 11
-  TypeInt96 -> 12
-  TypeInt104 -> 13
-  TypeInt112 -> 14
-  TypeInt120 -> 15
-  TypeInt128 -> 16
-  TypeInt136 -> 17
-  TypeInt144 -> 18
-  TypeInt152 -> 19
-  TypeInt160 -> 20
-  TypeInt168 -> 21
-  TypeInt176 -> 22
-  TypeInt184 -> 23
-  TypeInt192 -> 24
-  TypeInt200 -> 25
-  TypeInt208 -> 26
-  TypeInt216 -> 27
-  TypeInt224 -> 28
-  TypeInt232 -> 29
-  TypeInt240 -> 30
-  TypeInt248 -> 31
-  TypeInt256 -> 32
-  TypeInt -> 32
-  TypeAddress -> 20
-  TypeBytes1 -> 1
-  TypeBytes2 -> 2
-  TypeBytes3 -> 3
-  TypeBytes4 -> 4
-  TypeBytes5 -> 5
-  TypeBytes6 -> 6
-  TypeBytes7 -> 7
-  TypeBytes8 -> 8
-  TypeBytes9 -> 9
-  TypeBytes10 -> 10
-  TypeBytes11 -> 11
-  TypeBytes12 -> 12
-  TypeBytes13 -> 13
-  TypeBytes14 -> 14
-  TypeBytes15 -> 15
-  TypeBytes16 -> 16
-  TypeBytes17 -> 17
-  TypeBytes18 -> 18
-  TypeBytes19 -> 19
-  TypeBytes20 -> 20
-  TypeBytes21 -> 21
-  TypeBytes22 -> 22
-  TypeBytes23 -> 23
-  TypeBytes24 -> 24
-  TypeBytes25 -> 25
-  TypeBytes26 -> 26
-  TypeBytes27 -> 27
-  TypeBytes28 -> 28
-  TypeBytes29 -> 29
-  TypeBytes30 -> 30
-  TypeBytes31 -> 31
-  TypeBytes32 -> 32
-  TypeArrayStatic len ty -> len * typeStaticByteSize ty
-
-typeStaticBitSize :: TypeStatic -> Word256
-typeStaticBitSize = (* 8) . typeStaticByteSize
-
-data TypeDynamic
-  = TypeBytes
+  | TypeBytesStatic Word256
+  | TypeBytesDynamic
   | TypeString
-  | TypeArrayDynamic TypeStatic
+  | TypeArrayStatic Word256 Type
+  | TypeArrayDynamic Type
   deriving (Eq,Show)
+
+validType :: Type -> Bool
+validType = \case
+  TypeBool -> True
+  TypeUInt Nothing -> True
+  TypeUInt (Just n) -> n `mod` 8 == 0 && 8 <= n && n <= 256
+  TypeInt Nothing -> True
+  TypeInt (Just n) -> n `mod` 8 == 0 && 8 <= n && n <= 256
+  TypeAddress -> True
+  TypeBytesStatic n -> 1 <= n && n <= 32
+  TypeBytesDynamic -> True
+  TypeString -> True
+  TypeArrayStatic _ ty -> validType ty
+  TypeArrayDynamic ty -> validType ty
+
+typeIsDynamic :: Type -> Bool
+typeIsDynamic = \case
+  TypeBool -> False
+  TypeUInt _ -> False
+  TypeInt _ -> False
+  TypeAddress -> False
+  TypeBytesStatic _ -> False
+  TypeBytesDynamic -> True
+  TypeString -> True
+  TypeArrayStatic n ty -> n /= 0 && typeIsDynamic ty
+  TypeArrayDynamic _ -> True
+
+typeByteSize :: Type -> Maybe Word256
+typeByteSize = fmap (`div` 8) . typeBitSize
+
+typeBitSize :: Type -> Maybe Word256
+typeBitSize = \case
+  TypeBool -> return 8
+  TypeUInt n -> return $ fromMaybe 256 n
+  TypeInt n -> return $ fromMaybe 256 n
+  TypeAddress -> return 160
+  TypeBytesStatic n -> return $ 8 * n
+  TypeBytesDynamic -> Nothing
+  TypeString -> Nothing
+  TypeArrayStatic len ty ->
+    if len == 0 then return 0 else (len *) <$> typeBitSize ty
+  TypeArrayDynamic _ -> Nothing
--- ethereum-abi/test/BlockApps/Ethereum/Abi/ValueSpec.hs
+++ ethereum-abi/test/BlockApps/Ethereum/Abi/ValueSpec.hs
@@ -19,49 +19,49 @@ spec =
       context "official Ethereum ABI Tests: found at https://github.com/ethereum/tests/blob/develop/ABITests/basic_abi_tests.json" $ do
 
         it "should convert 4 args with types: uint256, uint32[], bytes10, and bytes" $ encodeDecodeValues
-          [ ValueStatic $ ValueUInt256 291
-            , ValueDynamic $ ValueArrayDynamic
-              [ ValueUInt32 1110
-              , ValueUInt32 1929
+          [ ValueUInt 291
+            , ValueArrayDynamic
+              [ ValueUInt 1110
+              , ValueUInt 1929
               ]
-            , ValueStatic $ ValueBytes10 "1234567890"
-            , ValueDynamic $ ValueBytes "Hello, world!"
+            , ValueBytesStatic "1234567890"
+            , ValueBytesDynamic "Hello, world!"
             ]
-          [ TypeStatic TypeUInt256
-          , TypeDynamic $ TypeArrayDynamic TypeUInt32
-          , TypeStatic TypeBytes10
-          , TypeDynamic TypeBytes
+          [ TypeUInt (Just 256)
+          , TypeArrayDynamic (TypeUInt (Just 32))
+          , TypeBytesStatic 10
+          , TypeBytesDynamic
           ]
           "00000000000000000000000000000000000000000000000000000000000001230000000000000000000000000000000000000000000000000000000000000080313233343536373839300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000e0000000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000004560000000000000000000000000000000000000000000000000000000000000789000000000000000000000000000000000000000000000000000000000000000d48656c6c6f2c20776f726c642100000000000000000000000000000000000000"
 
         it "should convert 1 arg with type uint256" $ encodeDecodeValues
-          [ ValueStatic $ ValueUInt256 98127491 ]
-          [ TypeStatic TypeUInt256 ]
+          [ ValueUInt 98127491 ]
+          [ TypeUInt (Just 256) ]
           "0000000000000000000000000000000000000000000000000000000005d94e83"
 
         it "should convert 2 arg with type uint256, address" $ encodeDecodeValues
-          [ ValueStatic $ ValueUInt256 324124
-          , ValueStatic $ ValueAddress $
+          [ ValueUInt 324124
+          , ValueAddress $
               Address 0xcd2a3d9f938e13cd947ec05abc7fe734df8dd826
           ]
-          [ TypeStatic TypeUInt256
-          , TypeStatic TypeAddress
+          [ TypeUInt (Just 256)
+          , TypeAddress
           ]
           "000000000000000000000000000000000000000000000000000000000004f21c000000000000000000000000cd2a3d9f938e13cd947ec05abc7fe734df8dd826"
 
       it "should convert 1 arg with type uint" $ encodeDecodeValues
-        [ ValueStatic $ ValueUInt 3 ]
-        [ TypeStatic TypeUInt ]
+        [ ValueUInt 3 ]
+        [ TypeUInt Nothing ]
         "0000000000000000000000000000000000000000000000000000000000000003"
 
       it "should convert 1 arg with type int" $ encodeDecodeValues
-        [ ValueStatic $ ValueInt (-1) ]
-        [ TypeStatic TypeInt ]
+        [ ValueInt (-1) ]
+        [ TypeInt Nothing ]
         "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
 
       it "should convert 1 arg with type address" $ encodeDecodeValues
-        [ ValueStatic $ ValueAddress $ Address 0xdeadbeef ]
-        [ TypeStatic TypeAddress ]
+        [ ValueAddress $ Address 0xdeadbeef ]
+        [ TypeAddress ]
         "00000000000000000000000000000000000000000000000000000000deadbeef"
 
       it "should convert 1 arg with type bytes" $ do
@@ -70,8 +70,8 @@ spec =
           bytes16 = "adb591795f9e9047f9117163b83c2ebcd5edc4503644d59a98cf911aef0367f8adb591795f9e9047f9117163b83c2ebcd5edc4503644"
           (bytes,_) = Base16.decode bytes16
         encodeDecodeValues
-          [ ValueDynamic $ ValueBytes bytes ]
-          [ TypeDynamic TypeBytes ]
+          [ ValueBytesDynamic bytes ]
+          [ TypeBytesDynamic ]
           undefined
 
       it "should convert 1 arg with type bytes32" $ do
@@ -79,13 +79,13 @@ spec =
           bytes16 = "adb591795f9e9047f9117163b83c2ebcd5edc4503644d59a98cf911aef0367f8"
           (bytes,_) = Base16.decode bytes16
         encodeDecodeValues
-          [ ValueStatic $ ValueBytes32 bytes ]
-          [ TypeStatic TypeBytes32 ]
+          [ ValueBytesStatic bytes ]
+          [ TypeBytesStatic 32 ]
           bytes16
 
       it "should convert 1 arg with type bool" $ encodeDecodeValues
-        [ ValueStatic $ ValueBool True ]
-        [ TypeStatic TypeBool ]
+        [ ValueBool True ]
+        [ TypeBool ]
         "0000000000000000000000000000000000000000000000000000000000000001"
 
       it "should convert 1 arg with type string" $ do
@@ -96,24 +96,25 @@ spec =
             \lineup over the reef Mavericks. Taj Burrow crumbly lip flow carves top turn barreling sandbar.\
             \ Pose on the nose blonde rigs lip pumping good poked the nose, Snapper Rocks."
         encodeDecodeValues
-          [ ValueDynamic $ ValueString str ]
-          [ TypeDynamic TypeString ]
+          [ ValueString str ]
+          [ TypeString ]
           "0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000014a4f757420746865206261636b20506970652070756e7420636f6d626f20437261696720416e646572736f6e20696e6e65722062617220676c6173732066726573686965206169722064726f702e20436c61772068616e64732070756d70696e67206d616b652074686520706164646c65207370726179204f6365616e2042656163682073757266696e6720686f6c6c6f7720747572647320696e20746865206c696e657570206f766572207468652072656566204d6176657269636b732e2054616a20427572726f77206372756d626c79206c697020666c6f772063617276657320746f70207475726e2062617272656c696e672073616e646261722e20506f7365206f6e20746865206e6f736520626c6f6e64652072696773206c69702070756d70696e6720676f6f6420706f6b656420746865206e6f73652c20536e617070657220526f636b732e00000000000000000000000000000000000000000000"
 
       it "should convert 1 arg with type uint[]" $ encodeDecodeValues
-        [ ValueDynamic $ ValueArrayDynamic
+        [ ValueArrayDynamic
           [ ValueUInt i | i <- [1,2,3,4,4,5,66,75,754,98] ]
         ]
-        [ TypeDynamic $ TypeArrayDynamic TypeUInt ]
+        [ TypeArrayDynamic (TypeUInt Nothing) ]
         "0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000a0000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000030000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000000050000000000000000000000000000000000000000000000000000000000000042000000000000000000000000000000000000000000000000000000000000004b00000000000000000000000000000000000000000000000000000000000002f20000000000000000000000000000000000000000000000000000000000000062"
 
       it "should convert 1 arg with type uint[3]" $ encodeDecodeValues
-        [ ValueStatic $ ValueArrayStatic [ ValueUInt i | i <- [1,2,3] ] ]
-        [ TypeStatic $ TypeArrayStatic 3 TypeUInt ]
+        [ ValueArrayStatic [ ValueUInt i | i <- [1,2,3] ] ]
+        [ TypeArrayStatic 3 (TypeUInt Nothing) ]
         "000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000003"
 
 encodeDecodeValues :: [Value] -> [Type] -> ByteString -> Expectation
 encodeDecodeValues args argTypes bytes16 = do
+  zipWith validValue argTypes args `shouldSatisfy` and
   let (encoded,_) = Base16.decode bytes16
   encodeValues args `shouldBe` encoded
   decodeValues encoded argTypes `shouldBe` Just args
