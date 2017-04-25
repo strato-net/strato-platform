c5b8db740f68435222dbefaee1058456a733f81e
Jamshid <qq@qq.com>
2017-04-25 12:48:34 -0700

    Filling in more of the selector code.

--- solidity/test/BlockApps/Solidity/Parse/SelectorSpec.hs
+++ solidity/test/BlockApps/Solidity/Parse/SelectorSpec.hs
@@ -22,8 +22,39 @@ spec = do
     it "should generate a selector for a function with three varied intN arguments" $ do
       generateSelector [] "doit" (map SimpleType [TypeInt8, TypeInt48, TypeInt200]) "31f92024"
 
+    it "should generate a selector for a function with one uint argument" $ do
+      generateSelector [] "goForIt" [SimpleType TypeUInt] "e7b3ef24"
+    it "should generate a selector for a function with three varied uintN arguments" $ do
+      generateSelector [] "goForIt" (map SimpleType [TypeUInt8, TypeUInt48, TypeUInt200]) "e7b3ef24"
+      
+    it "should generate a selector for a function with one bytes argument" $ do
+      generateSelector [] "makeItHappen" [SimpleType TypeBytes] "3aae8c6c"
+    it "should generate a selector for a function with three varied bytesN arguments" $ do
+      generateSelector [] "makeItHappen" (map SimpleType [TypeBytes1, TypeBytes10, TypeBytes20]) "f719f0c1"
+      
+    it "should generate a selector for a function with one string argument" $ do
+      generateSelector [] "f" [SimpleType TypeString] "91e145ef"
+      
+    it "should generate a selector for a function with one fixed array argument" $ do
+      generateSelector [] "doit" [TypeArrayFixed 10 (SimpleType TypeInt)] "2e6938eb"
+    it "should generate a selector for a function with one dynamic array argument" $ do
+      generateSelector [] "doit" [TypeArrayDynamic (SimpleType TypeString)] "2a146d1b"
+    it "should generate a selector for a function with one mapping argument" $ do
+      generateSelector [] "doit" [TypeMapping TypeInt (SimpleType TypeString)] ""
+    it "should generate a selector for a function with one enum argument" $ do
+      generateSelector [("Pets", 4)] "doit" [TypeEnum "Pets"] "79197ca6"
+
 
 generateSelector :: [(Text, Int)]->Text->[Type]->ByteString -> Expectation
 generateSelector enumSizes name args expectedSelector = do
   let selector = deriveSelector enumSizes name args
-  (Base16.encode selector) `shouldSatisfy` (==expectedSelector)
+  Base16.encode selector `shouldBe` expectedSelector
+
+
+{-
+formatArg typesL (Mapping d c) =
+  text "mapping" <+> parens (pretty typesL d <+> text "=>" <+> pretty typesL c)
+formatArg typesL (Typedef name) = 
+  case typesL Map.! name of
+    EnumLayout s -> pretty typesL (UnsignedInt s)
+    _ -> text name -}
