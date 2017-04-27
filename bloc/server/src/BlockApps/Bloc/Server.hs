451902855afa9ef1e6f52ac42c6d70b706c6ec53
Eitan Chatav <eitan.chatav@gmail.com>
2017-04-27 13:39:48 -0400

    serve docs

--- bloc/server/src/BlockApps/Bloc/Server.hs
+++ bloc/server/src/BlockApps/Bloc/Server.hs
@@ -1,11 +1,17 @@
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE TypeFamilies     #-}
-{-# LANGUAGE TypeOperators    #-}
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications  #-}
+{-# LANGUAGE TypeFamilies      #-}
+{-# LANGUAGE TypeOperators     #-}
 
 module BlockApps.Bloc.Server where
 
+import           Control.Lens             ((&), (.~), (?~))
 import           Data.Proxy
+import           Data.Swagger
 import           Servant
+import           Servant.Swagger
+import           Servant.Swagger.UI
 
 import           BlockApps.Bloc.API
 import           BlockApps.Bloc.Monad
@@ -45,5 +51,20 @@ getHomepage = return whoWouldveThoughtThisIsActuallyTheHomepage
 serveBloc :: BlocEnv -> Server BlocAPI
 serveBloc env = enter (NT (enterBloc env)) bloc
 
+blocSwagger :: Swagger
+blocSwagger = toSwagger (Proxy @BlocAPI)
+    & info.title   .~ "Bloc API"
+    & info.version .~ "0.1"
+    & info.description ?~ "This is the API for the BlocH"
+    & host ?~ Host "localhost" (Just 8000)
+
+type BlocDocsAPI = SwaggerSchemaUI "swagger-ui" "swagger.json"
+
+serveBlocAndDocs
+  :: BlocEnv
+  -> Server (BlocAPI :<|> BlocDocsAPI)
+serveBlocAndDocs blocEnv = serveBloc blocEnv
+  :<|> swaggerSchemaUIServer blocSwagger
+
 appBloc :: BlocEnv -> Application
-appBloc = serve (Proxy @ BlocAPI) . serveBloc
+appBloc = serve (Proxy @ (BlocAPI :<|> BlocDocsAPI)) . serveBlocAndDocs
