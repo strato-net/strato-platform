bae3db7ef003a0545b74f6349e6965673a737772
Nikita Mendelbaum <mendelbaum_nm@bitworks-software.com>
2017-04-28 18:31:48 -0400

    docker-publish script updated

--- docker-publish-images.sh
+++ docker-publish-images.sh
@@ -3,12 +3,9 @@ project="blockapps-repo"
 tag="latest"
 src_dtr="registry-aws.blockapps.net:5010"
 target_dtr="registry-aws.blockapps.net:5000"
-sudo docker login -u blockapps -p blockAPPS123 $target_dtr
-#imageID='sudo docker images | grep $line | grep $tag | awk '{ print $3 }''
-while IFS='' read -r line || [[ -n "$line" ]]; do
-    echo "image name: $line"
-    #echo "docker pull $src_dtr/$line:$tag"
-    imageID=`sudo docker images | grep "$line " | grep $tag | awk '{ print $3 }'`
-    docker tag $imageID $target_dtr/$project/$line:latest
-    docker push $target_dtr/$project/$line:latest
-done < "$1"
+docker login -u blockapps -p blockAPPS123 $target_dtr
+basil targets | while read line ; do
+    imageID=`docker images | grep silo-$line | grep $tag | awk '{ print $3 }' | head -n 1`
+    docker tag $imageID $target_dtr/$project/silo-$line:latest
+    docker push $target_dtr/$project/silo-$line:latest
+done
