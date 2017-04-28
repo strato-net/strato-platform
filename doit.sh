3a2cdc257f80874f20f0820ad2443deba7e9da8d
Kishore Bhatia <bhatia.kishore@gmail.com>
2017-04-28 03:33:36 +0000

    don't hard-code stratourl here

--- doit.sh
+++ doit.sh
@@ -10,12 +10,12 @@ echo "Environment variables:"
 setEnv pguser postgres
 setEnv pgpasswd api
 setEnv pghost postgres
-setEnv stratourl http://localhost 
+#setEnv stratourl http://localhost 
 setEnv cirrusurl http://localhost/cirrus 
 
 blocserver="/usr/bin/blockapps-bloc"
 locale-gen "en_US.UTF-8"
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
-echo "Executing this: $blocserver --pghost="$pghost" --pguser="$pguser" --password="$pgpasswd" --stratourl="$stratourl/strato-api/eth/v1.2" --cirrusurl="$cirrusurl""
+echo "Executing this: $blocserver --pghost="$pghost" --pguser="$pguser" --password="$pgpasswd" --stratourl="http://$stratourl/strato-api/eth/v1.2" --cirrusurl="$cirrusurl""
 $blocserver --pghost="$pghost" --pguser="$pguser" --password="$pgpasswd" --stratourl="$stratourl/strato-api/eth/v1.2" --cirrusurl="$cirrusurl" 2>&1
