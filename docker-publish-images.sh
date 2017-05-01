3c049a35c2006cdd8fe35ec52977dbae8d475027
Nikita Mendelbaum <mendelbaum_nm@bitworks-software.com>
2017-05-01 17:03:14 -0400

    docker-publish script fixed

--- docker-publish-images.sh
+++ docker-publish-images.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 project="blockapps-repo"
 tag="latest"
-src_dtr="registry-aws.blockapps.net:5010"
+#src_dtr="registry-aws.blockapps.net:5010"
 target_dtr="registry-aws.blockapps.net:5000"
 docker login -u blockapps -p blockAPPS123 $target_dtr
 basil targets | while read line ; do
-    imageID=`docker images | grep silo-$line | grep $tag | awk '{ print $3 }' | head -n 1`
+    imageID=`docker images | grep "silo-$line " | grep $tag | awk '{ print $3 }' | head -n 1`
     docker tag $imageID $target_dtr/$project/silo-$line:latest
     docker push $target_dtr/$project/silo-$line:latest
 done
