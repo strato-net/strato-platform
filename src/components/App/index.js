ffef91566a7da44cf74e7fe0ad0fc11b48bd5abe
Samrit Sangal <samrit@samrit.io>
2017-05-02 15:46:11 -0400

    Theming and icons

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Toolbar from 'react-md/lib/Toolbars';
+import Button from 'react-md/lib/Buttons';
 import './App.css';
 
 class App extends Component {
   render() {
+    const nav = <Button key="nav" icon>menu</Button>;
+
     return (
-      <Toolbar themed title="Strato Management Dashboard" />
+      <Toolbar colored title="Strato Management Dashboard" nav={nav} />
     );
   }
 }
