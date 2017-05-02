38843af3ff488c1650e2a1dbbbe549cbef230b3c
Samrit Sangal <samrit@samrit.io>
2017-05-02 15:12:29 -0400

    react-md setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Toolbar from 'react-md/lib/Toolbars';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Toolbar themed title="Strato Management Dashboard" />
+    );
+  }
+}
+
+export default App;
