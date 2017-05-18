29f36eee2abbc189aa756ab338976ccde5d047e7
Kishore Bhatia <bhatia.kishore@gmail.com>
2017-05-18 00:42:38 +0000

    suppressed health-check for postgres

--- doit.sh
+++ doit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-until netcat -z ${PG_ENV_POSTGRES_HOST} ${PG_ENV_POSTGRES_PORT} >&/dev/null
-do echo "Waiting for postgres to start"
-   sleep 1
-done
+#until netcat -z ${PG_ENV_POSTGRES_HOST} ${PG_ENV_POSTGRES_PORT} >&/dev/null
+#do echo "Waiting for postgres to start"
+#   sleep 1
+#done
 
 while true
 do postgrest postgres://${PG_ENV_POSTGRES_USER}:${PG_ENV_POSTGRES_PASSWORD}@${PG_ENV_POSTGRES_HOST}:${PG_PORT_5432_TCP_PORT}/${PG_ENV_POSTGRES_DB} \
