ca307eceaac457f1ecf13123f88b5ba1a2c37bd0
Kristoffer Josefsson <kejace@gmail.com>
2017-05-23 18:08:31 +0000

    Undoing options to doit.sh

--- doit.sh
+++ doit.sh
@@ -14,15 +14,14 @@ setEnv pghost postgres
 setEnv stratoHost nginx
 setEnv stratourl http://$stratoHost/strato-api/eth/v1.2
 setEnv cirrusurl http://localhost/cirrus
-setEnv blochost  http://localhost
-setEnv blocport 8000
-setEnv blocpath '/.'
+#setEnv blochost http://localhost
+#seEnv blocport 8000
+#setEnv blocpath '/.'
 
 blocserver="/usr/bin/blockapps-bloc"
 locale-gen "en_US.UTF-8"
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 
-$blocserver --blochost="$blochost" --blocport="$blocport" --blocpath="$blocpath" \
-            --pghost="$pghost" --pguser="$pguser" --password="$pgpasswd" \
+$blocserver --pghost="$pghost" --pguser="$pguser" --password="$pgpasswd" \
             --stratourl="$stratourl" --cirrusurl="$cirrusurl" 2>&1
