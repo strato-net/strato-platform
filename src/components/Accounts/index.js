f6f4afdb855c28e2ee0ac7b850e8588457e96ca8
Ali Mousa <amousa11@gmail.com>
2017-05-24 13:11:40 -0700

    NumberCards on Account data

--- src/components/Accounts/index.js
+++ src/components/Accounts/index.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-import { Button, Table, Cell, Column } from '@blueprintjs/core';
+import React, {Component} from 'react';
+import {Button, Table, Cell, Column} from '@blueprintjs/core';
+import NumberCard from '../NumberCard';
 import 'normalize.css/normalize.css';
 import '@blueprintjs/core/dist/blueprint.css';
 import 'bootstrap/dist/css/bootstrap.css'
@@ -13,36 +14,36 @@ class Accounts extends Component {
                 "account": "0xC1f16c418B66A5aE47A5820e1fdb947A77f29531",
                 "username": "Alice",
                 "balance": "12",
-            },{
+            }, {
                 "account": "0xC1f16c418B66A5aE47A5820e1fdb947A77f29531",
                 "username": "Bob",
                 "balance": "23",
-            },{
+            }, {
                 "account": "0xC1f16c418B66A5aE47A5820e1fdb947A77f29531",
                 "username": "Charlie",
                 "balance": "34",
-            },{
+            }, {
                 "account": "0xC1f16c418B66A5aE47A5820e1fdb947A77f29531",
                 "username": "Desiree",
                 "balance": "45",
-            },{
+            }, {
                 "account": "0xC1f16c418B66A5aE47A5820e1fdb947A77f29531",
                 "username": "Edward",
                 "balance": "56",
             }];
 
         let tableRows = data.map(function (val, i) {
-           return <tr>
-               <td className="col-sm-4">
-               {val.account}
-               </td>
-               <td className="col-sm-4">
-                   {val.username}
-               </td>
-               <td className="col-sm-4">
-                   {val.balance}
-               </td>
-           </tr>
+            return <tr>
+                <td className="col-sm-4">
+                    {val.account}
+                </td>
+                <td className="col-sm-4">
+                    {val.username}
+                </td>
+                <td className="col-sm-4">
+                    {val.balance}
+                </td>
+            </tr>
         });
 
         console.log(tableRows);
@@ -50,13 +51,22 @@ class Accounts extends Component {
         return (
             <div className="container account-container pt-card pt-elevation-1">
                 <div className="row accounts-row justify-content-left">
-                    <div className="col-sm-12 justift-content-sm-left">
+                    <div className="col-sm">
                         <h2>Accounts Tab</h2>
                     </div>
                 </div>
                 <div className="row accounts-row">
-                    <div className="col-sm-4">
-                        <h4>6 Users</h4>
+                    <div className="col-sm-3">
+                        <NumberCard number={1230498} description="Ether"/>
+                    </div>
+                    <div className="col-sm-3">
+                        <NumberCard number={234241} description="TX Volume"/>
+                    </div>
+                    <div className="col-sm-3">
+                        <NumberCard number={245} description="Users"/>
+                    </div>
+                    <div className="col-sm-3">
+                        <NumberCard number={123456} description="Arbitrary User Metric"/>
                     </div>
                 </div>
                 <div className="row accounts-row">
@@ -69,13 +79,13 @@ class Accounts extends Component {
                     <div>
                         <table className="pt-table accounts-table">
                             <thead>
-                                    <th className="col-sm-4"><h4>Account</h4></th>
-                                    <th className="col-sm-4"><h4>Username</h4></th>
-                                    <th className="col-sm-4"><h4>Balance</h4></th>
+                            <th className="col-sm-4"><h4>Account</h4></th>
+                            <th className="col-sm-4"><h4>Username</h4></th>
+                            <th className="col-sm-4"><h4>Balance</h4></th>
                             </thead>
 
                             <tbody>
-                                {tableRows}
+                            {tableRows}
                             </tbody>
                         </table>
                     </div>
