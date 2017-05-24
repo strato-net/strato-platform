bf517b686f939a882a571c726e7e1259344fc2ba
Nikita Mendelbaum <mendelbaum_nm@bitworks-software.com>
2017-05-24 12:25:47 -0400

    compile error fix

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
-import Test from './components/Test';
 
 export const routes = (
   <Switch>
