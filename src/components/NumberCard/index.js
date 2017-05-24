51e4963da9abe52a5cc8788e4c90f9a6f419cf00
Ali Mousa <amousa11@gmail.com>
2017-05-24 13:09:10 -0700

    cleanup

--- src/components/NumberCard/index.js
+++ src/components/NumberCard/index.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-
+import 'bootstrap/dist/css/bootstrap.css'
 import './NumberCard.scss'
 
 class NumberCard extends Component {
   render() {
     return (
-      <div className="container pt-card pt-elevation-3">
-        <span className="difficulty"><h1>{this.props.number}</h1></span>
-        <span className="desc"><h5>{this.props.description}</h5></span>
+      <div className="col-sm pt-card pt-elevation-2">
+        <div className="text-right difficulty"><h1>{this.props.number}</h1></div>
+        <div className="text-right desc"><h5>{this.props.description}</h5></div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,19 +3,20 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-import { Provider } from 'react-redux';
-import routes from './routes';
-import { BrowserRouter as Router } from 'react-router-dom'
+import {Provider} from 'react-redux';
+import {BrowserRouter as Router} from 'react-router-dom'
 import {
-  createStore,
-  applyMiddleware,
-  combineReducers
+    createStore,
+    applyMiddleware,
+    combineReducers,
 } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-
-import { routerReducer } from 'react-router-redux';
-import { reducer as formReducer } from 'redux-form';
+import { fork } from 'redux-saga/effects';
+import {routerReducer} from 'react-router-redux';
+import {reducer as formReducer} from 'redux-form';
 import {reducer as burgerMenu} from 'redux-burger-menu';
+
+import App from "./App/App.js";
 
 
 import difficultyReducer from './components/Difficulty/difficulty.reducer'
@@ -25,40 +26,40 @@ import watchFetchDifficulty from './components/Difficulty/difficulty.saga'
 import watchFetchTx from './components/Transactions/transactions.saga'
 
 const rootReducer = combineReducers({
-  form: formReducer,
-  routing: routerReducer,
-  // YOUR REDUCERS HERE
-  difficulty: difficultyReducer,
-  transactions: transactionsReducer,
-  burgerMenu
+    form: formReducer,
+    routing: routerReducer,
+    // YOUR REDUCERS HERE
+    difficulty: difficultyReducer,
+    transactions: transactionsReducer,
+    burgerMenu
 });
 
 const rootSaga = function* startForeman() {
-  yield [
-    // YOUR SAGAS HERE
-    fork(watchFetchDifficulty),
-    fork(watchFetchTx),
-  ]
+    yield [
+        // YOUR SAGAS HERE
+        fork(watchFetchDifficulty),
+        fork(watchFetchTx),
+    ]
 };
 
 // create the saga middleware
 const sagaMiddleware = createSagaMiddleware();
 // mount it on the Store
 const store = createStore(
-  rootReducer,
-  applyMiddleware(sagaMiddleware),
-  //window.devToolsExtension ? window.devToolsExtension() : f => f,
+    rootReducer,
+    applyMiddleware(sagaMiddleware),
+    //window.devToolsExtension ? window.devToolsExtension() : f => f,
 );
 
 // then run the saga
 sagaMiddleware.run(rootSaga)
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>,
+    document.getElementById('root')
 );
 registerServiceWorker();
