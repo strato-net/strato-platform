b7f2cfeeeabb2af1dcd0efc3b884cec924da7d6e
Samrit Sangal <samrit@samrit.io>
2017-06-14 17:17:44 -0400

    Deployment changes to support env variables

--- docker-run.sh
+++ docker-run.sh
@@ -3,6 +3,10 @@ set -e
 set -x
 
 sed -i "s|__NODE_NAME__|$NODE_NAME|g" build/index.html
-sed -i "s|__NODE_URL__|$NODE_URL|g" build/index.html
+sed -i "s|__BLOC_URL__|$BLOC_URL|g" build/index.html
+sed -i "s|__STRATO_URL__|$STRATO_URL|g" build/index.html
+sed -i "s|__STRATO_DOC_URL__|$STRATO_DOC_URL|g" build/index.html
+sed -i "s|__BLOC_DOC_URL__|$BLOC_DOC_URL|g" build/index.html
+
 
 serve --port 3002 build
