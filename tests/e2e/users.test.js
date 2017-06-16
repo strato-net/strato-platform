6f2aceed3f57381f0736c53c50d8918deeb3b6ed
Samrit Sangal <samrit@samrit.io>
2017-06-16 16:01:18 -0400

    user.test.js testing

--- tests/e2e/users.test.js
+++ tests/e2e/users.test.js
@@ -9,12 +9,14 @@ const assert = common.assert;
 
 describe('User Test',function() {
   this.timeout(config.timeout);
+
   const scope = {}
 
   const username = util.uid('TEST');
   const password = '1234';
   const expectedBalance = new BigNumber(1000).times(constants.ETHER);
-  it('should create and faucet a user acc', function(done){
+
+  it('should create and faucet a user account', function(done){
     rest
       .setScope(scope)
       .then(rest.createUser(username, password))
@@ -25,9 +27,11 @@ describe('User Test',function() {
         const address = scope.users[username].address;
         assert.isOk(util.isAddress(address), 'the address should be valid');
         const balance = new BigNumber(scope.accounts[address][0].balance);
-        assert.isOk(balance.equal(expectedBalance), 'the balance should be equal to 1000 ether');
+        assert.isOk(balance.equals(expectedBalance), 'the balance should be equal to 1000 ether');
         done();
       })
       .catch(done);
-  })
+  });
+
+
 })
