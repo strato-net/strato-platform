444ab9a8916c28109adf4b307f09554da61b804f
Ali Mousa <amousa11@gmail.com>
2017-06-22 14:55:01 -0700

    test address with cleaned string representation

--- tests/e2e/dataTypes/address.test.js
+++ tests/e2e/dataTypes/address.test.js
@@ -27,7 +27,7 @@ describe('address data type', function () {
 
   it('should upload the address storage contract with constructor arguments', function*() {
     const state = yield rest.getState(contract);
-    assert.equal(util.trimLeadingZeros(state.storedData), constructorArgs._storedData, 'storedData');
+    assert.equal(addressToString(state.storedData), addressToString(constructorArgs._storedData), 'storedData');
     assert.equal(state.storedDatum.length, 0, 'storedDatum');
   });
 
@@ -35,7 +35,7 @@ describe('address data type', function () {
     const methodName = 'get';
     const returnsArray = yield rest.callMethod(adminUser, contract, methodName);
     const result = returnsArray[0];
-    assert.equal(constructorArgs._storedData, util.trimLeadingZeros(result), 'address returned from get()');
+    assert.equal(addressToString(constructorArgs._storedData), addressToString(result), 'address returned from get()');
   });
 
   it('set (address)', function*() {
@@ -43,7 +43,7 @@ describe('address data type', function () {
     const args = {value: '0'};
     const returnsArray = yield rest.callMethod(adminUser, contract, methodName, args);
     const state = yield rest.getState(contract);
-    assert.equal(util.trimLeadingZeros(state.storedData), args.value, 'address returned from get()');
+    assert.equal(addressToString(state.storedData), addressToString(args.value), 'address returned from get()');
   });
 
   it('setArray (address[]) / getArray() returns (address[])', function*() {
@@ -57,57 +57,59 @@ describe('address data type', function () {
     // get array
     const returnsArray = yield rest.callMethod(adminUser, contract, 'getArray');
     const result = returnsArray[0];
-    assert.deepEqual(result, args.values, 'after calling getArray()');
+    assert.deepEqual(addressToString(result), addressToString(args.values), 'after calling getArray()');
   });
 
   it('getTuple(address, address, address) returns (address, address, address)', function*() {
     const methodName = 'getTuple';
-    const args = {v1: 'ea674fdde714fd979de3edf0f56aa9716b898ec8', v2: 'e71647d2b46e1a063509c64e2cd084e4f2493d11', v3: 'cc11be9f73a4505c384baa0d6851bbbb249fe3b3'};
+    const args = {v1: '0ea674fdde714fd979de3edf0f56aa9716b898ec8', v2: '0e71647d2b46e1a063509c64e2cd084e4f2493d11', v3: '0cc11be9f73a4505c384baa0d6851bbbb249fe3b3'};
     const returnsArray = yield rest.callMethod(adminUser, contract, methodName, args);
     const result = returnsArray;
-    assert.deepEqual(result, [args.v1, args.v2, args.v3], 'address,address,address returned from getTuple()');
+    assert.deepEqual(addressToString(result), addressToString([args.v1, args.v2, args.v3]), 'address,address,address returned from getTuple()');
   });
 
   it('setStruct(address value, address[] values) return (address, address[])', function*() {
     // function setStruct(address value, address[] values) returns (address, address[])
     const methodName = 'setStruct';
-    const args = {value: 'ea674fdde714fd979de3edf0f56aa9716b898ec8', values: ['8d12a197cb00d4747a1fe03395095ce2a5cc6819', '8d12a197cb00d4747a1fe03395095ce2a5cc6819', '1ce98ed5dc2592b71dd3f2b753b3a44869d7389e']};
+    const args = {value: '0ea674fdde714fd979de3edf0f56aa9716b898ec8', values: ['08d12a197cb00d4747a1fe03395095ce2a5cc6819', '08d12a197cb00d4747a1fe03395095ce2a5cc6819', '01ce98ed5dc2592b71dd3f2b753b3a44869d7389e']};
     const returnsArray = yield rest.callMethod(adminUser, contract, methodName, args);
     // check the returned tuple
-    assert.equal(returnsArray[0], args.value);
-    assert.deepEqual(returnsArray[1], args.values);
+    assert.equal(addressToString(returnsArray[0]), addressToString(args.value));
+    assert.deepEqual(addressToString(returnsArray[1]), addressToString(args.values));
     // check the struct state
     const state = yield rest.getState(contract);
-    assert.equal(state.storedStruct.value, args.value);
-    assert.deepEqual(state.storedStruct.values, args.values);
+    assert.equal(addressToString(state.storedStruct.value), addressToString(args.value));
+    assert.deepEqual(addressToString(state.storedStruct.values), addressToString(args.values));
   });
 
   it('setStructArray(address value, address[] values)', function*() {
     // function setStructArray(address value, address[] values)
     const methodName = 'setStructArray';
-    const args = {value: 'e80a58e5c445c4ecdc5998e2803d216786c9a771', values: ['8d12a197cb00d4747a1fe03395095ce2a5cc6819', 'a10af358d01eca18a446ba768585d23ff055a89e', '1e9939daaad6924ad004c2560e90804164900341']};
+    const args = {value: '0e80a58e5c445c4ecdc5998e2803d216786c9a771', values: ['08d12a197cb00d4747a1fe03395095ce2a5cc6819', '0a10af358d01eca18a446ba768585d23ff055a89e', '01e9939daaad6924ad004c2560e90804164900341']};
     yield rest.callMethod(adminUser, contract, methodName, args);
     // check the struct state
     const state = yield rest.getState(contract);
     state.storedStructs.map(function (storedStruct) {
       assert.equal(storedStruct.value, args.value, 'Struct Array - See issue API-8 (https://blockapps.atlassian.net/browse/API-8)');
-      assert.deepEqual(trimArrayZeros(storedStruct.values), args.values);
+      assert.deepEqual(addressToString(storedStruct.values), addressToString(args.values));
     })
   });
 
   it('setMapping(address value, address key)', function* () {
     // function setMapping(address value, address key) returns (address value)
     const methodName = 'setMapping';
-    const args = {value: 'e80a58e5c445c4ecdc5998e2803d216786c9a771', key: 'a10af358d01eca18a446ba768585d23ff055a89e'};
+    const args = {value: '0e80a58e5c445c4ecdc5998e2803d216786c9a771', key: '0a10af358d01eca18a446ba768585d23ff055a89e'};
     const returnsArray = yield rest.callMethod(adminUser, contract, methodName, args);
     const result = returnsArray[0];
-    assert.equal(result, args.value);
+    assert.equal(addressToString(result), addressToString(args.value));
   });
 });
 
-function trimArrayZeros(array) {
-  return array.map(function (val) {
-    return util.trimLeadingZeros(val);
-  });
-
+function addressToString(addr) {
+  if (Array.isArray(addr)) {
+    return addr.map(function(member) {
+      return util.trimLeadingZeros(member.toLowerCase());
+    });
+  }
+  return util.trimLeadingZeros(addr.toLowerCase());
 }
