36522e5cb18163705415edbabc7d9ff22682adbf
Samrit Sangal <samrit@samrit.io>
2017-06-28 17:25:02 -0400

    tooltip anchor

--- src/components/BarGraph/index.js
+++ src/components/BarGraph/index.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import * as Plottable from 'plottable';
+import { Tooltip } from '@blueprintjs/core';
 import './bar-graph.css';
 
 class BarGraph extends Component {
@@ -9,6 +10,10 @@ class BarGraph extends Component {
     this.state = {
       plot: null,
       dataset: null,
+      interaction: null,
+      tooltipAnchor: null,
+      tooltipVisible: false,
+      tooltipContent: '',
       firstRender: true
     }
   }
@@ -46,6 +51,8 @@ class BarGraph extends Component {
     let scaleMin = min - ((max - min) / 50 + 1) < 0 ? 0 : min - ((max - min) / 50 + 1);
     const xScale = new Plottable.Scales.Linear().domain([0, 16]);
     const yScale = new Plottable.Scales.Linear().domain([scaleMin, scaleMax]);
+    let self = this;
+
 
     // TODO: fix this. Do not mutate state directly. Use redux.
     this.state.dataset = new Plottable.Dataset(this.props.data);
@@ -58,8 +65,39 @@ class BarGraph extends Component {
         return d.y;
       }, yScale)
       //.animator(Plottable.Plots.Animator.MAIN, new Plottable.Animators.Easing().easingMode('quad'))
-      .animated(true)
-      .renderTo("div#bg" + this.props.identifier);
+      .animated(true);
+
+    this.state.interaction = new Plottable.Interactions.Pointer();
+    this.state.interaction.onPointerMove(function(p){
+      const entity = self.state.plot.entityNearest(p);
+      self.state.tooltipAnchor
+        .attr('cx',entity.position.x)
+        .attr('cy',entity.position.y);
+      self.state.tooltipVisible = true;
+      self.state.tooltipContent = entity.datum.y;
+    });
+
+    this.state.interaction.onPointerExit(function(){
+      self.state.tooltipVisible = false;
+      self.state.tooltipContent = '';
+    });
+
+    this.state.interaction.attachTo(this.state.plot);
+    this.state.plot.renderTo("div#bg" + this.props.identifier);
+
+    this.state.tooltipAnchor = this.state.plot
+      .foreground()
+      .append("circle")
+      .attr('r',5)
+      .attr('opacity', 1);
+
+    this.state.tooltip =
+      <Tooltip
+        isOpen={this.state.tooltipVisible}
+        content={this.state.tooltipContent}
+      >
+        this.state.tooltipAnchor.node()
+      </Tooltip>;
   }
 
   componentDidMount() {
