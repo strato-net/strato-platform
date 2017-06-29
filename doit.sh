b48a2d7bd8305cd6e27bb2d678d8e4a3153f2b2a
Nikita Mendelbaum <mendelbaum_nm@bitworks-software.com>
2017-06-29 15:51:16 -0400

    slow container init time fix

--- doit.sh
+++ doit.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+
+until curl strato:3000 >& /dev/null
+do  echo "Waiting for STRATO port 3000 to become available"
+    sleep 1
+done
+
 cirrus="/usr/lib/strato/cirrus/main.js"
 node $cirrus
