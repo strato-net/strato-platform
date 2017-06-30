1f506373f7b98a8764a53e276410dc10ecf25afe
Lior Saar <liorsaar1@gmail.com>
2017-06-30 10:34:49 -0700

    mempool cleanup

--- tests/e2e/mempool.test.js
+++ tests/e2e/mempool.test.js
@@ -10,55 +10,29 @@ const constants = common.constants;
 const assert = common.assert;
 const config = common.config;
 
-function createBatchTxWithNonce(batchValue, toUser, nonces) {
-  var txs = [];
-  return nonces.map(function(nonce) {
-    return {
-      value: batchValue,
-      toAddress: toUser.address,
-      txParams: {nonce: nonce},
-    };
-  });
-}
+const password = '1234';
+const batchValueEther = 2;
 
 describe("Send Transaction List with nonces", function() {
   this.timeout(config.timeout);
 
-  const password = '1234';
-
   const testArray = [
-    {nonces: [0,1,2,3], expectedStatii: {success:4, unresolved:0, rejected:0} },
-    {nonces: [0,1,2,3,4,5,6], expectedStatii: {success:7, unresolved:0, rejected:0} },
-    {nonces: [6,5,4,3,2,1,0], expectedStatii: {success:7, unresolved:0, rejected:0} },
-    {nonces: [0,1,2, 4], expectedStatii: {success:3, unresolved:1, rejected:0} },
+    // {nonces: [0,1,2,3], expectedStatii: {success:4, unresolved:0, rejected:0} },
+    // {nonces: [0,1,2,3,4,5,6], expectedStatii: {success:7, unresolved:0, rejected:0} },
+    // {nonces: [6,5,4,3,2,1,0], expectedStatii: {success:7, unresolved:0, rejected:0} },
+    // {nonces: [0,1,2, 4], expectedStatii: {success:3, unresolved:1, rejected:0} },
     {nonces: [0,1,2,3,3,3], expectedStatii: {success:4, unresolved:2, rejected:0} },
     {nonces: [0,1,2, 4, 4, 4, 3], expectedStatii: {success:5, unresolved:2, rejected:0} },
   ];
 
   testArray.map(function(test) {
-    it('resolve==false ' + JSON.stringify(test), function* () {
-      const uid = util.uid();
-      const aliceName = 'Alice' + uid;
-      const bobName = 'Bob' + uid;
-      const alice = yield rest.createUser(aliceName, password);
-      const bob = yield rest.createUser(bobName, password);
-      const batchValueEther = 2;
-
-      // must use BigNumber for balances
-      alice.startingBalance = yield rest.getBalance(alice.address);
-      bob.startingBalance = yield rest.getBalance(bob.address);
-      assert.isOk(alice.startingBalance.equals(bob.startingBalance), "balances should be equal before sending ether");
-
-      // send List
-      const resolve = false;
-      const txs = createBatchTxWithNonce(batchValueEther, bob, test.nonces);
-      const receipts = yield rest.sendList(alice, txs, resolve);
-      const results = yield Promise.all(checkResults(receipts));
-      const statii = getStatii(results);
-      assert.deepEqual(statii, test.expectedStatii);
+    it(JSON.stringify(test), function* () {
+      yield testSendList(test);
     });
   });
 });
+
+
 
 /**
 
@@ -91,10 +65,8 @@ NO mkTest("transactions should work, if called after rejections" , p6, [0,1,2], 
 
 
 
-describe.only("Send Transaction List with nonces", function() {
+describe("Send Transaction List with nonces", function() {
   this.timeout(config.timeout);
-
-  const password = '1234';
 
   const testArray = [
     { prompt: 'should do all transactions',
@@ -110,28 +82,11 @@ describe.only("Send Transaction List with nonces", function() {
   ];
 
   testArray.map(function(test) {
-    it('resolve==false ' + JSON.stringify(test), function* () {
-      const uid = util.uid();
-      const aliceName = 'Alice' + uid;
-      const bobName = 'Bob' + uid;
-      const alice = yield rest.createUser(aliceName, password);
-      const bob = yield rest.createUser(bobName, password);
-      const batchValueEther = 2;
-
-      // must use BigNumber for balances
-      alice.startingBalance = yield rest.getBalance(alice.address);
-      bob.startingBalance = yield rest.getBalance(bob.address);
-      assert.isOk(alice.startingBalance.equals(bob.startingBalance), "balances should be equal before sending ether");
-
-      // send List
-      const resolve = false;
-      const txs = createBatchTxWithNonce(batchValueEther, bob, test.nonces);
-      const receipts = yield rest.sendList(alice, txs, resolve);
-      const results = yield Promise.all(checkResults(receipts));
-      const statii = getStatii(results);
-      assert.deepEqual(statii, test.expectedStatii);
+    it.only(JSON.stringify(test), function* () {
+      yield testSendList(test);
     });
   });
+
 });
 
 function getStatii(results) {
@@ -178,4 +133,45 @@ function checkResults(receipts) {
         return({message:'Unresolved!'});
       });
   });
+}
+
+function createBatchTxWithNonce(batchValue, toUser, nonces) {
+  var txs = [];
+  return nonces.map(function(nonce) {
+    return {
+      value: batchValue,
+      toAddress: toUser.address,
+      txParams: {nonce: nonce},
+    };
+  });
+}
+
+function* testSendList(test) {
+  const uid = util.uid();
+  const aliceName = 'Alice' + uid;
+  const bobName = 'Bob' + uid;
+  const alice = yield rest.createUser(aliceName, password);
+  const bob = yield rest.createUser(bobName, password);
+
+  // must use BigNumber for balances
+  alice.startingBalance = yield rest.getBalance(alice.address);
+  bob.startingBalance = yield rest.getBalance(bob.address);
+  assert.isOk(alice.startingBalance.equals(bob.startingBalance), "balances should be equal before sending ether");
+
+  // send List
+  const resolve = false;
+  const txs = createBatchTxWithNonce(batchValueEther, bob, test.nonces);
+  const receipts = yield rest.sendList(alice, txs, resolve);
+  const results = yield Promise.all(checkResults(receipts));
+  const statii = getStatii(results);
+  assert.deepEqual(statii, test.expectedStatii);
+
+  // check balances
+  alice.endBalance = yield rest.getBalance(alice.address);
+  bob.endBalance = yield rest.getBalance(bob.address);
+
+  //TODO Calculate gas cost and factor into balance
+  const delta = new BigNumber(batchValueEther).mul(constants.ETHER).mul(test.expectedStatii.success);
+  assert.isOk(alice.startingBalance.minus(delta).greaterThan(alice.endBalance), "alice's balance should be slightly less than expected due to gas costs");
+  assert.isOk(bob.startingBalance.plus(delta).equals(bob.endBalance), "bob's balance should be as expected after sending ether");
 }
