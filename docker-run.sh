b83e7c2cd03d96527618383984b7e498357351c4
Nikita Mendelbaum <mendelbaum_nm@bitworks-software.com>
2017-07-14 15:24:09 -0400

    cirrus_url hotfix

--- docker-run.sh
+++ docker-run.sh
@@ -7,6 +7,6 @@ sed -i "s|__BLOC_URL__|$BLOC_URL|g" build/index.html
 sed -i "s|__STRATO_URL__|$STRATO_URL|g" build/index.html
 sed -i "s|__STRATO_DOC_URL__|$STRATO_DOC_URL|g" build/index.html
 sed -i "s|__BLOC_DOC_URL__|$BLOC_DOC_URL|g" build/index.html
-
+sed -i "s|__CIRRUS_URL__|$CIRRUS_URL|g" build/index.html
 
 serve --port 3002 build
