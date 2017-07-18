96d7d314a04e0e1dd1e6e5e41d6df59332af64db
Nikita Mendelbaum <mendelbaum_nm@bitworks-software.com>
2017-07-18 18:16:42 -0400

    container run stats collect

--- docker-run.sh
+++ docker-run.sh
@@ -8,20 +8,20 @@ sed -i "s|__STRATO_URL__|$STRATO_URL|g" build/index.html
 sed -i "s|__STRATO_DOC_URL__|$STRATO_DOC_URL|g" build/index.html
 sed -i "s|__BLOC_DOC_URL__|$BLOC_DOC_URL|g" build/index.html
 
-# Started by non-BA user
-if [ "$GS_MODE" != "1" ]; then
+# Started by non-BA user (smd_container_started)
+if [ "$STRATO_GS_MODE" != "1" ]; then
   curl http://api.mixpanel.com/track/?data=ewogICAgImV2ZW50IjogInNtZF9jb250YWluZXJfc3RhcnRlZCIsCiAgICAicHJvcGVydGllcyI6IHsKICAgICAgICAidG9rZW4iOiAiZGFmMTcxZTkwMzBhYmIzZTMwMmRmOWQ3OGI2YjFhYTAiCiAgICB9Cn0=&ip=1
 fi
-# Started locally
-if [ "$GS_MODE" = "0" ]; then
+# Started locally (smd_container_started_local)
+if [ "$STRATO_GS_MODE" = "0" ]; then
   curl http://api.mixpanel.com/track/?data=ewogICAgImV2ZW50IjogInNtZF9jb250YWluZXJfc3RhcnRlZF9sb2NhbCIsCiAgICAicHJvcGVydGllcyI6IHsKICAgICAgICAidG9rZW4iOiAiZGFmMTcxZTkwMzBhYmIzZTMwMmRmOWQ3OGI2YjFhYTAiCiAgICB9Cn0=&ip=1
 fi
-# Started on Azure
-if [ "$GS_MODE" = "2" ]; then
+# Started on Azure (smd_container_started_azure)
+if [ "$STRATO_GS_MODE" = "2" ]; then
   curl http://api.mixpanel.com/track/?data=ewogICAgImV2ZW50IjogInNtZF9jb250YWluZXJfc3RhcnRlZF9henVyZSIsCiAgICAicHJvcGVydGllcyI6IHsKICAgICAgICAidG9rZW4iOiAiZGFmMTcxZTkwMzBhYmIzZTMwMmRmOWQ3OGI2YjFhYTAiCiAgICB9Cn0=&ip=1
 fi
-# Started on AWS
-if [ "$GS_MODE" = "3" ]; then
+# Started on AWS (smd_container_started_aws)
+if [ "$STRATO_GS_MODE" = "3" ]; then
   curl http://api.mixpanel.com/track/?data=ewogICAgImV2ZW50IjogInNtZF9jb250YWluZXJfc3RhcnRlZF9hd3MiLAogICAgInByb3BlcnRpZXMiOiB7CiAgICAgICAgInRva2VuIjogImRhZjE3MWU5MDMwYWJiM2UzMDJkZjlkNzhiNmIxYWEwIgogICAgfQp9&ip=1
 fi
 
