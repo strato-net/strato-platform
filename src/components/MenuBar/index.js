6d3bff5e2b7c841d8cbb8fc502d302bd24ff9115
Samrit Sangal <samrit@samrit.io>
2017-08-10 11:52:54 -0400

    Add version number

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -27,6 +27,7 @@ class MenuBar extends Component {
           <div className="pt-navbar-heading">STRATO Management Dashboard</div>
         </div>
         <div className="pt-navbar-group pt-align-right">
+          <small className="pt-text-muted">v{ process.env.REACT_APP_VERSION } </small>
           <span className="pt-navbar-divider"/>
           <a href={ env.BLOC_DOC_URL } target="_blank" rel="noopener noreferrer">
             <button className="pt-button pt-minimal pt-small" onClick={() => {mixpanelWrapper.track("bloc_docs_click")}}>Bloc API</button>
