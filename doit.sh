94f9faf4516d98e157c6e7749b5b31a74074b583
nikitamendelbaum <nikitamendelbaum@gmail.com>
2017-08-30 17:51:30 -0400

    stratourl may be changed

--- doit.sh
+++ doit.sh
@@ -3,10 +3,12 @@
 stratourl=${stratourl:-strato:3000}
 echo "stratourl is: ${stratourl}"
 
-until curl stratourl >& /dev/null
-do  echo "Waiting for STRATO port 3000 to become available"
+until curl ${stratourl} >& /dev/null
+do  echo "Waiting for STRATO to become available at ${stratourl}"
     sleep 1
 done
+
+echo "Connected to STRATO"
 
 cirrus="/usr/lib/strato/cirrus/main.js"
 node $cirrus
