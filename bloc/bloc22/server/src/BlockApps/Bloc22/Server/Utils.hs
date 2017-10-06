c54fe0c45b1553a5bfdb0e042427f53e9264adf7
dustinnorwood <dnorwood2010@aol.com>
2017-10-06 14:30:50 -0400

    Did some cleanup in Utils.hs

--- bloc/bloc22/server/src/BlockApps/Bloc22/Server/Utils.hs
+++ bloc/bloc22/server/src/BlockApps/Bloc22/Server/Utils.hs
@@ -8,7 +8,6 @@ module BlockApps.Bloc22.Server.Utils where
 
 import           Control.Concurrent
 import           Control.Monad.IO.Class
-import           Control.Monad.Log
 import           Control.Monad.Loops
 import qualified Data.ByteString.Base16 as BS16
 import           Data.Maybe
@@ -41,18 +40,6 @@ waitNewBlock = do
       . withoutNext
       . head <$> getBlocksLast 0
 
-waitNewAccount :: Address -> ClientM Account
-waitNewAccount addr = do
-  account <- getAccount
-  case account of
-    Just acc -> return acc
-    Nothing -> untilJust $ delay1 >> getAccount
-  where
-    getAccount = listToMaybe <$> getAccountsFilter accountsFilterParams{qaAddress = Just addr}
-    delay1 = liftIO $ do
-      putStrLn $ "Waiting on transaction result for new account at address " ++ addressString addr
-      threadDelay 1000000
-
 maybeTxResult :: Keccak256 -> Bloc (Maybe TransactionResult)
 maybeTxResult hash = listToMaybe <$> blocStrato (getTxResult hash)
 
@@ -76,31 +63,6 @@ getBlocTxStatus hash = do
           "Success!" -> return (Success, mtx, mtxr)
           _          -> return (Failure, mtx, mtxr)
 
-pollTxResult :: Keccak256 -> Bloc TransactionResult
-pollTxResult hash = go 1
-  where
-    attempts = 30 :: Int
-    hashString = keccak256String hash
-    go n | n > attempts = blocError . AnError . Text.pack $ "Strato result polling timeout after " ++ show attempts ++ " attempts on transaction hash: " ++ hashString
-         | otherwise = do
-      liftIO $ threadDelay 1000000
-      logWith logNotice . Text.pack $ "[" ++ show n ++ "/" ++ show attempts ++ "] Polling result for transaction hash: " ++ hashString
-      result <- blocStrato (getTxResult hash)
-      case listToMaybe result of
-        Nothing  -> go (n+1)
-        Just res -> return res
-
-pollTxResultBatch :: [Keccak256] -> Bloc BatchTransactionResult
-pollTxResultBatch keccaks = go 1 where
-    attempts   = 15 :: Int
-    hashString = show (keccak256String <$> keccaks)
-    go n | n > attempts = blocError . AnError . Text.pack $ "Strato result batch polling timeout after " ++ show attempts ++ " attempts for hashes: " ++ hashString
-         | otherwise    = do
-             logWith logNotice . Text.pack $ "[" ++ show n ++ "/" ++ show attempts ++ "] Looking up " ++ hashString
-             resolutions <- blocStrato (postTxResultBatch keccaks)
-             if any null (unBatchTransactionResult resolutions)
-                 then liftIO (threadDelay 1000000) >> go (n + 1)
-                 else return resolutions
 emptyTxParams :: TxParams
 emptyTxParams = TxParams Nothing Nothing Nothing
 
