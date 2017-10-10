7873390623d288cac604c02273a459d84b0f37f5
Samrit Sangal <samrit@samrit.io>
2017-10-10 13:01:35 -0400

    validate js

---
+++ src/components/Accounts/components/SendEther/validate.js
@@ -0,0 +1,35 @@
+const validate = (values) => {
+    const errors = {};
+
+    if (!values.from) {
+        errors.value = 'Please select a user';
+    }
+
+    if (!values.fromAddress) {
+        errors.value = 'Please select a address';
+    }
+
+    if (!values.password) {
+        errors.value = 'Please enter a password';
+    }
+
+    if(!values.radio && !values.toAddress) {
+        errors.value = "Please select address"
+    }
+
+    if(values.radio === "0" && !values.toAddress ) {
+        errors.value = "Please select address"
+    }
+
+    if (values.radio === "1" && !values.contractAddress) {
+        errors.value = "Please select address"
+    }
+
+    if (!values.value) {
+        errors.value = 'Please enter a value';
+    }
+
+    return errors;
+};
+
+export default validate;
