a1eff532b1ee5ef30b2ee4aa7c31e4d53b3ba787
nikitamendelbaum <nikitamendelbaum@gmail.com>
2017-10-20 11:30:08 -0400

    X-API renamed to APEX, url env var fix

--- docker-run.sh
+++ docker-run.sh
@@ -8,7 +8,7 @@ sed -i "s|__STRATO_URL__|$STRATO_URL|g" build/index.html
 sed -i "s|__STRATO_DOC_URL__|$STRATO_DOC_URL|g" build/index.html
 sed -i "s|__BLOC_DOC_URL__|$BLOC_DOC_URL|g" build/index.html
 sed -i "s|__CIRRUS_URL__|$CIRRUS_URL|g" build/index.html
-sed -i "s|__XAPI_URL__|$XAPI_URL|g" build/index.html
+sed -i "s|__APEX_URL__|$APEX_URL|g" build/index.html
 
 STRATO_GS_MODE=${STRATO_GS_MODE:-}
 # Started by non-BA user (smd_container_started)
