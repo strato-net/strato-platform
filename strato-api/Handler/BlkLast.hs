cc9f794509461d02b43d8e99a0d11cf1a2d9906e
Samrit Sangal <samrit@samrit.io>
2017-10-24 12:53:49 -0400

    Add distinct to SQL query

--- strato-api/Handler/BlkLast.hs
+++ strato-api/Handler/BlkLast.hs
@@ -9,7 +9,7 @@ import qualified Prelude            as P
 
 getBlkLastR :: Integer -> Handler Value
 getBlkLastR n      =            do addHeader "Access-Control-Allow-Origin" "*"
-                                   blks <- runDB $ E.select $
+                                   blks <- runDB $ E.select $ E.distinct $
                                         E.from $ \(a, t) -> do
                                         E.where_ (  a E.^. BlockDataRefBlockId E.==. t E.^. BlockId)
                                         E.limit $ P.max 1 $ P.min (fromIntegral n :: Int64) fetchLimit
