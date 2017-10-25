509c99fafc28cd79201f3ae2904bae924e835306
Samrit Sangal <samrit@samrit.io>
2017-10-25 11:16:46 -0400

    Remove DISTINCT from sql query

--- strato-api/Handler/BlkLast.hs
+++ strato-api/Handler/BlkLast.hs
@@ -9,7 +9,7 @@ import qualified Prelude            as P
 
 getBlkLastR :: Integer -> Handler Value
 getBlkLastR n      =            do addHeader "Access-Control-Allow-Origin" "*"
-                                   blks <- runDB $ E.select $ E.distinct $
+                                   blks <- runDB $ E.select $
                                         E.from $ \(a, t) -> do
                                         E.where_ (  a E.^. BlockDataRefBlockId E.==. t E.^. BlockId)
                                         E.limit $ P.max 1 $ P.min (fromIntegral n :: Int64) fetchLimit
