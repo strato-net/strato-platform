f3e7cc7ce6885bd0c7db76f4be1ebe50235bec2c
Charles Crain <craincharles@gmail.com>
2017-10-30 15:18:48 +0000

    canonRedis works printing out invalid canonical blocks

--- blockapps-tools/src/CanonRedis.hs
+++ blockapps-tools/src/CanonRedis.hs
@@ -2,9 +2,9 @@
 module CanonRedis where
 
 import           Control.Monad.IO.Class
+import           Control.Monad
 import           Data.Monoid                           ((<>))
 import           Database.Redis
-import Data.Foldable
 
 -- import           Blockchain.EthConf                    (lookupRedisBlockDBConfig)
 import           Blockchain.Data.BlockHeader
@@ -17,41 +17,54 @@ import           Blockchain.Strato.RedisBlockDB.Models
 canonRedis :: String -> Int -> Int -> IO ()
 canonRedis ip start range = do
     conn <- connect defaultConnectInfo{connectHost=ip}
-    -- conn <- connect defaultConnectInfo{connectHost="40.76.17.40"}
-    -- conn <- connect defaultConnectInfo{connectHost="13.72.79.3"}
-    -- conn <- connect defaultConnectInfo{connectHost="40.76.29.122"}
     runRedis conn $ do
       Just bestBlockNumer <- getBestBlockNumber
       let range' = min range (bestBlockNumer - start)
           starts = [start,start+500..range']
       forM_ starts $ \s -> do
-        shas <- getCanonicalChain (fromIntegral $ s) 500
-        hs :: [(SHA, Maybe BlockHeader)] <- getHeaders shas
-        liftIO $ mapM_ printBlockHeader hs
+        hs <- getCanonicalHeaderChain (fromIntegral $ s) 500
+        let pHash = parentHash . snd . head $ hs
+            pNum  = (number . snd . head $ hs) - 1
+        parentIsCanon <- isCanonical pHash pNum
+        if parentIsCanon
+          then validateChainAndLogInvalid hs
+          else do
+            liftIO $ putStrLn "Parent of block not in Canonical"
+            liftIO $ printBlockHeader $ head hs
 
   where
+    getBestBlockNumber :: Redis (Maybe Int)
     getBestBlockNumber = ((fromIntegral . bestBlockNumber) <$>) <$>  getBestBlockInfo
-    
-printBlockHeader :: (SHA,Maybe BlockHeader) -> IO ()
-printBlockHeader (sha, Just h) = do
-  liftIO . putStrLn $ "Number "
-                   <> show (blockHeaderBlockNumber h)
-                   <> " -- Hash "
-                   <>show (shaToHex $ sha)
-                   <> " -- Parent hash "
-                   <> show (shaToHex $ blockHeaderParentHash h)
-printBlockHeader _ = undefined
 
--- dumpRedis :: Integer -> IO ()
--- dumpRedis _ = do
---     conn <- checkedConnect lookupRedisBlockDBConfig
---     bb <- runRedis conn getBestBlockInfo
---     case bb of
---         Nothing -> putStrLn "No best block in Redis"
---         Just b  -> putStrLn . formatBB $ b
+    validateChainAndLogInvalid :: [(SHA,BlockHeader)] -> Redis ()
+    validateChainAndLogInvalid hs = do
+      let b = head hs
+          bs = tail hs
+      foldM_ validateMethod b bs
 
-formatBB :: RedisBestBlock -> String
-formatBB b = unlines [ ("Best block number:\t" ++) . show . bestBlockNumber $ b
-                     , ("Best block tot. diff:\t" ++) . show . bestBlockTotalDifficulty $ b
-                     , ("Best block hash:\t" ++) . shaToHex . bestBlockHash $ b
-                     ]
+    validateMethod :: (SHA,BlockHeader) -> (SHA,BlockHeader) -> Redis (SHA,BlockHeader)
+    validateMethod prev curr = do
+      if (fst prev == (parentHash . snd $ curr) )
+        then return ()
+        else do
+          liftIO $ putStrLn "Invalid Parent Header relation."
+          liftIO $ printBlockHeader curr
+          liftIO $ printBlockHeader prev
+      return curr
+
+    isCanonical :: SHA -> Integer -> Redis Bool
+    isCanonical hash num = do
+      mHash <- getCanonical num
+      case mHash of
+        Nothing -> return False
+        Just hash' ->return $  hash == hash'
+
+    printBlockHeader :: (SHA,BlockHeader) -> IO ()
+    printBlockHeader (sha, h) = do
+      putStrLn $ "Number "
+        <> show (blockHeaderBlockNumber h)
+        <> " -- Hash "
+        <>show (shaToHex $ sha)
+        <> " -- Parent hash "
+        <> show (shaToHex $ blockHeaderParentHash h)
+
