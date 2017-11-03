9e1540b45c9ef1473f1e8a549b1c8d7bde729d46
dustinnorwood <dnorwood2010@aol.com>
2017-11-03 12:10:19 -0400

    Change test to handle multiple simultaneous transactions

--- tests/e2e/timing.test.js
+++ tests/e2e/timing.test.js
@@ -25,7 +25,7 @@ const config = common.config;
 
 
 describe("Send Transaction Test", function() {
-  this.timeout(config.timeout);
+  this.timeout(config.timeout * config.nodes.length);
 
   const uid = util.uid();
   const userPairs = [];
@@ -38,12 +38,12 @@ describe("Send Transaction Test", function() {
 
   before(function* () {
     //stats.blockNumber = yield rest.getLastBlockNumber();
-    stats.startTime = moment().valueOf();
+    //stats.startTime = moment().valueOf();
     // create a pair of users on every node
     yield createUserPairs(uid, password, userPairs);
   });
 
-  it('should send correct amount MULTIPLE TIMES between all pairs.  https://blockapps.atlassian.net/browse/API-20', function* () {
+  it.skip('should send correct amount MULTIPLE TIMES between all pairs.  https://blockapps.atlassian.net/browse/API-20', function* () {
     const count = 20;
     var total = new BigNumber(0);
     // send multiple
@@ -56,7 +56,7 @@ describe("Send Transaction Test", function() {
         yield send(node.id, pair.alice, pair.bob, nodeValue);
       }
     }
-    // block times
+    //// block times
     //const blockTimes = yield getBlockTimes(count*nodes.length + 20);
     //// write timing to file
     //console.log(JSON.stringify(txTiming, null, 2));
@@ -70,6 +70,29 @@ describe("Send Transaction Test", function() {
     yield checkBalance(nodes[0].alice,nodes[0].bob,total);
     //const pair = userPairs[0];
       //yield checkBalance(pair.alice, pair.bob, total);
+  });
+
+  it('should send correct amount MULTIPLE TIMES between all pairs.  https://blockapps.atlassian.net/browse/API-20', function* () {
+    const count = 20;
+    var total = new BigNumber(0);
+    // send multiple
+    for (var i=0; i < count; i++) {
+      const nodeValue = value.plus(i*100);
+      total = total.plus(nodeValue);
+        const txPending = [];
+      for (let node of nodes) {
+        // send alice->bob on that node
+        const pair = userPairs[node.id];
+          console.log('Sending', nodeValue.toString(), 'on node', node.id);
+          txPending.push({node:node.id, status:'Pending', hash: yield rest.send(pair.alice,pair.bob,nodeValue,true,undefined,node.id)});
+      }
+        for (let pend of txPending) {
+            console.log('########## Resolving node', pend.node, '##########');
+            txResult = yield getResolved(function*(){return yield pend;},pend.node);
+            assert.equal(txResult.status,'Success', 'batch tx status');
+        }
+    }
+    yield checkBalance(nodes[0].alice,nodes[0].bob,total);
   });
 
   it.skip('should send correct amount of ether', function* () {
@@ -104,19 +127,33 @@ describe("Send Transaction Test", function() {
     assert.isOk(bob.startingBalance.plus(value).equals(bob.endBalance), "bob's balance should be as expected after sending ether");
   });
 
+    function* getResolved(func, node) {
+        var txResult = yield func();
+        var i = 1;
+        while(txResult.status == 'Pending') {
+            console.log('Got pending result', i);
+            i++;
+            yield sleep(1000);
+            txResult = yield rest.getBlocResult(txResult.hash, false, node.id);
+        }
+        return txResult;
+    }
+
   // ================================================================
   function* createUserPairs(uid, password, pairs) {
     console.log('creating users');
     for (let node of nodes) {
       // create
       const aliceName = `Alice_${node.id}_${uid}`;
-      const alice = yield rest.createUser(aliceName, password, false, node.id);
+      const alice = yield rest.createUser(aliceName, password, true, node.id);
+        yield getResolved(function* (){return yield rest.fill(alice, false, node.id);}, node);
       console.log('alice', alice);
-      for(var i=0; i<1000000000; i++) {}; //about 3s delay. Done to allow p2p network to sync new faucet nonce and make it adjust for block time
+      //for(var i=0; i<1000000000; i++) {}; //about 3s delay. Done to allow p2p network to sync new faucet nonce and make it adjust for block time
       const bobName = `Bob_${node.id}_${uid}`;
-      const bob = yield rest.createUser(bobName, password, false, node.id);
+      const bob = yield rest.createUser(bobName, password, true, node.id);
+        yield getResolved(function* (){return yield rest.fill(bob, false, node.id);}, node);
       console.log('bob', bob);
-      for(var i=0; i<1000000000; i++) {}; //about 3s delay. Done to allow p2p network to sync new faucet nonce and make it adjust for block time
+      //for(var i=0; i<1000000000; i++) {}; //about 3s delay. Done to allow p2p network to sync new faucet nonce and make it adjust for block time
       const pair = {alice: alice, bob:bob};
       pairs[node.id]= pair;
       // test creation on the node
@@ -149,9 +186,8 @@ describe("Send Transaction Test", function() {
   function* send(nodeId, alice, bob, value, nonce) {
     // it is OK for nonce to be undefined!
     console.log('send', nodeId, alice.name, bob.name, value.toString(), nonce);
-    const startTime = timerStart();
-      yield rest.send(alice, bob, value, false, nonce, nodeId);
-
+    //const startTime = timerStart();
+      txResult = yield getResolved(function*(){hash = yield rest.send(alice,bob,value,true,nonce,nodeId); return {status: 'Pending', hash: hash};},nodeId);
     //const txElapsed = timerStop(startTime);
     ////const txResult = yield rest.transactionResult(receipt.hash, nodeId);
     //const blockElapsed = yield getLastBlockTime(nodeId);
@@ -160,14 +196,14 @@ describe("Send Transaction Test", function() {
     //  txElapsed: txElapsed,
     //  txResultTime: txResult[0].time,
     //  blockElapsed: blockElapsed,
-    //  blockHash: txResult[0].blockHash,
+    //  blockHash: txResult[0].blockHash
     //  //txResult: txResult,
     //};
     //txTiming.push(txTimingObject);
     //console.log('send tx: txElapsed', txElapsed, 'txResult.time', txResult[0].time);
-    //assert.equal(txResult.status, 'success', 'tx status');
+    assert.equal(txResult.status, 'Success', 'tx status');
 
-    //return txResult;
+    return txResult;
   }
 
   function* checkBalance(alice, bob, value) {
@@ -184,61 +220,61 @@ describe("Send Transaction Test", function() {
     }
   }
 
-  function* getBlockTimes(count, node) {
-    const blocks = yield api.strato.last(count, node);
-    const blockTimeStamps = blocks.map(block => {
-      return {
-        time: Date.parse(block.blockData.timestamp),
-        difficulty: block.blockData.difficulty,
-      };
-    });
+  //function* getBlockTimes(count, node) {
+  //  const blocks = yield api.strato.last(count, node.id);
+  //  const blockTimeStamps = blocks.map(block => {
+  //    return {
+  //      time: Date.parse(block.blockData.timestamp),
+  //      difficulty: block.blockData.difficulty
+  //    };
+  //  });
 
-    const blockTimeDelta = blockTimeStamps
-      .slice(1)
-      .map((block, index) => {
-        const delta = blockTimeStamps[index+0].time - blockTimeStamps[index+1].time;
-        return {
-          delta: delta,
-          difficulty: block.difficulty,
-        };
-      });
+  //  const blockTimeDelta = blockTimeStamps
+  //    .slice(1)
+  //    .map((block, index) => {
+  //      const delta = blockTimeStamps[index+0].time - blockTimeStamps[index+1].time;
+  //      return {
+  //        delta: delta,
+  //        difficulty: block.difficulty
+  //      };
+  //    });
 
-    return blockTimeDelta;
-  }
+  //  return blockTimeDelta;
+  //}
 
-  function* getLastBlockTime(node) {
-    const blocks = yield api.strato.last(2, node);
-    // console.log(blocks);
-    const t0 = Date.parse(blocks[0].blockData.timestamp);
-    const t1 = Date.parse(blocks[1].blockData.timestamp);
-    const elapsed = t0 - t1;
-    console.log('block elapsed', elapsed, t0, t1);
-    return elapsed;
-  }
+  //function* getLastBlockTime(node) {
+  //  const blocks = yield api.strato.last(2, node.id);
+  //  // console.log(blocks);
+  //  const t0 = Date.parse(blocks[0].blockData.timestamp);
+  //  const t1 = Date.parse(blocks[1].blockData.timestamp);
+  //  const elapsed = t0 - t1;
+  //  console.log('block elapsed', elapsed, t0, t1);
+  //  return elapsed;
+  //}
 
 });
 
 
-function* getBlockTimes(count, node) {
-  const blocks = yield api.strato.last(count, node);
-  const blockTimeStamps = blocks.map(block => {
-    return {
-      time: Date.parse(block.blockData.timestamp),
-      difficulty: block.blockData.difficulty,
-      number: block.blockData.number,
-    };
-  });
-
-  const blockTimeDelta = blockTimeStamps
-    .slice(1)
-    .map((block, index) => {
-      const delta = blockTimeStamps[index+0].time - blockTimeStamps[index+1].time;
-      return {
-        delta: delta,
-        difficulty: block.difficulty,
-        number: block.number,
-      };
-    });
-
-  return blockTimeDelta;
-}
+//function* getBlockTimes(count, node) {
+//  const blocks = yield api.strato.last(count, node.id);
+//  const blockTimeStamps = blocks.map(block => {
+//    return {
+//      time: Date.parse(block.blockData.timestamp),
+//      difficulty: block.blockData.difficulty,
+//      number: block.blockData.number
+//    };
+//  });
+//
+//  const blockTimeDelta = blockTimeStamps
+//    .slice(1)
+//    .map((block, index) => {
+//      const delta = blockTimeStamps[index+0].time - blockTimeStamps[index+1].time;
+//      return {
+//        delta: delta,
+//        difficulty: block.difficulty,
+//        number: block.number
+//      };
+//    });
+//
+//  return blockTimeDelta;
+//}
