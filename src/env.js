4e0f3f20031ba252ae8bfb5632c8b3229aa68854
Samrit Sangal <samrit@samrit.io>
2017-11-27 17:14:59 -0500

    Some more ws url experiments

--- src/env.js
+++ src/env.js
@@ -9,5 +9,5 @@ export const env = {
   POLLING_FREQUENCY: window.POLLING_FREQUENCY && window.POLLING_FREQUENCY !== '__POLLING_FREQUENCY__' ? window.POLLING_FREQUENCY : 5 * 1000,
   STRATO_GS_MODE: window.STRATO_GS_MODE && window.STRATO_GS_MODE !== '__STRATO_GS_MODE__' ? window.STRATO_GS_MODE : '0',
   SINGLE_NODE: window.SINGLE_NODE && window.SINGLE_NODE !== '__SINGLE_NODE__' ? window.SINGLE_NODE : 'false',
-  SOCKET_SERVER: window.APEX_URL && window.APEX_URL !== '__APEX_URL__' ? window.APEX_URL.replace('http','ws') : 'ws://localhost'
+  SOCKET_SERVER: window.APEX_URL && window.APEX_URL !== '__APEX_URL__' ? `ws://${(new URL(window.APEX_URL)).host}/` : 'ws://localhost'
 };
