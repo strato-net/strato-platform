6b9d298207dc7952c377f298d8e1d0f7772c7998
Tanuj Soni <tanuj@blockapps.net>
2017-12-08 17:48:20 +0530

    [STRATO-testcases] Feature: added contracts snapshot tests

--- src/tests/Nodes/index.spec.js
+++ src/tests/Nodes/index.spec.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Nodes from '../../components/Nodes/index';
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
+test('renders correctly', () => {
   const node = renderer.create(<Nodes/>).toJSON();
   expect(node).toMatchSnapshot();
 });
