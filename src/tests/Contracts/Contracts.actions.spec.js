53c040524cd355efecc78929df1949fe154ed751
Tanuj Soni <tanuj@blockapps.net>
2017-12-08 16:18:34 +0530

    [STRATO-testcases] Fix: contract reducer

--- src/tests/Contracts/Contracts.actions.spec.js
+++ src/tests/Contracts/Contracts.actions.spec.js
@@ -6,35 +6,40 @@ import {
   fetchContractsFailure,
   FETCH_CONTRACTS_FAILED,
   changeContractFilter,
-  CHANGE_CONTRACT_FILTER } from '../../components/Contracts/contracts.actions';
+  CHANGE_CONTRACT_FILTER
+} from '../../components/Contracts/contracts.actions';
 
-test('Should create an action to fetch contracts', () => {
-  const expectedAction = {
-    type: 'FETCH_CONTRACTS'
-  }
-  expect(fetchContracts()).toEqual(expectedAction)
-});
+describe('Test contracts actions', () => {
 
-test('Should fetch contracts success', () => {
-  const expectedAction = {
-    type: 'FETCH_CONTRACTS_SUCCESSFUL',
-    contracts: 'contracts'
-  }
-  expect(fetchContractsSuccess('contracts')).toEqual(expectedAction)
-});
+  test('should create an action to fetch contracts', () => {
+    const expectedAction = {
+      type: 'FETCH_CONTRACTS'
+    }
+    expect(fetchContracts()).toEqual(expectedAction)
+  });
 
-test('Should fetch contracts fail', () => {
-  const expectedAction = {
-    type: 'FETCH_CONTRACTS_FAILED',
-    error: 'error'
-  }
-  expect(fetchContractsFailure('error')).toEqual(expectedAction)
-});
+  test('should return contracts after successfull response', () => {
+    const expectedAction = {
+      type: 'FETCH_CONTRACTS_SUCCESSFUL',
+      contracts: 'contracts'
+    }
+    expect(fetchContractsSuccess('contracts')).toEqual(expectedAction)
+  });
 
-test('Should contract filter change', () => {
-  const expectedAction = {
-    type: 'CHANGE_CONTRACT_FILTER',
-    filter: 'filter'
-  }
-  expect(changeContractFilter('filter')).toEqual(expectedAction)
+  test('should return error after failure response', () => {
+    const expectedAction = {
+      type: 'FETCH_CONTRACTS_FAILED',
+      error: 'error'
+    }
+    expect(fetchContractsFailure('error')).toEqual(expectedAction)
+  });
+
+  test('should change contract filter', () => {
+    const expectedAction = {
+      type: 'CHANGE_CONTRACT_FILTER',
+      filter: 'filter'
+    }
+    expect(changeContractFilter('filter')).toEqual(expectedAction)
+  });
+
 });
--- src/tests/Contracts/Contracts.reducer.spec.js
+++ src/tests/Contracts/Contracts.reducer.spec.js
@@ -10,154 +10,160 @@ import {
 import {
   FETCH_STATE_SUCCESS,
   SELECT_CONTRACT_INSTANCE,
+  FETCH_CIRRUS_INSTANCES_SUCCESS,
   FETCH_ACCOUNT_SUCCESS
 } from '../../components/Contracts/components/ContractCard/contractCard.actions';
+import { deepClone } from '../helper/testHelper';
 
-// INITIAL_STATE
-test('Should initial state set', () => {
-  const initialState = {
-    contracts: {},
-    filter: '',
-    error: null,
-  };
-  expect(reducer(undefined, {})).toEqual(initialState)
-});
 
-// FETCH_CONTRACTS
-test('should FETCH_CONTRACTS initiate', () => {
-  const action = {
-    type: FETCH_CONTRACTS
-  };
+describe('Test contracts reducer', () => {
 
-  const stateAfter = {
-    contracts: contractState,
-    filter: filter,
-    error: null,
-  }
+  // INITIAL_STATE
+  test('should set initial state', () => {
+    const initialState = {
+      contracts: {},
+      filter: '',
+      error: null,
+    };
+    expect(reducer(undefined, {})).toEqual(initialState)
+  });
 
-  expect(reducer({ contracts: contractState, filter: filter }, action)).toEqual(stateAfter)
-})
+  // FETCH_CONTRACTS
+  test('should fetch contracts', () => {
+    const action = {
+      type: FETCH_CONTRACTS
+    };
 
-// FETCH_CONTRACTS_SUCCESSFUL
-test('should FETCH_CONTRACTS_SUCCESSFUL', () => {
-  const action = {
-    type: FETCH_CONTRACTS_SUCCESSFUL,
-    contracts: contracts
-  };
+    const stateAfter = {
+      contracts: contractState,
+      filter: filter,
+      error: null,
+    }
 
-  const stateAfter = {
-    contracts: contractState,
-    filter: filter,
-    error: 'error'
-  }
+    expect(reducer({ contracts: contractState, filter: filter }, action)).toEqual(stateAfter)
+  })
 
-  expect(reducer({ contracts: {}, filter: filter, error: 'error' }, action)).toEqual(stateAfter)
-})
+  // FETCH_CONTRACTS_SUCCESSFUL
+  test('should store contracts after success', () => {
+    const action = {
+      type: FETCH_CONTRACTS_SUCCESSFUL,
+      contracts: contracts
+    };
 
-// FETCH_CONTRACTS_FAILED
-test('should FETCH_CONTRACTS_FAILED', () => {
-  const action = {
-    type: FETCH_CONTRACTS_FAILED,
-    error: 'error'
-  };
+    const stateAfter = {
+      contracts: contractState,
+      filter: filter,
+      error: 'error'
+    }
 
-  const stateAfter = {
-    contracts: contracts,
-    filter: filter,
-    error: 'error'
-  };
+    expect(reducer({ contracts: {}, filter: filter, error: 'error' }, action)).toEqual(stateAfter)
+  })
 
-  expect(reducer({ contracts: contracts, filter: filter }, action)).toEqual(stateAfter)
-})
+  // FETCH_CONTRACTS_FAILED
+  test('should update error if fetch contracts failed', () => {
+    const action = {
+      type: FETCH_CONTRACTS_FAILED,
+      error: 'error'
+    };
 
-// CHANGE_CONTRACT_FILTER
-test('Should CHANGE_CONTRACT_FILTER', () => {
-  const action = {
-    type: CHANGE_CONTRACT_FILTER,
-    filter: filter
-  };
+    const stateAfter = {
+      contracts: contracts,
+      filter: filter,
+      error: 'error'
+    };
 
-  const stateAfter = {
-    contracts: contracts,
-    filter: filter,
-    error: 'error'
-  }
+    expect(reducer({ contracts: contracts, filter: filter }, action)).toEqual(stateAfter)
+  })
 
-  expect(reducer({ contracts: contracts, error: 'error' }, action)).toEqual(stateAfter)
-})
+  // CHANGE_CONTRACT_FILTER
+  test('should update filter while searching contract', () => {
+    const action = {
+      type: CHANGE_CONTRACT_FILTER,
+      filter: filter
+    };
 
-// FETCH_STATE_SUCCESS
-test('Should FETCH_STATE_SUCCESS', () => {
-  const action = {
-    address: "0293f9b10a4453667db7fcfe74728c9d821add4b",
-    name: "GreeterA",
-    state: {
+    const stateAfter = {
+      contracts: contracts,
+      filter: filter,
+      error: 'error'
+    }
+
+    expect(reducer({ contracts: contracts, error: 'error' }, action)).toEqual(stateAfter)
+  })
+
+  // FETCH_STATE_SUCCESS
+  test('should fetch state and append it to contract', () => {
+    const action = {
+      address: "0293f9b10a4453667db7fcfe74728c9d821add4b",
+      name: "GreeterA",
+      state: {
+        "greetA": "function () returns (Address)",
+        "greetingA": "Aaaaaaaaa"
+      },
+      type: "FETCH_STATE_SUCCESS"
+    }
+
+    let contract = deepClone(contractState);
+    contract['GreeterA']['instances'][0]['selected'] = true;
+
+    let expectedContract = deepClone(contractState);
+    expectedContract['GreeterA']['instances'][0]['selected'] = true;
+    expectedContract['GreeterA']['instances'][0]['state'] = {
       "greetA": "function () returns (Address)",
       "greetingA": "Aaaaaaaaa"
-    },
-    type: "FETCH_STATE_SUCCESS"
-  }
+    };
 
-  let contract = contractState;
-  contract['GreeterA']['instances'][0]['selected'] = true;
+    const stateAfter = {
+      contracts: expectedContract,
+      filter: filter,
+      error: 'error'
+    }
 
-  let expectedContract = contractState;
-  expectedContract['GreeterA']['instances'][0]['selected'] = true;
-  expectedContract['GreeterA']['instances'][0]['state'] = {
-    "greetA": "function () returns (Address)",
-    "greetingA": "Aaaaaaaaa"
-  };
+    expect(reducer({ contracts: contract, error: 'error', filter: filter }, action)).toEqual(stateAfter)
+  })
 
-  const stateAfter = {
-    contracts: expectedContract,
-    filter: filter,
-    error: 'error'
-  }
+  // SELECT_CONTRACT_INSTANCE
+  test('should select contract instance', () => {
+    const action = {
+      address: "0293f9b10a4453667db7fcfe74728c9d821add4b",
+      name: "GreeterA",
+      type: "SELECT_CONTRACT_INSTANCE"
+    }
 
-  expect(reducer({ contracts: contract, error: 'error', filter: filter }, action)).toEqual(stateAfter)
+    let expectedContract = deepClone(contractState);
+    expectedContract['GreeterA']['instances'][0]['selected'] = true;
+
+    const stateAfter = {
+      contracts: expectedContract,
+      filter: filter,
+      error: 'error'
+    }
+
+    expect(reducer({ contracts: contractState, error: 'error', filter: filter }, action)).toEqual(stateAfter)
+  })
+
+  // FETCH_ACCOUNT_SUCCESS
+  test('should fetch account', () => {
+    const action = {
+      account: [{ balance: 0 }],
+      address: "0293f9b10a4453667db7fcfe74728c9d821add4b",
+      name: "GreeterA",
+      type: "FETCH_ACCOUNT_SUCCESS"
+    }
+
+    let contract = deepClone(contractState);
+    contract['GreeterA']['instances'][0]['selected'] = true;
+
+    let expectedContract = deepClone(contractState);
+    expectedContract['GreeterA']['instances'][0]['selected'] = true;
+    expectedContract['GreeterA']['instances'][0]['balance'] = 0;
+
+    const stateAfter = {
+      contracts: expectedContract,
+      filter: filter,
+      error: null
+    }
+
+    expect(reducer({ contracts: contract, filter: filter }, action)).toEqual(stateAfter)
+  })
 })
-
-// SELECT_CONTRACT_INSTANCE
-test('Should SELECT_CONTRACT_INSTANCE', () => {
-  const action = {
-    address: "0293f9b10a4453667db7fcfe74728c9d821add4b",
-    name: "GreeterA",
-    type: "SELECT_CONTRACT_INSTANCE"
-  }
-
-  let expectedContract = contractState;
-  expectedContract['GreeterA']['instances'][0]['selected'] = true;
-
-  const stateAfter = {
-    contracts: expectedContract,
-    filter: filter,
-    error: 'error'
-  }
-
-  expect(reducer({ contracts: contractState, error: 'error', filter: filter }, action)).toEqual(stateAfter)
-})
-
-// FETCH_ACCOUNT_SUCCESS
-test('Should FETCH_ACCOUNT_SUCCESS', () => {
-  const action = {
-    account: [{ balance: 0 }],
-    address: "0293f9b10a4453667db7fcfe74728c9d821add4b",
-    name: "GreeterA",
-    type: "FETCH_ACCOUNT_SUCCESS"
-  }
-
-  let contract = contractState;
-  contract['GreeterA']['instances'][0]['selected'] = true;
-
-  let expectedContract = contractState;
-  expectedContract['GreeterA']['instances'][0]['selected'] = true;
-  expectedContract['GreeterA']['instances'][0]['balance'] = 0;
-
-  const stateAfter = {
-    contracts: expectedContract,
-    filter: filter,
-    error: null
-  }
-  expect(reducer({ contracts: contract, filter: filter }, action)).toEqual(stateAfter)
-})
-
