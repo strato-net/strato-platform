915f5c23baa3cde842e8fc2a5eb8e1215bc49bbf
nikitamendelbaum <nikitamendelbaum@gmail.com>
2017-12-12 14:13:13 +0700

    apex check added; tracking temporary off

--- run.sh
+++ run.sh
@@ -29,5 +29,12 @@ if [ "$authBasic" != true ] ; then
 	sed -i '/auth_basic/d' /etc/nginx/nginx.conf
 fi
 
+echo 'Waiting for apex to be available...'
+until curl --silent --output /dev/null --fail --location http://apex:3001/health-check
+do
+  sleep 0.5
+done
+echo 'apex is available'
+
 service nginx start || (tail -n 5 /var/log/nginx/error.log && exit 1) # Restart container if nginx failed to start (wait for all upstreams to become available)
 tail -n0 -F /var/log/nginx/*.log
