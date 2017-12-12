1dc3b3bef5e431001907e3c7b3e10fc544c97ad9
nikitamendelbaum <nikitamendelbaum@gmail.com>
2017-12-12 13:51:59 +0700

    service host env vars cleanup; service availability checks

--- doit.sh
+++ doit.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 set -e
+set -x
 
-function setEnv {
-  [[ -n ${!1} ]] || eval $1=$2
-  echo "$1 = ${!1}"
-}
-echo "Environment variables:"
+stratoRoot=http://${stratoHost}/eth/v1.2
+cirrusRoot=http://${cirrusHost}
 
-setEnv pguser postgres
-setEnv pgpasswd api
-setEnv pghost postgres
-setEnv stratoHost nginx
-setEnv stratourl http://$stratoHost/strato-api/eth/v1.2
-setEnv cirrusurl http://localhost/cirrus
+echo "Environment variables:
+stratoHost=${stratoHost}
+stratoRoot=${stratoRoot}
+cirrusHost=${cirrusHost}
+cirrusRoot=${cirrusRoot}
+postgres_host=${postgres_host}
+postgres_port=${postgres_port}
+postgres_user=${postgres_user}
+postgres_password=${postgres_password}
+"
 
 blocserver="/usr/bin/blockapps-bloc"
 stratoserver="/usr/bin/blockapps-strato-server"
@@ -21,7 +23,29 @@ locale-gen "en_US.UTF-8"
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 
+echo "Waiting for STRATO to be available..."
+until curl ${stratoRoot} >& /dev/null; do
+    sleep 0.5
+done
+echo "STRATO is available"
+
+# TODO: fix the circuit dependency (cirrus is depending on bloc and won't start until bloc is ready)
+#echo "Waiting for cirrus to be available..."
+#until curl ${cirrusRoot} >& /dev/null; do
+#    sleep 0.5
+#done
+#echo "cirrus is available"
+
+echo 'Waiting for postgres to be available...'
+while true; do
+    curl ${postgres_host}:${postgres_port} > /dev/null 2>&1 || EXIT_CODE=$? && true
+    if [ ${EXIT_CODE} = 52 ]; then
+        break
+    fi
+    sleep 0.5
+done
+
 $stratoserver &
 
-$blocserver --pghost="$pghost" --pguser="$pguser" --password="$pgpasswd" \
-            --stratourl="$stratourl" --cirrusurl="$cirrusurl" 2>&1
+$blocserver --pghost="$postgres_host" --pgport="$postgres_port" --pguser="$postgres_user" --password="$postgres_password" \
+            --stratourl="$stratoRoot" --cirrusurl="${cirrusRoot}" 2>&1
