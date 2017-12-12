58808c71d0232f28d5be1a633c9fc49acf0dbf97
nikitamendelbaum <nikitamendelbaum@gmail.com>
2017-12-12 15:48:36 +0700

    apex check temporary off

--- run.sh
+++ run.sh
@@ -29,12 +29,13 @@ if [ "$authBasic" != true ] ; then
 	sed -i '/auth_basic/d' /etc/nginx/nginx.conf
 fi
 
-echo 'Waiting for apex to be available...'
-until curl --silent --output /dev/null --fail --location http://apex:3001/health-check
-do
-  sleep 0.5
-done
-echo 'apex is available'
+# TODO: Enable when cirrus and postgrest calls are divided in bloch and separate hosts are used (otherwise we use nginx routed paths and there's a circuit container dependency)
+#echo 'Waiting for apex to be available...'
+#until curl --silent --output /dev/null --fail --location http://apex:3001/health-check
+#do
+#  sleep 0.5
+#done
+#echo 'apex is available'
 
 service nginx start || (tail -n 5 /var/log/nginx/error.log && exit 1) # Restart container if nginx failed to start (wait for all upstreams to become available)
 tail -n0 -F /var/log/nginx/*.log
