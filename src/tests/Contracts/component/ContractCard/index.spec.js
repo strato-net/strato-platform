e26db4a5860d9fbb6be2b62c0d2bb810404adf2c
Tanuj Soni <tanuj@blockapps.net>
2017-12-13 19:10:56 +0530

    [STRATO-testcases][WIP] Added: testcases for index

--- src/tests/Contracts/component/ContractCard/index.spec.js
+++ src/tests/Contracts/component/ContractCard/index.spec.js
@@ -1,19 +1,44 @@
 import React from 'react';
 import ContractCard from '../../../../components/Contracts/components/ContractCard/index';
 
-const props = {
-  contract: '',
-  fetchCirrusInstances: () => { },
-  fetchAccount: () => { },
-  fetchState: () => { },
-  selectContractInstance: () => { }
-}
 
-test('renders contracts card', () => {
-  let wrapper = shallow(
-    <ContractCard.WrappedComponent {...props} />
-  );
-  expect(wrapper).toMatchSnapshot();
+describe('Test ContractCard index', () => {
+  
+  test('renders contract with empty values', () => {
+    const props = {
+      contract: '',
+      fetchCirrusInstances: () => { },
+      fetchAccount: () => { },
+      fetchState: () => { },
+      selectContractInstance: () => { }
+    }
+
+    let wrapper = shallow(
+      <ContractCard.WrappedComponent {...props} />
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test('render contract with mocked values', () => {
+    const props = {
+      contract: {name: 'Greeter', contract: {"instances": [
+        {
+          "createdAt": 1512481078000,
+          "address": "0293f9b10a4453667db7fcfe74728c9d821add4b",
+          "fromBloc": true
+        }
+      ]}},
+      fetchCirrusInstances: () => { },
+      fetchAccount: () => { },
+      fetchState: () => { },
+      selectContractInstance: () => { }
+    }
+    
+    let wrapper = shallow(
+      <ContractCard.WrappedComponent {...props} />
+    );
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
 });
-
-
--- src/tests/Contracts/component/ContractMethodCall/index.spec.js
+++ src/tests/Contracts/component/ContractMethodCall/index.spec.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ContractMethodCall from '../../../../components/Contracts/components/ContractMethodCall/index';
 
-test('renders contracts card', () => {
-  const  props = {}
-  let wrapper = shallow(
-    <ContractMethodCall.WrappedComponent {...props} />
-  );
-  expect(wrapper).toMatchSnapshot();
+describe('Test ContractMethodCall index', () => {
+    
+  test('renders contracts card with empty props', () => {
+    const  props = {}
+    let wrapper = shallow(
+      <ContractMethodCall.WrappedComponent {...props} />
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+
 });
 
 
