591d2aa10ca1e0f18ea6e97ac6b730836eeee756
Tanuj Soni <tanuj@blockapps.net>
2017-12-18 19:06:39 +0530

    [STRATO-testcases] Module: HexText tests

---
+++ src/tests/HexText/index.spec.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import HexText from '../../components/HexText';
+import TestRenderer from 'react-test-renderer';
+
+describe('Test Hextext index', () => {
+
+  it('should render with empty values', () => {
+    const props = {};
+
+    const wrapper = shallow(
+      <HexText {...props} />
+    );
+
+    expect(wrapper).toMatchSnapshot();
+  })
+
+  it('should render with mocked values', () => {
+    const props = {
+      value: 'Hextext Name',
+      classes: 'class'
+    };
+
+    const wrapper = shallow(
+      <HexText {...props} />
+    );
+
+    expect(wrapper).toMatchSnapshot();
+  })
+
+  it('should copy value', () => {
+    const props = {
+      value: 'Hextext Name',
+      classes: 'class'
+    };
+
+    const wrapper = shallow(
+      <HexText {...props} />
+    );
+
+    wrapper.find('span').at(1).simulate('click', { stopPropagation() {}, preventDefault() {} }, wrapper.setState({ copied: true }))
+    expect(wrapper.state('copied')).toBe(true);    
+  })
+
+  it('should reset copied on mouse out', () => {
+    const props = {
+      value: 'Hextext Name',
+      classes: 'class'
+    };
+
+    const wrapper = mount(
+      <HexText {...props} />
+    );
+
+    wrapper.setState({ copied: true })
+    wrapper.find('span').at(3).simulate('mouseOut');
+    expect(wrapper.state('copied')).toBe(false);
+  })
+
+})
