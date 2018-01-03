197239f29bdcd7e4fa689921eef027e71f19451b
Tanuj Soni <tanuj@blockapps.net>
2018-01-03 14:53:33 +0530

    [STRATO-testcases] Refactor: Dashboard

--- src/tests/Dashboard/dashboard.action.spec.js
+++ src/tests/Dashboard/dashboard.action.spec.js
@@ -16,88 +16,114 @@ import {
 } from '../../components/Dashboard/dashboard.action'
 import { dashboard } from './dashboardMock';
 
-describe('Test dashboard actions', () => {
+describe('Dashboard: actions', () => {
 
-  test('should load block number', () => {
-    expect(preloadBlockNumber(dashboard.lastBlockNumber)).toMatchSnapshot();
-  })
+  describe('blockNumber:', () => {
 
-  test('should update block number', () => {
-    let lastBlockNumber = 5;
-    expect(updateBlockNumber(lastBlockNumber)).toMatchSnapshot();
-  })
+    test('load', () => {
+      expect(preloadBlockNumber(dashboard.lastBlockNumber)).toMatchSnapshot();
+    });
 
-  test('should load contracts count', () => {
-    expect(preloadContractCount(dashboard.contractsCount)).toMatchSnapshot();
-  })
+    test('update', () => {
+      let lastBlockNumber = 5;
+      expect(updateBlockNumber(lastBlockNumber)).toMatchSnapshot();
+    });
 
-  test('should update contracts count', () => {
-    let contractsCount = 2;
-    expect(updateContractCount(contractsCount)).toMatchSnapshot();
-  })
+  });
 
-  test('should load users count', () => {
-    expect(preloadUsersCount(dashboard.usersCount)).toMatchSnapshot();
-  })
+  describe('contractsCount:', () => {
 
-  test('should update users count', () => {
-    let usersCount = 2;
-    expect(updateUsersCount(usersCount)).toMatchSnapshot();
-  })
+    test('load', () => {
+      expect(preloadContractCount(dashboard.contractsCount)).toMatchSnapshot();
+    });
 
-  test('should load transactions count', () => {
-    expect(preloadTransactionsCount(dashboard.transactionsCount)).toMatchSnapshot();
-  })
+    test('update', () => {
+      let contractsCount = 2;
+      expect(updateContractCount(contractsCount)).toMatchSnapshot();
+    });
 
-  test('should update transactions count', () => {
-    let transactionsCount = 2;
-    expect(updateTransactionCount(transactionsCount)).toMatchSnapshot();
-  })
+  });
 
-  test('should load block propagation', () => {
-    expect(preloadBlockPropagation(dashboard.blockPropagation)).toMatchSnapshot();
-  })
+  describe('usersCount:', () => {
 
-  test('should update block propagation', () => {
-    let blockPropagation = [
-      {
-        "x": 0,
-        "y": 0
-      },
-      {
-        "x": 1,
-        "y": 4
-      }
-    ];
-    expect(updateBlockPropagation(blockPropagation)).toMatchSnapshot();
-  })
+    test('load', () => {
+      expect(preloadUsersCount(dashboard.usersCount)).toMatchSnapshot();
+    });
 
-  test('should load block difficulty', () => {
-    expect(preloadBlockDifficulty(dashboard.blockDifficulty)).toMatchSnapshot();
-  })
+    test('update', () => {
+      let usersCount = 2;
+      expect(updateUsersCount(usersCount)).toMatchSnapshot();
+    });
 
-  test('should update block difficulty', () => {
-    let blockDifficulty = [
-      {
-        "x": 0,
-        "y": 5
-      }
-    ];
-    expect(updateBlockDifficulty(blockDifficulty)).toMatchSnapshot();
-  })
+  });
 
-  test('should load transaction type', () => {
-    expect(preloadTransactionType(dashboard.transactionTypes)).toMatchSnapshot();
-  })
+  describe('transactionsCount:', () => {
 
-  test('should update transaction type', () => {
-    let transactionTypes = [
-      {
-        "val": 3,
-        "type": "FunctionCall"
-      }
-    ];
-    expect(updateTransactionType(transactionTypes)).toMatchSnapshot();
-  })
+    test('load', () => {
+      expect(preloadTransactionsCount(dashboard.transactionsCount)).toMatchSnapshot();
+    });
+
+    test('update', () => {
+      let transactionsCount = 2;
+      expect(updateTransactionCount(transactionsCount)).toMatchSnapshot();
+    });
+  });
+
+  describe('blockPropagation:', () => {
+
+    test('load', () => {
+      expect(preloadBlockPropagation(dashboard.blockPropagation)).toMatchSnapshot();
+    });
+
+    test('update', () => {
+      let blockPropagation = [
+        {
+          "x": 0,
+          "y": 0
+        },
+        {
+          "x": 1,
+          "y": 4
+        }
+      ];
+      expect(updateBlockPropagation(blockPropagation)).toMatchSnapshot();
+    });
+  });
+
+  describe('blockDifficulty:', () => {
+
+    test('load', () => {
+      expect(preloadBlockDifficulty(dashboard.blockDifficulty)).toMatchSnapshot();
+    });
+
+    test('update', () => {
+      let blockDifficulty = [
+        {
+          "x": 0,
+          "y": 5
+        }
+      ];
+      expect(updateBlockDifficulty(blockDifficulty)).toMatchSnapshot();
+    });
+
+  });
+
+  describe('transactionTypes:', () => {
+
+    test('load', () => {
+      expect(preloadTransactionType(dashboard.transactionTypes)).toMatchSnapshot();
+    });
+
+    test('update', () => {
+      let transactionTypes = [
+        {
+          "val": 3,
+          "type": "FunctionCall"
+        }
+      ];
+      expect(updateTransactionType(transactionTypes)).toMatchSnapshot();
+    });
+
+  });
 
 })
--- src/tests/Dashboard/index.spec.js
+++ src/tests/Dashboard/index.spec.js
@@ -7,103 +7,95 @@ import {
   subscribeRoomMock
 } from './dashboardMock';
 
-describe('Test Dashboard index', () => {
+describe('Dashboard: index', () => {
 
-  test('should render dashboard with empty values', () => {
-    const props = {
-      dashboard: {
-        "transactionsCount": [],
-        "blockPropagation": [],
-        "blockDifficulty": [],
-        "transactionTypes": []
-      },
-      node: {
-        "name": "LOCALHOST",
-        "peers": {},
-        "coinbase": ""
-      },
-      subscribeRoom: () => { },
-      unSubscribeRoom: () => { }
-    }
+  describe('render component', () => {
 
-    const wrapper = shallow(
-      <Dashboard.WrappedComponent {...props} />
-    );
+    test('without values', () => {
+      const props = {
+        dashboard: {
+          "transactionsCount": [],
+          "blockPropagation": [],
+          "blockDifficulty": [],
+          "transactionTypes": []
+        },
+        node: {
+          "name": "LOCALHOST",
+          "peers": {},
+          "coinbase": ""
+        },
+        subscribeRoom: jest.fn(),
+        unSubscribeRoom: jest.fn()
+      }
+      const wrapper = shallow(
+        <Dashboard.WrappedComponent {...props} />
+      );
+      expect(wrapper).toMatchSnapshot();
+    });
 
-    expect(wrapper).toMatchSnapshot();
+    test('with values', () => {
+      const props = {
+        dashboard,
+        node,
+        subscribeRoom: jest.fn(),
+        unSubscribeRoom: jest.fn()
+      };
+      const wrapper = shallow(
+        <Dashboard.WrappedComponent {...props} />
+      );
+      expect(wrapper).toMatchSnapshot();
+    });
+
   });
 
-  test('should render dashboard with mocked values', () => {
-    const props = {
-      dashboard,
-      node,
-      subscribeRoom: () => { },
-      unSubscribeRoom: () => { }
-    }
-
-    const wrapper = shallow(
-      <Dashboard.WrappedComponent {...props} />
-    );
-
-    expect(wrapper).toMatchSnapshot();
-  });
-
-  test('should test component functions', () => {
+  test('valid functions as a props', () => {
     const props = {
       dashboard,
       node,
       subscribeRoom: jest.fn().mockReturnValue('subscribeRoom'),
       unSubscribeRoom: jest.fn().mockReturnValue('unSubscribeRoom')
-    }
-
+    };
     const wrapper = shallow(
       <Dashboard.WrappedComponent {...props} />
     );
-
     expect(wrapper.instance().props.subscribeRoom()).toBe('subscribeRoom');
     expect(wrapper.instance().props.unSubscribeRoom()).toBe('unSubscribeRoom');
   });
 
-  test('should invoke componentWillUnmount', () => {
+  test('componentWillUnmount', () => {
     const props = {
       dashboard,
       node,
-      subscribeRoom: () => { },
+      subscribeRoom: jest.fn(),
       unSubscribeRoom: jest.fn()
-    }
-
+    };
     const wrapper = shallow(
       <Dashboard.WrappedComponent {...props} />
     );
-
-    wrapper.instance().componentWillUnmount();
+    wrapper.unmount();
     expect(props.unSubscribeRoom).toHaveBeenCalled();
     expect(props.unSubscribeRoom.mock.calls).toEqual(unSubscribeRoomMock);
     expect(wrapper).toMatchSnapshot();
   });
 
-  test('should invoke subscribeRoom on componentDidMount', () => {
+  test('componentDidMount', () => {
     const props = {
       dashboard,
       node,
       subscribeRoom: jest.fn()
-    }
-
+    };
     const wrapper = shallow(
       <Dashboard.WrappedComponent {...props} />
     );
-
     expect(props.subscribeRoom).toHaveBeenCalled();
     expect(props.subscribeRoom.mock.calls).toEqual(subscribeRoomMock);
   });
 
-  test('test mapStateToProps function', () => {
+  test('mapStateToProps', () => {
     const state = {
       dashboard,
       node
     }
-
     expect(mapStateToProps(state)).toMatchSnapshot();
   });
-
-})
+});
