4b35c967f217d8c3510028bf3f3d6dc17542d73a
Charles Crain <craincharles@gmail.com>
2018-01-05 20:03:49 +0000

    ErrorCodes passing

--- bloc/bloc22/server/test/contracts/ErrorCodesGetSource.sol
+++ bloc/bloc22/server/test/contracts/ErrorCodesGetSource.sol
@@ -11,6 +11,5 @@ contract ErrorCodes {
   }
 
   function __getSource__() constant returns (string) {
-    return "contract ErrorCodes {  enum ErrorCodes {    NULL,    SUCCESS,    ERROR,    NOT_FOUND,    EXISTS,    RECURSIVE,    INSUFFICIENT_BALANCE  }}";
-  }
+    return "contract ErrorCodes {\n\n  enum ErrorCodes {\n    NULL,\n    SUCCESS,\n    ERROR,\n    NOT_FOUND,\n    EXISTS,\n    RECURSIVE,\n    INSUFFICIENT_BALANCE\n  }\n}\n";  }
 }
