6b26b1407970e6d52b5246c82c5328af2a835e7a
Tanuj Soni <tanuj@blockapps.net>
2018-02-06 19:35:32 +0530

    [STRATO-755][WIP] Social Sharing

--- src/components/ApplicationCard/index.js
+++ src/components/ApplicationCard/index.js
@@ -5,7 +5,6 @@ import { withRouter } from 'react-router-dom';
 import { launchApp } from '../Applications/applications.actions';
 import { Menu, MenuItem, Popover, Position, Button } from '@blueprintjs/core';
 import ReactLoading from 'react-loading';
-import Login from '../Login';
 import { openLoginOverlay } from '../User/user.actions';
 
 class ApplicationCard extends Component {
@@ -60,17 +59,17 @@ class ApplicationCard extends Component {
     return (
       <div className="pt-card app-card">
         <div className="row">
-          <div className="col-sm-2 text-right">
+          <div className="col-sm-1 text-center">
             <i className="fa fa-rocket fa-5x" aria-hidden="true"></i>
           </div>
-          <div className="col-sm-10">
+          <div className="col-sm-11">
             <div className="row">
-              <div className="col-sm-10">
+              <div className="col-sm-8">
                 <div className="heading">
                   <strong> {app.appName} </strong> - <span> v{app.version} </span>
                 </div>
               </div>
-              <div className="col-sm-2">
+              <div className="col-sm-4 launchAndShare">
                 <Popover
                   position={Position.BOTTOM}
                   content={
@@ -89,24 +88,23 @@ class ApplicationCard extends Component {
                       <MenuItem onClick={this.handleDelete} text="State of Dapps" />
                     </Menu>}
                   popoverClassName={"popoverClassName"}
+                  className={"share-popover"}
                 >
                   <button
-                    className="pt-button pt-intent-primary"
+                    className="pt-button pt-intent-primary pull-right"
                     onClick={this.handleClick}
                   >
                     Share
                   </button>
                 </Popover>
-                {!this.props.isLoggedIn? this.renderLogin() : (app.isLoading ?
+                {!this.props.isLoggedIn ? this.renderLogin() : (app.isLoading ?
                   <ReactLoading type="bars" color="#f5f8fa" className="pull-right" height={0} width={30} /> :
-                  <a rel="noopener noreferrer">
-                    <button
-                      className="pt-button pt-intent-primary pull-right"
-                      onClick={() => this.launch(app)}
-                    >
-                      Launch
-                    </button>
-                  </a>)
+                  <button
+                    className="pt-button pt-intent-primary pull-right"
+                    onClick={() => this.launch(app)}
+                  >
+                    Launch
+                    </button>)
                 }
               </div>
             </div>
