f48641243e3b417589cbddcab118722b0bafa54d
Tim Holland <tim@blockapps.net>
2018-02-23 16:06:18 -0500

    Initialize apex_test in tests (thanks nikita!)

--- api/test/auth.test.js
+++ api/test/auth.test.js
@@ -3,14 +3,19 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const assert = chai.assert;
 
+const initDb = require('../migrations/init-script/initdb.js');
+const models = require('../models');
 const app = require('../app');
 
 
 chai.use(chaiHttp);
 
-// TODO: write the tests
-
 describe('App', function() {
+  before(async function() {
+    await initDb();
+    await models.sequelize.sync();
+  });
+
   describe('post /login', function() {
     it('replies Bad Request without un/pw', function(done) {
       chai.request(app)
@@ -21,5 +26,15 @@ describe('App', function() {
           done();
         });
     });
+    it('replies 401 with incorrect un/pw', function(done) {
+      chai.request(app)
+        .post('/login')
+        .send({username: "me", password: "hunter2"})
+        .end(function(err, res) {
+          assert(res.text.includes("does not exist"));
+          assert.equal(res.status, '401');
+          done();
+        });
+     });
   });
 });
