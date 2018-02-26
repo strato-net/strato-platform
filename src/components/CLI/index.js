129b3ccbddd37a0cea71749da1af45848f242574
Tanuj Soni <tanuj@blockapps.net>
2018-02-26 18:23:45 +0530

    [STRATO-783][WIP]: Walkthrough css fixes

--- src/components/CLI/index.js
+++ src/components/CLI/index.js
@@ -9,34 +9,34 @@ class CLI extends Component {
 
   render() {
     return (
-        <div>
-          <div className="pt-dialog-body">
-            <div className="pt-form-group">
-              <div className="pt-form-group pt-intent-danger">
-                <div className="pt-form-content">
-                  <a href="https://www.npmjs.com/package/strato-cli">Click here</a> to learn how you can easily deploy dapps to STRATO
+      <div>
+        <div className="pt-dialog-body">
+          <div className="pt-form-group">
+            <div className="pt-form-group pt-intent-danger">
+              <div className="pt-form-content">
+                <a href="https://www.npmjs.com/package/strato-cli">Click here</a> to learn how you can easily deploy dapps to STRATO
                 </div>
-              </div>
             </div>
           </div>
-
-          {this.props.addApp ? null : <div className="pt-dialog-footer">
-            <div className="pt-dialog-footer-actions">
-              <Button text="Back" onClick={() => {
-                mixpanelWrapper.track('faucet_close_click');
-                this.props.handleBack();
-              }} />
-              <Button
-                intent={Intent.PRIMARY}
-                onClick={() => {
-                  this.setState({ initialModal: "Faucet" });
-                  this.props.handleFinish();
-                }}
-                text="Finish"
-              />
-            </div>
-          </div>}
         </div>
+
+        {this.props.addApp ? null : <div className="pt-dialog-footer">
+          <div className="pt-dialog-footer-actions">
+            <Button text="Back" onClick={() => {
+              mixpanelWrapper.track('faucet_close_click');
+              this.props.handleBack();
+            }} />
+            <Button
+              intent={Intent.PRIMARY}
+              onClick={() => {
+                this.setState({ initialModal: "Faucet" });
+                this.props.handleFinish();
+              }}
+              text="Finish"
+            />
+          </div>
+        </div>}
+      </div>
     );
   }
 }
