6071048992a5b05b7dc60ad5feac0207afeb6c0e
Tim Holland <tim@blockapps.net>
2018-03-08 17:44:51 -0500

    Sketch out testdata for contract inclusion

---
+++ strato-init/test/testdata/contracts/Vehicle.sol
@@ -0,0 +1,16 @@
+contract Vehicle {
+  uint timestamp;
+  string public vin;
+  string public s0;
+  string public s1;
+
+  function vin() public returns (string) {
+    return vin;
+  }
+
+  function init(string _vin, string _s0) public {
+    timestamp = block.timestamp;
+    vin = _vin;
+    _s0 = s0;
+  }
+}
