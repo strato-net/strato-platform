a7561189cf7f86cb3e3758b9a6509c3ff0dc6cba
Tim Holland <tim@blockapps.net>
2018-03-12 11:42:54 -0400

    Insert AccountInfos to genesisInfo.json

--- strato-init/src/Blockchain/Generation.hs
+++ strato-init/src/Blockchain/Generation.hs
@@ -1,11 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Blockchain.Generation (
   insertContracts
 ) where
 
-import Data.ByteString
-import qualified Data.Aeson as Aeson
+import Data.ByteString hiding (map, count)
+import qualified Data.ByteString.Base16 as B16
 
 import Blockchain.Strato.Model.Address
+import Blockchain.Strato.Model.SHA
+import Blockchain.Data.GenesisInfo
 
-insertContracts :: ByteString -> Address -> Integer -> Aeson.Value -> Aeson.Value
-insertContracts _ _ _ = id
+insertContracts :: ByteString -> Address -> Integer -> GenesisInfo -> GenesisInfo
+insertContracts code start count gi =
+  let initial = genesisInfoAccountInfo gi
+      (decoded, extra) = B16.decode code
+      codeHash = if extra /= ""
+                   then error "bytecode not encoded in base16"
+                   -- else (ByteArray.convert $ hash decoded :: Digest Keccak_256) :: ByteString
+                   else superProprietaryStratoSHAHash decoded
+      rng = [toInteger start..(toInteger start) + count - 1]
+      range = map fromInteger rng
+  in gi {genesisInfoAccountInfo = initial ++ map (mkContract codeHash) range}
+
+mkContract :: SHA -> Address -> AccountInfo
+mkContract code addr = Contract addr 0 code
--- strato-init/test/GenerationSpec.hs
+++ strato-init/test/GenerationSpec.hs
@@ -1,28 +1,45 @@
 {-# LANGUAGE OverloadedStrings #-}
 module GenerationSpec where
-import Data.Aeson
-import Data.Aeson.Diff
 import qualified Data.ByteString as BS
+import qualified Data.ByteString.Base16 as B16
 import Test.Hspec
 
+import Blockchain.Data.GenesisInfo
 import Blockchain.Generation
 import Blockchain.Strato.Model.Address
+import Blockchain.Strato.Model.SHA
 
-jsonShouldBe :: Value -> Value -> Expectation
-jsonShouldBe l r = diff l r `shouldBe` Patch []
+start :: GenesisInfo
+start = defaultGenesisInfo
 
 emptyContract :: BS.ByteString
-emptyContract = "60606040525b600080fd00a165627a7a723058209b97b86115f9dfccb5f10ab93044730e948264e405825b26dccd1605775663710029"
+emptyContract = B16.encode "60606040525b600080fd00a165627a7a723058209b97b86115f9dfccb5f10ab93044730e948264e405825b26dccd1605775663710029"
+
+emptyHash :: SHA
+emptyHash = SHA 0x6ee829aad0ec74494a6cf1433f563bbbe7e556a2a067d6ecd596dda0aecd8202
 
 sharedStart :: Address
 sharedStart = Address 0x692a70d2e424a56d2c6c27aa97d1a86395877b3a
+-- sharedStart = Address 0x17
 
 spec :: Spec
 spec = do
-  describe "hello world" $ do
-    it "should do nothing" $ do
-      let input = Null
-      let want = input
-      let got = insertContracts emptyContract sharedStart 0 input
-      got `jsonShouldBe` want
+  describe "Insertion of empty contracts" $ do
+    it "should insert no contracts" $
+      let input = defaultGenesisInfo
+          want = []
+          got = insertContracts emptyContract sharedStart 0 input
+      in genesisInfoAccountInfo got `shouldBe` want
 
+    it "should insert 1 contract" $
+      let input = defaultGenesisInfo
+          want = [Contract sharedStart 0 emptyHash]
+          got = insertContracts emptyContract sharedStart 1 input
+      in genesisInfoAccountInfo got `shouldBe` want
+
+    it "should insert 1m contracts" $
+      let total = 1000000 :: Integer
+          input = defaultGenesisInfo
+          want = map (\n -> Contract (sharedStart + fromIntegral n) 0 emptyHash) [0..total-1]
+          got = insertContracts emptyContract sharedStart total input
+      in genesisInfoAccountInfo got `shouldBe` want
