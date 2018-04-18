a07a8d870b643d2cabe48deedc379e4fa31d154f
Tanuj Soni <tanuj@blockapps.net>
2018-04-18 19:25:59 +0530

    [STRATO-889] Unset localStorage token for enterprise mode

--- smd-ui/src/lib/localStorage.js
+++ smd-ui/src/lib/localStorage.js
@@ -1,5 +1,12 @@
+import { isModePublic } from './checkMode';
+
 export function currentUser() {
   let token = localStorage.getItem('token');
+
+  if (!isModePublic() && token) {
+    localStorage.removeItem('token');
+    return {};
+  }
 
   if (token !== null) {
     return JSON.parse(token);
