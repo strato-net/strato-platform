aaaf31115f0042bc2f4e335005e602e53789f7c3
Tanuj Soni <tanuj@blockapps.net>
2018-04-20 09:52:36 +0530

    [STRATO-889] Testcase: Sidebar

--- smd-ui/src/tests/SideBar/index.spec.js
+++ smd-ui/src/tests/SideBar/index.spec.js
@@ -1,18 +1,72 @@
 import React from 'react';
 import SideBar from "../../components/SideBar";
+import * as checkMode from '../../lib/checkMode';
 
 describe('SideBar: index', () => {
   let wrapper;
 
-  beforeEach(() => {
+  test('render component for public mode', () => {
+    checkMode.isModePublic = jest.fn().mockReturnValue(true);
     wrapper = shallow(<SideBar />);
-  });
-
-  test('render component', () => {
     expect(wrapper.debug()).toMatchSnapshot();
   });
 
-  describe('sequence of links', () => {
+  describe('sequence of links for public mode', () => {
+
+    beforeEach(() => {
+      checkMode.isModePublic = jest.fn().mockReturnValue(true);
+      wrapper = shallow(<SideBar />);
+    });
+
+    test('first position /apps', () => {
+      wrapper.find('NavLink').at(0).simulate('click');
+      expect(wrapper.find('NavLink').get(0)).toMatchSnapshot();
+    });
+
+    test('secound position /home', () => {
+      wrapper.find('NavLink').at(1).simulate('click');
+      expect(wrapper.find('NavLink').get(1)).toMatchSnapshot();
+    });
+
+    test('third position /blocks', () => {
+      wrapper.find('NavLink').at(2).simulate('click');
+      expect(wrapper.find('NavLink').get(2)).toMatchSnapshot();
+    });
+
+    test('fourth position /transactions', () => {
+      wrapper.find('NavLink').at(3).simulate('click');
+      expect(wrapper.find('NavLink').get(3)).toMatchSnapshot();
+    });
+
+    test('fifth position /accounts', () => {
+      wrapper.find('NavLink').at(4).simulate('click');
+      expect(wrapper.find('NavLink').get(4)).toMatchSnapshot();
+    });
+
+    test('sixth position /contracts', () => {
+      wrapper.find('NavLink').at(5).simulate('click');
+      expect(wrapper.find('NavLink').get(5)).toMatchSnapshot();
+    });
+
+    test('seventh position /code_editor', () => {
+      wrapper.find('NavLink').at(6).simulate('click');
+      expect(wrapper.find('NavLink').get(6)).toMatchSnapshot();
+    });
+
+  });
+
+  test('render component for enterprise mode', () => {
+    checkMode.isModePublic = jest.fn().mockReturnValue(false);
+    wrapper = shallow(<SideBar />);
+    expect(wrapper.debug()).toMatchSnapshot();
+  });
+
+  describe('sequence of links for enterprise mode', () => {
+
+    beforeEach(() => {
+      checkMode.isModePublic = jest.fn().mockReturnValue(false);
+      wrapper = shallow(<SideBar />);
+    });
 
     test('first position /home', () => {
       wrapper.find('NavLink').at(0).simulate('click');
