2e29d92f4aa36ff30eb5ca25e5b0b445d7f3165e
Tanuj Soni <tanuj@blockapps.net>
2018-04-26 15:34:12 +0530

    [STRATO-751] Display transactions for a block

--- smd-ui/src/components/Blocks/components/BlockView/index.js
+++ smd-ui/src/components/Blocks/components/BlockView/index.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import mixpanelWrapper from '../../../../lib/mixpanelWrapper';
-import {connect} from 'react-redux';
-import {withRouter} from 'react-router-dom';
-import {Button, Text} from '@blueprintjs/core';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { Button, Text, Position, Tooltip } from '@blueprintjs/core';
 import { fetchBlockData } from '../../../BlockData/block-data.actions';
 import HexText from '../../../HexText';
 import moment from 'moment';
@@ -13,9 +13,41 @@ class BlockView extends Component {
     this.props.fetchBlockData();
   }
 
+  handleClick(hash) {
+    mixpanelWrapper.track('transactions_row_click');
+    const history = this.props.history;
+    history.push('/transactions/' + hash);
+  }
+
   render() {
+    const self = this
     const blockNumber = this.props.match.params.block;
     const block = this.props.block;
+    const recipientTransactions = block && block.receiptTransactions
+    let txRows = recipientTransactions && recipientTransactions.map(
+      function (tx, i) {
+        return (
+          <tr key={i} onClick={() => {
+            self.handleClick(tx.hash)
+          }}>
+            <td width="60%">
+              <HexText value={tx.hash} classes="small smd-pad-4" />
+            </td>
+            <td width="20%">
+              <Text ellipsize={true}>
+                <Tooltip tooltipClassName="smd-padding-8" content={tx.value + ' wei'} position={Position.TOP_LEFT}>
+                  <small>{tx.value} wei</small>
+                </Tooltip>
+              </Text>
+            </td>
+            <td width="20%">
+              <small>{tx.transactionType}</small>
+            </td>
+          </tr>
+        )
+      }
+    );
+
     return (
       <div className="container-fluid pt-dark">
         <div className="row">
@@ -24,7 +56,7 @@ class BlockView extends Component {
           </div>
           <div className="col-sm-3 smd-pad-16 text-right">
             <Button
-              onClick={(e) => {mixpanelWrapper.track('block_view_go_back_click'); this.props.history.goBack()}}
+              onClick={(e) => { mixpanelWrapper.track('block_view_go_back_click'); this.props.history.goBack() }}
               className="pt-icon-arrow-left"
               text="Back"
             />
@@ -34,30 +66,30 @@ class BlockView extends Component {
           <div className="row">
             <div className="col-sm-12">
               <div className="pt-card">
-                  <table>
-                    <tbody>
+                <table>
+                  <tbody>
                     <tr colSpan={2}>No data</tr>
-                    </tbody>
-                  </table>
-                </div>
+                  </tbody>
+                </table>
               </div>
             </div>
-            :
-            <div className="row">
-              <div className="col-sm-12">
-                <div className="pt-card">
-                  <table className="pt-table pt-str">
-                    <thead>
+          </div>
+          :
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="pt-card">
+                <table className="pt-table pt-str">
+                  <thead>
                     <tr>
                       <th>Field</th>
                       <th>Value</th>
                     </tr>
-                    </thead>
-                    <tbody>
+                  </thead>
+                  <tbody>
                     <tr>
                       <td>Parent Hash</td>
                       <td>
-                        <HexText value={block.blockData.parentHash} classes="small smd-pad-4"/>
+                        <HexText value={block.blockData.parentHash} classes="small smd-pad-4" />
                       </td>
                     </tr>
                     <tr>
@@ -83,19 +115,19 @@ class BlockView extends Component {
                     <tr>
                       <td>Coinbase</td>
                       <td>
-                        <HexText value={block.blockData.coinbase} classes="small smd-pad-4"/>
+                        <HexText value={block.blockData.coinbase} classes="small smd-pad-4" />
                       </td>
                     </tr>
                     <tr>
                       <td>State Root</td>
                       <td>
-                        <HexText value={block.blockData.stateRoot} classes="small smd-pad-4"/>
+                        <HexText value={block.blockData.stateRoot} classes="small smd-pad-4" />
                       </td>
                     </tr>
                     <tr>
                       <td>Transactions Root</td>
                       <td>
-                        <HexText value={block.blockData.transactionsRoot} classes="small smd-pad-4"/>
+                        <HexText value={block.blockData.transactionsRoot} classes="small smd-pad-4" />
                       </td>
                     </tr>
                     <tr>
@@ -108,12 +140,34 @@ class BlockView extends Component {
                         </small>
                       </td>
                     </tr>
-                    </tbody>
-                  </table>
-                </div>
+                  </tbody>
+                </table>
               </div>
             </div>
-          }
+          </div>
+        }
+
+        <div className="row">
+          <div className="col-sm-12">
+            <div className="h3">Transactions </div>
+            <table className="pt-table pt-interactive pt-condensed pt-striped"
+              style={{ tableLayout: 'fixed', width: "100%" }}>
+              <thead>
+                <tr>
+                  <th width="60%"><h5>Hash</h5></th>
+                  <th width="20%" className="text-right"><h5>Value</h5></th>
+                  <th width="20%"><h5>Type</h5></th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {txRows && txRows.length === 0 ? <tr>
+                  <td colSpan={5}>No Data</td>
+                </tr> : txRows}
+              </tbody>
+            </table>
+          </div>
+        </div>
 
       </div>
     );
@@ -125,7 +179,7 @@ export function mapStateToProps(state, ownProps) {
   return {
     block: state.blockData.blockData.filter((val) => {
       return val.blockData.number === blockNumber
-    })[0] || state.queryEngine.queryResult.filter((val) => {return val.blockData.number === blockNumber})[0]
+    })[0] || state.queryEngine.queryResult.filter((val) => { return val.blockData.number === blockNumber })[0]
   };
 }
 
