748a1c384d86d1519eb6310f1cc02c71f5648b6b
Tanuj Soni <tanuj@blockapps.net>
2018-05-22 19:13:15 +0530

    [STRATO-949] Remove fetching users in public mode

--- smd-ui/src/components/LaunchPad/index.js
+++ smd-ui/src/components/LaunchPad/index.js
@@ -25,7 +25,7 @@ class LaunchPad extends Component {
 
   componentWillMount() {
     if (this.props.launchPad.firstLoad) {
-      if (Object.getOwnPropertyNames(this.props.accounts).length === 0) {
+      if (Object.getOwnPropertyNames(this.props.accounts).length === 0 && !isModePublic()) {
         this.props.fetchAccounts(true, false);
       }
       this.props.loadLaunchPad();
@@ -107,14 +107,11 @@ class LaunchPad extends Component {
         <option value={isPublicMode ? this.props.currentUser.username : null}>
           {isPublicMode && this.props.currentUser.username}
         </option>
-        {
-          (!isPublicMode && users) ?
-            users.map((user, i) => {
-              return (
-                <option key={'user' + i} value={user}>{user}</option>
-              )
-            })
-            : ''
+        {users.map((user, i) => {
+          return (
+            <option key={'user' + i} value={user}>{user}</option>
+          )
+        })
         }
       </Field>
     </div>)
@@ -123,7 +120,7 @@ class LaunchPad extends Component {
   renderAddress = (isPublicMode) => {
     const userAddresses = this.props.accounts && this.props.launchPad.username ?
       Object.getOwnPropertyNames(this.props.accounts[this.props.launchPad.username])
-      : null;
+      : [];
     return (<div className={isPublicMode ? "" : "pt-select"}>
       <Field
         className="pt-input"
@@ -137,13 +134,11 @@ class LaunchPad extends Component {
           {isPublicMode && this.props.currentUser.accountAddress}
         </option>
         {
-          (!isPublicMode && userAddresses) ?
-            userAddresses.map((address, i) => {
-              return (
-                <option key={address} value={address}>{address}</option>
-              )
-            })
-            : ''
+          userAddresses.map((address, i) => {
+            return (
+              <option key={address} value={address}>{address}</option>
+            )
+          })
         }
       </Field>
     </div>)
