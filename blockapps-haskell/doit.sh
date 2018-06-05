63b225eb5c289eba7a13a757da91e8a6d834505a
nikitamendelbaum <nikitamendelbaum@gmail.com>
2018-06-05 16:18:28 -0400

    slipstream kafka config

--- blockapps-haskell/doit.sh
+++ blockapps-haskell/doit.sh
@@ -5,6 +5,7 @@ set -x
 
 stratoRoot=http://${stratoHost}/eth/v1.2
 cirrusRoot=http://${cirrusHost}
+kafkaPort=9092
 
 echo "Environment variables:
 slipstream:
@@ -14,8 +15,8 @@ slipstream:
 --password=\$postgres_password="${postgres_password}"
 --database=\$postgres_slipstream_db="${postgres_slipstream_db}"
 --stratourl\$stratoRoot="${stratoRoot}"
---kafkahost=\$kafka_host="${kafkahost}"
---kafkaport=\$kafka_port="${kafkaport}"
+--kafkahost=\$kafkaHost="${kafkaHost}"
+--kafkaport=${kafkaPort}"
 
 strato-server:
 no vars/flags set
@@ -59,7 +60,7 @@ mkdir logs
 # TODO: add kafka/zk connection flags to run slipstream (when slipstream supports them) and may be others (strato? bloc?..)
 /usr/bin/slipstream --pghost="$postgres_host" --pgport="$postgres_port" --pguser="$postgres_user" --password="$postgres_password" \
             --database="$postgres_slipstream_db"  --stratourl="$stratourl" \
-            --kafkahost="$kafkahost" --kafkaport="$kafkaport" >> logs/slipstream 2>&1 &
+            --kafkahost="$kafkaHost" --kafkaport="$kafkaPort" >> logs/slipstream 2>&1 &
 
 /usr/bin/blockapps-bloc --pghost="$postgres_host" --pgport="$postgres_port" --pguser="$postgres_user" --password="$postgres_password" \
             --stratourl="$stratoRoot" --loglevel="${loglevel:-4}" --cirrusurl="$cirrusRoot" +RTS -N1 2>&1
