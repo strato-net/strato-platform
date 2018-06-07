7b4bfdd4ddf87bd28ca582d49278bc6288daf933
ccossette <carlocossette@gmail.com>
2018-06-07 12:28:08 -0700

    Modified Config

--- blockapps-haskell/doit.sh
+++ blockapps-haskell/doit.sh
@@ -9,53 +9,18 @@ kafkaPort=9092
 
 echo "Environment variables:
 slipstream:
-<<<<<<< 5a0e9ef6fe7ec52c47d851b8a5693462e679754a
-<<<<<<< 483e77a7f0dcefeecb2284cebc0a59e3ea56a6f1
-=======
->>>>>>> slipstream with postgrest on
 --pghost=\$postgres_host="${postgres_host}"
 --pgport=\$postgres_port="${postgres_port}"
 --pguser=\$postgres_user="${postgres_user}"
 --password=\$postgres_password="${postgres_password}"
 --database=\$postgres_slipstream_db="${postgres_slipstream_db}"
-<<<<<<< ace13098df5d7782d6f03348411e9e3b6b40a0fb
-<<<<<<< 5a0e9ef6fe7ec52c47d851b8a5693462e679754a
-=======
---pghost=postgres_host=${postgres_host}
---pgport=postgres_port=${postgres_port}
---pguser=postgres_user=${postgres_user}
---password=postgres_password=${postgres_password}
---database=postgres_slipstream_db=${postgres_slipstream_db}
->>>>>>> slipstream added to bloc container; cirrus off (step 1); tests temporary off
-=======
->>>>>>> slipstream with postgrest on
-=======
---stratourl\$stratoRoot="${stratoRoot}"
-<<<<<<< dae5cb4044bcd91dab6748652964c4d9d7464d07
---kafkahost=\$kafka_host="${kafkahost}"
---kafkaport=\$kafka_port="${kafkaport}"
-<<<<<<< 3f730be9ec2443c949c35334466f0c604b431554
---topicname=\$kafka_topic="${topicname}"
->>>>>>> Modified Config
-=======
->>>>>>> Removed Topic Name Suffixes
-=======
+--stratourl=\$stratoRoot="${stratoRoot}"
 --kafkahost=\$kafkaHost="${kafkaHost}"
-<<<<<<< fd1d7384efc1840461c3b67b4a8495b7be03a991
---kafkaport=${kafkaPort}"
->>>>>>> slipstream kafka config
-=======
 --kafkaport=${kafkaPort}
->>>>>>> slipstream kafka config, fix
 
 strato-server:
 no vars/flags set
-
 bloc:
-<<<<<<< 5a0e9ef6fe7ec52c47d851b8a5693462e679754a
-<<<<<<< 483e77a7f0dcefeecb2284cebc0a59e3ea56a6f1
-=======
->>>>>>> slipstream with postgrest on
 stratoHost="${stratoHost}"
 --cirrusurl=\$cirrusHost="${cirrusHost}"
 --stratourl=\$stratoRoot="${stratoRoot}"
@@ -64,19 +29,6 @@ stratoHost="${stratoHost}"
 --pguser=\$postgres_user="${postgres_user}"
 --password=\$postgres_password="${postgres_password}"
 --loglevel=\$loglevel="${loglevel:-4}"
-<<<<<<< 5a0e9ef6fe7ec52c47d851b8a5693462e679754a
-=======
-stratoHost=${stratoHost}
---cirrusurl=cirrusHost=${cirrusHost}
---stratourl=stratoRoot=${stratoRoot}
---pghost=postgres_host=${postgres_host}
---pgport=postgres_port=${postgres_port}
---pguser=postgres_user=${postgres_user}
---password=postgres_password=${postgres_password}
---loglevel=loglevel=${loglevel:-4}
->>>>>>> slipstream added to bloc container; cirrus off (step 1); tests temporary off
-=======
->>>>>>> slipstream with postgrest on
 "
 
 locale-gen "en_US.UTF-8"
@@ -99,7 +51,6 @@ while true; do
 done
 
 mkdir logs
-<<<<<<< 483e77a7f0dcefeecb2284cebc0a59e3ea56a6f1
 
 # TODO: refactor bloc (and monstrato) dockerization using supervisord for more process control, log aggregation and health monitoring
 
@@ -107,50 +58,8 @@ mkdir logs
 
 # TODO: add kafka/zk connection flags to run slipstream (when slipstream supports them) and may be others (strato? bloc?..)
 /usr/bin/slipstream --pghost="$postgres_host" --pgport="$postgres_port" --pguser="$postgres_user" --password="$postgres_password" \
-<<<<<<< 2fd8abdcc895b286428b5c70c4c182fbc5646d54
-<<<<<<< 3f730be9ec2443c949c35334466f0c604b431554
-<<<<<<< 0366ad58ccf58dde76d447c4d62f57dc30e11016
-<<<<<<< 134aebe64f1f11f164cf8f819921c6758fbedad0
-<<<<<<< 5a0e9ef6fe7ec52c47d851b8a5693462e679754a
-            --database="$postgres_slipstream_db"  --topicname="$topic_name" >> logs/slipstream 2>&1 &
-=======
-            --database="$postgres_slipstream_db"  --topicname="$topicname" \
-<<<<<<< ace13098df5d7782d6f03348411e9e3b6b40a0fb
-            --stratourl="$stratoHost" --kafkahost="$kafkahost" --kafkaport="$kafkaport" >> logs/slipstream 2>&1 &
->>>>>>> Added HFlags
-
-/usr/bin/blockapps-bloc --pghost="$postgres_host" --pgport="$postgres_port" --pguser="$postgres_user" --password="$postgres_password" \
-            --stratourl="$stratoRoot" --loglevel="${loglevel:-4}" --cirrusurl="$cirrusRoot" +RTS -N1 2>&1
-=======
-
-# TODO: refactor bloc (and monstrato) dockerization using supervisord for more process control, log aggregation and health monitoring
->>>>>>> slipstream added to bloc container; cirrus off (step 1); tests temporary off
-
-/usr/bin/blockapps-strato-server >> logs/strato-server 2>&1 &
-
-# TODO: add kafka/zk connection flags to run slipstream (when slipstream supports them) and may be others (strato? bloc?..)
-/usr/bin/slipstream --pghost="$postgres_host" --pgport="$postgres_port" --pguser="$postgres_user" --password="$postgres_password" \
-            --database="$postgres_db" >> logs/slipstream 2>&1 &
-=======
-            --database="$postgres_slipstream_db" >> logs/slipstream 2>&1 &
->>>>>>> slipstream with postgrest on
-=======
-            --database="$postgres_slipstream_db"  --topicname="$topic_name" >> logs/slipstream 2>&1 &
->>>>>>> Fixed getMessages
-=======
-            --stratourl="$stratourl" --kafkahost="$kafkahost" --kafkaport="$kafkaport" >> logs/slipstream 2>&1 &
->>>>>>> Modified Config
-=======
-            --database="$postgres_slipstream_db"  --stratourl="$stratourl" \
-<<<<<<< dae5cb4044bcd91dab6748652964c4d9d7464d07
-            --kafkahost="$kafkahost" --kafkaport="$kafkaport" >> logs/slipstream 2>&1 &
->>>>>>> Removed Topic Name Suffixes
-=======
-=======
             --database="$postgres_slipstream_db"  --stratourl="$stratoRoot" \
->>>>>>> slipstream stratourl fix
             --kafkahost="$kafkaHost" --kafkaport="$kafkaPort" >> logs/slipstream 2>&1 &
->>>>>>> slipstream kafka config
 
 /usr/bin/blockapps-bloc --pghost="$postgres_host" --pgport="$postgres_port" --pguser="$postgres_user" --password="$postgres_password" \
             --stratourl="$stratoRoot" --loglevel="${loglevel:-4}" --cirrusurl="$cirrusRoot" +RTS -N1 2>&1
