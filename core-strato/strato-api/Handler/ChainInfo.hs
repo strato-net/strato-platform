19417d0a0bcaca807acb9c2651640eb9c3eda98e
Shelley Zhong <shelley.zhong@columbia.edu>
2018-06-19 14:31:50 +0000

    modify postChainR to return chainID

--- core-strato/strato-api/Handler/ChainInfo.hs
+++ core-strato/strato-api/Handler/ChainInfo.hs
@@ -7,7 +7,6 @@ module Handler.ChainInfo where
 import           Data.Aeson
 import qualified Data.Text                      as T
 
---import           Blockchain.Data.GenesisInfo
 import           Blockchain.Data.TXOrigin
 import           Blockchain.EthConf             (runKafkaConfigured)
 import           Blockchain.Sequencer.Event     (IngestEvent (IEGenesis), IngestGenesis (..))
@@ -15,7 +14,10 @@ import           Blockchain.Sequencer.Kafka     (writeUnseqEvents)
 import           Import
 
 import           Blockchain.Data.ChainInfo
-
+import qualified Data.ByteString.Lazy             as BL
+import           System.Entropy
+import           Data.Binary.Get                  (runGet, getWord64be)
+import           Data.LargeWord
 
 emitKafkaTransactions :: (MonadIO m, MonadLogger m) => [ChainInfo] -> m ()
 emitKafkaTransactions gs = do
@@ -27,6 +29,17 @@ emitKafkaTransactions gs = do
         Right resps -> $logDebug $ "writeUnseqEventsEnd Kafka commit: " Import.++ (T.pack $ show resps)
     return ()
 
+byteStringToWord256 :: ByteString -> Word256
+byteStringToWord256 bs =
+  let
+    [w4,w3,w2,w1] = flip runGet (BL.fromStrict bs) $ do
+      w_4 <- getWord64be
+      w_3 <- getWord64be
+      w_2 <- getWord64be
+      w_1 <- getWord64be
+      return [w_4,w_3,w_2,w_1]
+  in LargeKey w1 (LargeKey w2 (LargeKey w3 w4))
+
 postChainR :: Handler Text
 postChainR = do
   addHeader "Access-Control-Allow-Origin" "*"
@@ -36,7 +49,7 @@ postChainR = do
     Success gen -> do
       liftIO $ putStrLn $ T.pack $ show gen
       emitKafkaTransactions [gen]
-      return . T.pack . show $ gen
+      bytes <- liftIO $ getEntropy 32
+      return . T.pack . show $ byteStringToWord256 bytes
     _ -> invalidArgs ["could not parse the args"]
-
 
