cb56c81628fad8e4c9a4a9c20962f63d80e8ce58
Shelley Zhong <shelley.zhong@columbia.edu>
2018-06-20 14:18:49 +0000

    Use fromInteger . byteString2Integer to replace byteStringToWord256 in converting ByteString to Word256

--- core-strato/strato-api/Handler/ChainInfo.hs
+++ core-strato/strato-api/Handler/ChainInfo.hs
@@ -14,10 +14,8 @@ import           Blockchain.Sequencer.Kafka     (writeUnseqEvents)
 import           Import
 
 import           Blockchain.Data.ChainInfo
-import qualified Data.ByteString.Lazy             as BL
 import           System.Entropy
-import           Data.Binary.Get                  (runGet, getWord64be)
-import           Data.LargeWord
+import           Blockchain.Util
 
 emitKafkaTransactions :: (MonadIO m, MonadLogger m) => [ChainInfo] -> m ()
 emitKafkaTransactions gs = do
@@ -29,16 +27,6 @@ emitKafkaTransactions gs = do
         Right resps -> $logDebug $ "writeUnseqEventsEnd Kafka commit: " Import.++ (T.pack $ show resps)
     return ()
 
-byteStringToWord256 :: ByteString -> Word256
-byteStringToWord256 bs =
-  let
-    [w4,w3,w2,w1] = flip runGet (BL.fromStrict bs) $ do
-      w_4 <- getWord64be
-      w_3 <- getWord64be
-      w_2 <- getWord64be
-      w_1 <- getWord64be
-      return [w_4,w_3,w_2,w_1]
-  in LargeKey w1 (LargeKey w2 (LargeKey w3 w4))
 
 postChainR :: Handler Text
 postChainR = do
@@ -50,8 +38,9 @@ postChainR = do
       liftIO $ putStrLn $ T.pack $ show gen
       emitKafkaTransactions [gen]
       bytes <- liftIO $ getEntropy 32
-      return . T.pack . show $ byteStringToWord256 bytes
+      return . T.pack . show $ fromInteger $ byteString2Integer bytes
     _ -> invalidArgs ["could not parse the args"]
 
 -- todo: implement getChainR
   
+
