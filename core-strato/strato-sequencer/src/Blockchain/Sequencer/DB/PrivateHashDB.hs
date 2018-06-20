6f75a66ea83cb7cd4cbd08884d15e6ddf5ff9452
dustinnorwood <dnorwood2010@aol.com>
2018-06-20 12:03:46 -0400

    Fix PrivateHashDB

--- core-strato/strato-sequencer/src/Blockchain/Sequencer/DB/PrivateHashDB.hs
+++ core-strato/strato-sequencer/src/Blockchain/Sequencer/DB/PrivateHashDB.hs
@@ -2,7 +2,10 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 module Blockchain.Sequencer.DB.PrivateHashDB where
 
-import           Blockchain.Data.ExtendedWord  (Word256)
+import           Blockchain.ExtWord           (Word256)
+import           Blockchain.Data.RLP
+import           Blockchain.Data.Transaction
+import           Blockchain.Data.TransactionDef
 import           Blockchain.SHA
 import           Control.Monad.Trans.Resource
 
@@ -13,49 +16,49 @@ import qualified Data.Set                     as S
 
 import           Blockchain.Strato.Model.Class
 
-data (TransactionLike t) => PrivateHashDB t =
-                            PrivateHashDB { txHashMap    :: Map SHA t
-                                          , chainHashMap :: Map SHA Word256
-                                          , chainBuffers :: Map Word256 (CircularBuffer SHA)
-                                          }
+data PrivateHashDB =
+     PrivateHashDB { txHashMap    :: Map SHA Transaction -- TODO: Make these LDB entries
+                   , chainHashMap :: Map SHA Word256
+                   -- , chainBuffers :: Map Word256 (CircularBuffer SHA) -- TODO: Use buffers to remove old entries
+                   }
 
-data CircularBuffer a =
-     CircularBuffer { capacity :: Int
-                    , size     :: Int
-                    , queue    :: Q.Seq a
-                    }
-
-maxBufferCapacity :: Int
-maxBufferCapacity = 4096
-
-emptyCircularBuffer :: CircularBuffer a
-emptyCircularBuffer = CircularBuffer maxBufferCapacity 0 Q.empty
+-- data CircularBuffer a =
+--      CircularBuffer { capacity :: Int
+--                     , size     :: Int
+--                     , queue    :: Q.Seq a
+--                     }
+--
+-- maxBufferCapacity :: Int
+-- maxBufferCapacity = 4096
+--
+-- emptyCircularBuffer :: CircularBuffer a
+-- emptyCircularBuffer = CircularBuffer maxBufferCapacity 0 Q.empty
 
 emptyPrivateHashDB :: PrivateHashDB
-emptyPrivateHashDB  = PrivateHashDB Map.empty Map.empty Map.empty
+emptyPrivateHashDB  = PrivateHashDB Map.empty Map.empty -- Map.empty
 
 class (MonadResource m) => HasPrivateHashDB m where
     getPrivateHashDB :: m PrivateHashDB
     putPrivateHashDB :: PrivateHashDB -> m ()
     {-# MINIMAL getPrivateHashDB, putPrivateHashDB #-}
 
-    getTxHashMap :: TransactionLike t => m (Map SHA t)
+    getTxHashMap :: m (Map SHA Transaction)
     getTxHashMap = txHashMap <$> getPrivateHashDB
 
-    putTxHashMap :: TransactionLike t => Map SHA t -> m ()
+    putTxHashMap :: Map SHA Transaction -> m ()
     putTxHashMap m = getPrivateHashDB >>= \db -> putPrivateHashDB db{ txHashMap = m }
 
-    lookupTransaction :: TransactionLike t => SHA -> m (Maybe t)
+    lookupTransaction :: SHA -> m (Maybe Transaction)
     lookupTransaction h = Map.lookup h <$> getTxHashMap
 
-    insertTransaction :: TransactionLike t => t -> m ()
+    insertTransaction :: Transaction -> m ()
     insertTransaction tx = getTxHashMap >>= putTxHashMap . Map.insert (txHash tx) tx
 
     getChainHashMap :: m (Map SHA Word256)
     getChainHashMap = chainHashMap <$> getPrivateHashDB
 
     putChainHashMap :: Map SHA Word256 -> m ()
-    putChainHashMap m = getPrivateHashDB >>= \db -> putPrivateHashDB db{ txHashMap = m }
+    putChainHashMap m = getPrivateHashDB >>= \db -> putPrivateHashDB db{ chainHashMap = m }
 
     lookupChainHash :: SHA -> m (Maybe Word256)
     lookupChainHash h = Map.lookup h <$> getChainHashMap
@@ -63,35 +66,34 @@ class (MonadResource m) => HasPrivateHashDB m where
     insertChainHash :: SHA -> Word256 -> m ()
     insertChainHash h cid = getChainHashMap >>= putChainHashMap . Map.insert h cid
 
-    getChainBuffers :: m (Map Word256 (CircularBuffer SHA))
-    getChainBuffers = chainBuffers <$> getPrivateHashDB
+--    getChainBuffers :: m (Map Word256 (CircularBuffer SHA))
+--    getChainBuffers = chainBuffers <$> getPrivateHashDB
+--
+--    putChainBuffers :: Map SHA Word256 -> m ()
+--    putChainBuffers m = getPrivateHashDB >>= \db -> putPrivateHashDB db{ chainBuffers = m }
+--
+--    lookupChainBuffer :: Word256 -> m (CircularBuffer SHA)
+--    lookupChainBuffer cid = maybe emptyCircularBuffer id . Map.lookup cid <$> getChainBuffers
+--
+--    insertChainBuffer :: Word256 -> CircularBuffer SHA -> m ()
+--    insertChainBuffer cid buf = getChainBuffers >>= putChainBuffesr . Map.insert cid buf
+--
+--    createChainBuffer :: Word256 -> m ()
+--    createChainBuffer = flip insertChainBuffer emptyCircularBuffer
+--
+--    insertChainBufferEntry :: Word256 -> SHA -> m ()
+--    insertChainBufferEntry cid h = do
+--      CircularBuffer cap sz q <- lookupChainBuffer cid
 
-    putChainBuffers :: Map SHA Word256 -> m ()
-    putChainBuffers m = getPrivateHashDB >>= \db -> putPrivateHashDB db{ chainBuffers = m }
 
-    lookupChainBuffer :: Word256 -> m (CircularBuffer SHA)
-    lookupChainBuffer cid = maybe emptyCircularBuffer id . Map.lookup cid <$> getChainBuffers
-
-    insertChainBuffer :: Word256 -> CircularBuffer SHA -> m ()
-    insertChainBuffer cid buf = getChainBuffers >>= putChainBuffesr . Map.insert cid buf
-
-    createChainBuffer :: Word256 -> m ()
-    createChainBuffer = flip insertChainBuffer emptyCircularBuffer
-
-    insertChainBufferEntry :: Word256 -> SHA -> m ()
-    insertChainBufferEntry cid h = do
-      CircularBuffer cap sz q <- lookupChainBuffer cid
-
-
-    insertPrivateHash :: TransactionLike t => t -> m ()
-    insertPrivateHash tx = do
-      db <- getTxHashMap
-      let r = txSigR tx
-          s = txSigS tx
-          h = txHash tx
-          rs = hash (r,s)
-          sr = hash (s,r)
-      insertTransaction tx
-      insertChainHash rs (txChainId tx)
-      insertChainHash sr (txChainId tx)
-      putTxHashMap Map.insert h tx db
+    insertPrivateHash :: Transaction -> m ()
+    insertPrivateHash tx = case txChainId tx of
+      Nothing -> return ()
+      Just chainId -> do
+        let r = txSigR tx
+            s = txSigS tx
+            rs = hash . rlpSerialize $ RLPArray [rlpEncode r, rlpEncode s]
+            sr = hash . rlpSerialize $ RLPArray [rlpEncode s, rlpEncode r]
+        insertTransaction tx
+        insertChainHash rs chainId
+        insertChainHash sr chainId
