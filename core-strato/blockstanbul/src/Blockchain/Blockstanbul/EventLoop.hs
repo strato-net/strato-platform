5783290196f5b95a34d35a3bc5ea0ea8f59c915b
Tim Holland <tim@blockapps.net>
2018-06-26 16:23:45 -0400

    Write a simple draft of preprepare and lensify the state

--- core-strato/blockstanbul/src/Blockchain/Blockstanbul/EventLoop.hs
+++ core-strato/blockstanbul/src/Blockchain/Blockstanbul/EventLoop.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TemplateHaskell #-}
 module Blockchain.Blockstanbul.EventLoop where
 
 import Conduit
+import Control.Lens
 import Control.Monad
 import Control.Monad.State.Class
 
+import Blockchain.Data.Address
 import Blockchain.Data.BlockDB
 import Blockchain.Blockstanbul.Messages
 
@@ -13,33 +16,43 @@ type StateMachineM m = (MonadIO m, MonadState BlockstanbulContext m)
 
 data BlockstanbulContext = BlockstanbulContext {
   -- roundId describes which consensus round is under consideration.
-  roundId :: RoundId
+    _roundId :: RoundId
+  -- authenticator authenticates wire messages are coming from the right sender
+  , _authenticator :: BlockstanbulEvent -> Bool
+  -- The block proposed for this round
+  , _proposal :: Maybe Block
+  -- The designated participant to suggest a block for this round
+  , _proposer :: Maybe Address
+  -- The total group of participants
+  , _validators :: [Address]
 
-  -- validator authenticates wire messages are coming from the right sender
-  , validator :: BlockstanbulEvent -> Bool
 }
+makeLenses ''BlockstanbulContext
 
 roundChange :: (StateMachineM m) => Conduit BlockstanbulEvent m BlockstanbulEvent
 roundChange = do
-  ri <- gets (roundidRound . roundId)
-  yield . RoundChange (error "TODO(tim): supply a private key to StateMachineM") . (+1) $ ri
+  r <- uses roundId roundidRound
+  yield . RoundChange (error "TODO(tim): supply a private key to StateMachineM") . (+1) $ r
 
 eventLoop :: (StateMachineM m) => Conduit BlockstanbulEvent m BlockstanbulEvent
 eventLoop = do
   wm' <- await
-  valid <- gets validator
+  isAuthentic <- use authenticator
   case wm' of
     Nothing -> return ()
-    Just msg@(Preprepare auth ri pp) -> when (valid msg) $ do
-      yield (Prepare auth ri (blockHash pp))
+    Just msg@(Preprepare auth ri pp) -> when (isAuthentic msg) $ do
+      pr <- use proposer
+      when (Just (sender (auth)) == pr) $ do
+        proposal .= Just pp
+        yield (Prepare auth ri (blockHash pp))
       eventLoop
-    Just msg@(Prepare auth ri di) -> when (valid msg) $ do
+    Just msg@(Prepare auth ri di) -> when (isAuthentic msg) $ do
       yield (Commit auth ri di)
       eventLoop
-    Just msg@(Commit auth ri _) -> when (valid msg) $ do
+    Just msg@(Commit auth ri _) -> when (isAuthentic msg) $ do
       yield (RoundChange auth (roundidRound ri + 1))
       eventLoop
-    Just msg@(RoundChange _ _) -> when (valid msg) $ do
+    Just msg@(RoundChange _ _) -> when (isAuthentic msg) $ do
       return ()
     Just Timeout -> roundChange >> eventLoop
     Just (CommitFailure _) -> roundChange >> eventLoop
--- core-strato/blockstanbul/test/Main.hs
+++ core-strato/blockstanbul/test/Main.hs
@@ -6,6 +6,7 @@ import Test.Hspec
 import Test.QuickCheck
 
 import Conduit
+import Control.Lens
 import Control.Monad.Trans.State
 
 import Blockchain.Data.ArbitraryInstances()
@@ -23,22 +24,21 @@ testContext :: BlockstanbulContext
 testContext = BlockstanbulContext
   (RoundId 20 18)
   (const True)
+  Nothing
+  Nothing
+  []
 
 runTest :: StateT BlockstanbulContext IO () -> IO ()
 runTest = flip evalStateT testContext
 
 spec :: Spec
 spec = do
-  describe "A harness? what do you need that for?" $ do
-    it "it secures a seat on the horse" $ do
-      let v1 = RoundId 200 30
-      roundidSequence v1 `shouldBe` 30
-
-  describe "The event loop" $ do
+  describe "The blockstanbul event loop" $ do
     let sendMessages :: (StateMachineM m) => [BlockstanbulEvent] -> m [BlockstanbulEvent]
         sendMessages wms = runConduit (yieldMany wms .| eventLoop .| sinkList)
     it "does nothing to the messages" $ property $ \auth blk ->
       runTest $ do
+        proposer .= Just (sender auth)
         let m1 = Preprepare auth (RoundId 0 0) blk
         let hash = blockHash blk
         m2 <- sendMessages [m1]
@@ -50,8 +50,44 @@ spec = do
         m5 <- sendMessages m4
         m5 `expectAs` [] :: StateT BlockstanbulContext IO ()
 
-    it "Changes rounds in response to a timeout or insertion failure" $ do
+    it "changes rounds in response to a timeout or insertion failure" $ do
       runTest $ do
         let input = [Timeout, CommitFailure "invalid hash"]
         got <- sendMessages input
         map roundchangeRound got `expectAs` [21, 21]
+
+    it "sets the current proposal in response a preprepare message" $ property $ \auth blk ->
+      runTest $ do
+        proposer .= Just (sender auth)
+        validators .= [sender auth]
+        curRound <- use roundId
+        let input = [Preprepare auth curRound blk]
+        let hash = blockHash blk
+        got <- sendMessages input
+        -- TODO(tim): change the auth to be that of the eventloop
+        got `expectAs` [Prepare auth curRound hash]
+        gotProp <- use proposal
+        gotProp `expectAs` Just blk
+
+    it "rejects an unauthenticated preprepare" $ property $ \auth blk ->
+      runTest $ do
+        proposer .= Just (sender auth)
+        validators .= [sender auth]
+        authenticator .= const False
+        curRound <- use roundId
+        let input = [Preprepare auth curRound blk]
+        got <- sendMessages input
+        got `expectAs` []
+        gotProp <- use proposal
+        gotProp `expectAs` Nothing
+
+    it "rejects a preprepare from a non-proposer" $ property $ \auth blk addr ->
+      runTest $ do
+        proposer .= Just addr
+        validators .= [sender auth, addr]
+        curRound <- use roundId
+        let input = [Preprepare auth curRound blk]
+        got <- sendMessages input
+        got `expectAs` []
+        gotProp <- use proposal
+        gotProp `expectAs` Nothing
