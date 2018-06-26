91c04c5e8c9b53ea3c92863b39682f6a8486fdd7
Daniel Mesko <dpm2153@columbia.edu>
2018-06-26 23:27:06 +0000

    added Arbitrary tests

--- core-strato/test-suite/test/Blockchain/Sequencer/ArbitraryInstances.hs
+++ core-strato/test-suite/test/Blockchain/Sequencer/ArbitraryInstances.hs
@@ -7,6 +7,7 @@ import           Test.QuickCheck
 
 import           Blockchain.Data.ArbitraryInstances ()
 import           Blockchain.Data.ChainInfo
+import           Blockchain.Data.Enode
 import           Blockchain.Sequencer.Event
 
 derive makeArbitrary ''IngestEvent
@@ -19,9 +20,9 @@ derive makeArbitrary ''OutputTx
 derive makeArbitrary ''OutputBlock
 derive makeArbitrary ''OutputGenesis
 derive makeArbitrary ''ChainInfo
+derive makeArbitrary ''Enode
+derive makeArbitrary ''IPAddress
 
 -- just end me fam
 instance Arbitrary JsonRpcCommand where
    arbitrary = JRCGetBalance <$> arbitrary <*> arbitrary <*> arbitrary
-
-
