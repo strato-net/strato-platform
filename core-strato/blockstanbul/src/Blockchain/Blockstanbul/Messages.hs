d05923c7ea7a656c23ce7a7b0ff672f4b91007b2
Tim Holland <tim@blockapps.net>
2018-06-27 18:04:18 -0400

    Write a nonfunctional version of authentication

--- core-strato/blockstanbul/src/Blockchain/Blockstanbul/Messages.hs
+++ core-strato/blockstanbul/src/Blockchain/Blockstanbul/Messages.hs
@@ -9,7 +9,7 @@ import Test.QuickCheck
 import Blockchain.ExtWord
 import Blockchain.Data.Address
 import Blockchain.Data.ArbitraryInstances ()
-import Blockchain.Data.DataDefs
+import Blockchain.Data.BlockDB
 import Blockchain.SHA
 import Blockchain.ExtendedECDSA
 
@@ -47,6 +47,26 @@ getAuth (Prepare a _ _) = Just a
 getAuth (Commit a _ _ _) = Just a
 getAuth (RoundChange a _) = Just a
 getAuth _ = Nothing
+
+getHash :: BlockstanbulEvent -> Maybe SHA
+-- This is wrong, because this means that the prepare and commits
+-- will have the same signature despite being different messages.
+-- It also needs a code for the message type.
+getHash (Preprepare _ _ blk) = Just . blockHash $ blk
+getHash (Prepare _ _ di) = Just di
+getHash (Commit _ _ di _) = Just di
+getHash (RoundChange _ _) = error "not yet defined for roundchange"
+getHash _ = Nothing
+
+authenticate :: BlockstanbulEvent -> Bool
+authenticate msg = case getAuth msg of
+  Nothing -> True
+  Just a ->
+    let MsgAuth addr sig = a
+        mMsgHash = getHash msg
+        mKey = getPubKeyFromSignature sig . unSHA =<< mMsgHash
+        mAddress = pubKey2Address <$> mKey
+    in mAddress == Just addr
 
 -- TODO(tim): JSON instances
 -- TODO(tim): RLP instances
