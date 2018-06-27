f949e6d5788421fac0a25a3bd4ba3c167ffd9aa2
Tanuj Soni <tanuj@blockapps.net>
2018-06-27 16:35:23 +0530

    [external-storage] Fix: Upload component

--- smd-ui/src/components/ExternalStorage/UploadFile/index.js
+++ smd-ui/src/components/ExternalStorage/UploadFile/index.js
@@ -1,24 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import { Dialog, Button, Intent } from '@blueprintjs/core';
+import { Dialog } from '@blueprintjs/core';
 import mixpanelWrapper from '../../../lib/mixpanelWrapper';
-import { closeUploadModal, uploadFileRequest, resetError, changeUsername } from './uploadFile.actions';
-import { Field, reduxForm } from 'redux-form';
-import Dropzone from 'react-dropzone';
+import { closeUploadModal, resetError } from './uploadFile.actions';
 import { toasts } from '../../Toasts';
-import { validate } from './validate';
 import { isModePublic } from '../../../lib/checkMode';
-import { fetchAccounts, fetchUserAddresses } from '../../Accounts/accounts.actions';
+import { fetchAccounts } from '../../Accounts/accounts.actions';
+import UploadForm from './UploadForm';
+import UploadData from './UploadData';
 
 import './uploadFile.css';
 
 class UplaodFile extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = { errors: null }
-  }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.uploadError) {
@@ -29,286 +23,7 @@ class UplaodFile extends Component {
 
   componentDidMount() {
     mixpanelWrapper.track("external_storage_loaded");
-    this.props.reset();
     !isModePublic() && this.props.fetchAccounts(true, false);
-  }
-
-  renderDropzoneInput = (field) => {
-    const touchedAndHasErrors = field.meta.touched && field.meta.error
-    return (
-      <div className="dropzoneContainer text-center upload-file">
-        <Dropzone
-          className="dropzone"
-          name={'file'}
-          onDrop={(filesToUpload, e) => this.handleFileDrop(filesToUpload, field)}
-        >
-          {({ isDragActive, isDragReject, acceptedFiles }) => {
-            if (isDragActive) {
-              return (<p className="pt-intent-success">Drop to Upload!</p>);
-            }
-            return (<p className="pt-intent-success">{acceptedFiles.length > 0 ? acceptedFiles[0].name : 'Drop a file here, or click to select files to upload.'}</p>)
-          }}
-        </Dropzone>
-        {touchedAndHasErrors && <span className="error">{field.meta.error}</span>}
-      </div>
-    );
-  };
-
-  handleFileDrop = (files, dropZoneField) => {
-    dropZoneField.input.onChange(files);
-  };
-
-  submit = (values) => {
-    let errors = validate(values);
-    this.setState({ errors });
-
-    if (!Object.values(errors).length) {
-      values.file = values.content[0];
-      this.props.uploadFileRequest(values);
-      this.props.reset();
-    }
-  }
-
-  errorMessageFor(fieldName) {
-    if (this.state.errors && this.state.errors[fieldName]) {
-      return this.state.errors[fieldName];
-    }
-    return null;
-  }
-
-  handleUsernameChange = (e) => {
-    this.props.changeUsername(e.target.value);
-    this.props.fetchUserAddresses(e.target.value, true)
-  };
-
-  renderUsername = (isPublicMode) => {
-    const users = Object.getOwnPropertyNames(this.props.accounts);
-    return (<div className={isPublicMode ? "" : "pt-select"}>
-      <Field
-        className="pt-input"
-        component="select"
-        name="username"
-        onChange={this.handleUsernameChange}
-        required
-        disabled={isPublicMode}
-      >
-        <option value={isPublicMode ? this.props.initialValues.username : null}>
-          {isPublicMode && this.props.initialValues.username}
-        </option>
-        {
-          users.map((user, i) => {
-            return (
-              <option key={'user' + i} value={user}>{user}</option>
-            )
-          })
-        }
-      </Field>
-    </div>)
-  };
-
-  renderAddress = (isPublicMode) => {
-    const userAddresses = this.props.accounts && this.props.username ?
-      Object.getOwnPropertyNames(this.props.accounts[this.props.username])
-      : [];
-    return (<div className={isPublicMode ? "" : "pt-select"}>
-      <Field
-        className="pt-input"
-        component="select"
-        name="address"
-        required
-        disabled={isPublicMode}
-      >
-        <option value={isPublicMode ? this.props.initialValues.address : null}>
-          {isPublicMode && this.props.initialValues.address}
-        </option>
-        {
-          userAddresses.map((address, i) => {
-            return (
-              <option key={address} value={address}>{address}</option>
-            )
-          })
-        }
-      </Field>
-    </div>);
-  };
-
-  uploadForm() {
-    let isPublicMode = isModePublic();
-
-    return (
-      <div className="pt-dialog-body upload-form">
-
-        <div className="row">
-          <div className="col-sm-3 text-right">
-            <label className="pt-label smd-pad-4">
-              Username
-            </label>
-          </div>
-          <div className="col-sm-9 smd-pad-4">
-            {this.renderUsername(isPublicMode)}
-            <br /><span className="error-text">{this.errorMessageFor('username')}</span>
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col-sm-3 text-right">
-            <label className="pt-label smd-pad-4">
-              Address
-            </label>
-          </div>
-          <div className="col-sm-9 smd-pad-4">
-            {this.renderAddress(isPublicMode)}
-            <br /> <span className="error-text">{this.errorMessageFor('address')}</span>
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col-sm-3 text-right">
-            <label className="pt-label smd-pad-4">
-              Password
-            </label>
-          </div>
-          <div className="col-sm-9 smd-pad-4">
-            <Field
-              name="password"
-              component="input"
-              type="password"
-              placeholder="Password"
-              className="pt-input form-width"
-              tabIndex="3"
-              required
-            /> <br />
-            <span className="error-text">{this.errorMessageFor('password')}</span>
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col-sm-3 text-right">
-            <label className="pt-label smd-pad-4">
-              Drop Video
-            </label>
-          </div>
-          <div className="col-sm-9 smd-pad-4">
-            <Field
-              id="input-b"
-              component={this.renderDropzoneInput}
-              className="form-width pt-input"
-              name="content"
-              dir="auto"
-              title="Content"
-              tabIndex="4"
-              required
-            /> <br />
-            <span className="error-text">{this.errorMessageFor('content')}</span>
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col-sm-3 text-right">
-            <label className="pt-label smd-pad-4">
-              Provider
-            </label>
-          </div>
-          <div className="col-sm-9 smd-pad-4">
-            <div className="pt-select">
-              <Field
-                className="pt-input"
-                component="select"
-                name="provider"
-                tabIndex="5"
-                required
-              >
-                <option value={'s3'}> s3 </option>
-              </Field> <br />
-              <span className="error-text">{this.errorMessageFor('provider')}</span>
-            </div>
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col-sm-3 text-right">
-            <label className="pt-label smd-pad-4">
-              Description
-            </label>
-          </div>
-          <div className="col-sm-9 smd-pad-4">
-            <Field
-              name="description"
-              component="input"
-              type="text"
-              placeholder="Description"
-              className="pt-input form-width"
-              tabIndex="6"
-              required
-            /> <br />
-            <span className="error-text">{this.errorMessageFor('description')}</span>
-          </div>
-        </div>
-
-        <div className="pt-dialog-footer upload-button">
-          <div className="pt-dialog-footer-actions button-center upload">
-            <Button
-              intent={Intent.PRIMARY}
-              onClick={this.props.handleSubmit(this.submit)}
-              disabled={this.props.isLoading}
-              text="Upload"
-            />
-          </div>
-        </div>
-      </div>
-    );
-  }
-
-  renderSuccess(data) {
-    return (
-      <div>
-        <div className="pt-dialog-body">
-
-          <div className="row content-margin">
-            <div className="col-sm-4">
-              <label> Contract Address </label>
-            </div>
-            <div className="col-sm-8">
-              <label> {data.contractAddress} </label>
-            </div>
-          </div>
-
-          <div className="row content-margin">
-            <div className="col-sm-4">
-              <label> URI </label>
-            </div>
-            <div className="col-sm-8">
-              <label> {data.uri} </label>
-            </div>
-          </div>
-
-          <div className="row content-margin">
-            <div className="col-sm-4">
-              <label> Description </label>
-            </div>
-            <div className="col-sm-8">
-              <label> {data.metadata} </label>
-            </div>
-          </div>
-
-        </div>
-
-        <div className="pt-dialog-footer">
-          <div className="pt-dialog-footer-actions button-center">
-            <Button
-              intent={Intent.PRIMARY}
-              onClick={() => this.closeModal()}
-              text="Close"
-            />
-          </div>
-        </div>
-      </div>
-    )
-  }
-
-  closeModal() {
-    this.props.closeUploadModal();
-    this.props.reset();
   }
 
   render() {
@@ -316,20 +31,18 @@ class UplaodFile extends Component {
 
     return (
       <div>
-        <form>
-          <Dialog
-            isOpen={this.props.isOpen}
-            onClose={() => {
-              mixpanelWrapper.track('close_upload_modal');
-              this.closeModal();
-            }}
-            iconName={result ? 'saved' : 'pt-icon-upload'}
-            title={result ? 'URI Upload Success' : 'Upload'}
-            className="pt-dark upload-dialog"
-          >
-            {result ? this.renderSuccess(result) : this.uploadForm()}
-          </Dialog>
-        </form>
+        <Dialog
+          isOpen={this.props.isOpen}
+          onClose={() => {
+            mixpanelWrapper.track('close_upload_modal');
+            this.props.closeUploadModal();
+          }}
+          iconName={result ? 'saved' : 'pt-icon-upload'}
+          title={result ? 'URI Upload Success' : 'Upload'}
+          className="pt-dark upload-dialog"
+        >
+          {result ? <UploadData result={this.props.result} closeModal={() => this.props.closeUploadModal()} /> : <UploadForm />}
+        </Dialog>
       </div>
     )
   }
@@ -339,29 +52,17 @@ export function mapStateToProps(state) {
   return {
     isOpen: state.uploadFile.isOpen,
     uploadError: state.uploadFile.error,
-    result: state.uploadFile.result,
-    accounts: state.accounts.accounts,
-    username: state.uploadFile.username,
-    isLoading: state.uploadFile.isLoading,
-    initialValues: {
-      username: state.user.currentUser.username,
-      address: state.user.currentUser.accountAddress,
-      provider: 's3'
-    }
+    result: state.uploadFile.result
   };
 }
 
-const formed = reduxForm({ form: 'upload-file' })(UplaodFile);
 const connected = connect(
   mapStateToProps,
   {
     closeUploadModal,
-    uploadFileRequest,
     resetError,
-    fetchAccounts,
-    fetchUserAddresses,
-    changeUsername
+    fetchAccounts
   }
-)(formed);
+)(UplaodFile);
 
 export default withRouter(connected);
