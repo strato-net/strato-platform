2648a0097ce280dc8b01a96993d6e42c1e94d112
Tim Holland <tim@blockapps.net>
2018-06-28 17:31:41 -0400

    Break out messages from the other events It doesn't make sense to RLP serialize a timeout

--- core-strato/blockstanbul/src/Blockchain/Blockstanbul/EventLoop.hs
+++ core-strato/blockstanbul/src/Blockchain/Blockstanbul/EventLoop.hs
@@ -24,7 +24,7 @@ data BlockstanbulContext = BlockstanbulContext {
   -- view describes which consensus round is under consideration.
     _view :: View
   -- authenticator authenticates wire messages are coming from the right sender
-  , _authenticator :: BlockstanbulEvent -> Bool
+  , _authenticator :: WireMessage -> Bool
   -- The block proposed for this round
   , _proposal :: Maybe Block
   -- The designated participant to suggest a block for this round
@@ -48,34 +48,34 @@ data BlockstanbulContext = BlockstanbulContext {
 
 makeLenses ''BlockstanbulContext
 
-isAuthorized :: (StateMachineM m) => BlockstanbulEvent -> m Bool
-isAuthorized event = case getAuth event of
-    -- Timeouts and failures are trusted as they are from this node.
-    Nothing -> return True
-    Just (MsgAuth addr _) -> do
-      authn <- use authenticator
-      if not (authn event)
-        then return False
-        else do
-          authorized <- use validators
-          return $ addr `elem` authorized
+isAuthorized :: (StateMachineM m) => InEvent -> m Bool
+isAuthorized (IMsg wm) = do
+  let MsgAuth addr _ = getAuth wm
+  authn <- use authenticator
+  if not (authn wm)
+    then return False
+    else do
+      authorized <- use validators
+      return $ addr `elem` authorized
+-- Internally generated events are trusted implicitly
+isAuthorized _ = return True
 
 hasSameHash :: (StateMachineM m) => SHA -> m Bool
 hasSameHash di = uses proposal $ maybe False ((==di) . blockHash)
 
-roundChange :: (StateMachineM m) => Conduit BlockstanbulEvent m BlockstanbulEvent
+roundChange :: (StateMachineM m) => Conduit InEvent m OutEvent
 roundChange = do
   r <- use (view . round)
-  yield . RoundChange (error "TODO(tim): supply a private key to StateMachineM") $ r+1
+  yield . OMsg . RoundChange (error "TODO(tim): supply a private key to StateMachineM") $ r+1
 
 -- TODO(tim): Define an exit type from the conduit for sending blocks to the EVM
 -- TODO(tim): what to do if the block hasn't arrived yet?
-commit :: (StateMachineM m) => Maybe Block -> Conduit BlockstanbulEvent m BlockstanbulEvent
+commit :: (StateMachineM m) => Maybe Block -> Conduit InEvent m OutEvent
 commit _ = do
   s <- use $ view . sequence
   nextRound . Sequence $ s+1
 
-nextRound :: (StateMachineM m) => NextType -> Conduit BlockstanbulEvent m BlockstanbulEvent
+nextRound :: (StateMachineM m) => NextType -> Conduit InEvent m OutEvent
 nextRound nt = do
   case nt of
     Sequence s -> view . sequence .= s
@@ -91,20 +91,20 @@ nextRound nt = do
   hasPrepared .= False
   pendingRound .= Nothing
 
-eventLoop :: (StateMachineM m) => Conduit BlockstanbulEvent m BlockstanbulEvent
+eventLoop :: (StateMachineM m) => Conduit InEvent m OutEvent
 eventLoop = awaitForever $ \ev -> do
   authz <- lift $ isAuthorized ev
   v <- use view
   when authz $ case ev of
-    Preprepare auth v' pp -> do
+    IMsg (Preprepare auth v' pp) -> do
       pr <- use proposer
       when (sender auth == pr) $ do
         proposal .= Just pp
         if v == v'
           -- TODO(tim): use own auth
-          then yield (Prepare auth v (blockHash pp))
+          then yield . OMsg $ Prepare auth v (blockHash pp)
           else roundChange
-    Prepare auth v' di -> when (v <= v') $ do
+    IMsg (Prepare auth v' di) -> when (v <= v') $ do
       ps <- prepared <%= M.insert (sender auth) di
       total <- uses validators length
       let sameVoteCount = M.size . M.filter (==di) $ ps
@@ -113,8 +113,8 @@ eventLoop = awaitForever $ \ev -> do
       when (3 * sameVoteCount > 2 * total && sameHash && not hasSent) $ do
         hasPrepared .= True
         -- TODO(tim): use own auth
-        yield (Commit auth v di ())
-    Commit auth v' di seal -> when (v <= v') $ do
+        yield . OMsg $ Commit auth v di ()
+    IMsg (Commit auth v' di seal) -> when (v <= v') $ do
       cs <- committed <%= M.insert (sender auth) (di, seal)
       total <- uses validators length
       let sameVoteCount = M.size . M.filter ((==di) . fst) $ cs
@@ -122,7 +122,7 @@ eventLoop = awaitForever $ \ev -> do
       -- TODO(tim): Is it necessary to check that we have prepared?
       when (3 * sameVoteCount > 2 * total && sameHash) $ do
         join $ uses proposal commit
-    RoundChange auth rn -> when (_round v <= rn) $ do
+    IMsg (RoundChange auth rn) -> when (_round v <= rn) $ do
       rs <- roundChanged <%= M.insert (sender auth) rn
       total <- uses validators length
       sentRN <- use pendingRound
@@ -130,7 +130,7 @@ eventLoop = awaitForever $ \ev -> do
       when (3 * sameRNCount > total && Just rn > sentRN) $ do
         pendingRound .= Just rn
         -- TODO(tim): use own auth
-        yield (RoundChange auth rn)
+        yield . OMsg $ RoundChange auth rn
       when (3 * sameRNCount > 2 * total) $ do
         next <- use pendingRound
         case next of
--- core-strato/blockstanbul/src/Blockchain/Blockstanbul/Messages.hs
+++ core-strato/blockstanbul/src/Blockchain/Blockstanbul/Messages.hs
@@ -31,42 +31,44 @@ data MsgAuth = MsgAuth {
 instance Arbitrary MsgAuth where
   arbitrary = liftM2 MsgAuth arbitrary arbitrary
 
+data WireMessage = Preprepare MsgAuth View Block
+                 | Prepare MsgAuth View SHA
+                 | Commit MsgAuth View SHA Seal
+                 | RoundChange {roundchangeAuth :: MsgAuth,
+                                roundchangeRound :: RoundNumber }
+                 deriving (Eq, Show)
 
-data BlockstanbulEvent = Preprepare MsgAuth View Block
-                       | Prepare MsgAuth View SHA
-                       | Commit MsgAuth View SHA Seal
-                       | RoundChange {roundchangeAuth :: MsgAuth,
-                                      roundchangeRound :: RoundNumber }
-                       | Timeout
-                       | CommitFailure Text
-                       deriving (Eq, Show)
+data InEvent = IMsg {unIMsg :: WireMessage}
+             | Timeout
+             | CommitFailure Text
+             deriving (Eq, Show)
 
-getAuth :: BlockstanbulEvent -> Maybe MsgAuth
-getAuth (Preprepare a _ _) = Just a
-getAuth (Prepare a _ _) = Just a
-getAuth (Commit a _ _ _) = Just a
-getAuth (RoundChange a _) = Just a
-getAuth _ = Nothing
+data OutEvent = OMsg {unOMsg :: WireMessage}
+              | ReadyBlock Block
+              deriving (Eq, Show)
 
-getHash :: BlockstanbulEvent -> Maybe SHA
+getAuth :: WireMessage -> MsgAuth
+getAuth (Preprepare a _ _) = a
+getAuth (Prepare a _ _) = a
+getAuth (Commit a _ _ _) = a
+getAuth (RoundChange a _) = a
+
+getHash :: WireMessage -> SHA
 -- This is wrong, because this means that the prepare and commits
 -- will have the same signature despite being different messages.
 -- It also needs a code for the message type.
-getHash (Preprepare _ _ blk) = Just . blockHash $ blk
-getHash (Prepare _ _ di) = Just di
-getHash (Commit _ _ di _) = Just di
+getHash (Preprepare _ _ blk) = blockHash $ blk
+getHash (Prepare _ _ di) = di
+getHash (Commit _ _ di _) = di
 getHash (RoundChange _ _) = error "not yet defined for roundchange"
-getHash _ = Nothing
 
-authenticate :: BlockstanbulEvent -> Bool
-authenticate msg = case getAuth msg of
-  Nothing -> True
-  Just a ->
-    let MsgAuth addr sig = a
-        mMsgHash = getHash msg
-        mKey = getPubKeyFromSignature sig . unSHA =<< mMsgHash
-        mAddress = pubKey2Address <$> mKey
-    in mAddress == Just addr
+authenticate :: WireMessage -> Bool
+authenticate msg =
+  let MsgAuth addr sig = getAuth msg
+      msgHash = getHash msg
+      mKey = getPubKeyFromSignature sig . unSHA $ msgHash
+      mAddress = pubKey2Address <$> mKey
+  in mAddress == Just addr
 
 -- TODO(tim): JSON instances
 -- TODO(tim): RLP instances
