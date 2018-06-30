0d9d4dfb7de854fa88978eb2e889f83cced3db6d
Daniel Mesko <dpm2153@columbia.edu>
2018-06-30 21:27:34 +0000

    Started extending private chains messages to take lists of chainIds, chainInfos

--- core-strato/strato-p2p/src/Blockchain/Data/Wire.hs
+++ core-strato/strato-p2p/src/Blockchain/Data/Wire.hs
@@ -159,8 +159,10 @@ data Message =
   NewBlock Block Integer |
   
   -- private chains
-  GetChainDetails Word256 |
-  ChainDetails Word256 ChainInfo |
+  GetChainDetails [Word256] |
+  ChainDetails [(Word256, ChainInfo)] |
+  
+  -- should take [(Word256, TransactionRequest)]
   GetTransactions Word256 TransactionRequest deriving (Eq,Show) 
 
 instance Format Message where
@@ -207,11 +209,17 @@ instance Format Message where
   format (NewBlock b d) = CL.blue "NewBlock (" ++ show d ++ "):"  ++ tab("\n" ++ format b)
   
   -- private chains
-  format (GetChainDetails cid) = CL.blue "GetChainDetails\n" ++ "  chainID: " ++ (show cid)
-  format (ChainDetails chid ci) = 
-    CL.blue "Chain Details\n" ++ 
-    "  chainID: " ++ show chid ++ "\n" ++
-    "  chainInfo: " ++ show ci ++ "\n"
+  format (GetChainDetails cids) = CL.blue "GetChainDetails\n" ++ "  for chainIDs: " ++ (intersperse '\n' (show cids))
+
+  format (ChainDetails chPairs) = 
+    CL.blue "Chain Details\n" ++ formatPairs chPairs
+    where 
+      formatPairs :: [(Word256, ChainInfo)] -> String
+      formatPairs [] = ""
+      formatPairs ((chID, chInfo):xs) =
+        "\n  chainID: "  ++ show chID ++
+        "\n  chainInfo: " ++ show chInfo ++ formatPairs xs
+
   format (GetTransactions cid3 tr) = "GetTransactions\n" ++
     "  chainID: " ++ show cid3 ++ "\n" ++
     "  transactionData: " ++ show tr ++ "\n"
@@ -256,10 +264,10 @@ obj2WireMessage 0x17 (RLPArray [b, td]) =
   NewBlock (rlpDecode b) (rlpDecode td)
 
 -- private chains
-obj2WireMessage 0x1c cid = 
-  GetChainDetails (rlpDecode cid)
-obj2WireMessage 0x1d (RLPArray [chid, ci]) =
-  ChainDetails (rlpDecode chid) (rlpDecode ci) 
+obj2WireMessage 0x1c (RLPArray cids) = 
+  GetChainDetails (rlpDecode <$> cids)
+obj2WireMessage 0x1d (RLPArray chDetPairs) =
+  ChainDetails $ rlpDecode <$> chDetPairs
 obj2WireMessage 0x1e (RLPArray [c, tr]) =
   GetTransactions (rlpDecode c) (rlpDecode tr)
 
@@ -304,11 +312,11 @@ wireMessage2Obj (NewBlock b d) =
   (0x17, RLPArray [rlpEncode b, rlpEncode d])
 
 -- private chains
-wireMessage2Obj (GetChainDetails c) = 
-  (0x1c, rlpEncode c)
+wireMessage2Obj (GetChainDetails cIds) = 
+  (0x1c, RLPArray $ rlpEncode <$> cIds)
 
-wireMessage2Obj (ChainDetails chid ci) =  
-  (0x1d, RLPArray [rlpEncode chid, rlpEncode ci])
+wireMessage2Obj (ChainDetails chpairs) =  
+  (0x1d, RLPArray $ rlpEncode <$> chpairs)
 
 wireMessage2Obj (GetTransactions c tr) = 
   (0x1e, RLPArray [rlpEncode c, rlpEncode tr])
