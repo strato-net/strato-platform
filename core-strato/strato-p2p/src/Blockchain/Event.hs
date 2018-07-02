99ed17dd9fc7aa584dc9e21b59f3fe75f3bbaa80
Daniel Mesko <dpm2153@columbia.edu>
2018-07-02 21:21:55 +0000

    Updated event handlers for GetChainDetails and ChainDetails to take/yield lists - GetTransactions remains unchanged for the moment

--- core-strato/strato-p2p/src/Blockchain/Event.hs
+++ core-strato/strato-p2p/src/Blockchain/Event.hs
@@ -19,6 +19,7 @@ import           Control.Monad.Logger
 import           Control.Monad.State
 import           Data.Conduit
 import           Data.List
+import           Data.Maybe
 --import qualified Data.Set as S
 import qualified Data.ByteString                       as BS
 import qualified Data.ByteString.Base16                as BC16
@@ -252,38 +253,38 @@ handleEvents mode peer = awaitForever $ \case
                 stampActionTimestamp
 
     -- private chains
-    MsgEvt (GetChainDetails cid) -> do
+    MsgEvt (GetChainDetails cids) -> do
       stampActionTimestamp
-      $logInfoS "handleEvents/GetChainDetails" $ T.pack $ "details requested for chainID " ++ (show cid)
-      chDet <- lift (RBDB.withRedisBlockDB $ RBDB.getChainInfo cid)
-      case chDet of
-        Nothing ->  
-          $logInfoS "handleEvents/GetChainDetails" $ T.pack $ "No information " ++ 
-            "found about the chain with chainID " ++ (show cid)
-        Just (ci) -> do
-          -- check that the peer is authorized to receive these chain details, by verifying that their 
-          -- IPaddress is associated with one of the Enodes in the chain's member list
-          let pIp = readIP $ T.unpack (pPeerIp peer)
-          let enodeList = members ci
-          let ipList = ipAddress <$> enodeList 
-          let match = find (==pIp) ipList
+      $logInfoS "handleEvents/GetChainDetails" $ T.pack $ "details requested for chainIDs " ++ (intercalate "\n" $ show <$> cids)
+      chDets <- lift . RBDB.withRedisBlockDB $ mapM RBDB.getChainInfo cids
+      let unfilteredPairs =  makePairs cids chDets
+      let chPairs = fmap fromJust <$> filter (isJust . snd) unfilteredPairs -- remove pairs where ChainInfo is Nothing
+      let finalPairs = filter ((checkPeerIsMember peer) . snd) chPairs -- remove pairs where peer is not a member
 
-          case match of
-            Nothing -> do 
-              $logInfoS "handleEvents/GetChainDetails" $ T.pack $ "peer requesting chain details is not authorized for chainID " ++ (show cid)
-            Just _ -> do 
-              $logInfoS "handleEvents/GetChainDetails" $ T.pack $ "sending ChainDetails for chainID " ++ (show cid)
-              yield $ ChainDetails cid ci
-              stampActionTimestamp
-    
-    MsgEvt (ChainDetails cId cInfo) -> do
-      stampActionTimestamp
-      SK.emitKafkaChainDetails (Origin.PeerString $ peerString peer) cId cInfo
+      case finalPairs of 
+        [] -> return ()
+        _ -> do
+          yield $ ChainDetails finalPairs
+          stampActionTimestamp
+          $logInfoS "handleEvents/GetChainDetails" $ T.pack $ "the following (ChainId, ChainInfo) pairs were returned " ++ 
+            (intercalate "\n" $ show <$> finalPairs)
+      where 
+        makePairs :: [a] -> [b] -> [(a,b)]
+        makePairs [] _ = []
+        makePairs _ [] = []
+        makePairs (x:xs) (y:ys) = (x,y) : makePairs xs ys
 
-    MsgEvt (GetTransactions chid req) -> do
+    MsgEvt (ChainDetails chpairs) -> do
       stampActionTimestamp
-      $logInfoS "handleEvents/GetTransactions" $ T.pack $ "transaction info for chainID " ++ (show chid)
-        ++ "\n  " ++ (show req)
+      -- throwIO $ (uncurry RBDB.putChainInfo) <$> chpairs
+      RBDB.withRedisBlockDB $ mapM_ (uncurry RBDB.putChainInfo) chpairs
+      mapM_ (uncurry (SK.emitKafkaChainDetails (Origin.PeerString $ peerString peer))) chpairs
+
+    MsgEvt (GetTransactions trReqPairs) -> do
+      stampActionTimestamp
+--      $logInfoS "handleEvents/GetTransactions" $ T.pack $ "transaction info for chainID " ++ (show chid)
+--        ++ "\n  " ++ (show req)
+
       tr <- case req of 
         (Explicit ids) -> lift . RBDB.withRedisBlockDB $ mapM RBDB.getTransactions ids
         (Implicit ha _ _ _) -> lift . RBDB.withRedisBlockDB $ mapM RBDB.getTransactions [ha]
@@ -325,7 +326,7 @@ handleEvents mode peer = awaitForever $ \case
               $logInfoS "handleEvents/OEGenesis" $ T.pack $ "peer requesting chain details is not authorized for chainID " ++ (show cId)
             Just _ -> do 
               $logInfoS "handleEvents/OEGenesis" $ T.pack $ "sending ChainDetails for chainID " ++ (show cId)
-              yield $ ChainDetails cId cInfo
+              yield $ ChainDetails [(cId, cInfo)]
       _ -> return () -- shouldn't happen but our types don't prohibit us
 
     TimerEvt -> do
@@ -372,4 +373,18 @@ shouldSend peer tx = case tx of
     Origin.Quarry        -> True -- this should never reach this far anyway
     Origin.Morphism      -> -- probably means it was converted, see if this is a problem
         trace "NewTx of type Morphism came in. Should this even happen?" True
+
+-- check that the peer is authorized to receive these chain details, by verifying that their
+-- IPaddress is associated with one of the Enodes in the chain's member list
+checkPeerIsMember :: PPeer -> ChainInfo -> Bool
+checkPeerIsMember peer ci = 
+  case match of
+    Nothing -> False 
+    Just _ -> True
+  where 
+    pIp = readIP $ T.unpack (pPeerIp peer)
+    ipList = ipAddress <$> (members ci)
+    match = find (==pIp) ipList
+
+       
 
