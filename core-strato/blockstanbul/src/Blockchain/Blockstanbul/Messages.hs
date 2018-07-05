67643c7f220a28ecf3a38402243a9d1205ea44a8
Tim Holland <tim@blockapps.net>
2018-07-05 16:46:46 -0400

    Write a messy and unpleasant RLP encoding

--- core-strato/blockstanbul/src/Blockchain/Blockstanbul/Messages.hs
+++ core-strato/blockstanbul/src/Blockchain/Blockstanbul/Messages.hs
@@ -1,11 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 module Blockchain.Blockstanbul.Messages where
 
 import Control.Lens
 import Control.Monad
 import Data.Text
-
 import Test.QuickCheck
+
+import Blockchain.Data.RLP
 import Blockchain.ExtWord
 import Blockchain.Data.Address
 import Blockchain.Data.ArbitraryInstances ()
@@ -35,6 +38,12 @@ data WireMessage = Preprepare MsgAuth View Block
                  | RoundChange {roundchangeAuth :: MsgAuth,
                                 roundchangeView :: View }
                  deriving (Eq, Show)
+
+preprepareCode, prepareCode, commitCode, roundchangeCode :: Integer
+preprepareCode = 0
+prepareCode = 1
+commitCode = 2
+roundchangeCode = 3
 
 data InEvent = IMsg {unIMsg :: WireMessage}
              | Timeout
@@ -69,4 +78,67 @@ authenticate msg =
   in mAddress == Just addr
 
 -- TODO(tim): JSON instances
--- TODO(tim): RLP instances
+
+
+instance RLPSerializable View where
+  rlpEncode (View r s) = RLPArray [rlpEncode r, rlpEncode s]
+  rlpDecode (RLPArray [rlpr, rlps]) = View (rlpDecode rlpr) (rlpDecode rlps)
+  rlpDecode x = error $ "cannot rlpDecode value as as View: " ++ show x
+
+instance RLPSerializable WireMessage where
+  rlpEncode (Preprepare (MsgAuth addr sig) vw blk) = RLPArray
+      [ rlpEncode preprepareCode
+      , RLPString . rlpSerialize . RLPArray $
+        [ rlpEncode vw
+        , rlpEncode blk ]
+      , rlpEncode addr
+      , rlpEncode sig
+      , RLPString ""]
+  rlpEncode (Prepare (MsgAuth addr sig) vw digest) = RLPArray
+      [ rlpEncode prepareCode
+      , RLPString . rlpSerialize . RLPArray $
+        [ rlpEncode vw
+        , rlpEncode digest ]
+      , rlpEncode addr
+      , rlpEncode sig
+      , RLPString ""]
+  rlpEncode (Commit (MsgAuth addr sig) vw digest seal) = RLPArray
+      [ rlpEncode commitCode
+      , RLPString . rlpSerialize . RLPArray $
+        [ rlpEncode vw
+        , rlpEncode digest ]
+      , rlpEncode addr
+      , rlpEncode sig
+      , rlpEncode seal ]
+  rlpEncode (RoundChange (MsgAuth addr sig) vw) = RLPArray
+      [ rlpEncode roundchangeCode
+      , RLPString . rlpSerialize . RLPArray $
+        [ rlpEncode vw,
+          rlpEncode $ SHA 0]
+      , rlpEncode addr
+      , rlpEncode sig
+      , RLPString ""]
+  rlpDecode (RLPArray [code, (RLPString payload), addr, sig, seals ]) =
+      let auth = MsgAuth (rlpDecode addr) (rlpDecode sig)
+          body = rlpDeserialize payload
+      in case (rlpDecode code :: Integer) of
+          0 ->
+            case body of
+                RLPArray [vw, blk] -> Preprepare auth (rlpDecode vw) (rlpDecode blk)
+                _ -> error $ "invalid rlp payload for preprepare: " ++ show body
+          1 ->
+            case body of
+                RLPArray [vw, digest] -> Prepare auth (rlpDecode vw) (rlpDecode digest)
+                _ -> error $ "invalid rlp payload for preprepare: " ++ show body
+          2 ->
+            case body of
+                RLPArray [vw, digest] -> Commit auth (rlpDecode vw) (rlpDecode digest) (rlpDecode seals)
+                _ -> error $ "invalid rlp payload for commit: " ++ show body
+
+          3 ->
+            case body of
+                RLPArray [vw, _] -> RoundChange auth (rlpDecode vw)
+                _ -> error $ "invalid rlp payload for roundchange: " ++ show body
+
+          _ -> error $ "invalid code for blockstanbul message: " ++ show code
+  rlpDecode x = error $ "invalid rlp for blockstanbul message: " ++ show x
