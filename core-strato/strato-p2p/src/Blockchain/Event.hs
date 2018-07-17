bf450dfc0b05cdb8a79709527be16871a77a3e35
Tim Holland <tim@blockapps.net>
2018-07-17 11:42:05 -0400

    Add an origin for Blockstanbul

--- core-strato/strato-p2p/src/Blockchain/Event.hs
+++ core-strato/strato-p2p/src/Blockchain/Event.hs
@@ -315,4 +315,4 @@ shouldSend peer tx = case tx of
     Origin.Quarry        -> True -- this should never reach this far anyway
     Origin.Morphism      -> -- probably means it was converted, see if this is a problem
         trace "NewTx of type Morphism came in. Should this even happen?" True
-
+    Origin.Blockstanbul -> False
