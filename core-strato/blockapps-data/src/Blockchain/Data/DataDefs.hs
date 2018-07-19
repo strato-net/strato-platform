a1837e06a32a5648e1035e99d826afa50d51e798
Tim Holland <tim@blockapps.net>
2018-07-19 14:29:17 -0400

    Reduce duplication with a Lens' Block <ExtraData

--- core-strato/blockapps-data/src/Blockchain/Data/DataDefs.hs
+++ core-strato/blockapps-data/src/Blockchain/Data/DataDefs.hs
@@ -34,6 +34,7 @@ import           Blockchain.ExtWord
 import           Blockchain.SHA
 import           Data.Word
 
+import           Control.Lens
 import           Control.Lens.TH                         (makeLensesFor)
 import           Data.Aeson
 import           GHC.Generics
@@ -51,3 +52,6 @@ type Difficulty = Integer
 
 makeLensesFor [("blockDataExtraData", "extraDataLens"), ("blockDataMixHash", "mixHashlens")] ''BlockData
 makeLensesFor [("blockBlockData", "blockDataLens")] ''Block
+
+extraLens :: Lens' Block BS.ByteString
+extraLens = blockDataLens . extraDataLens
