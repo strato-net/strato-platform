cccbec5c2dedb62044161c8692b1d046a5a456dd
Shelley Zhong <shelley.zhong@columbia.edu>
2018-07-23 19:07:24 +0000

    transformXabi inserts variable values into xabi

--- blockapps-haskell/solidity/src/BlockApps/XAbiConverter.hs
+++ blockapps-haskell/solidity/src/BlockApps/XAbiConverter.hs
@@ -29,8 +29,19 @@ import qualified BlockApps.Solidity.Xabi.Type      as Xabi
 import qualified BlockApps.Storage                 as Storage
 
 transformXabi :: Xabi -> Map.Map Text Text -> Map.Map Word256 Word256
-transformXabi _ _ = error "transformXabi not implemented"
-
+transformXabi Xabi{..} vars = do
+  newXabiVars <- for (Map.toList vars) $ \(varName, val) -> do
+    case Map.lookup varName xabiVars of
+      Nothing -> error "Cannot assign value to a nonexiting contract variable"
+      Just Xabi.VarType{..} -> do 
+        let initialVal = Just $ Text.unpack val
+            newVarType = Xabi.VarType varTypeAtBytes varTypePublic varTypeConstant initialVal varTypeType
+        return $ (varName, newVarType) 
+  let updateVarVal varName curVal = if (Just curVal) == Map.lookup varName (Map.fromList newXabiVars) 
+                                       then Just curVal 
+                                       else Map.lookup varName (Map.fromList newXabiVars)
+  fmap (\(varName, _) -> Map.updateWithKey updateVarVal varName xabiVars) newXabiVars 
+  
 fieldsToStruct::TypeDefs->[((Text, Type), Maybe Text)]->Struct
 fieldsToStruct typeDefs' vars =
   let
