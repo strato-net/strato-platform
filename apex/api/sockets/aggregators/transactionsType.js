d9c06970b7d83c9fff280dc33780201ff1a8675f
nikitamendelbaum <nikitamendelbaum@gmail.com>
2018-07-25 16:52:23 +0700

    apex changes to comply with 'block' table removal in strato db

--- apex/api/sockets/aggregators/transactionsType.js
+++ apex/api/sockets/aggregators/transactionsType.js
@@ -1,93 +1,54 @@
-const _ = require('underscore');
-const { TRANSACTIONS_TYPE } = require('../rooms');
-const { emitter, ON_SOCKET_PUBLISH_EVENTS } = require('../eventBroker');
+const {TRANSACTIONS_TYPE} = require('../rooms');
+const {emitter, ON_SOCKET_PUBLISH_EVENTS} = require('../eventBroker');
 const config = require('../../config/app.config');
-const Block= require('../../models/strato/eth/block');
+const Transaction = require('../../models/strato/eth/transaction');
 
-let transactionsTypes;
+let transactionTypes;
 
 function getTransactionsType() {
-  Block
-    .findAll(
+  Transaction.findAll(
       {
         attributes: [
-          'receipt_transactions'
+          'to_address',
+          'code_or_data',
         ],
-        raw: true, 
-        limit: 15, 
-        order: [['id', 'DESC']] 
+        raw: true,
+        limit: 15,
+        order: [['id', 'DESC']]
       }
-    ).then(function (data) {
-      let receiptTransactions = [];
-
-      _.map(data, function (d) {
-        const blockReceiptTxs = JSON.parse(d.receipt_transactions);
-        _.map(blockReceiptTxs,function(rt){
-          receiptTransactions.push(rt);
-        });
+  ).then(function (data) {
+    if (!data.length) {
+      transactionTypes = [{val: 0, type: "No Transactions"}];
+    } else {
+      let typesCounter = {"FunctionCall": 0, "Transfer": 0, "Contract": 0};
+      data.forEach(tx => {
+        let txType;
+        if (tx.to_address) {
+          txType = tx.code_or_data.length ? 'FunctionCall' : 'Transfer';
+        } else {
+          txType = 'Contract';
+        }
+        typesCounter[txType] += 1;
       });
-
-      const currentTxType = extractTxTypes(receiptTransactions);
-      if (!_.isEqual(currentTxType, transactionsTypes)) {
-        transactionsTypes = currentTxType;
-        emitter.emit(ON_SOCKET_PUBLISH_EVENTS, TRANSACTIONS_TYPE, transactionsTypes);
-      }
-
-    })
-    .catch(function (err) {
-      console.log("getTransactionsType Error:", err);
-    });
-}
-
-function extractTxTypes(receiptTransactions) {
-  let types = { "FunctionCall": 0, "Transfer": 0, "Contract": 0 };
-  receiptTransactions.forEach(v => { types[parseTransactionType(v)]++; });
-  const filtered = _.keys(types)
-    .filter((type)=>{
-      return types[type] > 0;
-    })
-    .map((type) => {
-      return {
-        val: types[type],
-        type: type
-      };
-    });
-  if(filtered.length === 0) {
-    return [{
-      val: 0,
-      type: "No Transactions"
-    }];
-  }
-  return filtered;
-}
-
-// BEWARE :: The receipt transactions are stored as haskell types that
-// were turned into strings. See haskell's `read` and `show` functions
-// to understand why this works.
-// Until the Block table is normalized (or at least until the receipts
-// are stored as JSON) we regex the haskell type for the fields
-// required.
-function parseTransactionType(v) {
-  const toMatched = v.match(/transactionTo = /);
-  const noDataMatched = v.match(/transactionData = \"\"/);
-
-  if (toMatched && noDataMatched) {
-    // Found a `to` address and no data was attached
-	  return "Transfer";
-  } else if (toMatched) {
-    // Found a `to` address and there was a payload
-	  return "FunctionCall";
-  } else {
-    // No `to` address, therefore, contract creation
-	  return "Contract";
-  }
+      transactionTypes = Object.keys(typesCounter).map(typeName => {
+        return {
+          val: typesCounter[typeName],
+          type: typeName,
+        }
+      });
+    }
+    emitter.emit(ON_SOCKET_PUBLISH_EVENTS, TRANSACTIONS_TYPE, transactionTypes);
+  })
+      .catch(function (err) {
+        console.log("getTransactionsType Error:", err);
+      });
 }
 
 getTransactionsType();
 setInterval(getTransactionsType, config.webSockets.dbPollFrequency);
 
 function initialHydrate(socket) {
-  socket.emit(`PRELOAD_${TRANSACTIONS_TYPE}`, transactionsTypes);
+  socket.emit(`PRELOAD_${TRANSACTIONS_TYPE}`, transactionTypes);
 }
 
 module.exports = {
---
+++ apex/api/test/TODO.md
@@ -0,0 +1,1 @@
+Cover with tests the code in sockets/ directory (all websocket logic) + db models in models/strato/ 
