64f79e7f2c71d819a9b6935258922a7a77f32c3c
Daniel Mesko <dpm2153@columbia.edu>
2018-07-26 19:08:03 +0000

    Added specific ToJSON/FromJSON instances for AccountInfo and CodeInfo (mostly copied from the ones in Ethereum.hs)

--- core-strato/blockapps-data/src/Blockchain/Data/ChainInfo.hs
+++ core-strato/blockapps-data/src/Blockchain/Data/ChainInfo.hs
@@ -25,7 +25,6 @@ import           Blockchain.Strato.Model.Address
 -- import           Blockchain.TypeLits
 
 import           Data.Aeson
-import           Data.Aeson.TH                        as AT
 import qualified Data.ByteString                      as B
 import qualified Data.JsonStream.Parser               as JS
 import qualified Data.Map                             as M
@@ -37,10 +36,26 @@ import qualified GHC.Generics                         as GHCG
 import           Test.QuickCheck.Arbitrary
 
 
-data CodeInfo = CodeInfo B.ByteString String String
-  deriving (Show, Read, Eq, GHCG.Generic)
+data CodeInfo = CodeInfo
+  { codeInfoCode   :: B.ByteString
+  , codeInfoSource :: String
+  , codeInfoName   :: String
+  } deriving (Show, Read, Eq, GHCG.Generic)
 
-$(deriveJSON defaultOptions{sumEncoding = AT.UntaggedValue} ''CodeInfo)
+instance FromJSON CodeInfo where
+  parseJSON (Object o) =
+    CodeInfo
+    <$> o .: "code"
+    <*> o .: "src"
+    <*> o .: "name"
+  parseJSON _ = error "parseJSON CodeInfo: expected Object"
+
+instance ToJSON CodeInfo where
+  toJSON (CodeInfo bs s1 s2) = object
+    [ "code" .= bs
+    , "src"  .= s1
+    , "name" .= s2
+    ]
 
 instance RLPSerializable CodeInfo where
   rlpEncode (CodeInfo a b c) = 
@@ -57,14 +72,44 @@ instance Arbitrary CodeInfo where
 data AccountInfo = NonContract Address Integer
                  | ContractNoStorage Address Integer SHA
                  | ContractWithStorage Address Integer SHA [(Word256, Word256)]
-  deriving (Show, Read, Eq)
+   deriving (Show, Eq, Read, GHCG.Generic)
 
-$(deriveJSON defaultOptions{sumEncoding = AT.UntaggedValue} ''AccountInfo)
+instance FromJSON AccountInfo where
+  parseJSON (Object o) = do
+    a <- (o .: "address")
+    b <- (o .: "balance")
+    mc <- (o .:? "codeHash")
+    case mc of
+      Nothing -> return $ NonContract a b
+      Just c -> do
+        ms <- (o .:? "storage")
+        case ms of
+          Nothing -> return $ ContractNoStorage a b c
+          Just s -> do
+            return $ ContractWithStorage a b c s
+  parseJSON o = error $ "parseJSON AccountInfo: Expected object, got: " ++ show o
+
+instance ToJSON AccountInfo where
+  toJSON (NonContract a b) = object
+    [ "address" .= a
+    , "balance" .= b
+    ]
+  toJSON (ContractNoStorage a b c) = object
+    [ "address" .= a
+    , "balance" .= b
+    , "codeHash" .= c
+    ]
+  toJSON (ContractWithStorage a b c s) = object
+    [ "address" .= a
+    , "balance" .= b
+    , "codeHash" .= c
+    , "storage" .= s
+    ]
 
 instance RLPSerializable AccountInfo where
   rlpEncode (NonContract a b) = RLPArray [rlpEncode a, rlpEncode b]
   rlpEncode (ContractNoStorage a b c) = RLPArray [rlpEncode a, rlpEncode b, rlpEncode c]
-  rlpEncode (ContractWithStorage a b c d) = RLPArray [rlpEncode a, rlpEncode b, rlpEncode c, RLPArray (rlpEncode <$> d)]
+  rlpEncode (ContractWithStorage a b c d) = RLPArray [rlpEncode a, rlpEncode b, rlpEncode c, RLPArray $ rlpEncode <$> d]
 
   rlpDecode (RLPArray [a,b]) = NonContract (rlpDecode a) (rlpDecode b)
   rlpDecode (RLPArray [a,b,c]) = ContractNoStorage (rlpDecode a) (rlpDecode b) (rlpDecode c)
@@ -81,7 +126,7 @@ data ChainInfo = ChainInfo {
     acctInfo        :: [AccountInfo],
     codeInfo        :: [CodeInfo],
     members         :: M.Map Address Enode
-} deriving (Eq, Read, Show, GHCG.Generic)
+} deriving (Eq, Show, Read, GHCG.Generic)
 
 instance Arbitrary ChainInfo where
   arbitrary = ChainInfo
