acaa8ee76b12af017a5648666b2867b304852704
Tanuj Soni <tanuj@blockapps.net>
2018-07-26 19:48:04 +0530

    [oauth] API logging

--- oauth/Strato/Strato23/server/src/Lib.hs
+++ oauth/Strato/Strato23/server/src/Lib.hs
@@ -3,7 +3,12 @@ module Lib
     ) where
 
 import Strato.Strato23.Server (router)
+import Network.Wai
+import Network.Wai.Middleware.RequestLogger
 import Network.Wai.Handler.Warp
 
+logOauth :: Application -> Application
+logOauth = logStdoutDev
+
 someFunc :: IO ()
-someFunc = run 8000 router
+someFunc = run 8000 (logOauth router)
