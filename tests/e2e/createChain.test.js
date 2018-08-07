3eefeb71f6da96dee161cadb8cbc79d9eca4d561
dustinnorwood <dnorwood2010@aol.com>
2018-08-07 13:46:17 -0400

    Left-pad Word256s to correct length

--- tests/e2e/createChain.test.js
+++ tests/e2e/createChain.test.js
@@ -14,7 +14,7 @@ const password = '1234';
 
 const label = 'My chain label';
 const src = 'contract Governance { }';
-const args = [];
+const args = {};
 const members = [{
     address: "00000000000000000000000000000000deadbeef"
   , enode: "enode://6d8a80d14311c39f35f516fa664deaaaa13e85b2f7493f37f6144d86991ec012937307647bd3b9a82abe2974e1407241d54947bbb39763a4cac9f77166ad92a0@171.16.0.4:30303?discport=30303"
