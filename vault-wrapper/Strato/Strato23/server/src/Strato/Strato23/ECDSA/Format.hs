a82991bc340bbae538039d46314f41cb5bec49ac
Tanuj Soni <tanuj@blockapps.net>
2018-08-07 11:42:54 +0530

    [oauth][WIP] r,s and v signature list

---
+++ vault-wrapper/Strato/Strato23/server/src/Strato/Strato23/ECDSA/Format.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+
+module Strato.Strato23.ECDSA.Format (
+  Format(..)
+  ) where
+
+import qualified Data.ByteString        as B
+import qualified Data.ByteString.Base16 as B16
+import qualified Data.ByteString.Char8  as BC
+import qualified Data.NibbleString      as N
+
+import           Numeric
+
+class Format a where
+  format::a->String
+
+instance Format B.ByteString where
+  format = BC.unpack . B16.encode
+
+instance Format N.NibbleString where
+  format (N.EvenNibbleString bs)  = format bs
+  format (N.OddNibbleString n bs) = showHex n "" ++ format bs
