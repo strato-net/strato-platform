a2e65c6ee5a52bc7654aff5ed569cf4665d023c5
Shelley Zhong <shelley.zhong@columbia.edu>
2018-08-07 15:16:21 +0000

    Fixed accounts.saga to fetch real data

--- smd-ui/src/components/Accounts/accounts.saga.js
+++ smd-ui/src/components/Accounts/accounts.saga.js
@@ -114,8 +114,8 @@ export function postFaucet(address) {
 
 export function* getAccounts(action) {
   try {
-    // const response = yield call(getAccountsApi);
-    const response = ["sz1152", "sz2699"];
+    const response = yield call(getAccountsApi);
+    // const response = ["sz1152", "sz2699"];
     yield put(fetchAccountsSuccess(response));
     // dispatch the action if necessary
     if (action.loadAddresses && response.length > 0) {
@@ -133,8 +133,8 @@ export function* getAccounts(action) {
 
 export function* getUserAddresses(action) {
   try {
-    // const response = yield call(getUserAddressesApi, action.name);
-    const response = ["999"];
+    const response = yield call(getUserAddressesApi, action.name);
+    // const response = ["999"];
     yield put(fetchUserAddressesSuccess(action.name, response));
     if (action.loadBalances) {
       yield response.map(address => put(fetchAccountDetail(action.name, address)));
