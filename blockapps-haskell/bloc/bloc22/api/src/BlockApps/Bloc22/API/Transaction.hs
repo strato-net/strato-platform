b216fbb970bc65b806819c3a0b9a2a24418fc66b
Shelley Zhong <shelley.zhong@columbia.edu>
2018-08-09 17:33:57 +0000

    Created Transaction.hs for the /transaction route in bloc api

---
+++ blockapps-haskell/bloc/bloc22/api/src/BlockApps/Bloc22/API/Transaction.hs
@@ -0,0 +1,109 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedLists            #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE TypeOperators              #-}
+
+module BlockApps.Bloc22.API.Transaction where
+
+import           Control.Lens                       (mapped)
+import           Control.Lens.Operators             hiding ((.=))
+import           Data.Aeson                         hiding (Success)
+--import           Data.Aeson.Casing
+--import qualified Data.ByteString.Lazy               as ByteString.Lazy
+import           Data.Map                           (Map)
+--import qualified Data.Map                           as Map
+--import           Data.Proxy
+import           Data.Text                          (Text)
+--import qualified Data.Text.Encoding                 as Text
+import           Generic.Random.Generic
+import           GHC.Generics
+import           Numeric.Natural
+import           Servant.API                        as S
+import           Servant.Docs
+import           Test.QuickCheck                    hiding (Success,Failure)
+
+import           BlockApps.Bloc22.API.SwaggerSchema
+import           BlockApps.Bloc22.API.Users         (BlocTransactionResult)
+import           BlockApps.Bloc22.API.Utils
+--import           BlockApps.Bloc22.Crypto
+import           BlockApps.Ethereum
+import           BlockApps.Solidity.ArgValue
+--import           BlockApps.Solidity.SolidityValue
+--import           BlockApps.Solidity.Xabi
+import           BlockApps.Strato.Types
+
+--------------------------------------------------------------------------------
+---- Routes and Types
+--------------------------------------------------------------------------------
+
+data BlocTransactionType = CONTRACT | TRANSFER | FUNCTION deriving (Eq, Show, Generic)
+
+instance Arbitrary BlocTransactionType where
+  arbitrary = genericArbitrary uniform
+
+instance FromJSON BlocTransactionType where
+  parseJSON = genericParseJSON defaultOptions
+
+instance ToJSON BlocTransactionType where
+  toJSON = genericToJSON defaultOptions
+
+instance ToSchema BlocTransactionType where
+  declareNamedSchema proxy = genericDeclareNamedSchema blocSchemaOptions proxy
+    & mapped.schema.description ?~ "Bloc Transaction Type"
+    & mapped.schema.example ?~ toJSON CONTRACT
+
+--------------------------------------------------------------------------------
+
+type PostTransaction = "transaction"
+  :> S.Header "X-USER-UNIQUE-NAME" Text
+  :> QueryParam "chainid" ChainId
+  :> QueryFlag "resolve"
+  :> ReqBody '[JSON] PostTransactionRequest
+  :> Post '[JSON] BlocTransactionResult
+
+data PostTransactionRequest = PostTransactionRequest
+  { posttransactionrequestTransactionType :: BlocTransactionType
+  , posttransactionrequestPayload         :: BlocTransactionPayload
+  , posttransactionrequestTxParams        :: Maybe TxParams
+  } deriving (Eq, Show, Generic)
+
+--instance Arbitrary PostTransactionRequest where 
+  --arbitrary = genericArbitrary uniform
+
+instance ToJSON PostTransactionRequest where
+
+instance FromJSON PostTransactionRequest where
+
+instance ToSample PostTransactionRequest where
+  toSamples _ = noSamples
+
+instance ToSchema PostTransactionRequest where
+
+data BlocTransactionPayload = 
+  ContractPayload {
+    contractpayloadSrc      :: Text
+  , contractpayloadContract :: Maybe Text
+  , contractpayloadArgs     :: Maybe (Map Text ArgValue)
+  , contractpayloadValue    :: Maybe (Strung Natural)
+  } |
+  TransferPayload {
+    transferpayloadToAddress :: Address
+  , transferpayloadValue     :: Strung Natural
+  } |
+  FunctionPayload {
+    functionpayloadMethod :: Text
+  , functionpayloadArgs   :: Map Text ArgValue
+  , functionpayloadValue  :: Maybe (Strung Natural) 
+  } deriving (Eq, Show, Generic)
+
+instance ToJSON BlocTransactionPayload where
+
+instance FromJSON BlocTransactionPayload where
+
+instance ToSchema BlocTransactionPayload where
