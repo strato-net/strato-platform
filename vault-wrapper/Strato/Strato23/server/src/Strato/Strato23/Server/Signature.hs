538dd12e8757c5f296405fcd616d817636e92c45
dustinnorwood <dnorwood2010@aol.com>
2018-08-13 15:22:35 -0400

    Use secp256k1 for signing

--- vault-wrapper/Strato/Strato23/server/src/Strato/Strato23/Server/Signature.hs
+++ vault-wrapper/Strato/Strato23/server/src/Strato/Strato23/Server/Signature.hs
@@ -2,58 +2,53 @@
 
 module Strato.Strato23.Server.Signature where
 
-import           Servant
-import           Data.Text
-import           Data.List
 import           Control.Monad.IO.Class
+import           Crypto.Secp256k1
+import qualified Data.ByteString.Base16        as B16
+import qualified Data.ByteString.Char8         as C8
+import qualified Data.ByteString.Lazy.Char8    as Lazy.Char8
+import           Data.Maybe                    (fromJust, fromMaybe, isNothing)
+import           Data.List
+import           Data.Text
 import           GHC.Generics
+import           Servant
 import           Strato.Strato23.API.Signature
-import           Strato.Strato23.ECDSA.Util
-import           Data.Maybe (fromJust, isNothing)
 
-import           Strato.Strato23.ECDSA.ExtendedECDSA
-import           Strato.Strato23.ECDSA.ExtendedSignature
-import           Strato.Strato23.ECDSA.BigWord as BW  
-import           Strato.Strato23.ECDSA.Hash (hash256)
-import           Strato.Strato23.ECDSA.ECDSA as HK
-import           Strato.Strato23.ECDSA.Keys
-import qualified Data.ByteString.Lazy.Char8         as Lazy.Char8
-
-data PrivateKey = PrivateKey {
-  username :: String
-  , address :: String
-  , pk :: String
-} deriving (Eq, Show, Generic)
+data PrivateKey = PrivateKey
+  { username :: String
+  , address  :: String
+  , secKey   :: SecKey
+  } deriving (Eq, Show, Generic)
 
 privateKeys :: [PrivateKey]
-privateKeys = 
-  [ PrivateKey "tanuj500" "cb1a02b33d6fba1a226bdd8a45da4654e0f1ecd9" "5Kg1gnAjaLfKiwhhPpGS3QfRg2m6awQvaj98JCZBZQ5SuS2F15C"
-  , PrivateKey  "tanuj@blockapps.net" "84bcd6278fdde4e3147717123a4602faeeee83e4" "5HvaLAxPMej789DdFwgbvr6TqDo17jixKQwCKY4zZxnUy5p2i9x"
+privateKeys =
+  [ PrivateKey "tanuj500" "cb1a02b33d6fba1a226bdd8a45da4654e0f1ecd9" (toSecKey "00000000000000000000000000000000000000000000000000000000deadbeef")
+  , PrivateKey  "tanuj@blockapps.net" "84bcd6278fdde4e3147717123a4602faeeee83e4" (toSecKey "0000000000000000000000000000000000000000000000000000000012345687")
   ]
+
+toSecKey :: String -> SecKey
+toSecKey = fromMaybe (error "toSecKey: could not decode") . secKey . fst . B16.decode . C8.pack
 
 getHash256 :: String ->  Word256
 getHash256 inputStr = hash256 $ stringToBS inputStr
 
 getPrivateKeyOfUser :: String -> PrivateKey
-getPrivateKeyOfUser uname = Data.List.head [ x | x <- privateKeys, username x == uname]
-
-getPk :: String -> PrvKey
-getPk strPk= fromJust $ fromWif strPk
+getPrivateKeyOfUser = head . flip filter privateKeys . (== username)
 
 signatureDetails :: Maybe Text -> UserData -> Handler SignatureDetails
 signatureDetails userEmail (UserData queryToSig) = do
   if  (Data.List.null queryToSig)
-    then throwError err400 { errBody = Lazy.Char8.pack "msgHash not found" } 
+    then throwError err400 { errBody = Lazy.Char8.pack "msgHash not found" }
     else if (isNothing userEmail)
-        then throwError err404 { errBody = Lazy.Char8.pack "No cookie provided" } 
+        then throwError err404 { errBody = Lazy.Char8.pack "No cookie provided" }
         else do
           let emailId = fromJust userEmail
               prvKey = pk $ getPrivateKeyOfUser $ Data.Text.unpack emailId
               -- prvKey = pk $ getPrivateKeyOfUser $ "tanuj@blockapps.net"
               dataToSign = queryToSig
               word256 = getHash256 dataToSign
-          ExtendedSignature signature' yIsOdd' <- liftIO $ HK.withSource HK.devURandom $ extSignMsg word256 $ getPk prvKey
-          let r' = bsToHex $ integerToBS $ BW.getBigWordInteger $ HK.sigR signature'
-              s' = bsToHex $ integerToBS $ BW.getBigWordInteger $ HK.sigS signature'
-              v' = bsToHex $ integerToBS $ if yIsOdd' then 0x1c else 0x1b
-          return (SignatureDetails r' s' v')
+          case msg word256 of
+            Nothing -> throwError err500 "msgHash was not 32 bytes long"
+            Just msg' -> do
+              let CompactRecSig{..} = exportCompactRecSig . signRecMsg msg' $ secKey prvKey
+              return (SignatureDetails getCompactRecSigR getCompactRecSigS getCompactRecSigV)
