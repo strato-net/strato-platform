012157a3c52cc7f57071939ec4d7ca3448d81927
Tanuj Soni <tanuj@blockapps.net>
2018-08-13 13:34:46 +0530

    [oauth] Error handling

--- vault-wrapper/Strato/Strato23/server/src/Strato/Strato23/Server/Signature.hs
+++ vault-wrapper/Strato/Strato23/server/src/Strato/Strato23/Server/Signature.hs
@@ -9,7 +9,7 @@ import           Control.Monad.IO.Class
 import           GHC.Generics
 import           Strato.Strato23.API.Signature
 import           Strato.Strato23.ECDSA.Util
-import           Data.Maybe (fromJust)
+import           Data.Maybe (fromJust, isNothing)
 
 import           Strato.Strato23.ECDSA.ExtendedECDSA
 import           Strato.Strato23.ECDSA.ExtendedSignature
@@ -17,6 +17,7 @@ import           Strato.Strato23.ECDSA.BigWord as BW
 import           Strato.Strato23.ECDSA.Hash (hash256)
 import           Strato.Strato23.ECDSA.ECDSA as HK
 import           Strato.Strato23.ECDSA.Keys
+import qualified Data.ByteString.Lazy.Char8         as Lazy.Char8
 
 data PrivateKey = PrivateKey {
   username :: String
@@ -40,15 +41,19 @@ getPk :: String -> PrvKey
 getPk strPk= fromJust $ fromWif strPk
 
 signatureDetails :: Maybe Text -> UserData -> Handler SignatureDetails
-signatureDetails userEmail userData = do
-  let emailId = fromJust userEmail
-      prvKey = pk $ getPrivateKeyOfUser $ Data.Text.unpack emailId
-      -- prvKey = pk $ getPrivateKeyOfUser $ "tanuj@blockapps.net"
-      dataToSign = queryToSign userData
-      word256 = getHash256 dataToSign
-  ExtendedSignature signature' yIsOdd' <- liftIO $ HK.withSource HK.devURandom $ extSignMsg word256 $ getPk prvKey
-  let r' = bsToHex $ integerToBS $ BW.getBigWordInteger $ HK.sigR signature'
-      s' = bsToHex $ integerToBS $ BW.getBigWordInteger $ HK.sigS signature'
-      v' = bsToHex $ integerToBS $ if yIsOdd' then 0x1c else 0x1b
-  liftIO $ print $ signature'
-  return (SignatureDetails r' s' v')
+signatureDetails userEmail (UserData queryToSig) = do
+  if  (Data.List.null queryToSig)
+    then throwError err400 { errBody = Lazy.Char8.pack "msgHash not found" } 
+    else if (isNothing userEmail)
+        then throwError err404 { errBody = Lazy.Char8.pack "No cookie provided" } 
+        else do
+          let emailId = fromJust userEmail
+              prvKey = pk $ getPrivateKeyOfUser $ Data.Text.unpack emailId
+              -- prvKey = pk $ getPrivateKeyOfUser $ "tanuj@blockapps.net"
+              dataToSign = queryToSig
+              word256 = getHash256 dataToSign
+          ExtendedSignature signature' yIsOdd' <- liftIO $ HK.withSource HK.devURandom $ extSignMsg word256 $ getPk prvKey
+          let r' = bsToHex $ integerToBS $ BW.getBigWordInteger $ HK.sigR signature'
+              s' = bsToHex $ integerToBS $ BW.getBigWordInteger $ HK.sigS signature'
+              v' = bsToHex $ integerToBS $ if yIsOdd' then 0x1c else 0x1b
+          return (SignatureDetails r' s' v')
