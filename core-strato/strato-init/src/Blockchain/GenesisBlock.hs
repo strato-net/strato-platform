46e4620a46cd8319a010c87a250c6419240fc8e0
Tim Holland <tim@blockapps.net>
2018-08-13 16:45:30 -0400

    Revert "TMP: Debugging checkpoint"
    This reverts commit 6b080c13f1dd023d6d2f79932812ef6a18062c60.

--- core-strato/strato-init/src/Blockchain/GenesisBlock.hs
+++ core-strato/strato-init/src/Blockchain/GenesisBlock.hs
@@ -2,7 +2,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TupleSections     #-}
 {-# LANGUAGE TemplateHaskell   #-}
-{-# LANGUAGE BangPatterns      #-}
 
 module Blockchain.GenesisBlock (
   initializeGenesisBlock,
