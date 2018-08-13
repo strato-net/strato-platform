e10041592be312c80ed7d14857f52794b0e06636
Tim Holland <tim@blockapps.net>
2018-08-13 16:53:45 -0400

    Remove commented out imports

--- core-strato/merkle-patricia-db/src/Blockchain/Database/MerklePatricia/ForEach.hs
+++ core-strato/merkle-patricia-db/src/Blockchain/Database/MerklePatricia/ForEach.hs
@@ -1,31 +1,17 @@
-
 module Blockchain.Database.MerklePatricia.ForEach where
 
 import           Control.Monad
 import           Control.Monad.Trans.Resource
---import qualified Data.ByteString                              as B
---import           Data.Default
---import           Data.Function
---import           Data.List
---import           Data.Maybe
 import           Data.NibbleString (NibbleString)
 import qualified Data.NibbleString                            as N
---import qualified Database.LevelDB                             as DB
 
---import           Blockchain.Data.RLP
 import           Blockchain.Database.MerklePatricia.Internal
---import           Blockchain.Database.MerklePatricia.MPDB
 import           Blockchain.Database.MerklePatricia.NodeData
---import           Blockchain.Database.MerklePatricia.StateRoot
---import           Blockchain.Format
---import           Blockchain.Strato.Model.SHA                  (keccak256)
 
 forEach::MonadResource m=>MPDB->(Key->Val->m ())->m ()
-forEach db f = 
+forEach db f =
   let dbNodeRef = PtrRef $ stateRoot db
   in forEach_NodeRef db dbNodeRef N.empty f
-
-
 
 
 forEach_NodeData::MonadResource m=>MPDB->NodeData->NibbleString->(Key->Val->m ())->m ()
@@ -33,13 +19,11 @@ forEach_NodeData::MonadResource m=>MPDB->NodeData->NibbleString->(Key->Val->m ()
 forEach_NodeData _ EmptyNodeData _ _ = return ()
 
 forEach_NodeData db (FullNodeData {choices=cs}) partialKey f =
-  forM_ (zip cs [0..]) $ \(ref, n) -> 
+  forM_ (zip cs [0..]) $ \(ref, n) ->
     forEach_NodeRef db ref (partialKey `N.append` N.singleton n) f
 
 forEach_NodeData db ShortcutNodeData{nextNibbleString=s, nextVal=Left ref} partialKey f =
   forEach_NodeRef db ref (partialKey `N.append` s) f
-
-
 
 forEach_NodeData _ ShortcutNodeData{nextNibbleString=s, nextVal=Right val} partialKey f =
   f (partialKey `N.append` s) val
