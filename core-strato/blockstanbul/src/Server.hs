1c0ef9976e8a0e688539068ca0cd1b2f0b5809f1
Yunfan Jiang <yunfan@blockapps.net>
2018-08-14 14:44:12 -0400

    change on name convention

--- core-strato/blockstanbul/src/Server.hs
+++ core-strato/blockstanbul/src/Server.hs
@@ -8,12 +8,40 @@
 module Server where 
 
 import Servant
-import API
+--import Servant.Server
+--import Servant.Client
+import Control.Concurrent.STM.TMChan
+--import Network.Wai
+import Network.Wai.Handler.Warp
+--import Network.HTTP.Client (newManager, defaultManagerSettings)
 import Blockchain.Data.Address
-import Blockchain.Blockstanbul.Messages (InEvent(NewBeneficiary))
+--import Blockchain.Blockstanbul.Messages (InEvent(NewBeneficiary))
+
+import API
 
 admin :: Server AdminAPI 
-admin = createVote
+admin = createVote 
 
-createVote :: Address -> Bool -> Handler InEvent
-createVote addr for_against = return $ NewBeneficiary addr for_against
+createVote :: Address -> Bool -> Handler (Address, Bool)
+createVote addr for_against = return (addr,for_against)
+
+createWebServer :: Application
+createWebServer = serve adminAPI admin
+
+webserver :: IO()
+webserver = run 8081 createWebServer
+
+ {- where ch = do
+          x <- atomically $ newTMChan
+          return $ x :. EmptyContext-}
+
+--getVote :: Address -> Bool -> ClientM (Address,Bool)
+--getVote = client (Proxy @ GetVote)
+
+type VotingMessage = TMChan (Address,Bool)
+
+--constructChannel :: IO()
+--constructChannel = do
+  --manager <- newManager defaultManagerSettings
+  --vot <- runClientM getVote (mkClientEnv manager (BaseUrl Http "localhost" 8081 ""))
+  --atomically $ writeTMChan chan vot
