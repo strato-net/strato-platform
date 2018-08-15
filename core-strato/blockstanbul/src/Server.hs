e4fcdb0d92ff172032667cfc245b998563aa3eb1
Yunfan Jiang <yunfan@blockapps.net>
2018-08-15 15:26:51 -0400

    add writechan

--- core-strato/blockstanbul/src/Server.hs
+++ core-strato/blockstanbul/src/Server.hs
@@ -8,42 +8,30 @@
 module Server where 
 
 import Servant
---import Servant.Server
---import Servant.Client
 import Control.Concurrent.STM
 import Control.Concurrent.STM.TMChan
-import Control.Monad
---import Network.Wai
+import Control.Monad.IO.Class
 import Network.Wai.Handler.Warp
-import Network.HTTP.Simple
---import Network.HTTP.Client (newManager, defaultManagerSettings)
 import Blockchain.Data.Address
---import Blockchain.Blockstanbul.Messages (InEvent(NewBeneficiary))
 
 import API
 
-admin :: Server AdminAPI 
-admin = createVote 
+admin :: TMChan (Address, Bool) -> Server AdminAPI 
+admin = createVote  
 
-createVote :: Address -> Bool -> Handler (Address, Bool)
-createVote addr for_against = return (addr,for_against)
+createVote :: TMChan (Address, Bool) -> Address -> Bool -> Handler (Address, Bool)
+createVote ch addr for_against = do 
+  liftIO $ atomically $ writeTMChan ch (addr, for_against) 
+  return (addr,for_against)
 
-createWebServer :: Application
-createWebServer = serve adminAPI admin
+createWebServer :: TMChan (Address,Bool) -> Application
+createWebServer ch = serve adminAPI (admin ch)
 
 webserver :: TMChan (Address,Bool) -> IO()
-webserver ch = do
-  run 8081 createWebServer
-  chanIn ch
+webserver ch = run 8081 $ createWebServer ch
 
-chanIn ::TMChan (Address,Bool) -> IO ()
-chanIn ch = forever $ do                 -- need tests. streaming api instead?
-  response <- httpJSON "http://localhost:8081/vote/"
-  putStrLn $ "The status code was: " ++
-               show (getResponseStatusCode response)
-  print $ getResponseHeader "Content-Type" response
-  let vote = getResponseBody response :: (Address,Bool)
-  atomically $ writeTMChan ch vote
+  
+
 
  {- where ch = do
           x <- atomically $ newTMChan
@@ -51,8 +39,6 @@ chanIn ch = forever $ do                 -- need tests. streaming api instead?
 
 --getVote :: Address -> Bool -> ClientM (Address,Bool)
 --getVote = client (Proxy @ GetVote)
-
-type VotingMessage = TMChan (Address,Bool)
 
 --constructChannel :: IO()
 --constructChannel = do
