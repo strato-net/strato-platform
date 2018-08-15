3e3732fb0011736456fefdee444da635ce5e7eaf
dustinnorwood <dnorwood2010@aol.com>
2018-08-15 14:40:02 -0400

    Read users table in signature route

--- vault-wrapper/Strato/Strato23/server/src/Strato/Strato23/Server/Signature.hs
+++ vault-wrapper/Strato/Strato23/server/src/Strato/Strato23/Server/Signature.hs
@@ -5,46 +5,29 @@
 module Strato.Strato23.Server.Signature where
 
 import           Crypto.Secp256k1
-import qualified Data.ByteString.Base16        as B16
-import qualified Data.ByteString.Char8         as C8
-import           Data.Maybe                    (fromJust, fromMaybe, isNothing)
-import qualified Data.Text                     as T
-import           GHC.Generics
+import           Data.Monoid                      ((<>))
+import qualified Data.Text                        as T
 import           Strato.Strato23.Monad
 import           Strato.Strato23.API.Signature
 import           Strato.Strato23.API.Types
-import           Strato.Strato23.Server.Utils  (word256ToByteString)
-
-data PrivateKey = PrivateKey
-  { username :: String
-  , address  :: String
-  , sk       :: SecKey
-  } deriving (Eq, Show, Generic)
-
-privateKeys :: [PrivateKey]
-privateKeys =
-  [ PrivateKey "tanuj500" "cb1a02b33d6fba1a226bdd8a45da4654e0f1ecd9" (toSecKey "00000000000000000000000000000000000000000000000000000000deadbeef")
-  , PrivateKey  "tanuj@blockapps.net" "84bcd6278fdde4e3147717123a4602faeeee83e4" (toSecKey "0000000000000000000000000000000000000000000000000000000012345687")
-  ]
-
-toSecKey :: String -> SecKey
-toSecKey = fromMaybe (error "toSecKey: could not decode") . secKey . fst . B16.decode . C8.pack
-
-getPrivateKeyOfUser :: String -> PrivateKey
-getPrivateKeyOfUser uname = head $ filter (\PrivateKey{..} -> uname == username) privateKeys
+import           Strato.Strato23.Database.Queries (getUserKeyQuery)
+import           Strato.Strato23.Server.Utils     (word256ToByteString)
 
 signatureDetails :: Maybe T.Text -> UserData -> VaultM SignatureDetails
-signatureDetails userEmail (UserData (Hex msgHash)) = do
-  if (isNothing userEmail)
-    then vaultWrapperError $ UserError "No cookie provided"
-    else do
-      let emailId = fromJust userEmail
-          prvKey = getPrivateKeyOfUser $ T.unpack emailId
-      case msg (word256ToByteString msgHash) of
+signatureDetails mUserId (UserData (Hex msgHash)) = case mUserId of
+  Nothing -> vaultWrapperError $ UserError "No user ID provided"
+  Just userId -> do
+    mPrvKey <- vaultTransaction
+             . toUserError ("User " <> userId <> " not found")
+             . vaultQuery1
+             $ getUserKeyQuery userId
+    case secKey mPrvKey of
+      Nothing -> vaultWrapperError $ AnError "coult not decode private key"
+      Just prvKey -> case msg (word256ToByteString msgHash) of
         Nothing -> vaultWrapperError $ AnError "message was not 32 bytes long"
         Just msg' -> do
-          let sig = exportCompactRecSig $ signRecMsg (sk prvKey) msg'
+          let sig = exportCompactRecSig $ signRecMsg prvKey msg'
           return $ SignatureDetails
-                     (Hex $ getCompactRecSigR sig)
-                     (Hex $ getCompactRecSigS sig)
-                     (Hex $ getCompactRecSigV sig)
+                    (Hex $ getCompactRecSigR sig)
+                    (Hex $ getCompactRecSigS sig)
+                    (Hex $ getCompactRecSigV sig)
