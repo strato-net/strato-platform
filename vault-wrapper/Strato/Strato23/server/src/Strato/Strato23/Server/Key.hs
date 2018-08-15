e1a6c38848f53f6bbc2198a3d36e36d73e0e364d
dustinnorwood <dnorwood2010@aol.com>
2018-08-15 15:51:34 -0400

    Create and store private key in database

--- vault-wrapper/Strato/Strato23/server/src/Strato/Strato23/Server/Key.hs
+++ vault-wrapper/Strato/Strato23/server/src/Strato/Strato23/Server/Key.hs
@@ -1,8 +1,33 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Strato.Strato23.Server.Key where
 
-import           Data.Text             (Text)
-import           Strato.Strato23.Monad
+import           Control.Monad.IO.Class           (liftIO)
+import           Crypto.Random.Entropy
+import           Crypto.Secp256k1
+import qualified Data.ByteString                  as BS
+import           Data.Maybe                       (fromMaybe)
+import           Data.Text                        (Text)
 import           Strato.Strato23.API
+import           Strato.Strato23.Monad
+import           Strato.Strato23.Database.Queries (postUserKeyQuery)
+
+newSecKey :: IO SecKey
+newSecKey = fromMaybe err . secKey <$> getEntropy 32
+  where
+    err = error "could not generate secret key"
+
+deriveAddress :: SecKey -> Address
+deriveAddress = keccak256Address . BS.drop 1 . exportPubKey False . derivePubKey
 
 postKey :: Maybe Text -> VaultM Address
-postKey _ = return (Address 0)
+postKey mUserId = case mUserId of
+  Nothing -> vaultWrapperError $ UserError "No user ID provided"
+  Just userId -> do
+    pKey <- liftIO newSecKey
+    _ <- vaultTransaction
+       . toUserError ("User " <> userId <> " already exists")
+       . vaultModify
+       . postUserKeyQuery userId
+       $ getSecKey pKey
+    return $ deriveAddress pKey
