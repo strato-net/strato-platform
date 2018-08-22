1af988da4c911183e86b99b8290ee5da576de02b
Tim Holland <tim@blockapps.net>
2018-08-22 12:47:59 -0400

    Use random bytestrings to try to reduce unfairness

--- core-strato/ethereum-rlp/bench/RLPShootout.hs
+++ core-strato/ethereum-rlp/bench/RLPShootout.hs
@@ -1,50 +1,45 @@
+{-# OPTIONS_GHC -fno-warn-unused-top-binds #-}
 import qualified Data.ByteString as BS
+import qualified Data.ByteString.Random as BR
+
+import Control.Monad (replicateM)
 
 import Blockchain.Data.RLP
 
 import Criterion.Main
 
-benchSerialize_slow :: Int -> Benchmark
-benchSerialize_slow n = bench ("List based string serialization; size=" ++ show n)
+benchSerialize_slow :: BS.ByteString -> Benchmark
+benchSerialize_slow str = bench ("List based string serialization; size=" ++ show (BS.length str))
                       . nf rlpSerialize_slow
                       . RLPString
-                      $ BS.replicate n 0x49
+                      $ str
 
-benchSerialize :: Int -> Benchmark
-benchSerialize n = bench ("Put based string serialization; size=" ++ show n)
-                 . nf rlpSerialize
-                 . RLPString
-                 $ BS.replicate n 0x22
+benchSerialize :: BS.ByteString-> Benchmark
+benchSerialize str = bench ("Put based string serialization; size=" ++ show (BS.length str))
+                   . nf rlpSerialize
+                   . RLPString
+                   $ str
 
-benchObjSerialize_slow :: Int -> Benchmark
-benchObjSerialize_slow n = bench ("List based array serialization; size=1024x" ++ show n)
+benchObjSerialize_slow :: [BS.ByteString] -> Benchmark
+benchObjSerialize_slow arr = bench ("List based array serialization; size=1024x" ++ show (BS.length (head arr)))
                          . nf rlpSerialize_slow
                          . RLPArray
-                         . replicate n
-                         . RLPString
-                         $ BS.replicate 1024 0x88
+                         . map RLPString
+                         $ arr
 
-benchObjSerialize :: Int -> Benchmark
-benchObjSerialize n = bench ("Put based array serialization; size=1024x" ++ show n)
+benchObjSerialize :: [BS.ByteString] -> Benchmark
+benchObjSerialize arr = bench ("Put based array serialization; size=1024x" ++ show (BS.length (head arr)))
                     . nf rlpSerialize
                     . RLPArray
-                    . replicate n
-                    . RLPString
-                    $ BS.replicate 1024 0x88
-
-benchDeserialize :: Int -> Benchmark
-benchDeserialize n = bench ("List based deserialization; size=" ++ show n)
-                      . nf rlpDeserialize
-                      . rlpSerialize
-                      . RLPString
-                      $ BS.replicate n 0x49
+                    . map RLPString
+                    $ arr
 
 main :: IO ()
-main = let sizes = [1024, 1024 * 1024, 10 * 1024 * 1024]
-           objSizes = [1024, 10 * 1024]
-           tests = map benchSerialize_slow sizes
-                ++ map benchSerialize sizes
-                ++ map benchDeserialize sizes
-                ++ map benchObjSerialize_slow objSizes
-                ++ map benchObjSerialize objSizes
-       in defaultMain tests
+main = do
+  strings <- mapM BR.random [1024, 1024 * 1024, 10 * 1024 * 1024]
+  arrays <- mapM (replicateM 1024 . BR.random) [1, 1024, 10 * 1024]
+  let tests = map benchSerialize strings
+           ++ map benchSerialize_slow strings
+           ++ map benchObjSerialize arrays
+           ++ map benchObjSerialize_slow arrays
+  defaultMain tests
