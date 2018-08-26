228564074394a1fb46c078d8287808f0f1e7fc56
Jamshid <jamshidnh@gmail.com>
2018-08-26 01:24:30 -0400

    Reverting the manimal changes....  It was a mistake to branch from manimal in the first place, I meant to branch from develop.

--- blockapps-haskell/slipstream/src/Slipstream/OutputData.hs
+++ blockapps-haskell/slipstream/src/Slipstream/OutputData.hs
@@ -4,11 +4,7 @@
   , BangPatterns
 #-}
 
-module Slipstream.OutputData (
-  convertRet,
-  dbConnect,
-  dbInsert
-  ) where
+module Slipstream.OutputData where
 
 import qualified Data.ByteString.Char8 as BC
 import qualified Data.ByteString as B
@@ -20,15 +16,14 @@ import Slipstream.Options
 import Slipstream.SolidityValue
 import qualified Data.Map as Map
 import BlockApps.Solidity.Value
-import Data.List
 import Data.List.Utils (replace)
 import Slipstream.Events
 import Control.Monad
 import qualified Data.List as L
 import Data.IORef
 
---defaultMaxB :: Integer
---defaultMaxB = 32 * 1024 * 1024
+defaultMaxB :: Integer
+defaultMaxB = 32 * 1024 * 1024
 
 valueToTxt :: SolidityValue -> String
 valueToTxt (SolidityNum _) = "bigint"
@@ -36,24 +31,18 @@ valueToTxt (SolidityBool _) = "bool"
 valueToTxt (SolidityArray _) = "text []"
 valueToTxt (_) = "text"
 
+listToKeyStatement :: String -> [(T.Text, b)] -> String
+listToKeyStatement _ [] = []
+listToKeyStatement _ [(x, _)] = "\"" ++ T.unpack x ++ "\""
+listToKeyStatement s ((x,_):es) = "\"" ++ T.unpack x ++ "\"" ++ s ++ (listToKeyStatement s es)
 
-tableColumn :: (T.Text, SolidityValue) -> String
-tableColumn (x, y) = quoteIt (T.unpack x) ++ " " ++ valueToTxt y
-
-quoteIt :: String -> String
-quoteIt x = "\"" ++ x ++ "\"" -- need some type of escaping here also
-
-singleQuoteIt :: String -> String
-singleQuoteIt x = "'" ++ escapeQuotes x ++ "'"
-
-valueToString :: SolidityValue -> String
-valueToString (SolidityValueAsString x) = singleQuoteIt $ T.unpack x
-valueToString (SolidityBool x) = singleQuoteIt $ show x
-valueToString (SolidityNum x ) = singleQuoteIt $ show x
-valueToString (SolidityBytes x) = singleQuoteIt $ show x
-valueToString (SolidityArray x) =
-  singleQuoteIt $  "{" ++ intercalate ", " (map arrayContent x) ++ "}"
-valueToString (SolidityObject x) = singleQuoteIt $ show x
+valueToString :: String -> SolidityValue -> String
+valueToString s (SolidityValueAsString x) = s ++ (escapeQuotes $ T.unpack x) ++ s
+valueToString s (SolidityBool x) = s ++ show x ++ s
+valueToString s (SolidityNum x ) = s ++ show x ++ s
+valueToString s (SolidityBytes x) = s ++ (escapeQuotes $ show x) ++ s
+valueToString s (SolidityArray x) = s ++ "{" ++ arrayToString x ++ "}" ++ s
+valueToString s (SolidityObject x) = s ++ (escapeQuotes $ show x) ++ s
 
 escapeQuotes :: String -> String
 escapeQuotes x = replace "\'" "\'\'" $ replace "\"" "\\\"" x
@@ -66,13 +55,26 @@ arrayContent (SolidityBytes x) = escapeQuotes $ show x
 arrayContent (SolidityArray x) = escapeQuotes $ show x
 arrayContent (SolidityObject x) = escapeQuotes $ show x
 
+arrayToString :: [SolidityValue] -> String
+arrayToString [] = []
+arrayToString [x] =  arrayContent x
+arrayToString (x:es) = arrayContent x ++ ", " ++ arrayToString es
 
+listToValueStatement :: String -> [(a, SolidityValue)] -> String
+listToValueStatement _ [] = []
+listToValueStatement _ [(_, y)] = valueToString "\'" y
+listToValueStatement s ((_, y):es) = valueToString "\'" y ++ s ++ (listToValueStatement s es)
 
---tableUpsert :: [(T.Text, SolidityValue)] -> String
---tableUpsert x = intercalate ", " $ map (upsertCriteria . fst) x
+tableColumns :: [(T.Text, SolidityValue)] -> String
+tableColumns [] = []
+tableColumns [(x, y)] = "\"" ++ T.unpack x ++ "\"" ++ " " ++ valueToTxt y
+tableColumns ((x, y):es) = "\"" ++ T.unpack x ++ "\"" ++ " " ++ valueToTxt y ++ ", " ++ tableColumns es
 
-upsertCriteria :: T.Text -> String
-upsertCriteria x = "\"" ++ T.unpack x ++ "\"" ++ " = excluded." ++ "\"" ++ T.unpack x ++ "\""
+tableUpsert :: [(T.Text, SolidityValue)] -> String
+tableUpsert [] = []
+tableUpsert [(x, _)] = "\"" ++ T.unpack x ++ "\"" ++ " = excluded." ++ "\"" ++ T.unpack x ++ "\""
+tableUpsert ((x, _):es) = "\"" ++ T.unpack x ++ "\"" ++ " = excluded." ++ "\"" ++ T.unpack x ++ "\"" ++  ", " ++ tableUpsert es
+
 
 dbConnect :: PGDatabase
 dbConnect =  PGDatabase
@@ -98,103 +100,61 @@ isFunction (_) = True
 
 convertRet :: [ProcessedContract] -> PGConnection -> IORef (Map.Map String ContractAndXabi) -> IO()
 convertRet metadata conn cache = do
+  let firstContract = head metadata
+  let hashVal = codehash firstContract
+  contractCache <- readIORef cache
+  cachedContract <- case Map.lookup hashVal contractCache of
+    Just x -> return x
+    Nothing -> return ContractAndXabi{contract = Left "error", xabi = "error", name = "error", contractStored = False}
 
   if (length metadata > 1)
     then do
-      let list = Map.toList $ Map.map valueToSolidityValue $ Map.filter isFunction $ contractData $ head metadata
-      addNewContract conn cache metadata list
-
-      let keySt =
-            "("
-            ++ intercalate ", " ("address":"\"chainId\"":map (quoteIt . T.unpack . fst) list)
-            ++ ")"
-
-      vals <- forM metadata $ \row -> do
-            let rowList = Map.toList $ Map.map valueToSolidityValue $ Map.filter isFunction $ contractData row
-            let rowSt =
-                  "("
-                  ++ intercalate ", " (
-                             [singleQuoteIt (address row), singleQuoteIt (chain row)]
-                             ++ map (valueToString . snd) rowList
-                            )
-                  ++ ")"
-            return rowSt
-
-      let inserts = L.intercalate ", " vals
-
-          upsertList =
-              ["address = excluded.address", "\"chainId\" = excluded.\"chainId\""]
-              ++ map (upsertCriteria . fst) list
-
-
-      let ins = "insert into \"" ++ (contractName $ head metadata) ++ "\" " ++ keySt ++ " values " ++ inserts ++ " on conflict (address, \"chainId\") do update set " ++ intercalate ", " upsertList ++ ";"
-
-      dbInsert ins conn
-  else do
-    let row = head metadata
-    let list = Map.toList $ Map.map valueToSolidityValue $ Map.filter isFunction $ contractData $ head metadata
-
-    addNewContract conn cache metadata list
-
-    let keySt =
-            "("
-            ++ intercalate ", " ("address":"\"chainId\"":map (quoteIt . T.unpack . fst) list)
-            ++ ")"
-
-    let vals =
-         "("
-         ++ intercalate ", " (
-                 [singleQuoteIt (address row), singleQuoteIt (chain row)]
-                 ++ map (valueToString . snd) list
-               )
-         ++ ")"
-
-        upsertList =
-           ["address = excluded.address", "\"chainId\" = excluded.\"chainId\""]
-           ++ map (upsertCriteria . fst) list
-
-    let ins = "insert into \"" ++ contractName row ++ "\" " ++ keySt ++ " values " ++ vals ++ " on conflict (address, \"chainId\") do update set " ++ intercalate ", " upsertList ++ ";"
-    dbInsert ins conn
-  return ()
-
-
-
-
-
-
--------------------------------
-
-addNewContract :: PGConnection
-                  -> IORef (Map.Map String ContractAndXabi)
-                  -> [ProcessedContract]
-                  -> [(T.Text, SolidityValue)]
-                  -> IO ()
-addNewContract conn cache metadata list = do
-    contractCache <- readIORef cache
-    let firstContract = head metadata
-    let hashVal = codehash firstContract
-    cachedContract <- case Map.lookup hashVal contractCache of
-      Just x -> return x
-      Nothing -> return ContractAndXabi{contract = Left "error", xabi = "error", name = "error", contractStored = False}
-
-    when (not $ contractStored cachedContract) $ do
-          let conVals = "('" ++ hashVal ++ "', '" ++ contractName (head metadata) ++ "', '" ++ abi (head metadata) ++ "', '" ++ chain (head metadata) ++ "')"
+      when (not $ contractStored cachedContract) $ do
+          let conVals = "('" ++ (codehash $ head metadata) ++ "', '" ++ (contractName $ head metadata) ++ "', '" ++ (abi $ head metadata) ++ "', '" ++ (chain $ head metadata) ++ "')"
           let conIns = "insert into contract (\"codeHash\", contract, abi, \"chainId\") values " ++ conVals ++ " ON CONFLICT DO NOTHING;"
           let newState _ = ContractAndXabi{contract = contract cachedContract, xabi = xabi cachedContract, name = name cachedContract, contractStored = True}
           _ <- writeIORef cache (Map.adjust newState hashVal contractCache)
           dbInsert conIns conn
-                                                                                                 
-    let createSt =
-         "create table if not exists \"" ++ (contractName $ head metadata)
-         ++ "\" ("
-         ++ intercalate ", "
-                (
-                  ["address text", "\"chainId\" text"]
-                  ++ map tableColumn list
-                  ++ ["CONSTRAINT \""
-                      ++ contractName (head metadata)
-                      ++"_pkey\" PRIMARY KEY (address, \"chainId\")"]
-                )
-         ++ " );"
 
+      let fstContract = contractData $ head metadata
+      let list = Map.toList $ Map.map valueToSolidityValue $ Map.filter isFunction $ fstContract
+      let comma = if (length list == 0)
+          then ""
+          else ", "
+      let createSt = "create table if not exists \"" ++ (contractName $ head metadata) ++ "\" (address text, \"chainId\" text" ++ comma ++ tableColumns list ++ ", CONSTRAINT \"" ++ (contractName $ head metadata) ++ "_pkey\" PRIMARY KEY (address, \"chainId\") );"
+      dbInsert createSt conn
+
+      let keySt = "(" ++ "address, \"chainId\"" ++ comma ++ listToKeyStatement ", " list ++ ")"
+
+      vals <- forM metadata $ \row -> do
+            let rowList = Map.toList $ Map.map valueToSolidityValue $ Map.filter isFunction $ contractData row
+            let rowSt = "(" ++ "'" ++ address row ++ "', '" ++ chain row ++ "'" ++ comma ++ listToValueStatement ", " rowList ++ ")"
+            return rowSt
+
+      let inserts = L.intercalate ", " vals
+      let ins = "insert into \"" ++ (contractName $ head metadata) ++ "\" " ++ keySt ++ " values " ++ inserts ++ " on conflict (address, \"chainId\") do update set address = excluded.address, \"chainId\" = excluded.\"chainId\"" ++ comma ++ (tableUpsert list) ++ ";"
+
+      dbInsert ins conn
+  else do
+    let row = head metadata
+
+    if(contractStored cachedContract)
+      then return ()
+    else do
+          let conVals = "('" ++ codehash row ++ "', '" ++ contractName row ++ "', '" ++ abi row ++ "', '" ++ chain row ++ "')"
+          let conIns = "insert into contract (\"codeHash\", contract, abi, \"chainId\") values " ++ conVals ++ " ON CONFLICT DO NOTHING;"
+          let newState _ = ContractAndXabi{contract = contract cachedContract, xabi = xabi cachedContract, name = name cachedContract, contractStored = True}
+          _ <- writeIORef cache (Map.adjust newState hashVal contractCache)
+          dbInsert conIns conn
+    let list = Map.toList $ Map.map valueToSolidityValue $ Map.filter isFunction $ contractData row
+    let comma = if (length list == 0)
+        then ""
+        else ", "
+    let createSt = "create table if not exists \"" ++ contractName row ++ "\" (address text, \"chainId\" text" ++ comma ++ tableColumns list ++ ", CONSTRAINT \"" ++ contractName row ++"_pkey\" PRIMARY KEY (address, \"chainId\") );"
     dbInsert createSt conn
+
+    let keySt = "(" ++ "address, \"chainId\"" ++ comma ++ listToKeyStatement ", " list ++ ")"
+    let vals = "(" ++ "'" ++ address row ++ "', '" ++ chain row ++ "'" ++ comma  ++ listToValueStatement ", " list ++ ")"
+    let ins = "insert into \"" ++ contractName row ++ "\" " ++ keySt ++ " values " ++ vals ++ " on conflict (address, \"chainId\") do update set address = excluded.address, \"chainId\" = excluded.\"chainId\"" ++ comma ++ (tableUpsert list) ++ ";"
+    dbInsert ins conn
+  return ()
