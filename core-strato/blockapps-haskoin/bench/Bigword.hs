9365a743d7d0f8ae4c6e948b849bd422a063a287
Tim Holland <tim@blockapps.net>
2018-09-05 14:03:30 -0400

    Benchmark DoubleWord

--- core-strato/blockapps-haskoin/bench/Bigword.hs
+++ core-strato/blockapps-haskoin/bench/Bigword.hs
@@ -5,9 +5,13 @@ import Numeric
 import Control.DeepSeq
 import Criterion.Main
 import qualified Data.LargeWord as LW
+import qualified Data.DoubleWord as DW
 
 instance (NFData a, NFData b) => NFData (LW.LargeKey a b) where
   rnf (LW.LargeKey a b) = rnf (a, b)
+
+instance NFData DW.Word256 where
+  rnf (DW.Word256 a b) = a `seq` b `seq` ()
 
 
 benchSumInt :: Int -> Benchmark
@@ -52,8 +56,23 @@ benchShowHexWord256 n = bench ("Rendering hex Word256; size=" ++ show n)
 
 benchShowHexLargeWord :: Int -> Benchmark
 benchShowHexLargeWord n = bench ("Rendering hex LargeWord; size=" ++ show n)
-                        . nf (map $ flip showHex "")
+                        . nf (map $ flip showHex "" . toInteger)
                         $ (map fromIntegral [0..n] :: [LW.Word256])
+
+benchSumDouble :: Int -> Benchmark
+benchSumDouble n = bench ("Summing DoubleWord256; size=" ++ show n)
+                    . nf sum
+                    $ (map fromIntegral [1..n] :: [DW.Word256])
+
+benchProdDouble :: Int -> Benchmark
+benchProdDouble n = bench ("Multiplying DoubleWord256; size=" ++ show n)
+                     . nf product
+                     $ (map fromIntegral [1..n] :: [DW.Word256])
+
+benchShowHexDouble :: Int -> Benchmark
+benchShowHexDouble n = bench ("Rendering hex DoubleWord256; size=" ++ show n)
+                  . nf (map $ flip showHex "")
+                  $ (map fromIntegral [1..n] :: [DW.Word256])
 
 main :: IO ()
 main = let sizes =  [1, 10, 100, 4096, 65536, 1048576]
@@ -61,10 +80,13 @@ main = let sizes =  [1, 10, 100, 4096, 65536, 1048576]
        in defaultMain $ []
                      ++ map benchSumInt (drop 3 sizes)
                      ++ map benchSumWord256 (drop 3 sizes)
-                     ++ map benchSumLargeWord (take 3 $ drop 2 sizes))
+                     ++ map benchSumDouble (drop 3 sizes)
+                     ++ map benchSumLargeWord (take 3 $ drop 2 sizes)
                      ++ map benchProdInt msizes
                      ++ map benchProdWord256 msizes
+                     ++ map benchProdDouble msizes
                      ++ map benchProdLargeWord (take 1 msizes)
                      ++ map benchShowHexInt sizes
                      ++ map benchShowHexWord256 sizes
+                     ++ map benchShowHexDouble sizes
                      ++ map benchShowHexLargeWord (take 3 sizes)
