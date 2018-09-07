2ae764691e0f504cef700a6117656980e1238ca0
dustinnorwood <dnorwood2010@aol.com>
2018-09-07 11:52:25 -0400

    Make `address` optional in Bloc `/transaction`

--- blockapps-haskell/bloc/bloc22/server/src/BlockApps/Bloc22/Server/Transaction.hs
+++ blockapps-haskell/bloc/bloc22/server/src/BlockApps/Bloc22/Server/Transaction.hs
@@ -29,76 +29,80 @@ import           BlockApps.VaultWrapper.Client
 import           BlockApps.VaultWrapper.Types
 
 postBlocTransaction :: Maybe Text -> Maybe Text -> Maybe ChainId -> Bool -> PostBlocTransactionRequest -> Bloc [BlocTransactionResult]
-postBlocTransaction mUserName mUserId chainId resolve (PostBlocTransactionRequest addr txs' txParams) = do
+postBlocTransaction mUserName mUserId chainId resolve (PostBlocTransactionRequest mAddr txs' txParams) = do
   case (mUserName, mUserId) of
     (Nothing, _) -> error "Did not find X-USER-UNIQUE-NAME in the header"
     (Just _, Nothing) -> error "Did not find X-USER-ID in the header"
-    (Just userName, Just userId) -> fmap join . forM (partitionWith transactionType txs') $ \(ttype, txs) -> case ttype of
-      TRANSFER -> case txs of
-        [] -> return []
-        [x] -> do
-          p <- fromTransfer x
-          let btp = TransferParameters
-                      addr
-                      (transferpayloadToAddress p)
-                      (transferpayloadValue p)
-                      txParams
-                      chainId
-                      resolve
-          fmap (:[]) $ postUsersSend' btp (callSignature userName userId)
-        xs -> do
-          p <- mapM fromTransfer xs
-          let btlp = TransferListParameters
-                      addr
-                      (map (\(TransferPayload t v) -> SendTransaction t v txParams) p)
-                      chainId
-                      resolve
-          postUsersSendList' btlp (callSignature userName userId)
-      CONTRACT -> case txs of
-        [] -> return []
-        [x] -> do
-          p <- fromContract x
-          let bcp = ContractParameters
-                      addr
-                      (contractpayloadSrc p)
-                      (contractpayloadContract p)
-                      (contractpayloadArgs p)
-                      (contractpayloadValue p)
-                      txParams
-                      chainId
-                      resolve
-          fmap (:[]) $ postUsersContract' bcp (callSignature userName userId)
-        xs -> do
-          p <- mapM fromContract xs
-          let bclp = ContractListParameters
-                      addr
-                      (map (\(ContractPayload _ c a v) -> UploadListContract (fromJust c) (fromMaybe Map.empty a) txParams v) p)
-                      chainId
-                      resolve
-          postUsersUploadList' bclp (callSignature userName userId)
-      FUNCTION -> case txs of
-        [] -> return []
-        [x] -> do
-          p <- fromFunction x
-          let bfp = FunctionParameters
-                      addr
-                      ((\(ContractName c) -> c) $ functionpayloadContractName p)
-                      (functionpayloadContractAddress p)
-                      (functionpayloadMethod p)
-                      (functionpayloadArgs p)
-                      (functionpayloadValue p)
-                      txParams
-                      chainId
-                      resolve
-          fmap (:[]) $ postUsersContractMethod' bfp (callSignature userName userId)
-        xs -> do
-          p <- mapM fromFunction xs
-          let bflp = FunctionListParameters
-                      addr
-                      (map (\(FunctionPayload (ContractName n) a m r v) -> MethodCall n a m r (fromMaybe (Strung 0) v) txParams) p)
-                      chainId
-                      resolve
-          postUsersContractMethodList' bflp (callSignature userName userId)
+    (Just userName, Just userId) -> do
+      addr <- case mAddr of
+        Nothing -> fmap unStatusAndAddress . blocVaultWrapper $ getKey mUserName mUserId
+        Just addr' -> return addr'
+      fmap join . forM (partitionWith transactionType txs') $ \(ttype, txs) -> case ttype of
+        TRANSFER -> case txs of
+          [] -> return []
+          [x] -> do
+            p <- fromTransfer x
+            let btp = TransferParameters
+                        addr
+                        (transferpayloadToAddress p)
+                        (transferpayloadValue p)
+                        txParams
+                        chainId
+                        resolve
+            fmap (:[]) $ postUsersSend' btp (callSignature userName userId)
+          xs -> do
+            p <- mapM fromTransfer xs
+            let btlp = TransferListParameters
+                        addr
+                        (map (\(TransferPayload t v) -> SendTransaction t v txParams) p)
+                        chainId
+                        resolve
+            postUsersSendList' btlp (callSignature userName userId)
+        CONTRACT -> case txs of
+          [] -> return []
+          [x] -> do
+            p <- fromContract x
+            let bcp = ContractParameters
+                        addr
+                        (contractpayloadSrc p)
+                        (contractpayloadContract p)
+                        (contractpayloadArgs p)
+                        (contractpayloadValue p)
+                        txParams
+                        chainId
+                        resolve
+            fmap (:[]) $ postUsersContract' bcp (callSignature userName userId)
+          xs -> do
+            p <- mapM fromContract xs
+            let bclp = ContractListParameters
+                        addr
+                        (map (\(ContractPayload _ c a v) -> UploadListContract (fromJust c) (fromMaybe Map.empty a) txParams v) p)
+                        chainId
+                        resolve
+            postUsersUploadList' bclp (callSignature userName userId)
+        FUNCTION -> case txs of
+          [] -> return []
+          [x] -> do
+            p <- fromFunction x
+            let bfp = FunctionParameters
+                        addr
+                        ((\(ContractName c) -> c) $ functionpayloadContractName p)
+                        (functionpayloadContractAddress p)
+                        (functionpayloadMethod p)
+                        (functionpayloadArgs p)
+                        (functionpayloadValue p)
+                        txParams
+                        chainId
+                        resolve
+            fmap (:[]) $ postUsersContractMethod' bfp (callSignature userName userId)
+          xs -> do
+            p <- mapM fromFunction xs
+            let bflp = FunctionListParameters
+                        addr
+                        (map (\(FunctionPayload (ContractName n) a m r v) -> MethodCall n a m r (fromMaybe (Strung 0) v) txParams) p)
+                        chainId
+                        resolve
+            postUsersContractMethodList' bflp (callSignature userName userId)
   where fromTransfer = \case
           BlocTransfer t -> return t
           _ -> throwError $ UserError "Could not decode transfer arguments from body"
