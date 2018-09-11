2df00e20a83efd5c22533ca830c1e1d62ca5d714
Yunfan Jiang <yunfan@blockapps.net>
2018-09-11 12:24:40 -0400

    Fix Info name

--- core-strato/ethereum-vm/src/Blockchain/VMMetrics.hs
+++ core-strato/ethereum-vm/src/Blockchain/VMMetrics.hs
@@ -4,34 +4,34 @@ module Blockchain.VMMetrics where
 import           Prometheus                                as P
 
 vmBlocksProcessed :: P.Metric P.Counter
-vmBlocksProcessed = P.unsafeRegisterIO $ counter (P.Info "ctr_vm_blocks_processed" "evm counter for blocks processed")
+vmBlocksProcessed = P.unsafeRegisterIO $ counter (P.Info "vm_blocks_processed" "evm counter for blocks processed")
 
 vmBlocksMined:: P.Metric P.Counter
-vmBlocksMined = P.unsafeRegisterIO $ counter (P.Info "ctr_vm_blocks_mined" "evm counter for blocks mined")
+vmBlocksMined = P.unsafeRegisterIO $ counter (P.Info "vm_blocks_mined" "evm counter for blocks mined")
 
 vmBlocksUnmined :: P.Metric P.Counter
-vmBlocksUnmined = P.unsafeRegisterIO $ counter (P.Info "ctr_vm_blocks_unmined" "evm counter for blocks unmined")
+vmBlocksUnmined = P.unsafeRegisterIO $ counter (P.Info "vm_blocks_unmined" "evm counter for blocks unmined")
 
 vmBlocksValid :: P.Metric P.Counter
-vmBlocksValid = P.unsafeRegisterIO $ counter (P.Info "ctr_vm_blocks_valid" "evm counter for blocks valid")
+vmBlocksValid = P.unsafeRegisterIO $ counter (P.Info "vm_blocks_valid" "evm counter for blocks valid")
 
 vmBlocksInvalid :: P.Metric P.Counter
-vmBlocksInvalid = P.unsafeRegisterIO $ counter (P.Info "ctr_vm_blocks_invalid" "evm counter for blocks invalid")
+vmBlocksInvalid = P.unsafeRegisterIO $ counter (P.Info "vm_blocks_invalid" "evm counter for blocks invalid")
 
 vmTxsSuccessful :: P.Metric P.Counter
-vmTxsSuccessful = P.unsafeRegisterIO $ counter (P.Info "ctr_vm_txs_successful" "evm counter for txs successful")
+vmTxsSuccessful = P.unsafeRegisterIO $ counter (P.Info "vm_txs_successful" "evm counter for txs successful")
 
 vmTxsUnsuccessful :: P.Metric P.Counter
-vmTxsUnsuccessful = P.unsafeRegisterIO $ counter (P.Info "ctr_vm_txs_unsuccessful" "evm counter for txs unsuccessful")
+vmTxsUnsuccessful = P.unsafeRegisterIO $ counter (P.Info "vm_txs_unsuccessful" "evm counter for txs unsuccessful")
 
 vmTxsProcessed :: P.Metric P.Counter
-vmTxsProcessed = P.unsafeRegisterIO $ counter (P.Info "ctr_vm_txs_processed" "evm counter for txs processed")
+vmTxsProcessed = P.unsafeRegisterIO $ counter (P.Info "vm_txs_processed" "evm counter for txs processed")
 
 vmTxsCall :: P.Metric P.Counter
-vmTxsCall = P.unsafeRegisterIO $ counter (P.Info "ctr_vm_txs_call" "evm counter for txs call")
+vmTxsCall = P.unsafeRegisterIO $ counter (P.Info "vm_txs_call" "evm counter for txs call")
 
 vmTxsCreation :: P.Metric P.Counter
-vmTxsCreation = P.unsafeRegisterIO $ counter (P.Info "ctr_vm_txs_creation" "evm counter for txs creation")
+vmTxsCreation = P.unsafeRegisterIO $ counter (P.Info "vm_txs_creation" "evm counter for txs creation")
 
 vmBlockInsertionMined :: P.Metric P.Gauge
 vmBlockInsertionMined = P.unsafeRegisterIO $ gauge (P.Info "vm_block_insertion_mined" "evm gauge for block insertion mined")
--- core-strato/strato-sequencer/src/Blockchain/Sequencer/Metrics.hs
+++ core-strato/strato-sequencer/src/Blockchain/Sequencer/Metrics.hs
@@ -4,37 +4,36 @@ module Blockchain.Sequencer.Metrics where
 import           Prometheus                                as P
 
 seqLdbBatchWrites:: P.Metric P.Counter
-seqLdbBatchWrites = P.unsafeRegisterIO $ counter (P.Info "ctr_sequencer_ldb_batch_writes" "Sequencer counter for ldb batch writes")
+seqLdbBatchWrites = P.unsafeRegisterIO $ counter (P.Info "seq_ldb_batch_writes" "Sequencer counter for ldb batch writes")
 
 seqLdbBatchSize :: P.Metric P.Gauge
-seqLdbBatchSize = P.unsafeRegisterIO $ gauge (P.Info "ctr_sequencer_ldb_batch_size" "Sequencer gauge for ldb batch size")
-
+seqLdbBatchSize = P.unsafeRegisterIO $ gauge (P.Info "seq_ldb_batch_size" "Sequencer gauge for ldb batch size")
 
 seqTxsWitnessed :: P.Metric P.Counter
-seqTxsWitnessed = P.unsafeRegisterIO $ counter (P.Info "ctr_sequencer_txs_witnessed" "Sequencer counter for transactions witnessed")
+seqTxsWitnessed = P.unsafeRegisterIO $ counter (P.Info "seq_txs_witnessed" "Sequencer counter for transactions witnessed")
 
 seqTxsUnwitnessed :: P.Metric P.Counter
-seqTxsUnwitnessed = P.unsafeRegisterIO $ counter (P.Info "ctr_sequencer_txs_unwitnessed" "Sequencer counter for transactions unwitnessed")
+seqTxsUnwitnessed = P.unsafeRegisterIO $ counter (P.Info "seq_txs_unwitnessed" "Sequencer counter for transactions unwitnessed")
 
 seqBlocksEcrfail :: P.Metric P.Counter
-seqBlocksEcrfail = P.unsafeRegisterIO $ counter (P.Info "ctr_sequencer_blocks_ecrfail" "Sequencer counter for blocks ecrfail")
+seqBlocksEcrfail = P.unsafeRegisterIO $ counter (P.Info "seq_blocks_ecrfail" "Sequencer counter for blocks ecrfail")
 
 seqBlocksEnqueued :: P.Metric P.Counter
-seqBlocksEnqueued = P.unsafeRegisterIO $ counter (P.Info "ctr_sequencer_blocks_enqueued" "Sequencer counter for blocks enqueued")
+seqBlocksEnqueued = P.unsafeRegisterIO $ counter (P.Info "seq_blocks_enqueued" "Sequencer counter for blocks enqueued")
 
 seqKafkaCheckpointWrites :: P.Metric P.Counter
-seqKafkaCheckpointWrites = P.unsafeRegisterIO $ counter (P.Info "ctr_sequencer_kafka_checkpoint_writes" "Sequencer counter for kafka checkpoint writes")
+seqKafkaCheckpointWrites = P.unsafeRegisterIO $ counter (P.Info "seq_kafka_checkpoint_writes" "Sequencer counter for kafka checkpoint writes")
 
 seqKafkaCheckpointReads :: P.Metric P.Counter
-seqKafkaCheckpointReads = P.unsafeRegisterIO $ counter (P.Info "ctr_sequencer_kafka_checkpoint_reads" "Sequencer counter for kafka checkpoint reads")
+seqKafkaCheckpointReads = P.unsafeRegisterIO $ counter (P.Info "seq_kafka_checkpoint_reads" "Sequencer counter for kafka checkpoint reads")
 
 seqKafkaSeqWrites :: P.Metric P.Counter
-seqKafkaSeqWrites = P.unsafeRegisterIO $ counter (P.Info "ctr_sequencer_kafka_sequenced_writes" "Sequencer counter for kafka sequenced_writes")
+seqKafkaSeqWrites = P.unsafeRegisterIO $ counter (P.Info "seq_kafka_sequenced_writes" "Sequencer counter for kafka sequenced_writes")
 
 seqKafkaUnseqRead :: P.Metric P.Counter
-seqKafkaUnseqRead = P.unsafeRegisterIO $ counter (P.Info "ctr_sequencer_kafka_unsequenced_reads" "Sequencer counter for kafka unsequenced reads")
+seqKafkaUnseqRead = P.unsafeRegisterIO $ counter (P.Info "seq_kafka_unsequenced_reads" "Sequencer counter for kafka unsequenced reads")
 
 seqBlocksReleased :: P.Metric P.Counter
-seqBlocksReleased = P.unsafeRegisterIO $ counter (P.Info "ctr_sequencer_blocks_released" "Sequencer counter for blocks released")
+seqBlocksReleased = P.unsafeRegisterIO $ counter (P.Info "seq_blocks_released" "Sequencer counter for blocks released")
 
 
