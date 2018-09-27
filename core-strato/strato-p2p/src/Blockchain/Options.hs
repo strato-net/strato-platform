2029001729e36469ae9833f6943e08a8c291e0e5
dustinnorwood <dnorwood2010@aol.com>
2018-09-27 11:19:22 -0400

    Make maxReturnedHeaders a flag

--- core-strato/strato-p2p/src/Blockchain/Options.hs
+++ core-strato/strato-p2p/src/Blockchain/Options.hs
@@ -14,3 +14,4 @@ defineFlag "syncBacktrackNumber" (10::Integer) "block number to go back when syn
 defineFlag "debugFail" True "Fail on errors we're not supposed to reach. If false, just log insteand and go on"
 defineFlag "maxConn" (20::Int) "Maximum number of client connections."
 defineFlag "connectionTimeout" (300 :: Int) "Number of seconds to tolerate a useless peer"
+defineFlag "maxReturnedHeaders" (1000 :: Int) "Number of headers to return from a GetBlockHeaders request" -- todo: seriously???
