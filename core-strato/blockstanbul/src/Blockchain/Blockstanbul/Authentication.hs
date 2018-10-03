c2f01dee8aa0c7981451fa6c4f00e9312d6656c9
Tim Holland <tim@blockapps.net>
2018-10-03 12:22:10 -0400

    Convert replayHistoricBlock to use a set

--- core-strato/blockstanbul/src/Blockchain/Blockstanbul/Authentication.hs
+++ core-strato/blockstanbul/src/Blockchain/Blockstanbul/Authentication.hs
@@ -142,7 +142,7 @@ authenticate (IMsg (MsgAuth addr sig) tm) =
   in mAddress == Just addr
 authenticate _ = True -- Non-messages are trusted implicitly
 
-replayHistoricBlock :: [Address] -> Word256 -> Block -> Either String Word256
+replayHistoricBlock :: S.Set Address  -> Word256 -> Block -> Either String Word256
 replayHistoricBlock realValidators seqNo blk = do
   -- TODO(tim): This needs to be fixed for validator voting, as the current list
   -- may have diverged from the validators at the time of commit
@@ -157,12 +157,12 @@ replayHistoricBlock realValidators seqNo blk = do
       blockNo = fromIntegral . blockDataNumber . blockBlockData $ blk
   unless (seqNo + 1 == blockNo) $
     Left "unexpected block number"
-  unless (S.fromList realValidators == S.fromList _validatorList) $
+  unless (realValidators == S.fromList _validatorList) $
     Left "mismatched validators"
-  unless (mProp `elem` map Just realValidators) $
+  unless (mProp `S.member` S.map Just realValidators) $
     Left "no verifiable proposer seal"
-  unless (signers `S.isSubsetOf` S.fromList realValidators) $
+  unless (signers `S.isSubsetOf` realValidators) $
     Left "unknown signers"
-  unless (3 * S.size signers > 2 * length realValidators) $
+  unless (3 * S.size signers > 2 * S.size realValidators) $
     Left "not enough commit seals"
   Right . fromIntegral $ seqNo + 1
