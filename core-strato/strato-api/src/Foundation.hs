8626dec132f7595b9411a2ccdb8229d8a24dcdf0
th0114nd <th0114nd@gmail.com>
2018-10-17 13:51:11 -0400

    Revert "Remove yesod-raml from strato-api, since upstream has been abandoned"

--- core-strato/strato-api/src/Foundation.hs
+++ core-strato/strato-api/src/Foundation.hs
@@ -16,6 +16,7 @@ import qualified Network.Wai              as W
 import qualified Prelude                  as P
 import           Yesod.Core.Types         (Logger)
 import qualified Yesod.Core.Unsafe        as Unsafe
+import           Yesod.Raml.Routes
 
 import           Blockchain.DB.SQLDB
 
@@ -54,7 +55,7 @@ instance HasHttpManager App where
 -- generates the rest of the code. Please see the linked documentation for an
 -- explanation for this split.
 
-mkYesodData "App" $(parseRoutesFile "config/routes.txt")
+mkYesodData "App" $(parseRamlRoutesFile "config/routes.raml")
 
 -- | A convenient synonym for creating forms.
 type Form x = Html -> MForm (HandlerT App IO) (FormResult x, Widget)
