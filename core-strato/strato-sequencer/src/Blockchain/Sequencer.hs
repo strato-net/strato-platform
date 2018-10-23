a6d3da3bd1f72ededec9cb9bdb77867e64005aca
Tim Holland <tim@blockapps.net>
2018-10-23 17:58:36 -0400

    Reinclude PushBlocks

--- core-strato/strato-sequencer/src/Blockchain/Sequencer.hs
+++ core-strato/strato-sequencer/src/Blockchain/Sequencer.hs
@@ -173,7 +173,8 @@ blockstanbulSend msgs = do
         creates = [OECreateBlockCommand | MakeBlockCommand <- resp]
         vmevs = creates ++ mapMaybe rewriteBlock blocks
         p2pevs = [OEBlockstanbul (WireMessage a m) | OMsg a m <- resp]
-              ++ [OEAskForBlocks (h+1) n | GapFound h n <- resp]
+              ++ [OEAskForBlocks (h+1) l | GapFound h l <- resp]
+              ++ [OEPushBlocks (l+1) h | LeadFound h l <- resp]
 
 
     unless (null blocks) $ do
