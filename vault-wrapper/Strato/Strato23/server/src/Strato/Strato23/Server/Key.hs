5438b4f47597cca0f2f5446c66ca738b383fdde8
dustinnorwood <dnorwood2010@aol.com>
2018-11-08 12:51:31 -0500

    Wire up query param usage

--- vault-wrapper/Strato/Strato23/server/src/Strato/Strato23/Server/Key.hs
+++ vault-wrapper/Strato/Strato23/server/src/Strato/Strato23/Server/Key.hs
@@ -4,6 +4,7 @@
 
 module Strato.Strato23.Server.Key where
 
+import           Data.Maybe                       (fromMaybe, isJust)
 import           Data.Text                        (Text)
 import           Strato.Strato23.API
 import           Strato.Strato23.Crypto
@@ -11,12 +12,15 @@ import           Strato.Strato23.Monad
 import           Strato.Strato23.Database.Queries
 
 getKey :: Text -> Text -> Maybe Text -> VaultM StatusAndAddress
-getKey userName userId _ = do
-  (salt, nonce, encKey, (_ :: Address)) <- toUserError ("User " <> userName <> " doesn't exist")
-                         . vaultQuery1 $ getUserKeyQuery userName
-  case decryptSecKey (textPassword userId) salt nonce encKey of
-    Nothing -> vaultWrapperError $ UserError "X-USER-ID does not match entry for X-USER-UNIQUE-NAME"
-    Just pKey -> return . StatusAndAddress $ deriveAddress pKey
+getKey headerUserName userId queryParamUserName = do
+  let userName = fromMaybe headerUserName queryParamUserName
+  (salt, nonce, encKey, addr) <- toUserError ("User " <> userName <> " doesn't exist")
+                               . vaultQuery1 $ getUserKeyQuery userName
+  if isJust queryParamUserName          -- decrypt and derive the address if query param
+    then return $ StatusAndAddress addr -- not specified, to guarantee correctness
+    else case decryptSecKey (textPassword userId) salt nonce encKey of
+      Nothing -> vaultWrapperError $ UserError "X-USER-ID does not match entry for X-USER-UNIQUE-NAME"
+      Just pKey -> return . StatusAndAddress $ deriveAddress pKey
 
 postKey :: Text -> Text -> VaultM StatusAndAddress
 postKey userName userId = do
