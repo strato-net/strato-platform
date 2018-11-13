c32f1f4117f0728ba809c8a41b82c37db3e46299
Yunfan Jiang <yunfan@blockapps.net>
2018-11-13 09:54:27 -0500

    Make the test pass

--- core-strato/blockstanbul/test/AuthenticationSpec.hs
+++ core-strato/blockstanbul/test/AuthenticationSpec.hs
@@ -167,4 +167,4 @@ spec = do
       cSeal <- commitmentSeal (blockHash blk') private
       let blk = addCommitmentSeals [cSeal] blk'
           got = replayHistoricBlock vals 39 blk
-      got `shouldBe` Right 40
+      got `shouldBe` Right (40, S.elemAt 0 vals)
