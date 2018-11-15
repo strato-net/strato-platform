2a8bba323903ac43abf580d74871c0768b1ea0ee
Tim Holland <tim@blockapps.net>
2018-11-15 14:46:33 -0500

    Package 0.5.0 as well

--- blockapps-haskell/pull_solc.sh
+++ blockapps-haskell/pull_solc.sh
@@ -2,18 +2,18 @@
 set -e
 
 if [ $# -ne 3 ]; then
-  echo "usage: ${0} <version> <target_dir> <source_output_dir>"
+  echo "usage: ${0} <version> <target> <source_output_dir>"
   exit 1
 fi
 
 VERSION=$1
-TARGET_DIR=$2
+TARGET=$2
 SOURCE_OUTPUT_DIR=$3
 
-CONTAINER=$(docker run -d --entrypoint=sleep "ethereum/solc:${VERSION}" 10000)
+CONTAINER=$(docker run -d "ethereum/solc:${VERSION}")
 trap "docker rm -f ${CONTAINER}" EXIT
 
-docker cp "${CONTAINER}:/usr/bin/solc" "${TARGET_DIR}"
+docker cp "${CONTAINER}:/usr/bin/solc" "${TARGET}"
 
 # Fetching ethereum/solidity source code of same version as the GPLv3 license requires that
 if [ ! -d ${SOURCE_OUTPUT_DIR} ]
