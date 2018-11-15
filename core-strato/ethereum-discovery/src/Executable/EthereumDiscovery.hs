40babe78a8d17a1372d63ef6580a0a78b0a7fbfb
Tim Holland <tim@blockapps.net>
2018-11-15 14:50:43 -0500

    Unlift ethereum-discovery

--- core-strato/ethereum-discovery/src/Executable/EthereumDiscovery.hs
+++ core-strato/ethereum-discovery/src/Executable/EthereumDiscovery.hs
@@ -5,7 +5,7 @@ module Executable.EthereumDiscovery (
   ethereumDiscovery
   ) where
 
-import           Control.Exception.Lifted
+import           UnliftIO.Exception
 import           Control.Monad.IO.Class
 import           Control.Monad.Logger
 import           Control.Monad.Trans.Resource
