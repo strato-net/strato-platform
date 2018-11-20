e1e6fcdfe1e81d63689afb7d1f1c6d3a27ddb5d5
Tim Holland <tim@blockapps.net>
2018-11-20 17:05:35 -0500

    Create a library for time expired locks on blockhashes

---
+++ core-strato/strato-p2p/src/Blockchain/HashLocks.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE RecordWildCards #-}
+module Blockchain.HashLocks
+  ( HasClock(..)
+  , HashLocks
+  , newHashLocks
+  , totalSize
+  , tryGrabLock
+  , prunePast
+  ) where
+
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Reader
+import Data.Time.Clock
+import Data.Time.Clock.POSIX
+import Data.Hashable
+import Data.HashMap.Strict as M
+import UnliftIO.STM
+
+-- A module to limit the number of times a blockhash is requested
+-- at once. The only method for clearing a lock on a hash is for
+-- time to pass: a response may never be returned for a misbehaving
+-- peer, and even if a response is returned its expensive to hash
+-- all the blocks returned.
+
+class HasClock m where
+  getNow :: m POSIXTime
+
+instance HasClock IO where
+  getNow = getPOSIXTime
+
+instance (Monad m) => HasClock (ReaderT POSIXTime m) where
+  getNow = ask
+
+data HashLocks a = HashLocks
+  { timeout :: NominalDiffTime
+  , deadlines :: TVar (M.HashMap a POSIXTime)
+  }
+
+newHashLocks :: (Monad m, MonadIO m, Eq a) => NominalDiffTime -> m (HashLocks a)
+newHashLocks dt = HashLocks dt <$> atomically (newTVar M.empty)
+
+-- TotalSize does not take into account expiration of entries
+totalSize :: (MonadIO m) => HashLocks a -> m Int
+totalSize = fmap M.size . atomically . readTVar . deadlines
+
+-- Returns true if we were able to replace the time for this key
+tryGrabLock :: (Eq a, Hashable a, MonadIO m, HasClock m) => HashLocks a -> a -> m Bool
+tryGrabLock HashLocks{..} k = do
+  now <- getNow
+  -- In the common case of an existing key, readTVarIO is much faster because
+  -- it doesn't require a transaction.
+  staleMap <- readTVarIO deadlines
+  case M.lookup k staleMap of
+    Just t | t > now -> return False
+    _ -> atomically $ do
+      kvs <- readTVar deadlines
+      case M.lookup k kvs of
+        Just t | t > now -> return False
+        _ -> do
+          writeTVar deadlines $ M.insert k (now + timeout) kvs
+          return True
+
+-- Garbage collect entries that have expired
+-- TODO(tim): This would be a good application for Control.Reaper
+prunePast :: (MonadIO m, HasClock m) => HashLocks a -> m ()
+prunePast HashLocks{..} = do
+  now <- getNow
+  atomically . modifyTVar' deadlines $ M.filter (>now)
---
+++ core-strato/strato-p2p/unittest/Main.hs
@@ -0,0 +1,64 @@
+import Control.Monad.Trans.Reader
+import Data.Time.Clock.POSIX
+import Test.Hspec (hspec, Spec, describe, it)
+import Test.Hspec.Expectations.Lifted
+
+import Blockchain.HashLocks
+import UnliftIO.Async
+
+main :: IO ()
+main = hspec spec
+
+runTestClock :: (HashLocks Int -> ReaderT POSIXTime IO ()) -> IO ()
+runTestClock f = do
+  lock <- newHashLocks 10 :: IO (HashLocks Int)
+  runReaderT (f lock) 0x80000
+
+spec :: Spec
+spec = describe "HashLocks" $ do
+  it "should only allow locks on the first attempt" . runTestClock $ \h -> do
+    tryGrabLock h 8888 `shouldReturn` True
+    tryGrabLock h 6777 `shouldReturn` True
+    tryGrabLock h 8888 `shouldReturn` False
+
+  it "can reclaim expired entries" . runTestClock $ \h -> do
+    tryGrabLock h 420 `shouldReturn` True
+    local (+9.9) $ tryGrabLock h 420 `shouldReturn` False
+    local (+10) $ tryGrabLock h 420 `shouldReturn` True
+    local (+20) $ tryGrabLock h 420 `shouldReturn` True
+    local (+22) $ tryGrabLock h 420 `shouldReturn` False
+
+  it "should not prune entries if before the deadline" . runTestClock $  \h -> do
+    totalSize h `shouldReturn` 0
+    tryGrabLock h 0 `shouldReturn` True
+    tryGrabLock h 1 `shouldReturn` True
+    tryGrabLock h 2 `shouldReturn` True
+    totalSize h `shouldReturn` 3
+    prunePast h
+    totalSize h `shouldReturn` 3
+    local (+ 8) $ do
+      prunePast h
+      totalSize h `shouldReturn` 3
+
+  it "should prune entries in the past" . runTestClock $ \h -> do
+    mapM_ (tryGrabLock h) [1..40]
+    totalSize h `shouldReturn` 40
+    local (+20) $ do
+      prunePast h
+      totalSize h `shouldReturn` 0
+
+  it "should only prune expired entries" . runTestClock $ \h -> do
+    mapM_ (tryGrabLock h) [100..117]
+    totalSize h `shouldReturn` 18
+    local (+5) $ do
+      mapM_ (tryGrabLock h) [131..140]
+      totalSize h `shouldReturn` 28
+    local (+10) $ do
+      prunePast h
+      totalSize h `shouldReturn` 10
+
+  it "should be threadsafe" . runTestClock $ \h -> do
+    ts <- mapConcurrently (tryGrabLock h) [10000..14999]
+    length (filter id ts) `shouldBe` length ts
+    ts' <- mapConcurrently (tryGrabLock h) [10000..14999]
+    length (filter not ts') `shouldBe` length ts'
