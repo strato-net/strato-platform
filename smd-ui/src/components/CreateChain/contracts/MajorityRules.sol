05d88fb3dd4e9e820d5526f5999362df4b1850b4
dustinnorwood <dnorwood2010@aol.com>
2018-11-28 14:54:39 -0500

    Added MajorityRules.sol

---
+++ smd-ui/src/components/CreateChain/contracts/MajorityRules.sol
@@ -0,0 +1,31 @@
+contract MajorityRules {
+  event MemberAdded (address member, string enode);
+  event MemberRemoved (address member);
+
+  mapping (address => uint) addVotes;
+  mapping (address => uint) removeVotes;
+
+  address[] __members__;
+
+  function voteToAdd(address m, string e) {
+    uint votes = addVotes[m] + 1;
+    if (votes > __members__.length / 2) {
+      emit MemberAdded(m,e);
+      addVotes[m] = 0;
+    }
+    else {
+      addVotes[m] = votes;
+    }
+  }
+
+  function voteToRemove(address m) {
+    uint votes = removeVotes[m] + 1;
+    if (votes > __members__.length / 2) {
+      emit MemberRemoved(m);
+      removeVotes[m] = 0;
+    }
+    else {
+      removeVotes[m] = votes;
+    }
+  }
+}
