73d50e59b4154a8a07d1c3a660105c0f87d03087
dustinnorwood <dnorwood2010@aol.com>
2018-11-28 14:50:28 -0500

    Added AutoApprove.sol

---
+++ smd-ui/src/components/CreateChain/contracts/AutoApprove.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.24;
+
+contract AutoApprove {
+  event MemberAdded (address member, string enode);
+  event MemberRemoved (address member);
+
+  function voteToAdd(address m, string e) {
+    emit MemberAdded(m,e);
+  }
+
+  function voteToRemove(address m) {
+    emit MemberRemoved(m);
+  }
+}
