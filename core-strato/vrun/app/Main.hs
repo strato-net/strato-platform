fcf481806b5bb0edefaab326183c105922a3e9cc
Jamshid <jamshidnh@gmail.com>
2018-12-06 16:50:43 -0500

    Adding a new tool to run VM code.

---
+++ core-strato/vrun/app/Main.hs
@@ -0,0 +1,103 @@
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
+
+module Main where
+
+import qualified Data.ByteString                             as B
+import           Data.Maybe
+import           Data.Time.Clock.POSIX
+import           Control.Monad.IO.Class
+import           Control.Monad.Logger
+import           Control.Monad.Trans.Except
+import           HFlags
+import           Network.Haskoin.Crypto                      (withSource)
+import qualified Network.Haskoin.Internals                   as Haskoin
+
+import           Blockchain.BlockChain
+import           Blockchain.Data.AddressStateDB
+import           Blockchain.Data.BlockDB
+import           Blockchain.Data.Code
+import           Blockchain.Data.Transaction
+import qualified Blockchain.Data.TXOrigin                    as TO
+import qualified Blockchain.Database.MerklePatricia      as MP
+import           Blockchain.DB.MemAddressStateDB
+import           Blockchain.Sequencer.Event
+import           Blockchain.SHA
+import           Blockchain.Strato.Model.Address
+import           Blockchain.VMContext
+import           Blockchain.VMOptions       ()
+import           Executable.EVMFlags        ()
+
+
+main :: IO ()
+main = do
+  _ <- $initHFlags "The Ethereum Test program"
+
+  let secretKey = fromJust . Haskoin.makePrvKey $ 0x1234
+      t = createContractCreationTX 
+            0 --nonce
+            1 --gas price
+            1000000000000000000 --gas limit
+            1 --value
+--            (Code $ B.replicate 10000000 0x5b)
+            (Code $ B.replicate 1000000 0x5b)
+--            (Code $ B.replicate 1 0x5b)
+            Nothing
+            secretKey
+            
+  signedTransaction' <- liftIO $ withSource Haskoin.devURandom t
+
+  let blockData = BlockData {
+        blockDataParentHash = SHA 0xabcd,
+        blockDataNumber = 1,
+        blockDataCoinbase = Address 0xabcd,
+        blockDataDifficulty = 1,
+        blockDataUnclesHash = SHA 0xabcd,
+        blockDataStateRoot = MP.blankStateRoot,
+        blockDataTransactionsRoot = MP.blankStateRoot,
+        blockDataReceiptsRoot = MP.blankStateRoot,
+        blockDataLogBloom = "",
+        blockDataGasLimit = 100000000000000,
+        blockDataGasUsed = 1,
+        blockDataTimestamp = posixSecondsToUTCTime 0,
+        --timestamp = posixSecondsToUTCTime . fromInteger . read . currentTimestamp . env $ test,
+        blockDataExtraData = "",
+        blockDataNonce = 0,
+        blockDataMixHash=SHA 0
+        }
+        
+  let signedTransaction = txToOutputTx signedTransaction'
+
+  
+  (result, _) <- flip runLoggingT noLog $ runTestContextM $ do
+
+    let addr = Address 0xcf03dd0a894ef79cb5b601a43c4b25e3ae4c67ed
+    putAddressState addr AddressState{
+      addressStateNonce=0,
+        addressStateBalance=10000000000000000000000000000000000000000,
+        addressStateContractRoot=MP.blankStateRoot,
+        addressStateCodeHash=SHA 0,
+        addressStateChainId=Nothing
+      }
+
+    runExceptT $ addTransaction True blockData 10000000000000000000000000000 signedTransaction
+
+  case result of
+    Left e -> putStrLn $ show e
+    Right r -> putStrLn $ "vrun: " ++ show r
+
+
+
+
+noLog :: Loc -> LogSource -> LogLevel -> LogStr -> IO ()
+--noLog _ _ _ _ = error "piggy" -- putStrLn $ show s
+noLog _ _ _ s = putStrLn $ show s
+
+--noLog _ _ _ _ = return ()
+
+txToOutputTx :: Transaction -> OutputTx
+txToOutputTx = fromJust . wrapTransaction . IngestTx TO.Direct
+
+
+
