b6bda3be4caf4dcc7f43a3eaa858c53c5c157186
Tim Holland <tim@blockapps.net>
2019-02-08 15:54:08 -0500

    Render a JSON logs index; render proxied links

--- core-strato/doit.sh
+++ core-strato/doit.sh
@@ -19,7 +19,7 @@ function newnode {
   fi
 
   echo "Starting Strato processes. All output is logged to $PWD/logs."
-  logserver -d "${PWD}/logs" &> logs/logserver &
+  logserver --directory "${PWD}/logs" --uri_root=/strato/logs/ &> logs/logserver &
 
   if $mineBlocks
   then echo "Starting strato-adit"
