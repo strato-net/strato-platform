fc51352c3a44ff370486b463c6f9d18a5989073c
Tim Holland <tim@blockapps.net>
2019-02-12 12:03:18 -0500

    Test StorageDB

--- core-strato/blockapps-data/test/StorageSpec.hs
+++ core-strato/blockapps-data/test/StorageSpec.hs
@@ -1,60 +1,149 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 module StorageSpec (storageSpec) where
 
-import Test.Hspec
+import Control.DeepSeq
+import Control.Lens
+import Control.Monad
+import Control.Monad.Trans.State
+import Control.Monad.Trans.Resource
+import qualified Data.Map as M
+import qualified Blockchain.Database.MerklePatricia as MP
+import qualified Database.LevelDB as DB
+import qualified Database.LevelDB.Base as DBB
+import GHC.Generics
+import Prelude hiding (abs)
+import System.Posix.Temp
+import System.Directory
+import Test.Hspec (Spec, describe, it)
+import Test.Hspec.Expectations.Lifted
+import UnliftIO.Exception
+
+import Blockchain.Constants
+import Blockchain.Data.Address
+import Blockchain.Data.AddressStateDB
+import Blockchain.DB.HashDB
+import Blockchain.DB.MemAddressStateDB
+import Blockchain.DB.StateDB
+import Blockchain.DB.StorageDB
+import Blockchain.ExtWord
+import Blockchain.Strato.Model.SHA
+import qualified Data.NibbleString as N
+
+type SMap = M.Map (Address, Word256) Word256
+type AMap = M.Map Address AddressStateModification
+
+data CachedStorage = CS
+  { _sdb :: DB.DB
+  , _sdbsr :: MP.StateRoot
+  , _hdb :: HashDB
+  , _stx :: SMap
+  , _sbs :: SMap
+  , _atx :: AMap
+  , _abs :: AMap
+  } deriving (Generic, NFData)
+makeLenses ''CachedStorage
+
+type StorM = StateT CachedStorage (ResourceT IO)
+
+instance HasStorageDB StorM where
+  getStorageTxDB = liftM2 (,) (use sdb) (use stx)
+  putStorageTxMap = assign stx
+  getStorageBlockDB = liftM2 (,) (use sdb) (use sbs)
+  putStorageBlockMap = assign sbs
+
+instance HasMemAddressStateDB StorM where
+  getAddressStateTxDBMap = use atx
+  putAddressStateTxDBMap = assign atx
+  getAddressStateBlockDBMap = use abs
+  putAddressStateBlockDBMap = assign abs
+
+instance HasStateDB StorM where
+  getStateDB = liftM2 MP.MPDB (use sdb) (use sdbsr)
+  setStateDBStateRoot = assign sdbsr
+
+instance HasHashDB StorM where
+  getHashDB = use hdb
+
+initialEnv :: IO (FilePath, CachedStorage)
+initialEnv = do
+  tmpdir <- mkdtemp "/tmp/storage_spec"
+  let ldbOptions = DB.defaultOptions { DB.createIfMissing = True }
+      openDB b = DBB.open (tmpdir ++ b) ldbOptions
+  s <- openDB stateDBPath
+  h <- openDB hashDBPath
+  let st = CS s MP.emptyTriePtr h M.empty M.empty M.empty M.empty
+  fmap (tmpdir,) . runResourceT . flip execStateT st $ do
+    MP.initializeBlank =<< getStateDB
+
+runStorM :: StorM a -> IO a
+runStorM mv = bracket initialEnv
+                       (removePathForcibly . fst)
+                       (runResourceT . evalStateT mv . snd)
 
 storageSpec :: Spec
 storageSpec = do
   describe "StorageDB" $ do
-    it "can run a test" $ do
-      "ok" `shouldBe` "ok"
+    it "gets its puts" . runStorM $ do
+      getStorageKeyVal' 0x776 0x999 `shouldReturn` 0x0
+      putStorageKeyVal' 0x776 0x999 0x1234567890ab
+      getStorageKeyVal' 0x776 0x999 `shouldReturn` 0x1234567890ab
 
-    -- type SMap = M.Map (Address, Word256) Word256
--- type AMap = M.Map Address AddressStateModification
+    it "gets its puts after a partial flush" . runStorM $ do
+      putStorageKeyVal' 0x1 0x2 0x3
+      flushStorageTxDBToBlockDB
+      use stx `shouldReturn` M.empty
+      getStorageKeyVal' 0x1 0x2 `shouldReturn` 0x3
+      putStorageKeyVal' 0x1 0x2 0x77777
+      getStorageKeyVal' 0x1 0x2 `shouldReturn` 0x77777
 
--- data CachedStorage = CS
-  -- { _sdb :: DB.DB
-  -- , _sdbsr :: MP.StateRoot
-  -- , _hdb :: HashDB
-  -- , _stx :: SMap
-  -- , _sbs :: SMap
-  -- , _atx :: AMap
-  -- , _abs :: AMap
-  -- } deriving (Generic, NFData)
--- makeLenses ''CachedStorage
+    it "gets its puts after a full flush" . runStorM $ do
+      putStorageKeyVal' 0x1 0x2 0x3
+      flushMemStorageDB
+      use stx `shouldReturn` M.empty
+      use sbs `shouldReturn` M.empty
+      getStorageKeyVal' 0x1 0x2 `shouldReturn` 0x3
 
--- type StorM = StateT CachedStorage (ResourceT IO)
+    it "getAll returns nothing before a flush" . runStorM $ do
+      putStorageKeyVal' 0x1 0x2 0x3
+      putStorageKeyVal' 0x1 0x3 0x4
+      putStorageKeyVal' 0x1 0x4 0x5
+      putStorageKeyVal' 0x1 0x3 0x6
+      getAllStorageKeyVals' 0x1 `shouldReturn` []
 
--- instance HasStorageDB StorM where
-  -- getStorageTxDB = liftM2 (,) (use sdb) (use stx)
-  -- putStorageTxMap = assign stx
-  -- getStorageBlockDB = liftM2 (,) (use sdb) (use sbs)
-  -- putStorageBlockMap = assign sbs
+    it "getAll puts after a flush" . runStorM $ do
+      putStorageKeyVal' 0x1 0x2 0x3
+      putStorageKeyVal' 0x1 0x3 0x4
+      putStorageKeyVal' 0x1 0x4 0x5
+      putStorageKeyVal' 0x1 0x3 0x6
+      flushMemStorageDB
+      use stx `shouldReturn` M.empty
+      use sbs `shouldReturn` M.empty
+      let toKey = N.EvenNibbleString . keccak256 . word256ToBytes
+      kvs <- getAllStorageKeyVals' 0x1
+      kvs `shouldMatchList` [ (toKey 2, 3)
+                            , (toKey 3, 6)
+                            , (toKey 4, 5)
+                            ]
 
--- instance HasMemAddressStateDB StorM where
-  -- getAddressStateTxDBMap = use atx
-  -- putAddressStateTxDBMap = assign atx
-  -- getAddressStateBlockDBMap = use abs
-  -- putAddressStateBlockDBMap = assign abs
+    it "put 0 should not change the state root" . runStorM $ do
+      want <- addressStateContractRoot <$> getAddressState 0x1234
+      want `shouldBe` "V\232\US\ETB\ESC\204U\166\255\131E\230\146\192\248n[H\224\ESC\153l\173\192\SOHb/\181\227c\180!"
+      putStorageKeyVal' 0x1234 0x3 0x0
+      flushMemStorageDB
+      got <- addressStateContractRoot <$> getAddressState 0x1234
+      want `shouldBe` got
 
--- instance HasStateDB StorM where
-  -- getStateDB = liftM2 MP.MPDB (use sdb) (use sdbsr)
-  -- setStateDBStateRoot = assign sdbsr
 
--- instance HasHashDB StorM where
-  -- getHashDB = use hdb
-
--- initialEnv :: IO (FilePath, CachedStorage)
--- initialEnv = do
-  -- tmpdir <- mkdtemp "/tmp/initial_env"
-  -- let ldbOptions = DB.defaultOptions { DB.createIfMissing = True }
-    --   openDB b = DBB.open (tmpdir ++ b) ldbOptions
-  -- s <- openDB stateDBPath
-  -- h <- openDB hashDBPath
-  -- let st = CS s MP.emptyTriePtr h M.empty M.empty M.empty M.empty
-  -- fmap (tmpdir,) . runResourceT . flip execStateT st $ do
-    -- MP.initializeBlank =<< getStateDB
-
--- benchStorM :: NFData a => String -> StorM a -> Benchmark
--- benchStorM name a = envWithCleanup initialEnv (\ ~(p, _) -> removePathForcibly p)
-    --                                           (\ ~(_, s) -> bench name . nfIO $ runStorM s a)
-
+    it "put 1 should change the state root" . runStorM $ do
+      want <- addressStateContractRoot <$> getAddressState 0x222
+      putStorageKeyVal' 0x1234 0x3 0x44
+      flushMemStorageDB
+      got <- addressStateContractRoot <$> getAddressState 0x1234
+      want `shouldNotBe` got
+      got `shouldBe` "E\RS\164\USe\177\214\249m\186\SI\248\136\\\215\137\172\231\135q\224;\178TWg\SUB\147n\134. "
