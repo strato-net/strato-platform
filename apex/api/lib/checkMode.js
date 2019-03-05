71424baf5d85c096ea1b258576132888f691148a
Jelani John <jelanijohn@gmail.com>
2019-03-05 13:41:34 -0500

    fix for test run inside container resolving variable to undefined

--- apex/api/lib/checkMode.js
+++ apex/api/lib/checkMode.js
@@ -2,7 +2,7 @@ const appConfig = require(`${process.cwd()}/config/app.config`);
 
 module.exports =  {
   checkMode: function (req, res, next) {
-    if (appConfig.SMD_MODE === 'public' || process.env.OAUTH_ENABLED == appConfig.oAuthEnabledTrueValue) {
+    if (appConfig.SMD_MODE === 'public' || (process.env.OAUTH_ENABLED && process.env.OAUTH_ENABLED == appConfig.oAuthEnabledTrueValue)) {
       return next();
     }
     res.status(404).json({
