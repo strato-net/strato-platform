1631846f30b275ecb8203af0957fccdc7d5a2dce
Yunfan Jiang <yunfan@blockapps.net>
2019-03-13 17:53:06 -0400

    Add simple metrics for health check in sequencer, vm, slipstream

--- shared-util/cross-monitoring/src/Blockapps/Crossmon.hs
+++ shared-util/cross-monitoring/src/Blockapps/Crossmon.hs
@@ -15,12 +15,22 @@ maxBlockNumberSeen = unsafeRegister
                          "Maximum of block numbers seen. This is most useful in\
                          \ consensus algorithms with a linear sequence of blocks"
 
+healthCheck :: Vector Text Gauge
+healthCheck = unsafeRegister
+                   . vector "location"
+                   . gauge
+                   $ Info "health_check"
+                         "Check if processes are running in the last 1 minute"
+reportOne :: Integer
+reportOne = 1
 
 -- Note: This function is not threadsafe across locations:
 --   recordMaxBlockNumber l1 n | recordMaxBlockNumber l1 m
 -- is nondeterministic in deciding whether to keep n or m.
 recordMaxBlockNumber :: (MonadIO m, Integral a) => Text -> a -> m ()
-recordMaxBlockNumber loc n = liftIO . withLabel maxBlockNumberSeen loc $ \g -> do
-  let new = fromIntegral n
-  current <- getGauge g
-  when (new > current) $ setGauge g new
+recordMaxBlockNumber loc n = liftIO $ do
+  withLabel healthCheck loc (flip setGauge . fromIntegral $ reportOne)
+  withLabel maxBlockNumberSeen loc $ \g -> do
+    let new = fromIntegral n
+    current <- getGauge g
+    when (new > current) $ setGauge g new
