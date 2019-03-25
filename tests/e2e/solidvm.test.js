2572e6bb4ac2019bf8d3ddad6a7e29c9a62bba8d
Tim Holland <tim@blockapps.net>
2019-03-25 13:23:04 -0400

    Add state and cirrus queries to solid-vm e2e

--- tests/e2e/solidvm.test.js
+++ tests/e2e/solidvm.test.js
@@ -1,9 +1,14 @@
 "use strict";
 const ba = require('blockapps-rest');
+const chai = require('chai');
+const assert = chai.assert
 const co = require('co');
 require('co-mocha');
 const config = ba.common.config;
+config.apiDebug = true
+console.log(`Config is ${JSON.stringify(config)}`);
 const rest = ba.rest6;
+const strato = ba.common.api.strato;
 const util = ba.common.util;
 
 const counter = `
@@ -40,6 +45,15 @@ async function read(user, contract, vm) {
     user, contract, "read", {'VM': vm});
 }
 
+async function state(contract) {
+  return await co.wrap(rest.getState)(contract);
+}
+
+async function index(contract) {
+  console.log(`Contract is ${JSON.stringify(contract)}`);
+  return await co.wrap(rest.query)(`${contract.name}?address=eq.${contract.address}`);
+}
+
 describe('Solid VM: Contract uploads', async () => {
   // it ('can upload an EVM counter', async () => {
   //   const [user, results] = await upload('EVM');
@@ -64,12 +78,19 @@ describe('Solid VM: Contract uploads', async () => {
 
   it ('can count upwards on the SolidVM counter', async () => {
     const [user, contract] = await upload('SolidVM');
+    console.log(`Contrcat is : ${JSON.stringify(contract)}`);
     console.log(`Counting to 5`);
     for (let i = 0; i < 5; i++) {
       await incr(user, contract, 'SolidVM');
     }
     console.log(`Checking our work`);
-    const results = await read(user, contract, 'EVM');
-    console.log(`Results: ${JSON.stringify(results)}`);
+    const gotRead = await read(user, contract, 'SolidVM');
+    assert.deepEqual(gotRead, ["5"]);
+    const gotState = await state(contract);
+    assert.equal(gotState.count, "5");
+    const gotIndex = await index(contract);
+    assert.equal(gotIndex[0].address, contract.address);
+    // TODO: enable the following line
+    // assert.equal(gotIndex[0].count, 5);
   }).timeout(config.timeout);
 })
