38f23b70b70edf71c10ed62a8a4a5f47c8e64500
Tim Holland <tim@blockapps.net>
2019-03-28 21:00:50 +0000

    Add forgotten test

---
+++ blockapps-haskell/solidity/test/BlockApps/SolidityVarReaderSpec.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE OverloadedStrings #-}
+module BlockApps.SolidityVarReaderSpec where
+
+import Control.Monad
+import qualified Data.Map.Ordered as OM
+import Test.Hspec
+
+import BlockApps.Ethereum
+import BlockApps.Solidity.Struct
+import qualified Data.Text as T
+import BlockApps.Solidity.Type
+import BlockApps.Solidity.Value
+import BlockApps.SolidityVarReader (structSort)
+
+addr :: Address -> Value
+addr = SimpleValue . ValueAddress
+
+
+structFromFields :: [T.Text] -> Struct
+structFromFields ts = Struct (OM.fromList [(t, (Left t, SimpleType TypeAddress)) | t <- ts])
+                    . fromIntegral $ length ts
+
+spec :: Spec
+spec = do
+  it "should be able to struct sort" $ do
+    let cases = [ (structFromFields ["a"]
+                    , [("a", addr 0x77)]
+                    , [("a", addr 0x77)])
+                , (structFromFields ["a", "b"]
+                    , [("a", addr 20), ("b", addr 42)]
+                    , [("a", addr 20), ("b", addr 42)])
+                , (structFromFields ["a", "b"]
+                    , [("b", addr 80), ("a", addr 234)]
+                    , [("a", addr 234), ("b", addr 80)]
+                    )
+                , (structFromFields ["d", "c", "b", "a"]
+                      , [("a", addr 0xa), ("c", addr 0xc), ("d", addr 0xd), ("b", addr 0xb)]
+                      , [("d", addr 0xd), ("c", addr 0xc), ("b", addr 0xb), ("a", addr 0xa)]
+                      )
+                ]
+    forM_ cases $ \(tipe, input, want) -> structSort tipe input `shouldBe` want
