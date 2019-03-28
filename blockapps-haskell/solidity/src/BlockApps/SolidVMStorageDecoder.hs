f8b909d106509ca7fcd71492de7fd2516cd739a4
Tim Holland <tim@blockapps.net>
2019-03-28 18:15:05 +0000

    Clean up

--- blockapps-haskell/solidity/src/BlockApps/SolidVMStorageDecoder.hs
+++ blockapps-haskell/solidity/src/BlockApps/SolidVMStorageDecoder.hs
@@ -3,18 +3,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TupleSections #-}
 
-{-# OPTIONS_GHC -fno-warn-unused-imports #-}
-{-# OPTIONS_GHC -fno-warn-unused-top-binds #-}
 module BlockApps.SolidVMStorageDecoder
   ( decodeSolidVMValues
   , decodeCacheValues
   , replayDelta -- Testing only
   , ReplayFailure(..)
   , synthesize -- Testing only
+  , TotalStorage
   ) where
 
 import Control.DeepSeq
-import Control.Exception
 import Control.Monad.Extra
 import Data.Bifunctor
 import Data.Bitraversable
@@ -39,64 +37,30 @@ decodeSolidVMValues :: [(HexStorage, HexStorage)] -> [(T.Text, SolidityValue)]
 decodeSolidVMValues hxs = either (error . printf "decodeSolidVMValues: %s" . show) id $ do
   pathValues <- mapM (bimapM hexStorageToPath hexStorageToBasic) hxs
   totalStorage <- bimap show HM.toList $ synthesize pathValues
-  mapMaybeM (bimapVToS bsToText) totalStorage
--- decodeSolidVMValues hexs = either (error . printf "decodeSolidVMValues: %s" . show) id $ do
---   pathValues <- mapM (bimapM hexStorageToPath hexStorageToBasic) hexs
---   totalStorage <- bimap show HM.toList $ synthesize pathValues
---   mapMaybeM (bimapSToS bsToText) totalStorage
+  mapMaybeM (bimapValue bsToText) totalStorage
 
-bimapVToS :: (t1 -> Either String t2) -> (t1, V.Value) -> Either String (Maybe (t2, SolidityValue))
-bimapVToS f (name', value') = do
+bimapValue:: (t1 -> Either String t2) -> (t1, V.Value) -> Either String (Maybe (t2, SolidityValue))
+bimapValue f (name', value') = do
   name <- f name'
   mValue <- valueToSolidityValue value'
   return $ fmap (name,) mValue
-
 
 
 decodeCacheValues :: M.Map B.ByteString B.ByteString -> [(T.Text, Value)] -> [(T.Text, Value)]
 decodeCacheValues hxs [] = either (error . printf "SVM.decodeCacheValues: %s" . show) id $ do
   pathValues <- mapM (bimapM (hexStorageToPath . HexStorage) (hexStorageToBasic . HexStorage)) $ M.toList hxs
   totalStorage <- bimap show HM.toList $ synthesize pathValues
-  mapMaybeM (bimapSToV bsToText) totalStorage
+  mapM (firstM bsToText) totalStorage
 decodeCacheValues _ _ = error "todo: updates"
 
 bsToText :: B.ByteString -> Either String T.Text
 bsToText = first show . decodeUtf8'
 
-bimapSToV :: (t1 -> Either String t2) -> (t1, V.Value) -> Either String (Maybe (t2, V.Value))
-bimapSToV f (name', value') = do
-  name <- f name'
-  mValue <- storableToValue value'
-  return $ fmap (name,) mValue
-
-
-storableToValue :: V.Value -> Either String (Maybe V.Value)
-storableToValue = Right . Just
-  -- BasicValue bv -> case bv of
-  --   BDefault -> sv $ ValueAddress 0x0 -- Is this the best way?
-  --   BInteger n -> sv $ ValueInt True Nothing n
-  --   BString t -> sv $ ValueBytes Nothing t
-  --   BBool b -> sv $ ValueBool b
-  --   BAddress a -> sv $ ValueAddress a
-  --   BContract _ c -> Right . Just $ ValueContract c
-  --   BEnumVal k n -> Right . Just $ ValueEnum k n 0x77777 -- TODO: SolidVM enums should be numbered
-  -- SArray ivs -> Just . ValueArrayDynamic <$> mapMaybeM storableToValue (unsparse $ I.toList ivs)
-  -- SArraySentinel{} -> Right Nothing
-  -- SMapping kvs -> Just . ValueMapping . M.fromList <$> mapMaybeM (bimapSToV idxToSimple) (HM.toList kvs)
-  -- SStruct fs -> Just . ValueStruct <$> mapMaybeM (bimapSToV bsToText) (HM.toList fs)
-
---  where sv :: SimpleValue -> Either String (Maybe Value)
---        sv = Right . Just . SimpleValue
-
---        idxToSimple :: IndexType -> Either String SimpleValue
---        idxToSimple = Right . \case
---           INum n -> ValueInt True Nothing n
---           IAddress a -> ValueAddress a
---           IText t -> ValueBytes Nothing t
---           IBool b -> ValueBool b
+firstM :: Monad m => (a1 -> m a2) -> (a1, b) -> m (a2, b)
+firstM f (a1, b) = (,b) <$> f a1
 
 -- Why another time?
---  - original vToSV can't handle sentinels without a monad
+--  - original vToSV can't handle sentinels without introducing a monad
 --  - SolidVM shares a bytes/string typeA and cannot hexencode all strings returned
 --  - Enums don't have numeric values in SolidVM
 valueToSolidityValue :: V.Value -> Either String (Maybe SolidityValue)
@@ -110,8 +74,8 @@ valueToSolidityValue = \case
   ValueEnum _ ev _ -> fromText ev
   ValueArraySentinel{} -> Right Nothing
   ValueContract c -> fromShowable c
-  ValueStruct fs -> Just . SolidityObject <$> mapMaybeM (bimapVToS Right) fs
-  ValueMapping kvs -> Just . SolidityObject <$> mapMaybeM (bimapVToS tshowIdx) (M.toList kvs)
+  ValueStruct fs -> Just . SolidityObject <$> mapMaybeM (bimapValue Right) fs
+  ValueMapping kvs -> Just . SolidityObject <$> mapMaybeM (bimapValue tshowIdx) (M.toList kvs)
   ValueArrayDynamic ivs -> Just . SolidityArray <$> mapMaybeM valueToSolidityValue (unsparse ivs)
   ValueArrayFixed{} -> Left "internal error: SolidVM generate state for static arrays"
   ValueFunction{} -> Left "internal error: SolidVM generating state for functions"
@@ -131,57 +95,6 @@ valueToSolidityValue = \case
           ValueBytes _ bs -> error $ "bytes index" ++ show bs
           ValueBool True -> Right "true"
           ValueBool False -> Right "false"
-
--- storableToSolidity :: StorableValue -> Either String (Maybe SolidityValue)
--- storableToSolidity = \case
---   BasicValue bv  -> case bv of
---     BDefault -> fromShowable (0 ::Int)
---     BInteger n -> fromShowable n
---     BString t -> Just . SolidityValueAsString <$> (first show . decodeUtf8') t
---     BBool b -> Right . Just $ SolidityBool b
---     BAddress a -> Right . Just . SolidityValueAsString . T.pack $ show a
---     BContract _ a -> fromShowable a
---     BEnumVal _ ev -> Right . Just $ SolidityValueAsString ev
---   SStruct fs -> Just . SolidityObject <$> mapMaybeM (bimapSToS bsToText) (HM.toList fs)
---   SArray ivs -> Just . SolidityArray <$> mapMaybeM storableToSolidity (unsparse $ I.toList ivs)
---   SArraySentinel{} -> Right Nothing
---   SMapping kvs -> Just . SolidityObject <$> mapMaybeM (bimapSToS tshowIdx) (HM.toList kvs)
-
---  where fromShowable :: (Show a) => a -> Either String (Maybe SolidityValue)
---        fromShowable = Right . Just . SolidityValueAsString . T.pack . show
---        tshowIdx :: IndexType -> Either String T.Text
---        tshowIdx = \case
---          INum n -> Right . T.pack . show $ n
---          IAddress a -> Right . T.pack . show $ a
---          IText t -> first show $ decodeUtf8' t
---          IBool True -> Right "true"
---          IBool False -> Right "false"
-
--- unsparse :: [(Int, StorableValue)] -> [StorableValue]
--- unsparse = go 0
---  where go _ [] = []
---        go n kvs@((k, v):kvs') | n == k = v:go (n +1) kvs'
---                               | otherwise = zeroOf v:go (n+1) kvs
-
--- zeroOf :: StorableValue -> StorableValue
--- zeroOf = \case
---   BasicValue bv -> BasicValue $ case bv of
---     BDefault -> BDefault
---     BInteger{} -> BInteger 0
---     BString{} -> BString ""
---     BBool{} -> BBool False
---     BAddress{} -> BAddress 0x0
---     BContract c _ -> BContract c 0x0
---     -- This is not quite accurate, as the minimum should be the BEnumVal
---     -- However, we don't have enough context to know which string is minimum
---     BEnumVal{} -> BInteger 0
---   SArray{} -> SArray I.empty
---   SArraySentinel{} -> SArray I.empty
---   SMapping{} -> SMapping HM.empty
---   SStruct fs -> SStruct $ HM.map zeroOf fs
-
-
-
 
 type TotalStorage = HM.HashMap B.ByteString V.Value
 
@@ -205,23 +118,6 @@ replayDelta ((p, _):_) _ = Left $ MissingPath p
 
 applyDelta :: StoragePath -> BasicValue -> V.Value -> Either ReplayFailure V.Value
 applyDelta (StoragePath sp) = applyDelta' sp
-
-fromBasic :: BasicValue -> V.Value
-fromBasic = \case
-  BBool b -> SimpleValue $ ValueBool b
-  BInteger n -> SimpleValue $! valueInt n
-  BString bs -> SimpleValue $! valueBytes bs
-  BAddress a -> SimpleValue $! ValueAddress a
-  BContract _ c -> ValueContract c
-  BEnumVal k n -> ValueEnum k n 0x77777 -- TODO: Keep enum ord in BasicValue
-  BDefault -> SimpleValue $ ValueAddress 0x0
-
-fromIndex :: IndexType -> V.SimpleValue
-fromIndex = \case
-  IBool b -> ValueBool b
-  INum n -> valueInt n
-  IText bs -> valueBytes bs
-  IAddress a -> ValueAddress a
 
 applyDelta' :: [StoragePathPiece] -> BasicValue -> V.Value -> Either ReplayFailure V.Value
 applyDelta' [] bv (SimpleValue{}) = Right $ fromBasic bv
@@ -284,3 +180,20 @@ synthesize' ((sp, bv):rest) =
  where go :: [(StoragePath, BasicValue)] -> V.Value -> Either ReplayFailure V.Value
        go [] sv' = Right sv'
        go ((sp',bv'):t) sv' = go t =<< applyDelta sp' bv' sv'
+
+fromBasic :: BasicValue -> V.Value
+fromBasic = \case
+  BBool b -> SimpleValue $ ValueBool b
+  BInteger n -> SimpleValue $! valueInt n
+  BString bs -> SimpleValue $! valueBytes bs
+  BAddress a -> SimpleValue $! ValueAddress a
+  BContract _ c -> ValueContract c
+  BEnumVal k n -> ValueEnum k n 0x77777 -- TODO: Keep enum ord in BasicValue
+  BDefault -> SimpleValue $ ValueAddress 0x0
+
+fromIndex :: IndexType -> V.SimpleValue
+fromIndex = \case
+  IBool b -> ValueBool b
+  INum n -> valueInt n
+  IText bs -> valueBytes bs
+  IAddress a -> ValueAddress a
