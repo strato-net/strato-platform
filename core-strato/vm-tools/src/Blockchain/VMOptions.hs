ff9c9086f20817a8ff83ebc5a9f04feb3bef8d93
Jamshid <jamshidnh@gmail.com>
2019-05-06 17:47:41 -0400

    Changing the default value of SolidVM trace to False, because it has become a performance problem.

--- core-strato/vm-tools/src/Blockchain/VMOptions.hs
+++ core-strato/vm-tools/src/Blockchain/VMOptions.hs
@@ -40,6 +40,6 @@ defineFlag "queryBlocks" (10000::Int) "Number of blocks to query from SQL to pro
 defineFlag "miningVerification" True "Flag to turn mining verification or/off"
 defineFlag "transactionRootVerification" False "Flag to turn transaction root verification or/off"
 defineFlag "startingBlock" (-1::Integer) "block in kafka to start running the VM on"
-defineFlag "svmTrace" (True::Bool) "Whether to have verbose logging in SolidVM"
+defineFlag "svmTrace" (False::Bool) "Whether to have verbose logging in SolidVM"
 
 defineEQFlag "miner" [| Instant :: MinerType |] "MINER" "What mining algorithm"
