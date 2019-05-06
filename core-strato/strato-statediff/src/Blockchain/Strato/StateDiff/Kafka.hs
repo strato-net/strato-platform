8114fba72140e18a994f1b7bf3232e90501fc2e5
Tim Holland <tim@blockapps.net>
2019-05-06 15:57:43 +0000

    Revert "Limit the number of actions written to statediff to the socket request size"
    This reverts commit aad921f5e8b182b4d43afa45a41ad1e908c59781.

--- core-strato/strato-statediff/src/Blockchain/Strato/StateDiff/Kafka.hs
+++ core-strato/strato-statediff/src/Blockchain/Strato/StateDiff/Kafka.hs
@@ -3,46 +3,29 @@ module Blockchain.Strato.StateDiff.Kafka
     , assertTopicCreation
     , writeActionJSONToKafka
     , filterResponse
-    , mkMessages -- For testing
     ) where
 
 import           Control.Lens.Getter               (view)
-import           Control.Monad.Extra
 import           Data.Aeson
-import qualified Data.ByteString                   as B
 import qualified Data.ByteString.Lazy              as L
 import qualified Network.Kafka                     as K
 import qualified Network.Kafka.Producer            as KW
 import qualified Network.Kafka.Protocol            as KP
 
 import           Blockchain.Strato.Model.Action
+import           Blockchain.KafkaTopics            (lookupTopic)
 
 stateDiffTopicName :: KP.TopicName
-stateDiffTopicName = KP.TName  . KP.KString $ "statediff"
+stateDiffTopicName = lookupTopic "statediff"
 
 assertTopicCreation :: K.Kafka k => k ()
 assertTopicCreation = K.updateMetadata stateDiffTopicName
 
--- Provide an approximate message size
-mkTopicAndMessage :: (ToJSON a) => a -> (Int, K.TopicAndMessage)
-mkTopicAndMessage item =
-  let bs = L.toStrict $! encode item
-  in (B.length bs, K.TopicAndMessage stateDiffTopicName $! KW.makeMessage bs)
-
-mkMessages :: (ToJSON a) => Int -> [a] -> [[K.TopicAndMessage]]
-mkMessages batchLimit = go 0 [] . map mkTopicAndMessage
-  where go :: Int -> [K.TopicAndMessage] -> [(Int, K.TopicAndMessage)] -> [[K.TopicAndMessage]]
-        go _ pendingMsgs [] = [pendingMsgs]
-        go pendingSize pendingMsgs ((thisSize, msg):rest) =
-            if pendingSize + thisSize > batchLimit
-              then pendingMsgs:go thisSize [msg] rest
-              else go (pendingSize + thisSize) (msg:pendingMsgs) rest
-
-defaultSocketRequestMaxBytes :: Int
-defaultSocketRequestMaxBytes = 104857600
+mkTopicAndMessage :: (ToJSON a) => a -> K.TopicAndMessage
+mkTopicAndMessage = K.TopicAndMessage stateDiffTopicName . KW.makeMessage . L.toStrict . encode
 
 writeActionJSONToKafka :: (K.Kafka k) => [Action] -> k [KP.ProduceResponse]
-writeActionJSONToKafka = concatMapM KW.produceMessages . mkMessages defaultSocketRequestMaxBytes
+writeActionJSONToKafka = KW.produceMessages . map mkTopicAndMessage
 
 filterResponse :: [KP.ProduceResponse] -> [(KP.Partition, KP.KafkaError, KP.Offset)]
 filterResponse = concatMap onlyErrors
