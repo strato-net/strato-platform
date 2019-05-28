b0438acf6a482d6d74cbf230ef8668f3c2059f15
Yunfan Jiang <yunfan@blockapps.net>
2019-05-27 23:24:10 -0400

    Make system health and warnings available in websocket

--- apex/api/sockets/aggregators/getHealthStatus.js
+++ apex/api/sockets/aggregators/getHealthStatus.js
@@ -1,54 +1,83 @@
-const { GET_HEALTH, GET_NODE_UPTIME} = require('../rooms');
-const { emitter, ON_SOCKET_PUBLISH_EVENTS } = require('../eventBroker');
+const {GET_HEALTH, GET_NODE_UPTIME, GET_SYSTEM_INFO} = require('../rooms');
+const {emitter, ON_SOCKET_PUBLISH_EVENTS} = require('../eventBroker');
 const rp = require('request-promise');
 const models = require('../../models');
 const config = require('../../config/app.config');
 
-let healthStatus, uptimeDur
+let healthStatus, uptimeDur, systemInfoStatus, systemInfoMessages;
 
 
 async function getHealthStatus() {
-    const healthInfo = await models.CurrentHealth.findOne({
-        where: {
-            processName: "HealthStat"
-        },
-        attributes: [
-            'latestHealthStatus',
-            'latestCheckTimestamp',
-            'lastFailureTimestamp'
-        ]})
-    const stallInfo = await models.CurrentHealth.findOne({
-        where: {
-            processName: "StallStat"
-        },
-        attributes: [
-            'latestHealthStatus',
-            'latestCheckTimestamp',
-            'lastFailureTimestamp'
-        ]})
-    let currentTime = Date.now();
+  const healthInfo = await models.CurrentHealth.findOne({
+    where: {
+      processName: "HealthStat"
+    },
+    attributes: [
+      'latestHealthStatus',
+      'latestCheckTimestamp',
+      'lastFailureTimestamp'
+    ]
+  })
+  const stallInfo = await models.CurrentHealth.findOne({
+    where: {
+      processName: "StallStat"
+    },
+    attributes: [
+      'latestHealthStatus',
+      'latestCheckTimestamp',
+      'lastFailureTimestamp'
+    ]
+  })
 
-    if (healthInfo && stallInfo){
-        healthStatus = healthInfo.dataValues.latestHealthStatus && stallInfo.dataValues.latestHealthStatus;
-        uptimeDur = (healthStatus) ? currentTime - healthInfo.dataValues.lastFailureTimestamp : 0;
-    }
-    emitter.emit(ON_SOCKET_PUBLISH_EVENTS, GET_HEALTH, healthStatus);
-    emitter.emit(ON_SOCKET_PUBLISH_EVENTS, GET_NODE_UPTIME, uptimeDur/1000);
+  const systemInfo = await models.CurrentHealth.findOne({
+    where: {
+      processName: "SystemInfoStat"
+    },
+    attributes: [
+      'latestHealthStatus',
+      'latestCheckTimestamp',
+      'lastFailureTimestamp',
+      'isBlocksValidInc',
+      'isLastPending',
+      'additionalInfo'
+    ],
+    raw: true,
+  }).catch(err => next(err));
+
+  let currentTime = Date.now();
+
+  if (healthInfo && stallInfo) {
+    healthStatus = healthInfo.dataValues.latestHealthStatus && stallInfo.dataValues.latestHealthStatus;
+    uptimeDur = (healthStatus) ? currentTime - healthInfo.dataValues.lastFailureTimestamp : 0;
+  }
+
+  if (systemInfo) {
+    systemInfoStatus = systemInfo.latestHealthStatus;
+    systemInfoMessages = systemInfo.additionalInfo.split('"Alerts":')[1].split('}')[0];
+  }
+  emitter.emit(ON_SOCKET_PUBLISH_EVENTS, GET_HEALTH, healthStatus);
+  emitter.emit(ON_SOCKET_PUBLISH_EVENTS, GET_NODE_UPTIME, uptimeDur / 1000);
+  emitter.emit(ON_SOCKET_PUBLISH_EVENTS, GET_SYSTEM_INFO, {status: systemInfoStatus, warnings: systemInfoMessages})
 }
 
 getHealthStatus()
 setInterval(getHealthStatus, config.webSockets.dbPollFrequency);
 
 function initialHydrateHealthStatus(socket) {
-    socket.emit(`PRELOAD_${GET_HEALTH}`, healthStatus);
+  socket.emit(`PRELOAD_${GET_HEALTH}`, healthStatus);
 }
 
 function initialHydrateUptime(socket) {
-    socket.emit(`PRELOAD_${GET_NODE_UPTIME}`, uptimeDur/1000);
+  socket.emit(`PRELOAD_${GET_NODE_UPTIME}`, uptimeDur / 1000);
+}
+
+function initialHydrateSystemInfo(socket) {
+  socket.emit(`PRELOAD_${GET_SYSTEM_INFO}`, {status: systemInfoStatus, warnings: systemInfoMessages});
 }
 
 module.exports = {
-    initialHydrateHealthStatus,
-    initialHydrateUptime
+  initialHydrateHealthStatus,
+  initialHydrateUptime,
+  initialHydrateSystemInfo
 }
 
