f3607495d54d73fe52006589e80c45caaec02b16
V2 <50056327+VictorHWong@users.noreply.github.com>
2019-06-05 14:45:44 -0700

    Update stratoLoad.test.js

--- tests/load/stratoLoad.test.js
+++ tests/load/stratoLoad.test.js
@@ -1,126 +1,147 @@
+'use strict'
 
-pipeline {
-  agent {
-    label "test"
-  }
-  options { disableConcurrentBuilds() }
-  stages {
-   stage('Prepare') {
-      steps {
-        sh '''#!/bin/bash -le
-          set -x
-          docker rm -f $(docker ps -aq) || true; docker system prune -f --volumes
-        '''
-      }
+const ba = require('blockapps-rest');
+const BigNumber = require('bignumber.js');
+const co = require('co');
+require('co-mocha');
+const rest = ba.rest;
+const common = ba.common;
+const config = common.config;
+const util = common.util;
+const api = common.api;
+const moment = require('moment');
+const path = require('path');
+
+const adminName = util.uid('Admin');
+const adminPassword = '1234';
+
+const contractName = 'Vehicle';
+const contractFilename = path.join(config.contractsPath,"Vehicle.sol");
+
+let txs = [];
+let txResults = [];
+
+describe('Strato Load Test', function() {
+  this.timeout(9999 * 1000);
+
+  let admin;
+
+  const batchSize = util.getArgInt('--batchSize', 1);
+  const batchCount = util.getArgInt('--batchCount', 1);
+  const batchDelay = util.getArgInt('--batchDelay', 0);
+
+
+  before(function * () {
+    console.log(`Creating admin user and contract`);
+    admin = yield rest.createUser(adminName, adminPassword);
+    console.log(`User: ${admin.name} @ ${admin.address}`);
+    yield rest.compileSearch([contractName], contractName, contractFilename);
+    let balance = new BigNumber(0);
+    while (balance.isZero()) {
+      yield promiseTimeout(500);
+      balance = yield rest.getBalance(admin.address);
+      console.log(`Balance is: ${balance}`);
     }
-    stage('Copy Strato Build') {
-      steps {
-        copyArtifacts fingerprintArtifacts: true, flatten: true, parameters: 'PLATFORM_BRANCH_NAME=develop', projectName: 'STRATO_test', selector: lastSuccessful()
-      }
-    }
-    stage('Run Strato') {
-      steps {
-        withCredentials([
-          usernamePassword(credentialsId: 'docker-aws-registry-login', passwordVariable: 'DOCKER_PASSWD', usernameVariable: 'DOCKER_USER'),
-        ]) {
-          sh '''
-            #!/bin/bash -le
-            set -x
-            docker login -u $DOCKER_USER -p $DOCKER_PASSWD registry-aws.blockapps.net:5000
-            echo "Pulling strato"
-            rm -rf strato-getting-started
-            git clone https://github.com/blockapps/strato-getting-started.git
-            cp docker-compose.yml ./strato-getting-started
-            cd strato-getting-started
-            chmod +x strato.sh
-            NODE_HOST=cd10.eastus.cloudapp.azure.com ./strato.sh -m 1
-            docker ps
-            counter=0
-            # Few quick tests
-            until curl --silent --output /dev/null --fail --location localhost/bloc/v2.2/users/
-            do
-              if [ "$counter" -gt 300 ]; then
-                echo "bloc took longer than 5 minutes to initialize! aborting."
-                exit 73
-              fi
-              echo "waiting for bloc to be available through nginx"
-              counter=$((counter+1))
-              sleep 1
-            done
-            echo "bloc is available"
-          '''
-        }
-      }
-    }
-    stage('Run platform load test, VictorHWong-patch-1 branch') {
-      steps {
-        withCredentials([
-          usernamePassword(credentialsId: 'docker-aws-registry-login', passwordVariable: 'DOCKER_PASSWD', usernameVariable: 'DOCKER_USER'),
-          usernamePassword(credentialsId: 'blockapps-cd-github', passwordVariable: 'GITHUB_TOKEN', usernameVariable: 'USR')
-        ]) {
-          sh '''#!/bin/bash -le
-            set -x
-            echo "Running strato-platform tests"
-            rm -rf strato-platform
-            git clone -b VictorHWong-patch-1 --single-branch https://${USR}:${GITHUB_TOKEN}@github.com/blockapps/strato-platform.git
-            cd strato-platform/tests
-            git pull origin VictorHWong-patch-1
-            sleep 2
-            git checkout VictorHWong-patch-1
-            sleep 1
-            npm install
-            sleep 5
-            echo "Running strato load performance tests"
-            SERVER=localhost npm run test:load10k
-            '''
-        }
-      }
-  }
-    stage('Plot .csv performance data') {
-      steps {
-        plot csvFileName: 'plot-76fdec7c-2312-49a5-8946-26090eeacf64.csv',
-            csvSeries: [[
-                               displayTableFlag: false, exclusionValues: '',
-                               file: '/master/graph_PERFORMANCE_multinode_strato_load.csv',
-                               inclusionFlag: 'OFF',
-                               url: '']],
-            group: 'PERFORMANCE_reports',
-            style: 'line',
-            title: 'PERFORMANCE_multinode_strato_load'
+  });
+
+  it('Upload contracts', function * () {
+    const startTime = moment();
+    let blocTime = 0;
+    for(let i = 0; i < batchCount; i++) {
+      console.log(`Creating ${batchSize} transactions for count ${i}`);
+      factory_createUploadList(batchSize, i);
+      const blocStartTime = moment();
+      const results = yield api.bloc.uploadList({
+        password: adminPassword,
+        contracts: txs.slice(batchSize * i, batchSize * i + batchSize),
+        resolve: false
+      }, admin.name, admin.address, false);
+      const blocEndTime = moment();
+      blocTime += blocEndTime.diff(blocStartTime, 'seconds');
+      console.log(`Received ${results.length} receipts`);
+      txResults = txResults.concat(results);
+      if(batchDelay > 100) {
+        yield promiseTimeout(batchDelay);
       }
     }
 
+    const lastHash = txResults[txResults.length -1].hash;
+
+    console.log(`Waiting on address '${admin.address}' to reach nonce ${batchSize*batchCount}`);
+    yield waitResult(admin.address, batchSize, batchCount);
+
+    const endTime = moment();
+    const seconds = endTime.diff(startTime, 'seconds');
+    console.log(`Total seconds: ${seconds}, Bloc Submission Time: ${blocTime}  TPS ${batchSize * batchCount/seconds}`);
+
+    var TPS = batchSize * batchCount/seconds;
+    var data  = [seconds,blocTime,TPS];
+    const fs = require('fs');
+    fs.appendFile("graph_PERFORMANCE_multinode_strato_load.csv", data, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+
+        //console.log("The file was saved!");
+        });
+    fs.appendFile("graph_PERFORMANCE_multinode_strato_load.csv", "\n", function(err) {
+        if(err) {
+            return console.log(err);
+        }
+
+        //console.log("The file was saved!");
+        });
+
+  });
+
+});
+
+function * waitResult(address, batchSize, batchCount) {
+  let nonce = 0;
+  while(nonce < batchSize*batchCount) {
+    yield promiseTimeout(500);
+    try {
+      console.log(`Current Nonce is: ${nonce}`)
+      let result = yield api.strato.account(address);
+      console.log(`Result: ${JSON.stringify(result)}`);
+      nonce = result[0].nonce;
+    } catch (e) {
+      console.error(e);
+    }
+  }
 }
-post {
-    success {
-      slackSend(
-        color: 'good',
-        message: "Build Successful: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})"
-      )
-      sh '''
-      echo "Build Successful for Git branch: $BRANCH_NAME"
-    '''
-    }
 
-    failure {
-      slackSend(
-        color: 'danger',
-        message: "Multinode plaform tests failed: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})"
-      )
-      slackSend(
-        channel: '#testjenkins',
-        color: 'danger',
-        message: "Multinode plaform tests failed: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})"
-      )
-    }
+function promiseTimeout(timeout) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      resolve();
+    }, timeout);
+  });
+}
 
-    fixed {
-      slackSend(
-        channel: '#testjenkins',
-        color: 'good',
-        message: "Multinode plaform tests fixed: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})"
-      )
-    }
+function isObject(val) {
+  if (val === null) { return false;}
+  return ( (typeof val === 'function') || (typeof val === 'object') );
+}
 
+function factory_createUploadList(batchSize, batchIndex) {
+  for (var i = 0; i < batchSize; i++) {
+    // function Vehicle(string _vin, string _s0, string _s1, string _s2, string _s3) public
+    txs.push({
+      contractName: contractName,
+      args: {
+        _vin: `vin_${batchIndex}_${i}`,
+        _s0: `s0_${batchIndex}_${i}`,
+        _s1: `s1_${batchIndex}_${i}`,
+        _s2: `s2_${batchIndex}_${i}`,
+        _s3: `s3_${batchIndex}_${i}`,
+      },
+      txParams: {
+        gasLimit: 10000000,
+        gasPrice: 1,
+        nonce: batchSize * batchIndex + i
+      }
+    });
   }
+  return txs;
 }
