763ba44e46ca91cd39fef857836ecdce8f7cc6fe
Tim Holland <tim@blockapps.net>
2019-06-08 17:39:06 +0000

    Loop over all strato tables

--- core-strato/copy_tables.sql
+++ core-strato/copy_tables.sql
@@ -1,5 +1,18 @@
-ALTER TABLE IF EXISTS storage RENAME TO storage_backup;
-CREATE TABLE storage AS storage_backup WITH NO DATA;
+CREATE OR REPLACE FUNCTION backup_all() RETURNS void AS $$
+DECLARE
+  table_name text;
+  backup_table text;
+BEGIN
+	FOR table_name in SELECT tablename FROM pg_catalog.pg_tables where schemaname = 'public' and tablename not like '%backup%'
+	    LOOP
+		backup_table := table_name||'_backup_'||now();
+		EXECUTE 'ALTER TABLE ' || quote_ident(table_name) || ' RENAME TO ' || quote_ident(backup_table) || ';';
+		EXECUTE 'CREATE TABLE ' || quote_ident(table_name) || ' AS TABLE ' || quote_ident(backup_table) || ' WITH NO DATA';
+	   END LOOP;
 
-ALTER TABLE IF EXISTS address_state_ref RENAME TO address_state_ref_backup;
-CREATE TABLE address_state_ref AS address_state_ref_backup WITH NO DATA;
+	RETURN;
+END
+
+$$ LANGUAGE plpgsql;
+
+SELECT backup_all();
