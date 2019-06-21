223cfbe3d65cadb29daab8b35a122bb918318e26
Yunfan Jiang <yunfan@blockapps.net>
2019-06-21 08:59:16 -0400

    Increase request timeout

--- apex/api/config/app.config.js
+++ apex/api/config/app.config.js
@@ -34,7 +34,7 @@ module.exports = {
     dbPollFrequency: 1 * 1000
   },
   healthCheck: {
-    requestTimeout: 1 * 1000,
+    requestTimeout: 3 * 1000,
     pollFrequency: 15 * 1000,
     pollTimeoutsForUnhealthy: 3, // number of timed out polls in a row to consider node unhealthy
     cleanFrequency: 5 * 60 * 1000, //clean db every 5 mins
