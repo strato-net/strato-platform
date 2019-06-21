7c18e3563b946606f56149399c9d23fa23f9555e
Tim Holland <tim@blockapps.net>
2019-06-21 17:20:55 +0000

    Attempt to reexec

--- shared-util/blockapps-init/src/BlockApps/Init.hs
+++ shared-util/blockapps-init/src/BlockApps/Init.hs
@@ -1,14 +1,34 @@
 module BlockApps.Init ( blockappsInit ) where
 
 import Control.Monad
-import Data.Text
+import Data.List (intercalate)
+import Data.Text hiding (intercalate)
+import Foreign hiding (void)
+import Foreign.C
+import System.Environment
+import System.IO
 import System.Posix.Signals
 
 import Blockapps.Crossmon
+
+foreign import ccall unsafe "execvp"
+  c_execvp :: CString -> Ptr CString -> IO CInt
+
+selfExec :: IO ()
+selfExec = do
+  putStrLn "attempting to self exec"
+  cmd <- getProgName
+  args <- getArgs
+  putStrLn $ "caught sig-hup; self-reexec: " ++ intercalate " " (cmd:args)
+  hFlush stdout
+  cmdC <- newCString cmd
+  argsC <- mapM newCString args
+  withArray argsC $ \argsPC ->
+    void $ c_execvp cmdC argsPC
 
 blockappsInit :: Text -> IO ()
 blockappsInit self = do
   initializeHealthChecks self
 
   -- TODO: exec self
-  void $ installHandler sigHUP (Catch (print (self, "sighup received!"))) Nothing
+  void $ installHandler sigHUP (Catch selfExec) Nothing
