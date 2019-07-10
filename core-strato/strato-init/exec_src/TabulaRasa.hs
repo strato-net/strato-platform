25dc13b6141ef8dcfea25a0124112d3faee7441a
Tim Holland <tim@blockapps.net>
2019-07-10 15:42:30 +0000

    Define the generation executable

--- core-strato/strato-init/exec_src/TabulaRasa.hs
+++ core-strato/strato-init/exec_src/TabulaRasa.hs
@@ -1,3 +1,17 @@
-main :: IO ()
-main = error "TODO(tim): TabulaRasa"
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+import Data.String
 
+import Blockchain.Init.Generator
+import Blockchain.Init.Options
+
+import HFlags
+
+main :: IO ()
+main = do
+  let kaddr = case flags_kafkahost of
+                  "" -> ("kafka", 9092)
+                  _ -> (fromString flags_kafkahost, 9092)
+
+  _ <- $initHFlags "tabula-rasa"
+  runGenM kaddr $ mkAll flags_genesisBlockName
