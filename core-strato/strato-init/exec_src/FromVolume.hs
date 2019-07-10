f916ef7261894977e1fa1ad2c4b67bae27310993
Tim Holland <tim@blockapps.net>
2019-07-10 22:10:28 +0000

    Define the restart init script

--- core-strato/strato-init/exec_src/FromVolume.hs
+++ core-strato/strato-init/exec_src/FromVolume.hs
@@ -1,2 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+import Data.String
+import Blockchain.Init.Options
+import Blockchain.Init.Protocol
+
+import HFlags
+
 main :: IO ()
-main = error "TODO(tim): FromVolume"
+main = do
+  _ <- $initHFlags "from-volume"
+  let kaddr = case flags_kafkahost of
+                  "" -> ("kafka", 9092)
+                  _ -> (fromString flags_kafkahost, 9092)
+  addEvent kaddr InitComplete
