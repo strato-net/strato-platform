24248a88c3fe51a5d0a0acfa5eb1439afb154aef
swapnanildeb <swapnanildeb3@gmail.com>
2019-07-24 15:21:33 -0400

    babelrc

--- blockapps-rest/lib/util/oauth.util.js
+++ blockapps-rest/lib/util/oauth.util.js
@@ -3,7 +3,7 @@ import simpleOauth from 'simple-oauth2'
 import unixTime from 'unix-time'
 import request from 'sync-request'
 import getPem from 'rsa-pem-from-mod-exp'
-import {parse} from 'flatted/cjs';
+import {parse} from 'flatted/esm';
 
 class OAuthUtil {
 	constructor(oauthConfig) {
