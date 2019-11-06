ac25226a0041dd45a1cbe465cce0a09fc9676f0e
Tanuj Soni <tanuj@blockapps.net>
2019-11-06 12:50:50 +0530

    [STRATO-1627] global password monitoring

--- apex/api/daemons/node-health-check.js
+++ apex/api/daemons/node-health-check.js
@@ -8,13 +8,13 @@ const disk = require('diskusage');
 const config = require('../config/app.config');
 
 const neededJobs = {
-    "slipstream_main":"slipstream",
-    "strato_p2p":"strato-p2p",
-    "vm_main":"vm-runner",
-    "seq_main":"strato-sequencer",
-    "blockapps-vault-wrapper-server":"vault-wrapper",
-    "blockapps-bloc":"bloc",
-    "strato-api":"strato-api"
+  "slipstream_main": "slipstream",
+  "strato_p2p": "strato-p2p",
+  "vm_main": "vm-runner",
+  "seq_main": "strato-sequencer",
+  "blockapps-vault-wrapper-server": "vault-wrapper",
+  "blockapps-bloc": "bloc",
+  "strato-api": "strato-api"
 }
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
@@ -22,7 +22,7 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 // DAEMON - query node-health-check every N sec
 winston.info('Starting node-health-check with a delay of', config.healthCheck.pollFrequency);
 
-(async() => {
+(async () => {
   await singleCheck()
   setInterval(async () => {
     await singleCheck()
@@ -39,138 +39,158 @@ async function singleCheck() {
 }
 
 function queryHealthStatus() {
-    return new Promise(async (resolve, _void) => {
-        try {
-            const metricsResult = await getHealthPrometheus();
-            await checkLatest();
-            const healthStatus = await compareTimeStamp(metricsResult);
-            const overallStatus = await updateHealthStat(healthStatus);
-            await updateCurrentHealth(overallStatus);
-            return resolve();
-        } catch (error) {
-            winston.error(`Error occurred while querying some of the health info: "${error.message ? error.message : 'no message'}"`);
-            return resolve();
-        }
-    }).timeout(config.healthCheck.requestTimeout - 80);
+  return new Promise(async (resolve, _void) => {
+    try {
+      const metricsResult = await getHealthPrometheus();
+      await checkLatest();
+      const healthStatus = await compareTimeStamp(metricsResult);
+      const overallStatus = await updateHealthStat(healthStatus);
+      console.log("----------------------------------", overallStatus);
+      await updateCurrentHealth(overallStatus);
+      return resolve();
+    } catch (error) {
+      winston.error(`Error occurred while querying some of the health info: "${error.message ? error.message : 'no message'}"`);
+      return resolve();
+    }
+  }).timeout(config.healthCheck.requestTimeout - 80);
 }
 
 function getHealthPrometheus() {
   if (!process.env['prometheusHost']) {
     throw Error('prometheusHost env var is not set - unable to get prometheus data');
   }
-    const options = {
-        method: 'GET',
-        url: `http://${process.env['prometheusHost']}/prometheus/api/v1/query?query=health_check`,
-        followRedirects: false,
-        timeout: config.healthCheck.requestTimeout-100,
-        json: true,
-    };
-    return rp(options);
+  const options = {
+    method: 'GET',
+    url: `http://${process.env['prometheusHost']}/prometheus/api/v1/query?query=health_check`,
+    followRedirects: false,
+    timeout: config.healthCheck.requestTimeout - 100,
+    json: true,
+  };
+  return rp(options);
 }
 
-function compareTimeStamp(obj) {
-    if (!(obj && obj.data && obj.data.result)) {
-        winston.warn(`Not Found results while querying health status: prometheus path might be incorrect`);
-        return {};
+async function isPasswordExist() {
+  const options = {
+    method: 'GET',
+    url: `http://${process.env['vaultWrapperHost']}/strato/v2.3/verify-password`,
+    followRedirects: false,
+    timeout: config.healthCheck.requestTimeout - 100,
+    json: true,
+  };
+  const data = await rp(options);
+  return data;
+}
+
+async function compareTimeStamp(obj) {
+  if (!(obj && obj.data && obj.data.result)) {
+    winston.warn(`Not Found results while querying health status: prometheus path might be incorrect`);
+    return {};
+  }
+  const timeNow = Date.now() / 1000;
+
+  res = obj.data.result;
+
+  const ret = {};
+  const checkJobs = Object.assign({}, neededJobs);
+  res.forEach((elem) => {
+    let name, value, loc;
+    if (elem && elem.metric && elem.value && elem.value.length >= 2) {
+      name = elem.metric.job;
+      loc = elem.metric.location.toString();
+
+      // check and remove from checkJObs list
+      if ((loc in checkJobs) && (checkJobs[loc] == name)) {
+        delete checkJobs[loc];
+      } else {
+        winston.warn(`Jobs are updated? The following prometheus job is not in the check list required: `, loc);
+      }
+
+      ret[name] = (Math.abs(timeNow - elem.value[0]) < config.healthCheck.pollFrequency * config.healthCheck.pollTimeoutsForUnhealthy / 1000) && (elem.value[1] == 1) ? true : false;
+    } else {
+      winston.info(`Metric format is updated; need to update its handling`);
     }
-    const timeNow = Date.now() / 1000;
+  })
 
-    res = obj.data.result;
+  Object.keys(checkJobs).forEach((elem) => {
+    ret[checkJobs[elem]] = false;
+    winston.warn(`${checkJobs[elem]} : ${elem} not found in the prometheus response; Not started`);
+  })
 
-    const ret = {};
-    const checkJobs = Object.assign({}, neededJobs);
-    res.forEach((elem) => {
-        let name, value,loc;
-        if (elem && elem.metric && elem.value && elem.value.length >= 2){
-            name = elem.metric.job;
-            loc = elem.metric.location.toString();
+  winston.info('Create entry for latest health status:', ret);
 
-            // check and remove from checkJObs list
-            if ((loc in checkJobs) && (checkJobs[loc] == name)){
-                delete checkJobs[loc];
-            } else {
-                winston.warn(`Jobs are updated? The following prometheus job is not in the check list required: `, loc);
-            }
-
-            ret[name] = (Math.abs(timeNow - elem.value[0]) < config.healthCheck.pollFrequency * config.healthCheck.pollTimeoutsForUnhealthy /1000 ) && (elem.value[1] == 1) ? true : false;
-        } else {
-            winston.info(`Metric format is updated; need to update its handling`);
-        }
-    })
-
-    Object.keys(checkJobs).forEach((elem) => {
-        ret[checkJobs[elem]] = false;
-        winston.warn(`${checkJobs[elem]} : ${elem} not found in the prometheus response; Not started`);
-    })
-
-    winston.info('Create entry for latest health status:', ret);
-
-    return ret;
+  return ret;
 }
 
 async function updateHealthStat(healthStatus) {
-    let overallStat = true;
-    let currentTime = Date.now();
-    let failedTask = [];
-    Object.keys(healthStatus).forEach(async(keyProcess) => {
-        if (!healthStatus[keyProcess]){
-            failedTask.push(keyProcess);
-        }
-        overallStat = healthStatus[keyProcess] && overallStat;
-        await models.HealthStat.create({
-            processName: keyProcess,
-            HealthStatus: healthStatus[keyProcess],
-            timestamp: currentTime
-        });
+  let overallStat = true;
+  let currentTime = Date.now();
+  let failedTask = [];
+
+  Object.keys(healthStatus).forEach(async (keyProcess) => {
+    if (!healthStatus[keyProcess]) {
+      failedTask.push(keyProcess);
+    }
+    overallStat = healthStatus[keyProcess] && overallStat;
+    await models.HealthStat.create({
+      processName: keyProcess,
+      HealthStatus: healthStatus[keyProcess],
+      timestamp: currentTime
     });
-    return [overallStat,failedTask];
+  });
+  return [overallStat, failedTask];
 }
 
 async function updateCurrentHealth(overallStat) {
-    let currentTime = Date.now();
-    let [systemInfoStatus, systemInfo] = await checkSystemInfo();
+  let currentTime = Date.now();
+  let [systemInfoStatus, systemInfo] = await checkSystemInfo();
+  const isPasswordExists = await isPasswordExist();
 
-    let [stat, created] = await models.CurrentHealth.findOrCreate({where: {processName: 'HealthStat'}, defaults: {
-            latestHealthStatus: overallStat[0],
-            latestCheckTimestamp: currentTime,
-            additionalInfo: overallStat[1].toString(),
-            lastFailureTimestamp: currentTime  // default first time marked as failure
-        }
-    })
-    if (!created) {
-      await stat.update(
-          {
-            latestCheckTimestamp: currentTime,
-            latestHealthStatus: overallStat[0],
-            additionalInfo: overallStat[1].toString(),
-            lastFailureTimestamp: overallStat[0] ? stat.lastFailureTimestamp : currentTime
-          },
-          {
-            where: {processName: 'HealthStat'}
-          }
-      );
+  let [stat, created] = await models.CurrentHealth.findOrCreate({
+    where: { processName: 'HealthStat' }, defaults: {
+      latestHealthStatus: isPasswordExists ? overallStat[0] : false,
+      latestCheckTimestamp: currentTime,
+      additionalInfo: overallStat[1].toString(),
+      lastFailureTimestamp: currentTime  // default first time marked as failure
     }
-    let [statSys, createdSys] = await models.CurrentHealth.findOrCreate({where: {processName: 'SystemInfoStat'}, defaults: {
-          latestHealthStatus: systemInfoStatus,
-          latestCheckTimestamp: currentTime,
-          additionalInfo: JSON.stringify(systemInfo),
-          lastFailureTimestamp: currentTime  // default first time marked as failure
-    }})
-    if (!createdSys){
-      await statSys.update(
-          {latestCheckTimestamp: currentTime,
-            latestHealthStatus: systemInfoStatus,
-            additionalInfo: JSON.stringify(systemInfo),
-            lastFailureTimestamp: systemInfoStatus ? statSys.lastFailureTimestamp : currentTime
-          },
-          {where: {processName: 'SystemInfoStat'}
-          })
+  })
+  if (!created) {
+    await stat.update(
+      {
+        latestCheckTimestamp: currentTime,
+        latestHealthStatus: isPasswordExists ? overallStat[0] : false,
+        additionalInfo: overallStat[1].toString(),
+        lastFailureTimestamp: overallStat[0] ? stat.lastFailureTimestamp : currentTime
+      },
+      {
+        where: { processName: 'HealthStat' }
+      }
+    );
+  }
+  let [statSys, createdSys] = await models.CurrentHealth.findOrCreate({
+    where: { processName: 'SystemInfoStat' }, defaults: {
+      latestHealthStatus: isPasswordExists ? systemInfoStatus : false,
+      latestCheckTimestamp: currentTime,
+      additionalInfo: JSON.stringify(systemInfo),
+      lastFailureTimestamp: currentTime  // default first time marked as failure
     }
-    return
+  })
+  if (!createdSys) {
+    await statSys.update(
+      {
+        latestCheckTimestamp: currentTime,
+        latestHealthStatus: isPasswordExists ? systemInfoStatus : false,
+        additionalInfo: JSON.stringify(systemInfo),
+        lastFailureTimestamp: systemInfoStatus ? statSys.lastFailureTimestamp : currentTime
+      },
+      {
+        where: { processName: 'SystemInfoStat' }
+      })
+  }
+  return
 }
 
 function formatPromethusTimestamp(timestamp) {
-    return ( timestamp.toString().split('.')[0])
+  return (timestamp.toString().split('.')[0])
 }
 
 async function checkLatest() {
@@ -221,13 +241,13 @@ async function checkSystemInfo() {
     }
     // FIXME: the callback function is async and the response may come late and be unprocessed!
     // Why checking the disk space with both - `diskspace` and `si` ?? Can we fully remove this faulty `diskspace` code block?
-    disk.check('/', function(err, info) {
+    disk.check('/', function (err, info) {
       if (err) {
         winston.warn("Error when checking for disk usage", err);
         isHealthy = false;
         additional_info.push("Could not check disk space")
       } else {
-        const diskUsageRatio = info.free / info.total *100;
+        const diskUsageRatio = info.free / info.total * 100;
         sysInfoCollected.disk_usage = diskUsageRatio;
         if (diskUsageRatio < config.healthCheck.diskUsageBound) {
           isHealthy = false;
@@ -282,8 +302,8 @@ async function checkSystemInfo() {
 }
 
 module.exports = {
-    updateCurrentHealth,
-    updateHealthStat,
-    compareTimeStamp,
-    neededJobs
+  updateCurrentHealth,
+  updateHealthStat,
+  compareTimeStamp,
+  neededJobs
 }
