a60c087afa5c609c75ebb955c20ecce29dcede55
Tanuj Soni <tanuj@blockapps.net>
2020-03-13 19:54:23 +0530

    [STRATO-1766][WIP] Beanstalk test case

--- tests-oauth/load/stratoLoadBeanstalk.test.js
+++ tests-oauth/load/stratoLoadBeanstalk.test.js
@@ -1,9 +1,31 @@
-import { assert } from 'blockapps-rest';
+import '../loadEnv';
+import config from '../loadConfig';
+import { rest, importer } from 'blockapps-rest';
+const { createUser, createContract } = rest;
+
+const user1Credentials = { token: process.env.USER1 };
 
 describe('Strato Load Test (beanstalk)', async () => {
 
-  it('Upload contracts', async () => {
+  const options = { config };
+  let user1;
 
+  before(async () => {
+    console.log('creating user')
+    user1 = await createUser(user1Credentials, options);
+  })
+
+  it('Upload contracts', async () => {
+    const contractArgs = {
+      name: `Agreement`,
+      source: await importer.combine('./contracts/beanstalk/agreement/Agreement.sol'),
+      // source: await importer.combine('./contracts/beanstalk/PermissionManager.sol'),
+      args: {}
+    };
+
+    console.log("-------------------", contractArgs)
+
+    const contract = await createContract(user1, contractArgs, options)
   });
 
 });
