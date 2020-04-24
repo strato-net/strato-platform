d6396ab94176f4c83735e346376f7d4e7f0eb3e9
dustinnorwood <dnorwood2010@aol.com>
2020-04-24 10:27:14 -0400

    Add caching in Vault Wrapper

--- blockapps-haskell/vault-wrapper/server/src/Strato/Strato23/Server/Signature.hs
+++ blockapps-haskell/vault-wrapper/server/src/Strato/Strato23/Server/Signature.hs
@@ -5,26 +5,36 @@
 module Strato.Strato23.Server.Signature where
 
 import           BlockApps.Ethereum
+import           Control.Monad.Reader             (asks)
 import           Crypto.HaskoinShim
+import qualified Data.Cache                       as Cache
 import           Data.Text                        (Text)
 import           Strato.Strato23.Monad
 import           Strato.Strato23.API.Types
 import           Strato.Strato23.Crypto
 import           Strato.Strato23.Database.Queries (getUserKeyQuery)
 import           Strato.Strato23.Server.Key       (postKey)
+import           UnliftIO
 
 postSignature :: Text -> UserData -> VaultM SignatureDetails
 postSignature userName (UserData (Hex msgHash)) = do
-  mpk <- vaultTransaction
-        . vaultQueryMaybe
-        $ getUserKeyQuery userName
-  (salt,nonce,pKey,(_ :: Address)) <- case mpk of
-    Just pk -> return pk
+  cache <- asks keyStoreCache
+  cachedPk <- liftIO $ Cache.lookup cache userName
+  (salt,nonce,pKey,_) <- case cachedPk of
+    Just (KeyStore a b c d) -> pure (a,b,c,d)
     Nothing -> do
-      _ <- postKey userName
-      vaultTransaction
-        . vaultQuery1
-        $ getUserKeyQuery userName
+      mpk <- vaultTransaction
+           . vaultQueryMaybe
+           $ getUserKeyQuery userName
+      (a,b,c,d) <- case mpk of
+        Just pk -> return pk
+        Nothing -> do
+          _ <- postKey userName
+          vaultTransaction
+            . vaultQuery1
+            $ getUserKeyQuery userName
+      liftIO . Cache.insert cache userName $ KeyStore a b c d
+      pure (a,b,c,d)
   withPassword $ \pw -> case decryptSecKey pw salt nonce pKey of
     Nothing -> vaultWrapperError IncorrectPasswordError
     Just prvKey -> case msg msgHash of
