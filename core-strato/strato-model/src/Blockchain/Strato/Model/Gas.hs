273d3f47cacce8546543b4bcd1d9982bea41ebdb
Jamshid <jamshidnh@gmail.com>
2020-04-25 17:01:09 -0400

    Adding class instances to Gas (from bloc Gas) in anticipation of replacing bloc Gas with strato-model Gas

--- core-strato/strato-model/src/Blockchain/Strato/Model/Gas.hs
+++ core-strato/strato-model/src/Blockchain/Strato/Model/Gas.hs
@@ -1,7 +1,14 @@
+{-# LANGUAGE OverloadedStrings     #-}
+
 module Blockchain.Strato.Model.Gas where
 
+import           Control.Lens.Operators
 import           Control.DeepSeq (NFData)
+import           Data.Aeson             hiding (Array, String)
+import           Data.RLP
+import           Data.Swagger
 import           GHC.Generics
+import           Test.QuickCheck        hiding ((.&.))
 
 newtype Gas = Gas Integer
   deriving newtype Num
@@ -9,3 +16,30 @@ newtype Gas = Gas Integer
   deriving newtype Real
   deriving anyclass (NFData)
   deriving (Show, Read, Enum, Eq, Ord, Generic)
+
+instance Arbitrary Gas where arbitrary = Gas <$> arbitrary
+
+instance ToJSON Gas where
+  toJSON (Gas g) = toJSON g
+
+instance FromJSON Gas where
+  parseJSON = fmap Gas . parseJSON
+
+instance ToParamSchema Gas where
+  toParamSchema _ = mempty
+    & type_ .~ SwaggerInteger
+    & minimum_ ?~ 0
+    & maximum_ ?~ (2^(256 :: Integer) - 1)
+    & format ?~ "hex string"
+
+instance ToSchema Gas where
+  declareNamedSchema _ = return $
+    NamedSchema (Just "Gas")
+      ( mempty
+        & type_ .~ SwaggerInteger
+        & example ?~ toJSON (Gas 1000)
+        & description ?~ "Number of Gas units" )
+
+instance RLPEncodable Gas where
+  rlpEncode (Gas n) = rlpEncode n
+  rlpDecode obj = Gas <$> rlpDecode obj
