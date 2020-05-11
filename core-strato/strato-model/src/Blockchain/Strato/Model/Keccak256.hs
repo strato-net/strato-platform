e5a1114a726a2912ef5f62d28c8f2c6185bdd15b
Jamshid <jamshidnh@gmail.com>
2020-05-11 17:18:34 -0400

    Removing some commented out code.

--- core-strato/strato-model/src/Blockchain/Strato/Model/Keccak256.hs
+++ core-strato/strato-model/src/Blockchain/Strato/Model/Keccak256.hs
@@ -67,9 +67,6 @@ instance NFData Keccak256
 keccak256ToWord256 :: Keccak256 -> Word256
 keccak256ToWord256 (Keccak256 val) = bytesToWord256 val 
 
---shaToByteString :: SHA -> ByteString
---shaToByteString (SHA val) = val
-
 keccak256ToByteString :: Keccak256 -> ByteString
 keccak256ToByteString (Keccak256 val) = val
 
@@ -78,11 +75,6 @@ unsafeCreateKeccak256FromByteString = Keccak256
 
 unsafeCreateKeccak256FromWord256 :: Word256 -> Keccak256
 unsafeCreateKeccak256FromWord256 = Keccak256 . word256ToBytes
-{-
-unsafeCreateKeccak256FromByteString :: ByteString -> Keccak256
-unsafeCreateKeccak256FromByteString = Keccak256
--}
-
 
 
 instance Binary Keccak256 where
@@ -138,6 +130,13 @@ keccak256ToHex (Keccak256 sha) = BC.unpack $ B16.encode sha
 -- todo: this shouldn't be partial... ever...
 keccak256FromHex :: String -> Keccak256
 keccak256FromHex = Keccak256 . fst . B16.decode . BC.pack
+
+stringKeccak256 :: String -> Maybe Keccak256
+stringKeccak256 string =
+  case B16.decode $ BC.pack string of
+    (x, "") -> Just $ Keccak256 x
+    _ -> Nothing
+
 
 formatKeccak256WithoutColor :: Keccak256 -> String
 formatKeccak256WithoutColor s
@@ -206,7 +205,6 @@ instance ToForm Keccak256 where
 instance FromForm Keccak256 where
   fromForm = parseUnique "hash"
 
-
 instance Arbitrary Keccak256 where
     arbitrary = do
         random256Bit <- fastRandBs 32
@@ -233,177 +231,3 @@ instance ToSchema Keccak256 where
 blockstanbulMixHash :: Keccak256
 blockstanbulMixHash = unsafeCreateKeccak256FromWord256 0x63746963616c2062797a616e74696e65206661756c7420746f6c6572616e6365
 
-
-
-
-{-
-{-# OPTIONS_GHC -fno-warn-orphans  #-}
-{-# LANGUAGE DataKinds             #-}
-{-# LANGUAGE FlexibleContexts      #-}
-{-# LANGUAGE FlexibleInstances     #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE OverloadedLists       #-}
-{-# LANGUAGE OverloadedStrings     #-}
-{-# LANGUAGE RecordWildCards       #-}
-{-# LANGUAGE TypeApplications      #-}
-
--- {-# OPTIONS_GHC -fno-warn-unused-imports  #-}
--- {-# OPTIONS_GHC -fno-warn-unused-top-binds  #-}
-
-
-module Blockchain.Strato.Model.Keccak256 (
-  Keccak256,
-  byteStringKeccak256,
-  keccak256,
-  keccak256ByteString,
-  keccak256lazy,
-  keccak256SHA,
-  keccak256String,
-  shaKeccak256,
-  stringKeccak256
-  ) where
-
---import           ClassyPrelude ((<>), Hashable(hashWithSalt))
-import           Control.DeepSeq (NFData)
-import           Control.Lens.Operators
-import           Control.Monad          ((<=<))
-import           Crypto.Hash
-import qualified Data.Aeson             as Aeson
-import           Data.Aeson
-import qualified Data.Aeson.Encoding    as AesonEnc
-import qualified Data.Binary            as Binary
-import qualified Data.ByteArray         as ByteArray
-import           Data.ByteString        (ByteString)
-import qualified Data.ByteString        as ByteString
-import qualified Data.ByteString.Base16 as Base16
-import qualified Data.ByteString.Char8  as Char8
-import qualified Data.ByteString.Lazy   as Lazy
-import           Data.Maybe
-import           Data.RLP
-import           Data.Swagger
-import qualified Data.Text              as Text
-import           GHC.Generics
-import           Servant.API
-import           Servant.Docs
-import           Test.QuickCheck        hiding ((.&.))
-import           Web.FormUrlEncoded     hiding (fieldLabelModifier)
-
-import           Blockchain.Strato.Model.SHA    hiding (hash)
-
-newtype Keccak256 = Keccak256 { digestKeccak256 :: Digest Keccak_256 }
-  deriving (Eq,Ord,Show,Generic)
-  deriving anyclass (NFData)
-
-keccak256ByteString :: Keccak256 -> ByteString
-keccak256ByteString = ByteArray.convert . digestKeccak256
-
-byteStringKeccak256 :: ByteString -> Maybe Keccak256
-byteStringKeccak256 = fmap Keccak256 . digestFromByteString
-
-keccak256String :: Keccak256 -> String
-keccak256String (Keccak256 digest) = show digest
-
-stringKeccak256 :: String -> Maybe Keccak256
-stringKeccak256 string =
-  if ByteString.null r then Keccak256 <$> digestFromByteString bs else Nothing
-  where
-    (bs, r) = Base16.decode $ Char8.pack string
-
-instance RLPEncodable Keccak256 where
-  rlpEncode = rlpEncode . keccak256ByteString
-  rlpDecode = maybe (Left "RLPEncodable.Keccak256: Could not decode") Right . byteStringKeccak256 <=< rlpDecode
-{-
-instance Hashable Keccak256 where
-  hashWithSalt salt = hashWithSalt salt . keccak256SHA
--}
-
-instance ToJSON Keccak256 where toJSON = toJSON . keccak256String
-
-instance FromJSON Keccak256 where
-  parseJSON value = do
-    string <- parseJSON value
-    case stringKeccak256 string of
-      Nothing      -> fail $ "Could not decode Keccak256: " <> string
-      Just hash256 -> return hash256
-
-instance ToJSONKey Keccak256 where
-    toJSONKey = ToJSONKeyText f f'
-        where f k = let (Aeson.String s) = toJSON k in s
-              f'  = AesonEnc.text . f
-              
-instance FromJSONKey Keccak256 where
-    fromJSONKey = FromJSONKeyTextParser (parseJSON . Aeson.String)
-
-instance ToHttpApiData Keccak256 where
-  toUrlPiece = Text.pack . keccak256String
-
-instance FromHttpApiData Keccak256 where
-  parseUrlPiece text = case stringKeccak256 (Text.unpack text) of
-    Nothing      -> Left $ "Could not decode Keccak256: " <> text
-    Just hash256 -> Right hash256
-
-instance ToForm Keccak256 where
-  toForm hash256 = [("hash", toQueryParam hash256)]
-
-instance FromForm Keccak256 where fromForm = parseUnique "hash"
-
-instance MimeUnrender PlainText Keccak256 where
-  mimeUnrender _ = maybe (Left "Couldn't read Keccak") Right . stringKeccak256 . Char8.unpack . Lazy.toStrict
-{-
-instance MimeRender PlainText Keccak256 where
-  mimeRender _ = Lazy.fromStrict . Char8.pack . keccak256String
--}
-instance MimeRender PlainText [Keccak256] where
-  mimeRender _ = encode
-
-instance MimeUnrender PlainText [Keccak256] where
-  mimeUnrender _ = maybe (Left "Couldn't decode [Keccak256]") Right . decode
-
-instance Arbitrary Keccak256 where
-  arbitrary = keccak256lazy . Binary.encode @ Integer <$> arbitrary
-
-instance ToCapture (Capture "hash" Keccak256) where
-  toCapture _ = DocCapture "hash" "a transaction hash"
-
-instance ToParamSchema Keccak256 where
-  toParamSchema _ = mempty & type_ .~ SwaggerString
-
-
-instance ToSample Keccak256 where
-  toSamples _ =
-    samples [keccak256lazy (Binary.encode @ Integer n) | n <- [1..10]]
-
-instance ToSchema Keccak256 where
-  declareNamedSchema _ = return $
-    NamedSchema (Just "Keccak256 hash, 32 byte hex encoded string")
-      ( mempty
-        & type_ .~ SwaggerString
-        & example ?~ toJSON (keccak256lazy (Binary.encode @ Integer 1))
-        & description ?~ "Keccak256 hash, 32 byte hex encoded string" )
-
-
-
-keccak256 :: ByteString -> Keccak256
-keccak256 = Keccak256 . hash
-
-keccak256lazy :: Lazy.ByteString -> Keccak256
-keccak256lazy = Keccak256 . hashlazy
-
-
-keccak256SHA :: Keccak256 -> SHA
-keccak256SHA = unsafeCreateSHAFromByteString . ByteArray.convert . digestKeccak256
-
-shaKeccak256 :: SHA -> Keccak256
-shaKeccak256 hsh = Keccak256
-                   . fromMaybe (error $ "internal error: shaKeccak256" ++ show hsh)
-                   . digestFromByteString
-                   $ shaToByteString hsh
--}
-
-
-stringKeccak256 :: String -> Maybe Keccak256
-stringKeccak256 string =
-  case B16.decode $ BC.pack string of
-    (x, "") -> Just $ Keccak256 x
-    _ -> Nothing
