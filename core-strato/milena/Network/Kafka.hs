5d20d2f71707e426bcc864064e8543428a4d2a83
Jamshid <jim@blockapps>
2020-05-25 04:12:43 +0000

    Moving our milena closer to milena 0.5.4.0

--- core-strato/milena/Network/Kafka.hs
+++ core-strato/milena/Network/Kafka.hs
@@ -1,7 +1,6 @@
 module Network.Kafka where
 
 import Control.Applicative
-import Control.DeepSeq
 import Control.Exception (Exception, IOException)
 import Control.Exception.Lifted (catch)
 import Control.Lens
@@ -52,9 +51,6 @@ data KafkaState = KafkaState { -- | Name to use as a client ID.
                              } deriving (Generic, Show)
 
 makeLenses ''KafkaState
-
-instance NFData KafkaState where
-  rnf ks = ks `seq` () -- TODO(tim): Supply a real deepseq
 
 -- | The core Kafka monad.
 type Kafka m = (MonadState KafkaState m, MonadError KafkaClientError m, MonadIO m, MonadBaseControl IO m)
