42101b1ac10b29c878906b20596df525d249b2af
Zach <zyuzon24@gmail.com>
2020-09-05 12:14:16 -0400

    Fix documentation for util.js

--- blockapps-rest/lib/util/util.js
+++ blockapps-rest/lib/util/util.js
@@ -1,5 +1,10 @@
 import path from 'path'
 
+/**
+ * This is the util interface
+ * @module util
+ */
+
 // odds
 const cwd = path.resolve(process.cwd())
 
@@ -7,7 +12,16 @@ const cwd = path.resolve(process.cwd())
  * @see https://github.com/ethereum/go-ethereum/blob/aa9fff3e68b1def0a9a22009c233150bf9ba481f/jsre/ethereum_js.go
  *
  * Checks if the given string is an address
- *
+ * @example
+ * const simpleStorageSrc = fsUtil.get("SimpleStorage.sol");
+ * const contractArgs = {
+ *   name: "SimpleStorage",
+ *   source: simpleStorageSrc,
+ *   args: {} // Any constructor args would go here. We dont have any.
+ * };
+ * const result = await rest.createContract(stratoUser, contractArgs, options);
+ * // Returns
+ * // true
  * @method isAddress
  * @param {String} address the given HEX adress
  * @return {Boolean}
@@ -31,7 +45,6 @@ function isHash(hash) {
 
 /**
  * Checks if the given string is a checksummed address
- *
  * @method isChecksumAddress
  * @param {String} address the given HEX adress
  * @return {Boolean}
@@ -51,6 +64,10 @@ function isChecksumAddress(_address) {
 
 /**
  * Generates a unique integer id
+ * @example
+ * util.iuid()
+ * // Returns
+ * // 654657412219
  * @method iuid
  * @return {Int}
  */
@@ -64,7 +81,13 @@ function sha3(address) {
 
 /**
  * Generates a unique id
+ * @example
+ * util.uid()
+ * // Returns
+ * // 100549
  * @method uid
+ * @param prefix Optional. The prefix of the unique id to be generated
+ * @param digits Optional. The number of digits of the unique id to be generated. Defaults to 6.
  * @return {Int}
  */
 function uid(prefix, digits = 6) {
@@ -76,9 +99,13 @@ function uid(prefix, digits = 6) {
 
 /**
  * Formats an object's properties
+ * @example
+ * util.usc({ size: 10, length: 5 })
+ * // Returns
+ * // { _size: 10, _length: 5 }
  * @method usc
  * @param {*} Object An object with properties
- * @return {Array}
+ * @return {Object}
  */
 function usc(args) {
   return Object.keys(args).reduce((acc, key) => {
@@ -114,10 +141,38 @@ async function sleep(milli) {
 
 /**
  * Perform action (a promise) until predicate evaluates to true
+ * @example
+ * // Consider the following function in the rest module
+ * // async function searchUntil(user, contract, predicate, options) {
+ * //  const action = async o => {
+ * //    return search(user, contract, o);
+ * //   };
+ * //
+ * //   const results = await util.until(predicate, action, options);
+ * //   return results;
+ * // }
+ *
+ * // predicate is created: to wait until response is available otherwise throws the error
+ * function predicate(data) {};
+ * const result = await rest.searchUntil(stratoUser, {name: "SimpleStorage"}, predicate, queryOptions);
+ * // Returns
+ * // [ { address: '60fb089dc62858df014819d618aa3f43391ddb9c',
+ * //     chainId: '',
+ * //     block_hash:
+ * //      'fb7edc20a2678ca60024f81d926d1637eb418012beae2fedb7e7c4250406ea82',
+ * //     block_timestamp: '2020-08-04 20:48:18 UTC',
+ * //     block_number: '3',
+ * //     transaction_hash:
+ * //      '32321367696882da2feb9483fa31346f68abf77cc1a01ace6ef5a2b774bc8d38',
+ * //     transaction_sender: '594f19ad4a55d6434711a51f628f22abf4afc55a',
+ * //     transaction_function_name: '',
+ * //     storedData: 10 } ]
+ * @method until
  * @param {*} predicate The function that evaluates to a boolean.
  * @param {*} action A promise to invoke. Could be a network request.
  * @param {*} options Ba-rest options
  * @param {*} timeout The length of time to wait before giving up on the predicate
+ * @return {Object}
  */
 
 async function until(predicate, action, options, timeout = 60000) {
@@ -142,6 +197,7 @@ function timeout(ms) {
 
 /**
  * Check for intersection
+ * @method filter_isContained
  * @param {*} setA A list
  * @param {*} setB Another list
  * @param {*} comparator Comparator to compare elements of setA and setB
@@ -163,7 +219,13 @@ function timeout(ms) {
 
 /**
  * Convert an array to a comma separated string
+ * @example
+ * util.toCsv([1, 2, 3, 4])
+ * // Returns
+ * // 1,2,3,4
+ * @method toCsv
  * @param {*} array List to convert to csv value
+ * @return {String}
  */
 function toCsv(array) {
   return array.reduce(function (prevVal, elem) {
