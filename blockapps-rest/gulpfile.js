0ee8ec9c88f35728622d0f74f581912e11f1e43b
Jamshid <jamshidnh@gmail.com>
2020-10-19 15:48:15 -0400

    Changing the build configuration to compile typescript

--- blockapps-rest/gulpfile.js
+++ blockapps-rest/gulpfile.js
@@ -1,20 +1,44 @@
 const { dest, series, src } = require('gulp');
+const ts = require("gulp-typescript");
 const babel = require('gulp-babel');
 const gulpClean = require('gulp-clean');
 const sourcemaps = require('gulp-sourcemaps');
+const merge = require("merge-stream");
 
 function clean() {
-  return src('dist', { read: false, allowEmpty: true }).pipe(gulpClean());
+    return merge([
+	src('dist', { read: false, allowEmpty: true }).pipe(gulpClean()),
+	src('lib', { read: false, allowEmpty: true }).pipe(gulpClean()),
+    ]);
 }
 
-function build() {
-  return src(['lib/**/*.js', '!**/test/', '!**/test/**'])
-    .pipe(sourcemaps.init())
-    .pipe(babel({ presets: ['@babel/preset-env', 'minify'] }))
-    .pipe(sourcemaps.write('.', { sourceRoot: '/lib' }))
-    .pipe(dest('dist'));
+var tsProject = ts.createProject("tsconfig.json");
+
+function compile() {
+    var tsResult = tsProject.src().pipe(tsProject());
+    return merge([
+	tsResult.js.pipe(dest("lib")),
+	tsResult.dts.pipe(dest("lib"))
+    ]);
 }
+
+function generate() {
+    return merge([
+	
+	src(['lib/**/*.js', '!**/test/', '!**/test/**'])
+	    .pipe(sourcemaps.init())
+	    .pipe(babel({ presets: ['@babel/preset-env', 'minify'] }))
+	    .pipe(sourcemaps.write('.', { sourceRoot: '/lib' }))
+	    .pipe(dest('dist')),
+	
+	src('./lib/**/*.d.ts')
+	    .pipe(dest('./dist/'))
+    ]);
+}
+
 
 exports.clean = clean;
-exports.build = build;
-exports.default = series(clean, build);
+exports.compile = compile;
+exports.generate = generate;
+exports.build = series(compile, generate);
+exports.default = series(clean, compile, generate);
