56222f62a697eb3d94f841c55c05f920ae5c0395
Jamshid <jamshidnh@gmail.com>
2020-10-23 15:16:30 -0400

    Adding a file with some types needed by the API

---
+++ blockapps-rest/src/types.ts
@@ -0,0 +1,45 @@
+
+import { OAuthConfig } from "./util/oauth.util";
+
+interface Options {
+  config: Config,
+  node?:number,
+  headers?:any,
+  params?:any,
+  getFullResponse?:boolean,
+  cacheNonce?:any,
+  isAsync?: boolean,
+  isDetailed?: boolean,
+  stateQuery?: any,
+  query?: any,
+  logger?:Console,
+  chainIds?:any,
+  history?:any
+}
+
+interface Config {
+  nodes:Node[],
+  apiDebug:boolean,
+  VM:string
+}  
+
+interface Node {
+  url:string,
+  oauth:OAuthConfig
+}
+
+interface StratoUser {
+  password:string,
+  username:string,
+}
+
+interface BlockChainUser {
+  address:string
+}
+
+interface OAuthUser {
+  token:string
+}
+
+
+export { Options, StratoUser, OAuthUser, BlockChainUser };
