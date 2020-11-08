78f19fea199ef623a76e1ada13350fe37533da0a
Jamshid <jamshidnh@gmail.com>
2020-11-08 18:48:47 -0500

    updating stratoLoad.test.ts to the newest blockapps-rest

--- tests/load/stratoLoad.test.ts
+++ tests/load/stratoLoad.test.ts
@@ -1,24 +1,38 @@
 'use strict'
 
-const ba = require('blockapps-rest');
-const BigNumber = require('bignumber.js');
-const co = require('co');
-require('co-mocha');
-const rest = ba.rest;
-const common = ba.common;
-const config = common.config;
-const util = common.util;
-const api = common.api;
-const moment = require('moment');
-const path = require('path');
+import * as path from 'path';
+import * as moment from "moment";
 
-const adminName = util.uid('Admin');
-const adminPassword = '1234';
+import {
+  OAuthUser,
+  BlockChainUser,
+  Options,
+  Contract,
+  Config,
+  rest,
+  util,
+  fsUtil,
+  oauthUtil,
+  assert,
+  constants,
+  importer,
+  ContractDefinition
+  } from 'blockapps-rest';
+
+import BigNumber from "bignumber.js";
+import * as chai from "chai";
+chai.should();
+chai.use(require('chai-bignumber')());
+
+const u = require("util");
+
+let config:Config = fsUtil.getYaml("config.yaml");
+let options:Options = {config};
 
 const contractName = 'Vehicle';
 const contractFilename = path.join(config.contractsPath,"Vehicle.sol");
 
-let txs = [];
+let txs:ContractDefinition[] = [];
 let txResults = [];
 
 describe('Strato Load Test', function() {
@@ -31,66 +45,67 @@ describe('Strato Load Test', function() {
   const batchDelay = util.getArgInt('--batchDelay', 0);
 
 
-  before(function * () {
+  before(async() => {
     console.log(`Creating admin user and contract`);
-    admin = yield rest.createUser(adminName, adminPassword);
+    const oauth:oauthUtil = oauthUtil.init(config.nodes[0].oauth);
+    let ouser:OAuthUser = await oauth.getAccessTokenByClientSecret();
+    admin = await rest.createUser(ouser, options);
     console.log(`User: ${admin.name} @ ${admin.address}`);
-    yield rest.compileSearch([contractName], contractName, contractFilename);
+//    await rest.createContract(admin, {name: contractName, source: await importer.combine(contractFilename), args: {}}, options);
     let balance = new BigNumber(0);
-    while (balance.isZero()) {
-      yield promiseTimeout(500);
-      balance = yield rest.getBalance(admin.address);
-      console.log(`Balance is: ${balance}`);
-    }
+//    while (balance.isZero()) {
+//      await promiseTimeout(500);
+//      const accounts = await rest.getAccounts(admin, {...options, params: {address: admin.address}})
+//      balance = accounts[0].balance;
+
+//      console.log(`Balance is: ${balance}`);
+//    }
   });
 
-  it('Upload contracts', function * () {
+  it('Upload contracts', async() => {
     const startTime = moment();
     let blocTime = 0;
     for(let i = 0; i < batchCount; i++) {
       console.log(`Creating ${batchSize} transactions for count ${i}`);
-      factory_createUploadList(batchSize, i);
+      await factory_createUploadList(batchSize, i);
       const blocStartTime = moment();
-      const results = yield api.bloc.uploadList({
-        password: adminPassword,
-        contracts: txs.slice(batchSize * i, batchSize * i + batchSize),
-        resolve: false
-      }, admin.name, admin.address, false);
+      const results = await rest.createContractList(admin,
+      	    txs.slice(batchSize * i, batchSize * i + batchSize), {...options, isAsync: true, config: {...options.config, VM: "SolidVM"}});
       const blocEndTime = moment();
       blocTime += blocEndTime.diff(blocStartTime, 'seconds');
       console.log(`Received ${results.length} receipts`);
       txResults = txResults.concat(results);
       if(batchDelay > 100) {
-        yield promiseTimeout(batchDelay);
+        await promiseTimeout(batchDelay);
       }
     }
-
-    const lastHash = txResults[txResults.length -1].hash;
+//    const lastHash = txResults[txResults.length -1].hash;
 
     console.log(`Waiting on address '${admin.address}' to reach nonce ${batchSize*batchCount}`);
-    yield waitResult(admin.address, batchSize, batchCount);
+    await waitResult(admin, admin.address, batchSize, batchCount);
 
     const endTime = moment();
     const seconds = endTime.diff(startTime, 'seconds');
     console.log(`Total seconds: ${seconds}, Bloc Submission Time: ${blocTime}  TPS ${batchSize * batchCount/seconds}`);
-
   });
 
 });
 
-function * waitResult(address, batchSize, batchCount) {
+async function waitResult(admin, address, batchSize, batchCount) {
   let nonce = 0;
-  while(nonce < batchSize*batchCount) {
-    yield promiseTimeout(500);
+    while(nonce < batchSize*batchCount) {
+    await promiseTimeout(500);
     try {
       console.log(`Current Nonce is: ${nonce}`)
-      let result = yield api.strato.account(address);
+      console.log("waiting for : " + batchSize*batchCount)
+      let result = await rest.getAccounts(admin, {...options, params: {address}})
       console.log(`Result: ${JSON.stringify(result)}`);
       nonce = result[0].nonce;
     } catch (e) {
       console.error(e);
     }
   }
+  console.log("final nonce = " + nonce);
 }
 
 function promiseTimeout(timeout) {
@@ -101,16 +116,12 @@ function promiseTimeout(timeout) {
   });
 }
 
-function isObject(val) {
-  if (val === null) { return false;}
-  return ( (typeof val === 'function') || (typeof val === 'object') );
-}
-
-function factory_createUploadList(batchSize, batchIndex) {
-  for (var i = 0; i < batchSize; i++) {
+async function factory_createUploadList(batchSize, batchIndex):Promise<ContractDefinition[]> {
+  for (let i = 0; i < batchSize; i++) {
     // function Vehicle(string _vin, string _s0, string _s1, string _s2, string _s3) public
     txs.push({
-      contractName: contractName,
+      name: contractName,
+      source: await importer.combine(contractFilename),
       args: {
         _vin: `vin_${batchIndex}_${i}`,
         _s0: `s0_${batchIndex}_${i}`,
@@ -122,8 +133,7 @@ function factory_createUploadList(batchSize, batchIndex) {
         gasLimit: 10000000,
         gasPrice: 1,
         nonce: batchSize * batchIndex + i
-      },
-      metadata: {"VM": "SolidVM"}
+      }
     });
   }
   return txs;
