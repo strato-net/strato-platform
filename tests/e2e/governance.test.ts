45db81574170e22822f6f140da862d05a7925b83
Jamshid <jamshidnh@gmail.com>
2020-11-11 01:00:14 -0500

    updating governance.test.ts

--- tests/e2e/governance.test.ts
+++ tests/e2e/governance.test.ts
@@ -1,16 +1,25 @@
-const ba = require('blockapps-rest');
-require('co-mocha');
+import * as path from "path";
 
-const rest = ba.rest;
-const common = ba.common;
-const api = common.api;
-const util = common.util;
-const BigNumber = common.BigNumber;
-const constants = common.constants;
-const assert = common.assert;
-const config = common.config;
+import {
+  OAuthUser,
+  BlockChainUser,
+  Options,
+  Config,
+  rest,
+  util,
+  fsUtil,
+  oauthUtil,
+  assert,
+  constants
+  } from 'blockapps-rest';
 
-const password = '1234';
+import BigNumber from "bignumber.js";
+import * as chai from "chai";
+chai.should();
+chai.use(require('chai-bignumber')());
+
+let config:Config=fsUtil.getYaml("config.yaml");
+let options:Options={config}
 
 const label = 'My chain label';
 const src = 'contract Governance { enum Rule { NOTHING, AUTO_APPROVE, TWO_VOTES_IN, MAJORITY_RULES } event MemberAdded(address member, string enode); event MemberRemoved(address member); function voteToAdd(address m, string e) { MemberAdded(m,e); } function voteToRemove(address m) { MemberRemoved(m); } }';
@@ -29,23 +38,27 @@ const balances = [
            { address: "0000000000000000000000000000000012345678"
            , balance: 0
            }];
+
+let oauth:oauthUtil;
+let ouser1:OAuthUser;
+let ouser2:OAuthUser;
+
 var alicename;
 var bobname;
-var alice;
-var bob;
+var alice:BlockChainUser;
+var bob:BlockChainUser;
 var chainId;
 
 describe("Create Chain", function() {
+  this.timeout(config.timeout);
 
-  before(function * () {
-    this.timeout(config.timeout);
-    const uid = util.uid();
-    alicename = 'Alice' + uid;
-    bobname = 'Bob' + uid;
+  before(async() => {
     // create user
-    const isAsync = true;
-    alice = yield rest.createUser(alicename, password, isAsync);
-    bob   = yield rest.createUser(bobname, password, isAsync);
+    oauth = oauthUtil.init(config.nodes[0].oauth);
+    ouser1 = await oauth.getAccessTokenByResourceOwnerCredential("user1", "1234", "strato-devel");
+    ouser2 = await oauth.getAccessTokenByResourceOwnerCredential("user2", "1234", "strato-devel");
+    alice = await rest.createUser(ouser1, options);
+    bob = await rest.createUser(ouser2, options);
 
     const bals = [{ address: alice.address, balance: 1000000000000000000000}
                  ,{ address: bob.address, balance: 0}
@@ -53,31 +66,29 @@ describe("Create Chain", function() {
     const mems = [{address: alice.address, enode: members[0].enode}
                  ,{address: bob.address, enode: members[1].enode}
 		 ];
-    chainId = yield rest.createChain(label, mems, bals, src, args);
-    yield promiseTimeout(1000);
-    const chainInfo = yield rest.getChainInfo(chainId);
+    chainId = await rest.createChain(alice, {label, members: mems, balances: bals, src, args}, {name: "Governance"}, options);
+    await promiseTimeout(1000);
+    const chainInfo = await rest.getChain(ouser1, chainId, options);
     console.log('###CHAININFO###', chainInfo);
   });
   
-  it('should add and remover a member from the chain', function* () {
-    this.timeout(config.timeout);
-
+  it('should add and remover a member from the chain', async() => {
     const addName = 'voteToAdd';
     const removeName = 'voteToRemove';
     const gov = { name: 'Governance', address: '0000000000000000000000000000000000000100' }
     const args = { m: '00000000000000000000000000000000deadbeef', e: 'enode://6f8a80d14311c39f35f516fa664deaaaa13e85b2f7493f37f6144d86991ec012937307647bd3b9a82abe2974e1407241d54947bbb39763a4cac9f77166ad92a0@172.16.0.6:30303?discport=30303' }
 
-    yield rest.callMethod(alice, gov, addName, args, 0, false, chainId);
+    await rest.call(alice, {contract: gov, method: addName, args, chainid: chainId}, options);
 
-    const chainInfo1 = yield rest.getChainInfo(chainId);
+    const chainInfo1 = await rest.getChain(ouser1, chainId, options);
     console.log('###CHAININFO###',chainInfo1);
-    assert.deepEqual(chainInfo1.members.length, 3, "member should be added");
+    assert.deepEqual(chainInfo1.info.members.length, 3, "member should be added");
 
-    yield rest.callMethod(alice, gov, removeName, args, 0, false, chainId);
+    await rest.call(alice, {contract: gov, method: removeName, args, chainid: chainId}, options);
 
-    const chainInfo2 = yield rest.getChainInfo(chainId);
+    const chainInfo2 = await rest.getChain(ouser1, chainId, options);
     console.log('###CHAININFO###',chainInfo2);
-    assert.deepEqual(chainInfo2.members.length, 2, "member should be removed");
+    assert.deepEqual(chainInfo2.info.members.length, 2, "member should be removed");
 
   });
 
