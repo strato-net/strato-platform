d07d3f20525f42bd402632650ff4e156014cfa74
Jamshid <jamshidnh@gmail.com>
2020-11-25 12:39:53 -0500

    adding some calls to sleep(); to give the system time to populate the cirrus tables before querying them....  The tests had been failing sometimes because not enough time had been given.

--- tests/e2e/slipstream.test.ts
+++ tests/e2e/slipstream.test.ts
@@ -42,7 +42,9 @@ async function upload(name:string, source:string, options:Options):Promise<[Bloc
 
 
 
-describe('Slipstream', async () => {
+describe('Slipstream', function () {
+  this.timeout(config.timeout);
+  
   const stringArray = `
 contract StringArray {
   string[] xs = ["first", "second", "third"];
@@ -51,13 +53,14 @@ contract StringArray {
 
   it("can index string arrays", async () => {
     const [user, contract] = await upload("StringArray", stringArray, options);
+    await sleep(2000);
     const index = await rest.search(user, contract,
     	  {...options, query: {address: `eq.${contract.address}`}});
     console.log(`Index returned ${JSON.stringify(index, null, 2)}`);
     assert.equal(index[0].address, contract.address, "address");
     assert.deepEqual(index[0].xs, ["first", "second", "third"], "xs");
 
-  }).timeout(config.timeout);
+  });
 
   const newContract = `
 contract X {
@@ -73,12 +76,13 @@ contract Y {
 `;
   it("can index contracts recursively constructed", async () => {
     const [user, contract] = await upload("Y", newContract, options);
+    await sleep(2000);
     const indexY = await rest.search(user, {...contract, name: "Y"}, options);
     assert.equal(indexY.length, 1, JSON.stringify(indexY, null, 2));
     const indexX = await rest.search(user, {...contract, name: "X"}, options);
     console.log(`indexX returned ${JSON.stringify(indexX, null, 2)}`);
     assert.equal(indexX[0].z, "7624", "z");
-  }).timeout(config.timeout);
+  });
 
 
   const Counter = `
@@ -91,18 +95,21 @@ contract Z {
 `;
   it("Will index updates to a contract", async () => {
     const [user, contract] = await upload("Z", Counter, options);
+    await sleep(2000);
     let indexZ = await rest.search(user, {...contract, name: "Z"}, options);
     assert.equal(indexZ.length, 1, JSON.stringify(indexZ, null, 2));
     console.log(`Initial index: ${JSON.stringify(indexZ, null, 2)}`);
     let res = await rest.call(user, {contract, method: "incr", args: {}}, options);
     console.log(`Incr result 1: ${JSON.stringify(res, null, 2)}`);
+    await sleep(2000);
     indexZ = await rest.search(user, {...contract, name: "Z"}, {...options, query: {count: "eq.1"}});
     console.log(`Second index: ${JSON.stringify(indexZ, null, 2)}`);
     res = await rest.call(user, {contract, method: "incr", args: {}}, options);
     console.log(`Incr result 2: ${JSON.stringify(res, null, 2)}`);
+    await sleep(2000);
     indexZ = await rest.search(user, {...contract, name: "Z"}, {...options, query: {count: "eq.2"}});
     console.log(`Last index: ${JSON.stringify(indexZ, null, 2)}`);
-  }).timeout(config.timeout);
+  });
 
 
 const eventsContract = `
@@ -121,24 +128,33 @@ contract EventTest {
     const [user,contract] = await upload("EventTest", eventsContract, options);
     let magic = 97;
     let res = await rest.call(user, {contract, method: "emitTest", args: {magic}}, options);
+    await sleep(2000);
     res = await rest.search(user, {...contract, name: "SlipstreamTest"}, {...options, query: {magic: "eq.97"}});
     magic = 98;
     res = await rest.call(user, {contract, method: "emitTest", args: {magic}}, options);
+    await sleep(2000);
     res = await rest.search(user, {...contract, name: "SlipstreamTest"}, {...options, query: {magic: "eq.98"}});
     magic = 99;
     res = await rest.call(user, {contract, method: "emitTest", args: {magic}}, options);
+    await sleep(2000);
     res = await rest.search(user, {...contract, name: "SlipstreamTest"}, {...options, query: {magic: "eq.99"}});
    
     // insert with a different instance of the same contract (same table)
     const [user2,contract2] = await upload("EventTest", eventsContract, options);
     magic = 97;
     res = await rest.call(user2, {contract: contract2, method: "emitTest", args: {magic}}, options);
+    await sleep(2000);
     res = await rest.search(user, {...contract, name: "SlipstreamTest"}, {...options, query: {magic: "eq.97"}});
     magic = 900;
     res = await rest.call(user2, {contract: contract2, method: "emitTest", args: {magic}}, options);
+    await sleep(2000);
     res = await rest.search(user, {...contract, name: "SlipstreamTest"}, {...options, query: {magic: "eq.900"}});
-  }).timeout(config.timeout);
+  });
 
 
 
 });
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
