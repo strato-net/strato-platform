96218acd8c69ac90c577d1d52956ea78be191273
Jamshid <jamshidnh@gmail.com>
2021-02-04 18:57:50 -0500

    Adding new network info that includes the new blockappsnet peer.

--- core-strato/strato-networks/src/Blockchain/Network.hs
+++ core-strato/strato-networks/src/Blockchain/Network.hs
@@ -10,11 +10,18 @@ data PeerParams =
     }
 
 getParams :: String -> IO (Maybe [PeerParams])
-getParams "blockappsnet" = return $ Just
+getParams "oldblockappsnet" = return $ Just
   [
     NetworkParams {
       ethAddress = Address 0xeae0695468d78aa496259834855ed566e75bffcb,
       webAddress = "3.226.74.116"
+      }
+  ]
+getParams "blockappsnet" = return $ Just
+  [
+    NetworkParams {
+      ethAddress = Address 0xeae0695468d78aa496259834855ed566e75bffcb,
+      webAddress = "54.243.143.176" -- "engineering.stratoid.blockapps.net"
       }
   ]
 getParams _ = return Nothing
