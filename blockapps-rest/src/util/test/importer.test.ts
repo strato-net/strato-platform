567ede6bfa2696969ac7862c948995c4aa233a84
Dustin Norwood <dnorwood2010@aol.com>
2021-03-01 14:00:38 -0500

    Add missing function parameters so typescript will stop complaining

--- blockapps-rest/src/util/test/importer.test.ts
+++ blockapps-rest/src/util/test/importer.test.ts
@@ -20,7 +20,7 @@ describe("imports", function() {
 
   it("combines to string", async () => {
     const filename = `${fixtures}/importer/Main.sol`;
-    const source = await importer.combine(filename);
+    const source = await importer.combine(filename, false);
     assert.isAbove(source.indexOf("contract Main"), 0);
     assert.isAbove(source.indexOf("contract A"), 0);
     assert.isAbove(source.indexOf("contract B"), 0);
@@ -40,7 +40,7 @@ describe("imports", function() {
 
   it("combines to string and uploads", async () => {
     const filename = `${fixtures}/importer/Main.sol`;
-    const source = await importer.combine(filename);
+    const source = await importer.combine(filename, false);
     const name = `Main`;
     const args = util.usc({ size: 10 });
     const contractArgs = { name, source, args };
