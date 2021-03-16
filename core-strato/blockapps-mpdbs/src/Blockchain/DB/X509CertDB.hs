634569020eec447b94382c66a74f84417f1bd370
Luke Tollefson <luke.tollefson@hey.com>
2021-03-16 11:44:07 -0400

    Support actual X509 certs, not just placeholder bytestrings

--- core-strato/blockapps-mpdbs/src/Blockchain/DB/X509CertDB.hs
+++ core-strato/blockapps-mpdbs/src/Blockchain/DB/X509CertDB.hs
@@ -9,7 +9,6 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Blockchain.DB.X509CertDB (
-  X509Cert,
   X509CertDB(..),
   HasX509CertDB,
   genericLookupX509CertDB,
@@ -25,31 +24,30 @@ import           Control.Monad.Trans.Reader
 import           Data.Binary
 import qualified Data.ByteString.Lazy               as BL
 import           Data.Default
+import           Data.Either.Extra (eitherToMaybe)
 import qualified Database.LevelDB                   as DB
 import           Prelude                            hiding (lookup)
 
 import           Blockchain.Strato.Model.Address
-
-import Data.ByteString (ByteString)     -- for temporary use
-
-type X509Cert = ByteString
+import           BlockApps.X509 (X509Certificate, certToBytes, bsToCert)
 
 newtype X509CertDB = X509CertDB { unX509CertDB :: DB.DB }
 
 instance NFData X509CertDB where
   rnf (X509CertDB a) = a `seq` ()
 
-type HasX509CertDB m = (Address `Alters` X509Cert) m
+type HasX509CertDB m = (Address `Alters` X509Certificate) m
 
-genericLookupX509CertDB :: MonadIO m => m X509CertDB -> Address -> m (Maybe X509Cert)
+genericLookupX509CertDB :: MonadIO m => m X509CertDB -> Address -> m (Maybe X509Certificate)
 genericLookupX509CertDB f address = do
   db <- unX509CertDB <$> f
-  DB.get db def (BL.toStrict $ encode address)
+  maybeX509 <- DB.get db def (BL.toStrict $ encode address)
+  return $ (eitherToMaybe . bsToCert) =<< maybeX509
 
-genericInsertX509CertDB :: MonadIO m => m X509CertDB -> Address -> X509Cert -> m ()
+genericInsertX509CertDB :: MonadIO m => m X509CertDB -> Address -> X509Certificate -> m ()
 genericInsertX509CertDB f address cert = do
   db <- unX509CertDB <$> f
-  DB.put db def (BL.toStrict $ encode address) cert
+  DB.put db def (BL.toStrict $ encode address) (certToBytes cert)
 
 genericDeleteX509CertDB :: MonadIO m => m X509CertDB -> Address -> m ()
 genericDeleteX509CertDB f address = do
@@ -57,13 +55,13 @@ genericDeleteX509CertDB f address = do
   DB.delete db def (BL.toStrict $ encode address)
 
 -- instance MonadIO m => HasX509CertDB (ReaderT X509CertDB m)
-instance MonadIO m => (Address `Alters` X509Cert) (ReaderT X509CertDB m) where
+instance MonadIO m => (Address `Alters` X509Certificate) (ReaderT X509CertDB m) where
   lookup _ = genericLookupX509CertDB ask
   insert _ = genericInsertX509CertDB ask
   delete _ = genericDeleteX509CertDB ask
 
-x509CertDBPut :: HasX509CertDB m => Address -> X509Cert -> m ()
+x509CertDBPut :: HasX509CertDB m => Address -> X509Certificate -> m ()
 x509CertDBPut = insert Proxy
 
-x509CertDBGet :: HasX509CertDB m => Address -> m (Maybe X509Cert)
+x509CertDBGet :: HasX509CertDB m => Address -> m (Maybe X509Certificate)
 x509CertDBGet = lookup Proxy
