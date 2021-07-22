829f52acdb622df460b74b87b87b5217539ec0f8
ruviniruvi <ruviniruvi@gmail.com>
2021-07-22 14:50:39 -0400

    first commit

---
+++ FixedPointNumbersTest.sol
@@ -0,0 +1,17 @@
+
+contract FixedPointNumbersTest{
+
+ int256 val1;
+ int256 val2;
+ int256 val3;
+ int256 val4;
+
+
+constructor() {
+    val1 = 1+2;           
+    val2 = 1-2;          
+    val3 = 1*2;           
+    val4 = 1/1;  
+}
+
+}
