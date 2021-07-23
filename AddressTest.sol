5c25b585d18e8f0b99260115433f57023055d6af
ruviniruvi <ruviniruvi@gmail.com>
2021-07-22 21:34:22 -0400

    constructor added

--- AddressTest.sol
+++ AddressTest.sol
@@ -1,31 +1,18 @@
 contract AddressTest{
     
-    address public my_address; 
-    
-    // using address payable can send ether or vote after function executes
-    address payable public recipient_address;
-    
-    
-    constructor()  {
-         my_address = msg.sender;
+    address public my_address;
+    uint public balance_value;
+
+    constructor(address _my_address, uint _balance_value) payable {
+       my_address = _my_address;
+        balance_value = _balance_value;
+        getBalance();
+        
         
     }
 
-    function transferFund () external payable {
-        
-    }
-    
-    function getBalance () external view returns (uint) {
-        return address (this).balance;
-    }
-    
-      function etherTransfer (address payable _recipient_address) external {
-       _recipient_address.transfer(100);
-    }
-    
-
-    function etherSend (address payable _recipient_address) external {
-       _recipient_address.send(100);
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
     }
     
     
