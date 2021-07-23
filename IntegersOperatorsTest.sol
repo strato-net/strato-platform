fa173cb9dbdb8408d041f0ffd9ffa11bb4e9de59
ruviniruvi <ruviniruvi@gmail.com>
2021-07-22 21:26:49 -0400

    constructor added

--- IntegersOperatorsTest.sol
+++ IntegersOperatorsTest.sol
@@ -1,82 +1,107 @@
+
 contract IntegersOperatorsTest{
 
 int public num_1 = 1;
 int public num_2 = 2;
 
-//Arithmatic operators 
 
 
-int public addition = num_1 + num_2 ;
-int public substraction = num_2 - num_1;
-int public multiply = num_1 * num_2;
-int public division = num_2 / num_1 ;
-// unary-
 
-int public mod = num_2 % num_1;
+int public addition  ;
+int public substraction ;
+int public multiply ;
+int public division  ;
+int public mod ;
 
+int public value_1 = 5;
+bool  public value_5 ;
+bool public value_6 ;
+
+bool public isGreater ;
+bool public isLess  ;
+bool public isEqual  ;
+bool public isNotEqual ;
+
+bytes1 public byte_1 = 0x15 ;
+bytes1 public byte_2 = 0xf6;
+
+
+bytes1 public byte_value_1;
+bytes1 public byte_value_2;
+bytes1 public byte_value_3;
+bytes1 public byte_value_4;
+
+
+uint public  value_3 = 3;
+uint  public value_4 = 4 ;
+
+uint public value_7 ;
+uint public value_8 ;
+
+
+constructor( ){
+    
+ //Arithmatic operators 
+ 
+addition = num_1 + num_2; //int256: 3
+substraction = num_2 - num_1; //int256: 1
+multiply = num_1 * num_2; //int256: 2
+division = num_2 / num_1 ; //int256: 2
+mod = num_2 % num_1; //int256: 0
+
+ 
  //Comparisons
-
- function comparison_1(int value_1) public pure returns (bool){
-
-     if (value_1 >= 10 && value_1 <= 20){
-        return true;
-        }
-        return false;
- }
-
-bool public value_2 = true ;
-function comparison_2 (int value_1) public pure returns (bool){
-
-     if (value_1 > 10 || value_1 < 20){
-        return true;
-        }
-        return false;
-}
-
-//compare two values using operators > , < , == ,!=
-
-bool public isGreater = num_2 > num_1 ;
-bool public isLess = num_2 < num_1 ;
-bool public isEqual = num_2 == num_1 ;
-bool public isNotEqual = num_2 != num_1 ;
-
-//bytes1 equals to byte
+ if (value_1 >= 10 && value_1 <= 20){
+        value_5 ; }//bool: false
+        
+ if (value_1 > 10 || value_1 > 20){
+        value_6 ;  }//bool: false
+         
+        
+        
+ //2 > 1 => bool: true
+ isGreater = num_2 > num_1 ; 
+ 
+ //2 < 1 => bool: false
+ isLess = num_2 < num_1 ;
+ 
+ // 2 == 1 => bool: false
+ isEqual = num_2 == num_1 ;
+ 
+ //2 != 1 => bool: true
+ isNotEqual = num_2 != num_1 ;
+ 
+ 
+ //bytes1 equals to byte
 //Bit operator "&" (Bitwise AND) Performs boolean AND operation on each bit of integer argument
-
-function bitOperator_1(bytes1 byte_1 , bytes1 byte_2) public pure returns (bytes1){
-    return byte_1 & byte_2 ;
-}
-
-//Bit operator "|" (Bitwise OR) Performs boolean OR operation on each bit of integer argument
-
-function bitOperator_2(bytes1 byte_3 , bytes1 byte_4) public pure returns (bytes1){
-    return byte_3 | byte_4 ;
+ 
+ byte_value_1 = byte_1 & byte_2 ; //bytes1: 0x14
+ 
+ //Bit operator "|" (Bitwise OR) Performs boolean OR operation on each bit of integer argument
+ 
+ byte_value_2 = byte_1 | byte_2; //0xf7
+ 
+ //Bit operator "^" (Bitwise exclusive or / XOR) Performs boolean exclusive OR operation on each bit of integer argument
+ 
+ byte_value_3 =  byte_1 ^  byte_2 ; //bytes1: 0xea
+ 
+ //Bit operator "~" (Bitwise negation / Not) Performs boolean NOT operation on each bit of integer argument 
+ 
+ byte_value_3 = ~byte_1; //bytes1: 0x00
+ 
+ 
+ //Right Shift  ">>"    (Moves all bits of the first operand to the right by the number of places specified by the second operand)
+ 
+ value_7 = value_3 >> value_4; //uint256: 0
+ 
+ //Left Shift  ">>"    (Moves all bits of the first operand to the right by the number of places specified by the second operand)
+ 
+  value_8 = value_3 << value_4; //uint256: 48
+ 
 }
  
-//Bit operator "^" (Bitwise exclusive or / XOR) Performs boolean exclusive OR operation on each bit of integer argument
-
-function bitOperator_3(bytes1 byte_5 , bytes1 byte_6) public pure returns (bytes1){
-    return byte_5 ^  byte_6 ;
+ 
 }
-
-//Bit operator "~" (Bitwise negation / Not) Performs boolean NOT operation on each bit of integer argument 
-
-function bitOperator_4(bytes1 byte_7 ) public pure returns (bytes1){
-    return ~byte_7 ;
-}
+ 
 
 
-
-//Right Shift  ">>"    (Moves all bits of the first operand to the right by the number of places specified by the second operand)
-function rightShift(uint value_3 , uint value_4) public pure returns (uint ){
-    return value_3 >> value_4;
-}
-
-//Left Shift  ">>"    (Moves all bits of the first operand to the right by the number of places specified by the second operand)
-function lestShift(uint value_3 , uint value_4) public pure returns (uint ){
-    return value_3 << value_4;
-}
-
-
-
-}
