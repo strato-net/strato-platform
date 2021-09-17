9c43f160434d78314d8c30c8b294a32266ea5c5d
Jamshid <jim@blockapps.net>
2021-09-17 12:26:12 -0400

    I need to default the new `/fill` behavior to False, or the tests won't currently pass.  We will have to turn this back on later on.

--- api/bloc2/src/BlockApps/Bloc22/Server/BlocOptions.hs
+++ api/bloc2/src/BlockApps/Bloc22/Server/BlocOptions.hs
@@ -5,4 +5,4 @@ module BlockApps.Bloc22.Server.BlocOptions where
 
 import           HFlags
 
-defineFlag "useDeprecatedFillFailBehavior" (False :: Bool) "don't return an error when gas is off and user tries to fill gas (just fail silently).  This options is only included for backwards compatibility"
+defineFlag "useDeprecatedFillFailBehavior" (True :: Bool) "don't return an error when gas is off and user tries to fill gas (just fail silently).  This options is only included for backwards compatibility"
