c4ad5c05419c61f3e68e70eebd1adf5da33f8895
nick <nhjlopez@gmail.com>
2021-11-15 23:43:36 -0800

    This in theory should work

--- smd-ui/src/components/SideBar/index.js
+++ smd-ui/src/components/SideBar/index.js
@@ -1,68 +1,91 @@
-import React, { Component } from 'react';
-import './sidebar.css';
-import { NavLink } from 'react-router-dom';
-import logo from './blockapps-logo-horizontal-white.png';
+import React, { Component } from "react";
+import "./sidebar.css";
+import { NavLink } from "react-router-dom";
+import logo from "./blockapps-logo-horizontal-white.png";
 
-import 'normalize.css/normalize.css';
-import '@blueprintjs/core/dist/blueprint.css';
-import mixpanelWrapper from '../../lib/mixpanelWrapper';
-import { isS3Available } from '../../lib/checkS3Credentials';
+import "normalize.css/normalize.css";
+import "@blueprintjs/core/dist/blueprint.css";
+import mixpanelWrapper from "../../lib/mixpanelWrapper";
+import { isS3Available } from "../../lib/checkS3Credentials";
 
 class SideBar extends Component {
-
   // noOverlay
   // TODO: customCrossIcon={<div><div className="pt-icon-standard pt-icon-chevron-left"/></div>}
   render() {
-    const navLinksData = (
-      [
-        //{path: '/nodes', label: 'Nodes', id: 'nodes', icon: "pt-icon-layout-auto"},
-        { path: '/home', label: 'Dashboard', id: 'dashboard', icon: "fa-rocket" },
-        { path: '/chains', label: 'Chains', id: 'chains', icon: "fa-user-secret" },
-        { path: '/blocks', label: 'Blocks', id: 'blocks', icon: "fa-link" },
-        { path: '/transactions', label: 'Transactions', id: 'transactions', icon: "fa-exchange" },
-        { path: '/accounts', label: 'Accounts', id: 'accounts', icon: "fa-users" },
-        { path: '/contracts', label: 'Contracts', id: 'contracts', icon: "fa-gavel" },
-        { path: '/code_editor', label: 'Contract Editor', id: 'code_editor', icon: "fa-code" }
-      ]
-    );
+    const navLinksData = [
+      //{path: '/nodes', label: 'Nodes', id: 'nodes', icon: "pt-icon-layout-auto"},
+      { path: "/home", label: "Dashboard", id: "dashboard", icon: "fa-rocket" },
+      {
+        path: "/chains",
+        label: "Chains",
+        id: "chains",
+        icon: "fa-user-secret",
+      },
+      { path: "/blocks", label: "Blocks", id: "blocks", icon: "fa-link" },
+      {
+        path: "/transactions",
+        label: "Transactions",
+        id: "transactions",
+        icon: "fa-exchange",
+      },
+      {
+        path: "/accounts",
+        label: "Accounts",
+        id: "accounts",
+        icon: "fa-users",
+      },
+      {
+        path: "/contracts",
+        label: "Contracts",
+        id: "contracts",
+        icon: "fa-gavel",
+      },
+      /*      { path: '/code_editor', label: 'Contract Editor', id: 'code_editor', icon: "fa-code" } 
+
+        This is just to remove "Access" to the Contract Editor. I do not believe we need to delete the entire page due to needing it to for debugging purposes. 
+*/
+    ];
 
     return (
       <aside>
         <div className="menu">
-          {
-            navLinksData.map(data =>
-              <NavLink
-                key={data.id}
-                id={data.id}
-                to={data.path}
-                className="menu-item"
-                activeClassName="active-menu-item"
-                onClick={() => { mixpanelWrapper.track('nav_link_' + data.id + '_click') }}
-              >
-                <i className={'fa ' + data.icon}> </i>
-                <span className="menu-text"> {data.label}</span>
-              </NavLink>
-            )
-          }
-          {isS3Available() &&
+          {navLinksData.map((data) => (
             <NavLink
-              id={'external_storage'}
-              to={'/external_storage'}
+              key={data.id}
+              id={data.id}
+              to={data.path}
               className="menu-item"
               activeClassName="active-menu-item"
-              onClick={() => { mixpanelWrapper.track('nav_link_external_storage_click') }}
+              onClick={() => {
+                mixpanelWrapper.track("nav_link_" + data.id + "_click");
+              }}
             >
-              <i className='fa fa-cloud-upload'> </i>
+              <i className={"fa " + data.icon}> </i>
+              <span className="menu-text"> {data.label}</span>
+            </NavLink>
+          ))}
+          {isS3Available() && (
+            <NavLink
+              id={"external_storage"}
+              to={"/external_storage"}
+              className="menu-item"
+              activeClassName="active-menu-item"
+              onClick={() => {
+                mixpanelWrapper.track("nav_link_external_storage_click");
+              }}
+            >
+              <i className="fa fa-cloud-upload"> </i>
               <span className="menu-text">External Storage</span>
-            </NavLink>}
+            </NavLink>
+          )}
         </div>
         <div className="smd-sidebar-logo">
-          <a href="http://blockapps.net" target="_blank" rel="noopener noreferrer">
-            <img
-              src={logo}
-              width="120"
-              alt="Blockapps Logo"
-            />
+          <a
+            href="http://blockapps.net"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={logo} width="120" alt="Blockapps Logo" />
           </a>
         </div>
       </aside>
