1d20bf8ca76c0b63cdb76258e57df1ac256fbe78
charles37 <benprevor@gmail.com>
2022-02-17 13:10:47 -0500

    added unit test for disallowing cert registration on private chains

--- core-strato/solid-vm/tests/SolidVMSpec.hs
+++ core-strato/solid-vm/tests/SolidVMSpec.hs
@@ -2868,7 +2868,7 @@ contract qq {
       , BString "-----BEGIN CERTIFICATE-----\nMIIBiDCCAS2gAwIBAgIQCgO76hC29iXEFXJNco5ekjAMBggqhkjOPQQDAgUAMEYx\nDDAKBgNVBAMMA2RhbjEMMAoGA1UEBgwDVVNBMRIwEAYDVQQKDAlibG9ja2FwcHMx\nFDASBgNVBAsMC2VuZ2luZWVyaW5nMB4XDTIxMDMxODE1NDgwN1oXDTIyMDMxODE1\nNDgwN1owRjEMMAoGA1UEAwwDZGFuMQwwCgYDVQQGDANVU0ExEjAQBgNVBAoMCWJs\nb2NrYXBwczEUMBIGA1UECwwLZW5naW5lZXJpbmcwVjAQBgcqhkjOPQIBBgUrgQQA\nCgNCAAQY4p67l1IIEUdVC7L+rUDwF5Nv30bze0NV5y8ced7qwp+YFk3UAiOGkcYo\n7ba8F92rd0yf9AGpvZN1H3Dda8xdMAwGCCqGSM49BAMCBQADRwAwRAIgbKXO8tZ5\noPhBusPQFkNEQDnLO/MRru4KjtCpPnVb5sACIE0TwBJ7yeIGuPc/8G50/858Pf3a\n0t1hHbhYnJarPkNA\n-----END CERTIFICATE-----\n"
       ]
 
-  it "can't register x509 certs on a private chain" $ (runTest $ do
+  it "can't register a x509 certificate on a private chain" $ (runTest $ do
     void $ runArgsWithSender pirvateChainAcc "()" [r|
 contract qq {
     account myAccount = account("deadbeef:feedbeef");
