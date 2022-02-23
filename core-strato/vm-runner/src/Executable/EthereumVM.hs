a6d6c8dbb7940e03e730468d2866395f549b5152
charles37 <benprevor@gmail.com>
2022-02-23 16:40:08 -0500

    reverted last change

--- core-strato/vm-runner/src/Executable/EthereumVM.hs
+++ core-strato/vm-runner/src/Executable/EthereumVM.hs
@@ -331,7 +331,7 @@ runChainConstructors cId cInfo = do
          False --isRunningTests
          True --isHomestead
          False --noValueTransfer
-         False -- isRunChainConstructors
+         True -- isRunChainConstructors
          S.empty --pre-existing suicide list
          (BlockData
             (Keccak256.unsafeCreateKeccak256FromWord256 0)
