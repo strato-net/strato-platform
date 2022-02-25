068a1d253e2ce71efe0cd1baa6f6ea85dace658b
nikitamendelbaum <nikitamendelbaum@gmail.com>
2022-02-25 12:31:04 -0500

    fix oauth mode in smd

--- smd-ui/docker-run.sh
+++ smd-ui/docker-run.sh
@@ -6,7 +6,7 @@ ssl=${ssl:-false}
 
 sed -i "s|__NODE_HOST__|$NODE_HOST|g" build/index.html
 sed -i "s|__NODE_NAME__|$NODE_NAME|g" build/index.html
-sed -i "s|__OAUTH_ENABLED__|$OAUTH_ENABLED|g" build/index.html # TODO: remove line and the tests
+sed -i "s|__OAUTH_ENABLED__|true|g" build/index.html # Temporary measure required until the non-oauth code is fully removed from SMD, including the tests
 sed -i "s|__IS_SSL__|$ssl|g" build/index.html
 sed -i "s|__STRATO_VERSION__|$STRATO_VERSION|g" build/index.html
 
