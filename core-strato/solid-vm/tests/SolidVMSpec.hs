4f2e21556c1ace1f5817b5a8b75926492b2ab2e5
charles37 <benprevor@gmail.com>
2022-03-02 17:01:11 -0500

    cleaned up test

--- core-strato/solid-vm/tests/SolidVMSpec.hs
+++ core-strato/solid-vm/tests/SolidVMSpec.hs
@@ -2888,7 +2888,6 @@ contract qq {
 
   it "testyboi" $ (runTest (runBS [r|
 contract qq {
-  uint[] y;
   uint z;
   uint[] x;
   uint[] myVar;
@@ -2902,46 +2901,7 @@ contract qq {
     x[0] = 1;
     return x;
   }
-  function g() returns (uint[]) {
-    // assignment of second value
-    uint[] x;
-    x[1] = 2;
-    return x;
-  }
-  function h() returns (uint[]) {
-    // assignment of two values
-    uint[] x;
-    x[0] = 1;
-    x[1] = 2;
-    return x;
-  }
-  function i() {
-    // assignment, set state variable
-    uint[] x;
-    x[0] = 1;
-    y = x;
-  }
-  function j() {
-    // assignment of first value, set state variable to array element
-    uint[] x;
-    x[0] = 1;
-    z = x[0];
-  }
-  function k() {
-    // assignment of second value, set state variable to array element
-    uint[] x;
-    x[1] = 2;
-    z = x[1];
-  }
   }|])) `shouldThrow` anyInvalidWriteError
---    getFields ["z"] `shouldReturn` [BInteger 1]
---  getFields ["x"]
-    {-getAll
-      [ [Field "x", Field "length"]
-      , [Field "x",  ArrayIndex 0]
-      ] `shouldReturn` [  BInteger 1
-                        , BInteger 1]-}
-
 
   it "can parse an X509 certificate" . runTest $ do
     runBS [r|
