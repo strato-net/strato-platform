1849cf4fee9dee99cd10a5928171ec72b4fbaca0
Troy Kelley <trkelley28@gmail.com>
2022-03-02 15:49:50 -0500

    check for properties before redacting them

--- blockapps-rest/src/axios-wrapper.ts
+++ blockapps-rest/src/axios-wrapper.ts
@@ -118,48 +118,65 @@ function requestFormatter(_request) {
   if (request.headers && request.headers.Authorization) {
     const rhAuth = request.headers.Authorization
     if (rhAuth.startsWith('Bearer')) {
-      const headerString = rhAuth.substring(0, 15) + '...truncted...' + rhAuth.substring(rhAuth.length - 10)
+      const headerString = rhAuth.substring(0, 15) + '...truncated...' + rhAuth.substring(rhAuth.length - 10)
       request.headers.Authorization = headerString
     }
   }
+  let parsedRequest = request;
   // if this is a contract - remove the source from the debug
-  try {
-    request.data.txs[0].payload.src = 'source removed.'
-  } catch (e) {
+  if (request.hasOwnProperty('data')) {
+    if (request.data.hasOwnProperty('tx')) {
+      parsedRequest.data.tx = request.data.txs.map((tx) => {
+        return {
+          ...tx,
+          payload: {
+            ...tx.payload,
+            src: 'source removed.'
+          }
+        }
+      })
+    }
+    if (parsedRequest.data.hasOwnProperty('src')) {
+      parsedRequest.data.src = 'source removed.'
+    }
   }
-  try {
-    request.data.src = 'source removed.'
-  } catch (e) {
-  }
-  return JSON.stringify(request, null, 2)
+  return JSON.stringify(parsedRequest, null, 2)
 }
 
 function responseFormatter(response) {
   // if this is a contract - remove the source from the debug
-  try {
-    response.data[0].data.contents.src = 'source removed.'
-  } catch (e) {
+  let parsedResponse = response
+  if (response.hasOwnProperty('data') && Array.isArray(response.data)) {
+    parsedResponse.data = response.data.map((el) => {
+      let newEl = el
+      if (el.hasOwnProperty('data')) {
+        if (el.data.hasOwnProperty('contents')) {
+          const contents = el.data.contents
+          if (contents.hasOwnProperty('src')) {
+            contents.src = 'source removed.'
+          }
+          if (contents.hasOwnProperty('bin')) {
+            contents.bin = 'bin removed'
+          }
+          if (contents.hasOwnProperty('bin-runtime')) {
+            contents['bin-runtime'] = 'bin-runtime removed.'
+          }
+          if (contents.hasOwnProperty('xabi')) {
+            contents.xabi = 'xabi removed.'
+          }
+          newEl.data.contents = contents
+        }
+        if (el.data.hasOwnProperty('src')) {
+          newEl.data.src = 'source removed.'
+        }
+      }
+      return newEl
+    })
   }
-  try {
-    response.data[0].data.contents.bin = 'bin removed.'
-  } catch (e) {
+  if (parsedResponse.data) {
+    return JSON.stringify(parsedResponse.data, null, 2)
   }
-  try {
-    response.data[0].data.contents['bin-runtime'] = 'bin-runtime removed.'
-  } catch (e) {
-  }
-  try {
-    response.data[0].data.contents.xabi = 'xabi removed.'
-  } catch (e) {
-  }
-  try {
-    response.data[0].data.src = 'source removed.'
-  } catch (e) {
-  }
-  if (response.data) {
-    return JSON.stringify(response.data, null, 2)
-  }
-  return JSON.stringify(response, null, 2)
+  return JSON.stringify(parsedResponse, null, 2)
 }
 
 function errorFormatter(err) {
