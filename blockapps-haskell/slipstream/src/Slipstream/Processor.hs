4c5bf6fc926037b8912837650c126053d357fe27
David Moncayo <david_moncayo@blockapps.net>
2022-04-21 14:06:37 -0400

    removed InsertContractBloc_

--- blockapps-haskell/slipstream/src/Slipstream/Processor.hs
+++ blockapps-haskell/slipstream/src/Slipstream/Processor.hs
@@ -207,45 +207,19 @@ lookupT k = MaybeT . return . Map.lookup k
 -- Inserts the contract source into the Bloc DB.
 --  If they're not in the cache but they are in bloc database or action metadata,
 --  it reparses the whole source blob, and caches the info for every contract
-insertContractBloc_ :: ( MonadIO m
-                           , MonadLogger m
-                           , Accessible BlocEnv m
-                           , HasBlocSQL m
-                           , Selectable Account AddressState m
-                           , (Keccak256 `Alters` SourceMap) m
-                           )
-                        => IORef Globals -> AggregateAction -> m ()
-insertContractBloc_ g row = void $ runMaybeT  
-   $  checkCache 
-  <|> checkMetadata
-  <|> checkBloc 
+--insertContractBloc_ :: ( MonadIO m
+--                           , MonadLogger m
+--                           , HasBlocSQL m
+--
+--                           )
+--                        => IORef Globals -> AggregateAction -> m ()
+--insertContractBloc_ g row = void $ runMaybeT $ checkCache 
   
-  where checkCache = do
-          $logInfoS "insertContractBloc_" . T.pack $ "Checking cache for contract info"
-          MaybeT $ getSolidVMInfo g codePtr
+  --where checkCache = do
+    --      $logInfoS "insertContractBloc_" . T.pack $ "Checking cache for contract info"
+      --    MaybeT $ getSolidVMInfo g codePtr
         
-        checkMetadata = do
-          $logInfoS "insertContractBloc_" . T.pack $ "Checking metadata for contract info"
-          src <- lookupT "src" $ actionMetadata row 
-          parseAndSet $ deserializeSourceMap src
-        
-        checkBloc = do
-          $logInfoS "insertContractBloc_" . T.pack $ "Checking bloc database for contract info"
-          details <- (MaybeT $ either (const Nothing) Just <$> getContractDetailsByCodeHash codePtr)
-          parseAndSet $ OLD.contractdetailsSrc details
-
-        -- parse source code into a map, add the map of name -> contract to cache
-        -- this call is needed to insert the contract into bloc db contracts_source table
-        -- But since the contract_source table takes a serialized list of contracts from a code collection, we need to 
-        -- parse it first before inserting, since codeCollection could be multiple contracts.
-        parseAndSet src = do
-          details <- lift $ sourceToContractDetails (Don't Compile) src -- :: Map Text ContractDetails
-          lift $ insertContractDetailsQuery src -- insert contract details into bloc db contracts_source table
-          let infoMap = fmap OLD.contractdetailsCodeHash details
-          setSolidVMInfo g codePtr infoMap
-          pure infoMap
-          
-        codePtr = actionCodeHash row
+        --codePtr = actionCodeHash row
 
 
 -- EVM details are not cached, because the cache links all the contracts in a source blob by source hash, and we only have source hashes for SolidVM code pointers. 
@@ -533,7 +507,7 @@ processTheMessages env sqlEnv conn g messages = do
       case actionStorage row of
         Action.EVMDiff{} -> evmInsertsF g row actions acct
         Action.SolidVMDiff{} -> do
-          insertContractBloc_ g row
+          --insertContractBloc_ g row
           let cid = maybe "" (T.pack . chainIdString . ChainId) $ (actionAccount row ^. accountChainId)
               (SolidVMCode name _) = actionCodeHash row
               abiid = ABIID {
