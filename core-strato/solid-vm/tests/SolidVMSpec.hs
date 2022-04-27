ab5fdd4cb59ce8dad20382ae6d73bd095211b98b
Steven Glasford <lr4fzuk0@anonaddy.me>
2022-04-27 11:15:27 -0500

    Remove fit from the imports.

--- core-strato/solid-vm/tests/SolidVMSpec.hs
+++ core-strato/solid-vm/tests/SolidVMSpec.hs
@@ -28,7 +28,7 @@ import Data.Text.Encoding
 import Data.Time.Clock.POSIX
 import HFlags
 import Numeric
-import Test.Hspec (hspec, Spec, fit, describe, it, xit, pendingWith, anyException, shouldThrow, anyErrorCall, Selector)
+import Test.Hspec (hspec, Spec, describe, it, xit, pendingWith, anyException, shouldThrow, anyErrorCall, Selector)
 import Test.Hspec.Expectations.Lifted
 import Text.Printf
 import Text.RawString.QQ
