3e6166ba4bede17b40aaa85b1fa4fbc746375505
David Moncayo <david_moncayo@blockapps.net>
2022-06-01 22:39:56 -0400

    moved setup script to pipeline

--- utils/dev-scripts/syncspeedtest/getSyncData.sh
+++ utils/dev-scripts/syncspeedtest/getSyncData.sh
@@ -2,30 +2,13 @@
 
 for VARIABLE in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
 do
-
-	./strato --drop-chains
-	OAUTH_ENABLED=true \
-	OAUTH_CLIENT_ID=dev \
-	OAUTH_CLIENT_SECRET='d5e67b8c-4fbf-42c6-a8d9-29a4dd13575f' \
-	OAUTH_DISCOVERY_URL=https://keycloak.blockapps.net/auth/realms/strato-devel/.well-known/openid-configuration \
-	OAUTH_JWT_USERNAME_PROPERTY=email \
-	PASSWORD=123 \
-	vmDebug=true \
-	wsDebug=true \
-	svmTrace=true \
-	BOOT_NODE_IP='3.81.197.53' \
-	networkID=1720007323 \
-	validators='["9db9b9a5045f24912ece22bbdbeca1bc2884f3f7"]' \
-	blockstanbulAdmins='["9db9b9a5045f24912ece22bbdbeca1bc2884f3f7"]' \
-	./strato --blockstanbul
-
 	docker exec -it strato_strato_1 bash -c 'apt-get update --yes'
 	docker exec -it strato_strato_1 bash -c 'apt update --yes'
 	docker exec -it strato_strato_1 bash -c 'apt install python3-pip --yes'
 
 	docker exec -it strato_strato_1 bash -c 'pip3 install --upgrade pip'
 	docker exec -it strato_strato_1 bash -c 'pip3 install requests'
-	docker cp ~/strato-getting-started/getEntireLogs.py strato_strato_1:/var/lib/strato/logs
+	docker cp ./getEntireLogs.py strato_strato_1:/var/lib/strato/logs
 	docker exec -it strato_strato_1 bash -c 'cd logs && python3 getEntireLogs.py'
 
 
