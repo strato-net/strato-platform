de0c95e2bd0ed0df2c62dc73f4b02f680b857401
aariya50 <61092108+aariya50@users.noreply.github.com>
2022-06-29 17:22:06 -0500

    Strato 2612 add features to the certificate dapp

--- strato/tools/x509-certs/src/BlockApps/X509/Certificate.hs
+++ strato/tools/x509-certs/src/BlockApps/X509/Certificate.hs
@@ -294,9 +294,9 @@ getValidity = do
       endDate = DateTime dt{dateYear=(dateYear dt) + 1} tm -- all certs are valid for a year
   return (curDate, endDate)
 
-  getParentUserAddress :: X509Certificate -> Maybe Address
-  getParentUserAddress (X509Certificate (CertificateChain (_:c2:_))) = fmap (fromPublicKey . subPub) (getCertSubject (X509Certificate (CertificateChain [c2])))
-  getParentUserAddress _ = Nothing
+getParentUserAddress :: X509Certificate -> Maybe Address
+getParentUserAddress (X509Certificate (CertificateChain (_:c2:_))) = fmap (fromPublicKey . subPub) (getCertSubject (X509Certificate (CertificateChain [c2])))
+getParentUserAddress _ = Nothing
 
 getCertPub :: Subject -> PubKey
 getCertPub = serializeAndWrap . subPub
