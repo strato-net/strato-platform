c2a3d914f5fc8e5a09f6102ff498c52d218b4979
David Moncayo <david_moncayo@blockapps.net>
2022-07-07 17:24:01 -0400

    more accurate error

--- strato/api/bloc2/src/BlockApps/Bloc22/Server/TransactionResult.hs
+++ strato/api/bloc2/src/BlockApps/Bloc22/Server/TransactionResult.hs
@@ -116,7 +116,7 @@ getBlocTransactionResult' hashes@(txh:_) resolve =
       results <- mapM wait promises
       $logDebugLS "getBlocTransactionResult'/results" results
       case results of 
-        [] -> throwIO $ AnError "Empty list provided: no resolved transactions"
+        [] -> throwIO $ AnError "Empty list provided: results is empty"
         _ -> case filter ((== Success) . blocTransactionStatus) results of
           (winner:_) -> return winner
           [] -> return $ BlocTransactionResult Failure emptyHash Nothing Nothing 
