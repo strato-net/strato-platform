228a9d1cc852f2402fa47f651d08866340489d78
neelkanthpoosa <neelkanth372@gmail.com>
2022-07-11 16:43:41 -0400

    Added tests for checking Bool/Int/String default map value types

--- strato/VM/SolidVM/solid-vm/tests/SolidVMSpec.hs
+++ strato/VM/SolidVM/solid-vm/tests/SolidVMSpec.hs
@@ -4267,3 +4267,28 @@ contract qq {
     return blockhash(-1);
   }
 }|]) `shouldThrow` anyInvalidArgumentsError
+
+  it "return default value for index not present" . runTest $ do
+    runBS [r|
+pragma solidvm 3.2;
+contract qq {
+  
+  mapping(uint=>bool) booleanTest;
+  mapping(uint=>uint) integerTest;
+  mapping(uint=>string) stringTest;
+
+  bool x;
+  uint y;
+  string z;
+  
+  constructor() {
+    booleanTest[1] = true;
+    integerTest[1] = 1;
+    stringTest[1] = "testing";
+
+    x = booleanTest[9];
+    y = integerTest[9];
+    z = stringTest[9];
+  }
+}|]
+    getFields ["x","y","z"] `shouldReturn` [BDefault, BDefault, BDefault]
