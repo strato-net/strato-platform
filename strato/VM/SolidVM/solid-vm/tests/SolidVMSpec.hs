cee24dad83061fb64afea3675870e87d5a4d0087
Steven Glasford <lr4fzuk0@anonaddy.me>
2022-08-04 14:00:11 -0500

    Add new formating test that the code function needs to pass

--- strato/VM/SolidVM/solid-vm/tests/SolidVMSpec.hs
+++ strato/VM/SolidVM/solid-vm/tests/SolidVMSpec.hs
@@ -3918,6 +3918,30 @@ contract qq{
     getFields ["codeTest"] `shouldReturn`
       [ BString $ UTF8.fromString contractqq]
 
+  fit "can properly add the final } to a contract without a constructor using the code member function" . runTest $ do
+    let myContract :: String
+        myContract = [r|contract Test {
+  uint sixtyNine = 69;
+}|]
+        contract :: String
+        contract = [r|
+pragma solidvm 3.3;
+contract Test {
+  uint sixtyNine = 69;
+}
+
+pragma solidvm 3.3;
+contract qq {
+  string codeTest;
+  constructor(){
+    Test t = new Test();
+    codeTest = account(t).code("Test");
+  }
+}|]
+    runBS contract
+    getFields ["codeTest"] `shouldReturn`
+      [ BString $ UTF8.fromString myContract]
+
   fit "Can find a function within a codeCollection" . runTest $ do
     let myFunxion :: String
         myFunxion = [r|function myFunction() public returns (uint) {
