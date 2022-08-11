a499dfc1f87f7b57b228be202667cf35e8c54810
jxuan101 <jinhuai.xuan38@myhunter.cuny.edu>
2022-08-11 12:04:48 -0400

    Delete test for try catch

--- strato/VM/SolidVM/solid-vm/tests/SolidVMSpec.hs
+++ strato/VM/SolidVM/solid-vm/tests/SolidVMSpec.hs
@@ -5237,32 +5237,3 @@ contract qq {
   }
 }|]
     getFields ["myString"] `shouldReturn` [BDefault]
-
-  it "can catch custom errors the SOLIDVM WAY" . runTest $ do
-    runBS [r|
-pragma solidvm 3.3;
-
-contract qq {
-  error IsTen(int ten, string message);
-  int val;
-
-  constructor() {
-    setVal(10);
-  }
-
-  function checkTen(int _val) returns (int) {
-     if (_val == 10) {
-        throw IsTen(_val, "Stop trying to make ten happen, its not going to happen"); 
-     }
-     return _val;
-  }
-
-  function setVal(int _val) returns (int) {
-     try {
-        val = checkTen(_val);
-     } catch IsTen(vall, mes) { 
-        val = vall + 1;
-     }
-  }
-}|]
-    getFields ["val"] `shouldReturn` [BInteger 11]
