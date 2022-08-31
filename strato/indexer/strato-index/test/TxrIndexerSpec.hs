add528d727974f38baad8cb145d36303bf301c0a
Dustin Norwood <dnorwood2010@aol.com>
2022-08-31 13:37:50 -0400

    Update TxrIndexerSpec.hs

--- strato/indexer/strato-index/test/TxrIndexerSpec.hs
+++ strato/indexer/strato-index/test/TxrIndexerSpec.hs
@@ -21,13 +21,13 @@ spec = do
                 chainId = fromInteger 0x4920
                 orgId = OrgId "6f8a80d14311c39f35f516fa664deaaaa13e85b2f7493f37f6144d86991ec012937307647bd3b9a82abe2974e1407241d54947bbb39763a4cac9f77166ad92a0"
                 enode = Enode orgId (IPv4 $ fromInteger 192) 80 Nothing
-                event = EventDB (read "0x0") (Just chainId) "MemberAdded" [show addr, showEnode enode]
+                event = EventDB (read "0xdeadbeef") (Just chainId) "MemberAdded" [show addr, showEnode enode]
             in indexEventToTxrResults (EventDBEntry event)
                 `shouldBe` [PutEventDB event, AddMember $ Right ( chainId, addr, enode)]
         it "Index EventDBEntry for MemberRemoved" $
             let addr = fromInteger 0x1011
                 chainId = fromInteger 0x8203
-                event = EventDB (read "0x0") (Just chainId) "MemberRemoved" [show addr]
+                event = EventDB (read "0xdeadbeef") (Just chainId) "MemberRemoved" [show addr]
             in indexEventToTxrResults (EventDBEntry event)
                 `shouldBe` [PutEventDB event, RemoveMember $ Right (chainId, addr)]
         it "Index EventDBEntry for CertificateRegistered" $
@@ -44,22 +44,22 @@ spec = do
                     , "N8txKc8G9R27ZYAUuz15zF0="
                     , "-----END CERTIFICATE-----"
                     ]
-                event = EventDB (read "0x0") Nothing "CertificateRegistered" [certString]
+                event = EventDB (read "0xdeadbeef") Nothing "CertificateRegistered" [certString]
                 parsedCert = fromRight (error "Couldn't parse certString") $ bsToCert $ C8.pack $ certString
                 addr = fromInteger 0x74f014fef932d2728c6c7e2b4d3b88ac37a7e1d0
             in indexEventToTxrResults (EventDBEntry event)
-                `shouldBe` [PutEventDB event, RegisterCertificate $ Right ((read "0x0"), addr, X509CertInfoState{userAddress=addr, certificate=parsedCert, isValid=True, children=[]})]
+                `shouldBe` [PutEventDB event, RegisterCertificate $ Right ((read "0xdeadbeef"), addr, X509CertInfoState{userAddress=addr, certificate=parsedCert, isValid=True, children=[]})]
         it "Index EventDB for CertificateRevoked" $
             let userAddr = fromInteger 0x489384
-                event = EventDB (read "0x0") Nothing "CertificateRevoked" [show userAddr]
+                event = EventDB (read "0xdeadbeef") Nothing "CertificateRevoked" [show userAddr]
             in indexEventToTxrResults (EventDBEntry event)
-                `shouldBe` [PutEventDB event, CertificateRevoked . Right $ ((read "0x0"), userAddr)]
+                `shouldBe` [PutEventDB event, CertificateRevoked . Right $ ((read "0xdeadbeef"), userAddr)]
         it "Index EventDB for CertificateRegistryInitialized" $
-            let event = EventDB (read "0x0") Nothing "CertificateRegistryInitialized" []
+            let event = EventDB (read "0xdeadbeef") Nothing "CertificateRegistryInitialized" []
             in indexEventToTxrResults (EventDBEntry event)
-                `shouldBe` [PutEventDB event, CertificateRegistryInitialized . Right $ ((read "0x0"))]
+                `shouldBe` [PutEventDB event, CertificateRegistryInitialized . Right $ ((read "0xdeadbeef"))]
         it "Index EventDBEntry for non-special event" $
             let chainId = fromInteger 0x480244
-                event = EventDB (read "0x0") (Just chainId) "NotSpecial" ["48193"]
+                event = EventDB (read "0xdeadbeef") (Just chainId) "NotSpecial" ["48193"]
             in indexEventToTxrResults (EventDBEntry event)
                 `shouldBe` [PutEventDB event]
