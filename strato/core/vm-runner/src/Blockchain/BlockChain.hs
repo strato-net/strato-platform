333fe9daa43c36bcf8f5ba4cbefb4599a499858b
Dustin Norwood <dnorwood2010@aol.com>
2022-09-17 22:32:02 -0400

    Oops, use correct pragma list in Bagger

--- strato/core/vm-runner/src/Blockchain/BlockChain.hs
+++ strato/core/vm-runner/src/Blockchain/BlockChain.hs
@@ -312,7 +312,7 @@ mineTransactions' header remGas ran unran@(tx:txs) = do
     trr <- setNewAddresses $ TxRunResult tx result time' beforeMap afterMap []
     case result of
         Right execResult ->
-          let supportedPragmas = [("svm","3.0"),("svm","3.2"),("svm","3.3")]
+          let supportedPragmas = [("solidvm","3.0"),("solidvm","3.2"),("solidvm","3.3")]
               findInvalidPragmas pragma = if pragma `elem` supportedPragmas then id else (pragma:)
               invalidPragmasUsed = foldr findInvalidPragmas [] (erPragmas execResult) 
            in if not $ null invalidPragmasUsed
