6d6dff3166c998476a0c046119752fd311b9bb12
Imaad Ghouri <imaad_ghouri@blockapps.net>
2022-10-07 15:48:41 -0700

    OPS-3 Build fix

--- nginx-packager/docker-run.sh
+++ nginx-packager/docker-run.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-echo "APEX_HOST Before: ${APEX_HOST}"
-echo "SMD_HOST Before: ${SMD_HOST}"
-
 MIN_TIMEOUT_BLOCKCHAIN_ENDPOINTS=60
 BLOCK_TIME_MULTIPLIER_FOR_TIMEOUT=10
 blockTime=${blockTime:-13} # keep default the same as strato
@@ -32,9 +29,6 @@ STRATO_PORT_API2=${STRATO_PORT_API2:-strato:3001}
 STRATO_PORT_LOGS=${STRATO_PORT_LOGS:-strato:7065}
 STRATO_PORT_BLOCKSTANBUL_VOTE=${STRATO_PORT_BLOCKSTANBUL_VOTE:-strato:8050}
 VAULT_WRAPPER_HOST=${VAULT_WRAPPER_HOST:-vault-wrapper:8000}
-
-echo "APEX_HOST After: ${APEX_HOST}"
-echo "SMD_HOST After: ${SMD_HOST}"
 
 # If container is running for the first time - generate config:
 if [ ! -f /usr/local/openresty/nginx/conf/nginx.conf ]; then
@@ -105,17 +99,17 @@ if [ ! -f /usr/local/openresty/nginx/conf/nginx.conf ]; then
   sed -i 's/<BLOC_TIMEOUT>/'"$BLOC_TIMEOUT"'/g' /tmp/nginx.conf
 
   # Replacing HOST NAME PLACEHOLDERS
-  sed -i "s/__APEX_HOST__/$/g" /tmp/nginx.conf
-  sed -i "s/__DOCS_HOST__/$/g" /tmp/nginx.conf
-  sed -i "s/__POSTGREST_HOST__/$/g" /tmp/nginx.conf
-  sed -i "s/__PROMETHEUS_HOST__/$/g" /tmp/nginx.conf
-  sed -i "s/__SMD_HOST__/$/g" /tmp/nginx.conf
-  sed -i "s/__STRATO_HOST__/$/g" /tmp/nginx.conf
-  sed -i "s/__STRATO_PORT_API__/$/g" /tmp/nginx.conf
-  sed -i "s/__STRATO_PORT_API2__/$/g" /tmp/nginx.conf
-  sed -i "s/__STRATO_PORT_LOGS__/$/g" /tmp/nginx.conf
-  sed -i "s/__STRATO_PORT_BLOCKSTANBUL_VOTE__/$/g" /tmp/nginx.conf
-  sed -i "s/__VAULT_WRAPPER_HOST__/$/g" /tmp/nginx.conf
+  sed -i "s/__APEX_HOST__/$APEX_HOST/g" /tmp/nginx.conf
+  sed -i "s/__DOCS_HOST__/$DOCS_HOST/g" /tmp/nginx.conf
+  sed -i "s/__POSTGREST_HOST__/$POSTGREST_HOST/g" /tmp/nginx.conf
+  sed -i "s/__PROMETHEUS_HOST__/$PROMETHEUS_HOST/g" /tmp/nginx.conf
+  sed -i "s/__SMD_HOST__/$SMD_HOST/g" /tmp/nginx.conf
+  sed -i "s/__STRATO_HOST__/$STRATO_HOST/g" /tmp/nginx.conf
+  sed -i "s/__STRATO_PORT_API__/$STRATO_PORT_API/g" /tmp/nginx.conf
+  sed -i "s/__STRATO_PORT_API2__/$STRATO_PORT_API2/g" /tmp/nginx.conf
+  sed -i "s/__STRATO_PORT_LOGS__/$STRATO_PORT_LOGS/g" /tmp/nginx.conf
+  sed -i "s/__STRATO_PORT_BLOCKSTANBUL_VOTE__/$STRATO_PORT_BLOCKSTANBUL_VOTE/g" /tmp/nginx.conf
+  sed -i "s/__VAULT_WRAPPER_HOST__/$VAULT_WRAPPER_HOST/g" /tmp/nginx.conf
 
   ########
   ### Generate .lua scripts from templates according to configuration provided
