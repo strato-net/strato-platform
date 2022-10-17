2990c75341cb02dad9b912b2d7f5df45306e1af4
Imaad Ghouri <imaad_ghouri@blockapps.net>
2022-10-17 14:06:17 -0700

    OPS-4 Fixing typo

--- ecr_repo_push_tpl.sh
+++ ecr_repo_push_tpl.sh
@@ -5,11 +5,11 @@ set -x
 
 AWS_ACCESS_KEY_ID=$1 AWS_SECRET_ACCESS_KEY=$2 AWS_DEFAULT_REGION=$3 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 406773134706.dkr.ecr.us-east-1.amazonaws.com
 
-declare -a arr=("smd" "apex" "vault-wrapper", "strato", "postgrest", "nginx", "prometheus")
+declare -a arr=("smd" "apex" "vault-wrapper" "strato" "postgrest" "nginx" "prometheus")
 
 for image in "${arr[@]}"
 do
    echo "pushing image $image"
-   docker tag <REPO_URL>"$image":<VERSION> 406773134706.dkr.ecr.us-east-1.amazonaws.com/smd:<VERSION>
+   docker tag <REPO_URL>"$image":<VERSION> 406773134706.dkr.ecr.us-east-1.amazonaws.com/"$image":<VERSION>
    docker push 406773134706.dkr.ecr.us-east-1.amazonaws.com/"$image":<VERSION>
 done
