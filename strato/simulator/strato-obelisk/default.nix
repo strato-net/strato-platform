154fa85cfcfda0684b1dd91ac6816b8e057921ea
Dustin Norwood <dnorwood2010@aol.com>
2022-10-31 12:55:49 -0400

    Tried to make strato-obelisk work

--- strato/simulator/strato-obelisk/default.nix
+++ strato/simulator/strato-obelisk/default.nix
@@ -15,48 +15,69 @@
   }
 }:
 with obelisk;
-project ./. ({ ... }: {
+project ./. ({ pkgs, ... }: {
   android.applicationId = "systems.obsidian.obelisk.examples.minimal";
   android.displayName = "Obelisk Minimal Example";
   ios.bundleIdentifier = "systems.obsidian.obelisk.examples.minimal";
   ios.bundleName = "Obelisk Minimal Example";
   packages = {
-    strato-lite = ../strato-lite;
     blockapps-data = ../../core/blockapps-data;
     blockapps-datadefs = ../../core/blockapps-datadefs;
-    blockapps-haskoin = ../../libs/blockapps-haskoin;
-    blockapps-init = ../../libs/prometheus/blockapps-init;
     blockapps-mpdbs = ../../core/blockapps-mpdbs;
-    blockapps-tools = ../../tools/blockapps-tools;
     blockstanbul = ../../core/blockstanbul;
-    common-log = ../../libs/common-log;
-    cross-monitoring = ../../libs/prometheus/cross-monitoring;
     ethereum-discovery = ../../core/ethereum-discovery;
     ethereum-encryption = ../../core/ethereum-encryption;
-    ethereum-rlp = ../../libs/ethereum-rlp;
     fast-keccak256 = ../../core/fast-keccak256;
     fastMP = ../../core/fastMP;
-    format = ../../libs/format;
     merkle-patricia-db = ../../core/merkle-patricia-db;
-    milena = ../../libs/milena;
-    monad-alter = ../../libs/monad-alter;
+    blockapps-privacy = ../../core/privacy;
     seqevents = ../../core/seqevents;
-    solid-vm = ../../VM/SolidVM/solid-vm;
-    solid-vm-model = ../../VM/SolidVM/solid-vm-model;
-    strato-adit = ../../core/strato-adit;
+    # strato-adit = ../../core/strato-adit;
     strato-conf = ../../core/strato-conf;
-    strato-index = ../../indexer/strato-index;
+    strato-genesis = ../../core/strato-genesis;
+    strato-init = ../../core/strato-init;
     strato-model = ../../core/strato-model;
-    strato-redis-blockdb = ../../core/strato-redis-blockdb;
+    # strato-networks = ../../core/strato-networks;
+    strato-p2p = ../../core/strato-p2p;
+    # strato-redis-blockdb = ../../core/strato-redis-blockdb;
+    strato-sequencer = ../../core/strato-sequencer;
+    strato-statediff = ../../core/strato-statediff;
+    vm-runner = ../../core/vm-runner;
     vm-tools = ../../core/vm-tools;
+
+    # slipstream = ../../indexer/slipstream;
+    strato-index = ../../indexer/strato-index;
+
+    blockapps-haskoin = ../../libs/blockapps-haskoin;
+    clockwork = ../../libs/clockwork;
+    common-log = ../../libs/common-log;
+    ethereum-rlp = ../../libs/ethereum-rlp;
+    format = ../../libs/format;
+    labeled-error = ../../libs/labeled-error;
+    logserver = ../../libs/logserver;
+    # milena = ../../libs/kafka/milena;
+    milena-tools = ../../libs/kafka/milena-tools;
+    monad-alter = ../../libs/monad-alter;
+    nibblestring = ../../libs/nibblestring;
+    partitioner = ../../libs/partitioner;
+    blockapps-init = ../../libs/prometheus/blockapps-init;
+    # cross-monitoring = ../../libs/prometheus/cross-monitoring;
+    secp256k1-haskell = ../../libs/secp256k1-haskell;
+    type-lits = ../../libs/type-lits;
+    x509-certs = ../../libs/x509-certs;
+    strato-lite = ../../simulator/strato-lite;
+    x509-tools = ../../tools/x509-tools;
+    ethereum-vm = ../../VM/EVM/ethereum-vm;
+    evm-solidity = ../../VM/EVM/evm-solidity;
+    debugger = ../../VM/SolidVM/debugger;
+    solid-vm = ../../VM/SolidVM/solid-vm;
+    solid-vm-fuzzer = ../../VM/SolidVM/solid-vm-fuzzer;
+    solid-vm-model = ../../VM/SolidVM/solid-vm-model;
+    solid-vm-parser = ../../VM/SolidVM/solid-vm-parser;
+    solid-vm-static-analysis = ../../VM/SolidVM/solid-vm-static-analysis;
+    source-tools = ../../VM/SolidVM/source-tools;
   };
   overrides = self: super: let
-      nibblestringSrc = pkgs.fetchFromGitHub {
-        owner = "dustinnorwood";
-        repo = "nibblestring";
-        rev = "862120993fd44f73853b04e81136306e33f6e58e";
-        sha256 = "111cy89zyckdc2hp3y327prwibyshjkypcgfgp6jf5ylcjw5clfk";
-      };
       hpackHackage = self.callHackageDirect {
         pkg = "hpack";
         ver = "0.34.1";
@@ -84,7 +105,6 @@ project ./. ({ ... }: {
         ver = "2.6.5";
         sha256 = "0rcvkcv99bns7l0zxk7sv5027i40cdwv0cl0vsvcqi6zbziag5df";
       } {};
-      nibblestring = self.callCabal2nix "nibblestring" nibblestringSrc {};
       relapse = self.callHackageDirect {
         pkg = "relapse";
         ver = "1.0.0.0";
@@ -94,6 +114,7 @@ project ./. ({ ... }: {
       servant = pkgs.haskell.lib.dontCheck super.servant;
       milena = pkgs.haskell.lib.dontCheck super.milena;
       strato-redis-blockdb = pkgs.haskell.lib.dontCheck super.strato-redis-blockdb;
+      libsecp256k1 = ((import ./dep/libsecp256k1) self super);
       mkDerivation = drv: super.mkDerivation (drv // { jailbreak = true; doHaddock = false;});
   };
 })
