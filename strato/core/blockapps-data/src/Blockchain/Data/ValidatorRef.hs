8f678f23fa37f80ae216cf6a79538603b0b64613
enigmatized <going.to.any.lengths.work@gmail.com>
2022-11-01 14:33:05 -0400

    remove extra lines of code

--- strato/core/blockapps-data/src/Blockchain/Data/ValidatorRef.hs
+++ strato/core/blockapps-data/src/Blockchain/Data/ValidatorRef.hs
@@ -15,16 +15,10 @@ import           Blockchain.Strato.Model.Address
 import qualified Database.Esqueleto.Legacy          as E
 
 
--- Provide (False, [Addresses]) to remove from database
--- Provide (True,  [Addresses]) to add    to   database
 addRemoveValidator :: HasSQLDB m =>
-                            ([Address], [Address]) -> m ()-- m (Key ValidatorRef)
+                            ([Address], [Address]) -> m ()
 addRemoveValidator (remove, add) = do
-    --txrs   <-  fmap (map E.entityVal) $  sqlQuery . E.select . E.from $ \(a :: E.SqlExpr (E.Entity ValidatorRef)) -> return a
-    --let setDifference = addr L.\\ ( (\(ValidatorRef x) -> x ) <$> txrs)
-  when (add /= [])
-      (forM_ add $ (\x -> sqlQuery $  E.insert ValidatorRef{ validatorRefAddress = x })) >> pure ()
-        
---addRemoveValidator (False, addr) = 
-  if remove /= [] then (forM remove  $ (\x -> sqlQuery $ E.delete $ E.from $ \address -> E.where_ (address E.^. ValidatorRefAddress E.==. (E.val $ x :: E.SqlExpr (E.Value Address))))) >> pure ()                                                                       
-     else pure ()
+  when (add /= []) (forM_ add $ (\x -> sqlQuery $  E.insert ValidatorRef{ validatorRefAddress = x })) 
+  if remove /= [] 
+    then (forM remove  $ (\x -> sqlQuery $ E.delete $ E.from $ \address -> E.where_ (address E.^. ValidatorRefAddress E.==. (E.val $ x :: E.SqlExpr (E.Value Address))))) >> pure ()                                                                       
+    else pure ()        
