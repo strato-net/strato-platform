051ce95df846613dfcc867090364d933a2187760
Steven Glasford <lr4fzuk0@anonaddy.me>
2022-11-02 16:37:19 -0500

    Code Clean up

--- strato/core/vault-proxy/src/VaultProxyLib.hs
+++ strato/core/vault-proxy/src/VaultProxyLib.hs
@@ -54,7 +54,7 @@ import           GHC.Generics
 import           Network.HTTP.Client     hiding (Proxy)
 -- import           Network.HTTP.Client.TLS
 import           Network.OAuth.OAuth2    as OA  hiding (error)
-import           Network.OAuth.OAuth2.Internal as OAI hiding (error)
+-- import           Network.OAuth.OAuth2.Internal as OAI hiding (error)
 -- import           Network.URI
 import           Servant.API
 import           Servant.Client
@@ -68,21 +68,9 @@ import           Data.ByteString.Base64
 --Datas
 --------------------------------------------------------------------------------
 
--- OAuth2 flags_OAUTH_CLIENT_ID flags_OAUTH_CLIENT_SECRET getAuthorizedEndPoint getTokenEndPoint sendBackRedirect
---     where
---         getAuthorizedEndPoint = "https://keycloak.blockapps.net/auth/realms/strato-devel/protocol/openid-connect/auth" -- get from the initial poking of the discovery url
---         getTokenEndPoint = "https://keycloak.blockapps.net/auth/realms/strato-devel/protocol/openid-connect/token" --get from initial poking of the discovery url
---         sendBackRedirect = "http://localhost:8080" --Whatever port I choose for the vault-proxy to be working for (I am pretty sure)
-
-
 data RawOauth = RawOauth {
     _authorization_endpoint :: T.Text,
     _token_endpoint :: T.Text --,
-    -- _token_introspection_endpoint :: T.Text,
-    -- _userinfo_endpoint :: T.Text,
-    -- _end_session_endpoint :: T.Text,
-    -- _jwks_uri :: T.Text,
-    -- _check_session_iframe :: T.Text
 } deriving (Show, Generic, Eq)
 makeLenses ''RawOauth
 
@@ -90,12 +78,7 @@ instance FromJSON RawOauth where
   parseJSON (Object o) = do
     aue  <- o .: "authorization_endpoint"
     ton  <- o .: "token_endpoint"
-    -- tie  <- o .: "token_introspection_endpoint"
-    -- us1  <- o .: "userinfo_endpoint"
-    -- ese  <- o .: "end_session_endpoint"
-    -- jwk  <- o .: "jwks_uri"
-    -- csi  <- o .: "check_session_iframe"
-    --Ensure the correct data types are coming into the system
+
     authend <- case aue of
         (String s) -> pure s
         (Object _) -> error $ "Expected a JSON String under the key \"authorization_endpoint\", but got something different."
@@ -104,29 +87,7 @@ instance FromJSON RawOauth where
         (String s) -> pure s
         (Object _) -> error $ "Expected a JSON String under the key \"token_endpoint\", but got something different."
         _          -> error $ "Expected a JSON String under the key \"token_endpoint\", but got something different."
-    -- token_introspection_endpoint <- case tie of
-    --     (String s) -> pure s
-    --     (Object _) -> error $ "Expected a JSON String under the key \"token_introspection_endpoint\", but got something different."
-    --     _          -> error $ "Expected a JSON String under the key \"token_introspection_endpoint\", but got something different."
-    -- userinfo_endpoint <- case us1 of
-    --     (String s) -> pure s
-    --     (Object _) -> error $ "Expected a JSON String under the key \"userinfo_endpoint\", but got something different."
-    --     _          -> error $ "Expected a JSON String under the key \"userinfo_endpoint\", but got something different."
-    -- end_session_endpoint <- case ese of
-    --     (String s) -> pure s
-    --     (Object _) -> error $ "Expected a JSON String under the key \"end_session_endpoint\", but got something different."
-    --     _          -> error $ "Expected a JSON String under the key \"end_session_endpoint\", but got something different."
-    -- jwks_uri <- case jwk of
-    --     (String s) -> pure s
-    --     (Object _) -> error $ "Expected a JSON String under the key \"jwks_uri\", but got something different."
-    --     _          -> error $ "Expected a JSON String under the key \"jwks_uri\", but got something different."
-    -- --can't call it scope, so I called it scone, bon appetit
-    -- check_session_iframe <- case csi of
-    --     (String s) -> pure s
-    --     (Object _) -> error $ "Expected a JSON String under the key \"check_session_iframe\", but got something different."
-    --     _          -> error $ "Expected a JSON String under the key \"check_session_iframe\", but got something different."
---   parseJSON wat = typeMismatch "Spec" wat
-    return $ RawOauth authend tokend --token_introspection_endpoint userinfo_endpoint end_session_endpoint jwks_uri check_session_iframe
+    return $ RawOauth authend tokend 
   parseJSON wat = typeMismatch "Spec" wat
 
 --This is the received information from the OpenId Connect response
@@ -214,18 +175,14 @@ type BlockAppsTokenAPI =
 --This will get a fresh brand new, minty fresh clean token from the OAuth provider,
 --User never really needs to use this function, it is mostly called by getAwesomeToken 
 getVirginToken :: MonadIO m => Manager -> T.Text -> T.Text -> RawOauth -> m OAuth2Token --OAuth2Token ---Might need to include the discovery URL later
-getVirginToken manny clientId clientSecret additionalOauth = oat --virginToken
-    where 
-        -- local_place = case (UB.parseURI UB.strictURIParserOptions $ TE.encodeUtf8 $ T.pack "http://localhost:8080") of
-        --     Left _ -> error "Could not parse the Vault Proxy endpoint."
-        --     Right x -> x
-        authEnd = case (UB.parseURI UB.strictURIParserOptions $ TE.encodeUtf8 $ additionalOauth ^. authorization_endpoint) of 
+getVirginToken manny clientId clientSecret additionalOauth = do --virginToken
+
+    let authEnd = case (UB.parseURI UB.strictURIParserOptions $ TE.encodeUtf8 $ additionalOauth ^. authorization_endpoint) of 
             Left _ -> error "Could not parse the authorization endpoint, This is probably a fault of the token provider, please contact your network administration."
             Right uri -> uri
         tokenEnd = case (UB.parseURI UB.strictURIParserOptions $ TE.encodeUtf8 $ additionalOauth ^. token_endpoint) of 
             Left _ -> error "Could not parse the token endpoint, This is probably a fault of the token provider, please contact your network administration."
             Right uri -> uri
-        -- quered = QueryParam [(BS.pack )]
         oa = OAuth2 {
             oauthClientId = clientId,
             oauthClientSecret = Just clientSecret,
@@ -234,11 +191,6 @@ getVirginToken manny clientId clientSecret additionalOauth = oat --virginToken
             oauthCallback = Nothing
         }
         exchangeToken = ExchangeToken $ T.concat [T.pack "Basic ", encodeBase64 $ TE.encodeUtf8 $ T.concat [clientId, ":", clientSecret]]
-        oat = getAccessToken manny oa exchangeToken
-
---This is the actual function that gets the token, use getVirginToken instead
-getAccessToken :: MonadIO m => Manager -> OAuth2 -> ExchangeToken -> m OAI.OAuth2Token
-getAccessToken manny oa exchangeToken = do
     super <- runExceptT $ liftIO $ OA.fetchAccessToken manny oa exchangeToken
     --13
     attttttttttttt <- case super of 
@@ -275,98 +227,3 @@ connectToken ct a bt = getToken ct' a' bt
     where
         ct' = Just ct
         a' = Just a
-
--- --Gets the raw token from the OAUTH provider
--- getVirginToken :: Manager -> String -> ContentType -> Authorization -> BlockAppsTokenRequest-> (IO (Either ClientError VaultToken), Int64)
--- getVirginToken mngr tokenUrl ct tokenAuthorization extraStuff = ((Either ClientError VaultToken), Int64)
---     where
---         clientm   = connectToken ct tokenAuthorization extraStuff
---         baseUrl   = case (parseBaseUrl tokenUrl) of
---             Just url -> url
---             Nothing -> error "Invalid token url supplied 💔💔💔💔💔"
---         clientenv = mkClientEnv mngr baseUrl
---         token     = runClientM clientm clientenv -- (ClientEnv mngr (BaseUrl Https "keycloak.blockapps.net" 443 "/auth/realms/strato-devel/protocol/openid-connect/token")) 
---         time    = getCurrentTime ^. utctDayTime
---         -- time      = 
-
-
-
--- getVaultToken :: TVar (ClientM VaultToken, Int64) -> Manager -> BaseUrl -> ContentType -> Authorization -> BlockAppsTokenRequest -> T.Text
--- getVaultToken superToken mngr tokenUrl ct tokenAuthorization extraStuff = do
---     let goodToken    = readTVar superToken
---         token        = fst goodToken
---         timeAccessed = snd goodToken
---         currentTime  = liftIO $ getTime Monotonic
---         validityTime = token ^. expiresIn
---         validUntil   = validityTime + timeAccessed
---     --Refresh the token if it is soon to be expired 
---     if (validUntil - currentTime < 10) then 
---         swapTVar goodToken (getVirginToken mngr tokenUrl ct tokenAuthorization extraStuff)
---         pure $ getAccessToken goodToken
---         --return just the JWT token though
---     else
---         pure $ getAccessToken goodToken
-
--- getAccessToken :: TVar (ClientM VaultToken, Int64) -> T.Text
--- getAccessToken input = (fst $ readTVar input) ^. accessToken
-------------------------------------------------------------------------------
---Getting information from the vault proxy section
-------------------------------------------------------------------------------
-
--- https://keycloak.blockapps.net/auth/realms/strato-devel/protocol/openid-connect/token
--- -- type GuardianAPI = "search"
--- --   :> QueryParam "q" SearchTerm
--- --   :> QueryParam "api-key" ApiKey
--- --   :> Get '[JSON] GuardianResponse
-
-
-
-
-
---Not sure if this is the correct method to parse the JSON information
--- instance FromJSON GuardianResponse where
---   parseJSON = genericParseJSON
---     return $ GuardianResponse t
-
---Send this into the post body request for the servant API (this should be BlockAppsTokenRequest)
--- But it should have an hflag for the authorization code
-
-------------------------------------------------------------------------------
---Putting information into the vault proxy section
-------------------------------------------------------------------------------
-
--- data GuardianResponse = GuardianResponse {
---   total :: Int
---   } deriving (Eq, Show)
-
--- instance FromJSON GuardianResponse where
---   parseJSON = withObject "response" $ \o -> do
---     r <- o .: "response"
---     t <- r .: "total"
---     return $ GuardianResponse t
-
--- type SearchTerm = T.Text
--- type ApiKey = T.Text
-
--- type GuardianAPI = "search"
---   :> QueryParam "q" SearchTerm
---   :> QueryParam "api-key" ApiKey
---   :> Get '[JSON] GuardianResponse
-
--- guardianAPI :: Proxy GuardianAPI
--- guardianAPI = Proxy
-
--- search = client guardianAPI
-
--- queries :: SearchTerm -> ClientM GuardianResponse
--- queries q = do
---   t <- search (Just q) (Just "test")
---   return t
-
--- main :: IO ()
--- main = do
---   (x:xs) <- getArgs
---   manager' <- newManager tlsManagerSettings
---   res <- runClientM (queries (T.pack x)) (mkClientEnv manager' (BaseUrl Https "content.guardianapis.com" 443 ""))
---   print res
-
