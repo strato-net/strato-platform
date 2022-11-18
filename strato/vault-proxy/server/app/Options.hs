d5487def272762aef00ad4c51b89c25599296f6f
Steven Glasford <lr4fzuk0@anonaddy.me>
2022-11-18 13:46:15 -0600

    Fix inputs for the vaultproxy executable

--- strato/vault-proxy/server/app/Options.hs
+++ strato/vault-proxy/server/app/Options.hs
@@ -8,7 +8,6 @@ import           Data.Text       as T
 
 defineFlag "OAUTH_ENABLED" (True :: Bool) "Enable OAuth2"
 defineFlag "OAUTH_DISCOVERY_URL" (T.pack "" :: Text) "OAuth2 Discovery URL"
-defineFlag "OAUTH_JWT_USERNAME_PROPERTY" (T.pack "" :: Text) "OAuth2 JWT Username Property"
 defineFlag "OAUTH_CLIENT_ID" (T.pack "" :: Text) "OAuth2 Client ID"
 defineFlag "OAUTH_CLIENT_SECRET" (T.pack "" :: Text) "OAuth2 Client Secret"
 defineFlag "OAUTH_RESERVE_SECONDS" (13 :: Int) "How long the system should reserve for the token to expire, default is 13 seconds."
