e44eafe4f744ca614d85a1b01cf09338a895403a
enigmatized <going.to.any.lengths.work@gmail.com>
2022-12-01 23:44:12 -0500

    Remove comments and improve comment

--- utils/dev-scripts/migrate-to-shared-vault/MigrationStep1.sh
+++ utils/dev-scripts/migrate-to-shared-vault/MigrationStep1.sh
@@ -3,7 +3,6 @@ IndexOfLastIndexOfNewVaultDb=$2
 
 
 # STEP 1: Get data from old vault  --> We only need message table and user table
-#docker exec strato_strato_1 bash -c 'apt-get update -y' 
 docker exec strato_strato_1 bash -c $'PGPASSWORD=api psql -U postgres -h postgres oauth -c "\COPY users TO \'userTable.csv\' DELIMITER \',\' CSV HEADER;"'
 docker cp strato_strato_1:var/lib/strato/userTable.csv ./
 
@@ -13,8 +12,7 @@ docker cp strato_strato_1:var/lib/strato/messageTable.csv ./
 
 #STEP 2: remove header from csv file and give proper index to columns, remove columns from old schema, add new oauth provicder id column 
 #     Note this python script cleans both Message table and User table
-#     I don't think message table is really needed
-#     Note this produces an entirely new table
+#     Note this produces an entirely new table just in case there is a issue in the migration process
 python3 cleanDevelopVault.py $ServiceProvider  $IndexOfLastIndexOfVaultDb
 
 
