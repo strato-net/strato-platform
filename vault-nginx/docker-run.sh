8f0d4d1077547bf79a1f7bd286b6f929cd30d564
nikitamendelbaum <nikitamendelbaum@users.noreply.github.com>
2022-12-06 17:13:41 -0500

    support for multiple identity providers in shared vault

--- vault-nginx/docker-run.sh
+++ vault-nginx/docker-run.sh
@@ -6,7 +6,7 @@ ssl=${ssl:-false}
 sslCertFileType=${sslCertFileType:-crt}
 INITIAL_OAUTH_DISCOVERY_URL=${INITIAL_OAUTH_DISCOVERY_URL:-NULL}
 INITIAL_OAUTH_ISSUER=${INITIAL_OAUTH_ISSUER:-NULL}
-INITIAL_OAUTH_JWT_USERNAME_PROPERTY=${INITIAL_OAUTH_JWT_USERNAME_PROPERTY:-sub}
+INITIAL_OAUTH_JWT_USERNAME_CLAIM=${INITIAL_OAUTH_JWT_USERNAME_CLAIM:-sub}
 VAULT_WRAPPER_HOST=${VAULT_WRAPPER_HOST:-vault-wrapper:8000}
 
 # If container is running for the first time - generate config:
@@ -14,15 +14,25 @@ if [ ! -f /usr/local/openresty/nginx/conf/nginx.conf ]; then
   ########
   ### Check the validity of variables combination
   ########
-  # TODO: need to check all INITIAL_values but may be skipped if config file not empty
-  if [[ ${INITIAL_OAUTH_DISCOVERY_URL} = NULL ]] ; then
-    echo 'INITIAL_OAUTH_DISCOVERY_URL is a required env var. Exit'
-    exit 5
-  fi
-  if ! curl --silent --output /dev/null --fail --location "${INITIAL_OAUTH_DISCOVERY_URL}"
-  then
-    echo "OAuth OpenID Connect Discovery URL is unreachable: ${INITIAL_OAUTH_DISCOVERY_URL}. Exit"
-    exit 6
+  if [ ! -f /config/config.json ]; then
+    if [[ ${INITIAL_OAUTH_DISCOVERY_URL} = NULL || ${INITIAL_OAUTH_ISSUER} = NULL ]]; then
+      echo 'INITIAL_OAUTH_DISCOVERY_URL and INITIAL_OAUTH_ISSUER are required env vars when no config file exists. Exit'
+      exit 5
+    fi
+    if ! curl --silent --output /dev/null --fail --location "${INITIAL_OAUTH_DISCOVERY_URL}"
+    then
+      echo "OAuth OpenID Connect Discovery URL is unreachable: ${INITIAL_OAUTH_DISCOVERY_URL}. Exit"
+      exit 6
+    fi
+    echo "{
+  \"identity_providers\": [
+    {
+      \"ISSUER\": \"${INITIAL_OAUTH_ISSUER}\",
+      \"DISCOVERY_URL\": \"${INITIAL_OAUTH_DISCOVERY_URL}\",
+      \"USER_ID_CLAIM\": \"${INITIAL_OAUTH_JWT_USERNAME_CLAIM}\"
+    }
+  ]
+}" > /config/config.json
   fi
 
   ########
@@ -45,10 +55,6 @@ if [ ! -f /usr/local/openresty/nginx/conf/nginx.conf ]; then
   ### Generate .lua scripts from templates according to configuration provided
   ########
   cp /tmp/openid.tpl.lua /tmp/openid.lua
-  # TODO: oauth data is in files
-  sed -i 's*<INITIAL_OAUTH_JWT_USERNAME_PROPERTY_PLACEHOLDER>*'"$INITIAL_OAUTH_JWT_USERNAME_PROPERTY"'*g' /tmp/openid.lua
-  sed -i 's*<INITIAL_OAUTH_DISCOVERY_URL_PLACEHOLDER>*'"$INITIAL_OAUTH_DISCOVERY_URL"'*g' /tmp/openid.lua
-  sed -i 's*<INITIAL_OAUTH_ISSUER_PLACEHOLDER>*'"$INITIAL_OAUTH_ISSUER"'*g' /tmp/openid.lua
 
   if [ "$ssl" = true ] ; then
     sed -i 's/<IS_SSL_PLACEHOLDER_YES_NO>/yes/g' /tmp/openid.lua
--- vault-nginx/openid.tpl.lua
+++ vault-nginx/openid.tpl.lua
@@ -1,13 +1,80 @@
 local openidc = require("resty.openidc")
 local r_jwt = require("resty.jwt")
 local cjson_s = require("cjson.safe")
+local unb64 = ngx.decode_base64
+
+local config = require("cfg-loader")
+
+
+assert(type(config) == "table", "Config should be in the expected format.")
+
+local function get_bearer_access_token_from_header(header)
+  local err
+  local divider = header:find(' ')
+  if divider == 0 or string.lower(header:sub(0, divider - 1)) ~= string.lower("Bearer") then
+    err = "no Bearer authorization header value found"
+    ngx.log(ngx.ERR, err)
+    return nil, err
+  end
+
+  local access_token = header:sub(divider + 1)
+  if access_token == nil then
+    err = "no Bearer access token value found"
+    ngx.log(ngx.ERR, err)
+    return nil, err
+  end
+
+  return access_token, err
+end
+
+
+-- From lua-resty-openidc
+local function openidc_base64_url_decode(input)
+  local reminder = #input % 4
+  if reminder > 0 then
+    local padlen = 4 - reminder
+    input = input .. string.rep('=', padlen)
+  end
+  input = input:gsub('%-', '+'):gsub('_', '/')
+  return unb64(input)
+end
+
+
+local function get_access_token_issuer_unverified(header)
+  local access_token, err1 = get_bearer_access_token_from_header(header)
+  if err or not access_token then
+    ngx.status = 401
+    ngx.say("Wrong Authorization header format. Error: " .. (err or 'unknown error'))
+    ngx.exit(ngx.HTTP_UNAUTHORIZED)
+    return
+  end
+  local enc_hdr, enc_payload, enc_sign = string.match(header, '^(.+)%.(.+)%.(.*)$')
+  if not enc_hdr or not enc_payload or not enc_sign then
+    ngx.status = 401
+    ngx.say("Authorization error: Wrong JWT format")
+    ngx.exit(ngx.HTTP_UNAUTHORIZED)
+    return
+  end
+  local payload = cjson_s.decode(openidc_base64_url_decode(enc_payload))
+  if payload then
+    if not payload['iss'] then
+      ngx.status = 401
+      ngx.say("Authorization error: No 'iss' in JWT payload")
+      ngx.exit(ngx.HTTP_UNAUTHORIZED)
+      return
+    end
+    return payload['iss']
+  end
+  ngx.status = 401
+  ngx.say("Authorization error: Wrong access token format.")
+  ngx.exit(ngx.HTTP_UNAUTHORIZED)
+  return
+end
+
 
 local function isEmpty(s)
   return s == nil or s == ''
 end
-
--- Which property of access token payload to use as STRATO account name
-local username_property = "<INITIAL_OAUTH_JWT_USERNAME_PROPERTY_PLACEHOLDER>"
 
 local node_host_with_protocol = string.format("<REDIRECT_URI_SCHEME_PLACEHOLDER_HTTP_HTTPS>://%s/", ngx.var.http_host)
 
@@ -16,59 +83,15 @@ local USER_ID = ''
 if ngx.req.get_headers()["Authorization"] then
   
   local auth_header = ngx.req.get_headers()["Authorization"]
-  
-  -- TODO: MOVE TO CONFIG (if config empty - validate INITIAL_ vars and create config)
-  local identity_providers = {
-    ["<INITIAL_OAUTH_ISSUER_PLACEHOLDER>"] = {DISCOVERY_URL = "<INITIAL_OAUTH_DISCOVERY_URL_PLACEHOLDER>", USER_ID_CLAIM = "<INITIAL_OAUTH_JWT_USERNAME_PROPERTY_PLACEHOLDER>"},
-    ["https://keycloak.blockapps.net/auth/realms/public"] = {DISCOVERY_URL = "https://keycloak.blockapps.net/auth/realms/public/.well-known/openid-configuration", USER_ID_CLAIM = "sub"},
-  }
-  
---   -- TODO: get ISSUER from Bearer
---   local header = auth_header
---   local divider = header:find(' ')
---   if divider == 0 or string.lower(header:sub(0, divider - 1)) ~= string.lower("Bearer") then
---     err = "no Bearer authorization header value found"
---     ngx.log(ngx.STDERR, err)
---     return nil, err
---   end
---    
---   local access_token = header:sub(divider + 1)
---   if access_token == nil then
---     err = "no Bearer access token value found"
---     ngx.log(ngx.STDERR, err)
---     return nil, err
---   end
--- 
---   local enc_hdr, enc_payload, enc_sign = string.match(auth_header, '^(.+)%.(.+)%.(.*)$')
---   local payload = cjson_s.decode(openidc.openidc_base64_url_decode(enc_payload))
---   local ISSUER = payload['iss']
-  
--- --   ngx.log(payload)
--- --   ngx.say(payload)
--- --   ngx.exit(ngx.HTTP_FORBIDDEN)
---   local jwt = require "resty.jwt"
---   local jwt_obj = jwt:load_jwt(access_token)
---   local cjson = require "cjson"
---   ngx.say(cjson.encode(jwt_obj))
---   ngx.exit(ngx.HTTP_FORBIDDEN)
-  
-  
---   if enc_payload then
---     ngx.say('before jwt')
---     local jwt = openidc.openidc_load_jwt_none_alg(enc_hdr, enc_payload)
---     ngx.say(jwt)
---     if jwt then
---       ngx.say(jwt)
---       ngx.exit(ngx.HTTP_FORBIDDEN)
---     end
---   end
-  
-  
-  
-  local ISSUER = 'https://keycloak.blockapps.net/auth/realms/strato-devel'
-  
+  local identity_providers = config["identity_providers_keyed"]
+  local ISSUER = get_access_token_issuer_unverified(auth_header)
   local PROVIDER_DATA = identity_providers[ISSUER]
 
+  if not PROVIDER_DATA then
+    ngx.status = 401
+    ngx.say("Authorization error: Unsupported access token issuer (unknown identity provider)")
+    ngx.exit(ngx.HTTP_UNAUTHORIZED)
+  end
 
   local verify_opts = {
     discovery = PROVIDER_DATA['DISCOVERY_URL'],
@@ -80,9 +103,9 @@ if ngx.req.get_headers()["Authorization"] then
   local verify_res, verify_err = openidc.bearer_jwt_verify(verify_opts)
 
   if verify_err or not verify_res then
-    ngx.status = 403
+    ngx.status = 401
     ngx.say("Authorization header is provided but the bearer token is invalid or expired. Error: " .. (verify_err or 'unknown error'))
-    ngx.exit(ngx.HTTP_FORBIDDEN)
+    ngx.exit(ngx.HTTP_UNAUTHORIZED)
   end
 
   if not isEmpty(verify_res[PROVIDER_DATA['USER_ID_CLAIM']]) then
@@ -90,7 +113,7 @@ if ngx.req.get_headers()["Authorization"] then
   else 
     ngx.status = 500
     user_err_msg = 'Could not authenticate the request. Unexpected format of bearer token for that issuer.'
-    ngx.log(ngx.STDERR, user_err_msg .. ' Error details: Failed to find claims \''..PROVIDER_DATA['USER_ID_CLAIM']..'\' in payload of id_token obtained with openidc.authenticate(). Possible reason: OAUTH_SCOPE does not have the required scope for \''..username_property..'\' claim (current scope value: \''..authenticate_opts.scope..'\')')
+    ngx.log(ngx.STDERR, user_err_msg .. ' Error details: Failed to find claim \''..PROVIDER_DATA['USER_ID_CLAIM']..'\' in payload of the token.')
     ngx.say(user_err_msg..' Please contact STRATO Vault administrator.')
     ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
   end
@@ -104,5 +127,5 @@ end
 -- set request headers to forward to APIs
 ngx.req.set_header("X-USER-UNIQUE-NAME", USER_ID)
 ngx.req.set_header("X-IDENTITY-PROVIDER-ID", ISSUER)
--- removing the Authorization header FROM REQUEST to prevent Postgrest's built-in JWT permissioning to trigger
+-- removing the Authorization header FROM REQUEST to prevent downstream services from using it by mistake.
 ngx.req.clear_header("Authorization")
