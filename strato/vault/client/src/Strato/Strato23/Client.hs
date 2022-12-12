93734ee56d71ec05b3fee4b33a41d254309440b4
enigmatized <going.to.any.lengths.work@gmail.com>
2022-12-12 13:26:00 -0500

    Remove un-used client bindings, also for types associated with client bindings change header to X-USER-ACCESS-TOKEN rather than X-USER-UNIQUE-NAME

--- strato/vault/client/src/Strato/Strato23/Client.hs
+++ strato/vault/client/src/Strato/Strato23/Client.hs
@@ -3,16 +3,10 @@
 module Strato.Strato23.Client
   ( getPing
   , getKey
-  , getKey'
   , postKey
-  , postKey'
-  , getKeys'
   , getSharedKey
-  , getSharedKey'
   , getUsers
-  , getUsers'
   , postSignature
-  , postSignature'
   , postPassword
   , verifyPassword
   ) where
@@ -28,35 +22,17 @@ getPing = client (Proxy @ GetPing)
 getKey :: Text -> Maybe Text -> ClientM AddressAndKey
 getKey = client (Proxy @ GetKey)
 
-getKey' :: Text -> Text -> Maybe Text -> ClientM AddressAndKey
-getKey' = client (Proxy @ GetKey')
-
-getKeys' :: Text -> Text -> Maybe Text -> ClientM [AddressAndKey]
-getKeys' = client (Proxy @ GetKeys')
-
 postKey :: Text -> ClientM AddressAndKey
 postKey = client (Proxy @ PostKey)
-
-postKey' :: Text -> Text -> ClientM AddressAndKey
-postKey' = client (Proxy @ PostKey')
 
 getSharedKey :: Text -> PublicKey -> ClientM SharedKey
 getSharedKey = client (Proxy @ GetSharedKey)
 
-getSharedKey' :: Text -> Text -> PublicKey -> ClientM SharedKey
-getSharedKey' = client (Proxy @ GetSharedKey')
-
 getUsers :: Text -> Maybe Address -> Maybe Int -> Maybe Int -> ClientM [User]
 getUsers = client (Proxy @ GetUsers)
 
-getUsers' :: Text ->  Text -> Maybe Address -> Maybe Int -> Maybe Int -> ClientM [User] -- External Vault
-getUsers' = client (Proxy @ GetUsers')
-
 postSignature :: Text -> MsgHash -> ClientM Signature
 postSignature = client (Proxy @ PostSignature)
-
-postSignature' :: Text -> Text ->  MsgHash -> ClientM Signature
-postSignature' = client (Proxy @ PostSignature')
 
 postPassword :: Text -> ClientM ()
 postPassword = client (Proxy @ PostPassword)
