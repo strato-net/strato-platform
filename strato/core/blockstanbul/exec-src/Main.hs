ced6c247b9c3bda99cc282a279be7d04d6b8d339
nikitamendelbaum <nikitamendelbaum@users.noreply.github.com>
2022-12-14 15:08:02 -0500

    nodekey is Nothing

--- strato/core/blockstanbul/exec-src/Main.hs
+++ strato/core/blockstanbul/exec-src/Main.hs
@@ -115,7 +115,7 @@ main = do
   mgr <- newManager defaultManagerSettings
   vaultUrl <- parseBaseUrl "http://localhost:8013/strato/v2.3"
   optSender <- do 
-    eAdAndKey <- runClientM (VC.getKey (T.pack "nodekey") Nothing) (mkClientEnv mgr vaultUrl)
+    eAdAndKey <- runClientM (VC.getKey Nothing Nothing) (mkClientEnv mgr vaultUrl)
     case eAdAndKey of
       Left err -> die $ "failed to get address from the admin node's vault: " ++ show err
       Right adAndKey -> return $ VC.unAddress adAndKey
