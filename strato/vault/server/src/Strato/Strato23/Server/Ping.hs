4c0e27131c2007d012264a54c86205c19ade9828
Steven Glasford <lr4fzuk0@anonaddy.me>
2022-12-21 11:06:08 -0600

    Remove Comment from getPing

--- strato/vault/server/src/Strato/Strato23/Server/Ping.hs
+++ strato/vault/server/src/Strato/Strato23/Server/Ping.hs
@@ -2,6 +2,5 @@ module Strato.Strato23.Server.Ping where
 
   import           Strato.Strato23.Monad
 
--- getPing will return the version of the vault found in the package.yaml file 
   getPing :: VaultM String
   getPing = return "pingDetail"
