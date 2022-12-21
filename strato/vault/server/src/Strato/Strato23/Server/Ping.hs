e860296842ba583befb6947aaae22f8093308ad7
Steven Glasford <lr4fzuk0@anonaddy.me>
2022-12-21 11:05:40 -0600

    Remove $

--- strato/vault/server/src/Strato/Strato23/Server/Ping.hs
+++ strato/vault/server/src/Strato/Strato23/Server/Ping.hs
@@ -4,4 +4,4 @@ module Strato.Strato23.Server.Ping where
 
 -- getPing will return the version of the vault found in the package.yaml file 
   getPing :: VaultM String
-  getPing = return $ "pingDetail"
+  getPing = return "pingDetail"
