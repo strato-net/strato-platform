8932735e1bb8b776ca4566d003954cac4a7183ae
Steven Glasford <lr4fzuk0@anonaddy.me>
2022-12-21 11:04:33 -0600

    Revert Ping.hs to normal

--- strato/vault/server/src/Strato/Strato23/Server/Ping.hs
+++ strato/vault/server/src/Strato/Strato23/Server/Ping.hs
@@ -1,10 +1,7 @@
 module Strato.Strato23.Server.Ping where
 
   import           Strato.Strato23.Monad
-  import           Data.Yaml
 
 -- getPing will return the version of the vault found in the package.yaml file 
   getPing :: VaultM String
-  getPing = do
-    veri <- 
-    return $ "pingDetail: " ++ veri
+  getPing = return $ "pingDetail: " ++ veri
