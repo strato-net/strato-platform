82bc6a20d15df759ba0205eb64f1f9343f807145
Tanuj Soni <tanuj@blockapps.net>
2022-12-23 21:14:55 +0530

    [STRATO-2944_dev-doc-link] Dev link updated

--- smd-ui/src/components/MenuBar/index.js
+++ smd-ui/src/components/MenuBar/index.js
@@ -28,8 +28,8 @@ class MenuBar extends Component {
           <button className="pt-button pt-minimal pt-small" onClick={() => { mixpanelWrapper.track("contact_blockapps_support_click") }}>Contact BlockApps Support</button>
         </a>
         <span className="pt-navbar-divider" />
-        <a href='https://internal.blockapps.net ' target="_black" rel="noopener noreferrer">
-          <button className="pt-button pt-minimal pt-small" onClick={() => { mixpanelWrapper.track("internal_blockapps_click") }}>Dev Docs</button>
+        <a href='https://docs.blockapps.net/' target="_black" rel="noopener noreferrer">
+          <button className="pt-button pt-minimal pt-small" onClick={() => { mixpanelWrapper.track("docs_blockapps_click") }}>Dev Docs</button>
         </a>
         {isOauthEnabled() && <span><span className="pt-navbar-divider" />
           <small className="pt-text-muted welcome-user"> {this.props.oauthUser ? this.props.oauthUser.commonName : ''} </small>
