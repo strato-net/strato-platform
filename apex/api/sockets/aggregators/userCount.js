cc6312a3572c154f197e3734c83a052185b4cb0e
nikitamendelbaum <nikitamendelbaum@users.noreply.github.com>
2022-12-29 12:56:29 -0500

    better user count logic for smd

--- apex/api/sockets/aggregators/userCount.js
+++ apex/api/sockets/aggregators/userCount.js
@@ -2,22 +2,24 @@ const { USERS_COUNT } = require('../rooms')
 const { emitter, ON_SOCKET_PUBLISH_EVENTS } = require('../eventBroker')
 const Transaction = require ('../../models/strato/eth/transaction');
 const config = require('../../config/app.config')
+const db = require("../../models/strato/eth/connection");
 
 let userCount
 
 function getUserCount() {
-  // TODO: replace with query for the count of registered Certs
+  // TODO: replace with query to count the registered user certificates
   // Temporary counting users who posted at least one transaction:
   Transaction.count(
       {
         distinct: true,
         col: 'from_address', 
         where: {
-          origin: 'API',
+          origin: { [db.Sequelize.Op.eq]: 'API' },
+          from_address: { [db.Sequelize.Op.ne]: '0000000000000000000000000000000000000000' },
         }
       }
   ).then(count => {
-    const newUserCount = count - 2  // Excluding the faucet account from genesis block and '0000000000000000000000000000000000000000'
+    const newUserCount = (count >= 1) ? count - 1 : count  // Excluding the faucet account address coming from a genesis block
     if (userCount !== newUserCount) {
       userCount = newUserCount
       emitter.emit(ON_SOCKET_PUBLISH_EVENTS, USERS_COUNT, userCount)
