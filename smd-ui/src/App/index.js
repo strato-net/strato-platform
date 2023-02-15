47e0a7afe5c5e3b43df4cfcfdbea2871f31e0fe9
Aya Abdelgawad <aya_abdelgawad@yahoo.com>
2023-02-15 16:07:40 -0500

    cleaned out test code and comments

--- smd-ui/src/App/index.js
+++ smd-ui/src/App/index.js
@@ -11,37 +11,13 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { env } from '../env';
 import LoadingBar from 'react-redux-loading-bar'
 import { connect } from 'react-redux';
-import { withRouter, useLocation } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import { isOauthEnabled } from '../lib/checkMode';
 import { getOrCreateOauthUserRequest } from '../components/User/user.actions';
 import { getUserFromLocal } from '../lib/localStorage';
-// for google analytics support vvv
-import { createBrowserHistory } from "history";
 import ReactGA from "react-ga4";
+
 ReactGA.initialize("G-PWGS3Z6YNQ");
-// version 1: uses history
-// pros: no need to update versions on any other dependencies
-// // cons: soooo ugly
-// const history = createBrowserHistory();
-// history.listen(location => {
-//   //window.location.href.split("#",2)[1]
-//   // alert(window.location.href.split("#",2)[1]);
-//   ReactGA.set({ page: window.location.href.split("#",2)[1] });
-//   ReactGA.send({hitType: "pageview", page: window.location.href.split("#",2)[1]});
-// });
-
-// version 2: uses `useLocation` webhook from react-router-dom
-// pros: cleaner
-// cons: lots of dependency updates -> break anything?
-// function Test(props) {
-//   const location = useLocation();
-//   alert(location.pathname);
-//   ReactGA.send({hitType: "pageview", page: location.pathname, title: "Hellloooo"});
-//   return null;
-// }
-
-
-
 mixpanelWrapper.init('62f1bec01cdb0096be8e8bdd693e0081');
 mixpanelWrapper.identify(env.NODE_NAME);
 
@@ -53,16 +29,9 @@ class App extends Component {
     }
   }
 
-  componentWillUnmount() {
-    console.log("unmountung app...");
-    ReactGA.send({hitType: "timing", timingCategory: "page_view", timingVar: "engagement_time_msec", timingValue: 200000});
-    alert("unmounting App...");
-  }
-
   render() {
     return (
       <div className="App" >
-        {/* <Test></Test> */}
         <LoadingBar style={{ top: '0px', backgroundColor: '#62d96b', zIndex: 999, height: '4px' }} />
         <MenuBar />
         <SideBar />
