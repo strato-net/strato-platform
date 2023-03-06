0bc4b98bc6894c5b704a9321849cc99d5442c943
David Moncayo <david_moncayo@blockapps.net>
2023-03-06 16:16:17 -0500

    test snapshots updated

--- smd-ui/src/tests/SideBar/index.spec.js
+++ smd-ui/src/tests/SideBar/index.spec.js
@@ -33,45 +33,51 @@ describe('SideBar: index', () => {
       expect(wrapper.find('NavLink').get(0)).toMatchSnapshot();
     });
 
-    test('secound position /chains', () => {
+    test('second position /appstore', () => {
       wrapper.find('NavLink').at(1).simulate('click');
       expect(wrapper.find('NavLink').get(1)).toMatchSnapshot();
     });
 
-    test('third position /blocks', () => {
+    test('third position /stats', () => {
       wrapper.find('NavLink').at(2).simulate('click');
       expect(wrapper.find('NavLink').get(2)).toMatchSnapshot();
     });
 
-    test('fourth position /transactions', () => {
+    test('fourth position /chains', () => {
       wrapper.find('NavLink').at(3).simulate('click');
       expect(wrapper.find('NavLink').get(3)).toMatchSnapshot();
     });
 
-    test('fifth position /accounts', () => {
+    test('fifth position /blocks', () => {
       wrapper.find('NavLink').at(4).simulate('click');
       expect(wrapper.find('NavLink').get(4)).toMatchSnapshot();
     });
 
-    test('sixth position /contracts', () => {
+    test('sixth position /transactions', () => {
       wrapper.find('NavLink').at(5).simulate('click');
       expect(wrapper.find('NavLink').get(5)).toMatchSnapshot();
     });
 
-    test('seventh position /code_editor', () => {
+    test('seventh position /accounts', () => {
       wrapper.find('NavLink').at(6).simulate('click');
       expect(wrapper.find('NavLink').get(6)).toMatchSnapshot();
     });
-    
-    test('ninth position /external-storage', () => {
+
+    test('eighth position /contracts', () => {
       wrapper.find('NavLink').at(7).simulate('click');
       expect(wrapper.find('NavLink').get(7)).toMatchSnapshot();
     });
 
-    test('eighth position /appstore', () => {
+    test('ninth position /code_editor', () => {
       wrapper.find('NavLink').at(8).simulate('click');
       expect(wrapper.find('NavLink').get(8)).toMatchSnapshot();
     });
+    
+    test('tenth position /external-storage', () => {
+      wrapper.find('NavLink').at(9).simulate('click');
+      expect(wrapper.find('NavLink').get(9)).toMatchSnapshot();
+    });
+
 
   });
 
