d9b35263a8e2033696fb28f96a08b00a1b57556a
enigmatized <going.to.any.lengths.work@gmail.com>
2023-03-10 12:46:58 -0500

    Remove changes I made

--- smd-ui/src/components/Accounts/components/OauthAccounts/index.js
+++ smd-ui/src/components/Accounts/components/OauthAccounts/index.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import SendTokens from '../SendTokens';
 import AccountDetail from '../../../AccountDetail';
-import {Alert } from '@blueprintjs/core';
 import {
   fetchOauthAccountDetail,
   resetOauthUserAccount,
