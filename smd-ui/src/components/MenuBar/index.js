e1a55ed6f5fc9aeb3ae9c7ca8bc5e8441cc44f7a
David Moncayo <david_moncayo@blockapps.net>
2023-03-15 17:46:58 -0400

    style changes

--- smd-ui/src/components/MenuBar/index.js
+++ smd-ui/src/components/MenuBar/index.js
@@ -6,7 +6,7 @@ import './menubar.css';
 import logo from './BlockAppsLogos_DarkBG-Stacked.png';
 import { env } from '../../env';
 import { isOauthEnabled } from '../../lib/checkMode';
-import { Popover, Button, Menu, Position, MenuItem, MenuDivider  } from '@blueprintjs/core';
+import { Popover, Button, Menu, Position, MenuItem } from '@blueprintjs/core';
 
 class MenuBar extends Component {
 
@@ -24,7 +24,10 @@ class MenuBar extends Component {
     return (
       <div>
         <Popover content={userDropdown} position={Position.BOTTOM_RIGHT}>
-          <Button disabled={!this.props.oauthUser || !isOauthEnabled()} iconName={this.props.oauthUser ? "user" : "social-media"} text={this.props.oauthUser ? this.props.oauthUser.commonName : 'Verification Pending'} />
+          <Button 
+            className={"pt-large pt-minimal " + (this.props.oauthUser ? 'pt-intent-primary' : 'pt-intent-warning')} 
+            iconName={this.props.oauthUser ? "user" : "social-media"} 
+            text={this.props.oauthUser ? (this.props.oauthUser.commonName + ', ' + this.props.oauthUser.organization + ' ' + this.props.oauthUser.organizationalUnit) : 'Verification Pending'} />
         </Popover>
       </div>
     );
@@ -33,8 +36,21 @@ class MenuBar extends Component {
   render() {
     const helpDropdown = (
       <Menu>
-          <MenuItem className="pt-button pt-minimal pt-small" onClick={() => { mixpanelWrapper.track("docs_blockapps_click") }} href='https://docs.blockapps.net/' target="_black" rel="noopener noreferrer" iconName="document" text="Documentation" />
-          <MenuItem className="pt-button pt-minimal pt-small" onClick={() => { mixpanelWrapper.track("contact_blockapps_support_click") }} href='https://support.blockapps.net' target="_black" rel="noopener noreferrer" iconName="headset" text="Contact Us" />
+          <MenuItem 
+            className="pt-button pt-minimal pt-small" 
+            onClick={() => { mixpanelWrapper.track("docs_blockapps_click") }} 
+            href='https://docs.blockapps.net/' 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            iconName="document"
+            text="Documentation" />
+          <MenuItem className="pt-button pt-minimal pt-small" 
+            onClick={() => { mixpanelWrapper.track("contact_blockapps_support_click") }}
+            href='https://support.blockapps.net' 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            iconName="headset" 
+            text="Support" />
           <small className="pt-text-muted pt-align-right">STRATO {env.STRATO_VERSION}</small>
       </Menu>
     );
@@ -59,7 +75,7 @@ class MenuBar extends Component {
         <div className="pt-navbar-group pt-align-right">
           {this.afterLoggedIn()}
           <Popover content={helpDropdown} position={Position.BOTTOM_RIGHT}>
-            <Button style={{ marginLeft: 10}} iconName="help"/>
+            <Button className="pt-minimal pt-large" style={{ marginLeft: 10}} iconName="help"/>
           </Popover>
         </div>
       </nav>
