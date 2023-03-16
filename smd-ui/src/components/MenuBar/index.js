8b5dd86735321df65d96fc9aeb8491f06e19d132
David Moncayo <david_moncayo@blockapps.net>
2023-03-16 15:02:00 -0400

    added my profile button

--- smd-ui/src/components/MenuBar/index.js
+++ smd-ui/src/components/MenuBar/index.js
@@ -5,22 +5,23 @@ import mixpanelWrapper from '../../lib/mixpanelWrapper';
 import './menubar.css';
 import logo from './strato-mercata-beta-white.png';
 import { env } from '../../env';
-import { Popover, Button, Menu, Position, MenuItem } from '@blueprintjs/core';
+import { Popover, Button, Menu, Position, MenuItem, Dialog, Intent, MenuDivider } from '@blueprintjs/core';
 import {
   searchQueryRequest,
 } from '../SearchResults/searchresults.actions';
 import {
   getUserCertificateRequest,
 } from "../User/user.actions"
+import HexText from '../HexText';
 
 class MenuBar extends Component {
   constructor() {
     super()
     this.state = {
-     searchQuery:""
+     searchQuery:"",
+     isUserMenuOpen: false
     }
   }
-
   componentWillReceiveProps(newProps) {
     if (newProps.oauthUser && !newProps.userCertificate) {
       this.props.getUserCertificateRequest(newProps.oauthUser.address)
@@ -38,6 +39,41 @@ class MenuBar extends Component {
     this.setState({ searchQuery: searchQuery });
   }
 
+  renderUserProfileInfo = () => {
+    const { 
+      certificateString, 
+      organization, 
+      organizationalUnit, 
+      commonName,
+      address,
+      block_timestamp
+    } = this.props.userCertificate
+
+    const dateCreated = new Date(block_timestamp).toLocaleDateString('en-us', {year:"numeric", month:"short", day:"numeric"}) 
+
+    return (
+      <div className='pt-dark'>
+        <h3>Organization</h3>
+        <h4>{organization} {organizationalUnit ? `| ${organizationalUnit}`: ""}</h4>
+        <MenuDivider />
+
+        <h3>Name</h3>
+        <h4>{commonName}</h4>
+        <MenuDivider />
+
+        <h3>STRATO Mercata Address</h3>
+        <HexText value={'0x' + address} shorten={false}/>
+
+        <h3>Date Created</h3>
+        <h4>{dateCreated}</h4>
+        <MenuDivider />
+
+        <h3>Certificate <span className='pt-monospace-text'>.pem</span></h3>
+        <pre>{certificateString}</pre>
+      </div>      
+    )
+  }
+
 
   handleKeyDown = (e) => {
 
@@ -47,14 +83,61 @@ class MenuBar extends Component {
     }
   }
   
+  // on-submit search function calls searchQueryRequest
+
+  toggleDialog = () => {
+    this.setState({ isUserMenuOpen: !this.state.isUserMenuOpen })
+  }
+
   afterLoggedIn() {
     const userDropdown =
       <Menu>
+        <MenuItem className="pt-button pt-minimal" onClick={this.toggleDialog} target="_blank" rel="noopener noreferrer" iconName="mugshot" text={this.props.userCertificate ? "My Profile" : "More Info"}/> 
         <MenuItem className="pt-button pt-minimal" onClick={this.logout} target="_blank" rel="noopener noreferrer" iconName="log-out" text="Logout" /> 
       </Menu>
 
     return (
       <div>
+        <Dialog
+          className="pt-dark"
+          iconName="mugshot"
+          isOpen={this.state.isUserMenuOpen}
+          onClose={this.toggleDialog}
+          title="My Profile"
+        >
+          <div className="pt-dialog-body">
+            {
+              this.props.userCertificate ? (
+                <div>
+                  {this.renderUserProfileInfo()}
+                </div> ) 
+                : (
+                <div>
+                  <h4>Your STRATO Mercata address: <br/><HexText value={this.props.oauthUser ? "0x" + this.props.oauthUser.address : null} shorten={false}/></h4><br/>
+                  <h4>Your STRATO Mercata account is currently being verified. You should receive an e-mail confirmation when your account is ready. Welcome to the Block!</h4><br/>
+                  <MenuDivider />
+                  <h5>If your account still hasn't been verified, <a
+                      onClick={() => { mixpanelWrapper.track("contact_blockapps_support_click") }}
+                      href='https://support.blockapps.net' 
+                      target="_blank" 
+                      rel="noopener noreferrer">contact support here.</a>
+                  </h5>
+                </div>
+              )
+            }
+          </div>
+          <div className="pt-dialog-footer">
+              <div className="pt-dialog-footer-actions">
+                  <Button
+                      className="pt-minimal"
+                      intent={Intent.DANGER}
+                      onClick={this.toggleDialog}
+                      text="Close"
+                  />
+              </div>
+          </div>
+        </Dialog>
+
         <Popover content={userDropdown} position={Position.BOTTOM_RIGHT}>
           <Button 
             className={"pt-large pt-minimal " + (this.props.userCertificate ? 'pt-intent-primary' : 'pt-intent-warning')} 
