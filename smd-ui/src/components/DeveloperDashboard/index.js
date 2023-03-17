d3e7f8c9b8b2d2f6fd20b3d40b0045213be3fdcc
Ariya Amarjargal <amarj003@umn.edu>
2023-03-17 10:21:48 -0500

    Commit before getting new logos

--- smd-ui/src/components/DeveloperDashboard/index.js
+++ smd-ui/src/components/DeveloperDashboard/index.js
@@ -4,7 +4,9 @@ import mixpanelWrapper      from '../../lib/mixpanelWrapper';
 import React, { Component } from 'react';
 import ReactGA              from 'react-ga4';
 import githubLogo      from './github.png';
-import blockappsLogo       from './BlockAppsLogos_DarkBG-Horizontal.png';
+// import blockappsLogo       from './BlockAppsLogos_DarkBG-Horizontal.png';
+import APILogo       from './apidocs.png';
+import devLogo       from './devdocs.png';
 
 
 
@@ -25,32 +27,32 @@ class DevDash extends Component {
     render() {
       
       const appsList = [{ 
-          appName: "Youtube Guides @BlockApps", 
+          appName: "BlockApps Youtube", 
           urlToApp: 'https://www.youtube.com/@BlockApps ' ,
-          description: 'Include text here',
+          description: 'Video walkthroughs and tutorials to help guide you through your development on STRATO Mercata.',
           image: youtubeLogo
         } ,  {
           appName: "Asset Framework Repository", 
           urlToApp: 'https://github.com/blockapps/asset-framework ', 
-          description: 'Include text here',
+          description: 'Asset Framework on Github — a project generation tool for asset-based applications built on STRATO Mercata.',
           image: githubLogo
       }, 
       {
         appName: "API Docs", 
         urlToApp: 'https://docs.blockapps.net/blockapps-rest/', 
-        description: 'Include text here',
-        image: blockappsLogo
+        description: 'STRATO API reference - Reference guide containing all available STRATO Mercata API endpoints to use on your development journey.',
+        image: APILogo
       }, 
       {
         appName: "Developer Docs", 
         urlToApp: 'https://docs.blockapps.net/', 
-        description: 'Include text here',
-        image: blockappsLogo
+        description: 'Comprehensive documentation on STRATO Mercata features, key concepts, application development, and more!',
+        image: devLogo
       },
       {
         appName: "VS Code Extension", 
         urlToApp: 'https://marketplace.visualstudio.com/items?itemName=BlockApps.strato-vscode', 
-        description: 'Include text here',
+        description: 'Easily interact and develop on STRATO Mercata using an Integrated Development Environment directly through VS Code.',
         image: vscodeLogo
       }
     ];
