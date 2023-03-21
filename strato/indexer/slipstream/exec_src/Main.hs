5919378b413b2b04a0ea5e33e46eb18041193593
enigmatized <going.to.any.lengths.work@gmail.com>
2023-03-21 18:58:25 -0400

    [WIP] code clean up

--- strato/indexer/slipstream/exec_src/Main.hs
+++ strato/indexer/slipstream/exec_src/Main.hs
@@ -33,7 +33,6 @@ import System.Clock
 import System.Exit
 import Text.Printf
 import Text.RawString.QQ
-import Text.Regex.Posix
 
 import BlockApps.Bloc22.Monad (BlocEnv(..))
 import BlockApps.Init
@@ -44,6 +43,7 @@ import Control.Monad.Composable.Kafka
 import Control.Monad.Composable.SQL
 
 import Slipstream.MessageConsumer
+import Slipstream.Data.Globals
 import Slipstream.Globals
 import Slipstream.GlobalsColdStorage
 import Slipstream.Options
@@ -98,8 +98,8 @@ main = do
     -- After a ./strato --stop
     -- The in memory state gets wiped
     -- This causes cirrus issues
-    -- Below scrapes the cirrus tableNames and column names
-    -- to restore the in memory map that tracks this
+    -- Below scrapes the cirrus table names and column names
+    -- to restore the in memeory information
 
     let getPGValues :: MonadIO m => B.ByteString -> m [PGValues]
         getPGValues = liftIO . pgQuery conn
@@ -125,55 +125,14 @@ main = do
     let convertFromPGTextValueToText :: PGValue -> Maybe T.Text
         convertFromPGTextValueToText c = case  c of (PGTextValue txt )  -> Just $ decodeUtf8 txt ;  _ -> Nothing 
         
-        thisListOfTableNames = (mapMaybe convertFromPGTextValueToByteString) <$> allTableNamesInPGValue :: IO [B.ByteString]
+        thisListOfTableNames = (mapMaybe convertFromPGTextValueToByteString) <$> allTableNamesInPGValue        :: IO [B.ByteString]
+        sqlStatement x       = (encodeUtf8 "SELECT column_name FROM information_schema.columns WHERE table_name Like \'") <> x <> (encodeUtf8 "\';")   :: B.ByteString
+        tableNamesWithPgValues      = join $ (sequence <$>) $ (map  (\tableNam -> ((parseStringToTableName $ show tableNam ,)  <$>) $ getColumnNames . sqlStatement $ tableNam ) <$> thisListOfTableNames)     :: IO [(TableName, [PGValues])]
+        createdTables        = (M.map ( mapMaybe convertFromPGTextValueToText . concat) <$>)  $ M.fromList <$>  tableNamesWithPgValues :: IO (M.Map  TableName TableColumns)              
+    -- Scrape Finished 
 
-        sqlStatement x = (encodeUtf8 "SELECT column_name FROM information_schema.columns WHERE table_name Like \'") <> x <> (encodeUtf8 "\';")  :: B.ByteString
-    
-        namesToPgValues  =  (map  (\tableNam -> ((show tableNam ,)  <$>) $ getColumnNames . sqlStatement $ tableNam ) <$> thisListOfTableNames) :: IO [IO  (String, [PGValues])]
-
-        tableNameMap = (M.map  concat <$>)  $ (M.fromList <$>) $ join $ (sequence) <$> namesToPgValues   :: IO (M.Map  String [PGValue])
-
-        mapBytStringTablecolumns = (M.map ( mapMaybe convertFromPGTextValueToText )) <$> tableNameMap  :: IO (M.Map  String TableColumns) --TODO Make sure maybeMap does not filter things we want
-    
-    -- Helpers to make TableNames
-    let stringArrToHistoryTableName :: [T.Text]  -> TableName
-        stringArrToHistoryTableName [contract]           = HistoryTableName T.empty T.empty  contract
-        stringArrToHistoryTableName [org, contract]      = HistoryTableName  org  contract  contract
-        stringArrToHistoryTableName [org, app, contract] = HistoryTableName  org  app  contract
-        stringArrToHistoryTableName _ = error "whoops"
-    let stringArrToIndexTableName :: [T.Text]  -> TableName
-        stringArrToIndexTableName [contract]           = IndexTableName T.empty T.empty  contract
-        stringArrToIndexTableName [org, contract]      = IndexTableName  org  contract  contract
-        stringArrToIndexTableName [org, app, contract] = IndexTableName  org  app  contract
-        stringArrToIndexTableName _ = error "whoops"
-    let stringArrToEventTableName :: [T.Text]  -> TableName
-        stringArrToEventTableName [contract, eventName]           = EventTableName T.empty T.empty contract eventName
-        stringArrToEventTableName [org, contract, eventName]      = EventTableName org contract contract eventName
-        stringArrToEventTableName [org, app, contract, eventName] = EventTableName org app contract eventName
-        stringArrToEventTableName _ = error "whoops"
-        
-        period = "."         :: String
-        history = "history@" :: String
-
-    let parseStringToTableName :: String -> TableName
-        parseStringToTableName bs
-            | bs =~ period  :: Bool = let (tableStuff, _, eventName) = bs =~ period  :: (String, String, String)
-                                         in stringArrToEventTableName $ (T.splitOn (T.pack "-") $ T.pack tableStuff ) ++ [(T.pack eventName)]
-            | bs =~ history :: Bool = let (_, _, tableStuff) = bs =~ history  :: (String, String, String) 
-                                         in stringArrToHistoryTableName $ T.splitOn  (T.pack "-") $ T.pack tableStuff
-            | otherwise                = stringArrToIndexTableName $ T.splitOn (T.pack "-") (T.pack bs)                                           
-        
-
-        createdTables =  (M.mapKeysMonotonic parseStringToTableName) <$> mapBytStringTablecolumns :: IO (M.Map  TableName TableColumns)
-
-
-    liftIO $ join $ putStrLn . show <$> createdTables 
-    
+    liftIO $ join $ putStrLn . show <$> createdTables     
     liftIO $  putStrLn "HELLO2 GARRETT"
-
-
-
-
 
     -- There are three permanent connections/pools to postgres:
     -- 1. The `workerConn` is from persistent-postgresql for the storage worker in the background
@@ -188,3 +147,4 @@ main = do
     sqlEnv <- createBlocSQLEnv flags_pghost (fromIntegral flags_pgport) flags_pguser flags_password
       
     getAndProcessMessages env sqlEnv conn gref
+    
--- strato/indexer/slipstream/src/Slipstream/Data/Globals.hs
+++ strato/indexer/slipstream/src/Slipstream/Data/Globals.hs
@@ -5,7 +5,11 @@
 module Slipstream.Data.Globals (
   Globals(..),
   TableColumns,
-  TableName(..)
+  TableName(..),
+  stringArrToHistoryTableName,
+  stringArrToIndexTableName,
+  stringArrToEventTableName,
+  parseStringToTableName
   ) where
 
 import           Control.DeepSeq
@@ -13,8 +17,10 @@ import           Data.Cache.LRU
 import qualified Data.HashMap.Strict as HM
 import qualified Data.Map.Strict     as M
 import qualified Data.Set            as S
-import           Data.Text
+import qualified Data.Text           as T 
 import           GHC.Generics
+import           Text.Regex.Posix
+
 
 import           BlockApps.Solidity.Value
 import           Blockchain.Strato.Model.Account
@@ -33,27 +39,61 @@ instance NFData (TableName) where
 data Globals = Globals { createdTables :: M.Map TableName TableColumns
                        , historyList :: M.Map TableName Bool
                        , createdInstances :: S.Set CodePtr -- lets us avoid an extra bloc call
-                       , solidVMInfo :: HM.HashMap Keccak256 (M.Map Text CodePtr)
-                       , contractStates :: LRU Account [(Text, Value)]
+                       , solidVMInfo :: HM.HashMap Keccak256 (M.Map T.Text CodePtr)
+                       , contractStates :: LRU Account [(T.Text, Value)]
                        , csHandle :: Handle
                        } deriving (Generic, NFData)
 
 data TableName = 
     IndexTableName
-      { itOrganization :: Text
-      , itApplication  :: Text
-      , itContractName :: Text
+      { itOrganization :: T.Text
+      , itApplication  :: T.Text
+      , itContractName :: T.Text
       }
   | HistoryTableName -- technically the same as index, but logically different
-      { htOrganization :: Text
-      , htApplication  :: Text
-      , htContractName :: Text
+      { htOrganization :: T.Text
+      , htApplication  :: T.Text
+      , htContractName :: T.Text
       }
   | EventTableName
-      { etOrganization :: Text
-      , etApplication  :: Text
-      , etContractName :: Text
-      , etEventName    :: Text
+      { etOrganization :: T.Text
+      , etApplication  :: T.Text
+      , etContractName :: T.Text
+      , etEventName    :: T.Text
       } deriving (Show, Eq, Ord)
 
-type TableColumns = [Text]
+type TableColumns = [T.Text]
+
+stringArrToHistoryTableName :: [T.Text]  -> TableName
+stringArrToHistoryTableName [contract]           = HistoryTableName T.empty T.empty  contract
+stringArrToHistoryTableName [org, contract]      = HistoryTableName  org  contract  contract
+stringArrToHistoryTableName [org, app, contract] = HistoryTableName  org  app  contract
+stringArrToHistoryTableName _ = error "whoops"
+
+
+stringArrToIndexTableName :: [T.Text]  -> TableName
+stringArrToIndexTableName [contract]           = IndexTableName T.empty T.empty  contract
+stringArrToIndexTableName [org, contract]      = IndexTableName  org  contract  contract
+stringArrToIndexTableName [org, app, contract] = IndexTableName  org  app  contract
+stringArrToIndexTableName _ = error "whoops"
+  
+stringArrToEventTableName :: [T.Text]  -> TableName
+stringArrToEventTableName [contract, eventName]           = EventTableName T.empty T.empty contract eventName
+stringArrToEventTableName [org, contract, eventName]      = EventTableName org contract contract eventName
+stringArrToEventTableName [org, app, contract, eventName] = EventTableName org app contract eventName
+stringArrToEventTableName _ = error "whoops"
+
+period :: String
+period = "."
+
+history :: String         
+history = "history@" 
+
+parseStringToTableName :: String -> TableName
+parseStringToTableName bs
+    | bs =~ period  :: Bool = let (tableStuff, _, eventName) = bs =~ period  :: (String, String, String)
+                                 in stringArrToEventTableName $ (T.splitOn (T.pack "-") $ T.pack tableStuff ) ++ [(T.pack eventName)]
+    | bs =~ history :: Bool = let (_, _, tableStuff) = bs =~ history  :: (String, String, String) 
+                                 in stringArrToHistoryTableName $ T.splitOn  (T.pack "-") $ T.pack tableStuff
+    | otherwise                = stringArrToIndexTableName $ T.splitOn (T.pack "-") (T.pack bs)                                           
+        
