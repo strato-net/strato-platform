60ead3b0ce6c95376d6af7f573e372f0448330f6
David Moncayo <david_moncayo@blockapps.net>
2023-04-21 16:26:07 -0400

    fix

--- strato/doit.sh
+++ strato/doit.sh
@@ -50,19 +50,19 @@ fi
 
 
 function newnode {
-  
+
   # if alternative log in methods are provided then use them
   mkdir -p logs
-  
+
   VPACI=${OAUTH_VAULT_PROXY_ALT_CLIENT_ID:-${OAUTH_CLIENT_ID}}
   VPACS=${OAUTH_VAULT_PROXY_ALT_CLIENT_SECRET:-${OAUTH_CLIENT_SECRET}}
 
   echo "trying to see if the alternative OAUTH parameters are available"
 
-  if [[ -z ${VPACI} || -z ${VPACS} ]]; then 
+  if [[ -z ${VPACI} || -z ${VPACS} ]]; then
     echo "Could not obtain OAUTH parameters for Vault Proxy"
     exit 2
-  else 
+  else
     echo "OAUTH parameters for Vault Proxy are available"
   fi
 
@@ -237,7 +237,7 @@ function newnode {
 
   echo "Starting strato-api"
   # Leave the +RTS -N1, it is important
-  runBackgroundProcess strato-api --minLogLevel=$evmMinLogLevel --gasOn=$gasOn --evmCompatible=$evmCompatible +RTS -N1 >> logs/strato-api 2>&1 
+  runBackgroundProcess strato-api --minLogLevel=$evmMinLogLevel --gasOn=$gasOn --evmCompatible=$evmCompatible +RTS -N1 >> logs/strato-api 2>&1
 
   if [ "${evmCompatible}" = true ]; then
       echo "EVM Compatibility mode is on, so Slipstream EVM contract indexing is being turned on."
@@ -360,6 +360,11 @@ function doInit {
     fi
   fi
 
+  if [ "${USE_OLD_STRATO_API}" != "true" ]; then
+      echo "initializing bloc database"
+      strato-api-init
+  fi
+
   #we need to create the private key for the faucet
   mkdir config
 
@@ -461,7 +466,7 @@ fi
 #else
 #    if [[ "$VAULT_URL" == *"172.17.0.1"* ]]; then
 #        echo "VAULT_URL provided is http with local docker ip for debugging."
-#    else 
+#    else
 #        echo "VAULT_URL provided is not valid, expected the value starting with 'https://' or 'http://172.17.0.1'"
 #        exit 3
 #    fi
