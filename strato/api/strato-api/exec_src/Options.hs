50caccca282cde3820b330760e6d4893ac3bd5a9
jxuan101 <jinhuai.xuan38@myhunter.cuny.edu>
2023-05-08 15:53:57 -0400

    Update API limit values to equal VM limits

--- strato/api/strato-api/exec_src/Options.hs
+++ strato/api/strato-api/exec_src/Options.hs
@@ -19,5 +19,5 @@ import           HFlags
 --defineFlag "txQueueSize" (4096::Integer) "The maximum number of requests to queue"
 defineFlag "gasOn" (True :: Bool) "Whether or not to throw an error if an account sending a TX has no balance - used in conjunction with the VM gasOn flag"
 defineFlag "evmCompatible" (False :: Bool) "Whether to turn off STRATO enhancements or not"
-defineFlag "txSizeLimit" (100000 :: Int) "The maximum length of a valid RLP encoded transaction bytestring"
-defineFlag "accountNonceLimit" (500::Integer) "The maximum number of transactions an account can make"
+defineFlag "txSizeLimit" (150000 :: Int) "The maximum length of a valid RLP encoded transaction bytestring"
+defineFlag "accountNonceLimit" (1000::Integer) "The maximum number of transactions an account can make"
