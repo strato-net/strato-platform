94ff13ffa0c9e51561d709a27d5b8cbe35b72c6b
Haile Thompson <hailethomp@gmail.com>
2023-05-09 14:38:38 -0400

    update loadDappHandler to use service user token if none is present

--- marketplace/backend/api/middleware/loadDappHandler.js
+++ marketplace/backend/api/middleware/loadDappHandler.js
@@ -3,6 +3,7 @@ import RestStatus from 'http-status-codes'
 import dappJs from '/dapp/dapp/dapp'
 import constants from '../../helpers/constants'
 import config from '/load.config'
+import oauthHelper from '/helpers/oauthHelper'
 
 const options = { config }
 
@@ -16,14 +17,26 @@ const loadDapp = async (req, res, next) => {
   // console.log('req: \n\n\n\n\n', req)
   console.log('userCredentials: \n\n\n\n\n', userCredentials)
   let address
-  try {
-    address = await rest.getKey(userCredentials, options)
-  } catch (e) {
-    // user isn't created in STRATO
-    if (e.response.status === RestStatus.BAD_REQUEST) {
-      rest.response.status(RestStatus.FORBIDDEN, res)
-      return next()
-    }
+
+  // For public use...If there is no accessToken use the serviceUserToken to handle the request. 
+  if (!accessToken) {
+    const serviceUserToken = await oauthHelper.getServiceToken()
+    const deploy = app.get(constants.deployParamName)
+
+    req.dapp = await dappJs.bind({ token: serviceUserToken }, deploy.dapp.contract, options)
+
+    return next()
+
+  } else {
+
+    try {
+      address = await rest.getKey(userCredentials, options)
+    } catch (e) {
+      // user isn't created in STRATO
+      if (e.response.status === RestStatus.BAD_REQUEST) {
+        rest.response.status(RestStatus.FORBIDDEN, res)
+        return next()
+      }
 
     // unexpected error
     return next(e)
