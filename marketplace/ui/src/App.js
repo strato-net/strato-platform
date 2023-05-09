40349ab139ecc325686116887973386a6bb5d24e
Haile Thompson <hailethomp@gmail.com>
2023-05-09 14:52:01 -0400

    update ui to not check for user login and redirect to login

--- marketplace/ui/src/App.js
+++ marketplace/ui/src/App.js
@@ -1,14 +1,9 @@
-import React, { useEffect } from "react";
-import {
-  useAuthenticateState,
-  useAuthenticateDispatch,
-} from "./contexts/authentication";
+import React from "react";
 import AuthenticatedRoutes from "./AuthenticatedRoutes";
 import "@shopify/polaris/build/esm/styles.css";
-import { actions } from "./contexts/authentication/actions";
 import { BrowserRouter } from "react-router-dom";
 import "./styles/app.css";
-import { Layout, Card, Spin } from "antd";
+import { Layout } from "antd";
 import HeaderComponent from "./components/Header/Header";
 
 import { UsersProvider } from "./contexts/users";
@@ -16,37 +11,8 @@ import { UsersProvider } from "./contexts/users";
 const { Content } = Layout;
 
 const App = () => {
-  // const theme = {
-  //   colors: {
-  //     topBar: {
-  //       background: "#fff",
-  //     },
-  //   },
-  // };
-
-  const dispatch = useAuthenticateDispatch();
-
-//  const { isAuthenticated, hasChecked, user, loginUrl, users } =
-//    useAuthenticateState();
-
-    const isAuthenticated = true;            // These are dummy hardcoded values, they need to be set properly
-    const hasChecked = true;
     const user = {commonName: "fred"}
-    const loginUrl = "abcd"
     const users = [];
-
-    
-  useEffect(() => {
-    if (hasChecked && !isAuthenticated && loginUrl !== undefined) {
-      window.location.href = loginUrl;
-    }
-  }, [isAuthenticated, hasChecked, loginUrl]);
-
-  // useEffect(() => {
-  //   if (isAuthenticated) {
-  //     actions.fetchUsers(dispatch);
-  //   }
-  // }, [isAuthenticated]);
 
   return (
     <BrowserRouter>
@@ -54,65 +20,10 @@ const App = () => {
         <UsersProvider>
           <HeaderComponent user={user} users={users} />
         </UsersProvider>
-
-        {hasChecked && isAuthenticated ? (
-          <Content>
-            <AuthenticatedRoutes user={user} users={users} />
-          </Content>
-        ) : (
-          <div className="w-96 m-auto h-screen mt-5">
-            {hasChecked && !isAuthenticated && loginUrl === undefined ? (
-              <Card size="small" title="Authentication Error">
-                <p>
-                  An error occured when attempting to verify user credentials.
-                  Perhaps the user does not exist in asset Framework yet.
-                  Contact a System Administrator
-                </p>
-              </Card>
-            ) : (
-              <Card title="Checking Authentication">
-                <div className="text-center">
-                  <Spin />
-                </div>
-              </Card>
-            )}
-          </div>
-        )}
+        <Content>
+          <AuthenticatedRoutes />
+        </Content>
       </Layout>
-      {/* <AppProvider theme={theme}>
-        <Frame
-          topBar={AssetFrameworkTopBar({ user, logout: () => actions.logout(dispatch) })}
-          navigation={
-            isAuthenticated ? AssetFrameworkNavigation({ isAuthenticated, user }) : null
-          }
-        >
-          {hasChecked && isAuthenticated ? (
-            <AuthenticatedRoutes user={user} users={users} />
-          ) : (
-            <Page>
-              <Layout>
-                <Layout.Section>
-                  <div style={{ width: "420px", margin: "auto" }}>
-                    {hasChecked && !isAuthenticated && loginUrl === undefined ? (
-                      <Card size="small" title="Authentication Error" sectioned>
-                        <p>
-                          An error occured when attempting to verify user
-                          credentials. Perhaps the user does not exist in
-                          asset Framework yet. Contact a System Administrator
-                        </p>
-                      </Card>
-                    ) : (
-                      <Card title="Checking Authentication" sectioned>
-                        <Spinner />
-                      </Card>
-                    )}
-                  </div>
-                </Layout.Section>
-              </Layout>
-            </Page>
-          )}
-        </Frame>
-      </AppProvider> */}
     </BrowserRouter>
   );
 };
