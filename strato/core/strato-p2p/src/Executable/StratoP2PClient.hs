f62e92481033c19f1c79dcfcc1b18b2f2cbb7b1f
Dustin Norwood <dnorwood2010@aol.com>
2023-05-11 21:59:14 -0400

    Update StratoP2PClient.hs

--- strato/core/strato-p2p/src/Executable/StratoP2PClient.hs
+++ strato/core/strato-p2p/src/Executable/StratoP2PClient.hs
@@ -158,7 +158,7 @@ stratoP2PClient runner = runner $ \_ -> do
         unless isRunning $ do
           (liftIO (SSem.tryWait sem)) >>= \case
             Nothing -> return ()
-            Just _  -> void . liftIO . forkIO $ do
+            Just _  -> void . liftIO . forkIO . runLoggingT . runner $ \_ -> do
               result <- try $ liftIO $ Async.withAsync (runLoggingT . runner $ \sSource -> runPeerInList p sSource) $ \res -> Async.waitCatch res
               handleRunPeerResult p result
               liftIO (SSem.signal sem)
