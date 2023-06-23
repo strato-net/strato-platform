a3ea49588aa4a60a7e08e00145a4dd6d1e4ff76b
neelkanthpoosa <poosaneelkanth@gmail.com>
2023-06-23 15:32:23 -0400

    moved order api tests

---
+++ marketplace/backend/test/v1/paymentOrder.test.js
@@ -0,0 +1,321 @@
+import { assert } from 'blockapps-rest'
+import { util } from '/blockapps-rest-plus'
+import dotenv from 'dotenv'
+import config from '../../load.config'
+import oauthHelper from '/helpers/oauthHelper'
+import RestStatus from 'http-status-codes';
+import { get, post } from '/helpers/rest'
+import { Order, Product, Inventory } from '../../api/v1/endpoints'
+import { productArgs } from './factories/product'
+import { inventoryArgs } from './factories/inventory'
+import certificateJs from '/dapp/certificates/certificate'
+import factory from './factories/order'
+
+const loadEnv = dotenv.config()
+assert.isUndefined(loadEnv.error)
+
+
+describe('Payment Tests', function () {
+  this.timeout(config.timeout)
+  let seller
+  let buyer
+  let buyerOrganization
+  let buyerCert
+
+  before(async () => {
+    let sellerToken
+    try {
+      sellerToken = await oauthHelper.getUserToken(
+        `${process.env.TEST_SELLER_ORG}`,
+        `${process.env.TEST_SELLER_PASSWORD}`,
+      )
+    } catch (e) {
+      console.error(
+        'ERROR: Unable to fetch the  user token, check your OAuth settings in config',
+        e,
+      )
+      throw e
+    }
+
+    const sellerCredentials = { token: sellerToken }
+
+    const sellerResponse = await oauthHelper.getStratoUserFromToken(sellerCredentials.token)
+
+    assert.strictEqual(
+      sellerResponse.status,
+      RestStatus.OK,
+      sellerResponse.message
+    )
+    seller = { ...sellerResponse.user, ...sellerCredentials }
+
+
+
+    
+    let buyerToken
+    try {
+      buyerToken = await oauthHelper.getUserToken(
+        `${process.env.TEST_BUYER_ORG}`,
+        `${process.env.TEST_BUYER_PASSWORD}`,
+      )
+    } catch (e) {
+      console.error(
+        'ERROR: Unable to fetch the  user token, check your OAuth settings in config',
+        e,
+      )
+      throw e
+    }
+
+    const buyerCredentials = { token: buyerToken }
+
+    const buyerResponse = await oauthHelper.getStratoUserFromToken(buyerCredentials.token)
+
+    assert.strictEqual(
+      buyerResponse.status,
+      RestStatus.OK,
+      buyerResponse.message
+    )
+    buyer = { ...buyerResponse.user, ...buyerCredentials }
+    buyerCert = await certificateJs.getCertificateMe(buyer)
+    buyerOrganization = buyerCert.organization;
+
+  })
+
+//////////////////////////////////////////  
+//** Pay Now/Later & Stripe URL Tests **//
+/////////////////////////////////////////
+
+/*////////////////////////////////*
+ Flow: 
+1. Seller creates Inventory
+2. Buyer will have the shipping address created
+3. Buyer clicks Pay Now, which invokes the `orders/payment` api endpoint to generate stripe checkout URL before creating order
+4. Creation of order & retrieval with Pay Now 
+5. Creation of order & retrieval with Pay Later
+/////////////////////////////////*/
+
+it('Get Payment URL', async () => {
+  // create inventory and post it as a seller
+    const createProductArgs = {
+      ...productArgs(util.uid()),
+    }
+
+    const createProductResponse = await post(
+      Product.prefix,
+      Product.create,
+      createProductArgs,
+      seller.token,
+    )
+    const [,productAddress]=createProductResponse.body.data;
+    
+    assert.equal(createProductResponse.status, RestStatus.OK, 'should be 200');
+    assert.isDefined(createProductResponse.body, 'body should be defined')
+    
+    const createInventoryArgs={
+      ...inventoryArgs(productAddress, util.uid()),
+    }
+
+    const createInventoryResponse=await post(
+      Inventory.prefix,
+      Inventory.create,
+      createInventoryArgs,
+      seller.token,
+    )
+    const [,inventoryAddress]=createInventoryResponse.body.data
+ 
+    assert.equal(createInventoryResponse.status, RestStatus.OK, 'should be 200');
+    assert.isDefined(createInventoryResponse.body, 'body should be defined')
+
+    const inventories=[inventoryAddress]
+
+      //*   shippingAddress  *//
+      
+    //create Shipping Address for a user (buyer)
+    const buyerAddressArgs = factory.getUserAddressArgs(util.uid())
+    const shipAddress = await post(
+      Order.prefix,
+      Order.userAddress,
+      buyerAddressArgs,
+      buyer.token,
+    )
+
+    assert.equal(shipAddress.status, 200, 'should be 200');
+    assert.isDefined(shipAddress.body, 'body should be defined');
+    assert.isDefined(shipAddress.body.data, 'body should be defined');
+
+    const [,userAddress]=shipAddress.body.data
+
+    //fetch Shipping Address for a user
+    const getShipAddress = await get(
+      Order.prefix,
+      Order.getAllUserAddress.replace(':address',userAddress),
+      {},
+      buyer.token,
+    )
+    
+    assert.equal(getShipAddress.status, 200, 'should be 200');
+    assert.isDefined(getShipAddress.body, 'body should be defined');
+    assert.isDefined(getShipAddress.body.data, 'body should be defined');
+
+    const buyerAddress = getShipAddress.body.data.filter(address=>address.address === userAddress)
+    assert.deepInclude(buyerAddress[0],buyerAddressArgs, 'should include the buyer address args')
+    
+    // pay now as a buyer
+    const payOrderArgs = factory.getCreatePaymentArgs(util.uid(),buyerOrganization,inventories,userAddress)
+    const payOrder = await post(
+      Order.prefix,
+      Order.payment,
+      payOrderArgs,
+      buyer.token,
+    )
+    
+    assert.equal(payOrder.status, 200, 'should be 200');
+    assert.isDefined(payOrder.body, 'body should be defined');
+    assert.isDefined(payOrder.body.data, 'body should be defined');
+    assert.equal(payOrder.body.data.url.substr(0,34),factory.paymentUrlDomain,'should have a stripe url generated')
+})
+
+////////////////////////////// Order Tests ////////////////////////////////////////
+
+it('Create an Order Pay Now', async () => {
+  // In this case the seller must have gone through the connect stripe flow
+  const createProductArgs = {
+    ...productArgs(util.uid()),
+  }
+
+  const createProductResponse = await post(
+    Product.prefix,
+    Product.create,
+    createProductArgs,
+    seller.token,
+  )
+  const [,productAddress]=createProductResponse.body.data;
+  
+  assert.equal(createProductResponse.status, RestStatus.OK, 'should be 200');
+  assert.isDefined(createProductResponse.body, 'body should be defined')
+  
+  const createInventoryArgs={
+    ...inventoryArgs(productAddress, util.uid()),
+  }
+
+  const createInventoryResponse=await post(
+    Inventory.prefix,
+    Inventory.create,
+    createInventoryArgs,
+    seller.token,
+  )
+  const [,inventoryAddress]=createInventoryResponse.body.data
+
+  assert.equal(createInventoryResponse.status, RestStatus.OK, 'should be 200');
+  assert.isDefined(createInventoryResponse.body, 'body should be defined')
+
+  // An array of inventory addresses created by the seller
+  const inventories=[inventoryAddress]
+
+  // Create a user address for the buyer's shipping information
+  const buyerAddressArgs = factory.getUserAddressArgs(util.uid())
+  const createUserAddressResponse = await post(
+    Order.prefix,
+    Order.userAddress,
+    buyerAddressArgs,
+    buyer.token
+  )
+
+  const [,userAddress]=createUserAddressResponse.body.data
+
+  const getBuyerAddressResponse = await get(
+    Order.prefix,
+    Order.getAllUserAddress.replace(':address',userAddress),
+    {},
+    buyer.token
+  )
+
+  const buyerAddress = getBuyerAddressResponse.body.data.filter(address=>address.address === userAddress)
+
+  assert.deepInclude(buyerAddress[0], buyerAddressArgs, 'should include the buyer address args')
+  
+  // The pay now functionality opens this endpoint to make a payment before posting the order
+  const createPaymentArgs = factory.getCreatePaymentArgs(util.uid(),buyerOrganization, inventories, userAddress)
+
+  const createPaymentSession = await post(
+    Order.prefix,
+    Order.payment,
+    createPaymentArgs,
+    buyer.token
+  )
+
+  assert.equal(createPaymentSession.status, RestStatus.OK, 'should be 200');
+
+  const createOrderArgs=factory.getCreateOrderArgs(util.uid(), buyerOrganization, inventories)
+  
+  const createOrderResponse = await post(
+    Order.prefix,
+    Order.create,
+    createOrderArgs,
+    buyer.token
+  )
+
+  const orderAddress = createOrderResponse.body.data[0][1]
+
+  assert.equal(createOrderResponse.status, RestStatus.OK, 'should be 200');
+  assert.isDefined(createOrderResponse.body, 'body should be defined')
+
+  // get
+  const getOrderResponse = await get(
+    Order.prefix,
+    Order.get.replace(':address',orderAddress),
+    {},
+    buyer.token,
+  )
+
+  assert.equal(getOrderResponse.status, RestStatus.OK, 'should be 200');
+  assert.isDefined(getOrderResponse.body, 'body should be defined');
+
+})
+
+it('Create an Order Pay Later', async () => {
+  const createProductArgs = {
+    ...productArgs(util.uid()),
+  }
+
+
+  const createProductResponse = await post(
+    Product.prefix,
+    Product.create,
+    createProductArgs,
+    seller.token,
+  )
+  const [,productAddress]=createProductResponse.body.data;
+  
+  assert.equal(createProductResponse.status, RestStatus.OK, 'should be 200');
+  assert.isDefined(createProductResponse.body, 'body should be defined')
+  
+  const createInventoryArgs={
+    ...inventoryArgs(productAddress, util.uid()),
+  }
+
+  const createInventoryResponse=await post(
+    Inventory.prefix,
+    Inventory.create,
+    createInventoryArgs,
+    seller.token,
+  )
+  const [,inventoryAddress]=createInventoryResponse.body.data
+
+  assert.equal(createInventoryResponse.status, RestStatus.OK, 'should be 200');
+  assert.isDefined(createInventoryResponse.body, 'body should be defined')
+
+  const inventories=[inventoryAddress]
+  const createOrderArgs=factory.getCreateOrderArgs(util.uid(),buyerOrganization,inventories)
+  
+  const createOrderResponse = await post(
+    Order.prefix,
+    Order.create,
+    createOrderArgs,
+    buyer.token
+  )
+
+  assert.equal(createOrderResponse.status, RestStatus.OK, 'should be 200');
+  assert.isDefined(createOrderResponse.body, 'body should be defined')
+})
+
+})
