975e658370b070fa367584bedac5e4dc3a087054
Vijay Rajasekaran <vprajasekaran@gmail.com>
2023-06-28 18:07:26 -0400

    recheck cert with delay for new registration flow

--- marketplace/backend/api/v1/authentication/authentication.controller.js
+++ marketplace/backend/api/v1/authentication/authentication.controller.js
@@ -52,13 +52,19 @@ class AuthenticationController {
       const userResponse = await oauthHelper.getStratoUserFromToken(accessToken)
       const user = { ...userResponse.user, ...userCredentials }
       try {
-        const cert = await certificateJs.getCertificateMe(user)
+        let cert = await certificateJs.getCertificateMe(user)
         // user does not have a valid certificate in STRATO!
         if (!cert) {
-          console.error('User does not have a valid certificate in STRATO!')
-          rest.response.status(RestStatus.UNAUTHORIZED, res, { message: 'User does not have a valid certificate in STRATO!' })
-          // rest.response.status('User does not have a valid certificate in STRATO!', res)
-          return next()
+          // delay for 6 seconds and check again if cert got created successfully
+          await new Promise(resolve => setTimeout(resolve, 6000));
+          cert = await certificateJs.getCertificateMe(user)
+
+          if (!cert) {
+            console.error('User does not have a valid certificate in STRATO!')
+            rest.response.status(RestStatus.UNAUTHORIZED, res, { message: 'User does not have a valid certificate in STRATO!' })
+            // rest.response.status('User does not have a valid certificate in STRATO!', res)
+            return next()
+          }
         }
       } catch (e) {
         // user does not have a valid certificate in STRATO!
