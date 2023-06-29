78cc617f652bb3b26e4c4702475143770f0880b6
Vijay Rajasekaran <vprajasekaran@gmail.com>
2023-06-28 20:57:22 -0400

    users me

--- marketplace/backend/api/v1/users/users.controller.js
+++ marketplace/backend/api/v1/users/users.controller.js
@@ -11,8 +11,16 @@ class UsersController {
       const { dapp, accessToken, decodedToken, address: userAddress } = req
       const username = decodedToken.preferred_username
 
-      const user = await dapp.getCertificate({ userAddress })
-     
+      let user = await dapp.getCertificate({ userAddress })
+
+      console.log('me USER ', user)
+      if (user === null || user === undefined) { 
+          console.log('user not found in first attempt')
+          await new Promise(resolve => setTimeout(resolve, 6000));
+          user = await dapp.getCertificate({ userAddress })
+          console.log('user content from second attempt', user)
+      }
+
       if (!user || Object.keys(user).length == 0) {
         rest.response.status400(res, { username }) 
       }
