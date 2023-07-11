18263e6cab9794da0d776ae08e456b599870f204
Dustin Norwood <dnorwood2010@aol.com>
2023-07-11 11:41:06 -0400

    Fixed bug in Verification.hs

--- strato/core/blockapps-data/src/Blockchain/Verification.hs
+++ strato/core/blockapps-data/src/Blockchain/Verification.hs
@@ -15,6 +15,7 @@ import           Blockchain.Data.DataDefs
 import           Blockchain.Data.RLP
 import           Blockchain.Data.Transaction
 import qualified Blockchain.Database.MerklePatricia as MP
+import qualified Blockchain.Database.MerklePatricia.Internal as MP
 import           Blockchain.Strato.Model.Keccak256
 import           Blockchain.Strato.Model.Util
 
@@ -29,7 +30,7 @@ transactionsVerificationValue = MP.sha2StateRoot . listToRLPVerificationValue
 addAllKVsMem :: (RLPSerializable obj, (MP.StateRoot `A.Alters` MP.NodeData) m) => MP.StateRoot -> [(Integer, obj)] -> m MP.StateRoot
 addAllKVsMem sr [] = return sr
 addAllKVsMem sr (x:rest) = do
-  sr' <- MP.putKeyVal sr (byteString2NibbleString $ rlpSerialize $ rlpEncode $ fst x) (rlpEncode $ rlpSerialize $ rlpEncode $ snd x)
+  sr' <- MP.unsafePutKeyVal sr (byteString2NibbleString $ rlpSerialize $ rlpEncode $ fst x) (rlpEncode $ rlpSerialize $ rlpEncode $ snd x)
   addAllKVsMem sr' rest
 
 transactionsVerificationValue :: [Transaction] -> MP.StateRoot
