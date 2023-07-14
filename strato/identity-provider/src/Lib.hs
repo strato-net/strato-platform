6e3cbc9089b9aea6d32b17e75a0623a0cbb27d0b
enigmatized <going.to.any.lengths.work@gmail.com>
2023-07-14 12:06:17 -0400

    Add ping for docker container health check

--- strato/identity-provider/src/Lib.hs
+++ strato/identity-provider/src/Lib.hs
@@ -82,6 +82,11 @@ getUserByUUID token uuid = do
     response <- httpLbs request manager
     return $ eitherDecode $ responseBody response
 
+type GetPingIdentity = "_ping" :> Get '[JSON] Int
+
+getPingIdentity ::  (MonadIO m) => m Int
+getPingIdentity = return $ 1
+
 -- newtype IDServerVaultConn = IDServerVaultConn {vaultConn :: ClientEnv}
 
 type PutIdentity = "identity"
@@ -94,14 +99,14 @@ type PutIdentity = "identity"
 --root cert just files on server's file system (flag to point to files)
 --use makeSignedCert function (or refactor the x509-gen tool)
 --add client binding to tx endpoint (then call it)
-type IdentityProviderAPI =  PutIdentity --only 1 endpoint
+type IdentityProviderAPI =  GetPingIdentity :<|> PutIdentity --only 1 endpoint
 
 -- use vault client bindings
 putIdentity :: (MonadIO m, MonadLogger m, HasVault m) => Text -> Text -> m Address
 putIdentity _ _ = do
     -- first check if a user exists in vault
     VaultData url mgr <- access Proxy
-    liftIO $ runClientM (getKey Nothing Nothing) (mkClientEnv mgr url)
+    _ <- liftIO $ runClientM (getKey Nothing Nothing) (mkClientEnv mgr url)
     $logInfoS "putIdentity" "just returning x509"
     return $ Address 0x509
 
@@ -116,7 +121,7 @@ runContextM' r = void . runResourceT . flip runReaderT r
 -- deriving instance HasVault   (ReaderT VaultData (LoggingT IO)))-- IDServerM
 
 server :: (MonadIO m, MonadLogger m, HasVault m) => ServerT IdentityProviderAPI m
-server = putIdentity
+server = getPingIdentity :<|> putIdentity
 
 
 hoistCoreServer :: String -> Server IdentityProviderAPI
