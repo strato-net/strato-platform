c2f8aa121e1e9cfda6693a610de8019b37a50d89
enigmatized <going.to.any.lengths.work@gmail.com>
2023-07-20 12:34:08 -0400

    Remove type that is not really needed

--- strato/identity-provider/api/src/IdentityProviderAPI.hs
+++ strato/identity-provider/api/src/IdentityProviderAPI.hs
@@ -11,19 +11,15 @@ module IdentityProviderAPI (
     ) where
 
 import qualified  Data.Text as T
-import           Servant.API
--- import           Servant.API.Header
-import           Blockchain.Strato.Model.Address
-
-type PutIdentityCommon = "identity"
-                :> Header' '[Required, Strict] "X-USER-UNIQUE-NAME" T.Text -- need for keycloak query
-                :> Put '[JSON] Address --should return user address
+import            Servant.API
+import            Blockchain.Strato.Model.Address
 
 type GetPingIdentity = "_ping" :> Get '[JSON] Int
 
 type PutIdentity = "identity" 
                 :> Header' '[Required, Strict] "X-ACCESS-USER-TOKEN" T.Text -- pass along for vault calls
-                :> PutIdentityCommon
+                :> Header' '[Required, Strict] "X-USER-UNIQUE-NAME" T.Text -- need for keycloak query
+                :> Put '[JSON] Address --should return user address
 
 type PutIdentityExternal = "identity" -- only to be used for external api client bindings
                 :> Header' '[Required, Strict]  "Authorization" T.Text
