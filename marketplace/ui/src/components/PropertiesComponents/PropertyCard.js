920b70b627fe7fc8198239812bb6f98cb0b9551d
michaelbtan2 <michael_tan@blockapps.net>
2023-07-26 18:36:27 -0400

    propertyCards part 1

--- marketplace/ui/src/components/PropertiesComponents/PropertyCard.js
+++ marketplace/ui/src/components/PropertiesComponents/PropertyCard.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import { Card, Carousel } from "antd";
 
-function PropertyCard() {
+function PropertyCard(props) {
+  const { property } = props;
+  const { Meta } = Card;
+
   return (
-    <div>PropertyCard</div>
-  )
+    <Card
+      hoverable
+      style={{ width: 300, margin: 10 }}
+      cover={
+        <Carousel arrows>
+          {property?.fields?.Photos?.map((img: any, key: any) => (
+            <div key={key}>
+              <img
+                style={{ width: "100%", height: 200 }}
+                alt={img.url}
+                src={`${img.url}`}
+              />
+            </div>
+          ))}
+        </Carousel>
+      }
+    >
+      <Meta
+        title={`${property?.PostalCity}, ${property?.StateOrProvince}`}
+        description={`${property?.fields?.Beds} br | ${
+          property?.fields?.Baths
+        } ba | ${
+          property?.fields?.SqFt
+        } sqft | $${property.ListPrice?.toLocaleString()}`}
+      />
+    </Card>
+  );
 }
 
-export default PropertyCard
+export default PropertyCard;
---
+++ marketplace/ui/src/components/PropertiesComponents/PropertyCreateModal.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PropertyCreateModal() {
+  return (
+    <div>PropertyCreateModal</div>
+  )
+}
+
+export default PropertyCreateModal
