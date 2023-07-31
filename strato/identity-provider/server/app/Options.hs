1dabf4989d471ffbdfa1e50a428db764ea32fe57
Aya Abdelgawad <aya_abdelgawad@yahoo.com>
2023-07-31 12:01:35 -0400

    removed unnecassary flags

--- strato/identity-provider/server/app/Options.hs
+++ strato/identity-provider/server/app/Options.hs
@@ -5,9 +5,6 @@ module Options
     , flags_nodeUrl
     , flags_issuerCertPath
     , flags_issuerPrivKeyPath
-    , flags_OAUTH_PROVIDER_URL
-    , flags_OAUTH_TOKEN_ENDPOINT
-    , flags_OAUTH_USER_ENDPOINT
     , flags_OAUTH_CLIENT_ID
     , flags_OAUTH_CLIENT_SECRET
     , flags_OAUTH_MASTER_CLIENT_ID
@@ -22,9 +19,6 @@ defineFlag "vaultProxyUrl" ("http://localhost:8013/strato/v2.3" :: String) "URL 
 defineFlag "nodeUrl" ("http://localhost" :: String) "URL of the Strato Node to register the cert on"
 defineFlag "issuerCertPath" ("/identity-provider/rootCert.pem" :: String) "The .pem filepath to the issuer's X509 cert"
 defineFlag "issuerPrivKeyPath" ("/identity-provider/rootPriv.pem" :: String) "The .pem filepath to the issuer's private key for signing new certs"
-defineFlag "OAUTH_PROVIDER_URL" ("http://localhost:8080" :: String) "OAuth provider URL"
-defineFlag "OAUTH_TOKEN_ENDPOINT" ("/realms/master/protocol/openid-connect/token" :: String) "Endpoint for retrieving token from OAuth provider"
-defineFlag "OAUTH_USER_ENDPOINT" ("/admin/realms/myrealm/users" :: String) "Endpoint for retrieving token from OAuth provider"
 defineFlag "OAUTH_DISCOVERY_URL" ("" :: String) "OAuth discovery URL"
 defineFlag "OAUTH_CLIENT_ID" ("" :: String) "OAuth client ID"
 defineFlag "OAUTH_CLIENT_SECRET" ("" :: String) "OAuth client secret"
