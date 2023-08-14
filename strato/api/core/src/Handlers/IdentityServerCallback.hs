1dc3618d94a0aa302c6b398724be690b215c0142
enigmatized <going.to.any.lengths.work@gmail.com>
2023-08-14 11:38:38 -0400

    Log failed ID server call

--- strato/api/core/src/Handlers/IdentityServerCallback.hs
+++ strato/api/core/src/Handlers/IdentityServerCallback.hs
@@ -50,7 +50,12 @@ redirect accessToken = do
   IdentityData url mgr <- access Proxy
   --Historical note: we decided to wait for ID serer response, but ignore any bad response
   --As the fail safe should catch any ID server failure
-  _ <- liftIO $ (try (runLoggingT $ (flip runReaderT ( IdentityData url mgr)  $ identitytWrapper $ putIdentityExternal ("Bearer " <> accessToken) )) :: IO (Either SomeException Address) )
+  idServerResult <- liftIO $ (try (runLoggingT $ (flip runReaderT ( IdentityData url mgr)  $ identitytWrapper $ putIdentityExternal ("Bearer " <> accessToken) )) :: IO (Either SomeException Address) )
+  case idServerResult of 
+    Left e -> do
+      logErrorCS callStack $ "Error calling Identity Server: " <> pack (show e)
+    Right _ -> do
+      logInfoCS callStack "Successfully called Identity Server"
   return $ addHeader "/" (Address 0x0) --At one point we wanted to redirect with an address, but we don't need to do that anymore, maybe we should remove the address from the type signature
 
 identitytWrapper :: (MonadIO m, MonadLogger m, HasIdentity m, HasCallStack) =>
