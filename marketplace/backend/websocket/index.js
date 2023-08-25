893cd11a93f748b2122a6f7ad712196209571d20
Aya Abdelgawad <aya_abdelgawad@yahoo.com>
2023-08-25 16:58:58 -0400

    bootstrapping a websocket server onto the marketplace backend

---
+++ marketplace/backend/websocket/index.js
@@ -0,0 +1,44 @@
+import { Kafka } from 'kafkajs';
+import WebSocket from 'ws';
+
+const kafka = new Kafka({
+    clientId: 'newsie',
+    brokers: ['kafka:9092']
+})
+
+const consumer = kafka.consumer({ groupId: 'newsies' })
+
+async function bootstrapWebsocketToExpress(expressServer) {
+    // create websocket server and add on to express server
+    const websocketServer = new WebSocket.Server({
+        noServer: true,
+        path: '/eventstream' // todo: better name?
+    });
+
+    expressServer.on('upgrade', (request, socket, head) => {
+        websocketServer.handleUpgrade(request, socket, head,
+            () => {
+                console.log("established websocket connection with client")
+            })
+    })
+
+    // set up kafka consumer
+    await consumer.connect()
+    await consumer.subscribe({topic: 'solidvmevents'})
+
+    await consumer.run({
+        eachMessage: async ({_topic, _partition, message}) => {
+            websocketServer.clients.forEach((client) => {
+                if (client.readyState == WebSocket.OPEN) {
+                    client.send(message.value)
+                }
+            })
+            console.log("Received the following kafka msg ", message.value)
+        }
+    })
+
+
+    return websocketServer;
+}
+
+export default bootstrapWebsocketToExpress
