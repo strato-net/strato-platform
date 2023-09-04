080212dd4d6ece9cd74f9e11c37af7f5de5c35ce
Tanuj Soni <tanuj@blockapps.net>
2023-09-04 19:34:34 +0530

    Minor improvements

--- marketplace/ui/src/components/Header/Header.js
+++ marketplace/ui/src/components/Header/Header.js
@@ -37,7 +37,9 @@ const HeaderComponent = ({ user, loginUrl }) => {
   const userDispatch = useAuthenticateDispatch();
   const { cartList } = useMarketplaceState();
   const storedData = useMemo(() => {
-    return window.localStorage.getItem("cartList") == null ? [] : JSON.parse(window.localStorage.getItem("cartList"));
+    return window.localStorage.getItem("cartList") == null
+      ? []
+      : JSON.parse(window.localStorage.getItem("cartList"));
   }, []);
 
   useEffect(() => {
@@ -48,16 +50,15 @@ const HeaderComponent = ({ user, loginUrl }) => {
   const [initials, setInitials] = useState("");
   const [roleIndex, setRoleIndex] = useState();
 
-
   const navItems = [
     // {
     //   role: 0,
     //   items: [
-    { label: <div id="marketplace">Marketplace</div>, key: '0' },
-    { label: <div id="orders">Orders</div>, key: '1' },
-    { label: <div id="inventory">Inventory</div>, key: '2' },
-    { label: <div id="products">Products</div>, key: '3' },
-    { label: <div id="events">Events</div>, key: '4' },
+    { label: <div id="marketplace">Marketplace</div>, key: "0" },
+    { label: <div id="orders">Orders</div>, key: "1" },
+    { label: <div id="inventory">Inventory</div>, key: "2" },
+    { label: <div id="products">Products</div>, key: "3" },
+    { label: <div id="events">Events</div>, key: "4" },
     //   ]
     // }
   ];
@@ -66,8 +67,13 @@ const HeaderComponent = ({ user, loginUrl }) => {
     { name: "arts", icon: arts, label: "Arts", url: "/category/Art" },
     { name: "property", icon: property, label: "Property", url: "/properties" },
     { name: "carbon", icon: carbon, label: "Carbon", url: "/category/Carbon" },
-    { name: "loyalty", icon: loyalty, label: "Loyalty", url: "category/Real Estate" }
-  ]
+    {
+      name: "loyalty",
+      icon: loyalty,
+      label: "Loyalty",
+      url: "category/Real Estate",
+    },
+  ];
 
   const navUrls = [
     routes.Marketplace.url,
@@ -80,7 +86,7 @@ const HeaderComponent = ({ user, loginUrl }) => {
   const logout = () => {
     TagManager.dataLayer({
       dataLayer: {
-        event: 'logout',
+        event: "logout",
       },
     });
     userActions.logout(userDispatch);
@@ -90,8 +96,13 @@ const HeaderComponent = ({ user, loginUrl }) => {
     let pathName = window.location.pathname;
     // if (pathName.includes("/marketplace")) {
     //   setSelectedTab("0");
-    // } else 
-    if (pathName.includes("/order") || pathName.includes("/orders") || pathName.includes('sold-orders') || pathName.includes('bought-orders')) {
+    // } else
+    if (
+      pathName.includes("/order") ||
+      pathName.includes("/orders") ||
+      pathName.includes("sold-orders") ||
+      pathName.includes("bought-orders")
+    ) {
       setSelectedTab("1");
     } else if (pathName.includes("/inventories")) {
       setSelectedTab("2");
@@ -104,91 +115,105 @@ const HeaderComponent = ({ user, loginUrl }) => {
     }
   }, [window.location.pathname]);
 
-  const items = user ? [
-    {
-      key: '2',
-      label: (
-        <div>
-          <p>
-            {user == null ? "" : user.commonName}
-          </p>
-          <p className="text-xs">
-            {user == null ? "" : user.preferred_username}
-          </p>
-        </div>
-      ),
-    },
-    {
-      key: '1',
-      label: (
-        <div type="text" id="logout" className="w-full text-secondryB text-sm !hover:bg-success" onClick={logout}>
-          Logout
-        </div>
-      ),
-    },
-  ] : [
-    {
-      key: '2',
-      label: (
-        <a href={loginUrl}> Login </a>
-      ),
-    },
-  ];
+  const items = user
+    ? [
+        {
+          key: "2",
+          label: (
+            <div>
+              <p>{user == null ? "" : user.commonName}</p>
+              <p className="text-xs">
+                {user == null ? "" : user.preferred_username}
+              </p>
+            </div>
+          ),
+        },
+        {
+          key: "1",
+          label: (
+            <div
+              type="text"
+              id="logout"
+              className="w-full text-secondryB text-sm !hover:bg-success"
+              onClick={logout}
+            >
+              Logout
+            </div>
+          ),
+        },
+      ]
+    : [
+        {
+          key: "2",
+          label: <a href={loginUrl}> Login </a>,
+        },
+      ];
 
   useEffect(() => {
     let temp = "";
     if (user != null) {
       if (user.commonName.split(" ").length > 1) {
-        temp = user.commonName.split(" ")[0].substring(0, 1) + user.commonName.split(" ")[1].substring(0, 1);
+        temp =
+          user.commonName.split(" ")[0].substring(0, 1) +
+          user.commonName.split(" ")[1].substring(0, 1);
       } else {
         temp = user.commonName.split(" ")[0].substring(0, 1);
       }
     }
     setInitials(temp);
-  }, [user])
+  }, [user]);
 
   useEffect(() => {
-    if (user) setRoleIndex(0)
-    else setRoleIndex(1)
-  }, [user])
+    if (user) setRoleIndex(0);
+    else setRoleIndex(1);
+  }, [user]);
 
   const redirect = (url) => {
-    setOpen(false)
-    navigate(url)
-  }
+    setOpen(false);
+    navigate(url);
+  };
 
   const isLogin = () => {
-
     if (loginUrl) {
-      return <a href={loginUrl} id="login" className="text-base text-white"
-        onClick={() => {
-          TagManager.dataLayer({
-            dataLayer: {
-              event: 'login_register_click'
-            }
-          })
-        }} >
-        Login / Register
-      </a>
+      return (
+        <a
+          href={loginUrl}
+          id="login"
+          className="text-base text-white"
+          onClick={() => {
+            TagManager.dataLayer({
+              dataLayer: {
+                event: "login_register_click",
+              },
+            });
+          }}
+        >
+          Login / Register
+        </a>
+      );
     }
-  }
+  };
 
   return (
     <Header className="!bg-primary flex">
       <Space>
         <div
           className="mt-6 cursor-pointer"
-          onClick={() => { navigate(routes.Marketplace.url) }}
+          onClick={() => {
+            navigate(routes.Marketplace.url);
+          }}
         >
           <Image src={Images.logo} width={35} preview={false} />
         </div>
-        {!(roleIndex === undefined || roleIndex === 1) && <div className="ml-7 w-72">
-          <Input
-            size="large"
-            placeholder="Search"
-            prefix={<SearchOutlined style={{ color: "#989898" }} />}
-          />
-        </div>}
+        {!(roleIndex === undefined || roleIndex === 1) && (
+          <div className="ml-7 w-72">
+            <Input
+              size="large"
+              placeholder="Search"
+              prefix={<SearchOutlined style={{ color: "#989898" }} />}
+            />
+          </div>
+        )}
       </Space>
       <Menu
         mode="horizontal"
@@ -197,82 +222,93 @@ const HeaderComponent = ({ user, loginUrl }) => {
         disabledOverflow={true}
         className="h-16 bg-primary text-tertiaryB m-auto"
         onClick={(item) => {
-          setSelectedTab(item.key)
+          setSelectedTab(item.key);
           if (item.key === "0") {
             TagManager.dataLayer({
               dataLayer: {
-                event: 'view_marketplace_page',
+                event: "view_marketplace_page",
               },
             });
           }
           if (item.key === "1") {
             TagManager.dataLayer({
               dataLayer: {
-                event: 'view_orders_page',
+                event: "view_orders_page",
               },
             });
           }
           if (item.key === "2") {
             TagManager.dataLayer({
               dataLayer: {
-                event: 'view_inventory_page',
+                event: "view_inventory_page",
               },
             });
           }
           if (item.key === "3") {
             TagManager.dataLayer({
               dataLayer: {
-                event: 'view_products_page',
+                event: "view_products_page",
               },
             });
           }
           if (item.key === "4") {
             TagManager.dataLayer({
               dataLayer: {
-                event: 'view_events_page',
+                event: "view_events_page",
               },
             });
-            navigate(navUrls[item.key], { state: { tab: "EventType" } })
-          }
-          else navigate(navUrls[item.key]);
+            navigate(navUrls[item.key], { state: { tab: "EventType" } });
+          } else navigate(navUrls[item.key]);
         }}
         items={navItems}
       />
 
       <Space size="large">
-        {!(roleIndex === undefined || roleIndex === 1) && <Button className="border-0 mt-5" onClick={() => setOpen(true)}>
-          {sell}
-          {/* <Typography.Text style={{color:"white"}}> Sell</Typography.Text> */}
-        </Button>}
-        {!(roleIndex === undefined || roleIndex === 1) && <Badge
-          className="cursor-pointer"
-          count={cartList.length}
-          onClick={() => {
-            TagManager.dataLayer({
-              dataLayer: {
-                event: 'view_shopping_cart',
-              },
-            });
-            navigate("/checkout");
-          }}
-        >
-          <Avatar
-            style={{
-              backgroundColor: "#181EAC",
+        {!(roleIndex === undefined || roleIndex === 1) && (
+          <Button className="border-0 mt-5" onClick={() => setOpen(true)}>
+            {sell}
+            {/* <Typography.Text style={{color:"white"}}> Sell</Typography.Text> */}
+          </Button>
+        )}
+        {!(roleIndex === undefined || roleIndex === 1) && (
+          <Badge
+            className="cursor-pointer"
+            count={cartList.length}
+            onClick={() => {
+              TagManager.dataLayer({
+                dataLayer: {
+                  event: "view_shopping_cart",
+                },
+              });
+              navigate("/checkout");
             }}
-            icon={<ShoppingCartOutlined />}
-          />
-        </Badge>
-        }
-        {
-          roleIndex === undefined || roleIndex === 1
-            ? isLogin()
-            : <Dropdown menu={{ items }} placement="bottomLeft" trigger={["click"]} overlayStyle={{ marginTop: "40px" }}>
-              <a onClick={(e) => e.preventDefault()} className="text-base text-white" id="user-dropdown">
-                {initials}
-              </a>
-            </Dropdown>
-        }
+          >
+            <Avatar
+              style={{
+                backgroundColor: "#181EAC",
+              }}
+              icon={<ShoppingCartOutlined />}
+            />
+          </Badge>
+        )}
+        {roleIndex === undefined || roleIndex === 1 ? (
+          isLogin()
+        ) : (
+          <Dropdown
+            menu={{ items }}
+            placement="bottomLeft"
+            trigger={["click"]}
+            overlayStyle={{ marginTop: "40px" }}
+          >
+            <a
+              onClick={(e) => e.preventDefault()}
+              className="text-base text-white"
+              id="user-dropdown"
+            >
+              {initials}
+            </a>
+          </Dropdown>
+        )}
       </Space>
 
       <Modal
@@ -286,17 +322,22 @@ const HeaderComponent = ({ user, loginUrl }) => {
         footer={null}
       >
         <Row gutter={[16, 16]} className="mt-5">
-          {menu.map((item, index) => {
-            const { icon, label, url } = item;
-            return (
-              <Col span={6} offset={4} key={index} className="menu-card" onClick={() => { redirect(url) }} >
-                <div className="m-auto text-center" >
-                  {icon}
-                  <p className="mt-0.5" >{label}</p>
-                </div>
-              </Col>
-            );
-          })}
+          {menu.map(({ icon, label, url }, index) => (
+            <Col
+              span={6}
+              offset={4}
+              key={index}
+              className="menu-card"
+              onClick={() => {
+                redirect(url);
+              }}
+            >
+              <div className="m-auto text-center">
+                {icon}
+                <p className="mt-0.5">{label}</p>
+              </div>
+            </Col>
+          ))}
         </Row>
       </Modal>
     </Header>
