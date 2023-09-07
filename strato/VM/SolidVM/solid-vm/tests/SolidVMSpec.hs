22b3491888372d36c2415079a6da77207813e464
jxuan101 <jinhuai.xuan38@myhunter.cuny.edu>
2023-09-07 14:43:27 -0400

    Fix .code tests

--- strato/VM/SolidVM/solid-vm/tests/SolidVMSpec.hs
+++ strato/VM/SolidVM/solid-vm/tests/SolidVMSpec.hs
@@ -4197,7 +4197,7 @@ contract qq{
   it "can get the code for a contract if supplied an empty string" . runTest $ do
     let codeSnippet :: String
         codeSnippet = [r|contract Test {
-
+  
   constructor () public {
     }
 }
@@ -4530,7 +4530,7 @@ contract qq {
   it "Can get just the contract if empty string is fed to the code function. using .code" . runTest $ do
     let codeSnippet :: String
         codeSnippet = [r|contract Test {
-
+  
   constructor () public {
     }
 }
