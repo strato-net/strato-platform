ffb07348aac13090035742281983febcd475a5a3
Hadi Salameh <hsalameh123@gmail.com>
2023-09-29 13:14:06 -0400

    Another fix in fetching top selling products

--- marketplace/backend/dapp/marketplace/marketplace.js
+++ marketplace/backend/dapp/marketplace/marketplace.js
@@ -108,7 +108,7 @@ async function getTopSellingProducts(admin, args = {}, options) {
         isActive: true,
         isDeleted: false,
         isInventoryAvailable: true,
-        limit: 200,
+        limit: 350,
         ...restArgs
     }, options);
 
@@ -135,7 +135,7 @@ async function getTopSellingProducts(admin, args = {}, options) {
     }
 
     const inventoryResults = await Promise.all(inventoryPromises);
-    let inventoriesWithProductInfo = [];
+    const inventoriesWithProductInfo = [];
 
     inventoryResults.forEach(inventory => {
         inventory.forEach(item => {
@@ -146,8 +146,6 @@ async function getTopSellingProducts(admin, args = {}, options) {
             }
         });
     });
-
-    inventoriesWithProductInfo = inventoriesWithProductInfo.slice(0, 12);
 
     return inventoriesWithProductInfo.map(inventory => marshalOut(inventory));
 }
