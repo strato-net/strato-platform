1b15e571dc280712d2c456dfd9d5e3c75a065b1a
Dustin Norwood <dnorwood2010@aol.com>
2023-10-03 11:57:45 -0400

    Remove user credential logging in loadDappsHandler

--- marketplace/backend/api/middleware/loadDappHandler.js
+++ marketplace/backend/api/middleware/loadDappHandler.js
@@ -15,7 +15,7 @@ const loadDapp = async (req, res, next) => {
     username,
     ...accessToken,
   }
-  console.log('User Credentials: \n\n\n\n\n', userCredentials)
+
   const user = {
     ...userCredentials,
     node: config.nodes[0],
