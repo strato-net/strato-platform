05669113990f57eb989f7113590b77a12960fcbd
Haile Thompson <hailethomp@gmail.com>
2023-10-11 12:28:37 -0400

    rm unused code

--- marketplace/backend/dapp/items/test/item.test.js
+++ marketplace/backend/dapp/items/test/item.test.js
@@ -18,78 +18,78 @@ assert.isUndefined(loadEnv.error);
 /**
  * Test out functionality of Item
  */
-// describe('Item', function () {
-//     this.timeout(config.timeout);
+describe('Item', function () {
+    this.timeout(config.timeout);
 
-//     let globalAdmin;
-//     let contract;
-//     let newOptions;
+    let globalAdmin;
+    let contract;
+    let newOptions;
 
-//     const factoryArgs = () => ({ ...(factory.getItemArgs(util.uid())) });
+    const factoryArgs = () => ({ ...(factory.getItemArgs(util.uid())) });
 
-//     before(async () => {
-//         assert.isDefined(
-//             config.configDirPath,
-//             "configDirPath is  missing. Set in config"
-//         )
-//         assert.isDefined(
-//             config.deployFilename,
-//             "deployFilename is missing. Set in config"
-//         )
-//         assert.isDefined(
-//             process.env.GLOBAL_ADMIN_NAME,
-//             "GLOBAL_ADMIN_NAME is missing. Add it to .env file"
-//         )
-//         assert.isDefined(
-//             process.env.GLOBAL_ADMIN_PASSWORD,
-//             "GLOBAL_ADMIN_PASSWORD is missing. Add it to .env file"
-//         )
+    before(async () => {
+        assert.isDefined(
+            config.configDirPath,
+            "configDirPath is  missing. Set in config"
+        )
+        assert.isDefined(
+            config.deployFilename,
+            "deployFilename is missing. Set in config"
+        )
+        assert.isDefined(
+            process.env.GLOBAL_ADMIN_NAME,
+            "GLOBAL_ADMIN_NAME is missing. Add it to .env file"
+        )
+        assert.isDefined(
+            process.env.GLOBAL_ADMIN_PASSWORD,
+            "GLOBAL_ADMIN_PASSWORD is missing. Add it to .env file"
+        )
 
-//         let adminUserName = process.env.GLOBAL_ADMIN_NAME
-//         let adminUserPassword = process.env.GLOBAL_ADMIN_PASSWORD
+        let adminUserName = process.env.GLOBAL_ADMIN_NAME
+        let adminUserPassword = process.env.GLOBAL_ADMIN_PASSWORD
 
-//         let adminUserToken
-//         try {
-//             adminUserToken = await oauthHelper.getUserToken(adminUserName, adminUserPassword)
-//         } catch (e) {
-//             console.error("ERROR: Unable to fetch the user token, check your username and password in your .env", e)
-//             throw e
-//         }
-//         let adminCredentials = { token: adminUserToken }
-//         console.log("getting admin user's address:", adminUserName)
-//         const adminResponse = await oauthHelper.getStratoUserFromToken(adminCredentials.token)
+        let adminUserToken
+        try {
+            adminUserToken = await oauthHelper.getUserToken(adminUserName, adminUserPassword)
+        } catch (e) {
+            console.error("ERROR: Unable to fetch the user token, check your username and password in your .env", e)
+            throw e
+        }
+        let adminCredentials = { token: adminUserToken }
+        console.log("getting admin user's address:", adminUserName)
+        const adminResponse = await oauthHelper.getStratoUserFromToken(adminCredentials.token)
 
 
-//         assert.strictEqual(
-//             adminResponse.status,
-//             RestStatus.OK,
-//             adminResponse.message
-//         )
-//         globalAdmin = { ...adminResponse.user, ...adminCredentials }
+        assert.strictEqual(
+            adminResponse.status,
+            RestStatus.OK,
+            adminResponse.message
+        )
+        globalAdmin = { ...adminResponse.user, ...adminCredentials }
 
-//         const cert = await certificateJs.getCertificateMe(globalAdmin)
-//         const userOrganization = cert.organization;
+        const cert = await certificateJs.getCertificateMe(globalAdmin)
+        const userOrganization = cert.organization;
 
-//         newOptions = {
-//             org: userOrganization,
-//             ...options
-//         }
-//     });
+        newOptions = {
+            org: userOrganization,
+            ...options
+        }
+    });
 
-//     it('Create Item', async () => {
-//         // Create Item via upload
-//         const args = factoryArgs()
-//         contract = await itemJs.uploadContract(globalAdmin, args.itemArgs, newOptions);
-//         const state = await contract.get();
+    it('Create Item', async () => {
+        // Create Item via upload
+        const args = factoryArgs()
+        contract = await itemJs.uploadContract(globalAdmin, args.itemArgs, newOptions);
+        const state = await contract.get();
 
-//         delete args.itemArgs.uniqueProductCode
-//         delete args.itemArgs.rawMaterialProductName
-//         delete args.itemArgs.rawMaterialSerialNumber
-//         delete args.itemArgs.rawMaterialProductId
+        delete args.itemArgs.uniqueProductCode
+        delete args.itemArgs.rawMaterialProductName
+        delete args.itemArgs.rawMaterialSerialNumber
+        delete args.itemArgs.rawMaterialProductId
 
-//         assert.deepInclude(
-//             // Convert the Product data into strings as the args are in strings
-//             R.map(v => '' + v, state),
-//             R.map(v => '' + v, { ...args.itemArgs }));
-//     });
-// });
+        assert.deepInclude(
+            // Convert the Product data into strings as the args are in strings
+            R.map(v => '' + v, state),
+            R.map(v => '' + v, { ...args.itemArgs }));
+    });
+});
--- marketplace/backend/dapp/items/test/itemManager.test.js
+++ marketplace/backend/dapp/items/test/itemManager.test.js
@@ -3,12 +3,12 @@ import { rest, util, assert } from '/blockapps-rest-plus';
 import config from '/load.config';
 import oauthHelper from '/helpers/oauthHelper';
 import dotenv from 'dotenv';
+
 import RestStatus from 'http-status-codes';
 
-import itemManagerJs from '../itemManager';
-import itemManagerFactory from '/dapp/items/factory/itemManager.factory.js';
+import itemJs from '/dapp/items/item';
+import factory from '/dapp/items/factory/item.factory';
 import certificateJs from '/dapp/certificates/certificate'
-import dappJs from '/dapp/dapp/dapp';
 
 const options = { config };
 
@@ -16,26 +16,16 @@ const loadEnv = dotenv.config();
 assert.isUndefined(loadEnv.error);
 
 /**
- * Test out functionality of Item Manager
+ * Test out functionality of Item
  */
-describe('Item Manager', function () {
+describe('Item', function () {
     this.timeout(config.timeout);
 
     let globalAdmin;
-    let certifier;
-    let certifierAddress;
     let contract;
     let newOptions;
-    let args = {};
-    let permissionManagerContract;
-    let dapp;
 
-    const getfactoryArgs = () => ({ ...(itemManagerFactory.getItemArgs(util.uid())) });
-    const getfactoryArgsNoSerialNumber = () => ({ ...(itemManagerFactory.getItemArgsNoSerialNumber(util.uid())) });
-    const eventFactoryArgs = (itemsAddress, certifierAddress) => ({ ...(itemManagerFactory.getEventArgs(itemsAddress, certifierAddress, util.uid())) });
-    const updatefactoryArgs = (address) => ({ ...(itemManagerFactory.updateItemArgs(address, util.uid())) });
-    const certifyEventFactoryArgs = (eventAddress) => ({ ...(itemManagerFactory.certifyEventArgs(eventAddress, util.uid())) });
-
+    const factoryArgs = () => ({ ...(factory.getItemArgs(util.uid())) });
 
     before(async () => {
         assert.isDefined(
@@ -58,15 +48,9 @@ describe('Item Manager', function () {
         let adminUserName = process.env.GLOBAL_ADMIN_NAME
         let adminUserPassword = process.env.GLOBAL_ADMIN_PASSWORD
 
-        let certifierName = process.env.CERTIFIER_NAME
-        let certifierPassword = process.env.CERTIFIER_PASSWORD
-
         let adminUserToken
-        let certifierToken
-
         try {
             adminUserToken = await oauthHelper.getUserToken(adminUserName, adminUserPassword)
-            certifierToken = await oauthHelper.getUserToken(certifierName, certifierPassword)
         } catch (e) {
             console.error("ERROR: Unable to fetch the user token, check your username and password in your .env", e)
             throw e
@@ -75,6 +59,7 @@ describe('Item Manager', function () {
         console.log("getting admin user's address:", adminUserName)
         const adminResponse = await oauthHelper.getStratoUserFromToken(adminCredentials.token)
 
+
         assert.strictEqual(
             adminResponse.status,
             RestStatus.OK,
@@ -82,298 +67,29 @@ describe('Item Manager', function () {
         )
         globalAdmin = { ...adminResponse.user, ...adminCredentials }
 
-        let certifierCredentials = { token: certifierToken }
-        console.log("getting admin user's address:", certifierName)
-        const certifierResponse = await oauthHelper.getStratoUserFromToken(certifierCredentials.token)
-
-        assert.strictEqual(
-            certifierResponse.status,
-            RestStatus.OK,
-            certifierResponse.message
-        )
-        certifier = { ...certifierResponse.user, ...certifierCredentials }
-
         const cert = await certificateJs.getCertificateMe(globalAdmin)
         const userOrganization = cert.organization;
 
         newOptions = {
-            app: itemManagerJs.contractName,
             org: userOrganization,
             ...options
         }
-
-        dapp = await dappJs.uploadContract(globalAdmin, newOptions);
-
-        contract = await itemManagerJs.uploadContract(globalAdmin, {}, newOptions);
-        certifierAddress = certifier.address;
     });
 
-    // it('Create an event', async () => {
-    //     // Create Item via itemManager
-    //     const args = getfactoryArgs()
-    //     const [restStatus1, itemAddresses,] = await contract.addItem(args.itemArgs);
+    it('Create Item', async () => {
+        // Create Item via upload
+        const args = factoryArgs()
+        contract = await itemJs.uploadContract(globalAdmin, args.itemArgs, newOptions);
+        const state = await contract.get();
 
-    //     const itemAddressArr = itemAddresses.split(",");
+        delete args.itemArgs.uniqueProductCode
+        delete args.itemArgs.rawMaterialProductName
+        delete args.itemArgs.rawMaterialSerialNumber
+        delete args.itemArgs.rawMaterialProductId
 
-    //     // Check if Item was created
-    //     const items = await contract.getItems({ address: itemAddressArr }, newOptions)
-    //     assert.equal(restStatus1, RestStatus.OK);
-    //     assert.equal(items.length, args.itemArgs.itemObject.length)
-
-    //     const itemsAddress = items.map(item => item.address)
-    //     // Create Event via upload
-    //     const eventArgs = eventFactoryArgs(itemsAddress, certifierAddress)
-    //     const [restStatus2, eventAddresses] = await contract.addEvent({ ...eventArgs });
-
-    //     const eventAddressArr = eventAddresses.split(",");
-    //     assert.equal(restStatus2, RestStatus.CREATED);
-
-    //     // Check if Event was created
-    //     const events = await contract.getEvents({ address: eventAddressArr }, newOptions)
-
-    //     assert.equal(restStatus2, RestStatus.CREATED);
-    //     assert.equal(events.length, eventArgs.itemsAddress.length)
-
-    //     events.forEach(event => {
-    //         assert.deepInclude(R.map(v => '' + v, event),
-    //             R.map(v => '' + v, { eventTypeId: eventArgs.eventTypeId, eventBatchId: eventArgs.eventBatchId, date: eventArgs.date, summary: eventArgs.summary, certifier: eventArgs.certifier, createdDate: eventArgs.createdDate }));
-    //     });
-    // });
-
-    // it('certify an event', async () => {
-    //     // Create Item via itemManager
-    //     const args = getfactoryArgs()
-    //     const [restStatus1, itemAddresses,] = await contract.addItem(args.itemArgs);
-
-    //     const itemAddressArr = itemAddresses.split(",");
-
-    //     // Check if Item was created
-    //     const items = await contract.getItems({ address: itemAddressArr }, newOptions)
-
-    //     assert.equal(restStatus1, RestStatus.OK);
-    //     assert.equal(items.length, args.itemArgs.itemObject.length)
-
-    //     const itemsAddress = items.map(item => item.address)
-    //     // Create Event via upload
-    //     const eventArgs = eventFactoryArgs(itemsAddress, certifierAddress)
-    //     const [restStatus2, eventAddresses] = await contract.addEvent({ ...eventArgs });
-
-    //     const eventAddressArr = eventAddresses.split(",");
-    //     assert.equal(restStatus2, RestStatus.CREATED);
-
-    //     // Check if Event was created
-    //     const events = await contract.getEvents({ address: eventAddressArr }, newOptions)
-
-    //     assert.equal(restStatus2, RestStatus.CREATED);
-    //     assert.equal(events.length, eventArgs.itemsAddress.length)
-
-    //     const eventAddress = events.map(event => event.address);
-
-    //     // const _contract = {
-    //     //     name: contract.name,
-    //     //     address: contract.address
-    //     // }
-
-    //     const _contract = await itemManagerJs.bindAddress(
-    //         certifier,
-    //         contract.address,
-    //         newOptions
-    //     )
-    //     // Update an Event
-    //     const certifyEventArgs = certifyEventFactoryArgs(eventAddress);
-    //     const [status, responseMessage] = await itemManagerJs.certifyEvent(certifier, _contract, certifyEventArgs, newOptions);
-    //     assert.equal(status, RestStatus.OK)
-    //     assert.equal(responseMessage, "event has been certified")
-
-    //     // Check if Event has been updated
-    //     const updatedEvents = await contract.getEvents({ address: eventAddressArr }, newOptions)
-    //     assert.equal(updatedEvents[0].certifierComment, certifyEventArgs.updates['certifierComment'])
-    //     assert.equal(updatedEvents[0].certifiedDate, certifyEventArgs['certifiedDate'])
-
-    // });
-
-    // it('Other than assigned certifier no one can update the event -401', async () => {
-    //     // Create Item via itemManager
-    //     const args = getfactoryArgs()
-    //     const [restStatus1, itemAddresses,] = await contract.addItem(args.itemArgs);
-
-    //     const itemAddressArr = itemAddresses.split(",");
-
-    //     // Check if Item was created
-    //     const items = await contract.getItems({ address: itemAddressArr }, newOptions)
-
-    //     assert.equal(restStatus1, RestStatus.OK);
-    //     assert.equal(items.length, args.itemArgs.itemObject.length)
-
-    //     const itemsAddress = items.map(item => item.address)
-    //     // Create Event via upload
-    //     const eventArgs = eventFactoryArgs(itemsAddress, certifierAddress)
-    //     const [restStatus2, eventAddresses] = await contract.addEvent({ ...eventArgs });
-
-    //     const eventAddressArr = eventAddresses.split(",");
-    //     assert.equal(restStatus2, RestStatus.CREATED);
-
-    //     // Check if Event was created
-    //     const events = await contract.getEvents({ address: eventAddressArr }, newOptions)
-
-    //     assert.equal(restStatus2, RestStatus.CREATED);
-    //     assert.equal(events.length, eventArgs.itemsAddress.length)
-
-    //     const eventAddress = events.map(event => event.address);
-
-    //     const _contract = await itemManagerJs.bindAddress(
-    //         globalAdmin,
-    //         contract.address,
-    //         newOptions
-    //     )
-    //     // Update an Event
-    //     const certifyEventArgs = certifyEventFactoryArgs(eventAddress);
-
-    //     await assert.restStatus(async () => {
-    //         await itemManagerJs.certifyEvent(globalAdmin, _contract, certifyEventArgs, newOptions);
-    //     }, RestStatus.UNAUTHORIZED);
-
-
-    // });
-
-    it('ItemManager: Create items', async () => {
-        // Create Item via itemManager
-        const args = getfactoryArgs()
-        console.log("args======>", args)
-
-        console.log("dapp======>", dapp)
-
-        const [restStatus, itemAddresses,] = await dapp.addItem(args.itemArgs);
-
-        console.log("itemAddresses=======>", itemAddresses)
-        
-        const itemAddressArr = itemAddresses.split(",") 
-        const itemsArrCleaned = itemAddressArr.filter(address => address != "")
-        const items = await dapp.getItems({ address: itemsArrCleaned }, newOptions);
-
-        console.log("itemsArrCleaned=======>", items)
-
-        const totalItems = args.itemArgs.itemObject.length;
-        // assert.equal(totalItems, items.length)
-
-        // Check if Raw Materials were created
-        const rawMaterials = await contract.getRawMaterials({}, newOptions)
-
-        const itemSerialNumbers = items.map(item => item.serialNumber)
-        let rawMaterialsCreated = []
-
-        itemSerialNumbers.forEach(serialNumber => {
-            rawMaterials.filter(rawMaterial => {
-                if (rawMaterial.rawMaterialSerialNumber.includes(serialNumber)) {
-                    rawMaterialsCreated.push(rawMaterial)
-                }
-            })
-        })
-        
-        // assert.equal(rawMaterialsCreated.length, args.itemArgs.itemObject.length)
-        rawMaterialsCreated.forEach(rawMaterial => {
-            assert.isDefined(rawMaterial.rawMaterialProductId)
-            assert.isDefined(rawMaterial.rawMaterialSerialNumber)
-            assert.isDefined(rawMaterial.rawMaterialProductName)
-        })
-
-        // function transferOwnership(
-        //     address[] _itemsAddress,
-        //     address _newOwner,
-        //     address _dappAddress,
-        //     int _newQuantity,
-        //     uint _itemNumber,
-        //     bool _isGiftedTransfer
-
-        console.log("itemAddressArr", itemAddressArr)
-        
-        const beforeTransferOwnership = await contract.getItems({ address: itemAddressArr }, newOptions);
-        // Transfer ownership of items
-        const itemResponse = await contract.transferOwnership({ itemsAddress: itemsArrCleaned, newOwner: certifier.address, dappAddress: dapp.address, itemNumber: 1, newQuantity: 1, isGiftedTransfer: true });
-        assert.equal(itemResponse[0], RestStatus.OK);
-
-
-
-        // get items after transfer of ownership
-        const afterTransferOwnership = await contract.getItems({ address: itemAddressArr }, newOptions);
-        assert.notEqual(beforeTransferOwnership[0].owner, afterTransferOwnership[0].owner);
+        assert.deepInclude(
+            // Convert the Product data into strings as the args are in strings
+            R.map(v => '' + v, state),
+            R.map(v => '' + v, { ...args.itemArgs }));
     });
-
-    // it('ItemManager: Create items with no serialNumber', async () => {
-    //     // Create Item via itemManager
-    //     const args = getfactoryArgsNoSerialNumber()
-    //     const [restStatus, itemAddresses,] = await contract.addItem(args.itemArgs);
-        
-    //     const itemAddressArr = itemAddresses.split(",");
-    //     const items = await contract.getItems({ address: itemAddressArr }, newOptions);
-
-    //     const totalItems = args.itemArgs.itemObject.length;
-    //     assert.equal(totalItems, items.length)
-
-    //     items.forEach(item => {
-    //         assert.equal(item.serialNumber, '')
-    //     })
-    // });
-
-    // it('ItemManager: get item ownership history', async () => {
-    //     // Create Item via itemManager
-    //     const args = getfactoryArgs()
-    //     const [, itemAddresses,] = await contract.addItem(args.itemArgs);
-
-    //     const itemAddressArr = itemAddresses.split(",");
-    //     const itemEvents = await contract.getAllOwnershipEvents({ itemAddress: itemAddressArr }, newOptions)
-
-    //     assert.equal(itemEvents.length, args.itemArgs.itemObject.length)
-    // })
-
-    // it('ItemManager: Should return repeated serialNumber for a UPC', async () => {
-    //     // Create Item via itemManager
-    //     const args = getfactoryArgs()
-    //     const [restStatus, itemAddresses,] = await contract.addItem(args.itemArgs);
-
-    //     const [, , repeatedSerialNumbers] = await contract.addItem(args.itemArgs);
-    //     console.log(repeatedSerialNumbers);
-    //     // Convert the Item data into strings as the args are in strings
-    //     assert.deepInclude(repeatedSerialNumbers, R.map(v => '' + v, args.itemArgs.itemObject.length));
-    // });
-
-    // it('Update item', async () => {
-    //     // Create Item via upload
-    //     const args = getfactoryArgs(globalAdmin)
-    //     const [restStatus, itemAddresses] = await contract.addItem(args.itemArgs);
-    //     assert.equal(restStatus, RestStatus.OK);
-
-    //     // Check if Item was created
-    //     const itemAddressArr = itemAddresses.split(",").filter(address => address != "");
-    //     const item = await contract.getItems({ address: itemAddressArr }, newOptions);
-    //     assert.equal(item.length, args.itemArgs.itemObject.length)
-
-    //     const args2 = updatefactoryArgs(itemAddressArr);
-    //     const update = await contract.updateItem(args2)
-    //     assert.equal(update[0], RestStatus.OK)
-
-    //     const updatedItem = await contract.getItems({ address: itemAddressArr }, newOptions);
-    //     assert.equal(updatedItem[0].status, args2['status'])
-    //     assert.equal(updatedItem[0].comment, args2['comment'])
-    // });
-
-    // TODO: need to be implemented later
-    // it('Create and transfer ownership of a Item', async () => {
-    //     // Create Item via itemManager
-    //     const args = getfactoryArgs()
-    //     const [restStatus, itemAddresses,] = await contract.addItem(args.itemArgs);  
-    //     const itemAddressArr = itemAddresses.split(",").filter(address => address !== "");
-
-    //     // get items before transfer of ownership
-    //     const beforeTransferOwnership = await contract.getItems({ address: itemAddressArr }, newOptions);
-
-    //     // transfer ownership of items
-    //     const itemResponse = await contract.transferOwnership({itemsAddress: itemAddressArr, newOwner: constants.testOrg3 });      
-    //     assert.equal(itemResponse[0], RestStatus.OK);
-
-    //     // get items after transfer of ownership
-    //     const afterTransferOwnership = await contract.getItems({ address: itemAddressArr }, newOptions);
-    //     assert.notEqual(beforeTransferOwnership[0].owner, afterTransferOwnership[0].owner);
-    //     assert.notEqual(beforeTransferOwnership[0].ownerCommonName, afterTransferOwnership[0].ownerCommonName);
-    // });
 });
