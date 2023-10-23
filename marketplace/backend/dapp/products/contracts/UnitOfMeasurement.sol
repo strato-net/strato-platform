a323589b3e18ba517833bcba64bde5ccbf8a057d
David Nallapu <davidsam1998@gmail.com>
2023-10-23 13:42:18 -0400

    added marketplace v1 files back

---
+++ marketplace/backend/dapp/products/contracts/UnitOfMeasurement.sol
@@ -0,0 +1,19 @@
+ 
+
+/// @title A representation of UnitOfMeasurement of Product
+contract UnitOfMeasurement{
+    enum UnitOfMeasurement{
+        NULL,
+        LB,
+        OZ,
+        TON,
+        BAG,
+        BOX,
+        PIECE,
+        BALE,
+        GALLON,
+        POUND,
+        YARD,
+        MAX
+    }
+}
