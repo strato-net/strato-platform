5e0f8549ee5522510c76fd0da2585400eb6b5aa7
Tanuj Soni <tanuj@blockapps.net>
2023-10-27 16:12:28 +0530

    marketplace route cookie fixed

--- marketplace/ui/src/components/MarketPlace/CategoryProductCard.js
+++ marketplace/ui/src/components/MarketPlace/CategoryProductCard.js
@@ -112,8 +112,8 @@ const CategoryProductCard = ({ product, category }) => {
     }
   };
 
+  let route = `/memberships/all/${product.membershipId}?inventoryId=${product.address}`;
   const handleRedirect = () => {
-    let route = `/memberships/all/${product.membershipId}?inventoryId=${product.address}`;
     setCookie(
       "returnUrl",
       `/marketplace${route}`,
@@ -232,7 +232,7 @@ const CategoryProductCard = ({ product, category }) => {
                     ) {
                       setCookie(
                         "returnUrl",
-                        `/marketplace/productList/${product.address}`,
+                        `/marketplace${route}`,
                         10
                       );
                       window.location.href = loginUrl;
@@ -265,7 +265,7 @@ const CategoryProductCard = ({ product, category }) => {
                     ) {
                       setCookie(
                         "returnUrl",
-                        `/marketplace/productList/${product.address}`,
+                        `/marketplace${route}`,
                         10
                       );
                       window.location.href = loginUrl;
