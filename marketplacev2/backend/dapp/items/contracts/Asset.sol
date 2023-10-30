c0730ff5f1f1e29661d7c0cce87e0b29206d6481
David Nallapu <davidsam1998@gmail.com>
2023-10-30 13:51:31 -0400

    added marketplacev2 folder

---
+++ marketplacev2/backend/dapp/items/contracts/Asset.sol
@@ -0,0 +1,57 @@
+import <509>;
+
+abstract contract Asset {
+    string public ownerOrganization;
+    string public ownerOrganizationalUnit;
+    string public ownerCommonName;
+    Sale public sale;
+
+    constructor() {
+        CertificateRegistry r = CertificateRegistry(account(0x509, "main"));
+        Certificate c = CertificateRegistry(account(address(r), "main")).getUserCert(msg.sender);
+        ownerOrganization = Certificate(account(address(c), "main")).organization();
+        ownerOrganizationalUnit = Certificate(account(address(c), "main")).organizationalUnit();
+        ownerCommonName = Certificate(account(address(c), "main")).commonName();
+
+    }
+
+    modifier requireOwner(string action) {
+        CertificateRegistry r = CertificateRegistry(account(0x509, "main"));
+        Certificate c = CertificateRegistry(account(address(r), "main")).getUserCert(msg.sender);
+        string err = "Only "
+                   + ownerCommonName
+                   + " from "
+                   + ownerOrganization
+                   + " can perform "
+                   + action
+                   + ".";
+        string org = Certificate(account(address(c), "main")).organization();
+        require(org == ownerCommonName, err);
+        string commonName = Certificate(account(address(c), "main")).commonName();
+        require(commonName == ownerCommonName, err);
+        _;
+    }
+
+    function createBaseSale(string _purchaserOrganization, string _purchaserCommonName, string _purchasePrice) internal returns (Sale) {
+        Sale b = new Sale(
+            _purchaserOrganization,
+            _purchaserCommonName,
+            address(this),
+            _purchasePrice
+        );
+        return b;
+    }
+
+    function createSale(string _purchaserOrganization, string _purchaserCommonName, string _purchasePrice) public requireOwner("Create sale") {
+        require(address(sale) == address(0), "An open bill of sale already exists for this asset");
+        sale = createBaseSale(_purchaserOrganization, _purchaserCommonName, _purchasePrice);
+    }
+
+    function transferOwnership(string _newOwnerOrganization, string _newOwnerCommonName) public requireOwner("Ownership transfer") {
+        require(msg.sender == address(sale), "Ownership transfer must originate from the active bill of sale");
+        ownerOrganization = _newOwnerOrganization;
+        ownerCommonName = _newOwnerCommonName;
+        sale = Sale(address(0));
+    }
+}
+
