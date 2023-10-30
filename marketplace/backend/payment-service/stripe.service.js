34d2575b67c2731f861cae313f4b8f829cee2207
neelkanthpoosa <poosaneelkanth@gmail.com>
2023-10-30 09:54:00 -0400

    enable instant verification for ACH

--- marketplace/backend/payment-service/stripe.service.js
+++ marketplace/backend/payment-service/stripe.service.js
@@ -10,6 +10,11 @@ class StripeService {
             // Create a checkout session with Stripe
             return stripe.checkout.sessions.create({
                 payment_method_types: STRIPE_ENV.CHECKOUT.PAYMENT_METHOD_TYPES,
+                payment_method_options: {
+                    us_bank_account: {
+                      verification_method: 'instant',
+                  },
+                },
                 // For each item use the id to get it's information
                 // Take that information and convert it to Stripe's format
                 // shipping_address_collection: { allowed_countries: ['US'] },
