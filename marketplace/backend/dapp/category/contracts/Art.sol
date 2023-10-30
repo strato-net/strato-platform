ce698ea41a6ddcd13f07e28fdc63df27c2cb6998
jxuan101 <jinhuai.xuan38@myhunter.cuny.edu>
2023-10-30 16:32:23 -0400

    Add collection and style to art

--- marketplace/backend/dapp/category/contracts/Art.sol
+++ marketplace/backend/dapp/category/contracts/Art.sol
@@ -1,11 +1,19 @@
 
 /// @title The Art Category
 contract Art {
-  
+
     string public category = "Art";
     string public artist;
+    string public collection;
+    string public style;
 
-    constructor (string _artist) public {
+    constructor (
+        string _artist
+      , string _collection
+      , string _style
+    ) public {
         artist = _artist;
+        collection = _collection;
+        style = _style;
     }
 }
