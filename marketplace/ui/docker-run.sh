34b308484391aaabce713293000fe81b1c7cc850
Ariya Amarjargal <amarj003@umn.edu>
2023-11-06 16:11:26 -0500

    test2

--- marketplace/ui/docker-run.sh
+++ marketplace/ui/docker-run.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env sh
 
 # Creating .env file if using the relevant flags
-if [ -n "${ASSET_TABLE_NAME}" ] || [ -n "${SALE_TABLE_NAME}" ]; then
+if [ -n "${ASSET_TABLE_NAME}" ] || [ -n "${SALE_TABLE_NAME}" ] || [ -n "${REACT_APP_SITE_ID}" ]; then
   if [ ! -f .env ]; then
     # If it doesn't exist, create the .env file and insert environment variables.
     touch .env
 
     echo "REACT_APP_ASSET_TABLE_NAME=${ASSET_TABLE_NAME}" >> .env
     echo "REACT_APP_SALE_TABLE_NAME=${SALE_TABLE_NAME}" >> .env
+    echo "REACT_APP_SITE_ID=${REACT_APP_SITE_ID}" >> .env
   else
     # If the .env file exists, replace the environment variables.
     sed -i `s/REACT_APP_ASSET_TABLE_NAME=.*/REACT_APP_ASSET_TABLE_NAME=${ASSET_TABLE_NAME}/` .env
     sed -i `s/REACT_APP_SALE_TABLE_NAME=.*/REACT_APP_SALE_TABLE_NAME=${SALE_TABLE_NAME}/` .env
+    sed -i `s/REACT_APP_SITE_ID=.*/REACT_APP_SITE_ID=${REACT_APP_SITE_ID}/` .env
   fi
 fi
 
