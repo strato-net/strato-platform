9417cdd1c3c35fa5f95ad25546680894e11b5ccc
Matthew-Mosior <mattm.github@gmail.com>
2023-11-07 15:59:17 -0500

    Removing unnecessary commented out lines of code in strato/highway/client/src/Strato/Client.hs.

--- strato/highway/client/src/Strato/Client.hs
+++ strato/highway/client/src/Strato/Client.hs
@@ -1,28 +1,11 @@
 {-# LANGUAGE TypeApplications #-}
 
-module Strato.Client
-  --( getS3File,
-  --  putS3File
-  --)
-where
+module Strato.Client where
 
 import Data.Proxy
---import Servant.Client
 
---import Blockchain.Strato.Model.Keccak256
 import API
+
 
 highwayWrapperClientAPI :: Proxy HighwayWrapperAPI
 highwayWrapperClientAPI = Proxy
-
-{-
-highwayPutS3FileClientAPI :: Proxy HighwayPutS3File
-highwayPutS3FileClientAPI = Proxy
-
-getS3File :: Keccak256
-          -> ClientM ContentTypeAndBody
-getS3File = client (Proxy @HighwayGetS3File)
-
-putS3File :: Proxy HighwayPutS3File
-putS3File = highwayPutS3FileClientAPI
--}
