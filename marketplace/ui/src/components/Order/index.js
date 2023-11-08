097565c861d5fed4ad78a283f17d8d7525a8f811
Tanuj Soni <tanuj@blockapps.net>
2023-11-08 14:36:00 +0530

    breadCrumb added on orderlist

--- marketplace/ui/src/components/Order/index.js
+++ marketplace/ui/src/components/Order/index.js
@@ -3,41 +3,39 @@ import React from "react";
 import { useLocation } from "react-router-dom";
 import SoldOrdersTable from "./SoldOrdersTable";
 import BoughtOrdersTable from "./BoughtOrdersTable";
+import BreadCrumbComponent from "../BreadCrumb/BreadCrumbComponent";
 
 const { Search } = Input;
 
 const Order = ({ user }) => {
   // const naviroute = routes.OrderDetail.url;
-
-  const onChange = (key) => {
-   
-  };
-
+  const onChange = (key) => { };
   const { state } = useLocation();
 
-  
-
   return (
-    <div>
-      <Tabs
-        className="mx-16 mt-14"
-        defaultActiveKey={state == null ? "Sold" : state.defaultKey}
-        onChange={onChange}
-        tabBarExtraContent={<Search placeholder="Search" className="w-80" />}
-        items={[
-          {
-            label: <p id="sold-tab" className="font-medium text-base">Orders (Sold)</p>,
-            key: "Sold",
-            children: <SoldOrdersTable user={user} />,
-          },
-          {
-            label: <p id="bought-tab" className="font-medium text-base">Orders (Bought)</p>,
-            key: "Bought",
-            children: <BoughtOrdersTable user={user} />,
-          },
-        ]}
-      />
-    </div>
+    <>
+      <BreadCrumbComponent />
+      <div>
+        <Tabs
+          className="mx-16 mt-2"
+          defaultActiveKey={state == null ? "Sold" : state.defaultKey}
+          onChange={onChange}
+          tabBarExtraContent={<Search placeholder="Search" className="w-80" />}
+          items={[
+            {
+              label: <p id="sold-tab" className="font-medium text-base">Orders (Sold)</p>,
+              key: "Sold",
+              children: <SoldOrdersTable user={user} />,
+            },
+            {
+              label: <p id="bought-tab" className="font-medium text-base">Orders (Bought)</p>,
+              key: "Bought",
+              children: <BoughtOrdersTable user={user} />,
+            },
+          ]}
+        />
+      </div>
+    </>
   );
 };
 
