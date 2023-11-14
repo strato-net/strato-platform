d657ca9d27221437e78c4b15c73ebc8fa9ba36ae
Matthew-Mosior <mattm.github@gmail.com>
2023-11-13 20:00:56 -0500

    Wrapping main function in LoggingT monad, and performing flag checking upon highway start-up.

--- strato/highway/server/app/Main.hs
+++ strato/highway/server/app/Main.hs
@@ -1,10 +1,10 @@
-{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeApplications  #-}
+{-# LANGUAGE TypeFamilies      #-}
+{-# LANGUAGE TypeOperators     #-}
+{-# LANGUAGE QuasiQuotes       #-}
 
 module Main where
 
@@ -12,11 +12,13 @@ import API
 import Strato.Monad
 import Strato.Server
 import BlockApps.Init
+import BlockApps.Logging
 import Options
 
+import Control.Monad
+import Control.Monad.IO.Unlift
 import Data.ByteString.Char8 as DBC8
 import Data.Text as T
-import Control.Monad
 import HFlags
 import Network.HTTP.Client hiding (Proxy)
 import Network.Wai.Handler.Warp
@@ -60,15 +62,24 @@ highway3DMacroFont =
   |]
 
 main :: IO ()
-main = do
-  blockappsInit "blockapps-highway-wrapper-server"
-  Prelude.putStrLn highway3DMacroFont
-  forM_ [stdout, stderr] $ flip hSetBuffering LineBuffering --Do we need this?
-  _   <- $initHFlags "Setup Highway Wrapper AWS settings"
-  mgr <- newManager defaultManagerSettings
-  boundary <- genBoundary
-  let env = HighwayWrapperEnv mgr boundary (DBC8.pack flags_awsaccesskeyid) (DBC8.pack flags_awssecretaccesskey) (T.pack flags_awss3bucket)
-  run 8080 $ appHighwayWrapper env
+main = runLoggingT initHighway
+
+initHighway :: LoggingT IO ()
+initHighway = do
+  $logInfoS "highway/initHighway" $ T.pack $ "Starting up highway."
+  liftIO $ blockappsInit "blockapps-highway-wrapper-server"
+  liftIO $ Prelude.putStrLn highway3DMacroFont
+  liftIO $ forM_ [stdout, stderr] $ flip hSetBuffering LineBuffering --Do we need this?
+  args <- liftIO $ $initHFlags "Setup Highway Wrapper AWS settings"
+  case args of
+    []    -> do --st <- askUnliftIO
+                $logErrorS "highway/initHighway" $ T.pack $ "No highway env variables were passed in."
+                return ()
+    _     -> do $logInfoS "highway/initHighway" $ T.pack $ "Preparing environment for highway."
+                mgr <- liftIO $ newManager defaultManagerSettings
+                boundary <- liftIO genBoundary
+                let env = HighwayWrapperEnv mgr boundary (DBC8.pack flags_awsaccesskeyid) (DBC8.pack flags_awssecretaccesskey) (T.pack flags_awss3bucket)
+                liftIO $ run 8080 $ appHighwayWrapper env
 
 appHighwayWrapper :: HighwayWrapperEnv -> Application
 appHighwayWrapper env =
