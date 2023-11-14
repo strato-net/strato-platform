19f55f5c6ca8b4c2a71b944f64d72da95d79b695
Matthew-Mosior <mattm.github@gmail.com>
2023-11-14 17:17:31 -0500

    Updating strato/highway/server/app/Options.hs to remove hard-coded AWS variables.

--- strato/highway/server/app/Options.hs
+++ strato/highway/server/app/Options.hs
@@ -5,6 +5,6 @@ module Options where
 
 import HFlags
 
-defineFlag "a:awsaccesskeyid"     ("AKIAV5NMROVZIZQY4OAE" :: String)                     "AWS Access Key ID"
-defineFlag "s:awssecretaccesskey" ("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" :: String) "AWS Secret Access Key"
-defineFlag "b:awss3bucket"        ("mercata-testnet2" :: String)                         "AWS S3 Bucket"
+defineFlag "a:awsaccesskeyid"     ("" :: String) "AWS Access Key ID"
+defineFlag "s:awssecretaccesskey" ("" :: String) "AWS Secret Access Key"
+defineFlag "b:awss3bucket"        ("" :: String) "AWS S3 Bucket"
