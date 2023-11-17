2bde9145f0f50e393ad6cf89d21f41ba37fb78f0
Tanuj Soni <tanuj@blockapps.net>
2023-11-17 16:13:11 +0530

    Refactor code

--- marketplace/ui/src/components/Loader/LoaderComponent.js
+++ marketplace/ui/src/components/Loader/LoaderComponent.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import { Spin } from 'antd'
+import React from "react";
+import { Spin } from "antd";
 
 const LoaderComponent = () => {
   return (
-    <>
-      <div className="h-screen flex justify-center items-center mx-auto">
-        <Spin spinning={true} size="large" />
-      </div>
-    </>
-  )
-}
+    <div className="h-screen flex justify-center items-center mx-auto">
+      <Spin spinning={true} size="large" />
+    </div>
+  );
+};
 
-export default LoaderComponent
+export default LoaderComponent;
