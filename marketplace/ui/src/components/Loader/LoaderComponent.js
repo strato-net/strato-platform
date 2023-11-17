d1edc7ea07c2a401531e048a8e3cc4a1cb7e61d8
Tanuj Soni <tanuj@blockapps.net>
2023-11-17 13:43:21 +0530

    reusable components added

---
+++ marketplace/ui/src/components/Loader/LoaderComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Spin } from 'antd'
+
+const LoaderComponent = () => {
+  return (
+    <>
+      <div className="h-screen flex justify-center items-center mx-auto">
+        <Spin spinning={true} size="large" />
+      </div>
+    </>
+  )
+}
+
+export default LoaderComponent
