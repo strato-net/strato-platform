3f68531999ea072b18a14c519a2c1eeb7575d9ee
Tanuj Soni <tanuj@blockapps.net>
2023-11-17 10:09:00 +0530

    loader and ellipsis component added

---
+++ marketplace/ui/src/components/Ellipsis/ParagraphEllipsis.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import { Typography } from 'antd';
+const { Paragraph } = Typography;
+
+const ParagraphEllipsis = ({ description, className }) => {
+  const [isTruncated, setIsTruncated] = useState(true);
+  const truncatedContent = description.slice(0, 300);
+
+  const toggleContent = () => {
+    setIsTruncated(!isTruncated);
+  };
+
+  return (
+    <Paragraph
+      className={` text-[13px] mt-2 ${className ? className : "text-primaryC"}`}
+      id="prod-desc"
+    >
+      {/* {decodeURIComponent(inventoryDetails?.description).replace(/%0A/g, "\n").split('\n').map((line, index) => (
+                  <React.Fragment key={index}>
+                    {line ?? "--"}
+                    <br />
+                  </React.Fragment>
+                ))} */}
+      {isTruncated ? truncatedContent : description}
+      {description.length > 299 && <button onClick={toggleContent} className='font-bold text-black'>
+        {isTruncated ? '  ...Read more' : ' Read less'}
+      </button>}
+    </Paragraph>
+  )
+}
+
+export default ParagraphEllipsis
