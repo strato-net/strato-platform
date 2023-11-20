1e2d06aa6a785bb74e1581225644bca26ae7453d
Tanuj Soni <tanuj@blockapps.net>
2023-11-20 18:06:53 +0530

    header order route fix

--- marketplace/ui/src/components/Header/Header.js
+++ marketplace/ui/src/components/Header/Header.js
@@ -80,7 +80,7 @@ const HeaderComponent = ({ isOauth, user, loginUrl }) => {
 
   const navUrls = [
     routes.Marketplace.url,
-    routes.Orders.url,
+    routes.Orders.soldOrders,
     routes.Inventories.url,
     routes.Products.url,
     routes.Events.url,
