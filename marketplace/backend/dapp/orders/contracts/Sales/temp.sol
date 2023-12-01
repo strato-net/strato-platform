014889ad1a1076e023c12e9b3320618d86b0b83a
David Nallapu <davidsam1998@gmail.com>
2023-12-01 10:00:31 -0500

    further changes

--- marketplace/backend/dapp/orders/contracts/Sales/temp.sol
+++ marketplace/backend/dapp/orders/contracts/Sales/temp.sol
@@ -1,11 +1,9 @@
-import "/dapp/orders/contracts/Sales/MembershipSale.sol";
-
 pragma es6;
 pragma strict;
 import <0b469dbb1f0207a49cb014192ab05a72f5b2fcf3>;
 
-/// @title A representation of Membership assets
-contract Membership is ItemStatus, RestStatus, Asset {
+/// @title A representation of Membershipp assets
+contract Membershipp is ItemStatus, RestStatus, Asset {
     uint public units; // Number of units this asset represents
     string public serialNumber;
     string public projectType;
@@ -26,8 +24,7 @@ contract Membership is ItemStatus, RestStatus, Asset {
         address _owner,
         string _projectType,
         PaymentType[] _paymentTypes,
-        uint _expirationPeriodInMonths,
-        uint uid
+        uint _expirationPeriodInMonths
     ) Asset(_name, _description, _images, _createdDate) {
         units = _units;
         serialNumber = _serialNumber;
@@ -46,16 +43,16 @@ contract Membership is ItemStatus, RestStatus, Asset {
     }
 
     function changeUnitQuantity(uint _units) public requireOwner("change unit quantity") {
-        require(block.timestamp < expirationDate, "Membership is expired");
+        require(block.timestamp < expirationDate, "Membershipp is expired");
         for (uint i = 0; i < whitelistedSales.length; i++) {
-            MembershipSale(whitelistedSales[i]).changeSaleQuantity(_units);
+            MembershippSale(whitelistedSales[i]).changeSaleQuantity(_units);
         }
     }
 
     function splitAsset(address saleContract, uint splitUnits, address newOwner) public requireOwner("split asset") returns (address) {
-        require(block.timestamp < expirationDate, "Membership is expired");
+        require(block.timestamp < expirationDate, "Membershipp is expired");
         require(splitUnits < units, "Cannot split more units than available");
-        Membership newAsset = new Membership(name,
+        Membershipp newAsset = new Membershipp(name,
                                      description, 
                                      images, 
                                      createdDate, 
@@ -65,7 +62,9 @@ contract Membership is ItemStatus, RestStatus, Asset {
                                      0, 
                                      newOwner, 
                                      projectType, 
-                                     []);
+                                     [],
+                                     expirationPeriodInMonths
+                                     );
         units -= splitUnits;
 
         changeUnitQuantity(units);
@@ -76,21 +75,21 @@ contract Membership is ItemStatus, RestStatus, Asset {
     }
 
     function createSales(PaymentType[] _paymentTypes, uint _price, uint _units) public requireOwner("create sale") returns (uint) {
-        require(block.timestamp < expirationDate, "Membership is expired");
+        require(block.timestamp < expirationDate, "Membershipp is expired");
         for (uint i = 0; i < _paymentTypes.length; i++) {
-            whitelistSale(address(new MembershipSale(address(this), _paymentTypes[i], _price, _units)));
+            whitelistSale(address(new MembershippSale(address(this), _paymentTypes[i], _price, _units)));
         }
         status = ItemStatus.PUBLISHED;
         return RestStatus.OK;
     }
 
     function createSplitSale(PaymentType _paymentType, uint _price, uint _units) public returns (uint, string) {
-        require(block.timestamp < expirationDate, "Membership is expired");
-        address newSale = address(new MembershipSale(address(this), _paymentType, _price, _units));
+        require(block.timestamp < expirationDate, "Membershipp is expired");
+        address newSale = address(new MembershippSale(address(this), _paymentType, _price, _units));
         return (RestStatus.OK, string(newSale));
     }
 
-    function updateMembership(
+    function updateMembershipp(
         string _name, 
         string _description, 
         string[] _images, 
@@ -99,7 +98,7 @@ contract Membership is ItemStatus, RestStatus, Asset {
         string _projectType,
         uint _price,
         uint _units
-    ) public requireOwner("update membership") returns (uint) {
+    ) public requireOwner("update membershipp") returns (uint) {
         serialNumber = _serialNumber;
         projectType = _projectType;
         updateAsset(_name, _description, _images, _status, _price);
@@ -110,12 +109,52 @@ contract Membership is ItemStatus, RestStatus, Asset {
         return RestStatus.OK;
     }
 
-    function transferOwnership(address saleContract, string _newOwnerCommonName, address _newOwner) overrides public requireOwner("Ownership transfer") {
-        require(block.timestamp < expirationDate, "Membership is expired");
+    function transferOwnership(address saleContract, address _newOwner) override public requireOwner("ownership transfer") {
+        require(block.timestamp < expirationDate, "Membershipp is expired");
         require(isSaleWhitelisted(saleContract), "Sale not found in whitelist");
-        ownerCommonName = _newOwnerCommonName;
-        owner = _newOwner;
         disableAllSales();
+        status = ItemStatus.UNPUBLISHED;
+        owner = _newOwner;
+        mapping(string => string) ownerCert = getUserCert(owner);
+        ownerOrganization = ownerCert["organization"];
+        ownerCommonName = ownerCert["commonName"];
+   }
+}
+
+contract MembershippSale is Sale{
+    uint public units;
+
+    constructor(address _assetToBeSold, PaymentType _payment, uint _price, uint _units) Sale(_assetToBeSold, _price, _payment){
+        units=_units;
+    }
+
+    function changeSaleQuantity(uint _units) public requireSeller("change unit quantity") {
+        units = _units;
+    }
+
+    function transferOwnership(address _purchasersAddress, uint _orderId) public requireSeller("transfer ownership of Asset") override returns (uint) {
+        saleOrderID = _orderId;
+        executeUTXOSale(_purchasersAddress);
+        state = SaleState.Closed;
+        return RestStatus.OK;
+    }
+
+    function executeUTXOSale(address _purchasersAddress, uint[] ) public requireSeller("execute UTXO sale") {
+            // Before executing the sale, ensure the asset is a UTXO asset
+            Membershipp membershippAsset = Membershipp(address(assetToBeSold));
+
+            // Iterate over the units and create a new Membershipp instance for each unit
+            for (uint i = 0; i < units; i++) {//loop 10 times
+                // Call splitAsset on the UTXO asset for each unit
+                address newMembershippAssetAddress = membershippAsset.splitAsset(address(this), 1, _purchasersAddress);
+
+                // Create a new instance of Membershipp with a quantity of 1
+                Membershipp newMembershippAsset = Membershipp(newMembershippAssetAddress);
+
+            }
+
+            // Update the original asset's units
+            assetToBeSold = Membershipp(newCarbonAsset); 
     }
 }
 
