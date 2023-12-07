cf4e08f9815d91e3325e2f90a1f25c04d36f45bc
neelkanthpoosa <poosaneelkanth@gmail.com>
2023-12-07 15:44:07 -0500

    fixed orders fetch

--- marketplace/ui/src/contexts/order/actions.js
+++ marketplace/ui/src/contexts/order/actions.js
@@ -270,7 +270,7 @@ const actions = {
 
     try {
       const response = await fetch(
-        `${apiUrl}/order?limit=${limit}&offset=${offset}&${query}&order=${order}&buyerOrganization=${organization}`,
+        `${apiUrl}/order?limit=${limit}&offset=${offset}&${query}&buyerOrganization=${organization}&order=${order}`,
         {
           method: HTTP_METHODS.GET,
         }
