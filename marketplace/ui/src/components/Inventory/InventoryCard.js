7102be8a9e27d2d00f1420634d7d96ccc0ddb5d1
Hadi Salameh <hsalameh123@gmail.com>
2023-12-08 13:21:26 -0500

    Updated inventories page

--- marketplace/ui/src/components/Inventory/InventoryCard.js
+++ marketplace/ui/src/components/Inventory/InventoryCard.js
@@ -110,7 +110,7 @@ const InventoryCard = ({ inventory, category, debouncedSearchTerm, id }) => {
               </p>
             </div>
           </>
-          )
+        )
       case 'Clothing':
         return (
           <div className="flex mt-1.5 items-center">
@@ -133,139 +133,157 @@ const InventoryCard = ({ inventory, category, debouncedSearchTerm, id }) => {
               {itemData.source}
             </p>
           </div>)
+      case 'Membership':
+        return (
+          <>
+            <div className="flex mt-1.5 items-center">
+              <p className="text-primaryC text-sm w-40">Units</p>
+              <p text-secondryB text-sm>
+                :
+              </p>
+              <p className="text-secondryB text-sm ml-3">
+                {itemData.units}
+              </p>
+            </div>
+          </>
+        )
       default:
         break;
     }
   };
 
   return (
-    <Card className="w-full mt-6">
-      <div className="flex" id={id}>
-        <img className="w-52 object-contain" alt="" src={inventory.images && inventory.images.length > 0 ? inventory.images[0] : image_placeholder} />
-        <div className="ml-12 w-full">
-          <div className="flex justify-between items-center">
-            <div className="flex items-center">
-              <h3 className="font-semibold text-primaryB text-xl">
-                {decodeURIComponent(inventory.name)}
-              </h3>
-              {
-                <p className="font-medium text-secondryB text-base ml-2">
-                  ({getCategory()})
+    <div>
+      {(itemData.spent === "False" || !itemData.spent) &&
+        <Card className="w-full mt-6">
+          <div className="flex" id={id}>
+            <img className="w-52 object-contain" alt="" src={inventory.images && inventory.images.length > 0 ? inventory.images[0] : image_placeholder} />
+            <div className="ml-12 w-full">
+              <div className="flex justify-between items-center">
+                <div className="flex items-center">
+                  <h3 className="font-semibold text-primaryB text-xl">
+                    {decodeURIComponent(inventory.name)}
+                  </h3>
+                  {
+                    <p className="font-medium text-secondryB text-base ml-2">
+                      ({getCategory()})
+                    </p>
+                  }
+                </div>
+                <div className="flex items-center">
+                  <Button type="text"
+                    className="text-primary text-sm cursor-pointer"
+                    onClick={callDetailPage}
+                  >
+                    Preview
+                  </Button>
+                  <Popover
+                    placement="bottomLeft"
+                    open={openPop}
+                    className="ml-2"
+                    id="sideMenu"
+                    onOpenChange={handleOpenChange}
+                    title={
+                      <div className="font-medium">
+                        <div
+                          className="flex items-center mt-2 cursor-pointer"
+                          onClick={showEditModal}
+                        >
+                          <EditOutlined />
+                          <p className="ml-3">Edit</p>
+                        </div>
+                        <div
+                          className="flex items-center mt-2 cursor-pointer"
+                          onClick={showResellModal}
+                        >
+                          <PieChartOutlined />
+                          <p className="ml-3">Resell</p>
+                        </div>
+                      </div>
+                    }
+                    trigger="click"
+                  >
+                    <MoreOutlined />
+                  </Popover>
+                </div>
+              </div>
+              {categoricalProperties()}
+              {inventory.status === '2' ? <></> :
+                <div className="flex mt-1 items-center">
+                  <p className="text-primaryC text-sm w-40">Price</p>
+                  <p text-secondryB text-sm>
+                    :
+                  </p>
+                  <p className="text-secondryB text-sm ml-3">
+                    $ {inventory.price}
+                  </p>
+                </div>
+              }
+              <div className="flex mt-1 items-center">
+                <p className="text-primaryC text-sm w-40">Description</p>
+                <p text-secondryB text-sm>
+                  :
                 </p>
+                <p className="text-secondryB text-sm ml-3">
+                  {inventory.description}
+                </p>
+              </div>
+              <div className="flex mt-1 items-center">
+                <p className="text-primaryC text-sm w-40">Serial Number</p>
+                <p text-secondryB text-sm>
+                  :
+                </p>
+                <p className="text-secondryB text-sm ml-3">
+                  {itemData.serialNumber ? itemData.serialNumber : "No Serial Number Available"}
+                </p>
+              </div>
+              {inventory.status === '2' ?
+                (<div className="flex mt-2.5">
+                  <div className="text-error bg-[#FFF0F0] text-center py-1 rounded w-28 text-sm">
+                    <p>UNPUBLISHED</p>
+                  </div>
+                </div>)
+                :
+                (<div className="flex mt-2.5">
+                  <div className="text-primary bg-[#EBF7FF] text-center py-1 rounded w-28 text-sm">
+                    <p>PUBLISHED</p>
+                  </div>
+                </div>)
               }
             </div>
-            <div className="flex items-center">
-              <Button type="text"
-                className="text-primary text-sm cursor-pointer"
-                onClick={callDetailPage}
-              >
-                Preview
-              </Button>
-              <Popover
-                placement="bottomLeft"
-                open={openPop}
-                className="ml-2"
-                id="sideMenu"
-                onOpenChange={handleOpenChange}
-                title={
-                  <div className="font-medium">
-                    <div
-                      className="flex items-center mt-2 cursor-pointer"
-                      onClick={showEditModal}
-                    >
-                      <EditOutlined />
-                      <p className="ml-3">Edit</p>
-                    </div>
-                    <div
-                      className="flex items-center mt-2 cursor-pointer"
-                      onClick={showResellModal}
-                    >
-                      <PieChartOutlined />
-                      <p className="ml-3">Resell</p>
-                    </div>
-                  </div>
-                }
-                trigger="click"
-              >
-                <MoreOutlined />
-              </Popover>
-            </div>
           </div>
-          {categoricalProperties()}
-          { inventory.status === '2' ? <></> :
-            <div className="flex mt-1 items-center">
-              <p className="text-primaryC text-sm w-40">Price</p>
-              <p text-secondryB text-sm>
-                :
-              </p>
-              <p className="text-secondryB text-sm ml-3">
-                $ {inventory.price}
-              </p>
-            </div>
-          }
-          <div className="flex mt-1 items-center">
-            <p className="text-primaryC text-sm w-40">Description</p>
-            <p text-secondryB text-sm>
-              :
-            </p>
-            <p className="text-secondryB text-sm ml-3">
-              {inventory.description}
-            </p>
-          </div>
-          <div className="flex mt-1 items-center">
-            <p className="text-primaryC text-sm w-40">Serial Number</p>
-            <p text-secondryB text-sm>
-              :
-            </p>
-            <p className="text-secondryB text-sm ml-3">
-              {itemData.serialNumber ? itemData.serialNumber : "No Serial Number Available"}
-            </p>
-          </div>
-          { inventory.status === '2' ?
-            (<div className="flex mt-2.5">
-              <div className="text-error bg-[#FFF0F0] text-center py-1 rounded w-28 text-sm">
-                <p>UNPUBLISHED</p>
-              </div>
-            </div>)
-            :
-            (<div className="flex mt-2.5">
-              <div className="text-primary bg-[#EBF7FF] text-center py-1 rounded w-28 text-sm">
-                <p>PUBLISHED</p>
-              </div>
-            </div>)
-          }
-        </div>
-      </div>
-      {open && (
-        <PreviewInventoryModal
-          open={open}
-          handleCancel={handleCancel}
-          inventory={inventory}
-          category={category}
-        />
-      )}
-      {openEdit && (
-        <AddEventModal open={openEdit} handleCancel={handleCancelEdit} inventoryId={inventory.address} productId={inventory.productId} />
-      )}
-      {editModalOpen && (
-        <UpdateInventoryModal
-          open={editModalOpen}
-          handleCancel={handleEditModalClose}
-          debouncedSearchTerm={debouncedSearchTerm}
-          inventoryToUpdate={{
-            inventory: inventory,
-            category: category,
-          }}
-        />
-      )}
-      {resellModalOpen && (
-        <ResellModal
-          open={resellModalOpen}
-          handleCancel={handleResellModalClose}
-          inventory={inventory}
-        />
-      )}
-    </Card>
+          {open && (
+            <PreviewInventoryModal
+              open={open}
+              handleCancel={handleCancel}
+              inventory={inventory}
+              category={category}
+            />
+          )}
+          {openEdit && (
+            <AddEventModal open={openEdit} handleCancel={handleCancelEdit} inventoryId={inventory.address} productId={inventory.productId} />
+          )}
+          {editModalOpen && (
+            <UpdateInventoryModal
+              open={editModalOpen}
+              handleCancel={handleEditModalClose}
+              debouncedSearchTerm={debouncedSearchTerm}
+              inventoryToUpdate={{
+                inventory: inventory,
+                category: category,
+              }}
+            />
+          )}
+          {resellModalOpen && (
+            <ResellModal
+              open={resellModalOpen}
+              handleCancel={handleResellModalClose}
+              inventory={inventory}
+            />
+          )}
+        </Card>
+      }
+    </div>
   );
 };
 
