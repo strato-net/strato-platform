c42901114af5b39c7beda8c1143a19cd98b4b19b
David Nallapu <davidsam1998@gmail.com>
2023-12-13 15:17:58 -0500

    added

--- marketplace/backend/dapp/mercata-base-contracts/Templates/Assets/Mintable.sol
+++ marketplace/backend/dapp/mercata-base-contracts/Templates/Assets/Mintable.sol
@@ -50,7 +50,7 @@ abstract contract Mintable is UTXO {
         quantity += _quantity;
         return RestStatus.OK;
     }
-
+    
     function _callMint(address _newOwner, uint _quantity) internal virtual override{
         UTXO newAsset = mint(_quantity);
         Asset(newAsset).transferOwnership(_newOwner, _quantity);
