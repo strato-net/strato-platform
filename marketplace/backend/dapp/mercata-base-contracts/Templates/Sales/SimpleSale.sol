fd7d44b248449d7af3fb3bc596b0914c998a6b4b
Ariya Amarjargal <amarj003@umn.edu>
2023-12-13 16:38:15 -0500

    change addy

--- marketplace/backend/dapp/mercata-base-contracts/Templates/Sales/SimpleSale.sol
+++ marketplace/backend/dapp/mercata-base-contracts/Templates/Sales/SimpleSale.sol
@@ -1,6 +1,6 @@
 pragma es6;
 pragma strict;
-import <a3d9911aeffee71e0c6cd37946d80f3864b49d45>;
+import <ed13af446c955a0fe01417e962fca11ff3721b0f>;
 
 /// @title A representation of asset sale contract
 contract SimpleSale is Sale {
