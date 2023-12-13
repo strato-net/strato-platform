5c1794d7ba74701300916cf683392c6073604c35
David Nallapu <davidsam1998@gmail.com>
2023-12-13 10:42:14 -0500

    imported Mintable directly

--- marketplace/backend/dapp/mercata-base-contracts/Templates/Assets/SemiFungible.sol
+++ marketplace/backend/dapp/mercata-base-contracts/Templates/Assets/SemiFungible.sol
@@ -1,7 +1,8 @@
 pragma es6;
 pragma strict;
 
-import <86483be23fa65cf7f992d9cb35eca840e74090bc>;
+import "Mintable.sol";
+import "../Enums/RestStatus.sol";
 
 /// @title A representation of Carbon assets
 abstract contract SemiFungible is Mintable {
@@ -34,11 +35,11 @@ abstract contract SemiFungible is Mintable {
         return UTXO(address(c)); // Typechecker won't let me cast directly to UTXO
     }
 
-    function _callMint(address _newOwner, uint _quantity) internal override{
+    function _callMint(address _newOwner, uint _quantity) internal override returns(uint){
         for (uint i = 0; i < _quantity; i++) {
             UTXO newAsset = mint(1);
             Asset(newAsset).transferOwnership(_newOwner, 1);
         }
-        
+        return RestStatus.OK;
     }
 }
