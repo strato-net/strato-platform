3ff5ee3c63d27850ab5b03cbc1109d91d106cbd1
divyanshjain9 <divyansh.b@rejolut.com>
2023-12-15 18:42:12 +0530

    fix : new responsive cards for individual categories

---
+++ marketplace/ui/src/components/Order/ResponsiveTransferOrdersCard.js
@@ -0,0 +1,62 @@
+import { Button, Spin, Typography } from "antd";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import routes from "../../helpers/routes";
+
+export const ResponsiveTransferOrderCard = ({ data, isLoading}) => {
+    const statusComponent = (status) => {
+        let classes
+        switch (status) {
+            case 'Closed':
+                classes = "text-[#119B2D]"
+                break;
+            case 'Awaiting Fulfillment':
+                classes = "text-[#FF8C00]"
+                break;
+            case 'Cancelled':
+                classes = "text-[#FF0000]]"
+                break;
+            case 'Awaiting Shipment':
+                classes = "text-[#13188A]"
+                break;
+            default:
+                classes = "text-[#202020]"
+                break;
+        }
+        return classes;
+    }
+    return (
+        <Spin wrapperClassName="orders_responsive_cards" spinning={isLoading} delay={500} size="large">
+            { data.length > 0 ? data.map((item) => {
+                    return (
+                <div className="z-40 border border-[#E9E9E9] w-full rounded-md flex flex-col justify-center items-center gap-3 pb-4">
+                        <div className={`p-2 px-4 w-full flex justify-between bg-[#E9E9E9]`}>
+                            <Typography>Transfer Number</Typography>
+                            <Typography>{item?.transferNumber || 'N/A'}</Typography>
+                        </div>
+                        <div className={`p-2 px-4 w-full flex justify-between`}>
+                            <Typography>From</Typography>
+                            <Typography>{item?.oldOwnerOrganization || 'N/A'}</Typography>
+                        </div>
+                        <div className={`p-2 px-4 w-full flex justify-between`}>
+                            <Typography>To</Typography>
+                            <Typography className={`text-[#202020]`}>{item?.newOwnerOrganization || 'N/A'}</Typography>
+                        </div>
+                        <div className={`p-2 px-4 w-full flex justify-between`}>
+                            <Typography>Date</Typography>
+                            <Typography className={`text-[#202020]`}>{item?.date || 'N/A'}</Typography>
+                        </div>
+                        <div className={`p-2 px-4 w-full flex justify-between`}>
+                            <Typography>Product Name</Typography>
+                            <Typography className={`text-[#202020]`}>{item?.productName || 'N/A'}</Typography>
+                        </div>
+                        <div className={`p-2 px-4 w-full flex justify-between`}>
+                            <Typography>Quantity</Typography>
+                            <Typography className={`text-[#202020]`}>{item?.quantity || 'N/A'}</Typography>
+                        </div>
+                </div>
+                    )
+            }) : <Typography  className="text-center text-lg m-6 font-semibold">No data</Typography> }
+        </Spin>
+    )
+}
