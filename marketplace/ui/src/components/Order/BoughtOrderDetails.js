fed9c8f0ccea5f7025e97e5ec0181b0c135ba2f8
divyanshjain9 <divyansh.b@rejolut.com>
2023-12-15 16:34:02 +0530

    feat : order sold and order bought detail page ui

--- marketplace/ui/src/components/Order/BoughtOrderDetails.js
+++ marketplace/ui/src/components/Order/BoughtOrderDetails.js
@@ -11,8 +11,10 @@ import {
   Button,
   Spin,
   Image,
+  Tabs,
+  DatePicker,
 } from "antd";
-import { useMatch } from "react-router-dom";
+import { useLocation, useMatch } from "react-router-dom";
 import { actions } from "../../contexts/order/actions";
 import { useOrderDispatch, useOrderState } from "../../contexts/order";
 import routes from "../../helpers/routes";
@@ -28,6 +30,9 @@ import { apiUrl, HTTP_METHODS } from "../../helpers/constants";
 import RestStatus from "http-status-codes";
 import TagManager from "react-gtm-module";
 import image_placeholder from "../../images/resources/image_placeholder.png";
+import dayjs from "dayjs";
+import TransfersTable from "./TransfersTable";
+import SoldOrdersTable from "./SoldOrdersTable";
 
 const BoughtOrderDetails = ({ user, users }) => {
   const [comment, setcomment] = useState("");
@@ -40,6 +45,8 @@ const BoughtOrderDetails = ({ user, users }) => {
   const { TextArea } = Input;
   const [paid, setPaid] = useState(false)
   const [isLoadingPaymentStatus, setisLoadingPaymentStatus] = useState(false);
+  const [selectedDate, setSelectedDate] = useState("");
+  const { state } = useLocation()
 
   const navigate = useNavigate();
 
@@ -172,27 +179,47 @@ const BoughtOrderDetails = ({ user, users }) => {
   const OrderData = ({ title, value }) => {
     return (
       <Col>
-        <Text className="block text-primaryC text-[13px] mb-2">{title}</Text>
-        <Text className="block text-primaryC text-[17px]">{value}</Text>
+        <Text className="block text-[#6A6A6A] text-[13px] mb-2">{title}</Text>
+        <Text className="block text-[#202020] text-[17px] font-semibold">{value}</Text>
       </Col>
     );
   };
 
-  const statusComponent = (status) => {
-    let textClass = "text-orange bg-[#FFF6EC]";
-    if (status === getStatus(2)) {
-      textClass = "text-blue  bg-[#EBF7FF]";
-    } else if (status === getStatus(3)) {
-      textClass = "text-success  bg-[#EAFFEE]";
-    } else if (status === getStatus(4)) {
-      textClass = "text-error  bg-[#FFF0F0]";
-    }
+  const onDateChange = (date) => {
+    setSelectedDate(date);
+  };
 
+  const statusComponent = (status) => {
+    let textClass = "bg-[#FFF6EC]";
+    if (status === "Awaiting Shipment") {
+      textClass = "bg-[#EBF7FF]";
+    } else if (status === "Awaiting Fulfillment"){
+      textClass = "bg-[#FF8C0033]"
+    } else if (status === "Closed") {
+      textClass = "bg-[#119B2D33]";
+    } else if (status === "Canceled") {
+      textClass = "bg-[#FFF0F0]";
+    }
+    let bgClass = "bg-[#119B2D]";
+    if (status === "Awaiting Shipment") {
+      bgClass = "bg-[#13188A]";
+    } else if (status === "Awaiting Fulfillment"){
+      bgClass = "bg-[#FF8C00]"
+    } else if (status === "Closed") {
+      bgClass = "bg-[#119B2D]";
+    } else if (status === "Canceled") {
+      bgClass = "bg-[#FF0000]";
+    }
     return (
-      <div className={classNames(textClass, "text-center text-xs p-1 rounded")}>
-        <p>{status}</p>
+      <div className={classNames(textClass, "status_contain w-max text-center py-1 px-3 rounded-xl flex justify-start items-center gap-1 p-1")}>
+        <div className={classNames(bgClass, "h-3 w-3 rounded-sm")}></div>
+        <p className="!mb-0 text-sm">{status}</p>
       </div>
     );
+  };
+
+  const onChange = (key) => {
+    navigate(routes.Orders.url, { state: { defaultKey: key } })
   };
 
   let column = [
@@ -306,7 +333,7 @@ const BoughtOrderDetails = ({ user, users }) => {
         </div>
       ) : (
         <div>
-          <Breadcrumb className="text-xs ml-14 mt-14 mb-8">
+          <Breadcrumb className="text-sm ml-20 mt-14 mb-8">
             <Breadcrumb.Item href="" onClick={e => e.preventDefault()}>
               <ClickableCell href={routes.Marketplace.url}>
                 Home
@@ -322,87 +349,127 @@ const BoughtOrderDetails = ({ user, users }) => {
             </Breadcrumb.Item>
           </Breadcrumb>
 
-          <Card className="mx-12 mb-14">
-            <div className="flex">
-              <Text className="font-semibold text-primaryB">Order Details</Text>
-              {
-                !paid ? <div /> : <div className={classNames("text-success  bg-[#EAFFEE]", "ml-4 w-20 text-center text-xs p-1 rounded")}>
-                  <p>Paid</p>
-                </div>
-              }
-            </div>
-            <Row className="my-6 justify-between">
-              <OrderData title="NUMBER" value={`#${details.order.orderId}`} />
-              <Divider type="vertical" className="h-14 bg-secondryD" />
-              <OrderData
-                title="BUYER"
-                value={details.order.purchasersCommonName}
-              />
-              <Divider type="vertical" className="h-14 bg-secondryD" />
-              <OrderData
-                title="SELLER"
-                value={details.order.sellersCommonName}
-              />
-              <Divider type="vertical" className="h-14 bg-secondryD" />
-              <OrderData title="TOTAL ($)" value={details.order.totalPrice} />
-              <Divider type="vertical" className="h-14 bg-secondryD" />
-              <OrderData
-                title="DATE"
-                value={getStringDate(details.order.createdDate, US_DATE_FORMAT)}
-              />
-              <Divider type="vertical" className="h-14 bg-secondryD" />
-              <Col>
-                <Text className="block text-primaryC text-[13px] mb-2">
-                  STATUS
-                </Text>
-                {statusComponent(status)}
-              </Col>
-            </Row>
+          <Tabs
+              className="mx-20 mt-0"
+              onChange={onChange}
+              defaultActiveKey={state == null ? "Bought" : state.defaultKey}
+              items={[
+                {
+                  label: <p id="sold-tab" className="font-semibold text-base">Orders (Sold)</p>,
+                  key: "Sold",
+                  children: <SoldOrdersTable user={user} selectedDate={dayjs(selectedDate).startOf('day').unix()} />
+                  
+            },
+            {
+              label: <p id="bought-tab" className="font-semibold text-base">Orders (Bought)</p>,
+              key: "Bought",
+              children: 
+                <Card className="mb-14 shadow-card_shadow">
+                  <div className="flex mb-2">
+                  <Text className="font-semibold text-lg text-primaryB flex gap-4 items-center">Order Details {statusComponent(status)}</Text>
+                    {
+                      !paid ? <div /> : <div className={classNames("text-success  bg-[#EAFFEE]", "ml-4 w-20 text-center text-xs p-1 rounded")}>
+                        <p>Paid</p>
+                      </div>
+                    }
+                    </div>
+                  <Text className="text-black my-2 font-semibold">Please upload serial number(s) (if any) and/or enter the fulfillment date to close the order</Text>
+                  <Row className="my-6 justify-between bg-[#F6F6F6] p-4 pb-2 rounded">
+                    <OrderData title="NUMBER" value={`#${details.order.orderId}`} />
+                    <Divider type="vertical" className="h-14 bg-secondryD" />
+                    <OrderData
+                      title="BUYER"
+                      value={details.order.purchasersCommonName}
+                    />
+                    <Divider type="vertical" className="h-14 bg-secondryD" />
+                    <OrderData
+                      title="SELLER"
+                      value={details.order.sellersCommonName}
+                    />
+                    <Divider type="vertical" className="h-14 bg-secondryD" />
+                    <OrderData title="TOTAL ($)" value={details.order.totalPrice} />
+                    <Divider type="vertical" className="h-14 bg-secondryD" />
+                    <OrderData
+                      title="DATE"
+                      value={getStringDate(details.order.createdDate, US_DATE_FORMAT)}
+                    />
+                    <Divider type="vertical" className="h-14 bg-secondryD" />
+                    <Col>
+                      <Text className="block text-primaryC text-[13px] mb-2">
+                        STATUS
+                      </Text>
+                      {statusComponent(status)}
+                    </Col>
+                    <Divider type="vertical" className="h-14 bg-secondryD" />
+                    <div className="text-xs order_detail_date">
+                      <Text className="block text-primaryC text-[13px] mb-0">
+                        ORDER CLOSE DATE
+                      </Text>
+                      <DatePicker
+                        value={
+                          selectedDate
+                        }
+                        disabledDate={(current) => {
+                          return current && current < dayjs().endOf('day');
+                        }}
+                        onChange={onDateChange}
+                        disabled={details.order.status === "3" || details.order.status === "4"}
+                      />
+                    </div>
+                  </Row>
 
-            <Row className="flex-nowrap items-center justify-between mb-6">
-              <div className="w-full">
-                <Text className="block text-primaryC text-[13px] mb-2">
-                  COMMENTS
-                </Text>
-                <TextArea
-                  rows={2}
-                  placeholder="Enter Comments"
-                  value={decodeURIComponent(comment)}
-                  disabled={status !== getStatus(1) || details.order.paymentSessionId !== ""}
-                  onChange={(event) => {
-                    setcomment(event.target.value);
-                  }}
-                />
-              </div>
-              <Button
-                id="cancel-order-button"
-                type="primary"
-                className="w-48 h-9 ml-6 mt-3 bg-primary !hover:bg-primaryHover"
-                disabled={status !== getStatus(1) || comment === "" || details.order.paymentSessionId !== ""}
-                onClick={() => {
-                  handleCancelOrder()
-                  window.LOQ.push(['ready', async LO => {
-                    await LO.$internal.ready('events')
-                    LO.events.track('Order Details: Cancel Order')
-                  }])
-                  TagManager.dataLayer({
-                    dataLayer: {
-                      event: 'orderDetails_bought_cancel_click',
-                    },
-                  });
-                }}
-              >
-                Cancel Order
-              </Button>
-            </Row>
+                  <Row className="flex-nowrap items-center justify-between mb-6">
+                    <div className="w-full">
+                      <Text className="block text-primaryC text-[13px] mb-2">
+                        COMMENTS
+                      </Text>
+                      <TextArea
+                        rows={2}
+                        placeholder="Enter Comments"
+                        value={decodeURIComponent(comment)}
+                        disabled={status !== getStatus(1) || details.order.paymentSessionId !== ""}
+                        onChange={(event) => {
+                          setcomment(event.target.value);
+                        }}
+                      />
+                    </div>
+                    <Button
+                      id="cancel-order-button"
+                      type="primary"
+                      className="w-48 h-9 ml-6 mt-3 bg-primary !hover:bg-primaryHover"
+                      disabled={status !== getStatus(1) || comment === "" || details.order.paymentSessionId !== ""}
+                      onClick={() => {
+                        handleCancelOrder()
+                        window.LOQ.push(['ready', async LO => {
+                          await LO.$internal.ready('events')
+                          LO.events.track('Order Details: Cancel Order')
+                        }])
+                        TagManager.dataLayer({
+                          dataLayer: {
+                            event: 'orderDetails_bought_cancel_click',
+                          },
+                        });
+                      }}
+                    >
+                      Cancel Order
+                    </Button>
+                  </Row>
 
-            <DataTableComponent
-              columns={column}
-              data={data}
-              isLoading={false}
-              scrollX="100%"
-            />
-          </Card>
+                  <DataTableComponent
+                    columns={column}
+                    data={data}
+                    isLoading={false}
+                    scrollX="100%"
+                  />
+                </Card>
+            },
+            {
+              label: <p id="transfers-tab" className="font-semibold text-base">Transfers</p>,
+              key: "Transfers",
+              children: <TransfersTable user={user} selectedDate={dayjs(selectedDate).startOf('day').unix()} />
+            }
+          ]}
+        />
         </div>
       )}
 
