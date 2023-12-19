1c196f28aa3d3a33bc31ef5e389e91e29bd9391f
nikitamendelbaum <nikitamendelbaum@users.noreply.github.com>
2023-12-19 21:25:27 +0700

    HIGHWAY_URL empty flag value fix

--- strato/highway/doit.sh
+++ strato/highway/doit.sh
@@ -5,11 +5,12 @@ if [[ -n $HIGHWAY_URL ]]; then
     echo "HIGHWAY_URL must start with protocol (http:// or https://)"
     exit 1
   fi
+  HIGHWAY_URL_FLAG="--highwayUrl=${HIGHWAY_URL}"
 fi
 
 echo "Environment variables:
 highway:
---highwayUrl="${HIGHWAY_URL:-}" \
+${HIGHWAY_URL_FLAG:-}
 --awsaccesskeyid=$EXT_STORAGE_S3_ACCESS_KEY_ID
 --awssecretaccesskey=$EXT_STORAGE_S3_SECRET_ACCESS_KEY
 --awss3bucket=$EXT_STORAGE_S3_BUCKET
@@ -20,10 +21,8 @@ echo 'Starting up Highway...'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-# TODO: make sure we can pass empty string in flag and stil lthe default will be used. If not - pass flag optionally
-
 blockapps-highway-server \
-  --highwayUrl="${HIGHWAY_URL:-}" \
+  ${HIGHWAY_URL_FLAG:-} \
   --awsaccesskeyid="$EXT_STORAGE_S3_ACCESS_KEY_ID" \
   --awssecretaccesskey="$EXT_STORAGE_S3_SECRET_ACCESS_KEY" \
   --awss3bucket="$EXT_STORAGE_S3_BUCKET" \
