a769bbf722db92a7db98da8be8f8e020442e1c94
divyanshjain9 <divyansh.b@rejolut.com>
2023-12-22 15:57:25 +0530

    fix : user name render on logout submenu

--- marketplace/ui/src/components/Header/Header.js
+++ marketplace/ui/src/components/Header/Header.js
@@ -160,7 +160,7 @@ const HeaderComponent = ({ isOauth, user, loginUrl, showMenu, handleSubMenu, han
     {value: "marketplace", path: routes.MarketplaceProductList.url, label: "Marketplace"},
     {value: "orders", path: routes.Orders.url, label: "Orders"},
     {value: "mystore", path: "/mystore", label: "My Store"},
-    user ? {value: "logout", path: "/logout", label: "Logout"} : null,
+    user ? {value: "logout", path: "/logout", label: <div><p className="!mb-0">Logout</p><p className="text-xs text-gray">{user?.preferred_username}</p></div>} : null,
   ]
 
   const handleIntMenuTab = (data) => {
