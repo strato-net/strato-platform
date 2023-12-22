c790ac5f375b71114c382bc156d126a325d0357a
AvnishT24 <avnish.t@rejolut.com>
2023-12-22 13:17:19 +0530

    bug: Add to cart UI fix

--- marketplace/ui/src/components/MarketPlace/ResponsiveCart.js
+++ marketplace/ui/src/components/MarketPlace/ResponsiveCart.js
@@ -10,10 +10,16 @@ import {
 } from "../../contexts/marketplace";
 import { useAuthenticateState } from "../../contexts/authentication";
 import TagManager from "react-gtm-module";
-import image_placeholder from "../../images/resources/image_placeholder.png"
+import image_placeholder from "../../images/resources/image_placeholder.png";
 
-const ResponsiveCart = ({ data, confirm, AddQty, MinusQty, ValueQty, removeCartList }) => {
-
+const ResponsiveCart = ({
+  data,
+  confirm,
+  AddQty,
+  MinusQty,
+  ValueQty,
+  removeCartList,
+}) => {
   const navigate = useNavigate();
   const [tax, setTax] = useState(0);
   const [shipping, setShipping] = useState(0);
@@ -21,7 +27,9 @@ const ResponsiveCart = ({ data, confirm, AddQty, MinusQty, ValueQty, removeCartL
   const [total, setTotal] = useState(0);
   const marketplaceDispatch = useMarketplaceDispatch();
   let { hasChecked, isAuthenticated, loginUrl } = useAuthenticateState();
-  const [faqOpenState, setFaqOpenState] = useState(Array(data.length).fill(false));
+  const [faqOpenState, setFaqOpenState] = useState(
+    Array(data.length).fill(false)
+  );
   const toggleFaq = (index) => {
     setFaqOpenState((prev) => {
       const newState = [...prev];
@@ -53,160 +61,203 @@ const ResponsiveCart = ({ data, confirm, AddQty, MinusQty, ValueQty, removeCartL
     }
   });
 
-
-
   return (
-    <div className="h-max border border-[#E9E9E9]  rounded-md mt-3 flex flex-col gap-[18px] p-3  items-center   ">
+    <div className=" border border-[#E9E9E9]  rounded-md mt-3 flex flex-col gap-[18px] items-center    ">
       {data.map((element, index) => {
         let qty = element.qty;
-        let product = element
+        let product = element;
         return (
-          <div
-            className="p-3 border border-[#E9E9E9] rounded-md w-full sm:w-[95%] md:w-[90%]"
-            key={index}
-          >
-            <div className="flex justify-between ">
-              <div className="flex gap-x-3 items-center">
-                <img
-                  src={element?.item?.image || image_placeholder}
-                  alt={element?.item?.name}
-                  className="w-12 h-12 rounded-[4px] object-contain "
-                />
-                <Typography className="text-[#13188A] text-base font-medium ">
-                  {element?.item?.name}
+          <div className="p-3 w-full">
+            <div
+              className="p-3  border border-[#E9E9E9] rounded-md w-full "
+              key={index}
+            >
+              <div className="flex justify-between ">
+                <div className="flex gap-x-3 ">
+                  <img
+                    src={element?.item?.image}
+                    className="w-12 h-12 rounded-[4px] border "
+                  />
+                  <Typography className="text-[#13188A] text-base mt-[-4px] font-semibold ">
+                    {element?.item?.name}
+                  </Typography>
+                </div>
+                <div className="mt-[-9px]">
+                  <Button
+                    type="link"
+                    icon={
+                      <img
+                        src={Images.CancelIcon}
+                        alt="remove"
+                        className="w-[18px] h-[18px] "
+                      />
+                    }
+                    onClick={() => {
+                      removeCartList(element.action);
+                    }}
+                    className="hover:text-error cursor-pointer text-xl"
+                  />
+                </div>
+              </div>
+
+              <div className="flex justify-between ml-[20%]  items-center pt-[6px]">
+                <Typography className="font-semibold text-[#202020] text-sm">{`$${element?.unitPrice}`}</Typography>
+                <div>
+                  <div className="flex items-center justify-center mt-2">
+                    <div
+                      onClick={() => {
+                        MinusQty(qty, product);
+                      }}
+                      className="  w-5 h-5    bg-[#E9E9E9] flex justify-center items-center cursor-pointer rounded-full"
+                    >
+                      <p className="text-lg text-[#202020] font-medium">-</p>
+                    </div>
+                    <InputNumber
+                      className=" w-8 border-none text-[#202020] font-medium bg-[transparent]  rounded-none outline-none  text-sm text-center flex flex-col justify-center"
+                      min={1}
+                      value={qty}
+                      defaultValue={qty}
+                      controls={false}
+                      onChange={(e) => {
+                        ValueQty(product, e);
+                      }}
+                    />
+                    <div
+                      onClick={() => {
+                        AddQty(product);
+                      }}
+                      className="  w-5 h-5    bg-[#E9E9E9] flex justify-center items-center cursor-pointer rounded-full"
+                    >
+                     <p className="text-lg text-[#202020] font-medium">+</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="px-3 h-10 flex justify-between items-center rounded-md mt-[18px] bg-[#F6F6F6]">
+                <Typography className="text-[#202020] text-sm font-semibold ">
+                  Details
+                </Typography>
+                <Button
+                  type="link"
+                  icon={
+                    <img
+                      src={Images.Dropdown}
+                      alt=""
+                      className={`w-5 h-5 transition-transform transform ${
+                        faqOpenState[index] ? "rotate-180" : "rotate-0"
+                      } `}
+                      onClick={() => {
+                        toggleFaq(index);
+                      }}
+                    />
+                  }
+                ></Button>
+              </div>
+
+              {faqOpenState[index] && (
+                <div
+                  className={`overflow-hidden  ${
+                    faqOpenState[index] ? "" : "max-h-0"
+                  }`}
+                >
+                  <div
+                    className={` border-t bg-[#F6F6F6] rounded-b-md flex flex-col gap-3 p-2 `}
+                  >
+                    <div className="flex justify-between">
+                      <Typography className="text-sm text-[#202020] font-medium">Seller:</Typography>
+                      <Typography className="text-sm text-[#202020] font-semibold">{element?.sellersCommonName}</Typography>
+                    </div><div className="flex justify-between">
+                      <Typography className="text-sm text-[#202020] font-medium">Unit price($):</Typography>
+                      <Typography className="text-sm text-[#202020] font-semibold">{`$${element?.unitPrice}`}</Typography>
+                    </div>
+                    <div className="flex justify-between">
+                      <Typography className="text-sm text-[#202020] font-medium">shipping Charges:</Typography>
+                      <Typography className="text-sm text-[#202020] font-semibold">{element?.shippingCharges}</Typography>
+                    </div>
+                    <div className="flex justify-between">
+                      <Typography  className="text-sm text-[#202020] font-medium">Tax($):</Typography>
+                      <Typography className="text-sm text-[#202020] font-semibold">{element?.tax}</Typography>
+                    </div>
+                  </div>
+                </div>
+              )}
+
+              <div className="pt-[18px] flex justify-between ">
+                <Typography className="text-sm  font-semibold text-[#202020]">
+                  Amount($):
+                </Typography>
+                <Typography className="text-sm  font-semibold text-[#202020]">
+                  {element?.amount}
                 </Typography>
               </div>
-              <Button
-                type="link"
-                icon={<img src={Images.CancelIcon} alt="remove" className="w-[18px] h-[18px] " />}
-                onClick={() => {
-                  removeCartList(element.action);
-                }}
-                className="hover:text-error cursor-pointer text-xl"
-              />
-            </div>
-
-            <div className="flex justify-around  items-center">
-              <Typography >{`$${element?.unitPrice}`}</Typography>
-              <div>
-                <div className="flex items-center justify-center mt-2">
-                  <div
-                    onClick={() => {
-                      MinusQty(qty, product);
-                    }}
-                    className="  w-6 h-6   mr-1 bg-[#E9E9E9] flex justify-center items-center cursor-pointer rounded-full">
-                    <MinusOutlined className="text-[17px] text-[#202020] font-medium " />
-                  </div>
-                  <InputNumber className=" w-[43px] border-none text-[#202020]  bg-[transparent]  rounded-none outline-none font-medium text-sm text-center flex flex-col justify-center"
-                    min={1} value={qty} defaultValue={qty} controls={false}
-                    onChange={e => {
-                      ValueQty(product, e);
-                    }} />
-                  <div
-                    onClick={() => {
-                      AddQty(product);
-                    }}
-                    className="  w-6 h-6   ml-1 bg-[#E9E9E9] flex justify-center items-center cursor-pointer rounded-full">
-                    <PlusOutlined className="text-[17px] text-[#202020] font-medium" />
-                  </div>
-                </div>
-
-              </div>
-
-            </div>
-
-            <div className="px-3 py-[10px] flex justify-between items-center rounded-[4px] mt-[18px] bg-[#E9E9E9]">
-              <Typography className="text-[#202020] text-sm font-medium ">Details</Typography>
-              <Button type="link" icon={<img src={Images.Dropdown} alt="" className={`w-5 h-5 transition-transform transform ${faqOpenState[index] ? 'rotate-180' : 'rotate-0'} `} onClick={() => { toggleFaq(index) }} />} ></Button>
-            </div>
-
-            {faqOpenState[index] &&
-              <div className={`overflow-hidden transition-max-height ease-in-out duration-500 ${faqOpenState[index] ? 'max-h-[120px]' : 'max-h-0'}`}>
-
-                <div className={` border-t bg-[#E9E9E9] flex flex-col gap-2 p-2 `}>
-                  <div className="flex justify-between">
-                    <Typography>Unit price($):</Typography>
-                    <Typography>{element?.unitPrice}</Typography>
-                  </div><div className="flex justify-between">
-                    <Typography>shipping Charges:</Typography>
-                    <Typography>{element?.shippingCharges}</Typography>
-                  </div><div className="flex justify-between">
-                    <Typography>Tax($):</Typography>
-                    <Typography>{element?.tax}</Typography>
-                  </div>
-                </div>
-              </div>}
-
-
-            <div className="pt-[18px] flex justify-between">
-              <Typography className="text-sm  font-medium text-[#202020]">Amount($):</Typography>
-              <Typography className="text-sm  font-semibold text-[#202020]">{element?.amount}</Typography>
-
             </div>
           </div>
         );
       })}
 
-      <div className=" flex flex-col w-full sm:w-[95%] md:w-[90%]">
-        <Row className="p-3 mt-2  bg-[#F6F6F6]">
-          <Col className="w-full  bg-[#F6F6F6]">
-            <Row className="justify-between ">
-              <p className="text-sm w-36 mr-2">Item Total</p>
+      <div className=" flex flex-col w-full  bg-[#F6F6F6] px-[10px] py-3">
+        <div className=" flex flex-col gap-3">
+          <div className="flex justify-between">
+            <p className="text-sm  font-medium ">Sub Total:</p>
+            <p className="text-sm  text-right font-semibold">${total}</p>
+          </div>
+          <div className="flex justify-between">
+            <p className="text-sm font-medium  ">Tax:</p>
 
-              <p className="text-sm ml-2 w-20 text-right">${total}</p>
-            </Row>
-            <Row className="justify-between mt-0.5">
-              <p className="text-sm w-36 mr-2">Tax</p>
+            <p className="text-sm  font-semibold  text-right">${tax}</p>
+          </div>
+          <div className="flex justify-between">
+            <p className="text-sm font-medium  ">Shipping Charges:</p>
 
-              <p className="text-sm ml-2 w-20 text-right">${tax}</p>
-            </Row>
-            <Row className="justify-between mt-0.5">
-              <p className="text-sm w-36 mr-2">Shipping Charges</p>
+            <p className="text-sm  font-semibold  text-right">${shipping}</p>
+          </div>
+          <div className="w-full h-[1px] bg-[#E9E9E9]"></div>
+          <div className="flex justify-between">
+            <p className="text-sm font-medium ">Total</p>
 
-              <p className="text-sm ml-2 w-20 text-right">${shipping}</p>
-            </Row>
-            <Divider />
-            <Row className="justify-between">
-              <p className="text-sm font-semibold w-36 mr-2">Total</p>
+            <p className="text-sm   font-semibold  text-right">
+              ${total + tax + shipping}
+            </p>
+          </div>
+        </div>
 
-              <p className="text-sm font-semibold ml-2 w-20 text-right">
-                ${total + tax + shipping}
-              </p>
-            </Row>
-          </Col>
-        </Row>
-        {!confirm && <Row className="justify-center lg:mt-12 mt-2 ">
-          <Button
-            type="primary"
-            id="submit-order-button"
-            className=" w-full sm:w-44 h-9 bg-primary !hover:bg-primaryHover"
-            onClick={() => {
-              if (hasChecked && !isAuthenticated && loginUrl !== undefined) {
-                window.location.href = loginUrl;
-              } else {
-                actions.addItemToConfirmOrder(marketplaceDispatch, data);
-                window.LOQ.push(['ready', async LO => {
-                  // Track an event
-                  await LO.$internal.ready('events')
-                  LO.events.track('Submit Order (from cart)')
-                }])
-                TagManager.dataLayer({
-                  dataLayer: {
-                    event: 'submit_order_from_cart',
-                  },
-                });
-                navigate("/confirmOrder");
-              }
-            }}
-            disabled={data.length === 0}
-          >
-            Submit Order
-          </Button>
-        </Row>}
+        {!confirm && (
+          <Row className="justify-center mt-4">
+            <Button
+              type="primary"
+              id="submit-order-button"
+              className=" w-full sm:w-44 h-9 bg-[#13188A]"
+              onClick={() => {
+                if (hasChecked && !isAuthenticated && loginUrl !== undefined) {
+                  window.location.href = loginUrl;
+                } else {
+                  actions.addItemToConfirmOrder(marketplaceDispatch, data);
+                  window.LOQ.push([
+                    "ready",
+                    async (LO) => {
+                      // Track an event
+                      await LO.$internal.ready("events");
+                      LO.events.track("Submit Order (from cart)");
+                    },
+                  ]);
+                  TagManager.dataLayer({
+                    dataLayer: {
+                      event: "submit_order_from_cart",
+                    },
+                  });
+                  navigate("/confirmOrder");
+                }
+              }}
+              disabled={data.length === 0}
+            >
+              Submit Order
+            </Button>
+          </Row>
+        )}
       </div>
     </div>
   );
 };
 
 export default ResponsiveCart;
-
