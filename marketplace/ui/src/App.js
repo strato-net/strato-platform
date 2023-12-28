fd46b4d5764e8e7dab26ef1819e4c2e525dbe2a5
AvnishT24 <avnish.t@rejolut.com>
2023-12-28 15:27:44 +0530

    Fix: spacing issue in the carousel fix

--- marketplace/ui/src/App.js
+++ marketplace/ui/src/App.js
@@ -83,7 +83,7 @@ const App = () => {
             handleMenuTab={handleMenuTab}
           />
         </UsersProvider>
-        <Content className={`${showMenu ? 'overflow-y-hidden md:overflow-auto h-[83.5vh] md:h-auto w-[100vw] md:w-auto bg-[#00000020] md:bg-white relative mt-24 md:mt-28' : 'mt-[89px] md:mt-[107px] '}`}>
+        <Content className={`${showMenu ? 'overflow-y-hidden md:overflow-auto h-[83.5vh] md:h-auto w-[100vw] md:w-auto bg-[#00000020] md:bg-white relative mt-24 md:mt-28' : 'mt-[89px] md:mt-[98px] '}`}>
           <AuthenticatedRoutes user={user} users={users} />
         </Content>
       </Layout>
