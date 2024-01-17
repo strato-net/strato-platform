b06c7beb5f81c50bc797a7d5ca87b27e10e787bb
Tanuj Soni <tanuj@blockapps.net>
2024-01-17 19:08:17 +0530

    Add URL in fetchItemTransfers

--- marketplace/ui/src/contexts/inventory/actions.js
+++ marketplace/ui/src/contexts/inventory/actions.js
@@ -540,12 +540,18 @@ const actions = {
     dispatch({ type: actionDescriptors.fetchItemTransfers });
 
     try {
-      let range = '';
       let searchQuery = '';
       const end = date + 86400;
+      const baseUrl = new URL(`${apiUrl}/inventory/transfers/items`);
+      baseUrl.searchParams.set('limit', limit);
+      baseUrl.searchParams.set('order', `transferDate.${order}`);
+      baseUrl.searchParams.set('offset', offset);
+      baseUrl.searchParams.set('or', `(oldOwnerCommonName.eq.${ownerCommonName},newOwnerCommonName.eq.${ownerCommonName})`);
+
       if (date) {
-        range = `&range[]=transferDate,${date},${end}`
+        baseUrl.searchParams.set('range[]', `transferDate,${date},${end}`);
       }
+      let url = baseUrl.toString();
       if (search) {
         const searchValue = isNaN(search) ? search : parseInt(search);
         if (!isNaN(searchValue)) {
@@ -554,8 +560,8 @@ const actions = {
           searchQuery = search ? `&queryValue=${searchValue}&queryFields=assetName` : '';
         }
       }
-      let url = `${apiUrl}/inventory/transfers/items?limit=${limit}&order=transferDate.${order}&offset=${offset}&or=(oldOwnerCommonName.eq.${ownerCommonName},newOwnerCommonName.eq.${ownerCommonName})${searchQuery}${range}`
-
+      url += searchQuery;
+     
       const response = await fetch(url, {
         method: HTTP_METHODS.GET,
 
