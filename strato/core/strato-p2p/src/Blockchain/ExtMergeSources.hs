afdd0e0e53e0fac2b51724eb68bf71bed85a8e20
Matthew-Mosior <mattm.github@gmail.com>
2024-01-24 10:47:03 -0500

    Removing ResourceT and MonadResource constraint from mergeSourcesByForce in strato/core/strato-p2p/src/Blockchain/ExtMergeSources.hs.

--- strato/core/strato-p2p/src/Blockchain/ExtMergeSources.hs
+++ strato/core/strato-p2p/src/Blockchain/ExtMergeSources.hs
@@ -19,7 +19,6 @@ import           Control.Monad
 import           Control.Monad.IO.Class
 import           Control.Monad.IO.Unlift
 import           Control.Monad.Trans.Class
-import           Control.Monad.Trans.Resource
 import           Data.Conduit                 as DC
 import           Data.Conduit.TMChan          hiding (mergeSources)
 import qualified Data.Conduit.List            as CL
@@ -29,27 +28,10 @@ import           Ki.Unlifted as KIU
 import           UnliftIO.Exception
 import           UnliftIO.STM
 
--- | Modifies a TVar, returning its new value.
-modifyTVar'' :: TVar a
-             -> (a -> a)
-             -> STM a
-modifyTVar'' tv f = do
-  !x <- f <$> readTVar tv
-  writeTVar tv x
-  return x
-
 liftSTM :: forall (m :: Type -> Type) a. MonadIO m
         => STM a
         -> m a
 liftSTM = liftIO . atomically
-
-decRefcount :: TVar Int
-            -> TBMChan a
-            -> STM ()
-decRefcount tv chan = do
-  n <- modifyTVar'' tv (subtract 1)
-  when (n == 0) $
-    closeTBMChan chan
 
 -- | Convert channel into the source.
 --
@@ -80,34 +62,32 @@ chanSink ch writer = CL.mapM_ $ liftIO . atomically . writer ch
 {-# INLINE chanSink #-}
 
 -- | Custom mergeSourcesByForce function.
-mergeSourcesByForce :: ( MonadResource m1
-                       , Monad m2
+mergeSourcesByForce :: ( Monad m2
                        , MonadUnliftIO m1
                        , Eq a1
-                       , Data.String.IsString a1
+                       , IsString a1
                        , MonadLogger m1
+                       , Traversable t
                        )
-                    => [(a1,ConduitT () a2 m1 ())]
+                    => t (a1, ConduitT () a2 m1 ())
                     -> Int
                     -> Scope
                     -> m2 (ConduitT z a2 m1 ())
 mergeSourcesByForce sx bound scp =
   return $ do
-    (chkey,c) <- allocate (liftSTM $ newTBMChan bound)
-                          (liftSTM . closeTBMChan)
-    refcount <- liftSTM . newTVar $ length sx
-    st <- lift $ askUnliftIO
-    _  <- forM sx $ \(tag,s) ->
-            liftIO $ fork scp 
-                 ( (unliftIO st $
-                      runConduit $ s .| chanSink c writeTBMChan)
-                        `finally` ( case tag of
-                                      "canarySource" -> do
-                                        _ <- unliftIO st $ $logInfoS "canary/exit" "" >> killCanary
-                                        liftSTM $ decRefcount refcount c
-                                      _              ->
-                                        liftSTM $ decRefcount refcount c
-                                  )
+    c   <- liftSTM $ newTBMChan bound
+    st  <- lift $ askUnliftIO
+    _   <- forM sx $ \(tag,s) ->
+             liftIO $ fork scp 
+               ( (unliftIO st $
+                    runConduit $ s .| chanSink c writeTBMChan
                  )
+                      `finally` ( case tag of
+                                    "canarySource" -> do
+                                      unliftIO st $ $logInfoS "canary/exit" "" >> killCanary
+                                      liftSTM $ closeTBMChan c
+                                    _              ->
+                                      liftSTM $ closeTBMChan c                
+                                )
+               )
     chanSource c readTBMChan
-    release chkey
