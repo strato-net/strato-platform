9a8d95647bfe21abb9e95964a31ecb48002331ff
Tanuj Soni <tanuj@blockapps.net>
2024-02-08 19:20:13 +0530

    Minor fix

--- marketplace/ui/src/components/MarketPlace/CategoryProductList.js
+++ marketplace/ui/src/components/MarketPlace/CategoryProductList.js
@@ -50,10 +50,8 @@ const CategoryProductList = ({ user }) => {
   const [selectedCategories, setSelectedCategories] = useState(categoryQueryValueArr);
   const [selectedSubCategories, setSelectedSubCategories] = useState([]);
   const [selectedBrands, setSelectedBrands] = useState([]);
-  const [maxPrice, setMaxPrice] = useState();
-  const [minPrice, setMinPrice] = useState();
-  const minimumPrice = minPrice ? minPrice : 0
-  const maximumPrice = maxPrice ? maxPrice : MAX_PRICE
+  const [minPrice, setMinPrice] = useState(0);
+  const [maxPrice, setMaxPrice] = useState(MAX_PRICE);
   const [subCategories, setSubCategories] = useState([]);
   const [uniqueProductNames, setUniqueProductNames] = useState([]);
   const [desktopOpenFilter, setDesktopOpenFilter] = useState(true);
@@ -122,8 +120,8 @@ const CategoryProductList = ({ user }) => {
         arrayToStr(selectedCategories),
         arrayToStr(selectedSubCategories),
         arrayToStr(selectedBrands),
-        minimumPrice,
-        maximumPrice,
+        minPrice,
+        maxPrice,
         searchQueryValue
       );
     } else if (hasChecked && isAuthenticated) {
@@ -132,8 +130,8 @@ const CategoryProductList = ({ user }) => {
         arrayToStr(selectedCategories),
         arrayToStr(selectedSubCategories),
         arrayToStr(selectedBrands),
-        minimumPrice,
-        maximumPrice,
+        minPrice,
+        maxPrice,
         searchQueryValue
       );
     }
@@ -190,8 +188,8 @@ const CategoryProductList = ({ user }) => {
     navigate('/category')
     clearSelection()
     setSelectedCategories([]);
-    setMaxPrice()
-    setMinPrice()
+    setMinPrice(0)
+    setMaxPrice(MAX_PRICE)
   }
 
   const checkValues = (e, arr) => {
@@ -374,11 +372,11 @@ const CategoryProductList = ({ user }) => {
       {DesktopCollapseComponent(
         <Panel header={<Text strong className="text-base">Price ($)</Text>} key="1">
           <Space>
-            <InputNumber min={0} prefix='$' value={minPrice} placeholder="min" onChange={(e) => {
+            <InputNumber size="large" min={0} controls={false} prefix='$' value={minPrice} placeholder="min" onChange={(e) => {
               e === null ? setMinPrice(0) : setMinPrice(e)
             }} />
             -
-            <InputNumber min={minPrice} prefix='$' value={maxPrice} placeholder="max" onChange={(e) => {
+            <InputNumber size="large" controls={false} min={minPrice} prefix='$' value={maxPrice} placeholder="max" onChange={(e) => {
               e === null ? setMaxPrice(MAX_PRICE) : setMaxPrice(e)
             }} />
           </Space>
