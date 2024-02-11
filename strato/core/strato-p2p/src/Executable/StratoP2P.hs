c39b9e1cba998cfde0c1e3782720c862d0c4f7f1
Matthew-Mosior <mattm.github@gmail.com>
2024-02-11 01:02:31 -0500

    Got strato/core/strato-p2p/src/Executable/StratoP2PLoopback.hs and strato/core/strato-p2p/src/Executable/StratoP2P.hs to compile with the Config/Context datatype initialization to occur within forked peer thread(s).

--- strato/core/strato-p2p/src/Executable/StratoP2P.hs
+++ strato/core/strato-p2p/src/Executable/StratoP2P.hs
@@ -5,21 +5,46 @@
 
 module Executable.StratoP2P where
 
-import           Control.Concurrent.Async.Lifted.Safe(Concurrently(..),runConcurrently)
+--import           Control.Concurrent.Async.Lifted.Safe(Concurrently(..),runConcurrently)
 import           Control.Exception hiding (catch)
 import           Control.Exception.Lifted (catch)
+import           Control.Monad.Change.Alter
+import           Control.Monad.Change.Modify
+--import           Control.Monad.IO.Class
+import           Control.Monad.Trans.Resource
+import           Control.Monad.Trans.Control
+import           Control.Monad.Trans.Reader
 import           Control.Monad.Logger
-import           Data.Foldable            (asum)
+import           Crypto.Types.PubKey.ECC
+import           Data.ByteString
+--import           Data.Foldable            (asum)
 import qualified Data.Text as T
+import           UnliftIO.Async (race_)
 import           BlockApps.Logging as BL
+import           BlockApps.X509.Certificate
+import           Blockchain.Blockstanbul.Messages
 import           Blockchain.Context
+import           Blockchain.Data.Block
+import           Blockchain.Data.DataDefs
+import           Blockchain.Data.Enode
+import           Blockchain.Sequencer.Event
+import           Blockchain.Strato.Discovery.Data.Peer
+import           Blockchain.Strato.Model.Address
+import           Blockchain.Strato.Model.ChainMember
+import           Blockchain.Strato.Model.Keccak256
+import           Blockchain.Strato.Model.Secp256k1
+import           Network.Haskoin.Crypto.BigWord
 import           Executable.StratoP2PClient
 import           Executable.StratoP2PLoopback
 import           Executable.StratoP2PServer
 
-raceAll :: [BL.LoggingT IO a]
-        -> BL.LoggingT IO a
-raceAll = runConcurrently . asum . map Concurrently
+--raceAll :: [BL.LoggingT IO a]
+--        -> BL.LoggingT IO a
+--raceAll :: ( MonadBaseControl IO m
+--           )
+--        => [BL.LoggingT m a]
+--        -> BL.LoggingT m a
+--raceAll = runConcurrently . asum . map Concurrently
 
 {-
 stratoP2P :: ( MonadP2P n
@@ -40,8 +65,73 @@ stratoP2P runner =
 --             )
 --          => PeerRunner n (BL.LoggingT IO) () -> BL.LoggingT IO ()
 --stratoP2P runner =
+stratoP2P :: ( MonadBaseControl IO m
+             , MonadResource m
+             , MonadLogger m
+             , MonadUnliftIO m
+             , HasVault (BL.LoggingT m)
+             , Accessible [BlockData] (BL.LoggingT m)
+             , Accessible ActionTimestamp (BL.LoggingT m)
+             , Accessible RemainingBlockHeaders (BL.LoggingT m)
+             , Accessible PeerAddress (BL.LoggingT m)
+             , Accessible MaxReturnedHeaders (BL.LoggingT m)
+             , Accessible ConnectionTimeout (BL.LoggingT m)
+             , Accessible GenesisBlockHash (BL.LoggingT m)
+             , Accessible BestBlockNumber (BL.LoggingT m)
+             , Accessible PublicKey (BL.LoggingT m)
+             , Accessible BondedPeers (ReaderT Config (ResourceT (BL.LoggingT m)))
+             , Accessible BondedPeers (BL.LoggingT m)
+             , Accessible AvailablePeers (ReaderT Config (ResourceT (BL.LoggingT m)))
+             , Accessible AvailablePeers (BL.LoggingT m)
+             , Alters (T.Text,Keccak256) (Proxy (Outbound Blockchain.Blockstanbul.Messages.WireMessage)) (BL.LoggingT m)
+             , Alters (IPAsText,TCPPort) ActivityState (ReaderT Config (ResourceT (BL.LoggingT m)))
+             , Alters (IPAsText,TCPPort) ActivityState (BL.LoggingT m)
+             , Alters Keccak256 (Proxy (Inbound WireMessage)) (BL.LoggingT m)
+             , Alters Keccak256 BlockData (BL.LoggingT m)
+             , Alters Keccak256 OutputBlock (BL.LoggingT m)
+             , Modifiable ActionTimestamp (BL.LoggingT m)
+             , Modifiable RemainingBlockHeaders (BL.LoggingT m)
+             , Modifiable [BlockData] (BL.LoggingT m)
+             , Modifiable PeerAddress (BL.LoggingT m)
+             , Modifiable BestBlock (BL.LoggingT m)
+             , Modifiable WorldBestBlock (BL.LoggingT m)
+             , Replaceable (IPAsText,Point) PeerBondingState (ReaderT Config (ResourceT (BL.LoggingT m)))
+             , Replaceable (IPAsText,Point) PeerBondingState (BL.LoggingT m)
+             , Replaceable PPeer TcpEnableTime (ReaderT Config (ResourceT (BL.LoggingT m)))
+             , Replaceable PPeer TcpEnableTime (BL.LoggingT m)
+             , Replaceable PPeer UdpEnableTime (ReaderT Config (ResourceT (BL.LoggingT m)))
+             , Replaceable PPeer UdpEnableTime (BL.LoggingT m)
+             , Replaceable PPeer PeerDisable (ReaderT Config (ResourceT (BL.LoggingT m)))
+             , Replaceable PPeer PeerDisable (BL.LoggingT m)
+             , Replaceable PPeer T.Text (ReaderT Config (ResourceT (BL.LoggingT m)))
+             , Replaceable PPeer T.Text (BL.LoggingT m)
+             , Selectable (IPAsText,UDPPort,ByteString) Point (BL.LoggingT m)
+             , Selectable Word256 ChainMemberRSet (BL.LoggingT m)
+             , Selectable Word256 ChainInfo (BL.LoggingT m)
+             , Selectable Integer (Canonical BlockData) (BL.LoggingT m)
+             , Selectable Keccak256 (Private (Word256,OutputTx)) (BL.LoggingT m)
+             , Selectable Keccak256 ChainTxsInBlock (BL.LoggingT m)
+             , Selectable Address X509CertInfoState (BL.LoggingT m)
+             , Selectable IPAsText PPeer (BL.LoggingT m)
+             , Selectable Point PPeer (BL.LoggingT m)
+             , Selectable ChainMemberParsedSet [ChainMemberParsedSet] (BL.LoggingT m)
+             , Selectable ChainMemberParsedSet TrueOrgNameChains (BL.LoggingT m)
+             , Selectable ChainMemberParsedSet FalseOrgNameChains (BL.LoggingT m)
+             , Selectable ChainMemberParsedSet X509CertInfoState (BL.LoggingT m)
+             , Selectable ChainMemberParsedSet IsValidator (BL.LoggingT m)
+             , Stacks Block (BL.LoggingT m)
+             , Outputs (BL.LoggingT m) [IngestEvent]
+             , RunsClient (ReaderT Config (ResourceT (BL.LoggingT m)))
+             , RunsServer (ReaderT Config (ResourceT (BL.LoggingT m))) (BL.LoggingT m)
+             )
+          => BL.LoggingT m ()
 stratoP2P =
-  raceAll [ stratoP2PLoopback `catch` (\(e :: SomeException) -> $logErrorS "stratoP2PLoopback ERROR" . T.pack $ show e)
-          , stratoP2PClient   `catch` (\(e :: SomeException) -> $logErrorS "stratoP2PClient ERROR" . T.pack $ show e)
-          , stratoP2PServer   `catch` (\(e :: SomeException) -> $logErrorS "stratoP2PServer ERROR" . T.pack $ show e)
-          ]
+  race_ (stratoP2PLoopback `catch` (\(e :: SomeException) -> $logErrorS "stratoP2PLoopback ERROR" . T.pack $ show e))
+        ( race_ (stratoP2PClient   `catch` (\(e :: SomeException) -> $logErrorS "stratoP2PClient ERROR" . T.pack $ show e))
+                (stratoP2PServer   `catch` (\(e :: SomeException) -> $logErrorS "stratoP2PServer ERROR" . T.pack $ show e))
+        )   
+     
+  --raceAll [ stratoP2PLoopback `catch` (\(e :: SomeException) -> $logErrorS "stratoP2PLoopback ERROR" . T.pack $ show e)
+  --        , stratoP2PClient   `catch` (\(e :: SomeException) -> $logErrorS "stratoP2PClient ERROR" . T.pack $ show e)
+  --        , stratoP2PServer   `catch` (\(e :: SomeException) -> $logErrorS "stratoP2PServer ERROR" . T.pack $ show e)
+  --        ]
--- strato/core/strato-p2p/src/Executable/StratoP2PLoopback.hs
+++ strato/core/strato-p2p/src/Executable/StratoP2PLoopback.hs
@@ -9,14 +9,22 @@ module Executable.StratoP2PLoopback (stratoP2PLoopback) where
 import BlockApps.Logging
 import Blockchain.Blockstanbul (WireMessage)
 import Blockchain.Context
+import Blockchain.Options
 import Blockchain.Sequencer.Event
 import Blockchain.Sequencer.Kafka
+import Blockchain.SeqEventNotify
+--import Blockchain.Strato.Discovery.Data.Peer
 import Blockchain.Strato.Model.Keccak256
 import Conduit
+--import Control.Monad.Trans.Reader
 import qualified Control.Monad.Change.Alter as A
+--import Control.Monad.Change.Modify
+--import Crypto.Types.PubKey.ECC
 import qualified Data.Text as T
 import Prometheus
 import Text.Format
+import Data.IORef
+import Data.Set.Ordered (empty)
 
 {-# NOINLINE loopbackEvents #-}
 loopbackEvents :: Vector T.Text Counter
@@ -29,10 +37,28 @@ loopbackEvents =
 recordEvent :: MonadIO m => T.Text -> m ()
 recordEvent lab = liftIO $ withLabel loopbackEvents lab incCounter
 
-stratoP2PLoopback :: MonadP2P n => PeerRunner n (LoggingT IO) () -> LoggingT IO ()
-stratoP2PLoopback runner = do
+--stratoP2PLoopback :: MonadP2P n => PeerRunner n (LoggingT IO) () -> LoggingT IO ()
+--stratoP2PLoopback runner = do
+stratoP2PLoopback :: ( MonadLogger m
+                     , MonadUnliftIO m
+                     --, Accessible AvailablePeers (ReaderT Config (ResourceT (LoggingT m)))
+                     --, Accessible BondedPeers (ReaderT Config (ResourceT (LoggingT m)))
+                     --, A.Replaceable (IPAsText,Point) PeerBondingState (ReaderT Config (ResourceT (LoggingT m)))
+                     --, A.Replaceable PPeer TcpEnableTime (ReaderT Config (ResourceT (LoggingT m)))
+                     --, A.Replaceable PPeer UdpEnableTime (ReaderT Config (ResourceT (LoggingT m)))
+                     --, A.Replaceable PPeer PeerDisable (ReaderT Config (ResourceT (LoggingT m)))
+                     --, A.Replaceable PPeer T.Text (ReaderT Config (ResourceT (LoggingT m)))
+                     --, A.Alters (IPAsText,TCPPort) ActivityState (ReaderT Config (ResourceT (LoggingT m)))
+                     --, RunsServer (ReaderT Config (ResourceT (LoggingT m))) (LoggingT m)
+                     )
+                  => LoggingT m ()
+stratoP2PLoopback = do
+  wireMessagesRef <- liftIO $ newIORef empty
+  cfg <- initConfig wireMessagesRef flags_maxReturnedHeaders
+  let sSource  = seqEventNotificationSource $ contextKafkaState initContext
+      runner f = runContextM cfg $ f sSource
   $logInfoS "stratoP2PLoopback" "Reflecting PBFT back to unseq since 2019"
-  runner $ \sSource -> do
+  runner $ \sSource' -> do
     let toWireMessage = \case
           P2pBlockstanbul wm -> do
             let msgHash = rlpHash wm
@@ -48,7 +74,7 @@ stratoP2PLoopback runner = do
           _ -> pure Nothing
 
     runConduit $
-      sSource
+      sSource'
         .| iterMC (const $ recordEvent "in")
         .| concatMapMC toWireMessage
         .| iterMC (const $ recordEvent "out")
