ffb9f9fb39e5f87a5d84e22f64126323aace4715
Tanuj Soni <tanuj@blockapps.net>
2024-02-27 17:11:05 +0530

    Minor fix

--- marketplace/ui/src/components/MarketPlace/CategoryProductList.js
+++ marketplace/ui/src/components/MarketPlace/CategoryProductList.js
@@ -592,6 +592,7 @@ const CategoryProductList = ({ user }) => {
                     current={page}
                     onChange={onPageChange}
                     total={marketplaceListCount}
+                    defaultPageSize={limit}
                     showSizeChanger={false}
                     className="flex justify-center my-5 "
                   />
