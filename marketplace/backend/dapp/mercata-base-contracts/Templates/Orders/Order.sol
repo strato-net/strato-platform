96f8bae4b4dc447a2ebb99808a9d91c9233cf5ac
Neelkanth Poosa <neelkanthpoosa@blockappss-air.mynetworksettings.com>
2024-02-27 19:22:58 -0500

    (order-closing-automation):added modifier, updated constructor logic of order

--- marketplace/backend/dapp/mercata-base-contracts/Templates/Orders/Order.sol
+++ marketplace/backend/dapp/mercata-base-contracts/Templates/Orders/Order.sol
@@ -13,6 +13,7 @@ abstract contract Order is Utils {
         CLOSED,
         CANCELED,
         PAYMENT_PENDING,
+        PAID,
         MAX
     }
 
@@ -69,9 +70,14 @@ abstract contract Order is Utils {
         status = _status;
         shippingAddressId = _shippingAddressId;
         paymentSessionId = _paymentSessionId;
+        
+        if(status == OrderStatus.PAID)
+        {
+            completeOrder(_createdDate,"Thank you for your payment.");
+        }
     }
 
-    function completeOrder(uint _fulfillmentDate, string _comments) external returns (uint) {
+    function completeOrder(uint _fulfillmentDate, string _comments) returns (uint) {
         require(status != OrderStatus.CLOSED && status != OrderStatus.CANCELED, "Order already closed.");
         for (uint i = 0; i < saleAddresses.length; i++) {
             if (!completedSales[i]) {
