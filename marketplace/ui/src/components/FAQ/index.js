20fdd8aebabc76e9b8af2dbf23d0bded69e7f5a9
Michael Tan <tanmichael74@yahoo.com>
2024-03-01 15:20:48 -0500

    additional faq notes

--- marketplace/ui/src/components/FAQ/index.js
+++ marketplace/ui/src/components/FAQ/index.js
@@ -7,7 +7,16 @@ const faqs = [
     answer:
       "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
   },
-  // More questions...
+  {
+    question: "What's the best thing about Switzerland?",
+    answer:
+      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+  },
+  {
+    question: "What's the best thing about Switzerland?",
+    answer:
+      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+  },
 ]
 
 export default function Example() {
