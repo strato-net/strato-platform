508be2897931d911cddad6eedb3e6b9381acca42
Haile Thompson <hailethomp@gmail.com>
2024-03-05 19:39:38 -0500

    add: logic to editor rendering. If user is owner and clicks edit enable rendering

--- marketplace/ui/src/components/MarketPlace/RichEditor.js
+++ marketplace/ui/src/components/MarketPlace/RichEditor.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useEditor, EditorContent, BubbleMenu, Extension } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Color from "@tiptap/extension-color";
@@ -7,7 +7,7 @@ import ListItem from "@tiptap/extension-list-item";
 import TextAlign from "@tiptap/extension-text-align";
 import Link from "@tiptap/extension-link";
 import "./index.css";
-import { Dropdown } from "antd";
+import { Dropdown, Popconfirm, Button } from "antd";
 import { DownOutlined } from "@ant-design/icons";
 import {
   BoldOutlined,
@@ -116,11 +116,11 @@ const MenuBar = ({ editor, addLink }) => {
   editor.on("selectionUpdate", ({ editor }) => {
     const attrs = editor.getAttributes("textStyle");
 
-      // Set the active attributes if they are consistent across the selection
-      setSelectedFontSize(attrs.fontSize || "");
-      setSelectedFontFamily(
-        attrs.fontFamily ? attrs.fontFamily.split(",")[0] : ""
-      );
+    // Set the active attributes if they are consistent across the selection
+    setSelectedFontSize(attrs.fontSize || "");
+    setSelectedFontFamily(
+      attrs.fontFamily ? attrs.fontFamily.split(",")[0] : ""
+    );
   });
 
   const fontSizes = [
@@ -331,24 +331,26 @@ const MenuBar = ({ editor, addLink }) => {
   );
 };
 
-const Tiptap = () => {
+const RichEditor = ({ details, user, isAuthenticated }) => {
+  const isEditable = isAuthenticated && details.owner === user.userAddress;
+  const [editMode, setEditMode] = useState(false);
+
   const editor = useEditor({
     extensions: [
       StarterKit,
       Color.configure({ types: [TextStyle.name, ListItem.name] }),
       TextStyle.configure({ types: [ListItem.name] }),
-      TextAlign.configure({
-        types: ["heading", "paragraph"], // Make sure to configure it for the node types you want to align.
-      }),
+      TextAlign.configure({ types: ["heading", "paragraph"] }),
       Link,
       FontSize,
       FontFamily,
     ],
-    content: `<p>Hello World!</p>`,
+    content: details.description,
+    editable: isEditable,
   });
 
   if (!editor) {
-    return null; // Or some loading state
+    return null;
   }
 
   const addLink = () => {
@@ -357,7 +359,6 @@ const Tiptap = () => {
 
     // Check if a URL was provided
     if (url) {
-      // Use the editor commands to insert the link
       editor
         .chain()
         .focus()
@@ -367,83 +368,124 @@ const Tiptap = () => {
     }
   };
 
+  const handleSave = () => {
+    const updatedContent = editor.getHTML();
+    console.log("Saved content:", updatedContent);
+
+    setEditMode(false);
+  };
+
+  const handleCancel = () => {
+    editor.commands.setContent(details.description);
+    setEditMode(false);
+  };
+
   return (
-    <div className="tiptap">
-      {editor && (
-        <BubbleMenu
-          className="bubble-menu"
-          editor={editor}
-          tippyOptions={{ duration: 100 }}
+    <div className="tiptap" key={`editor-${editMode}`}>
+      {isEditable && !editMode && (
+        <Button
+          onClick={() => setEditMode(true)}
+          className="flex items-center px-4 py-5 !bg-primary !hover:bg-primaryHover !text-white"
         >
-          {/* Bold */}
-          <button
-            onClick={() => editor.chain().focus().toggleBold().run()}
-            disabled={!editor.can().chain().focus().toggleBold().run()}
-            className={editor.isActive("bold") ? "is-active" : ""}
-          >
-            <BoldOutlined />
-          </button>
-
-          {/* Italic */}
-          <button
-            onClick={() => editor.chain().focus().toggleItalic().run()}
-            disabled={!editor.can().chain().focus().toggleItalic().run()}
-            className={editor.isActive("italic") ? "is-active" : ""}
-          >
-            <ItalicOutlined />
-          </button>
-
-          {/* Strike */}
-          <button
-            onClick={() => editor.chain().focus().toggleStrike().run()}
-            disabled={!editor.can().chain().focus().toggleStrike().run()}
-            className={editor.isActive("strike") ? "is-active" : ""}
-          >
-            <StrikethroughOutlined />
-          </button>
-
-          {/* Align Text */}
-          <button
-            onClick={() => editor.chain().focus().setTextAlign("left").run()}
-          >
-            <AlignLeftOutlined />
-          </button>
-          <button
-            onClick={() => editor.chain().focus().setTextAlign("center").run()}
-          >
-            <AlignCenterOutlined />
-          </button>
-          <button
-            onClick={() => editor.chain().focus().setTextAlign("right").run()}
-          >
-            <AlignRightOutlined />
-          </button>
-
-          {/* Lists */}
-          <button
-            onClick={() => editor.chain().focus().toggleBulletList().run()}
-            className={editor.isActive("bulletList") ? "is-active" : ""}
-          >
-            <UnorderedListOutlined />
-          </button>
-          <button
-            onClick={() => editor.chain().focus().toggleOrderedList().run()}
-            className={editor.isActive("orderedList") ? "is-active" : ""}
-          >
-            <OrderedListOutlined />
-          </button>
-
-          {/* Insert Link */}
-          <button onClick={addLink}>
-            <LinkOutlined />
-          </button>
-        </BubbleMenu>
+          Edit
+        </Button>
       )}
+      {editMode && (
+        <>
+          <BubbleMenu
+            className="bubble-menu"
+            editor={editor}
+            tippyOptions={{ duration: 100 }}
+          >
+            {/* Bold */}
+            <button
+              onClick={() => editor.chain().focus().toggleBold().run()}
+              disabled={!editor.can().chain().focus().toggleBold().run()}
+              className={editor.isActive("bold") ? "is-active" : ""}
+            >
+              <BoldOutlined />
+            </button>
 
-      <MenuBar editor={editor} addLink={addLink} />
-      <EditorContent editor={editor} />
+            {/* Italic */}
+            <button
+              onClick={() => editor.chain().focus().toggleItalic().run()}
+              disabled={!editor.can().chain().focus().toggleItalic().run()}
+              className={editor.isActive("italic") ? "is-active" : ""}
+            >
+              <ItalicOutlined />
+            </button>
+
+            {/* Strike */}
+            <button
+              onClick={() => editor.chain().focus().toggleStrike().run()}
+              disabled={!editor.can().chain().focus().toggleStrike().run()}
+              className={editor.isActive("strike") ? "is-active" : ""}
+            >
+              <StrikethroughOutlined />
+            </button>
+
+            {/* Align Text */}
+            <button
+              onClick={() => editor.chain().focus().setTextAlign("left").run()}
+            >
+              <AlignLeftOutlined />
+            </button>
+            <button
+              onClick={() =>
+                editor.chain().focus().setTextAlign("center").run()
+              }
+            >
+              <AlignCenterOutlined />
+            </button>
+            <button
+              onClick={() => editor.chain().focus().setTextAlign("right").run()}
+            >
+              <AlignRightOutlined />
+            </button>
+
+            {/* Lists */}
+            <button
+              onClick={() => editor.chain().focus().toggleBulletList().run()}
+              className={editor.isActive("bulletList") ? "is-active" : ""}
+            >
+              <UnorderedListOutlined />
+            </button>
+            <button
+              onClick={() => editor.chain().focus().toggleOrderedList().run()}
+              className={editor.isActive("orderedList") ? "is-active" : ""}
+            >
+              <OrderedListOutlined />
+            </button>
+
+            {/* Insert Link */}
+            <button onClick={addLink}>
+              <LinkOutlined />
+            </button>
+          </BubbleMenu>
+          <MenuBar editor={editor} addLink={addLink} />
+
+          <EditorContent editor={editor} />
+          <div className="flex flex-row">
+            <Button type="primary" onClick={handleSave} className="flex items-center">
+              Save
+            </Button>
+            <Popconfirm
+              title="Are you sure you want to cancel your changes?"
+              onConfirm={handleCancel}
+              onCancel={() => {}}
+              okText="Yes"
+              cancelText="No"
+            >
+              <Button type="" className="flex items-center">Cancel</Button>
+            </Popconfirm>
+          </div>
+        </>
+      )}
+      {!editMode && (
+        <div dangerouslySetInnerHTML={{ __html: details.description }} />
+      )}
     </div>
   );
 };
 
-export default Tiptap;
+export default RichEditor;
