2184358a2076b228646a0f374a12db962eb364a9
Michael Tan <tanmichael74@yahoo.com>
2024-03-11 14:10:06 -0400

    faq updates

--- marketplace/ui/src/components/FAQ/index.js
+++ marketplace/ui/src/components/FAQ/index.js
@@ -5,31 +5,31 @@ const faqs = [
   {
     question: "About Mercata?",
     answer:
-      "Mercata is a marketplace built on the STRATO blockchain decentralizing the buying, selling, and trading of tokenized RWA.",
+    "Mercata is a marketplace built on the STRATO blockchain decentralizing the buying, selling, and trading of tokenized RWA."
   },
   {
     question: "What is a RWA?",
     answer:
-      "RWA stands for 'real world assets'. RWAs are non-traditional assets with unique markets that are now able to be tokenized and transacted through the blockchain.",
+    "RWA stands for 'real world assets'. RWAs are non-traditional assets with unique markets that are now able to be tokenized and transacted through the blockchain."
   },
   {
     question: "Using Mercata",
     answer:
-      "Create an account, discover unique market categories, and pick your favorite assets to buy, sell, and invest. In the future you also will also be able to work with other members of the community to obtain fractionalized ownership of assets.",
+    "Create an account, discover unique market categories, and pick your favorite assets to buy, sell, and invest. In the future you also will also be able to work with other members of the community to obtain fractionalized ownership of assets."
   },
   {
     question: "Investing with Mercata",
     answer:
-      "When you purchase a tokenized asset on Mercata, dependent on the rules set by the smart contract, you have the choice to either redeem the physical item, or continue to hold the token and speculate on its future price potential.",
+    "When you purchase a tokenized asset on Mercata, dependent on the rules set by the smart contract, you have the choice to either redeem the physical item, or continue to hold the token or list it for sale."
   },
   {
     question: "Trading RWA with Mercata",
     answer:
-      "To sell on Mercata either relist tokenized assets you have in your inventory, or tokenize your own items to be tradable on the marketplace",
+    "To sell on Mercata either relist tokenized assets you have in your inventory, or tokenize your own items to be tradable on the marketplace"
   },
   {
     question: "Privacy and Security",
-    answer: "See our privacy and security policy for more info",
+    answer: "See our privacy and security policy for more info"
   }, 
 ];
 
