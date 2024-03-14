0c8accc3a7eddee013b4bd087fe2dc1debc2499b
David <dkmoncayo@Gmail.com>
2024-03-14 16:14:25 -0400

    Replace string correctly

--- marketplace/backend/dapp/dapp/dapp.clean.js
+++ marketplace/backend/dapp/dapp/dapp.clean.js
@@ -3,6 +3,6 @@ import { replaceInFiles } from './dapp.helper.js';
 describe("dapp.clean.js", function () {
     it('Revert imports back to <BASE_CODE_COLLECTION>', async () => {
         const pattern = /(import <[0-9a-fA-F]{40}>)/
-        replaceInFiles('./dapp/', pattern, 'BASE_CODE_COLLECTION');
+        replaceInFiles('./dapp/', pattern, 'import <BASE_CODE_COLLECTION>');
     })
 })
