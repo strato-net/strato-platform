5fbefcba5ef349a9fe513b364357af876ab1e6d2
David <dkmoncayo@Gmail.com>
2024-03-14 16:13:44 -0400

    Update RegExp

--- marketplace/backend/dapp/dapp/dapp.clean.js
+++ marketplace/backend/dapp/dapp/dapp.clean.js
@@ -2,7 +2,7 @@ import { replaceInFiles } from './dapp.helper.js';
 
 describe("dapp.clean.js", function () {
     it('Revert imports back to <BASE_CODE_COLLECTION>', async () => {
-        const pattern = /[0-9a-fA-F]{40}/
+        const pattern = /(import <[0-9a-fA-F]{40}>)/
         replaceInFiles('./dapp/', pattern, 'BASE_CODE_COLLECTION');
     })
 })
