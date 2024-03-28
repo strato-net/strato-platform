79d0f29430975c2e01cb27f6ad0604bb1b93a8b7
Tanuj Soni <tanuj@blockapps.net>
2024-03-28 19:53:02 +0530

    Fix Card Size

--- marketplace/ui/src/components/MarketPlace/NewTrendingCard.js
+++ marketplace/ui/src/components/MarketPlace/NewTrendingCard.js
@@ -70,7 +70,7 @@ const NewTrendingCard = ({ topSellingProduct, addItemToCart, parent = "", api, c
     };
 
     return (
-        <div className={`relative trending_cards_container_card bg-white p-3 ${parent == 'Marketplace' ? 'min-w-[300px] w-auto' : 'min-w-[230px]'} min-w-[230px] md:min-w-[300px] rounded-md flex flex-col gap-2 md:gap-3 shadow-card_shadow h-max`}>
+        <div className={`relative trending_cards_container_card bg-white p-3 ${parent == 'Marketplace' ? 'min-w-[300px] w-auto' : 'min-w-[230px]'}  min-w-[320px] md:min-w-[300px] rounded-md flex flex-col gap-2 md:gap-3 shadow-card_shadow h-max`}>
             {contextHolder}
             {shouldShowWishlistIcon && (
                 <div onClick={toggleWishlist} className="absolute top-2 right-2 cursor-pointer hover:scale-110 transition-transform duration-200">
