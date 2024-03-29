29a1387a7585da9ee51fe759cfd674febccf7244
Haile Thompson <hailethomp@gmail.com>
2024-03-29 15:51:03 -0400

    add: one size option

--- marketplace/ui/src/components/Inventory/CreateInventoryModal.js
+++ marketplace/ui/src/components/Inventory/CreateInventoryModal.js
@@ -183,7 +183,7 @@ const CreateInventoryModal = ({
     if (type === "Shoes") {
       setSizeOptions(["3.5", "4", "4.5", "5", "5.5", "6", "6.5", "7", "7.5", "8", "8.5", "9", "9.5", "10", "10.5", "11", "11.5", "12", "12.5", "13", "13.5", "14", "14.5", "15", "16", "17", "18"]);
     } else {
-      setSizeOptions(["XXS", "XS", "S", "M", "L", "XL", "XXL"]);
+      setSizeOptions(["O/S (One Size)", "XXS", "XS", "S", "M", "L", "XL", "XXL"]);
     }
   };
 
