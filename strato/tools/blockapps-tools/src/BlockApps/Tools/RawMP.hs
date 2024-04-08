9b686cc7d7499665901978f93df21ad5d7607807
David Moncayo <david_moncayo@blockapps.net>
2024-04-08 16:04:05 -0400

    print out formatted AddressState in rawmp

--- strato/tools/blockapps-tools/src/BlockApps/Tools/RawMP.hs
+++ strato/tools/blockapps-tools/src/BlockApps/Tools/RawMP.hs
@@ -3,6 +3,7 @@
 module BlockApps.Tools.RawMP (doit) where
 
 import Blockchain.Data.RLP
+import Blockchain.Data.AddressStateDB (AddressState(..))
 import qualified Blockchain.Database.MerklePatricia as MP
 import qualified Blockchain.Database.MerklePatricia.Internal as MP
 import Control.Monad.IO.Class
@@ -13,20 +14,16 @@ import Text.Format
 
 formatKV :: (N.NibbleString, RLPObject) -> String
 formatKV (key, val) =
-  format key ++ ": " ++ format (rlpDeserialize $ rlpDecode val)
+  format key ++ ":\n  " ++ format ((rlpDecode :: RLPObject -> AddressState) $ rlpDeserialize $ rlpDecode val)
 
 showVals :: MonadIO m => DB.DB -> MP.StateRoot -> m ()
 showVals sdb sr = do
   kvs <- runReaderT (MP.unsafeGetKeyVals sr "") sdb
-  liftIO . print $ length kvs
-  --liftIO . putStrLn $ displayS (renderPretty 1.0 200 $ vsep $ formatKV <$> kvs) ""
   liftIO . putStrLn $ unlines $ formatKV <$> kvs
 
 
 doit :: String -> MP.StateRoot -> IO ()
 doit filename sr = DB.runResourceT $ do
-  --    dbs <- openDBs theType
-  --    homeDir <- liftIO getHomeDirectory
   sdb <-
     DB.open
       filename
