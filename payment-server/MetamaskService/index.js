e7e647b37ce47b13e16e033093c71a54e49cfc2e
Ariya Amarjargal <amarj003@umn.edu>
2024-04-09 15:32:49 -0400

    Add MetamaskService routes and controller

---
+++ payment-server/MetamaskService/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const MetamaskServiceController = require("./metamaskService.controller");
+
+const router = express.Router();
+
+// router.post(
+//   "/onboard",
+//   MetamaskServiceController.metamaskOnboarding
+// );
+
+// router.get(
+//   "/status/:commonName",
+//   MetamaskServiceController.metamaskConnectStatus
+// );
+
+module.exports = router;
