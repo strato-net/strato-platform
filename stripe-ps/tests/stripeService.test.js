36f17c8d0f261d3538fba1cb3a834d7852ed177e
jxuan101 <jinhuai.xuan38@myhunter.cuny.edu>
2024-04-12 17:23:06 -0400

    Added working Stripe tests

--- stripe-ps/tests/stripeService.test.js
+++ stripe-ps/tests/stripeService.test.js
@@ -1,14 +1,81 @@
+import bodyParser from 'body-parser';
 import request from 'supertest';
 import express from 'express';
 import stripService from '../StripeService/index';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = new express();
+app.use(bodyParser.json());
 app.use('/Stripe', stripService);
 
+const testAccountId = process.env.TEST_ACCT_ID;
+const marketplaceUrl = process.env.MARKETPLACE_URL;
+const testSessionId = process.env.TEST_SESSION_ID;
+const testSellerId = process.env.TEST_SELLER_ID;
+
 describe('Stripe Endpoint Tests', function() {
-  test('Able to onboard a user without account Id', async () => {
-    const res = await request(app).get('/Stripe/onboard');
-    console.log(res);
+
+  it('Onboard without accountId', async () => {
+    const res = await request(app).get('/Stripe/onboard').set('referer', marketplaceUrl);
     expect(res.statusCode).toBe(200);
-  })
+  });
+
+  it('Onboard with accountId', async () => {
+    const res = await request(app).get(`/Stripe/onboard/${testAccountId}`).set('referer', marketplaceUrl);
+    expect(res.statusCode).toBe(200);
+  });
+
+  it('Check the status of a Stripe account', async () => {
+    const res = await request(app).get(`/Stripe/status/${testAccountId}`);
+    expect(res.statusCode).toBe(200);
+  });
+
+  it('Checkout endpoint should return checkout url', async () => {
+    const res = await request(app)
+      .post('/Stripe/checkout')
+      .set('referer', marketplaceUrl)
+      .send({ 
+        paymentTypes: ['card', 'us_bank_account'], 
+        cartData: {
+          buyerOrganization: 'TestOrg',
+          orderList: [{
+            quantity: 1,
+            assetAddress: '0',
+            firstSale: true,
+            unitPrice: 123
+          }],
+          orderTotal: 123,
+          shippingAddressId: 1,
+          tax: 0,
+          user: 'user',
+          email: 'user'
+        }, 
+        orderDetail: [{
+          productName: 'Test',
+          unitPrice: '123',
+          quantity: 1
+        }],
+        accountId: testAccountId 
+      });
+    expect(res.statusCode).toBe(200);
+  });
+
+  if (testSellerId && testSessionId) {
+    console.log('Running optional session and intent tests');
+
+    it('Retrieve the Stripe session given sessionId', async () => {
+      const res = await request(app).get(`/Stripe/session/${testSessionId}/${testSellerId}`);
+      expect(res.statusCode).toBe(200);
+    });
+
+    it('Retrieve the Stripe intent given sessionId', async () => {
+      const res = await request(app).get(`/Stripe/intent/${testSessionId}/${testSellerId}`);
+      expect(res.statusCode).toBe(200);
+    });
+  }
+  else {
+    console.log('Skipping optional session and intent tests');
+  }
+
 })
