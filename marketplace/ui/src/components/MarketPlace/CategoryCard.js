c4b51ec17d8917f3f81cfa5759800fa4c1c059a2
Ariya Amarjargal <amarj003@umn.edu>
2024-04-23 15:34:24 -0400

    mobile and desktop views

--- marketplace/ui/src/components/MarketPlace/CategoryCard.js
+++ marketplace/ui/src/components/MarketPlace/CategoryCard.js
@@ -15,6 +15,16 @@ const CategoryCard = () => {
   const naviroute = routes.MarketplaceCategoryProductList.url;
   const { categorys } = useCategoryState();
 
+  const categoryImages = [
+    Images["Carbon-category"],
+    Images["Metal"],
+    Images["Clothing-category"],
+    Images["collectibles"],
+    Images["tokens_card"],
+    Images["Art-category"],
+    Images["membership_card"],
+  ];
+
   function Arrow(props) {
     const char = props.type === "next" ? ">" : "<";
     let className = props.type === "next" ? "slick-next" : "slick-prev";
@@ -28,16 +38,6 @@ const CategoryCard = () => {
       </Button>
     );
   }
-  
-  const categoryImages = [
-    Images["Carbon-category"],
-    Images["Metal"],
-    Images["Clothing-category"],
-    Images["collectibles"],
-    Images["tokens_card"],
-    Images["Art-category"],
-    Images["membership_card"],
-  ];
 
   const settings = {
     nextArrow: <Arrow type="next" />,
@@ -46,32 +46,7 @@ const CategoryCard = () => {
     infinite: true,
     speed: 500,
     slidesToShow: 5,
-    slidesToScroll: 5,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+    slidesToScroll: 5
   };
   
   return (
@@ -82,52 +57,100 @@ const CategoryCard = () => {
       </Title>
       </Fade>
       <Fade direction="left" triggerOnce>
-      <Slider {...settings}>
-        {categorys.map((category, index) => {
-          return (
-            <div
-              id={category.name}
-              key={index}
-              className="transition-transform duration-500 hover:scale-105 w-[162px] md:w-[210px] 2xl:w-[248px] h-[160px] md:h-[180px] 2xl:h-[200px] border border-tertiaryB shadow-category rounded-lg cursor-pointer"
-              onClick={() => {
-                const subCat = category.subCategories.map((item)=>item.contract).join(",")
-                const url = `${naviroute.replace(':category', category.name)}?sc=${subCat}`
-                navigate(url)
-                sessionStorage.setItem('scrollPosition', 0);
-                window.LOQ.push(['ready', async LO => {
-                  // Track an event
-                  await LO.$internal.ready('events')
-                  LO.events.track(`Homepage Filter - ${category.name}`)
-                }])
-                TagManager.dataLayer({
-                  dataLayer: {
-                    event: `${category.name}_filter_homepage`,
-                  },
-                });
-              }}
-            >
-              <div className="flex flex-col">
-                <img
-                  alt={SEO.IMAGE_META}
-                  title={SEO.IMAGE_META}
-                  src={categoryImages[index]}
-                  className="rounded-t-lg px-[9px] py-[6px] lg:px-[0px] lg:py-[0px] h-[110px] md:h-[125px] 2xl:h-[140px]"
-                  preview={false}
-                />
+      <div className="mobile-hide"> {/* Hide this div on mobile/tablet */}
+        <Slider {...settings}>
+          {categorys.map((category, index) => {
+            return (
+              <div
+                id={category.name}
+                key={index}
+                className="transition-transform duration-500 hover:scale-105 w-[162px] md:w-[210px] 2xl:w-[248px] h-[160px] md:h-[180px] 2xl:h-[200px] border border-tertiaryB shadow-category rounded-lg cursor-pointer"
+                onClick={() => {
+                  const subCat = category.subCategories.map((item)=>item.contract).join(",")
+                  const url = `${naviroute.replace(':category', category.name)}?sc=${subCat}`
+                  navigate(url)
+                  sessionStorage.setItem('scrollPosition', 0);
+                  window.LOQ.push(['ready', async LO => {
+                    // Track an event
+                    await LO.$internal.ready('events')
+                    LO.events.track(`Homepage Filter - ${category.name}`)
+                  }])
+                  TagManager.dataLayer({
+                    dataLayer: {
+                      event: `${category.name}_filter_homepage`,
+                    },
+                  });
+                }}
+              >
+                <div className="flex flex-col">
+                  <img
+                    alt={SEO.IMAGE_META}
+                    title={SEO.IMAGE_META}
+                    src={categoryImages[index]}
+                    className="rounded-t-lg px-[9px] py-[6px] lg:px-[0px] lg:py-[0px] h-[110px] md:h-[125px] 2xl:h-[140px]"
+                    preview={false}
+                  />
 
-                  <div className="py-2 xl:py-3 flex justify-center md:justify-start ">
-                    <Text
-                      type="secondary"
-                      className="text-lg md:text-xl lg:text-2xl !text-primaryB font-semibold"
-                    >
-                      <span className="p-3 font-sans">{category.name}</span>
-                    </Text>
+                    <div className="py-2 xl:py-3 flex justify-center md:justify-start ">
+                      <Text
+                        type="secondary"
+                        className="text-lg md:text-xl lg:text-2xl !text-primaryB font-semibold"
+                      >
+                        <span className="p-3 font-sans">{category.name}</span>
+                      </Text>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
-        </Slider>
+              );
+            })}
+          </Slider>
+        </div>
+        <div className="desktop-hide flex justify-start sm:justify-center md:justify-start gap-3 lg:gap-[15px] flex-wrap px-0 md:px-10 xl:grid xl:grid-cols-6"> {/* Show this div on mobile/tablet */}
+          {categorys.map((category, index) => {
+            return (
+              <div
+                id={category.name}
+                key={index}
+                className="transition-transform duration-500 hover:scale-105 w-[162px] md:w-[210px] 2xl:w-[248px] h-[160px] md:h-[180px] 2xl:h-[200px] border border-tertiaryB shadow-category rounded-lg cursor-pointer"
+                onClick={() => {
+                  const subCat = category.subCategories.map((item)=>item.contract).join(",")
+                  const url = `${naviroute.replace(':category', category.name)}?sc=${subCat}`
+                  navigate(url)
+                  sessionStorage.setItem('scrollPosition', 0);
+                  window.LOQ.push(['ready', async LO => {
+                    // Track an event
+                    await LO.$internal.ready('events')
+                    LO.events.track(`Homepage Filter - ${category.name}`)
+                  }])
+                  TagManager.dataLayer({
+                    dataLayer: {
+                      event: `${category.name}_filter_homepage`,
+                    },
+                  });
+                }}
+              >
+                <div className="flex flex-col">
+                  <img
+                    alt={SEO.IMAGE_META}
+                    title={SEO.IMAGE_META}
+                    src={categoryImages[index]}
+                    className="rounded-t-lg px-[9px] py-[6px] lg:px-[0px] lg:py-[0px] h-[110px] md:h-[125px] 2xl:h-[140px]"
+                    preview={false}
+                  />
+
+                    <div className="py-2 xl:py-3 flex justify-center md:justify-start ">
+                      <Text
+                        type="secondary"
+                        className="text-lg md:text-xl lg:text-2xl !text-primaryB font-semibold"
+                      >
+                        <span className="p-3 font-sans">{category.name}</span>
+                      </Text>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
       </Fade>
     </>
   );
