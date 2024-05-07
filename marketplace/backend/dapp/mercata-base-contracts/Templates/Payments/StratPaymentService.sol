3d3f09662010baa75804d9d37d466c0c3777ef6a
Dustin Norwood <dustin@blockapps.net>
2024-05-07 12:06:23 -0400

    Updated payment service contracts to cache orders and return tokens

--- marketplace/backend/dapp/mercata-base-contracts/Templates/Payments/StratPaymentService.sol
+++ marketplace/backend/dapp/mercata-base-contracts/Templates/Payments/StratPaymentService.sol
@@ -5,23 +5,21 @@ import <BASE_CODE_COLLECTION>;
 
 contract StratPaymentService is PaymentService {
     address public stratAddress;
+    uint public stratsPerDollar;
 
     constructor (
         address _stratAddress,
+        uint _stratsPerDollar,
         string _imageURL
-    ) public {
+    ) PaymentService("STRAT", _imageURL, "Checkout with STRAT") public {
         stratAddress = _stratAddress;
-        serviceName = "STRAT";
-        serviceURL = "";
-        imageURL = _imageURL;
-        onboardingText = "";
-        checkoutText = "Checkout with " + serviceName;
+        stratsPerDollar = _stratsPerDollar;
     }
 
-    mapping (address => uint) stratQuantities;
-    function _lockSales (
+    function _createOrder (
         address[] _saleAddresses,
-        uint[] _quantities
+        uint[] _quantities,
+        string token
     ) internal override returns (uint) {
         require(_saleAddresses.length == _quantities.length, "Number of sale addresses does not match number of quantities given");
         address[] stratRecipients;
@@ -30,62 +28,38 @@ contract StratPaymentService is PaymentService {
             Sale s = Sale(_saleAddresses[i]);
             Asset a = s.assetToBeSold();
             address recipient = a.owner();
-            if (stratQuantities[recipient] == 0) {
+            if (quantitiesMap[recipient] == 0) {
                 stratRecipients.push(recipient);
             }
             uint quantity = _quantities[i];
-            uint amount = s.price() * quantity * 100; // 1 STRAT currently equals 1 cent
+            uint amount = s.price() * quantity * stratsPerDollar * 100; // 1 STRAT = 1 STRAT cents
             totalAmount += amount;
-            stratQuantities[recipient] += amount;
-            s.lockQuantity(quantity, tx.origin); // The STRAT contract uses tx.origin for transfers, so it would be
-            s.completeSale(tx.origin);           // a security hole for us to use msg.sender here
+            quantitiesMap[recipient] += amount;
+            s.lockQuantity(quantity, token); // The STRAT contract uses tx.origin for transfers, so it would be
+            s.completeSale(token);           // a security hole for us to use msg.sender here
         }
         string err = "Your STRAT account balance is not high enough to cover the purchase.";
         uint myBalance = stratAddress.call("balance");
         require(myBalance >= totalAmount, err);
         for (uint j = 0; j < stratRecipients.length; j++) {
             address recipient = stratRecipients[j];
-            bool success = stratAddress.call("transfer", recipient, stratQuantities[recipient]);
-            emit Payment(getCommonName(tx.origin), getCommonName(recipient), stratQuantities[recipient]);
-            stratQuantities[recipient] = 0;
+            bool success = stratAddress.call("transfer", recipient, quantitiesMap[recipient]);
+            emit Payment(getCommonName(tx.origin), getCommonName(recipient), quantitiesMap[recipient]);
+            quantitiesMap[recipient] = 0;
             require(success, err);
         }
 
         return RestStatus.OK;
     }
 
-    function _completeSales (
-        address[] _saleAddresses,
-        address _purchaser
-    ) internal override returns (uint) {
+    function _completeOrder (
+        string token
+    ) internal override returns (address[]) {
         require(false, "Cannot call completeSales for STRAT payments.");
     }
 
-    function _update(
-        string _serviceURL
-    ,   string _imageURL
-    ,   string _onboardingText
-    ,   string _checkoutText
-    ,   uint   _scheme
-    ) internal override returns (uint) {
-
-      if (_scheme == 0) {
-        return RestStatus.OK;
-      }
-
-      if ((_scheme & (1 << 0)) == (1 << 0)) {
-        return RestStatus.CONFLICT;
-      }
-      if ((_scheme & (1 << 2)) == (1 << 2)) {
-        return RestStatus.CONFLICT;
-      }
-      if ((_scheme & (1 << 3)) == (1 << 3)) {
-        return RestStatus.CONFLICT;
-      }
-      if ((_scheme & (1 << 1)) == (1 << 1)) {
-        imageURL = _imageURL;
-      }
-
+    function updateStratsPerDollar(uint _stratsPerDollar) requireOwner() public returns (uint) {
+      stratsPerDollar = _stratsPerDollar;
       return RestStatus.OK;
     }
 }
