f444f946036e261d1d7d81b2a4f634e601fbfa0f
Dustin Norwood <dustin@blockapps.net>
2024-05-08 11:51:53 -0400

    Improve payment failure messaging

--- marketplace/backend/dapp/dapp/dapp.js
+++ marketplace/backend/dapp/dapp/dapp.js
@@ -993,7 +993,7 @@ async function bind(rawAdmin, _contract, _defaultOptions, serviceUser = false) {
     } catch (error) {
       console.log(error);
       if (error.response) {
-        throw new rest.RestError(error.response.status, error.response.statusText);
+        throw new rest.RestError(error.response.status, error.response.data);
       }
       throw new rest.RestError(RestStatus.BAD_REQUEST, "Error while updating the order");
     }
