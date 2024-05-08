acd440e3b4e53fa72c6e17bc600371e2208c9831
Aya Abdelgawad <aya_abdelgawad@yahoo.com>
2024-05-08 18:59:16 -0400

    update UserRegistry contract

--- strato/core/strato-model/src/Blockchain/Strato/Model/UserRegistry.hs
+++ strato/core/strato-model/src/Blockchain/Strato/Model/UserRegistry.hs
@@ -15,18 +15,70 @@ pragma builtinCreates;
 
 import { Certificate, CertificateRegistry } from <509>;
 
+enum SellerStatus {
+    NULL,
+    UNAUTHORIZED,
+    PENDING_REVIEW,
+    AUTHORIZED
+}
+
 contract UserRegistry {
+    error userError (string message);
     function createUser(string _commonName) public returns (address) {
         User newUser = new User{salt: _commonName}(_commonName);
         return address(newUser);
+    }
+
+    function requestReview(string commonName) public {
+        try {
+            // derive the address of the user contract w/ the salt (commonName)
+            User theUser = User(address(this).derive(commonName, commonName));
+            address(theUser).call("requestReview");
+        } catch Require {
+            throw userError("You must be the owner of the wallet to call this");
+        } catch {
+            throw userError("No User contract associated with the common name " + commonName + " in this registry");
+        }
+    }
+
+    function authorizeSeller(string commonName) public {
+        try {
+            // derive the address of the user contract w/ the salt (commonName)
+            User theUser = User(address(this).derive(commonName, commonName));
+            address(theUser).call("authorizeSeller");
+        } catch Require {
+            throw userError("You must be an admin to call this");
+        } catch {
+            throw userError("No User contract associated with the common name " + commonName + " in this registry");
+        }
+    }
+
+    function deauthorizeSeller(string commonName) public {
+        try {
+            // derive the address of the user contract w/ the salt (commonName)
+            User theUser = User(address(this).derive(commonName, commonName));
+            address(theUser).call("deauthorizeSeller");
+        } catch Require {
+            throw userError("You must be an admin to call this");
+        } catch {
+            throw userError("No User contract associated with the common name " + commonName + " in this registry");
+        }
     }
 }
 
 contract User {
     string public commonName;
+    SellerStatus public sellerStatus;
 
     constructor(string _commonName) {
         commonName = _commonName;
+        sellerStatus = SellerStatus.UNAUTHORIZED;
+    }
+
+    modifier onlyAdmins() {
+        mapping(string => string) cert = getUserCert(msg.sender);
+        require(cert["organization"] == "BlockApps");
+        _;
     }
 
     modifier authenticated() {
@@ -55,5 +107,18 @@ contract User {
             return cert.commonName() == commonName;
         }
         return false;
+    }
+
+    function requestReview() public authenticated {
+        require(sellerStatus != SellerStatus.AUTHORIZED, "You are already an authorized seller");
+        sellerStatus = SellerStatus.PENDING_REVIEW;
+    }
+    
+    function authorizeSeller() public onlyAdmins {
+        sellerStatus = SellerStatus.AUTHORIZED;
+    }
+
+    function deauthorizeSeller() public onlyAdmins {
+        sellerStatus = SellerStatus.UNAUTHORIZED;
     }
 }|]
