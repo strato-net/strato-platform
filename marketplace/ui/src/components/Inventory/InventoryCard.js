0c7d7871be089382f9c0b21ada7a87dfbc34587f
Hadi Salameh <hsalameh123@gmail.com>
2024-05-23 21:32:26 -0400

    Removed events and associated files/code

--- marketplace/ui/src/components/Inventory/InventoryCard.js
+++ marketplace/ui/src/components/Inventory/InventoryCard.js
@@ -9,7 +9,6 @@ import {
   SwapOutlined
 } from "@ant-design/icons";
 import PreviewInventoryModal from "./PreviewInventoryModal";
-import AddEventModal from "./AddEventModal";
 import { useNavigate } from "react-router-dom";
 import ListForSaleModal from "./ListForSaleModal";
 import UnlistModal from "./UnlistModal";
@@ -25,9 +24,7 @@ import { SEO } from "../../helpers/seoConstant";
 const InventoryCard = ({ inventory, category, debouncedSearchTerm, id, paymentProviderAddress, allSubcategories, limit, offset }) => {
   const textRef = useRef(null);
   const [isOverflowing, setIsOverflowing] = useState(false);
-  const [openPop, setOpenPop] = useState(false);
   const [open, setOpen] = useState(false);
-  const [openEdit, setOpenEdit] = useState(false);
   const [listModalOpen, setListModalOpen] = useState(false);
   const [unlistModalOpen, setUnlistModalOpen] = useState(false);
   const [resellModalOpen, setResellModalOpen] = useState(false);
@@ -43,19 +40,7 @@ const InventoryCard = ({ inventory, category, debouncedSearchTerm, id, paymentPr
     setOpen(false);
   };
 
-  const handleCancelEdit = () => {
-    setOpenEdit(false);
-  };
-
-  const hide = () => {
-    setOpenPop(false);
-  };
-  const handleOpenChange = (newOpen) => {
-    setOpenPop(newOpen);
-  };
-
   const showListModal = () => {
-    hide();
     setListModalOpen(true);
   };
 
@@ -64,7 +49,6 @@ const InventoryCard = ({ inventory, category, debouncedSearchTerm, id, paymentPr
   };
 
   const showUnlistModal = () => {
-    hide();
     setUnlistModalOpen(true);
   };
 
@@ -73,7 +57,6 @@ const InventoryCard = ({ inventory, category, debouncedSearchTerm, id, paymentPr
   };
 
   const showResellModal = () => {
-    hide();
     setResellModalOpen(true);
   };
 
@@ -82,7 +65,6 @@ const InventoryCard = ({ inventory, category, debouncedSearchTerm, id, paymentPr
   };
 
   const showTransferModal = () => {
-    hide();
     setTransferModalOpen(true);
   };
 
@@ -91,7 +73,6 @@ const InventoryCard = ({ inventory, category, debouncedSearchTerm, id, paymentPr
   };
 
   const showRedeemModal = () => {
-    hide();
     setRedeemModalOpen(true);
   };
 
@@ -293,14 +274,6 @@ const InventoryCard = ({ inventory, category, debouncedSearchTerm, id, paymentPr
           handleCancel={handleCancel}
           inventory={inventory}
           category={category}
-        />
-      )}
-      {openEdit && (
-        <AddEventModal
-          open={openEdit}
-          handleCancel={handleCancelEdit}
-          inventoryId={inventory.address}
-          productId={inventory.productId}
         />
       )}
       {listModalOpen && (
