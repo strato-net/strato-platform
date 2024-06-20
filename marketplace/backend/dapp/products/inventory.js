c2817c9b44b8cbe17caa50ac3114d26579e13779
jxuan101 <jinhuai.xuan38@myhunter.cuny.edu>
2024-06-20 15:30:43 -0400

    Fix for error when itemSale is undefined

--- marketplace/backend/dapp/products/inventory.js
+++ marketplace/backend/dapp/products/inventory.js
@@ -431,7 +431,7 @@ async function getAll(admin, args = {}, defaultOptions) {
                         saleAddress: itemSale?.address,
                         saleQuantity: itemSale?.quantity,
                         saleDate: itemSale?.block_timestamp,
-                        paymentProviders: itemSale['BlockApps-Mercata-Sale-paymentProviders'] ? itemSale['BlockApps-Mercata-Sale-paymentProviders'] : null,
+                        paymentProviders: itemSale ? (itemSale['BlockApps-Mercata-Sale-paymentProviders'] ? itemSale['BlockApps-Mercata-Sale-paymentProviders'] : null) : null,
                         totalLockedQuantity: itemSale?.totalLockedQuantity
                     });
                 }
@@ -492,7 +492,7 @@ async function getAll(admin, args = {}, defaultOptions) {
                                 saleQuantity: sales[0]?.quantity,
                                 saleDate: sales[0]?.block_timestamp,
                                 totalLockedQuantity: sales[0]?.totalLockedQuantity,
-                                paymentProviders: sales[0]['BlockApps-Mercata-Sale-paymentProviders'] ? sales[0]['BlockApps-Mercata-Sale-paymentProviders'] : null,
+                                paymentProviders: sales[0] ? (sales[0]['BlockApps-Mercata-Sale-paymentProviders'] ? sales[0]['BlockApps-Mercata-Sale-paymentProviders'] : null) : null,
                                 'BlockApps-Mercata-Sale': undefined  // Removing the nested sale data to avoid redundancy
                             });
                         }
@@ -504,7 +504,7 @@ async function getAll(admin, args = {}, defaultOptions) {
                             saleQuantity: sales[0]?.quantity,
                             saleDate: sales[0]?.block_timestamp,
                             totalLockedQuantity: sales[0]?.totalLockedQuantity,
-                            paymentProviders: sales[0]['BlockApps-Mercata-Sale-paymentProviders'] ? sales[0]['BlockApps-Mercata-Sale-paymentProviders'] : null,
+                            paymentProviders: sales[0] ? (sales[0]['BlockApps-Mercata-Sale-paymentProviders'] ? sales[0]['BlockApps-Mercata-Sale-paymentProviders'] : null) : null,
                             'BlockApps-Mercata-Sale': undefined  // Removing the nested sale data to avoid redundancy
                         });
                     }
