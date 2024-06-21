ebc6332957582b9c62be4095df2159d09d71e7a8
Tanuj Soni <tanuj@blockapps.net>
2024-06-21 13:13:49 +0530

    revert liftIO changes

--- strato/highway/server/src/Strato/Server/PutS3File.hs
+++ strato/highway/server/src/Strato/Server/PutS3File.hs
@@ -53,7 +53,7 @@ putS3File multipartdata =
       --Set up a ResourceT region with an available HTTP manager.
       $logInfoS "highway/putS3File" $ T.pack $ "Setting up a ResourceT region with an available HTTP manager."
       st  <- askUnliftIO
-      runResourceT $ do
+      liftIO $ runResourceT $ do
         let content = toStrict $ fdPayload file
         let body = RequestBodyBS content
         --Create a request object with S3.getObject and run the request with pureAws.
