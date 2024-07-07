7bfebed19381ed19ace16fd990c8e83d664fe319
Jamshid <jamshidnh@gmail.com>
2024-07-07 00:22:31 +0000

    Adding ory-init, a tool to create config files, etc for ory

---
+++ ory/ory-init/app/Resources.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Resources (
+  dockerComposeTemplate,
+  hydraConfig,
+  kratosConfig,
+  defaultSchema
+  ) where
+
+import Data.FileEmbed
+import Data.Text (Text)
+import Data.Text.Encoding
+
+dockerComposeTemplate :: Text
+dockerComposeTemplate = decodeUtf8Lenient $(embedFile "resources/docker-compose.tmp.yml")
+
+hydraConfig :: Text
+hydraConfig = decodeUtf8Lenient $(embedFile "resources/hydra.yml")
+
+kratosConfig :: Text
+kratosConfig = decodeUtf8Lenient $(embedFile "resources/kratos.yml")
+
+defaultSchema :: Text
+defaultSchema = decodeUtf8Lenient $(embedFile "resources/default.schema.json")
