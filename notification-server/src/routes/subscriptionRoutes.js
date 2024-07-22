3d5ade17fae68ff4aa13e284acb5d3d83598d596
Aya Abdelgawad <aya_abdelgawad@yahoo.com>
2024-07-22 14:45:13 -0400

    extract email from auth header

--- notification-server/src/routes/subscriptionRoutes.js
+++ notification-server/src/routes/subscriptionRoutes.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const { isSubscribed, subscribe } = require('../services/subscriptionService');
 const router = express.Router();
+const { jwtDecode } = require('jwt-decode');
 
 // Save user email
 router.put('/subscribe', async (req, res) => {
   try {
     // email will be inside the authorization header
     // username will be inside the body
-    const email = req.headers.authorization;
+    const token = req.headers.authorization;
+    const decodedToken = jwtDecode(token);
     const { username } = req.body;
+    const email = decodedToken.email;
+
+    if (!token) {
+      return res.status(401).json({ error: 'Authorization token missing' });
+    }
 
     // respond with text/plain content-type
     res.type('txt');
