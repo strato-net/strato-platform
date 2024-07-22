5fedba52ed027c76cd13d32befe6866e43f58ae2
Aya Abdelgawad <aya_abdelgawad@yahoo.com>
2024-07-22 12:59:46 -0400

    notification monad

--- strato/identity-provider/server/src/IdentityProvider/Email.hs
+++ strato/identity-provider/server/src/IdentityProvider/Email.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 
@@ -8,12 +9,15 @@
 module IdentityProvider.Email where
 
 import BlockApps.Logging
+import Control.Monad.Change.Modify (access)
+import Control.Monad.Composable.Notification
 import Control.Monad.IO.Class
 import Data.Aeson
 import Data.ByteString
+import Data.Proxy
 import qualified Data.Text as T
 import GHC.Generics
-import Network.HTTP.Client
+import Network.HTTP.Client hiding (Proxy)
 import Network.HTTP.Client.TLS
 import Network.HTTP.Types.Header (hAuthorization, hContentType)
 import NotificationServer.API
@@ -70,12 +74,10 @@ sendWelcomeEmail email' name uuid key = do
   $logInfoS "sendWelcomeEmail" $ T.pack $ "Sendgrid response for welcome email was " <> show (responseStatus response)
   return ()
 
-subscribeUser :: (MonadIO m, MonadLogger m) => T.Text -> T.Text -> String -> m ()
-subscribeUser auth user url = do
-  -- this is wrong; make a HasNotificationServer monad
-  mgr <- liftIO $ newManager defaultManagerSettings -- this is wrong; do https
-  notificationServerUrl <- liftIO $ parseBaseUrl url
-  eResp <- liftIO $ runClientM (putSubscribe auth (Username user)) (mkClientEnv mgr notificationServerUrl)
+subscribeUser :: (MonadIO m, MonadLogger m, HasNotification m) => T.Text -> T.Text -> m ()
+subscribeUser auth user = do
+  NotificationData url mgr <- access Proxy
+  eResp <- liftIO $ runClientM (putSubscribe auth (Username user)) (mkClientEnv mgr url)
   case eResp of 
     Right _ -> $logInfoS "subscribeUser" $ "Successfully subscribed user " <> user
     Left err -> $logErrorS "subscribeUser" . T.pack $ "Error while trying to subscribe: " <> show err
--- strato/identity-provider/server/src/IdentityProvider/Server.hs
+++ strato/identity-provider/server/src/IdentityProvider/Server.hs
@@ -35,6 +35,7 @@ import Control.Monad (void, when)
 import qualified Control.Monad.Change.Alter as A
 import Control.Monad.Change.Modify
 import Control.Monad.Composable.Vault
+import Control.Monad.Composable.Notification
 import Control.Monad.Reader
 import Control.Monad.Trans.Except
 import Data.Aeson hiding (Success)
@@ -170,6 +171,9 @@ instance Monad m => Accessible VaultData (VaultM m) where
 instance (Monad m, Accessible VaultData m) => Accessible VaultData (ReaderT IdentityServerData m) where
   access = lift . access
 
+instance Monad m => Accessible NotificationData (NotificationM m) where
+  access _ = ask
+
 getPingIdentity :: (MonadIO m) => m Int
 getPingIdentity = return 1
 
@@ -232,7 +236,7 @@ putIdentity accessToken uuid idProv name mEmail mCo mSub = do
                   registerUserWalletAsync realmToken rd name' realm uuid' a
                   -- subscribe if can and should
                   case (realmNoficicationServerUrl rd, mSub) of 
-                    (Just url, Just True) -> subscribeUser accessToken (T.pack name') url
+                    (Just url, Just True) -> runNotificationM url $ subscribeUser accessToken (T.pack name')
                     (_, _) -> return ()
                 -- User has a cert but no wallet, create wallet using cert's common name. This is for backwards compatibility with existing users.
                 [cert] -> do
@@ -253,7 +257,7 @@ putIdentity accessToken uuid idProv name mEmail mCo mSub = do
               registerUserWalletAsync realmToken rd name' realm uuid' a
               -- subscribe if can and should
               _ <- case (realmNoficicationServerUrl rd, mSub) of 
-                (Just url, Just True) -> subscribeUser accessToken (T.pack name') url
+                (Just url, Just True) -> runNotificationM url $ subscribeUser accessToken (T.pack name')
                 (_, _) -> return ()
               return a
         (_, Nothing) -> do
