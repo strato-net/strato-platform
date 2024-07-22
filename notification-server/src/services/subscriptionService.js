c48cacb12eadf6255421811b800dc2162859a8a1
Ariya Amarjargal <amarj003@umn.edu>
2024-07-22 10:26:21 -0400

    Update subscriptionService to handle missing emails for usernames

--- notification-server/src/services/subscriptionService.js
+++ notification-server/src/services/subscriptionService.js
@@ -37,9 +37,12 @@ const subscribe = async (username, email) => {
 const getEmailsByUsernames = async (usernames) => {
   try {
     const result = await client.query(
-      "SELECT email FROM contact_info WHERE username = ANY($1)",
+      "SELECT email FROM contact_info WHERE username = ANY($1::text[])",
       [usernames]
     );
+    if (result.rows.length !== usernames.length) {
+      throw new Error("Emails not found for all/some usernames");
+    }
     return result.rows.map((row) => row.email);
   } catch (error) {
     console.error("Error retrieving emails by usernames:", error);
