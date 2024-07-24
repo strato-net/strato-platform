a12dacde7ce3cf228488427c3c395512c6c22a53
Aya Abdelgawad <aya_abdelgawad@yahoo.com>
2024-07-24 13:27:39 -0400

    get auth token in non-dockerized mode and custom header in dockerized mode

--- notification-server/src/routes/subscriptionRoutes.js
+++ notification-server/src/routes/subscriptionRoutes.js
@@ -8,7 +8,7 @@ router.put('/subscribe', async (req, res) => {
   try {
     // email will be inside the authorization header
     // username will be inside the body
-    const token = req.headers.authorization;
+    const token = req.headers.authorization ? req.headers.authorization : req.headers['x-user-access-token'];
     const decodedToken = jwtDecode(token);
     const { username } = req.body;
     const email = decodedToken.email;
