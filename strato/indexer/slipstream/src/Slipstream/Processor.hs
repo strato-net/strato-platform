5282e7ffe8dae57c7c78ebb4c8733b68b859af16
David Nallapu <davidsam1998@gmail.com>
2024-07-31 11:26:49 -0400

    seperated mapping and array flow and added fkey for array and type

--- strato/indexer/slipstream/src/Slipstream/Processor.hs
+++ strato/indexer/slipstream/src/Slipstream/Processor.hs
@@ -270,15 +270,27 @@ parseEvents = concatMap parseEvent
           eventEvent = e
         }
 
-getCollectionNamesFromContract :: ContractF () -> [Text]
-getCollectionNamesFromContract c =
+getMappingNamesFromContract :: ContractF () -> [Text]
+getMappingNamesFromContract c =
   let storageDefs' = c ^. storageDefs
       storageDefsList = Map.toList storageDefs'
-      listOfArrays = filter (\(_, vd) -> case (_varType vd) of SVMType.Array _ _-> True; _ -> False) storageDefsList
       listOfMappings = filter (\(_, vd) -> case (_varType vd) of SVMType.Mapping _ _ _-> True; _ -> False) storageDefsList
       listOfMappingsWithRecords = filter (\(_, vd) -> _isRecord vd) listOfMappings
-      listOfCollections = listOfArrays ++ listOfMappingsWithRecords
+      listOfCollections = listOfMappingsWithRecords
    in T.pack . fst <$> listOfCollections
+
+getArrayNamesFromContract :: ContractF () -> Map.Map T.Text T.Text
+getArrayNamesFromContract c =
+  let storageDefs' = c ^. storageDefs
+      storageDefsList = Map.toList storageDefs'
+      listOfArraysWithTypename = map (\(name, vd) -> case (_varType vd) of
+                                                       SVMType.Array entry _ -> Just (T.pack name, extractLabelOrEntry entry)
+                                                       _ -> Nothing) storageDefsList
+   in Map.fromList $ catMaybes listOfArraysWithTypename
+
+extractLabelOrEntry :: SVMType.Type -> T.Text
+extractLabelOrEntry (SVMType.UnknownLabel solidString _) = T.pack solidString
+extractLabelOrEntry entry = T.pack (show entry)
 
 getContractsFromPC :: E.ProcessedContract -> [Text]
 getContractsFromPC pc = Map.keys $ Map.filter isValueContract (E.contractData pc)
@@ -327,20 +339,26 @@ processTheMessages env conn messages = do
             -- Here we will get the storageDefs attribute of the contract (c) and iterate through the Map of (Text, VariableDecl) and look for VariableDecls that have the last attribute (isRecord) true and thetype are mappings
             -- We will then create a table for each of these collections and add a foreign key to the main table
 
-            let collectionNames = getCollectionNamesFromContract c              
+            let mappingNames = getMappingNamesFromContract c  
+                arrayNamesAndTypes = Map.toList $ getArrayNamesFromContract c
             let historyTableNames = map (historyTableName cr ap) hl
+            $logInfoS "processTheMessages/arrayNamesAndTypes" $ T.pack $ show arrayNamesAndTypes
             $logDebugS "processTheMessages/historyTableNames" $ T.pack $ show historyTableNames
 
             let nameParts@(cr', ap',  n'') = (cr, ap, T.pack $ _contractName c)
             $logInfoS "processTheMessages/Contract Added" $ "ccreator=" <> cr' <> ", app=" <> ap' <> ", name=" <> n''
             multilineLog "processTheMessages/fields" $ boringBox $ map (show) $ Map.toList $ fmap _varType $ c ^. storageDefs
 
-            --Create collection tables
-            deferredForeignKeysForCollections <- fmap concat $
-              forM collectionNames $ \m -> do
-                outputData conn $ createCollectionTable g nameParts m --Tables are created
-            -- mark
+            --Create mapping tables
+            deferredForeignKeysForMappings <- fmap concat $
+              forM mappingNames $ \m -> do
+                outputData conn $ createMappingTable g nameParts m
 
+            --Create array tables
+            deferredForeignKeysForArrays <- fmap concat $
+              forM arrayNamesAndTypes $ \anat -> do
+                outputData conn $ createArrayTable g nameParts anat
+            
             deferredForeignKeys <- case (_contractType c) of
               AbstractType -> do
                 abstractfkeys <- outputData conn $ createExpandAbstractTable g c nameParts abstracts' cc
@@ -354,11 +372,13 @@ processTheMessages env conn messages = do
                 return indexfkeys
 
             $logInfoS "processTheMessages/deferredForeignKeys" $ T.pack $ show deferredForeignKeys
+            $logInfoS "processTheMessages/deferredForeignKeysForMappings" $ T.pack $ show deferredForeignKeysForMappings
+            $logInfoS "processTheMessages/deferredForeignKeysForArrays" $ T.pack $ show deferredForeignKeysForArrays
 
 
             outputData conn $ createExpandEventTables g c cc nameParts
 
-            return $ deferredForeignKeys ++ deferredForeignKeysForCollections
+            return $ deferredForeignKeys ++ deferredForeignKeysForMappings ++ deferredForeignKeysForArrays
 
         -- forM_ deferredForeignKeys $ \deferredForeignKey -> do
         --   outputData conn $ createForeignIndexesForJoins deferredForeignKey
@@ -461,6 +481,7 @@ processTheMessages env conn messages = do
   forM_ insertsByCodeHash $ \ins -> do
     outputData conn $ updateForeignKeysFromNULLAbstract (abstractInserts ins) -- not historic
     outputData conn $ updateForeignKeysFromNULLIndex (indexInsert ins)
+    unless ((length (collectionInserts ins) < 1)) $ outputData conn $ updateForeignKeysFromNULLArray (collectionInserts ins)
 
   forM_ concatFkeys $ \deferredForeignKey -> do
     outputData conn $ createForeignIndexesForJoins deferredForeignKey
