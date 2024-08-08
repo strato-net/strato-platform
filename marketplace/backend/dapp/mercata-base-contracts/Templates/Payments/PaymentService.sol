638e881105e868145dd19583812d06c21ab984cd
Haile Thompson <hailethomp@gmail.com>
2024-08-08 15:30:01 -0400

    add: seller address to the Order event

--- marketplace/backend/dapp/mercata-base-contracts/Templates/Payments/PaymentService.sol
+++ marketplace/backend/dapp/mercata-base-contracts/Templates/Payments/PaymentService.sol
@@ -44,6 +44,7 @@ abstract contract PaymentService is Utils {
         address purchaser,            // Purchaser address on the blockchain for ownershipTransfer
         string purchasersCommonName,  // Purchaser common name for lookup purposes
         string sellersCommonName,     // Seller common name for lookup purposes
+        address sellerAddress,        // Seller address on the blockchain for ownershipTransfer. Asset Owner
         address[] saleAddresses,      // List of the sale contracts for the assets in the order
         uint[] quantities,            // List of quantities for each asset being bought
         decimal amount,               // Total price of the order
@@ -263,12 +264,14 @@ abstract contract PaymentService is Utils {
     ) internal virtual returns (address[]){
         decimal totalAmount = 0;
         address[] assets;
-        string seller;
+        string sellerCommonName;
+        address sellerAddress;
         for (uint i = 0; i < _saleAddresses.length; i++) {
             Sale s = Sale(_saleAddresses[i]);
             Asset a = s.assetToBeSold();
             assets.push(address(a));
-            seller = getCommonName(a.owner());
+            sellerCommonName = getCommonName(a.owner());
+            sellerAddress = a.owner();
             totalAmount += s.price() * decimal(_quantities[i]);
         }
         emit Order(
@@ -276,7 +279,8 @@ abstract contract PaymentService is Utils {
             _orderId,
             _purchaser,
             _purchasersCommonName,
-            seller,
+            sellerCommonName,
+            sellerAddress,
             _saleAddresses,
             _quantities,
             totalAmount,
@@ -331,13 +335,15 @@ abstract contract PaymentService is Utils {
     ) internal virtual returns (address[]) {
         decimal totalAmount = 0;
         address[] assets;
-        string seller;
+        string sellerCommonName;
+        address sellerAddress;
         decimal totalFee = 0.0;
         for (uint i = 0; i < _saleAddresses.length; i++) {
             Sale s = Sale(_saleAddresses[i]);
             Asset a = s.assetToBeSold();
             assets.push(address(a));
-            seller = getCommonName(a.owner());
+            sellerCommonName = getCommonName(a.owner());
+            sellerAddress = a.owner();
             decimal saleAmount = s.price() * _quantities[i];
             totalAmount += saleAmount;
             if (address(a) == address(a.root)) {
@@ -360,7 +366,8 @@ abstract contract PaymentService is Utils {
             _orderId,
             _purchaser,
             _purchasersCommonName,
-            seller,
+            sellerCommonName,
+            sellerAddress,
             _saleAddresses,
             _quantities,
             totalAmount,
@@ -462,14 +469,16 @@ abstract contract PaymentService is Utils {
         string _comments
     ) internal virtual {
         decimal totalAmount = 0;
-        string seller;
+        string sellerCommonName;
+        address sellerAddress;
         address[] assets;
         for (uint i = 0; i < _saleAddresses.length; i++) {
             Sale s = Sale(_saleAddresses[i]);
             totalAmount += s.price() * _quantities[i];
             Asset a = s.assetToBeSold();
             assets.push(address(a));
-            seller = getCommonName(a.owner());
+            sellerCommonName = getCommonName(a.owner());
+            sellerAddress = a.owner();
             try {
                 s.unlockQuantity(_orderHash, _purchaser);
             } catch { // Support for legacy sales
@@ -485,7 +494,8 @@ abstract contract PaymentService is Utils {
             _orderId,
             _purchaser,
             _purchasersCommonName,
-            seller,
+            sellerCommonName,
+            sellerAddress,
             _saleAddresses,
             _quantities,
             totalAmount,
