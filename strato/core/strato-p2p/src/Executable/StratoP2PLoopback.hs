632f71f92a8964f63a189917ae19ad561cb87f35
jxuan101 <jinhuai.xuan38@myhunter.cuny.edu>
2024-08-13 15:24:36 -0400

    Bring back wire cache in p2p

--- strato/core/strato-p2p/src/Executable/StratoP2PLoopback.hs
+++ strato/core/strato-p2p/src/Executable/StratoP2PLoopback.hs
@@ -8,12 +8,16 @@
 module Executable.StratoP2PLoopback (stratoP2PLoopback) where
 
 import BlockApps.Logging
+import Blockchain.Blockstanbul (WireMessage)
 import Blockchain.Context
 import Blockchain.Sequencer.Event
 import Blockchain.Sequencer.Kafka
+import Blockchain.Strato.Model.Keccak256
 import Conduit
+import qualified Control.Monad.Change.Alter as A
 import qualified Data.Text as T
 import Prometheus
+import Text.Format
 
 {-# NOINLINE loopbackEvents #-}
 loopbackEvents :: Vector T.Text Counter
@@ -31,7 +35,17 @@ stratoP2PLoopback runner = do
   $logInfoS "stratoP2PLoopback" "Reflecting PBFT back to unseq since 2019"
   runner $ \sSource -> do
     let toWireMessage = \case
-          P2pBlockstanbul wm -> pure $ Just wm
+          P2pBlockstanbul wm -> do
+            let msgHash = rlpHash wm
+            msgExists <- A.exists (A.Proxy @(A.Proxy (Inbound WireMessage))) msgHash
+            if msgExists
+              then do
+                $logInfoS "stratoP2PLoopback/P2pBlockstanbul" . T.pack $ "Already seen inbound wire message " ++ format msgHash ++ ". Not forwarding to Sequencer."
+                pure Nothing
+              else do
+                $logInfoS "stratoP2PLoopback/P2pBlockstanbul" . T.pack $ "First time seeing inbound wire message " ++ format msgHash ++ ". Forwarding to Sequencer."
+                A.insert (A.Proxy @(A.Proxy (Inbound WireMessage))) msgHash A.Proxy
+                pure $ Just wm
           _ -> pure Nothing
 
     runConduit $
