c923fcbb4f6d9f17216392a950f7364750f6f8ca
Neelkanth Poosa <neelkanthpoosa@BlockAppss-Air.lan>
2024-08-21 14:59:11 -0500

    typo rm

--- marketplace/backend/dapp/dapp/dapp.js
+++ marketplace/backend/dapp/dapp/dapp.js
@@ -1075,20 +1075,6 @@ async function bind(rawAdmin, _contract, _defaultOptions, serviceUser = false) {
     }
     );
 
-    const checkoutEvent = await rest.searchUntil(
-      rawAdmin,
-      { name: "BlockApps-Mercata-PaymentService.Checkout" },
-      (r) => r.length === 1,
-      {
-        ...options,
-        query: {
-          limit: 1,
-          ['checkoutHash']: `eq.${checkoutHash}`,
-        }
-    }
-    );
-
-    
     return orderEvent;
   }
   }
