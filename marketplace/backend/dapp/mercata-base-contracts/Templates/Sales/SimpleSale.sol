290d70a1b35dfe3bc51c91d4eb0a49057a2b2e2b
David Moncayo <david_moncayo@blockapps.net>
2024-08-30 14:47:57 -0400

    SimpleSale.sol: fixed argument type

--- marketplace/backend/dapp/mercata-base-contracts/Templates/Sales/SimpleSale.sol
+++ marketplace/backend/dapp/mercata-base-contracts/Templates/Sales/SimpleSale.sol
@@ -8,7 +8,7 @@ contract SimpleSale is Sale {
         address _assetToBeSold,
         decimal _price,
         uint _quantity,
-        address[] _paymentProviders
+        PaymentProvider[] _paymentProviders
     ) Sale(_assetToBeSold, _price, _quantity, _paymentProviders) {
     }
 }
