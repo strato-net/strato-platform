69b0a38aacf50d9ebb92b0caefe7dc3be4eca739
Jamshid <jamshidnh@gmail.com>
2024-09-17 10:59:34 -0400

    removing unused milena uses

--- strato/core/strato-statediff/src/Blockchain/Strato/StateDiff/Kafka.hs
+++ strato/core/strato-statediff/src/Blockchain/Strato/StateDiff/Kafka.hs
@@ -1,40 +1,15 @@
+
 module Blockchain.Strato.StateDiff.Kafka
-  ( stateDiffTopicName,
+  ( 
     assertStateDiffTopicCreation,
-    --    , writeActionJSONToKafka
-    filterResponse,
   )
 where
 
---import           Data.Aeson
---import qualified Data.ByteString.Lazy              as L
-
---import qualified Network.Kafka.Producer            as KW
-
---import           Blockchain.Strato.Model.Action    (Action)
 import Blockchain.KafkaTopics (lookupTopic)
-import Control.Lens.Getter (view)
 import Control.Monad.Composable.Kafka
-import qualified Network.Kafka.Protocol as KP
 
-stateDiffTopicName :: KP.TopicName
+stateDiffTopicName :: TopicName
 stateDiffTopicName = lookupTopic "statediff"
 
 assertStateDiffTopicCreation :: HasKafka k => k ()
 assertStateDiffTopicCreation = createTopic stateDiffTopicName
-
-{-
-mkTopicAndMessage :: (ToJSON a) => a -> K.TopicAndMessage
-mkTopicAndMessage = K.TopicAndMessage stateDiffTopicName . KW.makeMessage . L.toStrict . encode
-
-writeActionJSONToKafka :: (K.Kafka k) => [Action] -> k [KP.ProduceResponse]
-writeActionJSONToKafka = KW.produceMessages . map mkTopicAndMessage
--}
-filterResponse :: [KP.ProduceResponse] -> [(KP.Partition, KP.KafkaError, KP.Offset)]
-filterResponse = concatMap onlyErrors
-  where
-    onlyErrors :: KP.ProduceResponse -> [(KP.Partition, KP.KafkaError, KP.Offset)]
-    onlyErrors resp =
-      let errs = concatMap snd . view KP.produceResponseFields $ resp
-          isError (_, e, _) = e /= KP.NoError
-       in filter isError errs
