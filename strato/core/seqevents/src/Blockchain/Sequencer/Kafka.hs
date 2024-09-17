f1c008e4eb11f40c83fb08d2477c13b3d8ea84a7
Jamshid <jamshidnh@gmail.com>
2024-09-17 13:14:20 -0400

    removing unused stuff, some milena usage

--- strato/core/seqevents/src/Blockchain/Sequencer/Kafka.hs
+++ strato/core/seqevents/src/Blockchain/Sequencer/Kafka.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TypeOperators #-}
 
-{-# OPTIONS -fno-warn-unused-top-binds #-}
+-- {-# OPTIONS -fno-warn-unused-top-binds #-}
 
 module Blockchain.Sequencer.Kafka
   ( assertSequencerTopicsCreation,
@@ -9,8 +9,6 @@ module Blockchain.Sequencer.Kafka
     seqVmEventsTopicName,
     seqP2pEventsTopicName,
     readUnseqEvents,
-    readSeqVmEvents,
-    readSeqP2pEvents,
     writeUnseqEvents,
     writeSeqVmEvents,
     writeSeqP2pEvents,
@@ -30,7 +28,6 @@ import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
 import qualified Network.Kafka as K
 import qualified Network.Kafka.Producer as KW
-import qualified Network.Kafka.Protocol as KP
 
 unseqEventsTopicName :: TopicName
 unseqEventsTopicName = lookupTopic "unseqevents"
@@ -47,58 +44,37 @@ assertSequencerTopicsCreation = do
   createTopic seqVmEventsTopicName
   createTopic seqP2pEventsTopicName
 
-readUnseqEvents :: HasKafka k => KP.Offset -> k [IngestEvent]
+readUnseqEvents :: HasKafka k => Offset -> k [IngestEvent]
 readUnseqEvents off = do
   events <- readUnseqEventsFromTopic unseqEventsTopicName off
   return events
 
-readUnseqEventsFromTopic :: HasKafka k => TopicName -> KP.Offset -> k [IngestEvent]
+readUnseqEventsFromTopic :: HasKafka k => TopicName -> Offset -> k [IngestEvent]
 readUnseqEventsFromTopic = readFromTopic'
 {-# INLINE readUnseqEventsFromTopic #-}
 
-readSeqVmEvents :: HasKafka k => KP.Offset -> k [VmEvent]
-readSeqVmEvents off = do
-  events <- readSeqVmEventsFromTopic seqVmEventsTopicName off
-  recordEvents seqVMReads events
-  return events
-
-readSeqVmEventsFromTopic :: HasKafka k => TopicName -> KP.Offset -> k [VmEvent]
-readSeqVmEventsFromTopic = readFromTopic'
-{-# INLINE readSeqVmEventsFromTopic #-}
-
-readSeqP2pEvents :: HasKafka m => KP.Offset -> m [P2pEvent]
-readSeqP2pEvents off = do
-  events <- readSeqP2pEventsFromTopic seqP2pEventsTopicName off
-  recordEvents seqP2PReads events
-  return events
-
-readSeqP2pEventsFromTopic :: HasKafka m => TopicName -> KP.Offset -> m [P2pEvent]
-readSeqP2pEventsFromTopic = readFromTopic'
-{-# INLINE readSeqP2pEventsFromTopic #-}
-
-writeUnseqEvents :: HasKafka k => [IngestEvent] -> k [KP.ProduceResponse]
+writeUnseqEvents :: HasKafka k => [IngestEvent] -> k [ProduceResponse]
 writeUnseqEvents events = do
   produceItems unseqEventsTopicName events
 
-
-writeSeqVmEvents :: HasKafka k => [VmEvent] -> k [KP.ProduceResponse]
+writeSeqVmEvents :: HasKafka k => [VmEvent] -> k [ProduceResponse]
 writeSeqVmEvents events = do
   recordEvents seqVMWrites events
   execKafka $ KW.produceMessagesAsSingletonSets $
     (K.TopicAndMessage seqVmEventsTopicName . KW.makeMessage . BL.toStrict . encode) <$> events
 
-writeSeqP2pEvents :: HasKafka k => [P2pEvent] -> k [KP.ProduceResponse]
+writeSeqP2pEvents :: HasKafka k => [P2pEvent] -> k [ProduceResponse]
 writeSeqP2pEvents events = do
   recordEvents seqP2PWrites events
   execKafka $ KW.produceMessagesAsSingletonSets $
     (K.TopicAndMessage seqP2pEventsTopicName . KW.makeMessage . BL.toStrict . encode) <$> events
 
-writeUnseqEventsWithLimits :: K.Kafka k => [B.ByteString] -> k [KP.ProduceResponse]
+writeUnseqEventsWithLimits :: K.Kafka k => [B.ByteString] -> k [ProduceResponse]
 writeUnseqEventsWithLimits events = do
   KW.produceMessagesAsSingletonSets $
     (K.TopicAndMessage unseqEventsTopicName . KW.makeMessage) <$> events
 
-readFromTopic' :: (Binary b, HasKafka k) => TopicName -> KP.Offset -> k [b]
+readFromTopic' :: (Binary b, HasKafka k) => TopicName -> Offset -> k [b]
 readFromTopic' = fetchItems
 
 emitBlockstanbulMsg :: (m `Outputs` [IngestEvent]) => PBFT.WireMessage -> m ()
