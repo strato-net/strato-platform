1b276d800235e77e6eab70c098da411fb95d7a3d
Dustin Norwood <dnorwood2010@aol.com>
2024-09-19 12:49:51 -0400

    Update Metrics.hs

--- strato/tools/process-monitor/src/Data/Metrics.hs
+++ strato/tools/process-monitor/src/Data/Metrics.hs
@@ -35,3 +35,59 @@ heapSizeMetric =
     . vector "process"
     . gauge
     $ Info "strato_rts_heap_size" "STRATO RTS heap size"
+
+genMetric :: Vector Text Gauge
+genMetric =
+  unsafeRegister
+    . vector "process"
+    . gauge
+    $ Info "strato_rts_gcdetails_gen" "STRATO RTS GC generation"
+
+threadsMetric :: Vector Text Gauge
+threadsMetric =
+  unsafeRegister
+    . vector "process"
+    . gauge
+    $ Info "strato_rts_gcdetails_threads" "STRATO RTS GC threads"
+
+allocatedBytesMetric :: Vector Text Gauge
+allocatedBytesMetric =
+  unsafeRegister
+    . vector "process"
+    . gauge
+    $ Info "strato_rts_gcdetails_allocated_bytes" "STRATO RTS GC allocated bytes"
+
+largeObjectsBytesMetric :: Vector Text Gauge
+largeObjectsBytesMetric =
+  unsafeRegister
+    . vector "process"
+    . gauge
+    $ Info "strato_rts_gcdetails_large_bjects_bytes" "STRATO RTS GC large objects bytes"
+
+compactBytesMetric :: Vector Text Gauge
+compactBytesMetric =
+  unsafeRegister
+    . vector "process"
+    . gauge
+    $ Info "strato_rts_gcdetails_compact_bytes" "STRATO RTS GC compact bytes"
+
+slopBytesMetric :: Vector Text Gauge
+slopBytesMetric =
+  unsafeRegister
+    . vector "process"
+    . gauge
+    $ Info "strato_rts_gcdetails_slop_bytes" "STRATO RTS GC slop bytes"
+
+copiedBytesMetric :: Vector Text Gauge
+copiedBytesMetric =
+  unsafeRegister
+    . vector "process"
+    . gauge
+    $ Info "strato_rts_gcdetails_copied_bytes" "STRATO RTS GC copied bytes"
+
+blockFragmentationBytesMetric :: Vector Text Gauge
+blockFragmentationBytesMetric =
+  unsafeRegister
+    . vector "process"
+    . gauge
+    $ Info "strato_rts_gcdetails_block_fragmentation_bytes" "STRATO RTS GC block fragmentation bytes"
