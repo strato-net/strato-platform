279fe38dc3772111ebb1205e07ffdcbae126d124
David Nallapu <davidsam1998@gmail.com>
2024-10-07 13:40:33 -0400

    reverted

--- marketplace/backend/dapp/mercata-base-contracts/Templates/Payments/TokenPaymentService.sol
+++ marketplace/backend/dapp/mercata-base-contracts/Templates/Payments/TokenPaymentService.sol
@@ -1,7 +1,7 @@
 pragma es6;
 pragma strict;
 
-import <a3f917a3663eb66dbaa657dfa60c1250b310729c>;
+import <BASE_CODE_COLLECTION>;
 
 contract TokenPaymentService is PaymentService {   
     // TODO: receipts for minting/removing?
