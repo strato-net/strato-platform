eb36faf525cb457d05a933a3e32743e54aa0a481
Tanuj Soni <tanuj@blockapps.net>
2024-10-25 17:09:04 +0530

    Fix STRAT Text in price

--- marketplace/ui/src/components/MarketPlace/NewTrendingCard.js
+++ marketplace/ui/src/components/MarketPlace/NewTrendingCard.js
@@ -140,7 +140,7 @@ const NewTrendingCard = ({ topSellingProduct, addItemToCart, parent = "", api, c
 
                         return (
                             <Typography className="font-semibold">
-                                {`$${adjustedPrice} `} <span className="font-normal text-xs mr-2 text-primary"><b> {`(${(adjustedPrice * STRATS_CONVERSION).toFixed(0)} STRATs)`} </b></span>
+                                {`$${adjustedPrice} `} <span className="font-normal text-xs mr-2 text-primary"><b>{`(${(adjustedPrice * STRATS_CONVERSION).toFixed(0)} ${(adjustedPrice * STRATS_CONVERSION).toFixed(0) == 1 ? 'STRAT' : 'STRATs'})`}</b></span>
                             </Typography>
                         );
                         })()
