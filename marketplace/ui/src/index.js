eb5388c76b9b17ec055e02a73befdd1dfe19e24f
Tanuj Soni <tanuj@blockapps.net>
2024-10-30 19:30:00 +0530

    Undo StrictMode

--- marketplace/ui/src/index.js
+++ marketplace/ui/src/index.js
@@ -9,13 +9,13 @@ import { MarketplaceProvider } from "./contexts/marketplace";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <>
+  <React.StrictMode>
     <AuthenticationProvider>
       <MarketplaceProvider>
         <App />
       </MarketplaceProvider>
     </AuthenticationProvider>
-  </>,
+  </React.StrictMode>,
   // document.getElementById("root")
 );
 
