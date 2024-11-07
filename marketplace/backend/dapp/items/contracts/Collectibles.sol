6532bfa935106ece69f8376e88fb87994c4779de
Hadi Salameh <hsalameh123@gmail.com>
2024-11-07 12:29:58 -0500

    Added collectiblesType to Collectibles

--- marketplace/backend/dapp/items/contracts/Collectibles.sol
+++ marketplace/backend/dapp/items/contracts/Collectibles.sol
@@ -1,11 +1,12 @@
 pragma es6;
 pragma strict;
 
-import <BASE_CODE_COLLECTION>;
+import <4f53bb81d6c382bc9030d792c0e1d79ad7ce49bb>;
 
 /// @title A representation of Collectible assets
 contract Collectibles is Mintable {
     string public condition;
+    string public collectiblesType;
 
     event OwnershipUpdate(
         string seller,
@@ -23,10 +24,12 @@ contract Collectibles is Mintable {
         uint _createdDate,
         uint _quantity,
         string _condition,
+        string _collectiblesType,
         AssetStatus _status,
         address _redemptionService
     ) public Mintable(_name, _description, _images, _files, _fileNames, _createdDate, _quantity, _status, _redemptionService) {
         condition = _condition;
+        collectiblesType = _collectiblesType;
     }
 
     function mint(uint _quantity) internal override returns (UTXO) {
@@ -39,6 +42,7 @@ contract Collectibles is Mintable {
             createdDate,
             _quantity,
             condition,
+            collectiblesType,
             status,
             address(redemptionService)
         );
