98f5f7d1455e9b765fc700feb6d7fa8b97762319
David Nallapu <davidsam1998@gmail.com>
2024-11-11 14:27:50 -0500

    made some changes

--- marketplace/backend/dapp/mercata-base-contracts/Templates/Staking/Escrow.sol
+++ marketplace/backend/dapp/mercata-base-contracts/Templates/Staking/Escrow.sol
@@ -1,58 +1,70 @@
+pragma solidity ^0.8.0;
+
 import "../Assets/Asset.sol";
 import "../Sales/Sale.sol";
 import "../Utils/Utils.sol";
 
-// TODO: Need to do the unstaking flow
-// TODO: Security review
-// TODO: Add the Oracle?
-
-contract Escrow is Sale {//Comment
+contract Escrow is Sale {
     address public reserve;
     address public borrower;
-    uint public silverAmount;
+    uint public totalSilverAmount;  // Represents the total amount of staked assets
     uint public stratsLoanAmount;
     uint public cataReward;
     uint public cataWeeklyReward;
 
-
-    event LoanClaimed(address indexed borrower, uint loanAmount);
-    event CollateralReleased(address indexed borrower, uint silverAmount);
+    event CollateralReleased(address indexed borrower, uint totalSilverAmount);
 
     modifier onlyReserve() {
-        require(msg.sender == reserve, "Only Reserve can call this");
+        require(msg.sender == reserve, "Only the Reserve contract can call this");
         _;
     }
 
     constructor(
         address _borrower,
-        uint _silverAmount,
-        address _silverAsset,
+        uint _totalSilverAmount,
+        address[] memory _silverAssets,
         uint _stratsLoanAmount,
         uint _cataReward,
         address _reserve
-    ) Sale(_silverAsset, 0, _silverAmount, new PaymentService[] ) {
+    ) Sale(_silverAssets, 0, _totalSilverAmount, new PaymentService[]()) {
         borrower = _borrower;
-        silverAmount = _silverAmount;
+        totalSilverAmount = _totalSilverAmount;
         stratsLoanAmount = _stratsLoanAmount;
         cataReward = _cataReward;
-        cataWeeklyReward = uint(cataRewardRate / 52);
+        cataWeeklyReward = (cataReward * 10) / 52;  // Assuming the CATA reward rate is provided externally
         reserve = _reserve;
 
-        // Attach this Escrow contract as the sale of the Silver asset
-        Asset(_silverAsset).attachSale();
+        // Attach this Escrow contract as the sale for each asset in the assetsToBeSold array
+        for (uint i = 0; i < _silverAssets.length; i++) {
+            Asset(_silverAssets[i]).attachSale();
+        }
     }
 
     function getAssetAmount() public view returns (uint) {
-        return silverAmount;
+        return totalSilverAmount;
+    }
+
+    function getLoanAmount() public view returns (uint) {
+        return stratsLoanAmount;
     }
 
     function releaseCollateral(address requester) external onlyReserve {
-        Asset(assetToBeSold).transferOwnership(requester, silverAmount, false, 0, 0);
-        emit CollateralReleased(requester, silverAmount);
+        for (uint i = 0; i < assetsToBeSold.length; i++) {
+            uint assetQuantity = assetsToBeSold[i].quantity();
+            Asset(assetsToBeSold[i]).transferOwnership(requester, assetQuantity, false, 0, 0);
+        }
+        emit CollateralReleased(requester, totalSilverAmount);
     }
 
     function attachEscrowToAsset(address asset) public onlyReserve {
         // Attach the Escrow contract to the specified asset
         Asset(asset).attachSale();
+    }
+
+    function detachEscrowFromAssets() external onlyReserve {
+        // Detach this Escrow contract from all assets upon loan repayment
+        for (uint i = 0; i < assetsToBeSold.length; i++) {
+            Asset(assetsToBeSold[i]).closeSale();
+        }
     }
 }
--- marketplace/backend/dapp/mercata-base-contracts/Templates/Staking/Reserve.sol
+++ marketplace/backend/dapp/mercata-base-contracts/Templates/Staking/Reserve.sol
@@ -3,13 +3,13 @@ pragma solidity ^0.8.0;
 import "../Assets/Asset.sol";
 import "../Items/STRATS.sol";
 import "./Escrow.sol";
-// import "./Oracle.sol";
 import "../Utils/Utils.sol";
 
 contract Reserve is Utils {
-    OracleService public oracle;// Using asset oracle service here
+    OracleService public oracle; // Asset Oracle service for fetching price data
     STRATSTokens public stratsToken;
     address public cataToken;
+    address public owner; // Owner (BlockApps) as source of STRATS tokens
 
     uint public loanToValueRatio = 50; // LTV ratio as percentage
     uint public cataAPYRate = 10; // 10% APY for CATA rewards
@@ -18,28 +18,29 @@ contract Reserve is Utils {
     event StakeCreated(address indexed user, address escrow, uint assetAmount, uint stratsLoan, uint cataReward);
     event UnstakeProcessed(address indexed user, address escrow, uint assetAmount, uint repayment);
 
-    constructor(address _assetOracle, address _stratsToken, address _cataToken) {
-        assetOracle = Oracle(_assetOracle);
+    constructor(address _assetOracle, address _stratsToken, address _cataToken, address _owner) {
+        oracle = OracleService(_assetOracle);
         stratsToken = STRATSTokens(_stratsToken);
         cataToken = _cataToken;
+        owner = _owner;
     }
 
-    function createEscrow(uint assetAmount, address assetAsset) public returns (address) {
-        require(assetEscrows[assetAsset] == address(0), "Escrow already exists for this asset");
+    function createEscrow(uint assetAmount, address assetAddress) public returns (address) {
+        require(assetEscrows[assetAddress] == address(0), "Escrow already exists for this asset");
 
         uint assetPrice = oracle.getLatestPrice();
         uint stratsLoanAmount = (assetAmount * assetPrice * loanToValueRatio) / 100;
         uint cataReward = calculateCATAReward(assetAmount);
 
-        // Transfer STRATS to the borrower
-        stratsToken.purchaseTransfer(msg.sender, stratsLoanAmount, 0, 0);
+        // Transfer STRATS from owner (BlockApps) to the borrower
+        stratsToken.transfer(owner, stratsLoanAmount);
 
         // Create new Escrow contract
-        Escrow escrow = new Escrow(msg.sender, assetAmount, assetAsset, stratsLoanAmount, cataReward, address(this));
-        assetEscrows[assetAsset] = address(escrow);
+        Escrow escrow = new Escrow(msg.sender, assetAmount, assetAddress, stratsLoanAmount, cataReward, address(this));
+        assetEscrows[assetAddress] = address(escrow);
 
-        // Attach the escrow to the Asset and STRATS assets
-        escrow.attachEscrowToAsset(assetAsset);
+        // Attach the escrow to both the Asset and STRATS assets
+        escrow.attachEscrowToAsset(assetAddress);
         escrow.attachEscrowToAsset(address(stratsToken));
 
         emit StakeCreated(msg.sender, address(escrow), assetAmount, stratsLoanAmount, cataReward);
@@ -55,8 +56,16 @@ contract Reserve is Utils {
 
     function processUnstake(uint repayment, address escrow) external {
         require(assetEscrows[escrow] != address(0), "Unauthorized Unstake request");
-        Escrow(escrow).releaseCollateral(msg.sender);
         
-        emit UnstakeProcessed(msg.sender, escrow, Escrow(escrow).getAssetAmount(), repayment);
+        Escrow escrowContract = Escrow(escrow);
+        require(escrowContract.getLoanAmount() == repayment, "Repayment amount mismatch");
+
+        // Transfer STRATS back to owner
+        stratsToken.transfer(owner, repayment);
+
+        // Release the collateral back to the borrower
+        escrowContract.releaseCollateral(msg.sender);
+
+        emit UnstakeProcessed(msg.sender, escrow, escrowContract.getAssetAmount(), repayment);
     }
 }
