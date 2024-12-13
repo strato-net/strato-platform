47873e58f2111c1e39088786eea23d2e83116074
David Nallapu <davidsam1998@gmail.com>
2024-12-13 14:40:15 -0500

    rest of the code

--- eth-bridge/src/contracts/EthBridge.sol
+++ eth-bridge/src/contracts/EthBridge.sol
@@ -2,19 +2,19 @@
 import "../marketplace/backend/dapp/items/contracts/Tokens.sol";
 
 contract MercataETHBridge is Tokens{
-    address private _owner;
-    bool private _paused;
-    mapping(address => uint256) private _reentrancyStatus;
-    uint256 private constant _NOT_ENTERED = 1;
-    uint256 private constant _ENTERED = 2;
+    address  _owner;
+    bool  _paused;
+    mapping(address => uint256)  _reentrancyStatus;
+    uint256 public constant _NOT_ENTERED = 1;
+    uint256 public constant _ENTERED = 2;
     Tokens public ethSt;
     string[] public transferHashes;
-    mapping(string => bool) private hashExists;
+    mapping(string => bool) public hashExists;
 
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
     event Paused(address account);
     event Unpaused(address account);
-    event ETHBridgeHashAdded(string hash);
+    event ETHBridgeHashAdded(string commonName, string txhash, string amount);
     event MintedETHST(address user, string username, uint256 amount);
 
     modifier onlyOwner() {
@@ -37,6 +37,7 @@ contract MercataETHBridge is Tokens{
     constructor() {
         _owner = msg.sender;
         _paused = false;
+        createEthSt();
     }
 
     function owner() public view returns (address) {
@@ -63,29 +64,24 @@ contract MercataETHBridge is Tokens{
         ethSt = new Tokens("ETHST", "ETHST", [], [] , [] , block.timestamp, 1, AssetStatus.ACTIVE, address(0));
     }
 
-    mapping(address => uint256) public balances;
-
-    function mintETHST(address userAddress, uint256 amount, string memory txHash) public onlyOwner nonReentrant {
+    function mintETHST(address userAddress, uint256 amount, string txHash) public onlyOwner nonReentrant {
         require(amount > 0, "Must mint some ETHST");
         require(!hashExists[txHash], "Hash already exists");
-        
-        hashExists[txHash] = true;
-        transferHashes.push(txHash);
-        balances[userAddress] += amount;
+
         Mintable(ethSt).mintNewUnits(amount);
         Asset(UTXO(Redeemable(Mintable(ethSt)))).automaticTransfer(userAddress, 0.01, amount, block.number);
         emit MintedETHST(userAddress, "User", amount);
     }
 
-    function addHash(string memory hash) public onlyOwner returns (bool) {
-        require(!hashExists[hash], "Hash already exists");
-        hashExists[hash] = true;
-        transferHashes.push(hash);
-        emit ETHBridgeHashAdded(hash);
+    function addHash(string commonName, string txHash, string amount) external returns (bool) {
+        require(!hashExists[txHash], "Hash already exists");
+        hashExists[txHash] = true;
+        emit ETHBridgeHashAdded(commonName, txHash, amount);
         return true;
     }
 
-    function checkHashExists(string memory hash) public view returns (bool) {
+    function checkHashExists(string hash) public view returns (bool) {
         return hashExists[hash];
     }
 }
+
