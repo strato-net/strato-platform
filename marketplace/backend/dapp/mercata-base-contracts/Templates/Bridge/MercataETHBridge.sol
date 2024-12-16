7df965814c3e207cbc25f120de1262691dc7b616
Dustin Norwood <dustin@blockapps.net>
2024-12-16 16:23:28 -0500

    Fixed type errors in burnETHST function

--- marketplace/backend/dapp/mercata-base-contracts/Templates/Bridge/MercataETHBridge.sol
+++ marketplace/backend/dapp/mercata-base-contracts/Templates/Bridge/MercataETHBridge.sol
@@ -1,4 +1,8 @@
-abstract contract MercataETHBridge{
+pragma solidvm 11.5;
+
+import "../Utils/Utils.sol";
+
+abstract contract MercataETHBridge is Utils {
     enum AssetStatus {
         NULL,
         ACTIVE,
@@ -8,7 +12,7 @@ abstract contract MercataETHBridge{
     }
 
     address public owner;
-    address public burnerAddress = 0x6ec8bbe4a5b87be18d443408df43a45e5972fa1b; // burner account
+    address public burnerAddress = address(0x6ec8bbe4a5b87be18d443408df43a45e5972fa1b); // burner account
     bool public isActive = true;
 
     address public ethSt;
@@ -62,19 +66,19 @@ abstract contract MercataETHBridge{
         string _baseAddress
     ) requireActive() external returns (uint) {
         require(_ethstAddresses.length > 0, "Pass at least one ETHST token address");
-        Escrow escrow = Escrow(_escrowAddress);
         uint ethstAmountOwed = _quantity;
         uint ethstAmountNet = ethstAmountOwed;
         uint ethstQuantity = 0;
         uint transferNumber = 0;
 
         for (uint j = 0; j < _ethstAddresses.length; j++) {
-            Asset ethstAsset = Asset(_ethstAddresses[j]);
+            address ethstAddress = _ethstAddresses[j];
+            Asset ethstAsset = Asset(ethstAddress);
             require(ethstAsset.root == ethSt.root, "Asset is not an ETHST asset");
             require(ethstAsset.ownerCommonName() == getCommonName(msg.sender), "Purchaser doesn't own this ETHST asset");
 
             ethstQuantity = ethstAsset.quantity();
-            transferNumber = (uint(string(_escrowAddress), 16) + j + block.timestamp) % 1000000;
+            transferNumber = (uint(string(ethstAddress), 16) + j + block.timestamp) % 1000000;
 
             ethstAsset.attachSale();
             if (ethstQuantity > ethstAmountNet) {
@@ -93,6 +97,6 @@ abstract contract MercataETHBridge{
         // require(ethstAmountNet == 0, "Your ethstS balance is not high enough to cover the repayment."); // Allow partial repayments
 
         uint ethstAmountRepaid = ethstAmountOwed - ethstAmountNet;
-        emit BurnedETHST(_userAddress, getCommonName(_userAddress), _baseAddress, ethstAmountRepaid);
+        emit BurnedETHST(msg.sender, getCommonName(msg.sender), _baseAddress, ethstAmountRepaid);
     }
 }
