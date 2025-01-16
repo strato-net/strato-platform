04a0617e12e306659057e44637d18284728dcce0
aariya50 <amarj003@umn.edu>
2025-01-16 09:56:35 -0500

    refactor: remove unnecessary logging in transferItem function

--- marketplace/backend/dapp/products/inventory.js
+++ marketplace/backend/dapp/products/inventory.js
@@ -263,7 +263,6 @@ async function requestRedemption(user, contract, args, options) {
 async function transferItem(user, argsArray, options) {
   try {
     // Prepare transfer call arguments
-    console.log('transferItem - argsArray', argsArray);
     const callArgsArray = argsArray.map((args) => ({
       contract: args.contract,
       method: 'automaticTransfer',
