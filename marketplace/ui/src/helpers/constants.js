c4171330bea0b658676de33bf0e4f6e63e21df38
Hadi Salameh <hsalameh123@gmail.com>
2025-02-19 15:25:09 -0500

    Made banner more responsive

--- marketplace/ui/src/helpers/constants.js
+++ marketplace/ui/src/helpers/constants.js
@@ -342,8 +342,8 @@ export const BANNER = [
     text: (
       <div className="staking_banner_text banner-text flex justify-between">
         <div className="banner-text">
-          <h1 className="title"> RWA + DeFi = Unbanked, On-Chain Living </h1>
-          <h1 className="w-3/4 mt-1 md:text-3xl font-normal sm:text-2xl text-sm">
+          <h1 className="title text-base md:text-4xl"> RWA + DeFi = Unbanked, On-Chain Living </h1>
+          <h1 className="w-3/4 mt-1 lg:text-2xl md:text-xl font-normal sm:text-2xl text-sm">
             Stake Silver, Gold, ETH, Wrapped Bitcoin, and many more stakeable
             assets to earn CATA, our Governance token!
           </h1>
@@ -353,7 +353,7 @@ export const BANNER = [
             src={Images.cata}
             alt={'Cata...'}
             title={'Cata'}
-            className="md:w-[160px] md:h-[160px] w-[120px] h-[120px] z-[11] relative md:mr-80 mr-48"
+            className="md:w-[160px] md:h-[160px] w-[90px] h-[90px] z-[11] relative md:mr-80 mr-32"
           />
         </div>
       </div>
