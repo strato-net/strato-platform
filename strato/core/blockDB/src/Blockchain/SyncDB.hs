d2ec4fca881ccc09bcab65a4ce546dbc718bbe3f
Jamshid <jamshidnh@gmail.com>
2025-03-11 11:59:29 -0400

    Adding a new SQL table to hold p2p sync tasks, that will allow different p2p threads to coordinate which blocks each peer thread should download.  This commit adds the table, but it isn't used by p2p yet.

--- strato/core/blockDB/src/Blockchain/SyncDB.hs
+++ strato/core/blockDB/src/Blockchain/SyncDB.hs
@@ -1,9 +1,13 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Blockchain.SyncDB
-  ( getBestBlockInfo,
+  ( HasSyncDB(..),
+    getBestBlockInfo,
     putBestBlockInfo,
     getBestSequencedBlockInfo,
     putBestSequencedBlockInfo,
@@ -18,16 +22,26 @@ where
 
 import BlockApps.Logging
 import Blockchain.BlockDB
+import Blockchain.DB.SQLDB
 import Blockchain.Model.SyncState
+import Blockchain.Model.SyncTask
 import Blockchain.Strato.Model.Keccak256
 import Blockchain.Strato.RedisBlockDB.Models as Models
 import Control.Concurrent (threadDelay)
 import Control.Monad
+import Control.Monad.Composable.SQL
 import Control.Monad.Trans
 import qualified Data.ByteString.Char8 as S8
+import Data.Text (Text)
 import qualified Data.Text as T
-import Database.Redis
+import Data.Time
+import Database.Esqueleto.Legacy
+import qualified Database.Persist.Sql as SQL
+import Database.Redis (Redis, RedisCtx)
+import qualified Database.Redis as REDIS
 import System.Random (randomIO)
+import Text.Format
+import qualified Text.Colors as CL
 
 liftLog :: LoggingT m a -> m a
 liftLog = runLoggingT
@@ -66,12 +80,12 @@ bestSequencedBlockInfoKey = S8.pack "<best_sequenced>"
 putBestBlockInfo ::
   Keccak256 ->
   Integer ->
-  Redis (Either Reply Status)
+  Redis (Either REDIS.Reply REDIS.Status)
 putBestBlockInfo newSha newNumber = do
   --liftIO . putStrLn . ("New args" ++) $ show (keccak256ToHex newSha, newNumber, newTDiff)
   oldBBI' <- getBestBlockInfo
   case oldBBI' of
-    Nothing -> return (Left $ SingleLine "Got no block from getBetstBlockInfo")
+    Nothing -> return (Left $ REDIS.SingleLine "Got no block from getBetstBlockInfo")
     Just (BestBlock oldSha oldNumber) -> do
       --liftIO . putStrLn . ("Old args" ++) $ show (keccak256ToHex oldSha, oldNumber, oldTDiff)
       helper' <- commonAncestorHelper oldNumber newNumber oldSha newSha
@@ -80,44 +94,44 @@ putBestBlockInfo newSha newNumber = do
         Right (updates, deletions) -> do
           --liftIO . putStrLn $ "Updates: \n" ++ unlines ((\(x, y) -> show (keccak256ToHex x, y)) <$> updates)
           --liftIO . putStrLn $ "Deletions: \n" ++ show deletions
-          res <- multiExec $ do
-            forM_ updates $ \(sha, num) -> set (inNamespace Canonical $ num) (toValue sha)
-            unless (null deletions) . void . del $ inNamespace Canonical . toKey <$> deletions
+          res <- REDIS.multiExec $ do
+            forM_ updates $ \(sha, num) -> REDIS.set (inNamespace Canonical $ num) (toValue sha)
+            unless (null deletions) . void . REDIS.del $ inNamespace Canonical . toKey <$> deletions
             forceBestBlockInfo newSha newNumber
           checkAndUpdateSyncStatus
           case res of
-            TxSuccess _ -> return $ Right Ok
-            TxAborted -> return . Left $ SingleLine (S8.pack "Aborted")
-            TxError e -> return . Left $ SingleLine (S8.pack e)
+            REDIS.TxSuccess _ -> return $ Right REDIS.Ok
+            REDIS.TxAborted -> return . Left $ REDIS.SingleLine (S8.pack "Aborted")
+            REDIS.TxError e -> return . Left $ REDIS.SingleLine (S8.pack e)
 
 -- | Used to seed the first bestBlock, e.g. genesis block in strato-setup
-forceBestBlockInfo :: RedisCtx m f => Keccak256 -> Integer -> m (f Status)
+forceBestBlockInfo :: RedisCtx m f => Keccak256 -> Integer -> m (f REDIS.Status)
 forceBestBlockInfo sha i =
   forceBestBlockInfo' bestBlockInfoKey (BestBlock sha i) --`totalRecall` (,,)
 
-forceBestBlockInfo' :: RedisCtx m f => S8.ByteString -> BestBlock -> m (f Status)
-forceBestBlockInfo' key = set key . toValue
+forceBestBlockInfo' :: RedisCtx m f => S8.ByteString -> BestBlock -> m (f REDIS.Status)
+forceBestBlockInfo' key = REDIS.set key . toValue
 
 getBestBlockInfo :: Redis (Maybe BestBlock)
 getBestBlockInfo = getBestBlockInfo' bestBlockInfoKey
 
 getBestSequencedBlockInfo :: Redis (Maybe BestSequencedBlock)
 getBestSequencedBlockInfo =
-  get bestSequencedBlockInfoKey >>= \case
+  REDIS.get bestSequencedBlockInfoKey >>= \case
     Left e -> error $ "error trying to get BestSequencedBlock: " ++ show e
     Right r ->  return $ fmap fromValue r
 
-putBestSequencedBlockInfo :: RedisCtx m f => BestSequencedBlock -> m (f Status)
-putBestSequencedBlockInfo = set bestSequencedBlockInfoKey . toValue
+putBestSequencedBlockInfo :: RedisCtx m f => BestSequencedBlock -> m (f REDIS.Status)
+putBestSequencedBlockInfo = REDIS.set bestSequencedBlockInfoKey . toValue
 
 getBestBlockInfo' :: S8.ByteString -> Redis (Maybe BestBlock)
 getBestBlockInfo' key =
-  get key >>= \case
+  REDIS.get key >>= \case
     Left x -> do
       liftLog $ $logErrorS "getBestBlockInfo'" . T.pack $ "got Left " ++ show x
       return Nothing
     Right r -> case r of
-      Nothing -> return Nothing -- return . Left $ SingleLine "No BestBlock data set in RedisBlockDB"
+      Nothing -> return Nothing -- return . Left $ REDIS.SingleLine "No BestBlock data set in RedisBlockDB"
       Just bs -> return . Just $ BestBlock sha num
         where
           BestBlock sha num = fromValue bs
@@ -142,27 +156,27 @@ defaultRedlockTTL = 3000
 defaultRedlockBackoff :: Int -- in microseconds
 defaultRedlockBackoff = 100 {- ms -} * 1000 {- us/ms -}
 
-redisSetNXPX :: (RedisCtx m f) => S8.ByteString -> S8.ByteString -> Int -> m (f Status)
-redisSetNXPX key value lockTTL = sendRequest ["SET", key, value, "NX", "PX", S8.pack (show lockTTL)]
+redisSetNXPX :: (RedisCtx m f) => S8.ByteString -> S8.ByteString -> Int -> m (f REDIS.Status)
+redisSetNXPX key value lockTTL = REDIS.sendRequest ["SET", key, value, "NX", "PX", S8.pack (show lockTTL)]
 
-acquireRedlock :: S8.ByteString -> Int -> Redis (Either Reply S8.ByteString)
+acquireRedlock :: S8.ByteString -> Int -> Redis (Either REDIS.Reply S8.ByteString)
 acquireRedlock key lockTTL = do
   random <- S8.pack . (show :: Integer -> String) <$> liftIO randomIO
   reply <- redisSetNXPX key random lockTTL
   return $ case reply of
-    Right Ok -> Right random
-    Right (Status "") -> Left $ SingleLine "could not acquire the lock due to NX condition unmet"
-    Right (Status s) -> Left . SingleLine $ "Somehow got a nonempty status, which makes no fucking sense: " `S8.append` s
-    Right Pong -> Left $ SingleLine "Somehow got a \"PONG\", which makes no fucking sense."
+    Right REDIS.Ok -> Right random
+    Right (REDIS.Status "") -> Left $ REDIS.SingleLine "could not acquire the lock due to NX condition unmet"
+    Right (REDIS.Status s) -> Left . REDIS.SingleLine $ "Somehow got a nonempty status, which makes no fucking sense: " `S8.append` s
+    Right REDIS.Pong -> Left $ REDIS.SingleLine "Somehow got a \"PONG\", which makes no fucking sense."
     Left err -> Left err
 
-releaseRedlock :: S8.ByteString -> S8.ByteString -> Redis (Either Reply Bool)
-releaseRedlock key lock = eval releaseRedlockScript [key] [lock]
+releaseRedlock :: S8.ByteString -> S8.ByteString -> Redis (Either REDIS.Reply Bool)
+releaseRedlock key lock = REDIS.eval releaseRedlockScript [key] [lock]
 
-acquireWorldBestBlockRedlock :: Int -> Redis (Either Reply S8.ByteString)
+acquireWorldBestBlockRedlock :: Int -> Redis (Either REDIS.Reply S8.ByteString)
 acquireWorldBestBlockRedlock = acquireRedlock worldBestBlockRedlockKey
 
-releaseWorldBestBlockRedlock :: S8.ByteString -> Redis (Either Reply Bool)
+releaseWorldBestBlockRedlock :: S8.ByteString -> Redis (Either REDIS.Reply Bool)
 releaseWorldBestBlockRedlock = releaseRedlock worldBestBlockRedlockKey
 
 worldBestBlockKey :: S8.ByteString
@@ -172,10 +186,10 @@ worldBestBlockKey = "<worldbest>"
 getWorldBestBlockInfo :: Redis (Maybe BestBlock)
 getWorldBestBlockInfo = getBestBlockInfo' worldBestBlockKey
 
-updateWorldBestBlockInfo :: Keccak256 -> Integer -> Redis (Either Reply Bool)
+updateWorldBestBlockInfo :: Keccak256 -> Integer -> Redis (Either REDIS.Reply Bool)
 updateWorldBestBlockInfo sha num = withRetryCount 0
   where
-    withRetryCount :: Int -> Redis (Either Reply Bool)
+    withRetryCount :: Int -> Redis (Either REDIS.Reply Bool)
     withRetryCount theRetryCount = do
       maybeLockID <- acquireWorldBestBlockRedlock defaultRedlockTTL
       case maybeLockID of
@@ -208,7 +222,7 @@ updateWorldBestBlockInfo sha num = withRetryCount 0
           didRelease <- releaseWorldBestBlockRedlock lockID
           return $ case didRelease of
             Right True -> Right didUpdate
-            Right False -> Left $ SingleLine "Couldn't release redlock, it either expired or we had the wrong key"
+            Right False -> Left $ REDIS.SingleLine "Couldn't release redlock, it either expired or we had the wrong key"
             err -> err
 
 -- Put this after any "best block" or "world best block" update.
@@ -249,12 +263,76 @@ syncStatusKey = "<sync_status>"
 {-# INLINE syncStatusKey #-}
 
 getSyncStatus :: Redis (Maybe Bool)
-getSyncStatus = fmap fromValue . eitherToMaybe <$> get syncStatusKey
+getSyncStatus = fmap fromValue . eitherToMaybe <$> REDIS.get syncStatusKey
   where
     eitherToMaybe :: Either a (Maybe b) -> Maybe b
     eitherToMaybe (Left _) = Nothing
     eitherToMaybe (Right a) = a
 
-putSyncStatus :: RedisCtx m f => Bool -> m (f Status)
-putSyncStatus status = set syncStatusKey $ toValue status
+putSyncStatus :: RedisCtx m f => Bool -> m (f REDIS.Status)
+putSyncStatus status = REDIS.set syncStatusKey $ toValue status
 
+class HasSyncDB m where
+  getCurrentSyncTask :: Text -> m SyncTask
+  getNewSyncTask :: Text -> m SyncTask
+  setSyncTaskFinished :: Text -> m ()
+  setSyncTaskNotReady :: Text -> m ()
+
+instance HasSQL m => HasSyncDB m where
+  getCurrentSyncTask host = sqlQuery $ do
+    vals <-
+        select $ from $ \syncTask -> do
+          where_ (
+            (syncTask^.SyncTaskHost ==. val host)
+            &&.
+            (syncTask ^. SyncTaskStatus ==. val Assigned)
+            )
+          return syncTask
+
+    -- This function shouldn't be called unless we have a current task assigned....
+    -- A connected peer could still trigger it even if none assigned (ie- by sending an unrequested BlockBody message),
+    -- however, this is bad behavior and it is good and right that we throw an erro and hang up on that peer
+    -- (hence, "error" is the correct response below).
+    case vals of
+      [v] -> return $ SQL.entityVal v
+      [] -> error $ "no current sync task found in call to getCurrentSyncTask: " ++ show host
+      _ -> error $ CL.red $ "multiple sync tasks found in call to getCurrentSyncTask:\n" ++ unlines (map (format . entityVal) vals)
+
+  getNewSyncTask host = sqlQuery $ do
+    now <- liftIO getCurrentTime
+    let oneMinuteAgo = addUTCTime (-60) now
+
+    vals <-
+      select $ from $ \syncTask -> do
+          where_ (
+            (syncTask^.SyncTaskAssignmentTime <=. val oneMinuteAgo)
+            &&.
+            ((syncTask^.SyncTaskStatus) !=. (val Finished))
+            )
+          return syncTask
+
+    case vals of
+      oneTask:_ -> return $ entityVal oneTask
+      [] -> do
+        --No existing task, make a new one
+        results <- SQL.rawSql "INSERT INTO sync_task (host) VALUES (?) RETURNING " [SQL.PersistText host]
+
+        case results of
+          [v] -> return $ SQL.entityVal v
+          _ -> error "insert failed in getSyncTask"
+
+  setSyncTaskFinished host = sqlQuery $ do
+    update $ \syncTask -> do
+      set syncTask [SyncTaskStatus =. val Finished]
+      where_ (syncTask^.SyncTaskHost ==. val host)
+    return ()
+
+  setSyncTaskNotReady host = sqlQuery $ do
+    update $ \syncTask -> do
+      set syncTask [SyncTaskStatus =. val NotReady]
+      where_ (
+        (syncTask^.SyncTaskHost ==. val host)
+        &&.
+        (syncTask^.SyncTaskStatus ==. val Assigned)
+        )
+    return ()
