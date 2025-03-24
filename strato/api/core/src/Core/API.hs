f0bcdb60a7992096e4ce38f0d008d9a19bb94912
Dustin Norwood <dustin@blockapps.net>
2025-03-24 14:26:54 -0400

    Added Core/API.hs

---
+++ strato/api/core/src/Core/API.hs
@@ -0,0 +1,123 @@
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MonoLocalBinds #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeOperators #-}
+
+{-# OPTIONS -fno-warn-orphans #-}
+
+module Core.API
+  ( CoreAPI
+  , MonadCoreAPI
+  , coreApiServer
+  , module Handlers.AccountInfo
+  , module Handlers.BlkLast
+  , module Handlers.Block
+  , module Handlers.Faucet
+  , module Handlers.Metadata
+  , module Handlers.Stats
+  , module Handlers.Storage
+  , module Handlers.Transaction
+  , module Handlers.TransactionResult
+  , module Handlers.TxLast
+  ) where
+
+import           BlockApps.Logging
+import           Blockchain.Data.Block
+import           Blockchain.Data.DataDefs
+import           Blockchain.Strato.Discovery.Data.Peer (ActivePeers)
+import           Blockchain.Strato.Model.Address
+import           Blockchain.Strato.Model.Keccak256
+import           Blockchain.Strato.Model.Options
+import           Control.Monad.Change.Alter
+import           Control.Monad.Change.Modify       (Accessible)
+import           Control.Monad.Composable.Identity
+import           Control.Monad.Composable.Vault    hiding (httpManager)
+import           Data.Source.Map
+import           Handlers.AccountInfo              hiding (API, server)
+import qualified Handlers.AccountInfo              as Account
+import           Handlers.BlkLast                  hiding (API, server)
+import qualified Handlers.BlkLast                  as BlkLast
+import           Handlers.Block                    hiding (API, server)
+import qualified Handlers.Block                    as Block
+import           Handlers.Faucet                   hiding (API, server)
+import qualified Handlers.Faucet                   as Faucet
+import qualified Handlers.IdentityServerCallback   as Identity
+import           Handlers.Metadata                 hiding (API, server)
+import qualified Handlers.Metadata                 as Metadata
+import qualified Handlers.Peers                    as Peers
+import qualified Handlers.QueuedTransactions       as QueuedTransactions
+import           Handlers.Stats                    hiding (API, server)
+import qualified Handlers.Stats                    as Stats
+import           Handlers.Storage                  hiding (API, server)
+import qualified Handlers.Storage                  as Storage
+import           Handlers.Transaction              hiding (API, server)
+import qualified Handlers.Transaction              as Transaction
+import           Handlers.TransactionResult        hiding (API, server)
+import qualified Handlers.TransactionResult        as TransactionResult
+import           Handlers.TxLast                   hiding (API, server)
+import qualified Handlers.TxLast                   as TxLast
+import           Servant
+import           UnliftIO
+
+type CoreAPI =
+  "eth" :> "v1.2"
+    :> ( Account.API
+           :<|> Account.CodeAPI
+           :<|> BlkLast.API
+           :<|> Block.API
+           :<|> Faucet.API
+           :<|> Identity.API
+           :<|> Metadata.API
+           :<|> Peers.API
+           :<|> QueuedTransactions.API
+           :<|> Stats.API
+           :<|> Storage.API
+           :<|> Transaction.API
+           :<|> TransactionResult.API
+           :<|> TxLast.API
+       )
+
+type MonadCoreAPI m =
+  ( MonadUnliftIO m,
+    MonadLogger m,
+    Accessible ActivePeers m,
+    Accessible Metadata.UrlMap m,
+    Accessible IdentityData m,
+    Accessible [RawTransaction] m,
+    Accessible Stats.TransactionCount m,
+    Accessible VaultData m,
+    BlkLast.GetLastBlocks m,
+    TxLast.GetLastTransactions m,
+    Selectable Account.AccountsFilterParams [AddressStateRef] m,
+    Selectable Address Integer m,
+    Selectable Block.BlocksFilterParams [Block] m,
+    Selectable Keccak256 SourceMap m,
+    Selectable Keccak256 [TransactionResult] m,
+    Selectable Storage.StorageFilterParams [Storage.StorageAddress] m,
+    Selectable Transaction.TxsFilterParams [RawTransaction] m
+  )
+
+coreApiServer :: MonadCoreAPI m => ServerT CoreAPI m
+coreApiServer =
+  Account.server
+    :<|> Account.codeServer
+    :<|> BlkLast.server
+    :<|> Block.server
+    :<|> Faucet.server
+    :<|> Identity.server
+    :<|> Metadata.server
+    :<|> Peers.server
+    :<|> QueuedTransactions.server
+    :<|> Stats.server
+    :<|> Storage.server
+    :<|> Transaction.server flags_txSizeLimit
+    :<|> TransactionResult.server
+    :<|> TxLast.server
