2bd4697fca61c0a246d8cc5fb41ef426b0b2478b
nikitamendelbaum <nikitamendelbaum@users.noreply.github.com>
2025-03-28 20:00:16 +0700

    support the numeric-only passwords in the Vault post password endpoint

--- strato/vault/api/src/Strato/Strato23/API/Password.hs
+++ strato/vault/api/src/Strato/Strato23/API/Password.hs
@@ -8,14 +8,25 @@ module Strato.Strato23.API.Password where
 
 import Data.Text
 import Servant.API
+import Data.Aeson
+import qualified Data.Text as T
+import GHC.Generics
 
 --------------------------------------------------------------------------------
 -- Routes and Types
 --------------------------------------------------------------------------------
 
+-- Define a newtype for password that can handle both string and numeric inputs
+newtype Password = Password { unPassword :: Text }
+
+instance FromJSON Password where
+  parseJSON (String s) = return $ Password s
+  parseJSON (Number n) = return $ Password $ T.pack $ show n
+  parseJSON v = fail $ "Expected String or Number for password, but got: " ++ show v
+
 type PostPassword =
   "password"
-    :> ReqBody '[JSON] Text
+    :> ReqBody '[JSON] Password
     :> Post '[JSON] ()
 
 type VerifyPassword =
