9bd6951e41d925f616da50dad9ece75f188a3194
Tanuj Soni <tanuj@blockapps.net>
2025-04-16 15:31:13 +0530

    fix test case

--- smd-ui/src/tests/Contracts/contracts.saga.spec.js
+++ smd-ui/src/tests/Contracts/contracts.saga.spec.js
@@ -38,21 +38,42 @@ describe('Contracts: saga', () => {
       expect(gen.next(contracts).value).toEqual(put(fetchContractsSuccess(contracts)));
       expect(gen.throw(error).value).toEqual(put(fetchContractsFailure(error)));
       expect(gen.next().done).toBe(true);
+
+      const gen2 = fetchContracts({ type: "FETCH_CONTRACTS", chainId: data.chainId, limit: data.limit, offset: data.offset , name: '001222'});
+      expect(gen2.next().value).toEqual(call(getContracts, data.chainId, data.limit, data.offset, '001222'));
+      expect(gen2.next(contracts).value).toEqual(put(fetchContractsSuccess(contracts)));
+      expect(gen2.throw(error).value).toEqual(put(fetchContractsFailure(error)));
+      expect(gen2.next().done).toBe(true);
     })
 
     describe('fetch contracts', () => {
+
       test('success', (done) => {
         fetch.mockResponse(JSON.stringify(contracts));
-        expectSaga(fetchContracts, data.chainId)
-          .call.fn(getContracts).put.like({ action: { type: FETCH_CONTRACTS_SUCCESSFUL } })
-          .run().then((result) => { done() });
+        expectSaga(fetchContracts, {
+          type: "FETCH_CONTRACTS",
+          chainId: data.chainId,
+          limit: data.limit,
+          offset: data.offset,
+          name: '00123'
+        })
+          .call.fn(getContracts)
+          .put.like({ action: { type: FETCH_CONTRACTS_SUCCESSFUL } })
+          .run().then(() => done());
       });
-
+      
       test('failure', (done) => {
         fetch.mockReject(JSON.stringify(contracts));
-        expectSaga(fetchContracts, data.chainId)
-          .call.fn(getContracts).put.like({ action: { type: FETCH_CONTRACTS_FAILED } })
-          .run().then((result) => { done() });
+        expectSaga(fetchContracts, {
+          type: "FETCH_CONTRACTS",
+          chainId: data.chainId,
+          limit: data.limit,
+          offset: data.offset,
+          name: data.name
+        })
+          .call.fn(getContracts)
+          .put.like({ action: { type: FETCH_CONTRACTS_FAILED } })
+          .run().then(() => done());
       });
 
       test('exception', () => {
