1d98b515f5947f0c83b063e042882f2892926b69
aariya50 <amarj003@umn.edu>
2025-04-21 16:31:42 -0400

    Refactor app configuration to conditionally load environment variables based on NODE_ENV

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,14 +2,15 @@ import express from "express";
 import cors from "cors";
 import routes from "./api/routes";
 
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 app.use(cors(), express.json());
 
 app.use("/api", routes);
 
-app.listen(3000, () => {
-    console.log(`Server running at http://localhost:${3000}`);
-  });
-  
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
 
 export default app;
--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,9 +1,10 @@
-require('dotenv').config();
+// Load local .env files when not in production
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 export const clientSecret = process.env.CLIENT_SECRET;
 export const clientId = process.env.CLIENT_ID;
 export const openIdDiscoveryUrl = process.env.OPENID_DISCOVERY_URL;
 export const nodeUrl = process.env.NODE_URL;
 export const serverHost = process.env.SERVER_HOST;
-
-
