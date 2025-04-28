1f56a0e3c8c1e2466651ee2be9d9e430a9c66716
witmk <maya.konaka@gmail.com>
2025-04-28 11:28:34 -0500

    more bytes32 changes

--- contracts/contracts/v1/concrete/Lending/PoolConfigurator.sol
+++ contracts/contracts/v1/concrete/Lending/PoolConfigurator.sol
@@ -5,8 +5,7 @@ pragma strict;
 import <BASE_CODE_COLLECTION>;
 
 contract PoolConfigurator is PoolConfiguratorBase {
-    constructor(address _lendingPool) {
-        require(_lendingPool != address(0), "Invalid LendingPool address");
-        lendingPool = LendingPoolBase(_lendingPool);
-     }
+
+     constructor(address _lendingPool) PoolConfiguratorBase(_lendingPool) {
+    }
 }
