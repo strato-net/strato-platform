e4a33329b0245a83f996468d35a6306ec6e97a3b
Maya <33349981+witmk@users.noreply.github.com>
2025-05-23 13:48:39 -0500

    remove unused contracts and reorganize folder structure (#114)
    * remove unused contracts and reorganize folder structure
    
    * fix: update contractsDir path to use relative directory
    
    ---------
    
    Co-authored-by: aariya50 <amarj003@umn.edu>

---
+++ contracts/concrete/BaseCodeCollection.sol
@@ -0,0 +1,62 @@
+//ERC20
+import "../abstract/ERC20/ERC20.sol";
+//import "ERC20/extensions/ERC20Burnable.sol";
+
+//Generic token
+import "Tokens/Token.sol";
+import "Tokens/TokenFaucet.sol";
+//import "Tokens/Metadata/TokenMetadata.sol";
+//import "Tokens/TokenAccess.sol";
+
+//Swap
+import "Pools/Pool.sol";
+import "Pools/PoolFactory.sol";
+
+//OnRamp
+import "OnRamp/OnRamp.sol";
+
+//Redemption
+//import "Redemptions/RedemptionService.sol";
+//import "Redemptions/CryptoRedemptionService.sol"; incomplete
+//import "Redemptions/PhysicalRedemptionService.sol"; doesn't compile
+
+//Lending
+import "Lending/CollateralVault.sol";
+import "Lending/LendingPool.sol";
+import "LendingRegistry.sol";
+import "Lending/LiquidityPool.sol";
+import "Lending/PoolConfigurator.sol";
+import "Lending/PriceOracle.sol";
+import "Lending/RateStrategy.sol";
+
+//Bridging
+import "Bridge/MercataEthBridge.sol";
+
+//TODO
+contract Mercata {
+    RateStrategy public rateStrategy;
+    PriceOracle public priceOracle;
+    CollateralVault public collateralVault;
+    LiquidityPool public liquidityPool;
+    LendingPool public lendingPool;
+    PoolConfigurator public poolConfigurator;
+    LendingRegistry public lendingRegistry;
+    MercataEthBridge public mercataEthBridge;
+    OnRamp public onRamp;
+    PoolFactory public poolFactory;
+
+    constructor() public {
+        rateStrategy = RateStrategy(new RateStrategy());
+        priceOracle = PriceOracle(new PriceOracle());
+        collateralVault = CollateralVault(new CollateralVault());
+        liquidityPool = LiquidityPool(new LiquidityPool());
+        lendingPool = LendingPool(new LendingPool(address(liquidityPool), address(collateralVault), address(rateStrategy), address(priceOracle)));
+        poolConfigurator = PoolConfigurator(new PoolConfigurator(address(lendingPool)));
+        lendingRegistry = LendingRegistry(new LendingRegistry(address(lendingPool), address(liquidityPool), address(collateralVault), address(rateStrategy)));
+        collateralVault.setLendingPool(address(lendingPool));
+        liquidityPool.setLendingPool(address(lendingPool));
+        mercataEthBridge = MercataEthBridge(new MercataEthBridge(address(msg.sender)));
+        onRamp = OnRamp(new OnRamp(address(priceOracle), msg.sender));
+        poolFactory = PoolFactory(new PoolFactory());
+    }
+}
