085ca8f76c18c2126acf18eb489497903507ad13
Tanuj Soni <tanuj@blockapps.net>
2025-06-06 18:15:48 +0530

    example env

---
+++ src/components/dashboard/Untitled-1.tsx
@@ -0,0 +1,10 @@
+
+// make an API call to backend as proxy and then bridge API to save the transaction hash, to, token, amount
+// get userAddress from accesstoken in the bridging service -> sub field
+// decode token then call ? /strato/v2.3/key?username=578bb2aa-ff6f-4588-80d7-f58378f1f918
+
+
+          // make an API call to backend as proxy and then bridge API to save the transaction hash, to, token, amount
+          // get userAddress from accesstoken in the bridging service -> sub field
+          // decode token then call ? /strato/v2.3/key?username=578bb2aa-ff6f-4588-80d7-f58378f1f918
+          
