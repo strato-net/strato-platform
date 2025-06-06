d81a90d72706a0f8850f1be09705e5464a9f71bc
Your Name <you@example.com>
2025-06-06 19:38:29 +0800

    update decider contract with flex solution

--- strato/core/strato-genesis/src/Blockchain/GenesisBlocks/Contracts/Decide.hs
+++ strato/core/strato-genesis/src/Blockchain/GenesisBlocks/Contracts/Decide.hs
@@ -1,9 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
 
-module Blockchain.GenesisBlocks.Contracts.Decide (
-  insertDecideContract
-  ) where
+module Blockchain.GenesisBlocks.Contracts.Decide
+  ( insertDecideContract,
+  )
+where
 
 import Blockchain.Data.GenesisInfo
 import Blockchain.Strato.Model.CodePtr
@@ -16,8 +17,8 @@ import Text.RawString.QQ
 insertDecideContract :: GenesisInfo -> GenesisInfo
 insertDecideContract gi =
   gi
-    { genesisInfoAccountInfo = genesisInfoAccountInfo gi ++ [decideAcct],
-      genesisInfoCodeInfo = genesisInfoCodeInfo gi ++ [CodeInfo dec1deContract (Just "Decider")]
+    { genesisInfoAccountInfo = genesisInfoAccountInfo gi ++ [decideAcct] ++ [decideStateAcct],
+      genesisInfoCodeInfo = genesisInfoCodeInfo gi ++ [CodeInfo dec1deContract (Just "Decider")] ++ [CodeInfo dec1deStateContract (Just "DeciderState")]
     }
   where
     decideAcct =
@@ -26,25 +27,82 @@ insertDecideContract gi =
         0
         (SolidVMCode "Decider" (KECCAK256.hash $ encodeUtf8 dec1deContract))
         []
+    decideStateAcct =
+      SolidVMContractWithStorage
+        0xDEC1DEFF
+        0
+        (SolidVMCode "DeciderState" (KECCAK256.hash $ encodeUtf8 dec1deStateContract))
+        []
 
 dec1deContract :: Text
 dec1deContract =
+  [r|
+interface GetImplContract {
+    function getImplContract() public view returns (address);
+}
+
+contract record Decider {
+    GetImplContract deciderStateContract = GetImplContract(address(0x937efa7e3a77e20bbdbd7c0d32b6514f368c1010));
+    string functionName = "PayFees";
+    constructor() {
+    }
+
+    function decide() returns (bool) {
+        
+        address payFeesImplContract = deciderStateContract.getImplContract();
+        payFeesImplContract.delegatecall(functionName);
+      
+        return true;
+    }
+}|]
+
+dec1deStateContract :: Text
+dec1deStateContract =
   [r|
 abstract contract ERC20_Template {
   function transfer(address _to, uint _amount) public;
 }
 
-contract record Decider {
-    constructor() {
+interface PayFees {
+    function payFees() external;
+}
+
+interface GetImplContract {
+    function getImplContract() public view returns (address);
+}
+
+contract record DeciderState is PayFees, GetImplContract {
+    address public owner;
+    address public currentFeeContract = address(this);
+
+    constructor(address _owner) {
+        owner = _owner;
     }
 
-    function decide() returns (bool) {
-      uint oneDollar = 1e18;
-      address USDST = address(0x937efa7e3a77e20bbdbd7c0d32b6514f368c1010);
-      address validatorPool = address(0x1234);
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
 
-      ERC20_Template(USDST).transfer(validatorPool, oneDollar / 10); //Each transaction costs 10¢
-      
-      return true;
+    function upateOwner(address _newOwner) external onlyOwner {
+        require(_newOwner != address(0), "Cannot set owner to zero address");
+        require(_newOwner != owner, "Should set new owner as different from current owner");
+        owner = _newOwner;
+    }
+
+    function getImplContract() public view override returns (address) {
+        return currentFeeContract;
+    }
+
+    function updatePayFeeContract(PayFees _newFeeContract) external onlyOwner {
+        require(address(_newFeeContract) != address(0), "Cannot set contract address to zero address");
+        currentFeeContract = address(_newFeeContract);
+    }
+
+    function payFees() override external {
+        uint oneDollar = 1e18;
+        address USDST = address(0x937efa7e3a77e20bbdbd7c0d32b6514f368c1010);
+        address validatorPool = address(0x1234);
+        ERC20_Template(USDST).transfer(validatorPool, oneDollar / 10);
     }
 }|]
