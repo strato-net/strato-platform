d6f983d3bdece13cd72a95172ddaed9c51a4c6fd
aariya50 <amarj003@umn.edu>
2025-06-13 14:38:09 -0400

    refactor: update TokenStatus enum in Token contract
    - Changed TokenStatus enum values from INACTIVE, ACTIVE, PENDING to NULL, PENDING, LEGACY, ACTIVE for improved clarity and management of token states.

--- contracts/concrete/Tokens/Token.sol
+++ contracts/concrete/Tokens/Token.sol
@@ -3,7 +3,7 @@ import "./TokenMetadata.sol";
 import "./TokenAccess.sol";
 import "../../abstract/ERC20.sol";
 
-enum TokenStatus { INACTIVE, ACTIVE, PENDING }
+enum TokenStatus { NULL, PENDING, LEGACY, ACTIVE }
 
 contract record Token is ERC20, Ownable, TokenMetadata, TokenAccess {
     uint8 public customDecimals;
