d1d83d374522625c7adc6c49ef5802b4ecc90aac
divyanshjain9 <divyansh.b@rejolut.com>
2025-07-03 13:40:30 +0530

    fix: handling of asset list to show earning and non earning tokens

--- mercata/ui/src/components/dashboard/AssetsList.tsx
+++ mercata/ui/src/components/dashboard/AssetsList.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { ArrowUpRight, Plus } from "lucide-react";
+import { useState } from "react";
+import { ArrowDown, ArrowUp, ArrowUpRight, Plus } from "lucide-react";
 import { Button } from "../ui/button";
 import DepositModal from "./DepositModal";
 import DepositOptionsModal from "./DepositOptionsModal";
@@ -9,12 +9,14 @@ import { formatUnits } from "ethers";
 interface AssetsProps {
   loading: boolean;
   tokens: Token[];
-  hideAddDepositsButton?: boolean;
+  isDashboard?: boolean;
+  inActiveTokens: Token[];
 }
 
-const AssetsList = ({ loading, tokens, hideAddDepositsButton }: AssetsProps) => {
+const AssetsList = ({ loading, tokens, inActiveTokens, isDashboard = true }: AssetsProps) => {
   const [isOptionsModalOpen, setIsOptionsModalOpen] = useState(false);
   const [isDepositModalOpen, setIsDepositModalOpen] = useState(false);
+  const [showTable, setShowTable] = useState(false);
 
   const handleOptionSelect = (option: "credit-card" | "bridge") => {
     setIsOptionsModalOpen(false);
@@ -47,7 +49,7 @@ const AssetsList = ({ loading, tokens, hideAddDepositsButton }: AssetsProps) => 
       <div className="p-5 border-b border-gray-100">
         <div className="flex justify-between items-center">
           <h2 className="font-bold text-lg">My Deposits</h2>
-          {!hideAddDepositsButton && (
+          {isDashboard && (
             <Button
               size="sm"
               className="bg-strato-blue hover:bg-strato-blue/90 text-white rounded-md flex items-center gap-1"
@@ -60,120 +62,255 @@ const AssetsList = ({ loading, tokens, hideAddDepositsButton }: AssetsProps) => 
         </div>
       </div>
 
-      <div className="overflow-x-auto">
-        <table className="w-full">
-          <thead>
-            <tr className="bg-gray-50">
-              <th className="text-left text-xs font-medium text-gray-500 uppercase tracking-wider py-3 px-4">
-                Asset
-              </th>
-              <th className="text-right text-xs font-medium text-gray-500 uppercase tracking-wider py-3 px-4">
-                Price
-              </th>
-              <th className="text-right text-xs font-medium text-gray-500 uppercase tracking-wider py-3 px-4">
-                Change
-              </th>
-              <th className="text-right text-xs font-medium text-gray-500 uppercase tracking-wider py-3 px-4">
-                Holdings
-              </th>
-              <th className="text-right text-xs font-medium text-gray-500 uppercase tracking-wider py-3 px-4">
-                Value
-              </th>
-            </tr>
-          </thead>
-          <tbody className="divide-y divide-gray-100">
-            {loading ?
-              <tr className="hover:bg-gray-50 transition-colors">
-                <td colSpan={5} className="py-4 px-4 whitespace-nowrap w-full">
-                  <div className="w-full flex justify-center items-center h-16">
-                    <div className="animate-spin rounded-full h-5 w-5 border-t-2 border-b-2 border-primary"></div>
-                  </div>
-                </td>
+      <div>
+        <div className="p-4 text-right border-t border-gray-100 flex justify-between">
+          <span className="font-bold">Earning Assets</span>
+          <a
+            href="/dashboard/deposits"
+            className="text-sm text-blue-500 hover:text-blue-600 flex items-center justify-end"
+          >
+            View All <ArrowUpRight size={14} className="ml-1" />
+          </a>
+        </div>
+        <div className="overflow-x-auto">
+          <table className="w-full">
+            <thead>
+              <tr className="bg-gray-50">
+                <th className="w-[28%] text-left text-xs font-medium text-gray-500 uppercase tracking-wider py-3 px-4">
+                  Asset
+                </th>
+                <th className="w-[18%] text-right text-xs font-medium text-gray-500 uppercase tracking-wider py-3 px-4">
+                  Price
+                </th>
+                <th className="w-[18%] text-right text-xs font-medium text-gray-500 uppercase tracking-wider py-3 px-4">
+                  Change
+                </th>
+                <th className="w-[18%] text-right text-xs font-medium text-gray-500 uppercase tracking-wider py-3 px-4">
+                  Holdings
+                </th>
+                <th className="w-[18%] text-right text-xs font-medium text-gray-500 uppercase tracking-wider py-3 px-4">
+                  Value
+                </th>
               </tr>
-              : tokens.length > 0 ? (
-                tokens.slice(0, 5).map((asset, index) => (
-                  <tr key={index} className="hover:bg-gray-50 transition-colors">
-                    <td className="py-4 px-4 whitespace-nowrap">
-                      <div className="flex items-center">
-                        <div className="ml-3">
-                          <p className="font-medium text-gray-900">
-                            {asset?.token?._name || ""}
-                          </p>
-                          <p className="text-gray-500 text-xs">
-                            {asset?.token?._symbol || ""}
-                          </p>
+            </thead>
+            <tbody className="divide-y divide-gray-100">
+              {loading ?
+                <tr className="hover:bg-gray-50 transition-colors">
+                  <td colSpan={5} className="py-4 px-4 whitespace-nowrap w-full">
+                    <div className="w-full flex justify-center items-center h-16">
+                      <div className="animate-spin rounded-full h-5 w-5 border-t-2 border-b-2 border-primary"></div>
+                    </div>
+                  </td>
+                </tr>
+                : tokens.length > 0 ? (
+                  tokens.slice(0, 4).map((asset, index) => (
+                    <tr key={index} className="hover:bg-gray-50 transition-colors">
+                      <td className="py-4 px-4 whitespace-nowrap">
+                        <div className="flex items-center">
+                          <div className="ml-3">
+                            <p className="font-medium text-gray-900">
+                              {asset?.token?._name || ""}
+                            </p>
+                            <p className="text-gray-500 text-xs">
+                              {asset?.token?._symbol || ""}
+                            </p>
+                          </div>
                         </div>
-                      </div>
-                    </td>
-                    <td className="py-4 px-4 whitespace-nowrap text-right">
-                      <p className="font-medium text-gray-900">
-                        {!asset?.["price"] ? "-" : 
-                          `$${parseFloat(formatUnits(BigInt(asset.price), 18)).toLocaleString(undefined, {
-                            minimumFractionDigits: 2,
-                            maximumFractionDigits: 2,
-                          })}`
-                        }
-                      </p>
-                    </td>
-                    <td className="py-4 px-4 whitespace-nowrap text-right">
-                      <div
-                        className={`inline-flex items-center px-2 py-0.5 rounded-full text-xs font-medium ${asset?.["change"] >= 0
-                          ? "bg-green-50 text-green-600"
-                          : "bg-red-50 text-red-600"
-                          }`}
-                      >
-                        {asset?.["change"] !== undefined
-                          ? `${asset?.["change"] >= 0 ? "+" : ""}${asset?.["change"]
-                          }%`
-                          : "-"}
-                      </div>
-                    </td>
-                    <td className="py-4 px-4 whitespace-nowrap text-right">
-                      <p className="font-medium text-gray-900">
-                        {!asset?.balance ? "-" :
-                          parseFloat(formatUnits(BigInt(asset.balance), 18)).toLocaleString(undefined, {
-                            minimumFractionDigits: 1,
-                            maximumFractionDigits: 4,
-                          })
-                        }
-                      </p>
-                    </td>
-                    <td className="py-4 px-4 whitespace-nowrap text-right">
-                      <p className="font-medium text-gray-900">
-                        {!asset?.["price"] || !asset?.balance ? "-" :
-                          `$${(parseFloat(formatUnits(BigInt(asset.price), 18)) * 
-                           parseFloat(formatUnits(BigInt(asset.balance), 18))
-                          ).toLocaleString(undefined, {
-                            minimumFractionDigits: 2,
-                            maximumFractionDigits: 2,
-                          })}`
-                        }
-                      </p>
-                    </td>
-                  </tr>
-                ))) :
-                (
+                      </td>
+                      <td className="py-4 px-4 whitespace-nowrap text-right">
+                        <p className="font-medium text-gray-900">
+                          {!asset?.["price"] ? "-" :
+                            `$${parseFloat(formatUnits(BigInt(asset.price), 18)).toLocaleString(undefined, {
+                              minimumFractionDigits: 2,
+                              maximumFractionDigits: 2,
+                            })}`
+                          }
+                        </p>
+                      </td>
+                      <td className="py-4 px-4 whitespace-nowrap text-right">
+                        <div
+                          className={`inline-flex items-center px-2 py-0.5 rounded-full text-xs font-medium ${asset?.["change"] >= 0
+                            ? "bg-green-50 text-green-600"
+                            : "bg-red-50 text-red-600"
+                            }`}
+                        >
+                          {asset?.["change"] !== undefined
+                            ? `${asset?.["change"] >= 0 ? "+" : ""}${asset?.["change"]
+                            }%`
+                            : "-"}
+                        </div>
+                      </td>
+                      <td className="py-4 px-4 whitespace-nowrap text-right">
+                        <p className="font-medium text-gray-900">
+                          {!asset?.balance ? "-" :
+                            parseFloat(formatUnits(BigInt(asset.balance), 18)).toLocaleString(undefined, {
+                              minimumFractionDigits: 1,
+                              maximumFractionDigits: 4,
+                            })
+                          }
+                        </p>
+                      </td>
+                      <td className="py-4 px-4 whitespace-nowrap text-right">
+                        <p className="font-medium text-gray-900">
+                          {!asset?.["price"] || !asset?.balance ? "-" :
+                            `$${(parseFloat(formatUnits(BigInt(asset.price), 18)) *
+                              parseFloat(formatUnits(BigInt(asset.balance), 18))
+                            ).toLocaleString(undefined, {
+                              minimumFractionDigits: 2,
+                              maximumFractionDigits: 2,
+                            })}`
+                          }
+                        </p>
+                      </td>
+                    </tr>
+                  ))) :
+                  (
+                    <tr className="hover:bg-gray-50 transition-colors">
+                      <td colSpan={5} className="py-4 px-4 whitespace-nowrap w-full">
+                        <div className="w-full flex justify-center items-center h-16">
+                          <div>No data to show</div>
+                        </div>
+                      </td>
+                    </tr>
+                  )
+              }
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      {isDashboard && <div>
+        <div className="p-4 text-right border-t border-gray-100 flex justify-between">
+          <span className="font-bold">Non earning Assets</span>
+          <div className="flex gap-4">
+            <button
+              onClick={() => setShowTable((prev) => !prev)}
+              className="text-sm bg-strato-blue hover:bg-strato-blue/90 text-white px-3 py-1 rounded"
+            >
+              <div className="flex gap-1 justify-center items-center">
+                {showTable ? <ArrowUp size={20}/> : <ArrowDown size={20} />}
+                <span>{showTable ? "Hide" : "Show"}</span>
+              </div>
+            </button>
+            <a
+              href="/dashboard/deposits"
+              className="text-sm text-blue-500 hover:text-blue-600 flex items-center justify-end"
+            >
+              View All <ArrowUpRight size={14} className="ml-1" />
+            </a>
+          </div>
+        </div>
+        <div
+          className={`transition-all duration-300 ease-in-out overflow-hidden ${showTable ? "max-h-[2000px] opacity-100" : "max-h-0 opacity-0"
+            }`}
+        >
+          <div className="overflow-x-auto">
+            <table className="w-full">
+              <thead>
+                <tr className="bg-gray-50">
+                  <th className="w-[28%] text-left text-xs font-medium text-gray-500 uppercase tracking-wider py-3 px-4">
+                    Asset
+                  </th>
+                  <th className="w-[18%] text-right text-xs font-medium text-gray-500 uppercase tracking-wider py-3 px-4">
+                    Price
+                  </th>
+                  <th className="w-[18%] text-right text-xs font-medium text-gray-500 uppercase tracking-wider py-3 px-4">
+                    Change
+                  </th>
+                  <th className="w-[18%] text-right text-xs font-medium text-gray-500 uppercase tracking-wider py-3 px-4">
+                    Holdings
+                  </th>
+                  <th className="w-[18%] text-right text-xs font-medium text-gray-500 uppercase tracking-wider py-3 px-4">
+                    Value
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-gray-100">
+                {loading ?
                   <tr className="hover:bg-gray-50 transition-colors">
                     <td colSpan={5} className="py-4 px-4 whitespace-nowrap w-full">
                       <div className="w-full flex justify-center items-center h-16">
-                        <div>No data to show</div>
+                        <div className="animate-spin rounded-full h-5 w-5 border-t-2 border-b-2 border-primary"></div>
                       </div>
                     </td>
                   </tr>
-                )
-            }
-          </tbody>
-        </table>
-      </div>
-
-      <div className="p-4 text-right border-t border-gray-100">
-        <a
-          href="/dashboard/deposits"
-          className="text-sm text-blue-500 hover:text-blue-600 flex items-center justify-end"
-        >
-          View All <ArrowUpRight size={14} className="ml-1" />
-        </a>
-      </div>
+                  : inActiveTokens.length > 0 ? (
+                    inActiveTokens.slice(0, 4).map((asset, index) => (
+                      <tr key={index} className="hover:bg-gray-50 transition-colors">
+                        <td className="py-4 px-4 whitespace-nowrap">
+                          <div className="flex items-center">
+                            <div className="ml-3">
+                              <p className="font-medium text-gray-900">
+                                {asset?.token?._name || ""}
+                              </p>
+                              <p className="text-gray-500 text-xs">
+                                {asset?.token?._symbol || ""}
+                              </p>
+                            </div>
+                          </div>
+                        </td>
+                        <td className="py-4 px-4 whitespace-nowrap text-right">
+                          <p className="font-medium text-gray-900">
+                            {!asset?.["price"] ? "-" :
+                              `$${parseFloat(formatUnits(BigInt(asset.price), 18)).toLocaleString(undefined, {
+                                minimumFractionDigits: 2,
+                                maximumFractionDigits: 2,
+                              })}`
+                            }
+                          </p>
+                        </td>
+                        <td className="py-4 px-4 whitespace-nowrap text-right">
+                          <div
+                            className={`inline-flex items-center px-2 py-0.5 rounded-full text-xs font-medium ${asset?.["change"] >= 0
+                              ? "bg-green-50 text-green-600"
+                              : "bg-red-50 text-red-600"
+                              }`}
+                          >
+                            {asset?.["change"] !== undefined
+                              ? `${asset?.["change"] >= 0 ? "+" : ""}${asset?.["change"]
+                              }%`
+                              : "-"}
+                          </div>
+                        </td>
+                        <td className="py-4 px-4 whitespace-nowrap text-right">
+                          <p className="font-medium text-gray-900">
+                            {!asset?.balance ? "-" :
+                              parseFloat(formatUnits(BigInt(asset.balance), 18)).toLocaleString(undefined, {
+                                minimumFractionDigits: 1,
+                                maximumFractionDigits: 4,
+                              })
+                            }
+                          </p>
+                        </td>
+                        <td className="py-4 px-4 whitespace-nowrap text-right">
+                          <p className="font-medium text-gray-900">
+                            {!asset?.["price"] || !asset?.balance ? "-" :
+                              `$${(parseFloat(formatUnits(BigInt(asset.price), 18)) *
+                                parseFloat(formatUnits(BigInt(asset.balance), 18))
+                              ).toLocaleString(undefined, {
+                                minimumFractionDigits: 2,
+                                maximumFractionDigits: 2,
+                              })}`
+                            }
+                          </p>
+                        </td>
+                      </tr>
+                    ))) :
+                    (
+                      <tr className="hover:bg-gray-50 transition-colors">
+                        <td colSpan={5} className="py-4 px-4 whitespace-nowrap w-full">
+                          <div className="w-full flex justify-center items-center h-16">
+                            <div>No data to show</div>
+                          </div>
+                        </td>
+                      </tr>
+                    )
+                }
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>}
 
       <DepositOptionsModal
         isOpen={isOptionsModalOpen}
