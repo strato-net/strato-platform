f2cca6480197a34c6680492a924543bbc554d10c
Bob Summerwill <bob@summerwill.net>
2025-07-10 20:33:03 -0400

    We were missing the header element for the history page

--- mercata/ui/src/pages/History.tsx
+++ mercata/ui/src/pages/History.tsx
@@ -1,10 +1,12 @@
 import DashboardSidebar from "../components/dashboard/DashboardSidebar";
+import DashboardHeader from "../components/dashboard/DashboardHeader";
 import HistorySection from "../components/dashboard/HistorySection";
 
 const History = () => (
   <div className="min-h-screen bg-gray-50 flex">
     <DashboardSidebar />
     <div className="flex-1 ml-64">
+      <DashboardHeader title="History" />
       <HistorySection />
     </div>
   </div>
