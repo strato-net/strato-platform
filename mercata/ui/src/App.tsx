cc10421fc8a5890ea5cdfdf51dfa81385267d8c1
Michael Tan <tanmichael74@yahoo.com>
2025-07-23 23:42:16 -0400

    Move UsdstBalanceBox component to layouts directory and update import path

--- mercata/ui/src/App.tsx
+++ mercata/ui/src/App.tsx
@@ -1,6 +1,6 @@
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import UsdstBalanceBox from "@/components/ui/UsdstBalanceBox";
+import UsdstBalanceBox from "@/components/layouts/UsdstBalanceBox";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { WagmiProvider } from "wagmi";
