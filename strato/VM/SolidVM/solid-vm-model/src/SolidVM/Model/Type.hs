08019239bfad6e2f0d69e6ecd44168385d6f26c9
Dustin Norwood <dnorwood2010@aol.com>
2025-08-11 11:31:40 -0400

    Simplified maybeEq

--- strato/VM/SolidVM/solid-vm-model/src/SolidVM/Model/Type.hs
+++ strato/VM/SolidVM/solid-vm-model/src/SolidVM/Model/Type.hs
@@ -10,6 +10,7 @@ import Control.Lens (mapped, (&), (?~))
 import Data.Aeson hiding (Array, String)
 import Data.Binary
 import Data.Int (Int32)
+import Data.Maybe (fromMaybe)
 import Data.Swagger
 import GHC.Generics
 import qualified Generic.Random as GR
@@ -40,7 +41,7 @@ data Type
   deriving (Eq, Show, Generic, NFData)
 
 maybeEq :: Eq a => Maybe a -> Maybe a -> Bool
-maybeEq a b = maybe True (uncurry (==)) ((,) <$> a <*> b)
+maybeEq a b = fromMaybe True $ (==) <$> a <*> b
 
 typeEquals :: Type -> Type -> Bool
 typeEquals (Int s1 b1) (Int s2 b2) = s1 `maybeEq` s2 && b1 `maybeEq` b2
