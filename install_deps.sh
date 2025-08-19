7f16ff1ddbb91fd5cfe4da26d63ce3612013f101
nikitamendelbaum <nikitamendelbaum@users.noreply.github.com>
2025-08-20 00:29:00 +0700

    amazon linux deps - add docker-compose

--- install_deps.sh
+++ install_deps.sh
@@ -213,6 +213,13 @@ Linux)
             
             # Install Docker
             sudo dnf install -q -y docker
+            sudo systemctl enable docker
+            sudo systemctl start docker
+            # Docker-compose
+            DOCKER_CONFIG=/usr/local/lib/docker
+            sudo mkdir -p $DOCKER_CONFIG/cli-plugins
+            sudo curl -SL https://github.com/docker/compose/releases/download/v2.36.0/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+            sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
             
             # Install Haskell Stack
             sudo dnf install -q -y \
