54e867800c938ff6b4b11689fbf7fff06f5c9404
aariya50 <amarj003@umn.edu>
2025-08-19 14:08:27 -0400

    Refactor import statements in DepositRouter.sol to use direct paths instead of relative paths for OpenZeppelin contracts.

--- mercata/contracts/concrete/Bridge/DepositRouter.sol
+++ mercata/contracts/concrete/Bridge/DepositRouter.sol
@@ -1,10 +1,10 @@
 pragma solidity ^0.8.26;
 
-import "../../node_modules/@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "../../node_modules/@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
-import "../../node_modules/@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
-import "../../node_modules/@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "../../node_modules/@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 contract DepositRouter is
     Initializable,
