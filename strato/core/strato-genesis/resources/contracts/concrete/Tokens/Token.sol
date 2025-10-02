8c51084c114e664d1d0afbe53be675a3d601479b
Dustin Norwood <dnorwood2010@aol.com>
2025-10-02 01:47:50 -0400

    amount -> value

--- strato/core/strato-genesis/resources/contracts/concrete/Tokens/Token.sol
+++ strato/core/strato-genesis/resources/contracts/concrete/Tokens/Token.sol
@@ -136,12 +136,12 @@ contract record Token is ERC20, Ownable, TokenMetadata, Pausable {
         return customDecimals;
     }
 
-    function transfer(address to, uint256 amount) public override whenNotPausedOrOwner returns (bool) {
-        return super.transfer(to, amount);
+    function transfer(address to, uint256 value) public override whenNotPausedOrOwner returns (bool) {
+        return super.transfer(to, value);
     }
 
-    function transferFrom(address from, address to, uint256 amount) public override whenNotPausedOrOwner returns (bool) {
-        return super.transferFrom(from, to, amount);
+    function transferFrom(address from, address to, uint256 value) public override whenNotPausedOrOwner returns (bool) {
+        return super.transferFrom(from, to, value);
     }
 
     function _update(address from, address to, uint256 value) internal override {
