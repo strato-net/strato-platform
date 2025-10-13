bac918302e039a89583722269096f7939de611d9
Dustin Norwood <dnorwood2010@aol.com>
2025-10-13 11:56:53 -0400

    Added fastForward.test.sol

--- mercata/contracts/tests/fastForward.test.sol
+++ mercata/contracts/tests/fastForward.test.sol
@@ -1,22 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
-contract Describe_FastForward {
+import "../concrete/BaseCodeCollection.sol";
 
-  function beforeAll() {
-  }
+/// @title Test to verify fastForward behavior in beforeEach
+/// @notice This test demonstrates a bug where fastForward in beforeEach doesn't persist to test execution
+contract Describe_BeforeEach_FastForward_Bug {
 
-  function beforeEach() {
-  }
+    constructor() {
+    }
 
-  function it_can_call_fastForward() {
-    log("Testing fastForward function");
-    uint256 before = block.timestamp;
-    fastForward(100);
-    uint256 after = block.timestamp;
-    log("Before timestamp", before);
-    log("After timestamp", after);
-    require(after == before + 100, "FastForward did not work");
-  }
+    function beforeEach() public {
+        // Fast forward time by 10 seconds
+        fastForward(10);
+    }
 
+    /// @notice This test will FAIL if fastForward in beforeEach doesn't work
+    /// @dev Expected: block.timestamp should be 10 after fastForward(10) in beforeEach
+    ///      Actual (if bug exists): block.timestamp will be 0
+    function it_should_have_block_timestamp_of_10_after_beforeEach() public {
+        // If fastForward in beforeEach worked, block.timestamp should be 10
+        require(block.timestamp == 10, "block.timestamp should be 10 after fastForward(10) in beforeEach. Got: " + string(block.timestamp));
+    }
+
+    /// @notice This test will PASS because fastForward is called within the test
+    /// @dev This proves that fastForward works when called inside the test function
+    function it_should_have_block_timestamp_of_30_when_called_in_test() public {
+        // Fast forward in the test itself
+        fastForward(10);
+
+        // Now block.timestamp should be 30
+        require(block.timestamp == 30, "block.timestamp should be 30 after fastForward(10) in test. Got: " + string(block.timestamp));
+    }
 }
