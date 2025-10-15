9f4ae4939fcb41029572a44fa1144bbc5f7106f7
Tanuj Soni <tanuj@blockapps.net>
2025-10-15 13:41:41 +0530

    hextext ui fixes

--- smd-ui/src/components/NumberCard/index.js
+++ smd-ui/src/components/NumberCard/index.js
@@ -7,7 +7,18 @@ class NumberCard extends Component {
   render() {
     let classes = 'pt-card pt-dark pt-elevation-2 ';
     classes += this.props.mode ? this.props.mode : 'neutral';
-    let textSize = this.props.textSize ? this.props.textSize : 'h2'
+    let textSize = this.props.textSize ? this.props.textSize : 'h2';
+    
+    // Check if the number prop contains a HexText component
+    const isHexText = this.props.number && 
+      (typeof this.props.number === 'object' && 
+       this.props.number.type && 
+       this.props.number.type.name === 'HexText');
+    
+    // Use different alignment for HexText vs regular numbers
+    const numberAlignment = isHexText ? 'text-left' : 'text-right';
+    const descriptionAlignment = 'text-right'; // Always keep description right-aligned
+    
     return (
       <div className={classes}>
         <div className="row">
@@ -15,10 +26,10 @@ class NumberCard extends Component {
             <i className={'fa ' + this.props.iconClass + ' fa-5x smd-pad-8'} aria-hidden="true"></i>
           </div>
           <div className="col-xs-8">
-            <div className={`${textSize} text-right`}>
+            <div className={`${textSize} ${numberAlignment}`}>
               <strong>{this.props.number}</strong>
             </div>
-            <div className="h4 text-right desc">
+            <div className={`h4 ${descriptionAlignment} desc`}>
               {this.props.description}
             </div>
           </div>
