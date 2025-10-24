366842ce3d3a2e1c7b47a41719006ffe4e06d51f
nikitamendelbaum <nikitamendelbaum@users.noreply.github.com>
2025-10-24 22:57:19 +0700

    buildtest restart command update

--- upgrade_testnet.sh
+++ upgrade_testnet.sh
@@ -1,4 +1,4 @@
 for i in {1..5}; do scp "$1" mercata-testnet-node$i:/datadrive/testnet/strato-getting-started/docker-compose.yml && ssh mercata-testnet-node$i "cd /datadrive/testnet/strato-getting-started && sudo ./strato --pull && sudo ./strato --wipe && sudo ./strato-run.sh" && echo "Completed node $i" && read -r blah; done
 
-scp "$1" mercata-testnet-buildtest:/home/ec2-user/strato-getting-started-symlink/docker-compose.yml && ssh mercata-testnet-buildtest "cd /home/ec2-user/strato-getting-started-symlink && sudo ./strato --pull && sudo ./strato --wipe && sudo ./strato-run.sh"
+scp "$1" mercata-testnet-buildtest:/home/ec2-user/bootstrap-docker-symlink/docker-compose.yml && ssh mercata-testnet-buildtest "cd /home/ec2-user/bootstrap-docker-symlink && sudo ./strato --pull && sudo ./strato --wipe && sudo ./strato-run.sh"
 scp "$1" mercata-testnet-node-app:/datadrive/testnet/strato-getting-started/docker-compose.yml && ssh mercata-testnet-node-app "cd /datadrive/testnet/strato-getting-started && sudo ./strato --pull && sudo ./strato --wipe && sudo ./strato-run.sh"
