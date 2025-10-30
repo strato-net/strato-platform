7cec517065949739e1babc74ee0125f4be72202d
EncodePanda <paul.szulc@gmail.com>
2025-10-30 14:41:05 +0100

    Proxify UserRegistry contract

--- strato/core/strato-genesis/src/Blockchain/GenesisBlocks/Contracts/UserRegistry.hs
+++ strato/core/strato-genesis/src/Blockchain/GenesisBlocks/Contracts/UserRegistry.hs
@@ -9,28 +9,58 @@ import Blockchain.Data.GenesisInfo
 import Blockchain.GenesisBlocks.Contracts.TH
 import Blockchain.Strato.Model.CodePtr
 import qualified Blockchain.Strato.Model.Keccak256 as KECCAK256
+import qualified Data.Aeson as JSON
 import Data.ByteString (ByteString)
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.Text as Text
 import Data.Text.Encoding
+import SolidVM.Model.Storable hiding (size)
+import System.FilePath (takeFileName)
 
 
 
--- | Inserts a User Registry contract into the genesis block with the BlockApps root cert as owner
 insertUserRegistryContract :: GenesisInfo -> GenesisInfo
 insertUserRegistryContract gi =
   gi
-    { addressInfo = initialAccounts ++ [registryAcct],
+    { addressInfo = initialAccounts ++ [logicAcct, proxyAcct],
       codeInfo = initialCode ++ [CodeInfo (decodeUtf8 userRegistryContract) (Just "UserRegistry")]
     }
   where
     initialAccounts = addressInfo gi
     initialCode = codeInfo gi
 
-    registryAcct =
+    logicAddr = 0x71f
+    proxyAddr = 0x720
+    owner = 0x100c
+
+    logicAcct =
       SolidVMContractWithStorage
-        0x720
-        720
+        logicAddr
+        0
         (SolidVMCode "UserRegistry" (KECCAK256.hash userRegistryContract))
-        $ []
+        []
+
+    proxyAcct =
+      SolidVMContractWithStorage
+        proxyAddr
+        720
+        (SolidVMCode "Proxy" (KECCAK256.hash userRegistryContract))
+        [ ("_owner", BAddress owner)
+        , ("logicContract", BAddress logicAddr)
+        ]
+
+embeddedFiles :: [(FilePath, ByteString)]
+embeddedFiles = $(typecheckAndEmbedFiles "resources"
+  [ "contracts/abstract/ERC20/access/Authorizable.sol"
+  , "contracts/abstract/ERC20/access/Ownable.sol"
+  , "contracts/abstract/ERC20/utils/Context.sol"
+  , "contracts/concrete/Admin/AdminRegistry.sol"
+  , "contracts/concrete/Proxy/Proxy.sol"
+  , "strato/UserRegistry.sol"
+  ])
+
+userRegistryContracts :: [[String]]
+userRegistryContracts = map (\(fp, bs) -> [takeFileName fp, Text.unpack $ decodeUtf8 bs]) embeddedFiles
 
 userRegistryContract :: ByteString
-userRegistryContract = $(typecheckAndEmbedFile "resources/strato/UserRegistry.sol")
+userRegistryContract = BL.toStrict $ JSON.encode userRegistryContracts
