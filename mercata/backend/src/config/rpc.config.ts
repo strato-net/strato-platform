37e89764c34cde8471ecc4eef447776a09686926
Adrian <adrian_self@blockapps.net>
2025-12-05 17:33:44 -0500

    Remove polygon; even though wagmi has a default for that, we don't currently use it; other chains should be added to rpc config only when needed

--- mercata/backend/src/config/rpc.config.ts
+++ mercata/backend/src/config/rpc.config.ts
@@ -5,8 +5,6 @@ const fallbackRpcUpstreams: RpcMapping = {
   "1": process.env.RPC_URL_MAINNET_FALLBACK || "https://eth.merkle.io",
   // Sepolia
   "11155111": process.env.RPC_URL_SEPOLIA_FALLBACK || "https://sepolia.drpc.org",
-  // Polygon
-  "137": process.env.RPC_URL_POLYGON_FALLBACK || "https://polygon-rpc.com",
 };
 
 const rpcUpstreams: RpcMapping = {
@@ -14,8 +12,6 @@ const rpcUpstreams: RpcMapping = {
   "1": process.env.RPC_URL_MAINNET || fallbackRpcUpstreams["1"],
   // Sepolia
   "11155111": process.env.RPC_URL_SEPOLIA || fallbackRpcUpstreams["11155111"],
-  // Polygon
-  "137": process.env.RPC_URL_POLYGON || fallbackRpcUpstreams["137"],
 };
 
 export function getRpcUpstream(chainId: string): {upstream: string | undefined, fallback: string | undefined} {
