70126defa2358d5d0811f64476f99ac5cc0e4dc6
Adrian <adrian_self@blockapps.net>
2026-01-06 13:53:05 -0500

    Collateral Management on the Right-Hand Side

--- mercata/ui/src/pages/Borrow.tsx
+++ mercata/ui/src/pages/Borrow.tsx
@@ -277,18 +277,18 @@ const Borrow = () => {
               </CardContent>
             </Card>
 
-            {/* Right Column - Your Position */}
-            <div>
+            {/* Right Column - Your Position and Collateral Management */}
+            <div className="space-y-6">
               <PositionSection loanData={loans} userCollaterals={collateralInfo} />
+              <CollateralManagementTable
+                collateralInfo={collateralInfo}
+                loadingCollateral={loadingCollateral}
+                loans={loans}
+                onSupply={handleSupply}
+                onWithdraw={handleWithdraw}
+              />
             </div>
           </div>
-          <CollateralManagementTable
-            collateralInfo={collateralInfo}
-            loadingCollateral={loadingCollateral}
-            loans={loans}
-            onSupply={handleSupply}
-            onWithdraw={handleWithdraw}
-          />
         </main>
       </div>
 
