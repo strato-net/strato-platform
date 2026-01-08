204074fb58d5c0799033bf11c6b5657f91058638
Adrian <adrian_self@blockapps.net>
2026-01-08 09:29:24 -0500

    remove whitespace

--- mercata/ui/src/components/borrow/BorrowForm.tsx
+++ mercata/ui/src/components/borrow/BorrowForm.tsx
@@ -129,7 +129,7 @@ const BorrowForm = ({ loans, borrowLoading, onBorrow, usdstBalance, voucherBalan
       // Extract collaterals into array, sort them, then build data array in sorted order
       const collateralsArray = Array.from(potentialCollateral.keys());
       sortCollateralAssets(collateralsArray);
-      
+
       for (const collateral of collateralsArray) {
         const price = BigInt(collateral.assetPrice ?? "0");
         const decimals = BigInt(10) ** BigInt(collateral.customDecimals ?? 18);
