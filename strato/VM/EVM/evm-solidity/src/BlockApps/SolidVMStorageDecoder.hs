b3e7a73b7e1134f1eabbcf13c626d9a13268293e
Jamshid <jim@blockapps.net>
2026-01-26 17:08:41 -0500

    Fix storage decoder crash when nested paths follow BDefault
    BDefault at intermediate storage paths (e.g., struct placeholders)
    gets converted to SimpleValue(address 0x0) since type info isn't
    available at decode time. When later paths reveal nested structure,
    the decoder crashed trying to access fields on an address.
    
    This is a workaround - proper fix requires threading type info
    through the decoder. For now, convert SimpleValue to struct when
    field access is attempted.

--- strato/VM/EVM/evm-solidity/src/BlockApps/SolidVMStorageDecoder.hs
+++ strato/VM/EVM/evm-solidity/src/BlockApps/SolidVMStorageDecoder.hs
@@ -177,6 +177,10 @@ applyDelta' [Field n] bv _ = do
 applyDelta' sp bv (ValueArraySentinel {}) = Right $ constructFromNothing' sp bv
 applyDelta' sp@[Index _] BDefault _ = Right $ constructFromNothing' sp BDefault
 applyDelta' sp@[Index _] _ _ = Right $ constructFromNothing' sp BDefault
+-- Handle case where BDefault created a SimpleValue but we now have nested fields
+applyDelta' (Field n : sp) bv (SimpleValue _) = do
+  n' <- first (UnicodeError n) $ decodeUtf8' n
+  Right . ValueStruct . M.singleton n' $ constructFromNothing' sp bv
 applyDelta' sp b s = Left $ TypeMismatch (StoragePath sp) b s
 
 constructFromNothing :: StoragePath -> BasicValue -> V.Value
