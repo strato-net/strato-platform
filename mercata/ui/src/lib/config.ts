85a99e1cd448682980071aa62ff0cac4e5fadb00
HT7x <ht7.bhr@gmail.com>
2026-02-23 09:59:51 -0500

    Added onramp page to the UI

--- mercata/ui/src/lib/config.ts
+++ mercata/ui/src/lib/config.ts
@@ -2,9 +2,10 @@ import { api } from "./axios";
 
 export interface ConfigData {
   projectId: string;
+  stripePublishableKey: string | null;
 }
 
 export const getConfig = async (): Promise<ConfigData> => {
   const response = await api.get("/config");
   return response.data.data;
-}; 
+};
