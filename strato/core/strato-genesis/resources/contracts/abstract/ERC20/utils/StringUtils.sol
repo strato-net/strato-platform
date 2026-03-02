72cbd4f8c77169654d83d19aa05a2dcd09a5f9ff
Dustin Norwood <dnorwood2010@aol.com>
2026-03-02 12:07:19 -0500

    Moved StringUtils changes to mercata/contracts/

--- strato/core/strato-genesis/resources/contracts/abstract/ERC20/utils/StringUtils.sol
+++ strato/core/strato-genesis/resources/contracts/abstract/ERC20/utils/StringUtils.sol
@@ -28,20 +28,6 @@ library BytesUtils {
         return dst;
     }
 
-    function substring(bytes b, uint start) internal pure returns (bytes) {
-        return substring(b, start, b.length);
-    }
-
-    function substring(bytes b, uint start, uint end) internal pure returns (bytes) {
-        require(end > start, "substring: end index must be greater than starting index");
-        require(end <= b.length, "substring: end index out of bounds");
-        bytes ret = new bytes(end - start);
-        for (uint i = start; i < end; i++) {
-            ret[i - start] = b[i];
-        }
-        return ret;
-    }
-
     function b16decode(bytes b) internal pure returns (bytes) {
         bool isEven = b.length % 2 == 0;
         uint offset = isEven ? 0 : 1;
