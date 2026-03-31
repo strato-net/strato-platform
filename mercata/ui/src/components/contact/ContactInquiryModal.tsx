e12d5affbf22c05e3aa7dee047116ab3e6f4d409
Maxim Voyevoda <maximvoyevoda@gmail.com>
2026-03-31 17:20:29 -0400

    change text

--- mercata/ui/src/components/contact/ContactInquiryModal.tsx
+++ mercata/ui/src/components/contact/ContactInquiryModal.tsx
@@ -126,7 +126,7 @@ const ContactInquiryModal = ({ open, onOpenChange }: ContactInquiryModalProps) =
                   Sending...
                 </>
               ) : (
-                "Send message"
+                "Send Inquiry"
               )}
             </Button>
           </form>
