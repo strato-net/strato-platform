a7bf9fe0b2df4e179b802e08ef8c64201da7030a
HT7x <ht7.bhr@gmail.com>
2026-07-12 22:14:00 -0400

    removed deafult config

--- mercata/ui/public/config.js
+++ mercata/ui/public/config.js
@@ -1,8 +1,8 @@
 // Runtime configuration (generated at container startup in production)
 // For local development, this file provides default values
 window.ENV = {
-  CHAIN_ID: 195049586845898,
-  NETWORK_NAME: "Helium",
+  CHAIN_ID: null,
+  NETWORK_NAME: "",
   LUCKY_ORANGE_SITE_ID: "",
   GOOGLE_ANALYTICS_ID: ""
 };
